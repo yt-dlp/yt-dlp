61e76c1e5fc6df3ebb4f728c5cc999349d36c55d
GreyAlien502 <greyalien502@gmail.com>
2020-10-27 02:20:18 +0000

    simplify second page fetch
    Co-authored-by: Merval <merval@users.noreply.github.com>

--- youtube_dlc/extractor/tiktok.py
+++ youtube_dlc/extractor/tiktok.py
@@ -134,7 +134,7 @@ class TikTokIE(TikTokBaseIE):
         video_id = self._match_id(url)
 
         # If we only call once, we get a 403 when downlaoding the video.
-        webpage = self._download_webpage(url, video_id, note='Downloading video webpage')
+        self._download_webpage(url, video_id)
         webpage = self._download_webpage(url, video_id, note='Downloading video webpage')
         json_string = self._search_regex(
             r'id=\"__NEXT_DATA__\"\s+type=\"application\/json\"\s*[^>]+>\s*(?P<json_string_ld>[^<]+)',
