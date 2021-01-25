yt-dlp/yt-dlp

commit 63be1aab2f6b6a99f289663ffd935e311aff5556

Deprecate unnecessary aliases in `formatSort`
(I should never have made so many aliases in the first-place)
The aliases remain functional for backward compatability, but will be left undocumented

youtube_dlc/extractor/common.py
@@ -1375,8 +1375,8 @@ class InfoExtractor(object):
                        'order': ['vp9', '(h265|he?vc?)', '(h264|avc)', 'vp8', '(mp4v|h263)', 'theora', '', None, 'none']},
             'acodec': {'type': 'ordered', 'regex': True,
                        'order': ['opus', 'vorbis', 'aac', 'mp?4a?', 'mp3', 'e?a?c-?3', 'dts', '', None, 'none']},
-            'protocol': {'type': 'ordered', 'regex': True,
-                         'order': ['(ht|f)tps', '(ht|f)tp$', 'm3u8.+', 'm3u8', '.*dash', '', 'mms|rtsp', 'none', 'f4']},
+            'proto': {'type': 'ordered', 'regex': True,
+                      'order': ['(ht|f)tps', '(ht|f)tp$', 'm3u8.+', 'm3u8', '.*dash', '', 'mms|rtsp', 'none', 'f4']},
             'vext': {'type': 'ordered', 'field': 'video_ext',
                      'order': ('mp4', 'webm', 'flv', '', 'none'),
                      'order_free': ('webm', 'mp4', 'flv', '', 'none')},
@@ -1384,14 +1384,14 @@ class InfoExtractor(object):
                      'order': ('m4a', 'aac', 'mp3', 'ogg', 'opus', 'webm', '', 'none'),
                      'order_free': ('opus', 'ogg', 'webm', 'm4a', 'mp3', 'aac', '', 'none')},
             'hidden': {'visible': False, 'forced': True, 'type': 'extractor', 'max': -1000},
-            'extractor_preference': {'priority': True, 'type': 'extractor'},
-            'has_video': {'priority': True, 'field': 'vcodec', 'type': 'boolean', 'not_in_list': ('none',)},
-            'has_audio': {'field': 'acodec', 'type': 'boolean', 'not_in_list': ('none',)},
-            'language_preference': {'priority': True, 'convert': 'ignore'},
+            'ie_pref': {'priority': True, 'type': 'extractor'},
+            'hasvid': {'priority': True, 'field': 'vcodec', 'type': 'boolean', 'not_in_list': ('none',)},
+            'hasaud': {'field': 'acodec', 'type': 'boolean', 'not_in_list': ('none',)},
+            'lang': {'priority': True, 'convert': 'ignore'},
             'quality': {'priority': True, 'convert': 'float_none'},
             'filesize': {'convert': 'bytes'},
-            'filesize_approx': {'convert': 'bytes'},
-            'format_id': {'convert': 'string'},
+            'fs_approx': {'convert': 'bytes'},
+            'id': {'convert': 'string'},
             'height': {'convert': 'float_none'},
             'width': {'convert': 'float_none'},
             'fps': {'convert': 'float_none'},
@@ -1399,32 +1399,42 @@ class InfoExtractor(object):
             'vbr': {'convert': 'float_none'},
             'abr': {'convert': 'float_none'},
             'asr': {'convert': 'float_none'},
-            'source_preference': {'convert': 'ignore'},
+            'source': {'convert': 'ignore'},
+
             'codec': {'type': 'combined', 'field': ('vcodec', 'acodec')},
-            'bitrate': {'type': 'combined', 'field': ('tbr', 'vbr', 'abr'), 'same_limit': True},
-            'filesize_estimate': {'type': 'combined', 'same_limit': True, 'field': ('filesize', 'filesize_approx')},
-            'extension': {'type': 'combined', 'field': ('vext', 'aext')},
-            'dimension': {'type': 'multiple', 'field': ('height', 'width'), 'function': min},  # not named as 'resolution' because such a field exists
-            'res': {'type': 'alias', 'field': 'dimension'},
-            'ext': {'type': 'alias', 'field': 'extension'},
-            'br': {'type': 'alias', 'field': 'bitrate'},
+            'br': {'type': 'combined', 'field': ('tbr', 'vbr', 'abr'), 'same_limit': True},
+            'size': {'type': 'combined', 'same_limit': True, 'field': ('filesize', 'fs_approx')},
+            'ext': {'type': 'combined', 'field': ('vext', 'aext')},
+            'res': {'type': 'multiple', 'field': ('height', 'width'), 'function': min},
+
+            # Most of these exist only for compatibility reasons
+            'dimension': {'type': 'alias', 'field': 'res'},
+            'resolution': {'type': 'alias', 'field': 'res'},
+            'extension': {'type': 'alias', 'field': 'ext'},
+            'bitrate': {'type': 'alias', 'field': 'br'},
             'total_bitrate': {'type': 'alias', 'field': 'tbr'},
             'video_bitrate': {'type': 'alias', 'field': 'vbr'},
             'audio_bitrate': {'type': 'alias', 'field': 'abr'},
             'framerate': {'type': 'alias', 'field': 'fps'},
-            'lang': {'type': 'alias', 'field': 'language_preference'},  # not named as 'language' because such a field exists
-            'proto': {'type': 'alias', 'field': 'protocol'},
-            'source': {'type': 'alias', 'field': 'source_preference'},
-            'size': {'type': 'alias', 'field': 'filesize_estimate'},
+            'language_preference': {'type': 'alias', 'field': 'lang'},  # not named as 'language' because such a field exists
+            'protocol': {'type': 'alias', 'field': 'proto'},
+            'source_preference': {'type': 'alias', 'field': 'source'},
+            'filesize_approx': {'type': 'alias', 'field': 'fs_approx'},
+            'filesize_estimate': {'type': 'alias', 'field': 'size'},
             'samplerate': {'type': 'alias', 'field': 'asr'},
             'video_ext': {'type': 'alias', 'field': 'vext'},
             'audio_ext': {'type': 'alias', 'field': 'aext'},
             'video_codec': {'type': 'alias', 'field': 'vcodec'},
             'audio_codec': {'type': 'alias', 'field': 'acodec'},
-            'video': {'type': 'alias', 'field': 'has_video'},
-            'audio': {'type': 'alias', 'field': 'has_audio'},
-            'extractor': {'type': 'alias', 'field': 'extractor_preference'},
-            'preference': {'type': 'alias', 'field': 'extractor_preference'}}
+            'video': {'type': 'alias', 'field': 'hasvid'},
+            'has_video': {'type': 'alias', 'field': 'hasvid'},
+            'audio': {'type': 'alias', 'field': 'hasaud'},
+            'has_audio': {'type': 'alias', 'field': 'hasaud'},
+            'extractor': {'type': 'alias', 'field': 'ie_pref'},
+            'preference': {'type': 'alias', 'field': 'ie_pref'},
+            'extractor_preference': {'type': 'alias', 'field': 'ie_pref'},
+            'format_id': {'type': 'alias', 'field': 'id'},
+        }
 
         _order = []
 

youtube_dlc/YoutubeDL.py
@@ -886,8 +886,8 @@ class YoutubeDL(object):
 
             try:
                 temp_id = str_or_none(
-                  ie.extract_id(url) if callable(getattr(ie, 'extract_id', None))
-                  else ie._match_id(url))
+                    ie.extract_id(url) if callable(getattr(ie, 'extract_id', None))
+                    else ie._match_id(url))
             except (AssertionError, IndexError, AttributeError):
                 temp_id = None
             if temp_id is not None and self.in_download_archive({'id': temp_id, 'ie_key': ie_key}):

commit f137c99e9fa6c57d5d44e566ff540e2f92b6923d

Fix some fields not sorting correctly
bug introduced by: 63be1aab2f6b6a99f289663ffd935e311aff5556

youtube_dlc/extractor/common.py
@@ -1375,7 +1375,7 @@ class InfoExtractor(object):
                        'order': ['vp9', '(h265|he?vc?)', '(h264|avc)', 'vp8', '(mp4v|h263)', 'theora', '', None, 'none']},
             'acodec': {'type': 'ordered', 'regex': True,
                        'order': ['opus', 'vorbis', 'aac', 'mp?4a?', 'mp3', 'e?a?c-?3', 'dts', '', None, 'none']},
-            'proto': {'type': 'ordered', 'regex': True,
+            'proto': {'type': 'ordered', 'regex': True, 'field': 'protocol',
                       'order': ['(ht|f)tps', '(ht|f)tp$', 'm3u8.+', 'm3u8', '.*dash', '', 'mms|rtsp', 'none', 'f4']},
             'vext': {'type': 'ordered', 'field': 'video_ext',
                      'order': ('mp4', 'webm', 'flv', '', 'none'),
@@ -1384,14 +1384,14 @@ class InfoExtractor(object):
                      'order': ('m4a', 'aac', 'mp3', 'ogg', 'opus', 'webm', '', 'none'),
                      'order_free': ('opus', 'ogg', 'webm', 'm4a', 'mp3', 'aac', '', 'none')},
             'hidden': {'visible': False, 'forced': True, 'type': 'extractor', 'max': -1000},
-            'ie_pref': {'priority': True, 'type': 'extractor'},
+            'ie_pref': {'priority': True, 'type': 'extractor', 'field': 'extractor_preference'},
             'hasvid': {'priority': True, 'field': 'vcodec', 'type': 'boolean', 'not_in_list': ('none',)},
             'hasaud': {'field': 'acodec', 'type': 'boolean', 'not_in_list': ('none',)},
-            'lang': {'priority': True, 'convert': 'ignore'},
+            'lang': {'priority': True, 'convert': 'ignore', 'field': 'language_preference'},
             'quality': {'priority': True, 'convert': 'float_none'},
             'filesize': {'convert': 'bytes'},
-            'fs_approx': {'convert': 'bytes'},
-            'id': {'convert': 'string'},
+            'fs_approx': {'convert': 'bytes', 'field': 'filesize_approx'},
+            'id': {'convert': 'string', 'field': 'format_id'},
             'height': {'convert': 'float_none'},
             'width': {'convert': 'float_none'},
             'fps': {'convert': 'float_none'},
@@ -1399,7 +1399,7 @@ class InfoExtractor(object):
             'vbr': {'convert': 'float_none'},
             'abr': {'convert': 'float_none'},
             'asr': {'convert': 'float_none'},
-            'source': {'convert': 'ignore'},
+            'source': {'convert': 'ignore', 'field': 'source_preference'},
 
             'codec': {'type': 'combined', 'field': ('vcodec', 'acodec')},
             'br': {'type': 'combined', 'field': ('tbr', 'vbr', 'abr'), 'same_limit': True},