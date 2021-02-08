yt-dlp/yt-dlp

commit 1bf540d28b9a53112532aefde12188db5b327891

[sponskrub] Don't raise error when the video does not exist
Eg: `--convert-sub srt --no-download --sponskrub` gave error before

:ci skip dl

youtube_dlc/YoutubeDL.py
@@ -2349,7 +2349,7 @@ class YoutubeDL(object):
                             downloaded.append(fname)
                             partial_success, real_download = dl(fname, new_info)
                             success = success and partial_success
-                        info_dict['__postprocessors'] = postprocessors
+                        info_dict['__postprocessors'].append(postprocessors)
                         info_dict['__files_to_merge'] = downloaded
                         # Even if there were no downloads, it is being merged only now
                         info_dict['__real_download'] = True

youtube_dlc/postprocessor/sponskrub.py
@@ -43,6 +43,10 @@ class SponSkrubPP(PostProcessor):
         if self.path is None:
             return [], information
 
+        filename = information['filepath']
+        if not os.path.exists(encodeFilename(filename)):  # no download
+            return [], information
+
         if information['extractor_key'].lower() != 'youtube':
             self.to_screen('Skipping sponskrub since it is not a YouTube video')
             return [], information
@@ -58,7 +62,6 @@ class SponSkrubPP(PostProcessor):
             if not information.get('__real_download', False):
                 self.report_warning('If sponskrub is run multiple times, unintended parts of the video could be cut out.')
 
-        filename = information['filepath']
         temp_filename = prepend_extension(filename, self._temp_ext)
         if os.path.exists(encodeFilename(temp_filename)):
             os.remove(encodeFilename(temp_filename))

commit efabc161652d2427c2fe5ccff6c944e91ea12ca0

[postprocessor] Fix bug (Closes #62) introduced by: 1bf540d28b9a53112532aefde12188db5b327891
:ci skip dl

youtube_dlc/YoutubeDL.py
@@ -2292,12 +2292,9 @@ class YoutubeDL(object):
                     downloaded = []
                     merger = FFmpegMergerPP(self)
                     if not merger.available:
-                        postprocessors = []
                         self.report_warning('You have requested multiple '
                                             'formats but ffmpeg is not installed.'
                                             ' The formats won\'t be merged.')
-                    else:
-                        postprocessors = [merger]
 
                     def compatible_formats(formats):
                         # TODO: some formats actually allow this (mkv, webm, ogg, mp4), but not all of them.
@@ -2349,7 +2346,8 @@ class YoutubeDL(object):
                             downloaded.append(fname)
                             partial_success, real_download = dl(fname, new_info)
                             success = success and partial_success
-                        info_dict['__postprocessors'].append(postprocessors)
+                        if merger.available:
+                            info_dict['__postprocessors'].append(merger)
                         info_dict['__files_to_merge'] = downloaded
                         # Even if there were no downloads, it is being merged only now
                         info_dict['__real_download'] = True