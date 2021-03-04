yt-dlp/yt-dlp

commit 7f7de7f94dfeacb0eb78cb9487dfcaf5707da381

Allow specifying path in `--external-downloader`

yt_dlp/downloader/external.py
@@ -85,16 +85,16 @@ class ExternalFD(FileDownloader):
         return self.params.get('external_downloader')
 
     @classmethod
-    def available(cls):
-        return check_executable(cls.get_basename(), [cls.AVAILABLE_OPT])
+    def available(cls, path=None):
+        return check_executable(path or cls.get_basename(), [cls.AVAILABLE_OPT])
 
     @classmethod
     def supports(cls, info_dict):
         return info_dict['protocol'] in cls.SUPPORTED_PROTOCOLS
 
     @classmethod
-    def can_download(cls, info_dict):
-        return cls.available() and cls.supports(info_dict)
+    def can_download(cls, info_dict, path=None):
+        return cls.available(path) and cls.supports(info_dict)
 
     def _option(self, command_option, param):
         return cli_option(self.params, command_option, param)

yt_dlp/downloader/__init__.py
@@ -53,7 +53,7 @@ def get_suitable_downloader(info_dict, params={}, default=HttpFD):
     external_downloader = params.get('external_downloader')
     if external_downloader is not None:
         ed = get_external_downloader(external_downloader)
-        if ed.can_download(info_dict):
+        if ed.can_download(info_dict, external_downloader):
             return ed
 
     if protocol.startswith('m3u8'):

yt_dlp/options.py
@@ -650,8 +650,8 @@ def parseOpts(overrideArguments=None):
         '--external-downloader',
         dest='external_downloader', metavar='NAME',
         help=(
-            'Use the specified external downloader. '
-            'Currently supports %s' % ', '.join(list_external_downloaders())))
+            'Name or path of the external downloader to use. '
+            'Currently supports %s (Recommended: aria2c)' % ', '.join(list_external_downloaders())))
     downloader.add_option(
         '--downloader-args', '--external-downloader-args',
         metavar='NAME:ARGS', dest='external_downloader_args', default={}, type='str',

commit 9e631877f8011a21967577c0044afca163010fe2

[downloader] Fix bug for ffmpeg/httpie Caused by: 7f7de7f94dfeacb0eb78cb9487dfcaf5707da381

yt_dlp/downloader/external.py
@@ -282,8 +282,8 @@ class Aria2cFD(ExternalFD):
 
 class HttpieFD(ExternalFD):
     @classmethod
-    def available(cls):
-        return check_executable('http', ['--version'])
+    def available(cls, path=None):
+        return check_executable(path or 'http', ['--version'])
 
     def _make_cmd(self, tmpfilename, info_dict):
         cmd = ['http', '--download', '--output', tmpfilename, info_dict['url']]
@@ -298,7 +298,7 @@ class FFmpegFD(ExternalFD):
     SUPPORTED_PROTOCOLS = ('http', 'https', 'ftp', 'ftps', 'm3u8', 'rtsp', 'rtmp', 'mms')
 
     @classmethod
-    def available(cls):
+    def available(cls, path=None):  # path is ignored for ffmpeg
         return FFmpegPostProcessor().available
 
     def _call_downloader(self, tmpfilename, info_dict):