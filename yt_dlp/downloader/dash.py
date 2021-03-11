2df3e4013545d49dd14b8be2c73c964daa8c9f6f
shirtjs <2660574+shirt-dev@users.noreply.github.com>
2021-03-11 14:09:03 -0500

    Update dash.py

--- yt_dlp/downloader/dash.py
+++ yt_dlp/downloader/dash.py
@@ -62,45 +62,6 @@ class DashSegmentsFD(FragmentFD):
                 'url': fragment_url,
             })
 
-            if real_downloader:
-                continue
-
-            # # In DASH, the first segment contains necessary headers to
-            # # generate a valid MP4 file, so always abort for the first segment
-            # fatal = i == 0 or not skip_unavailable_fragments
-            # count = 0
-            # while count <= fragment_retries:
-            #     try:
-            #         success, frag_content = self._download_fragment(ctx, fragment_url, info_dict)
-            #         if not success:
-            #             return False
-            #         self._append_fragment(ctx, frag_content)
-            #         break
-            #     except compat_urllib_error.HTTPError as err:
-            #         # YouTube may often return 404 HTTP error for a fragment causing the
-            #         # whole download to fail. However if the same fragment is immediately
-            #         # retried with the same request data this usually succeeds (1-2 attempts
-            #         # is usually enough) thus allowing to download the whole file successfully.
-            #         # To be future-proof we will retry all fragments that fail with any
-            #         # HTTP error.
-            #         count += 1
-            #         if count <= fragment_retries:
-            #             self.report_retry_fragment(err, frag_index, count, fragment_retries)
-            #     except DownloadError:
-            #         # Don't retry fragment if error occurred during HTTP downloading
-            #         # itself since it has own retry settings
-            #         if not fatal:
-            #             self.report_skip_fragment(frag_index)
-            #             break
-            #         raise
-
-            # if count > fragment_retries:
-            #     if not fatal:
-            #         self.report_skip_fragment(frag_index)
-            #         continue
-            #     self.report_error('giving up after %s fragment retries' % fragment_retries)
-            #     return False
-
         if real_downloader:
             info_copy = info_dict.copy()
             info_copy['fragments'] = fragments_to_download
