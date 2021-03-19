a665a7381f5de427a499172f647d0fcaae997730
pukkandan <pukkandan.ytdlp@gmail.com>
2021-03-19 16:31:22 +0530

    Merge webm formats into mkv if thumbnails are to be embedded (#173)
    Co-authored by: Damiano Amatruda <damiano.amatruda@outlook.com>

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -2343,10 +2343,17 @@ class YoutubeDL(object):
 
                     requested_formats = info_dict['requested_formats']
                     old_ext = info_dict['ext']
-                    if self.params.get('merge_output_format') is None and not compatible_formats(requested_formats):
-                        info_dict['ext'] = 'mkv'
-                        self.report_warning(
-                            'Requested formats are incompatible for merge and will be merged into mkv.')
+                    if self.params.get('merge_output_format') is None:
+                        if not compatible_formats(requested_formats):
+                            info_dict['ext'] = 'mkv'
+                            self.report_warning(
+                                'Requested formats are incompatible for merge and will be merged into mkv.')
+                        if (info_dict['ext'] == 'webm'
+                                and self.params.get('writethumbnail', False)
+                                and info_dict.get('thumbnails')):
+                            info_dict['ext'] = 'mkv'
+                            self.report_warning(
+                                'webm doesn\'t support embedding a thumbnail, mkv will be used.')
 
                     def correct_ext(filename):
                         filename_real_ext = os.path.splitext(filename)[1][1:]
