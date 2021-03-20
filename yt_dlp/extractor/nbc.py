5bf9dca62aa56c84753a5ad4b1377555aba4f215
2ShedsJackson <50157910+2ShedsJackson@users.noreply.github.com>
2021-03-20 08:17:25 -0700

    Add files via upload

--- yt_dlp/extractor/nbc.py
+++ yt_dlp/extractor/nbc.py
@@ -164,7 +164,7 @@ class NBCIE(ThePlatformIE):
             series = tpm.get('nbcu$seriesShortTitle')
         tags = video_data.get('keywords')
         if tags is None or len(tags) == 0:
-            tags = tpm.get('keywords'
+            tags = tpm.get('keywords')
 
         return {
             '_type': 'url_transparent',
