e1feb88fdf09203e64a9f4cf7a761c228a21d720
pukkandan <pukkandan.ytdlp@gmail.com>
2021-03-23 14:00:51 +0530

    [niconico] Fix for when logged in
    Related: https://github.com/yt-dlp/yt-dlp/issues/171#issuecomment-803692716
    Co-authored by: CXwudi, xtkoba

--- yt_dlp/extractor/niconico.py
+++ yt_dlp/extractor/niconico.py
@@ -492,13 +492,12 @@ class NiconicoIE(InfoExtractor):
         self._sort_formats(formats)
 
         # Start extracting information
-        title = get_video_info_web('originalTitle')
-        if not title:
-            title = self._og_search_title(webpage, default=None)
-        if not title:
-            title = self._html_search_regex(
+        title = (
+            get_video_info_web(['originalTitle', 'title'])
+            or self._og_search_title(webpage, default=None)
+            or self._html_search_regex(
                 r'<span[^>]+class="videoHeaderTitle"[^>]*>([^<]+)</span>',
-                webpage, 'video title')
+                webpage, 'video title'))
 
         watch_api_data_string = self._html_search_regex(
             r'<div[^>]+id="watchAPIDataContainer"[^>]+>([^<]+)</div>',
