33245766ab0b7be7971ff46f03c706d77a327a92
pukkandan <pukkandan.ytdlp@gmail.com>
2021-04-16 06:00:04 +0530

    [downloader] Fix ffmpeg selection for m3u8_native

--- yt_dlp/downloader/external.py
+++ yt_dlp/downloader/external.py
@@ -330,7 +330,7 @@ class HttpieFD(ExternalFD):
 
 
 class FFmpegFD(ExternalFD):
-    SUPPORTED_PROTOCOLS = ('http', 'https', 'ftp', 'ftps', 'm3u8', 'rtsp', 'rtmp', 'mms')
+    SUPPORTED_PROTOCOLS = ('http', 'https', 'ftp', 'ftps', 'm3u8', 'm3u8_native', 'rtsp', 'rtmp', 'mms')
 
     @classmethod
     def available(cls, path=None):
