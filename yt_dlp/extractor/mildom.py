70f0a10af42ccbd7c38934b78bb8efb7c01e5c4f
nao20010128nao <nao20010128@gmail.com>
2021-04-19 14:43:39 +0900

    [mildom] load-balancing between multiple servers
    Vercel is commented out. See #251

--- yt_dlp/extractor/mildom.py
+++ yt_dlp/extractor/mildom.py
@@ -6,6 +6,7 @@ import itertools
 import json
 import base64
 import re
+import random
 
 from .common import InfoExtractor
 from ..utils import (
@@ -24,6 +25,12 @@ from ..compat import (
 class MildomBaseIE(InfoExtractor):
     _GUEST_ID = None
     _DISPATCHER_CONFIG = None
+
+    _MILDOM_PROXY_HOSTS = (
+        # 'bookish-octo-barnacle.vercel.app',  # see #251 for reason
+        'free-mountain-goal.glitch.me',
+        'lesmimonabb.dip.jp',
+    )
 
     def _call_api(self, url, video_id, query={}, note='Downloading JSON metadata', init=False):
         url = update_url_query(url, self._common_queries(query, init=init))
@@ -69,7 +76,7 @@ class MildomBaseIE(InfoExtractor):
                 self._DISPATCHER_CONFIG = self._parse_json(base64.b64decode(tmp['data']), 'initialization')
             except ExtractorError:
                 self._DISPATCHER_CONFIG = self._download_json(
-                    'https://bookish-octo-barnacle.vercel.app/api/mildom/dispatcher_config', 'initialization',
+                    'https://%s/api/mildom/dispatcher_config' % self._mildom_proxy_host(), 'initialization',
                     note='Downloading dispatcher_config fallback')
         return self._DISPATCHER_CONFIG
 
@@ -95,6 +102,9 @@ class MildomBaseIE(InfoExtractor):
     def lang_code(self):
         'getCurrentLangCode'
         return 'ja'
+
+    def _mildom_proxy_host(self):
+        return random.choice(self._MILDOM_PROXY_HOSTS)
 
 
 class MildomIE(MildomBaseIE):
@@ -150,7 +160,7 @@ class MildomIE(MildomBaseIE):
             # Uses https://github.com/nao20010128nao/bookish-octo-barnacle by @nao20010128nao as a proxy
             parsed = compat_urlparse.urlparse(fmt['url'])
             parsed = parsed._replace(
-                netloc='bookish-octo-barnacle.vercel.app',
+                netloc=self._mildom_proxy_host(),
                 query=compat_urllib_parse_urlencode(stream_query, True),
                 path='/api/mildom' + parsed.path)
             fmt['url'] = compat_urlparse.urlunparse(parsed)
