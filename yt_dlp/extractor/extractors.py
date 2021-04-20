743d50ad8490e4fdfed0e55673338f98b06897b8
bhargav <12525622+brpy@users.noreply.github.com>
2021-04-20 13:43:48 +0530

    import Streamtape

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1243,6 +1243,7 @@ from .storyfire import (
 from .streamable import StreamableIE
 from .streamcloud import StreamcloudIE
 from .streamcz import StreamCZIE
+from .streamtape import StreamtapeIE
 from .streetvoice import StreetVoiceIE
 from .stretchinternet import StretchInternetIE
 from .stv import STVPlayerIE
