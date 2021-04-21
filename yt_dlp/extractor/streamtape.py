c3f90e3f714bab875dccf361421519fe76f95d98
Bhargav v <12525622+brpy@users.noreply.github.com>
2021-04-21 20:53:31 +0530

    Update streamtape.py

--- yt_dlp/extractor/streamtape.py
+++ yt_dlp/extractor/streamtape.py
@@ -7,19 +7,18 @@ from ..utils import js_to_json, urljoin
 split_string_part = r'(?:%s|%s)' % (r'"(?:[^"\\]|\\.)*"',
                                     r"'(?:[^'\\]|\\.)*'")
 split_string = r'(?:' + split_string_part + r'(?:\s*\+\s*' + split_string_part + r')*)'
-videolink = r"(?:'\+')?".join('videolink')
-videolink = r"document\.getElementById\('" + videolink + r"'\)\.innerHTML\s*=\s*(?P<data>" + split_string + r")"
+videolink = r"document\.getElementById\('" + r"(?:'\+')?".join('videolink') + r"'\)\.innerHTML\s*=\s*(?P<data>" + split_string + r")"
 
 
 class StreamtapeIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?streamtape.com/[ev]/(?P<id>[^/?#]+)'
     _TESTS = [{
-        'url': 'https://streamtape.com/v/AJD9gAVwMOcXrQ8/Big_Buck_Bunny_Trailer_400p.ogg',
-        'md5': '6f7cdddd436852f054728bfd4d3be873',
+        'url': 'https://streamtape.com/v/7qDqGjlQe4UA9MR/Soul_Land_03_VOSTFR.mp4',
+        'md5': '2bd8790b33d8e445575070774153c19f',
         'info_dict': {
-            'id': 'AJD9gAVwMOcXrQ8',
+            'id': '7qDqGjlQe4UA9MR',
             'ext': 'mp4',
-            'title': 'Big_Buck_Bunny_Trailer_400p.ogg.mp4',
+            'title': 'Soul Land 03 VOSTFR.mp4',
             'thumbnail': r're:^https?://.*\.jpg$',
             'age_limit': 18,
         },
@@ -30,17 +29,13 @@ class StreamtapeIE(InfoExtractor):
 
         webpage = self._download_webpage(url, video_id)
 
-        video = self._html_search_regex(videolink, webpage, 'video', group='data')
-        video = video.split('+')
-        video = [self._parse_json(v, video_id, js_to_json) for v in video]
-        video = urljoin(url, ''.join(video))
+        search_results = self._html_search_regex(videolink, webpage, 'video', group='data').split('+')
+        video_parsed = [self._parse_json(v, video_id, js_to_json) for v in search_results]
+        video = urljoin(url, ''.join(video_parsed))
 
-        try:
-            poster = self._html_search_regex(r' id="mainvideo"[^>]* poster="(?P<data>.*?)"',
-                                             webpage, 'poster', group='data')
-            poster = urljoin(url, poster)
-        except ValueError:
-            poster = None
+        poster = self._html_search_regex(r' id="mainvideo"[^>]* poster="(?P<data>.*?)"',
+                                         webpage, 'poster', group='data')
+        poster = urljoin(url, poster)
 
         title = self._og_search_title(webpage)
 
