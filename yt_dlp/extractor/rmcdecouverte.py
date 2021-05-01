88f06afc0c05cd110a8dfe14cf51aa966cbe8cbc
pukkandan <pukkandan.ytdlp@gmail.com>
2021-05-01 19:47:10 +0530

    [rmcdecouverte] Improve `_VALID_URL` Closes #291

--- yt_dlp/extractor/rmcdecouverte.py
+++ yt_dlp/extractor/rmcdecouverte.py
@@ -13,9 +13,24 @@ from ..utils import smuggle_url
 
 
 class RMCDecouverteIE(InfoExtractor):
-    _VALID_URL = r'https?://rmcdecouverte\.bfmtv\.com/(?:(?:[^/]+/)*program_(?P<id>\d+)|(?P<live_id>mediaplayer-direct))'
+    _VALID_URL = r'https?://rmcdecouverte\.bfmtv\.com/(?:[^/]+/(?P<id>[^?#/]+)|(?P<live_id>mediaplayer-direct))'
 
     _TESTS = [{
+        'url': 'https://rmcdecouverte.bfmtv.com/vestiges-de-guerre_22240/les-bunkers-secrets-domaha-beach_25303/',
+        'info_dict': {
+            'id': '6250879771001',
+            'ext': 'mp4',
+            'title': 'LES BUNKERS SECRETS D´OMAHA BEACH',
+            'uploader_id': '1969646226001',
+            'description': 'md5:aed573ca24abde62a148e0eba909657d',
+            'timestamp': 1619622984,
+            'upload_date': '20210428',
+        },
+        'params': {
+            'format': 'bestvideo',
+            'skip_download': True,
+        },
+    }, {
         'url': 'https://rmcdecouverte.bfmtv.com/wheeler-dealers-occasions-a-saisir/program_2566/',
         'info_dict': {
             'id': '5983675500001',
