9ccdc41b5f9625260efaeacd09a73197455332a5
nao20010128nao <nao20010128@gmail.com>
2021-05-02 21:40:29 +0900

    [whowatch] add fields we can extract

--- yt_dlp/extractor/whowatch.py
+++ yt_dlp/extractor/whowatch.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import datetime
+
 from .common import InfoExtractor
 from ..utils import (
     try_get,
@@ -13,6 +15,7 @@ class WhoWatchIE(InfoExtractor):
     IE_NAME = 'whowatch'
     _VALID_URL = r'https?://whowatch\.tv/viewer/(?P<id>\d+)'
 
+    _QUALITIES = {'low': 0, 'medium': 1, 'high': 2, 'veryhigh': 3}
     _TESTS = [{
         'url': 'https://whowatch.tv/viewer/21450171',
         'only_matching': True,
@@ -31,7 +34,7 @@ class WhoWatchIE(InfoExtractor):
 
         hls_url = live_data.get('hls_url')
         if not hls_url:
-            raise ExtractorError(live_data.get('error_message') or 'The live is offline.', expected=True)
+            raise ExtractorError(live_data.get('error_message') or 'The user is offline.', expected=True)
 
         formats = self._extract_m3u8_formats(
             hls_url, video_id, ext='mp4', entry_protocol='m3u8',
@@ -41,17 +44,19 @@ class WhoWatchIE(InfoExtractor):
             name = fmt.get('name') or 'source-%d' % i
             hls_url = fmt.get('hls_url')
             rtmp_url = fmt.get('rtmp_url')
+            audio_only = fmt.get('audio_only')
+            quality = -1 if audio_only else self._QUALITIES.get(fmt.get('quality'), 0)
 
             if hls_url:
                 hls_fmts = self._extract_m3u8_formats(
                     hls_url, video_id, ext='mp4', entry_protocol='m3u8',
-                    m3u8_id='hls')
+                    m3u8_id='hls', quality=quality)
                 formats.extend(hls_fmts)
             else:
                 hls_fmts = []
 
             # RTMP url for audio_only is same as high format, so skip it
-            if rtmp_url and not fmt.get('audio_only'):
+            if rtmp_url and not audio_only:
                 formats.append({
                     'url': rtmp_url,
                     'format_id': 'rtmp-%s' % name,
@@ -59,6 +64,7 @@ class WhoWatchIE(InfoExtractor):
                     'protocol': 'rtmp_ffmpeg',  # ffmpeg can, while rtmpdump can't
                     'vcodec': 'h264',
                     'acodec': 'aac',
+                    'quality': quality,
                     'format_note': fmt.get('label'),
                     # note: HLS and RTMP have same resolution for now, so it's acceptable
                     'width': try_get(hls_fmts, lambda x: x[0]['width'], int),
@@ -67,18 +73,28 @@ class WhoWatchIE(InfoExtractor):
 
         self._sort_formats(formats)
 
-        uploader_id = try_get(metadata, lambda x: x['live']['user']['user_path'], compat_str)
-        uploader_id_internal = try_get(metadata, lambda x: x['live']['user']['id'], int)
+        uploader_url = try_get(metadata, lambda x: x['live']['user']['user_path'], compat_str)
+        if uploader_url:
+            uploader_url = 'https://whowatch.tv/profile/%s' % uploader_url
+        uploader_id = compat_str(try_get(metadata, lambda x: x['live']['user']['id'], int))
         uploader = try_get(metadata, lambda x: x['live']['user']['name'], compat_str)
         thumbnail = try_get(metadata, lambda x: x['live']['latest_thumbnail_url'], compat_str)
+        upload_date = try_get(metadata, lambda x: x['live']['started_at'], int)
+        if upload_date is not None:
+            upload_date = datetime.datetime.fromtimestamp(upload_date / 1000).strftime('%Y%m%d')
+        view_count = try_get(metadata, lambda x: x['live']['total_view_count'], int)
+        comment_count = try_get(metadata, lambda x: x['live']['comment_count'], int)
 
         return {
             'id': video_id,
             'title': title,
             'uploader_id': uploader_id,
-            'uploader_id_internal': uploader_id_internal,
+            'uploader_url': uploader_url,
             'uploader': uploader,
             'formats': formats,
             'thumbnail': thumbnail,
+            'upload_date': upload_date,
+            'view_count': view_count,
+            'comment_count': comment_count,
             'is_live': True,
         }
