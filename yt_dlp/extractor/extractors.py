8b79dbaefe26f9d55e39800bdbbb2fa7f5e67336
Raúl Sánchez Siles <rasasi78@gmail.com>
2021-05-11 22:51:12 +0200

    [extractors]: Split rtve imports.

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1236,7 +1236,12 @@ from .rtl2 import (
 from .rtp import RTPIE
 from .rtrfm import RTRFMIE
 from .rts import RTSIE
-from .rtve import RTVEALaCartaIE, RTVELiveIE, RTVEInfantilIE, RTVELiveIE, RTVETelevisionIE
+from .rtve import (
+        RTVEALaCartaIE,
+        RTVELiveIE,
+        RTVEInfantilIE,
+        RTVETelevisionIE,
+)
 from .rtvnh import RTVNHIE
 from .rtvs import RTVSIE
 from .ruhd import RUHDIE
