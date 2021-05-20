yt-dlp/yt-dlp

commit 9c2b75b5616f058c3970e54c664e9543f86b9f0c

Field `additional_urls` to download additional videos from metadata

yt_dlp/YoutubeDL.py
@@ -1187,7 +1187,22 @@ class YoutubeDL(object):
 
         if result_type == 'video':
             self.add_extra_info(ie_result, extra_info)
-            return self.process_video_result(ie_result, download=download)
+            ie_result = self.process_video_result(ie_result, download=download)
+            additional_urls = ie_result.get('additional_urls')
+            if additional_urls:
+                # TODO: Improve MetadataFromFieldPP to allow setting a list
+                if isinstance(additional_urls, compat_str):
+                    additional_urls = [additional_urls]
+                self.to_screen(
+                    '[info] %s: %d additional URL(s) requested' % (ie_result['id'], len(additional_urls)))
+                self.write_debug('Additional URLs: "%s"' % '", "'.join(additional_urls))
+                ie_result['additional_entries'] = [
+                    self.extract_info(
+                        url, download, extra_info,
+                        force_generic_extractor=self.params.get('force_generic_extractor'))
+                    for url in additional_urls
+                ]
+            return ie_result
         elif result_type == 'url':
             # We have to add extra_info to the results because it may be
             # contained in a playlist

commit 28b0eb0f65fa48f44cf40b202fdfdcd7a7e957a6

[cleanup] See desc * Remove struct from `embedthumbnail` * Use bullet lists in readme where numbered list don't make sense * Fix error introduced in 9c2b75b5616f058c3970e54c664e9543f86b9f0c when `ie_result` is `None`

yt_dlp/YoutubeDL.py
@@ -1188,7 +1188,7 @@ class YoutubeDL(object):
         if result_type == 'video':
             self.add_extra_info(ie_result, extra_info)
             ie_result = self.process_video_result(ie_result, download=download)
-            additional_urls = ie_result.get('additional_urls')
+            additional_urls = (ie_result or {}).get('additional_urls')
             if additional_urls:
                 # TODO: Improve MetadataFromFieldPP to allow setting a list
                 if isinstance(additional_urls, compat_str):

yt_dlp/postprocessor/embedthumbnail.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 import base64
 import imghdr
 import os
-import struct
 import subprocess
 import re
 