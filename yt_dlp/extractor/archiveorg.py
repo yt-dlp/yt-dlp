0f8bd7fd0b3536fd3c4d55a5a68274fd99783c74
Matthew <colethedj@protonmail.com>
2021-05-29 19:05:27 +1200

    quotes

--- yt_dlp/extractor/archiveorg.py
+++ yt_dlp/extractor/archiveorg.py
@@ -276,7 +276,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
             'info_dict': {
                 'id': 'aYAGB11YrSs',
                 'ext': 'webm',
-                'title': "Unknown Video"
+                'title': 'Unknown Video'
             }
         },
         {
@@ -285,7 +285,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
             'info_dict': {
                 'id': '97t7Xj_iBv0',
                 'ext': 'mp4',
-                'title': "Unknown Video"
+                'title': 'Unknown Video'
             }
 
         },
@@ -295,7 +295,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
             'info_dict': {
                 'id': 'AkhihxRKcrs',
                 'ext': 'webm',
-                'title': "Unknown Video"
+                'title': 'Unknown Video'
             }
         },
         {
@@ -304,7 +304,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
             'info_dict': {
                 'id': 'jNQXAC9IVRw',
                 'ext': 'unknown_video',
-                'title': "Unknown Video"
+                'title': 'Unknown Video'
             }
         }
     ]
@@ -314,7 +314,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
         # Use link translator mentioned in https://github.com/ytdl-org/youtube-dl/issues/13655
         internal_fake_url = self._INTERNAL_URL_TEMPLATE % video_id
         video_file_webpage = self._request_webpage(
-            HEADRequest(internal_fake_url), video_id, errnote="Video is not archived or issue with web.archive.org")
+            HEADRequest(internal_fake_url), video_id, errnote='Video is not archived or issue with web.archive.org')
         video_file_url = compat_urllib_parse_unquote(video_file_webpage.url)
         video_file_url_qs = compat_parse_qs(compat_urlparse.urlparse(video_file_url).query)
 
@@ -330,7 +330,7 @@ class YoutubeWebArchiveIE(InfoExtractor):
             format.update({'ext': ext})
         return {
             'id': video_id,
-            'title': "Unknown Video",  # In this case we are not able to get a title reliably.
+            'title': 'Unknown Video',  # In this case we are not able to get a title reliably.
             'formats': [format],
             'webpage_url': url,
             'duration': str_to_int(try_get(video_file_url_qs, lambda x: x['dur'][0]))
