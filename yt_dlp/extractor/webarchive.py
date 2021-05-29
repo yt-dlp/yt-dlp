227b424163a720d31c55b0af0dfe86f0971644c2
Matthew <colethedj@protonmail.com>
2021-05-29 17:20:55 +1200

    Improve _VALID_URL. Support internal fake-url web archive url.

--- yt_dlp/extractor/webarchive.py
+++ yt_dlp/extractor/webarchive.py
@@ -16,13 +16,19 @@ from ..utils import (
 
 
 class YoutubeWebArchiveIE(InfoExtractor):
-    # TODO
-    # - Regex needs a lot more work
-    #       * fails on: https://web.archive.org/web/20120712231619oe/https://www.youtube.com/watch?v=AkhihxRKcrs (the date id can be anything, even *)
-    #       * also fails on anything after the video id (e.g. &list)
-    #       * support http://wayback-fakeurl.archive.org/yt/<video id> urls too
-    # - Put in archiveorg.py
-    _VALID_URL = r'https?:\/\/(?:www\.)?web\.archive\.org\/web\/([0-9]+)\/https?:\/\/(?:www\.)?youtube\.com\/watch\?v=(?P<id>[0-9A-Za-z_-]{1,11})$'
+    _VALID_URL = r"""(?x)^
+                (?:https?://|//)?web\.archive\.org\/
+                    (?:web/)? 
+                    (?:[0-9A-Za-z_*]+/)? # /web and the version index is optional
+                (?:https?:\/\/)?
+                (?:
+                    (?:\w+\.)?youtube\.com\/watch\?v= # Youtube URL
+                    |(wayback-fakeurl\.archive\.org\/yt\/) # Or optionally, also support the internal fake url
+                )    
+                (?P<id>[0-9A-Za-z_-]{11})(?(1).+)?(?:\#|&|$)
+                """
+
+    _INTERNAL_URL_TEMPLATE = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/%s"
     # _TEST = {
     #     'url': 'https://web.archive.org/web/20150415002341/https://www.youtube.com/watch?v=aYAGB11YrSs',
     #     'md5': 'ec44dc1177ae37189a8606d4ca1113ae',
@@ -35,32 +41,29 @@ class YoutubeWebArchiveIE(InfoExtractor):
     # https://web.archive.org/web/20120712202131/http://www.youtube.com/watch?v=Y2FzcnAaYiM&gl=US&hl=en
     # also test on very old videos (<2010)
 
-    _WAYBACK_FAKEURL = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/"
-
     def _real_extract(self, url):
-        # Get video ID and page
         video_id = self._match_id(url)
         # Use link translator mentioned in https://github.com/ytdl-org/youtube-dl/issues/13655
-        video_url = self._WAYBACK_FAKEURL + video_id
-        file_webpage = self._request_webpage(
-            HEADRequest(video_url), video_id, errnote="video not archived or issue with web.archive.org")
-        file_url = compat_urllib_parse_unquote(file_webpage.url)
-        file_url_qs = compat_parse_qs(compat_urlparse.urlparse(file_url).query)
+        internal_fake_url = self._INTERNAL_URL_TEMPLATE % video_id
+        video_file_webpage = self._request_webpage(
+            HEADRequest(internal_fake_url), video_id, errnote="Video is not archived or issue with web.archive.org")
+        video_file_url = compat_urllib_parse_unquote(video_file_webpage.url)
+        video_file_url_qs = compat_parse_qs(compat_urlparse.urlparse(video_file_url).query)
 
         # Attempt to recover any ext & format info from playback url
-        itag = try_get(file_url_qs, lambda x: x['itag'][0])
-        format = {'url': file_url}
+        format = {'url': video_file_url}
+        itag = try_get(video_file_url_qs, lambda x: x['itag'][0])
         if itag and itag in YoutubeIE._formats:
             format.update(YoutubeIE._formats[itag])
             format.update({'format_id': itag})
         else:
-            mime = try_get(file_url_qs, lambda x: x['mime'][0])
-            ext = mimetype2ext(mime) or determine_ext(video_url)
+            mime = try_get(video_file_url_qs, lambda x: x['mime'][0])
+            ext = mimetype2ext(mime) or determine_ext(video_file_url)
             format.update({'ext': ext})
         return {
             'id': video_id,
-            'title': None,
+            'title': None,  # In this case we are not able to get a title reliably.
             'formats': [format],
             'webpage_url': url,
-            'duration': str_to_int(try_get(file_url_qs, lambda x: x['dur'][0]))
+            'duration': str_to_int(try_get(video_file_url_qs, lambda x: x['dur'][0]))
         }
