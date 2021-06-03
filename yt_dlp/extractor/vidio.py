c8e63d6f09bab0f30da895c387bef33ee891ddcc
MinePlayersPE <mineplayerspealt@gmail.com>
2021-06-03 20:10:22 +0700

    whoops, 3x
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/extractor/vidio.py
+++ yt_dlp/extractor/vidio.py
@@ -244,7 +244,7 @@ class VidioLiveIE(VidioBaseIE):
             drm_hls_url = stream_meta.get('drm_stream_hls_url')
             if drm_hls_url:
                 formats.extend(self._extract_m3u8_formats(
-                    drm_hls_url, display_id, 'mp4', 'm3u8-native'))
+                    drm_hls_url, display_id, 'mp4', 'm3u8_native'))
         elif stream_meta.get('is_premium'):
             sources = self._download_json(
                 'https://www.vidio.com/interactions_stream.json?video_id=%s&type=livestreamings' % video_id,
