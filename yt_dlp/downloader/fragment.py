47d82f2ed44c099791d9d3467bd4ded676b41d0b
pukkandan <pukkandan.ytdlp@gmail.com>
2021-06-06 04:28:41 +0530

    cleanup

--- yt_dlp/downloader/fragment.py
+++ yt_dlp/downloader/fragment.py
@@ -334,22 +334,18 @@ class FragmentFD(FileDownloader):
             pack_func = lambda frag_content, _: frag_content
 
         def download_fragment(fragment):
-            frag_index = fragment['frag_index']
-            frag_url = fragment['url']
-            i = fragment.get('index') or frag_index
-
-            ctx['fragment_index'] = frag_index
+            frag_index = ctx['fragment_index'] = fragment['frag_index']
             headers = info_dict.get('http_headers', {})
             byte_range = fragment.get('byte_range')
             if byte_range:
                 headers['Range'] = 'bytes=%d-%d' % (byte_range['start'], byte_range['end'] - 1)
 
             # Never skip the first fragment
-            fatal = i == 0 or not skip_unavailable_fragments
+            fatal = (fragment.get('index') or frag_index) == 0 or not skip_unavailable_fragments
             count = 0
             while count <= fragment_retries:
                 try:
-                    success, frag_content = self._download_fragment(ctx, frag_url, info_dict, headers)
+                    success, frag_content = self._download_fragment(ctx, fragment['url'], info_dict, headers)
                     if not success:
                         return False, frag_index
                     break
@@ -378,18 +374,17 @@ class FragmentFD(FileDownloader):
 
         def decrypt_fragment(fragment, frag_content):
             decrypt_info = fragment.get('decrypt_info')
-            if decrypt_info and decrypt_info['METHOD'] == 'AES-128':
-                iv = decrypt_info.get('IV') or compat_struct_pack('>8xq', fragment['media_sequence'])
-                decrypt_info['KEY'] = decrypt_info.get('KEY') or self.ydl.urlopen(
-                    self._prepare_url(info_dict, info_dict.get('_decryption_key_url') or decrypt_info['URI'])).read()
-                # Don't decrypt the content in tests since the data is explicitly truncated and it's not to a valid block
-                # size (see https://github.com/ytdl-org/youtube-dl/pull/27660). Tests only care that the correct data downloaded,
-                # not what it decrypts to.
-                if not test:
-                    frag_content = AES.new(
-                        decrypt_info['KEY'], AES.MODE_CBC, iv).decrypt(frag_content)
-
-            return frag_content
+            if not decrypt_info or decrypt_info['METHOD'] != 'AES-128':
+                return frag_content
+            iv = decrypt_info.get('IV') or compat_struct_pack('>8xq', fragment['media_sequence'])
+            decrypt_info['KEY'] = decrypt_info.get('KEY') or self.ydl.urlopen(
+                self._prepare_url(info_dict, info_dict.get('_decryption_key_url') or decrypt_info['URI'])).read()
+            # Don't decrypt the content in tests since the data is explicitly truncated and it's not to a valid block
+            # size (see https://github.com/ytdl-org/youtube-dl/pull/27660). Tests only care that the correct data downloaded,
+            # not what it decrypts to.
+            if test:
+                return frag_content
+            return AES.new(decrypt_info['KEY'], AES.MODE_CBC, iv).decrypt(frag_content)
 
         def append_fragment(frag_content, frag_index):
             if not frag_content:
