125728b038a17f53397a85584b4e9205ca3a062c
Mevious <benbryant1@gmail.com>
2021-06-24 17:15:23 -0700

    [funimation] Add `FunimationShowIE` (#442)
    Closes #436
    
    Authored by: Mevious

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -455,7 +455,10 @@ from .frontendmasters import (
     FrontendMastersCourseIE
 )
 from .fujitv import FujiTVFODPlus7IE
-from .funimation import FunimationIE
+from .funimation import (
+    FunimationIE,
+    FunimationShowIE
+)
 from .funk import FunkIE
 from .fusion import FusionIE
 from .gaia import GaiaIE
