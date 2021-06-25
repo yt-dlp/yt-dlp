9f964f90d60a3012f13fc2b17ddf149197772432
llacb47 <llacb47@users.noreply.github.com>
2021-06-25 19:11:53 -0400

    restore indentation

--- yt_dlp/extractor/turner.py
+++ yt_dlp/extractor/turner.py
@@ -246,12 +246,12 @@ class TurnerBaseIE(AdobePassIE):
                 for chapter in stream_data.get('contentSegments', []):
                     start_time = float_or_none(chapter.get('start'))
                     chapter_duration = float_or_none(chapter.get('duration'))
-                if start_time is None or chapter_duration is None:
-                    continue
-                chapters.append({
-                    'start_time': start_time,
-                    'end_time': start_time + chapter_duration,
-                })
+                    if start_time is None or chapter_duration is None:
+                        continue
+                    chapters.append({
+                        'start_time': start_time,
+                        'end_time': start_time + chapter_duration,
+                    })
         self._sort_formats(formats)
 
         return {
