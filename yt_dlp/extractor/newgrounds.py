0f01060bac5b322a77db4ab7403b9d59486fe20e
SsSsS <54671367+u-spec-png@users.noreply.github.com>
2021-08-04 14:19:03 +0000

    [Newgrounds] flake8

--- yt_dlp/extractor/newgrounds.py
+++ yt_dlp/extractor/newgrounds.py
@@ -149,7 +149,6 @@ class NewgroundsIE(InfoExtractor):
 
         if '<dd>Song' in webpage:
             formats[0]['vcodec'] = 'none'
-        
         self._check_formats(formats, media_id)
         self._sort_formats(formats)
         
