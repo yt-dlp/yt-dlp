yt-dlp/yt-dlp

commit 89efdc15dd4dbdb4b51e82647637f33112156f61

[ffpmeg] Allow `--ffmpeg-location` to be a file with different name

yt_dlp/postprocessor/ffmpeg.py
@@ -111,19 +111,14 @@ class FFmpegPostProcessor(PostProcessor):
                     return
                 elif not os.path.isdir(location):
                     basename = os.path.splitext(os.path.basename(location))[0]
-                    if basename not in programs:
-                        self.report_warning(
-                            'Cannot identify executable %s, its basename should be one of %s. '
-                            'Continuing without ffmpeg.' %
-                            (location, ', '.join(programs)))
-                        self._versions = {}
-                        return None
-                    location = os.path.dirname(os.path.abspath(location))
+                    basename = next((p for p in programs if basename.startswith(p)), 'ffmpeg')
+                    dirname = os.path.dirname(os.path.abspath(location))
                     if basename in ('ffmpeg', 'ffprobe'):
                         prefer_ffmpeg = True
 
                 self._paths = dict(
-                    (p, os.path.join(location, p)) for p in programs)
+                    (p, os.path.join(dirname, p)) for p in programs)
+                self._paths[basename] = location
                 self._versions = dict(
                     (p, get_ffmpeg_version(self._paths[p])) for p in programs)
         if self._versions is None:

commit 8c0ae192a42965f318644373d05060e720af79c7

[ffmpeg] Fix `--ffmpeg-location` when directory is given Bug introduced in 89efdc15dd4dbdb4b51e82647637f33112156f61 Closes #654

yt_dlp/postprocessor/ffmpeg.py
@@ -109,7 +109,9 @@ class FFmpegPostProcessor(PostProcessor):
                         'Continuing without ffmpeg.' % (location))
                     self._versions = {}
                     return
-                elif not os.path.isdir(location):
+                elif os.path.isdir(location):
+                    dirname, basename = location, None
+                else:
                     basename = os.path.splitext(os.path.basename(location))[0]
                     basename = next((p for p in programs if basename.startswith(p)), 'ffmpeg')
                     dirname = os.path.dirname(os.path.abspath(location))
@@ -118,7 +120,8 @@ class FFmpegPostProcessor(PostProcessor):
 
                 self._paths = dict(
                     (p, os.path.join(dirname, p)) for p in programs)
-                self._paths[basename] = location
+                if basename:
+                    self._paths[basename] = location
                 self._versions = dict(
                     (p, get_ffmpeg_version(self._paths[p])) for p in programs)
         if self._versions is None: