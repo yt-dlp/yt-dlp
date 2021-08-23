fe629708e4bcee4da354c2b4187143feccfeb6fe
tandy1000 <tandy1000@cock.li>
2021-08-23 15:55:30 +0100

    fix all issues

--- yt_dlp/extractor/manoto.py
+++ yt_dlp/extractor/manoto.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-from .common import InfoExtractor
+from .common import InfoExtractor, traverse_obj
 
+
+_API_URL = 'https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/{}/{}?id={}'
+EXT = 'mp4'
 
 class ManotoTVIE(InfoExtractor):
     IE_NAME = 'Manoto TV (Episode)'
@@ -21,13 +24,13 @@ class ManotoTVIE(InfoExtractor):
             'title': 'سه ماه تعطیلی',
             'description': 'سه ماه تعطیلی فیلمی به کارگردانی و نویسندگی شاپور قریب ساختهٔ سال ۱۳۵۶ است.<br/><br/>',
             'thumbnail': r're:^https?://.*\.jpeg$',
-            'ext': 'mp4'
+            'ext': EXT
         }
     }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        episode_json = self._download_json('https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/showmodule/episodedetails?id=' + video_id, video_id)
+        episode_json = self._download_json(_API_URL.format('showmodule', 'episodedetails', video_id), video_id)
         details = episode_json.get('details', {})
         series = details.get('showTitle')
         season_number = details.get('analyticsSeasonNumber')
@@ -44,7 +47,7 @@ class ManotoTVIE(InfoExtractor):
         categories = [details.get('videoCategory')]
         video_url = details.get('videoM3u8Url')
         thumbnail = details.get('episodelandscapeImgIxUrl')
-        formats = self._extract_m3u8_formats(video_url, video_id, ext)
+        formats = self._extract_m3u8_formats(video_url, video_id, EXT)
         return {
             'id': video_id,
             'series': series,
@@ -57,7 +60,7 @@ class ManotoTVIE(InfoExtractor):
             'title': title,
             'description': description,
             'thumbnail': thumbnail,
-            'ext': ext,
+            'ext': EXT,
             'formats': formats,
         }
 
@@ -77,18 +80,16 @@ class ManotoTVShowIE(InfoExtractor):
 
     def _real_extract(self, url):
         show_id = self._match_id(url)
-        show_json = self._download_json('https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/showmodule/details?id=' + show_id, show_id)
+        show_json = self._download_json(_API_URL.format('showmodule', 'details', show_id), show_id)
         show_details = show_json.get('details', {})
         title = show_details.get('showTitle')
         description = show_details.get('showSynopsis')
 
-        series_json = self._download_json('https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/showmodule/serieslist?id=' + show_id, show_id)
-        series_details = series_json.get('details', {})
-        playlist_id = str(series_details.get('list', [])[0].get('id'))
+        series_json = self._download_json(_API_URL.format('showmodule', 'serieslist', show_id), show_id)
+        playlist_id = str(traverse_obj(series_json, ('details', 'list', 0, 'id')))
 
-        playlist_json = self._download_json('https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/showmodule/episodelist?id=' + playlist_id, playlist_id)
-        playlist_details = playlist_json.get('details', {})
-        playlist = playlist_details.get('list', [])
+        playlist_json = self._download_json(_API_URL.format('showmodule', 'episodelist', playlist_id), playlist_id)
+        playlist = traverse_obj(playlist_json, ('details', 'list')) or []
 
         entries = [
             self.url_result(
@@ -105,20 +106,20 @@ class ManotoTVLiveIE(InfoExtractor):
         'info_dict': {
             'id': 'live',
             'title': 'Manoto TV Live',
-            'ext': 'mp4',
+            'ext': EXT,
+            'is_live': True,
         }
     }
 
     def _real_extract(self, url):
         video_id = 'live'
-        json = self._download_json('https://dak1vd5vmi7x6.cloudfront.net/api/v1/publicrole/livemodule/details', video_id)
+        json = self._download_json(_API_URL.format('livemodule', 'details', ''), video_id)
         details = json.get('details', {})
         video_url = details.get('liveUrl')
-        ext = 'mp4'
-        formats = self._extract_m3u8_formats(video_url, video_id, ext)
+        formats = self._extract_m3u8_formats(video_url, video_id, EXT, live=True)
         return {
             'id': video_id,
             'title': 'Manoto TV Live',
-            'ext': ext,
+            'ext': EXT,
             'formats': formats,
         }
