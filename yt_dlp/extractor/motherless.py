b5ed84e8468da4216056739e55c35d2fe48c943a
coletdjnz <colethedj@protonmail.com>
2021-08-29 12:28:50 +1200

    add fallback uploader id regex (from https://github.com/ytdl-org/youtube-dl/issues/29626)

--- yt_dlp/extractor/motherless.py
+++ yt_dlp/extractor/motherless.py
@@ -127,7 +127,8 @@ class MotherlessIE(InfoExtractor):
 
         comment_count = webpage.count('class="media-comment-contents"')
         uploader_id = self._html_search_regex(
-            r'"media-meta-member">\s+<a href="/m/([^"]+)"',
+            (r'"media-meta-member">\s+<a href="/m/([^"]+)"',
+             r'<span class=\"username\">([^"]+)<\/span>'),
             webpage, 'uploader_id', fatal=False)
         categories = self._html_search_meta('keywords', webpage, default=None)
         if categories:
