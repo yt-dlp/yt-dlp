048a9ceb1248a9e66769b0778403c5d7fcbb2159
coletdjnz <colethedj@protonmail.com>
2021-08-29 19:21:35 +1200

    cleanup

--- yt_dlp/extractor/startv.py
+++ yt_dlp/extractor/startv.py
@@ -7,6 +7,8 @@ from ..compat import (
 )
 from ..utils import (
     clean_html,
+    ExtractorError,
+    traverse_obj,
     int_or_none,
 )
 
@@ -28,7 +30,7 @@ class StarTVIE(InfoExtractor):
             'info_dict': {
                 'id': '904972',
                 'display_id': '3-bolum',
-                'ext': 'm3u8',
+                'ext': 'mp4',
                 'title': '3. Bölüm',
                 'description': 'md5:3a8049f05a75c2e8747116a673275de4',
                 'thumbnail': r're:^https?://.*\.jpg(?:\?.*?)?$',
@@ -72,23 +74,22 @@ class StarTVIE(InfoExtractor):
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
-
         webpage = self._download_webpage(url, display_id)
-
         info_url = self._search_regex(
             r'(["\'])videoUrl\1\s*:\s*\1(?P<url>(?:(?!\1).)+)\1\s*',
             webpage, 'video info url', group='url')
 
-        info = self._download_json(info_url, display_id)['data']
-
-        video_id = compat_str(info['id'])
+        info = traverse_obj(self._download_json(info_url, display_id), 'data', expected_type=dict)
+        if not info:
+            raise ExtractorError('Failed to extract API data')
+        video_id = compat_str(info.get('id'))
         title = info.get('title') or self._og_search_title(webpage)
         description = clean_html(info.get('description')) or self._og_search_description(webpage, default=None)
         thumbnail = self._proto_relative_url(
             self._og_search_thumbnail(webpage), scheme='http:')
 
         formats = self._extract_m3u8_formats(
-            info['flavors']['hls'], video_id, entry_protocol='m3u8_native', m3u8_id='hls', fatal=False)
+            traverse_obj(info, ('flavors', 'hls')), video_id, entry_protocol='m3u8_native', m3u8_id='hls', fatal=False)
 
         return {
             'id': video_id,
