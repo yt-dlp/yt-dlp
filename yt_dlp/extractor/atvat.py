0547e8f188444870749fba2bf2bc39e33d73ec2a
coletdjnz <colethedj@protonmail.com>
2021-08-29 20:30:37 +1200

    cleanup

--- yt_dlp/extractor/atvat.py
+++ yt_dlp/extractor/atvat.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from .common import InfoExtractor
 from ..utils import (
     determine_ext,
+    dict_get,
     int_or_none,
     unescapeHTML,
 )
@@ -24,6 +25,35 @@ class ATVAtIE(InfoExtractor):
         'only_matching': True,
     }]
 
+    def _process_source_entry(self, source, part_id):
+        source_url = source.get('url')
+        if not source_url:
+            return
+        if determine_ext(source_url) == 'm3u8':
+            return self._extract_m3u8_formats(
+                source_url, part_id, 'mp4', 'm3u8_native',
+                m3u8_id='hls', fatal=False)
+        else:
+            return [{
+                'url': source_url,
+            }]
+
+    def _process_entry(self, entry):
+        part_id = entry.get('id')
+        if not part_id:
+            return
+        formats = []
+        for source in entry.get('sources', []):
+            formats.extend(self._process_source_entry(source, part_id) or [])
+
+        self._sort_formats(formats)
+        return {
+            'id': part_id,
+            'title': entry.get('title'),
+            'duration': int_or_none(entry.get('duration')),
+            'formats': formats
+        }
+
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
@@ -34,46 +64,11 @@ class ATVAtIE(InfoExtractor):
 
         first_video = video_data[0]
         video_id = first_video['id']
-        video_title = first_video.get('tvShowTitle', first_video['title'])
-
-        def process_source_entry(self, source, part_id):
-            source_url = source.get('url')
-            if not source_url:
-                return None
-            ext = determine_ext(source_url)
-            if ext == 'm3u8':
-                return self._extract_m3u8_formats(
-                    source_url, part_id, 'mp4', 'm3u8_native',
-                    m3u8_id='hls', fatal=False)
-            else:
-                return {
-                    'url': source_url,
-                }
-
-        def process_entry(self, entry):
-            part_id = entry['id']
-            formats = [process_source_entry(self, source, part_id) for source in entry["sources"]]
-            formats_flat = []
-            for f in formats:
-                if f is None:
-                    continue
-                elif type(f) is list:
-                    formats_flat.extend(f)
-                else:
-                    formats_flat.append(f)
-            self._sort_formats(formats_flat)
-            return {
-                'id': entry['id'],
-                'title': entry['title'],
-                'duration': int_or_none(entry.get('duration')),
-                'formats': formats_flat
-            }
-
-        entries = [process_entry(self, entry) for entry in video_data]
+        video_title = dict_get(first_video, ('tvShowTitle', 'title'))
 
         return {
             '_type': 'multi_video',
             'id': video_id,
             'title': video_title,
-            'entries': entries,
+            'entries': (self._process_entry(entry) for entry in video_data),
         }
