18c2fe167c733f999aea82649a8b57e7ffb8644b
coletdjnz <colethedj@protonmail.com>
2021-09-02 19:22:03 +1200

    improvements

--- yt_dlp/extractor/mediaklikk.py
+++ yt_dlp/extractor/mediaklikk.py
@@ -1,16 +1,16 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from yt_dlp.utils import str_or_none, unified_strdate
 from .common import InfoExtractor
 from ..compat import compat_urllib_parse_unquote
-import re
 
 
 class MediaKlikkIE(InfoExtractor):
     # Named regular expression group: (?P<name>...) used for referencing match as 'id'
-    _VALID_URL = r'https?:\/\/(?:www\.)?(?:mediaklikk|m4sport|hirado)\.hu\/.*?videok?\/(?P<id>[^\/]+)\/?'
+    _VALID_URL = r'''https?:\/\/(?:www\.)?(?:mediaklikk|m4sport|hirado)\.hu\/.*?videok?\/(?:(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/)?(?P<id>[^/#?_]+)'''
 
-    _TEST = {
+    _TESTS = [{
         'url': 'https://mediaklikk.hu/adal2020/video/2020/03/07/a-dal-donto/',
         'info_dict': {
             'id': 'kiberma-2020-04-30-i-adas',
@@ -18,38 +18,59 @@ class MediaKlikkIE(InfoExtractor):
             'title': 'KiberMa, 2020.04.30-i adás | MédiaKlikk',
             # no thumbnail extractable
         }
+    }, {
+        # m4sport
+        'url': 'https://m4sport.hu/video/2021/08/30/gyemant-liga-parizs/',
+        'info_dict': {
+            'id': '4754999',
+            'title': 'Gyémánt Liga, Párizs',
+            'ext': 'mp4',
+            'upload_date': '20210830',
+            'series': 'Videó/M4SPORT/Sportközvetítések',
+            'thumbnail': 'http://m4sport.hu/wp-content/uploads/sites/4/2021/08/vlcsnap-2021-08-30-18h21m20s10-1024x576.jpg'
+        }
+    }, {
+        # hirado
+        'url': 'https://hirado.hu/videok/felteteleket-szabott-a-fovaros/',
+        'info_dict': {
+            'id': '4760120',
+            'title': 'Feltételeket szabott a főváros',
+            'ext': 'mp4',
+            'thumbnail': 'http://hirado.hu/wp-content/uploads/sites/4/2021/09/vlcsnap-2021-09-01-20h20m37s165.jpg'
+        }
     }
+    ]
 
     def _real_extract(self, url):
-        mobj = re.match(self._VALID_URL, url)
-        video_id = mobj.group('id')
-        display_id = video_id  # we only have one id in url..
-        webpage = self._download_webpage(url, video_id)
+        video_id = display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
+        player_data_str = self._html_search_regex(
+            r"mtva_player_manager\.player\(document.getElementById\(.*\),\s?(\{.*\}).*\);", webpage, 'player data')
+        player_data = self._parse_json(player_data_str, video_id, compat_urllib_parse_unquote)
+        video_id = str_or_none(player_data.get('contentId')) or video_id
 
-        pattern = r"mtva_player_manager\.player\(document.getElementById\(.*\),\s?(\{.*\}).*\);"
-        info_json = self._html_search_regex(pattern, webpage, 'info_json')
-        info_meta = self._parse_json(compat_urllib_parse_unquote(info_json), None)
+        mobj = self._match_valid_url(url)
+        upload_date = unified_strdate(
+            f'%s-%s-%s' % (mobj.group('year'), mobj.group('month'), mobj.group('day')))
 
-        info_ret = {
-            '_type': 'video',
-            'title': info_meta.get('title') or video_id or self._og_search_title(webpage),
-            'ext': 'mp4',
-            'display_id': display_id,
-            'id': video_id
-        }
-
-        if 'series' in info_meta:
-            info_ret['series'] = info_meta['series']
-        info_meta['video'] = info_meta['token']
-        del info_meta['token']
-        playerpage = self._download_webpage('https://player.mediaklikk.hu/playernew/player.php', video_id, query=info_meta)
-        pattern = r"\"file\": \"(\\/\\/.*playlist\.m3u8)\","
+        player_data['video'] = player_data.pop('token')
+        player_page = self._download_webpage('https://player.mediaklikk.hu/playernew/player.php', video_id, query=player_data)
         playlist_url = 'https:' + compat_urllib_parse_unquote(
-            self._html_search_regex(pattern, playerpage, 'playlist_url'))\
-            .replace('\\/', '/')
+            self._html_search_regex(r'\"file\": \"(\\/\\/.*playlist\.m3u8)\",', player_page, 'playlist_url')).replace('\\/', '/')
+        
         formats = self._extract_wowza_formats(
             playlist_url, video_id, skip_protocols=['f4m', 'smil', 'dash'])
         self._sort_formats(formats)
-        info_ret['formats'] = formats
 
-        return info_ret
+        return {
+            '_type': 'video',
+            'title': player_data.get('title') or self._og_search_title(webpage),
+            'ext': 'mp4',
+            'display_id': display_id,
+            'id': video_id,
+            'formats': formats,
+            'upload_date': upload_date,
+            'series': player_data.get('series'),
+            'description': self._og_search_description(webpage, default=None),
+            'thumbnail': player_data.get('bgImage') or self._og_search_thumbnail(webpage)
+        }
