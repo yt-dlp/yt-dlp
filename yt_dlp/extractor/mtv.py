yt-dlp/yt-dlp

commit ee1e05581e32114c52e75e90983a66fb25fbc730

[mtv] Fix extractor by reverting changes made in youtube-dlc youtube-dl has since fixed the extractor and the changes from the two sources are incompatible

yt_dlp/extractor/mtv.py
@@ -7,7 +7,6 @@ from .common import InfoExtractor
 from ..compat import (
     compat_str,
     compat_xpath,
-    compat_urlparse,
 )
 from ..utils import (
     ExtractorError,
@@ -23,7 +22,6 @@ from ..utils import (
     unescapeHTML,
     update_url_query,
     url_basename,
-    get_domain,
     xpath_text,
 )
 
@@ -45,7 +43,7 @@ class MTVServicesInfoExtractor(InfoExtractor):
         # Remove the templates, like &device={device}
         return re.sub(r'&[^=]*?={.*?}(?=(&|$))', '', url)
 
-    def _get_feed_url(self, uri, url=None):
+    def _get_feed_url(self, uri):
         return self._FEED_URL
 
     def _get_thumbnail_url(self, uri, itemdoc):
@@ -211,9 +209,9 @@ class MTVServicesInfoExtractor(InfoExtractor):
             data['lang'] = self._LANG
         return data
 
-    def _get_videos_info(self, uri, use_hls=True, url=None):
+    def _get_videos_info(self, uri, use_hls=True):
         video_id = self._id_from_uri(uri)
-        feed_url = self._get_feed_url(uri, url)
+        feed_url = self._get_feed_url(uri)
         info_url = update_url_query(feed_url, self._get_feed_query(uri))
         return self._get_videos_info_from_url(info_url, video_id, use_hls)
 
@@ -259,41 +257,7 @@ class MTVServicesInfoExtractor(InfoExtractor):
     def _extract_child_with_type(parent, t):
         return next(c for c in parent['children'] if c.get('type') == t)
 
-    def _extract_new_triforce_mgid(self, webpage, url='', video_id=None):
-        if url == '':
-            return
-        domain = get_domain(url)
-        if domain is None:
-            raise ExtractorError(
-                '[%s] could not get domain' % self.IE_NAME,
-                expected=True)
-        url = url.replace("https://", "http://")
-        enc_url = compat_urlparse.quote(url, safe='')
-        _TRIFORCE_V8_TEMPLATE = 'https://%s/feeds/triforce/manifest/v8?url=%s'
-        triforce_manifest_url = _TRIFORCE_V8_TEMPLATE % (domain, enc_url)
-
-        manifest = self._download_json(triforce_manifest_url, video_id, fatal=False)
-        if manifest:
-            if manifest.get('manifest').get('type') == 'redirect':
-                self.to_screen('Found a redirect. Downloading manifest from new location')
-                new_loc = manifest.get('manifest').get('newLocation')
-                new_loc = new_loc.replace("https://", "http://")
-                enc_new_loc = compat_urlparse.quote(new_loc, safe='')
-                triforce_manifest_new_loc = _TRIFORCE_V8_TEMPLATE % (domain, enc_new_loc)
-                manifest = self._download_json(triforce_manifest_new_loc, video_id, fatal=False)
-
-        item_id = try_get(manifest, lambda x: x['manifest']['reporting']['itemId'], compat_str)
-        if not item_id:
-            self.to_screen('No id found!')
-            return
-
-        # 'episode' can be anything. 'content' is used often as well
-        _MGID_TEMPLATE = 'mgid:arc:episode:%s:%s'
-        mgid = _MGID_TEMPLATE % (domain, item_id)
-
-        return mgid
-
-    def _extract_mgid(self, webpage, url, title=None, data_zone=None):
+    def _extract_mgid(self, webpage):
         try:
             # the url can be http://media.mtvnservices.com/fb/{mgid}.swf
             # or http://media.mtvnservices.com/{mgid}
@@ -303,21 +267,6 @@ class MTVServicesInfoExtractor(InfoExtractor):
                 mgid = mgid[:-4]
         except RegexNotFoundError:
             mgid = None
-
-        if not title:
-            title = url_basename(url)
-
-        try:
-            window_data = self._parse_json(self._search_regex(
-                r'(?s)window.__DATA__ = (?P<json>{.+});', webpage,
-                'JSON Window Data', default=None, fatal=False, group='json'), title, fatal=False)
-            main_container = None
-            for i in range(len(window_data['children'])):
-                if window_data['children'][i]['type'] == 'MainContainer':
-                    main_container = window_data['children'][i]
-            mgid = main_container['children'][0]['props']['media']['video']['config']['uri']
-        except (KeyError, IndexError, TypeError):
-            pass
 
         if mgid is None or ':' not in mgid:
             mgid = self._search_regex(
@@ -331,10 +280,7 @@ class MTVServicesInfoExtractor(InfoExtractor):
                 r'embed/(mgid:.+?)["\'&?/]', sm4_embed, 'mgid', default=None)
 
         if not mgid:
-            mgid = self._extract_new_triforce_mgid(webpage, url)
-
-        if not mgid:
-            mgid = self._extract_triforce_mgid(webpage, data_zone)
+            mgid = self._extract_triforce_mgid(webpage)
 
         if not mgid:
             data = self._parse_json(self._search_regex(
@@ -348,8 +294,8 @@ class MTVServicesInfoExtractor(InfoExtractor):
     def _real_extract(self, url):
         title = url_basename(url)
         webpage = self._download_webpage(url, title)
-        mgid = self._extract_mgid(webpage, url, title=title)
-        videos_info = self._get_videos_info(mgid, url=url)
+        mgid = self._extract_mgid(webpage)
+        videos_info = self._get_videos_info(mgid)
         return videos_info
 
 

commit 02def2714cfe54d63943d058229cb2dc9bef8248

[southpark] Fix SouthParkDE (#812)
This was broken by https://github.com/yt-dlp/yt-dlp/commit/ee1e05581e32114c52e75e90983a66fb25fbc730
Authored by: coletdjnz

yt_dlp/extractor/mtv.py
@@ -44,7 +44,7 @@ class MTVServicesInfoExtractor(InfoExtractor):
         # Remove the templates, like &device={device}
         return re.sub(r'&[^=]*?={.*?}(?=(&|$))', '', url)
 
-    def _get_feed_url(self, uri):
+    def _get_feed_url(self, uri, url=None):
         return self._FEED_URL
 
     def _get_thumbnail_url(self, uri, itemdoc):
@@ -229,9 +229,9 @@ class MTVServicesInfoExtractor(InfoExtractor):
             data['lang'] = self._LANG
         return data
 
-    def _get_videos_info(self, uri, use_hls=True):
+    def _get_videos_info(self, uri, use_hls=True, url=None):
         video_id = self._id_from_uri(uri)
-        feed_url = self._get_feed_url(uri)
+        feed_url = self._get_feed_url(uri, url)
         info_url = update_url_query(feed_url, self._get_feed_query(uri))
         return self._get_videos_info_from_url(info_url, video_id, use_hls)
 
@@ -323,7 +323,7 @@ class MTVServicesInfoExtractor(InfoExtractor):
         title = url_basename(url)
         webpage = self._download_webpage(url, title)
         mgid = self._extract_mgid(webpage)
-        videos_info = self._get_videos_info(mgid)
+        videos_info = self._get_videos_info(mgid, url=url)
         return videos_info
 
 
@@ -352,7 +352,7 @@ class MTVServicesEmbeddedIE(MTVServicesInfoExtractor):
         if mobj:
             return mobj.group('url')
 
-    def _get_feed_url(self, uri):
+    def _get_feed_url(self, uri, url=None):
         video_id = self._id_from_uri(uri)
         config = self._download_json(
             'http://media.mtvnservices.com/pmt/e1/access/index.html?uri=%s&configtype=edge' % uri, video_id)

yt_dlp/extractor/southpark.py
@@ -56,9 +56,7 @@ class SouthParkEsIE(SouthParkIE):
 
 class SouthParkDeIE(SouthParkIE):
     IE_NAME = 'southpark.de'
-    _VALID_URL = r'https?://(?:www\.)?(?P<url>southpark\.de/(?:(en/(videoclip|collections|episodes))|(videoclip|collections|folgen))/(?P<id>(?P<unique_id>.+?)/.+?)(?:\?|#|$))'
-    # _FEED_URL = 'http://feeds.mtvnservices.com/od/feed/intl-mrss-player-feed'
-
+    _VALID_URL = r'https?://(?:www\.)?(?P<url>southpark\.de/(?:(en/(videoclip|collections|episodes|video-clips))|(videoclip|collections|folgen))/(?P<id>(?P<unique_id>.+?)/.+?)(?:\?|#|$))'
     _TESTS = [{
         'url': 'https://www.southpark.de/videoclip/rsribv/south-park-rueckzug-zum-gummibonbon-wald',
         'only_matching': True,
@@ -68,6 +66,41 @@ class SouthParkDeIE(SouthParkIE):
     }, {
         'url': 'https://www.southpark.de/collections/zzno5a/south-park-good-eats/7q26gp',
         'only_matching': True,
+    }, {
+        # clip
+        'url': 'https://www.southpark.de/en/video-clips/ct46op/south-park-tooth-fairy-cartman',
+        'info_dict': {
+            'id': 'e99d45ea-ed00-11e0-aca6-0026b9414f30',
+            'ext': 'mp4',
+            'title': 'Tooth Fairy Cartman',
+            'description': 'md5:db02e23818b4dc9cb5f0c5a7e8833a68',
+        },
+    }, {
+        # episode
+        'url': 'https://www.southpark.de/en/episodes/yy0vjs/south-park-the-pandemic-special-season-24-ep-1',
+        'info_dict': {
+            'id': 'f5fbd823-04bc-11eb-9b1b-0e40cf2fc285',
+            'ext': 'mp4',
+            'title': 'South Park',
+            'description': 'md5:ae0d875eff169dcbed16b21531857ac1',
+        },
+    }, {
+        # clip
+        'url': 'https://www.southpark.de/videoclip/ct46op/south-park-zahnfee-cartman',
+        'info_dict': {
+            'id': 'e99d45ea-ed00-11e0-aca6-0026b9414f30',
+            'ext': 'mp4',
+            'title': 'Zahnfee Cartman',
+            'description': 'md5:b917eec991d388811d911fd1377671ac'
+        },
+    }, {
+        # episode
+        'url': 'https://www.southpark.de/folgen/242csn/south-park-her-mit-dem-hirn-staffel-1-ep-7',
+        'info_dict': {
+            'id': '607115f3-496f-40c3-8647-2b0bcff486c0',
+            'ext': 'mp4',
+            'title': 'md5:South Park | Pink Eye | E 0107 | HDSS0107X deu | Version: 634312 | Comedy Central S1',
+        },
     }]
 
     def _get_feed_url(self, uri, url=None):
@@ -75,6 +108,9 @@ class SouthParkDeIE(SouthParkIE):
         config = self._download_json(
             'http://media.mtvnservices.com/pmt/e1/access/index.html?uri=%s&configtype=edge&ref=%s' % (uri, url), video_id)
         return self._remove_template_parameter(config['feedWithQueryParams'])
+
+    def _get_feed_query(self, uri):
+        return
 
 
 class SouthParkNlIE(SouthParkIE):