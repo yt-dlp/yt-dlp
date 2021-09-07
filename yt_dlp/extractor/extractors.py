eab3f867e246b064ff8cd38460f93623b03b4540
coletdjnz <colethedj@protonmail.com>
2021-09-07 22:49:57 +0000

    [nzherald] Add NZHeraldIE (#909)
    Authored-by: coletdjnz
    
    Related: https://github.com/ytdl-org/youtube-dl/issues/28267

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -957,6 +957,7 @@ from .nytimes import (
     NYTimesCookingIE,
 )
 from .nuvid import NuvidIE
+from .nzherald import NZHeraldIE
 from .nzz import NZZIE
 from .odatv import OdaTVIE
 from .odnoklassniki import OdnoklassnikiIE
