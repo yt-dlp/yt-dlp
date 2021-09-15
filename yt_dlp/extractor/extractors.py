e9b3f9dfddc283e317ca869c530b19904c910e06
Aleri Kaisattera <alerikaisattera@protonmail.com>
2021-09-15 23:42:28 +0600

    streamanity - new extractor

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1346,6 +1346,7 @@ from .storyfire import (
     StoryFireSeriesIE,
 )
 from .streamable import StreamableIE
+from .streamanity import StreamanityIE
 from .streamcloud import StreamcloudIE
 from .streamcz import StreamCZIE
 from .streetvoice import StreetVoiceIE
---
+++ yt_dlp/extractor/streamanity.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from .common import InfoExtractor
+
+
+class StreamanityIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?streamanity\.com/video/(?P<id>[A-Za-z0-9]+)'
+    _TEST = {
+        'url': 'https://streamanity.com/video/9DFPTnuYi8f2',
+        'info_dict': {
+            'id': '9DFPTnuYi8f2',
+            'ext': 'mp4',
+            'title': 'Bitcoin vs The Lighting Network',
+            'thumbnail': 'https://res.cloudinary.com/streamanity-next/image/upload/v1631475198/thumb/523908e0-e85b-4555-9050-78a8378835f1.png',
+            'description': '',
+            'uploader': 'Tom Bombadil (Freddy78)',
+        }
+    }
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+#       Download JSON that contains video information
+        video_info = self._download_json('https://app.streamanity.com/api/video/{}'.format(video_id), video_id)
+
+#       All relevant & useful information
+        uploader = video_info['data']['video']['author_name']
+        description = video_info['data']['video']['description']
+        play_id = video_info['data']['video']['play_id']
+        title = video_info['data']['video']['title']
+        token = video_info['data']['video']['token']
+        thumbnail = video_info['data']['video']['thumb']
+
+        actual_master_playlist_url = 'https://stream.mux.com/{}.m3u8?token={}'.format(play_id, token)
+
+        formats = self._extract_m3u8_formats(
+            actual_master_playlist_url, video_id, ext='mp4',
+            m3u8_id='hls', live=False)
+
+        return {
+            'id': video_id,
+            'title': title,
+            'description': description,
+            'uploader': uploader,
+            'is_live': False,
+            'thumbnail': thumbnail,
+            'formats': formats,
+        }
