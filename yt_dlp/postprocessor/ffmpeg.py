250a938de82fb6b023c09ce3d89471c5871ff830
shirt <2660574+shirt-dev@users.noreply.github.com>
2021-09-27 18:42:33 -0400

    [ffmpeg] Set max probesize to workaround AAC HLS stream issues (#1109)
    Fixes: #618, #998, #1039
    
    Authored by: shirt-dev

--- yt_dlp/postprocessor/ffmpeg.py
+++ yt_dlp/postprocessor/ffmpeg.py
@@ -262,7 +262,7 @@ class FFmpegPostProcessor(PostProcessor):
         oldest_mtime = min(
             os.stat(encodeFilename(path)).st_mtime for path, _ in input_path_opts if path)
 
-        cmd = [encodeFilename(self.executable, True), encodeArgument('-y')]
+        cmd = [encodeFilename(self.executable, True), encodeArgument('-y'), encodeArgument('-probesize'), encodeArgument('max')]
         # avconv does not have repeat option
         if self.basename == 'ffmpeg':
             cmd += [encodeArgument('-loglevel'), encodeArgument('repeat+info')]
