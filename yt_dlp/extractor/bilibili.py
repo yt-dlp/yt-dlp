228d9a3b6726b221a8b67a6e25951c099ca510ae
u-spec-png <54671367+u-spec-png@users.noreply.github.com>
2021-10-06 18:00:14 +0200

    [Bilibili] Fix

--- yt_dlp/extractor/bilibili.py
+++ yt_dlp/extractor/bilibili.py
@@ -800,7 +800,9 @@ class BiliIntlBaseIE(InfoExtractor):
             sub_data = self._download_json(sub_url, ep_id, fatal=False)
             if not sub_data:
                 continue
-            sub_data = self._parse_json(sub_data, ep_id)
+            sub_data = self._parse_json(sub_data, ep_id, fatal=False)
+            if not sub_data:
+                continue
             subtitles.setdefault(sub.get('key', 'en'), []).append({
                 'ext': 'srt',
                 'data': self.json2srt(sub_data)
