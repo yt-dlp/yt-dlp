yt-dlp/yt-dlp

commit 705e7c2005dfe67a905e18736c9f6345ee9d386b

[Hidive] Fix duplicate and incorrect formats

yt_dlp/extractor/hidive.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import re
-
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
@@ -14,7 +12,7 @@ from ..utils import (
 
 
 class HiDiveIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?hidive\.com/stream/(?P<title>[^/]+)/(?P<key>[^/?#&]+)'
+    _VALID_URL = r'https?://(?:www\.)?hidive\.com/stream/(?P<id>(?P<title>[^/]+)/(?P<key>[^/?#&]+))'
     # Using X-Forwarded-For results in 403 HTTP error for HLS fragments,
     # so disabling geo bypass completely
     _GEO_BYPASS = False
@@ -55,60 +53,43 @@ class HiDiveIE(InfoExtractor):
             self._LOGIN_URL, None, 'Logging in', data=urlencode_postdata(data))
 
     def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        title, key = mobj.group('title', 'key')
-        video_id = '%s/%s' % (title, key)
-        webpage = self._download_webpage(url, video_id, fatal=False)
-        data_videos = re.findall(r'data-video=\"([^\"]+)\"\s?data-captions=\"([^\"]+)\"', webpage)
-        formats = []
-        subtitles = {}
-        for data_video in data_videos:
-            _, _, _, version, audio, _, extra = data_video[0].split('_')
-            caption = data_video[1]
+        video_id, title, key = self._match_valid_url(url).group('id', 'title', 'key')
+        settings = self._download_json(
+            'https://www.hidive.com/play/settings', video_id,
+            data=urlencode_postdata({
+                'Title': title,
+                'Key': key,
+                'PlayerId': 'f4f895ce1ca713ba263b91caeb1daa2d08904783',
+            }))
 
-            settings = self._download_json(
-                'https://www.hidive.com/play/settings', video_id,
-                data=urlencode_postdata({
-                    'Title': title,
-                    'Key': key,
-                    'PlayerId': 'f4f895ce1ca713ba263b91caeb1daa2d08904783',
-                    'Version': version,
-                    'Audio': audio,
-                    'Captions': caption,
-                    'Extra': extra,
-                }))
+        restriction = settings.get('restrictionReason')
+        if restriction == 'RegionRestricted':
+            self.raise_geo_restricted()
+        if restriction and restriction != 'None':
+            raise ExtractorError(
+                '%s said: %s' % (self.IE_NAME, restriction), expected=True)
 
-            restriction = settings.get('restrictionReason')
-            if restriction == 'RegionRestricted':
-                self.raise_geo_restricted()
-
-            if restriction and restriction != 'None':
-                raise ExtractorError(
-                    '%s said: %s' % (self.IE_NAME, restriction), expected=True)
-
-            for rendition_id, rendition in settings['renditions'].items():
-                m3u8_url = url_or_none(try_get(rendition, lambda x: x['bitrates']['hls']))
-                if not m3u8_url:
-                    continue
+        formats, subtitles, urls = [], {}, {None}
+        for rendition_id, rendition in settings['renditions'].items():
+            audio, version, extra = rendition_id.split('_')
+            m3u8_url = url_or_none(try_get(rendition, lambda x: x['bitrates']['hls']))
+            if m3u8_url not in urls:
+                urls.add(m3u8_url)
                 frmt = self._extract_m3u8_formats(
-                    m3u8_url, video_id, 'mp4', entry_protocol='m3u8_native',
-                    m3u8_id='%s-%s-%s-%s' % (version, audio, extra, caption), fatal=False)
+                    m3u8_url, video_id, 'mp4', entry_protocol='m3u8_native', m3u8_id=rendition_id, fatal=False)
                 for f in frmt:
                     f['language'] = audio
+                    f['format_note'] = f'{version}, {extra}'
                 formats.extend(frmt)
 
-                for cc_file in rendition.get('ccFiles', []):
-                    cc_url = url_or_none(try_get(cc_file, lambda x: x[2]))
-                    # name is used since we cant distinguish subs with same language code
-                    cc_lang = try_get(cc_file, (lambda x: x[1].replace(' ', '-').lower(), lambda x: x[0]), str)
-                    if cc_url and cc_lang:
-                        subtitles.setdefault(cc_lang, []).append({'url': cc_url})
+            for cc_file in rendition.get('ccFiles', []):
+                cc_url = url_or_none(try_get(cc_file, lambda x: x[2]))
+                # name is used since we cant distinguish subs with same language code
+                cc_lang = try_get(cc_file, (lambda x: x[1].replace(' ', '-').lower(), lambda x: x[0]), str)
+                if cc_url not in urls and cc_lang:
+                    urls.add(cc_url)
+                    subtitles.setdefault(cc_lang, []).append({'url': cc_url})
         self._sort_formats(formats)
-
-        season_number = int_or_none(self._search_regex(
-            r's(\d+)', key, 'season number', default=None))
-        episode_number = int_or_none(self._search_regex(
-            r'e(\d+)', key, 'episode number', default=None))
 
         return {
             'id': video_id,
@@ -116,7 +97,9 @@ class HiDiveIE(InfoExtractor):
             'subtitles': subtitles,
             'formats': formats,
             'series': title,
-            'season_number': season_number,
-            'episode_number': episode_number,
+            'season_number': int_or_none(
+                self._search_regex(r's(\d+)', key, 'season number', default=None)),
+            'episode_number': int_or_none(
+                self._search_regex(r'e(\d+)', key, 'episode number', default=None)),
             'http_headers': {'Referer': url}
         }

commit f2cad2e496843889274b79deb3f7f6e1c8c3f948

[Hidive] Fix subtitles broken by 705e7c2005dfe67a905e18736c9f6345ee9d386b

yt_dlp/extractor/hidive.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from __future__ import unicode_literals
+import re
 
 from .common import InfoExtractor
 from ..utils import (
@@ -52,15 +52,39 @@ class HiDiveIE(InfoExtractor):
         self._download_webpage(
             self._LOGIN_URL, None, 'Logging in', data=urlencode_postdata(data))
 
+    def _call_api(self, video_id, title, key, data={}, **kwargs):
+        data = {
+            **data,
+            'Title': title,
+            'Key': key,
+            'PlayerId': 'f4f895ce1ca713ba263b91caeb1daa2d08904783',
+        }
+        return self._download_json(
+            'https://www.hidive.com/play/settings', video_id,
+            data=urlencode_postdata(data), **kwargs) or {}
+
+    def _extract_subtitles_from_rendition(self, rendition, subtitles, parsed_urls):
+        for cc_file in rendition.get('ccFiles', []):
+            cc_url = url_or_none(try_get(cc_file, lambda x: x[2]))
+            # name is used since we cant distinguish subs with same language code
+            cc_lang = try_get(cc_file, (lambda x: x[1].replace(' ', '-').lower(), lambda x: x[0]), str)
+            if cc_url not in parsed_urls and cc_lang:
+                parsed_urls.add(cc_url)
+                subtitles.setdefault(cc_lang, []).append({'url': cc_url})
+
+    def _get_subtitles(self, url, video_id, title, key, subtitles, parsed_urls):
+        webpage = self._download_webpage(url, video_id, fatal=False) or ''
+        for caption in set(re.findall(r'data-captions=\"([^\"]+)\"', webpage)):
+            renditions = self._call_api(
+                video_id, title, key, {'Captions': caption}, fatal=False,
+                note=f'Downloading {caption} subtitle information').get('renditions') or {}
+            for rendition_id, rendition in renditions.items():
+                self._extract_subtitles_from_rendition(rendition, subtitles, parsed_urls)
+        return subtitles
+
     def _real_extract(self, url):
         video_id, title, key = self._match_valid_url(url).group('id', 'title', 'key')
-        settings = self._download_json(
-            'https://www.hidive.com/play/settings', video_id,
-            data=urlencode_postdata({
-                'Title': title,
-                'Key': key,
-                'PlayerId': 'f4f895ce1ca713ba263b91caeb1daa2d08904783',
-            }))
+        settings = self._call_api(video_id, title, key)
 
         restriction = settings.get('restrictionReason')
         if restriction == 'RegionRestricted':
@@ -69,12 +93,12 @@ class HiDiveIE(InfoExtractor):
             raise ExtractorError(
                 '%s said: %s' % (self.IE_NAME, restriction), expected=True)
 
-        formats, subtitles, urls = [], {}, {None}
+        formats, subtitles, parsed_urls = [], {}, {None}
         for rendition_id, rendition in settings['renditions'].items():
             audio, version, extra = rendition_id.split('_')
             m3u8_url = url_or_none(try_get(rendition, lambda x: x['bitrates']['hls']))
-            if m3u8_url not in urls:
-                urls.add(m3u8_url)
+            if m3u8_url not in parsed_urls:
+                parsed_urls.add(m3u8_url)
                 frmt = self._extract_m3u8_formats(
                     m3u8_url, video_id, 'mp4', entry_protocol='m3u8_native', m3u8_id=rendition_id, fatal=False)
                 for f in frmt:
@@ -82,19 +106,13 @@ class HiDiveIE(InfoExtractor):
                     f['format_note'] = f'{version}, {extra}'
                 formats.extend(frmt)
 
-            for cc_file in rendition.get('ccFiles', []):
-                cc_url = url_or_none(try_get(cc_file, lambda x: x[2]))
-                # name is used since we cant distinguish subs with same language code
-                cc_lang = try_get(cc_file, (lambda x: x[1].replace(' ', '-').lower(), lambda x: x[0]), str)
-                if cc_url not in urls and cc_lang:
-                    urls.add(cc_url)
-                    subtitles.setdefault(cc_lang, []).append({'url': cc_url})
+            self._extract_subtitles_from_rendition(rendition, subtitles, parsed_urls)
         self._sort_formats(formats)
 
         return {
             'id': video_id,
             'title': video_id,
-            'subtitles': subtitles,
+            'subtitles': self.extract_subtitles(url, video_id, title, key, subtitles, parsed_urls),
             'formats': formats,
             'series': title,
             'season_number': int_or_none(