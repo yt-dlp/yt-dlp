8103872cbea2c75b4dfb0b9e5281eb03d9e0f1e7
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-10-10 12:11:46 +0200

    Add macos code
    Update build.yml
    
    Update build.yml

--- pyinst.py
+++ pyinst.py
@@ -6,15 +6,23 @@ import sys
 import platform
 
 from PyInstaller.utils.hooks import collect_submodules
-from PyInstaller.utils.win32.versioninfo import (
-    VarStruct, VarFileInfo, StringStruct, StringTable,
-    StringFileInfo, FixedFileInfo, VSVersionInfo, SetVersion,
-)
+
+if platform.system() == 'Windows':
+    from PyInstaller.utils.win32.versioninfo import (
+        VarStruct, VarFileInfo, StringStruct, StringTable,
+        StringFileInfo, FixedFileInfo, VSVersionInfo, SetVersion,
+    )
 import PyInstaller.__main__
 
+suffix = ''
 arch = platform.architecture()[0][:2]
 assert arch in ('32', '64')
 _x86 = '_x86' if arch == '32' else ''
+
+if platform.system() == 'Windows':
+    suffix = _x86
+if platform.system() == 'Darwin':
+    suffix = '_macos'
 
 # Compatability with older arguments
 opts = sys.argv[1:]
@@ -37,39 +45,40 @@ VERSION_LIST = list(map(int, VERSION_LIST)) + [0] * (4 - len(VERSION_LIST))
 print('Version: %s%s' % (VERSION, _x86))
 print('Remember to update the version using devscipts\\update-version.py')
 
-VERSION_FILE = VSVersionInfo(
-    ffi=FixedFileInfo(
-        filevers=VERSION_LIST,
-        prodvers=VERSION_LIST,
-        mask=0x3F,
-        flags=0x0,
-        OS=0x4,
-        fileType=0x1,
-        subtype=0x0,
-        date=(0, 0),
-    ),
-    kids=[
-        StringFileInfo([
-            StringTable(
-                '040904B0', [
-                    StringStruct('Comments', 'yt-dlp%s Command Line Interface.' % _x86),
-                    StringStruct('CompanyName', 'https://github.com/yt-dlp'),
-                    StringStruct('FileDescription', FILE_DESCRIPTION),
-                    StringStruct('FileVersion', VERSION),
-                    StringStruct('InternalName', 'yt-dlp%s' % _x86),
-                    StringStruct(
-                        'LegalCopyright',
-                        'pukkandan.ytdlp@gmail.com | UNLICENSE',
-                    ),
-                    StringStruct('OriginalFilename', 'yt-dlp%s.exe' % _x86),
-                    StringStruct('ProductName', 'yt-dlp%s' % _x86),
-                    StringStruct(
-                        'ProductVersion',
-                        '%s%s on Python %s' % (VERSION, _x86, platform.python_version())),
-                ])]),
-        VarFileInfo([VarStruct('Translation', [0, 1200])])
-    ]
-)
+if platform.system() == 'Windows':
+    VERSION_FILE = VSVersionInfo(
+        ffi=FixedFileInfo(
+            filevers=VERSION_LIST,
+            prodvers=VERSION_LIST,
+            mask=0x3F,
+            flags=0x0,
+            OS=0x4,
+            fileType=0x1,
+            subtype=0x0,
+            date=(0, 0),
+        ),
+        kids=[
+            StringFileInfo([
+                StringTable(
+                    '040904B0', [
+                        StringStruct('Comments', 'yt-dlp%s Command Line Interface.' % _x86),
+                        StringStruct('CompanyName', 'https://github.com/yt-dlp'),
+                        StringStruct('FileDescription', FILE_DESCRIPTION),
+                        StringStruct('FileVersion', VERSION),
+                        StringStruct('InternalName', 'yt-dlp%s' % _x86),
+                        StringStruct(
+                            'LegalCopyright',
+                            'pukkandan.ytdlp@gmail.com | UNLICENSE',
+                        ),
+                        StringStruct('OriginalFilename', 'yt-dlp%s.exe' % _x86),
+                        StringStruct('ProductName', 'yt-dlp%s' % _x86),
+                        StringStruct(
+                            'ProductVersion',
+                            '%s%s on Python %s' % (VERSION, _x86, platform.python_version())),
+                    ])]),
+            VarFileInfo([VarStruct('Translation', [0, 1200])])
+        ]
+    )
 
 
 def pycryptodome_module():
@@ -86,11 +95,11 @@ def pycryptodome_module():
     return 'Cryptodome'
 
 
-dependancies = [pycryptodome_module(), 'mutagen'] + collect_submodules('websockets')
+dependancies = [pycryptodome_module(), 'mutagen', 'certifi'] + collect_submodules('websockets')
 excluded_modules = ['test', 'ytdlp_plugins', 'youtube-dl', 'youtube-dlc']
 
 PyInstaller.__main__.run([
-    '--name=yt-dlp%s' % _x86,
+    '--name=yt-dlp%s' % suffix,
     '--icon=devscripts/logo.ico',
     *[f'--exclude-module={module}' for module in excluded_modules],
     *[f'--hidden-import={module}' for module in dependancies],
@@ -99,4 +108,6 @@ PyInstaller.__main__.run([
     *opts,
     'yt_dlp/__main__.py',
 ])
-SetVersion('dist/%syt-dlp%s.exe' % ('yt-dlp/' if '--onedir' in opts else '', _x86), VERSION_FILE)
+
+if platform.system() == 'Windows':
+    SetVersion('dist/%syt-dlp%s.exe' % ('yt-dlp/' if '--onedir' in opts else '', _x86), VERSION_FILE)
