4cd497d3d9231c7efcb1b4dcbade282e1fc28052
Ashish Gupta <Ashish08@protonmail.com>
2021-10-10 14:50:40 +0530

    Apply suggestions.

--- yt_dlp/extractor/euscreen.py
+++ yt_dlp/extractor/euscreen.py
@@ -29,14 +29,22 @@ class EUScreenIE(InfoExtractor):
         'params': {'skip_download': True}
     }]
 
+    _payload = b'<fsxml><screen><properties><screenId>-1</screenId></properties><capabilities id="1"><properties><platform>Win32</platform><appcodename>Mozilla</appcodename><appname>Netscape</appname><appversion>5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36</appversion><useragent>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36</useragent><cookiesenabled>true</cookiesenabled><screenwidth>784</screenwidth><screenheight>758</screenheight><orientation>undefined</orientation><smt_browserid>Sat, 07 Oct 2021 08:56:50 GMT</smt_browserid><smt_sessionid>1633769810758</smt_sessionid></properties></capabilities></screen></fsxml>'
+
     def _real_extract(self, url):
         id = self._match_id(url)
-        args_for_js_request = self._download_webpage(f'https://euscreen.eu/lou/LouServlet/domain/euscreenxl/html5application/euscreenxlitem?actionlist=itempage&id={id}',
-                                                     id, data=b'<fsxml><screen><properties><screenId>-1</screenId></properties><capabilities id="1"><properties><platform>Win32</platform><appcodename>Mozilla</appcodename><appname>Netscape</appname><appversion>5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36</appversion><useragent>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36</useragent><cookiesenabled>true</cookiesenabled><screenwidth>784</screenwidth><screenheight>758</screenheight><orientation>undefined</orientation><smt_browserid>Sat, 07 Oct 2021 08:56:50 GMT</smt_browserid><smt_sessionid>1633769810758</smt_sessionid></properties></capabilities></screen></fsxml>')
-        info_js = self._download_webpage('https://euscreen.eu/lou/LouServlet/domain/euscreenxl/html5application/euscreenxlitem',
-                                         id, data=args_for_js_request.replace('screenid', 'screenId').encode())
-        video_json = self._parse_json(js_to_json(self._search_regex(r'setVideo\(({.+})\)\(\$end\$\)put', info_js, 'Video JSON')), id)
-        meta_json = self._parse_json(js_to_json(self._search_regex(r'setData\(({.+})\)\(\$end\$\)', info_js, 'Video JSON')), id)
+        args_for_js_request = self._download_webpage(
+            'https://euscreen.eu/lou/LouServlet/domain/euscreenxl/html5application/euscreenxlitem',
+            id, data=self._payload, query={'actionlist': 'itempage', 'id': id})
+        info_js = self._download_webpage(
+            'https://euscreen.eu/lou/LouServlet/domain/euscreenxl/html5application/euscreenxlitem',
+            id, data=args_for_js_request.replace('screenid', 'screenId').encode())
+        video_json = self._parse_json(
+            self._search_regex(r'setVideo\(({.+})\)\(\$end\$\)put', info_js, 'Video JSON'),
+            id, transform_source=js_to_json)
+        meta_json = self._parse_json(
+            self._search_regex(r'setData\(({.+})\)\(\$end\$\)', info_js, 'Metadata JSON'),
+            id, transform_source=js_to_json)
         formats = [{
             'url': source['src'],
         } for source in video_json.get('sources', [])]
