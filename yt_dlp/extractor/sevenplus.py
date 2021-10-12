0df313510c291a79bfc57f03c23a90365172d9c6
pukkandan <pukkandan.ytdlp@gmail.com>
2021-10-12 13:54:39 +0530

    Make login process non-fatal

--- yt_dlp/extractor/sevenplus.py
+++ yt_dlp/extractor/sevenplus.py
@@ -47,44 +47,40 @@ class SevenPlusIE(BrightcoveNewIE):
         self.token = None
 
         cookies = self._get_cookies('https://7plus.com.au')
-        if not cookies:
+        api_key = next((x for x in cookies if x.startswith('glt_')), '')[4:]
+        if not api_key:  # Cookies are signed out, skip login
             return
 
-        try:
-            api_key = next(x for x in cookies if x.startswith('glt_'))[4:]
-            login_token = cookies[f'glt_{api_key}'].value
-        except StopIteration:
-            # Cookies are signed out, skip login
-            return
-
-        login_resp = self._download_json('https://login.7plus.com.au/accounts.getJWT', None, 'Logging in', query={
-            'APIKey': api_key,
-            'sdk': 'js_latest',
-            'login_token': login_token,
-            'authMode': 'cookie',
-            'pageURL': 'https://7plus.com.au/',
-            'sdkBuild': '12471',
-            'format': 'json',
-        })
+        login_resp = self._download_json(
+            'https://login.7plus.com.au/accounts.getJWT', None, 'Logging in', fatal=False,
+            query={
+                'APIKey': api_key,
+                'sdk': 'js_latest',
+                'login_token': cookies[f'glt_{api_key}'].value,
+                'authMode': 'cookie',
+                'pageURL': 'https://7plus.com.au/',
+                'sdkBuild': '12471',
+                'format': 'json',
+            }) or {}
 
         if 'errorMessage' in login_resp:
-            raise ExtractorError(f'7plus returned an error: {login_resp["errorMessage"]}', expected=True)
+            self.report_warning(f'Unable to login: 7plus said: {login_resp["errorMessage"]}')
+            return
+        id_token = login_resp.get('id_token')
+        if not id_token:
+            self.report_warning('Unable to login: Could not extract id token')
+            return
 
-        try:
-            id_token = login_resp['id_token']
-        except KeyError:
-            raise ExtractorError('Unable to extract id token')
-
-        token_resp = self._download_json('https://7plus.com.au/auth/token', None, 'Getting auth token', data=json.dumps({
-            'idToken': id_token,
-            'platformId': 'web',
-            'regSource': '7plus',
-        }).encode('utf-8'), headers={'Content-Type': 'application/json'})
-
-        try:
-            self.token = token_resp['token']
-        except KeyError:
-            raise ExtractorError('Unable to extract auth token')
+        token_resp = self._download_json(
+            'https://7plus.com.au/auth/token', None, 'Getting auth token', fatal=False,
+            headers={'Content-Type': 'application/json'}, data=json.dumps({
+                'idToken': id_token,
+                'platformId': 'web',
+                'regSource': '7plus',
+            }).encode('utf-8')) or {}
+        self.token = token_resp.get('token')
+        if not self.token:
+            self.report_warning('Unable to log in: Could not extract auth token')
 
     def _real_extract(self, url):
         path, episode_id = self._match_valid_url(url).groups()
