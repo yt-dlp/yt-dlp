ab630a57b9df229fa150a7eaa19ff51750597fbc
pukkandan <pukkandan.ytdlp@gmail.com>
2021-10-28 02:14:33 +0530

    [viewlift] Fix typo in 5be76d1ab7fed65a5894b221c7b7f896a18fc820

--- yt_dlp/extractor/viewlift.py
+++ yt_dlp/extractor/viewlift.py
@@ -92,81 +92,6 @@ class ViewLiftEmbedIE(ViewLiftBaseIE):
     }, {
         'url': 'http://www.snagfilms.com/embed/player?filmId=0000014c-de2f-d5d6-abcf-ffef58af0017',
         'only_matching': True,
-    }, {  # Free film with langauge code
-        'url': 'https://www.hoichoi.tv/bn/films/title/shuyopoka',
-        'info_dict': {
-            'id': '7a7a9d33-1f4c-4771-9173-ee4fb6dbf196',
-            'ext': 'mp4',
-            'title': 'Shuyopoka',
-            'description': 'md5:e28f2fb8680096a69c944d37c1fa5ffc',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20211006',
-            'series': None
-        },
-        'params': {'skip_download': True},
-    }, {  # Free film
-        'url': 'https://www.hoichoi.tv/films/title/dadu-no1',
-        'info_dict': {
-            'id': '0000015b-b009-d126-a1db-b81ff3780000',
-            'ext': 'mp4',
-            'title': 'Dadu No.1',
-            'description': 'md5:605cba408e51a79dafcb824bdeded51e',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20210827',
-            'series': None
-        },
-        'params': {'skip_download': True},
-    }, {  # Free episode
-        'url': 'https://www.hoichoi.tv/webseries/case-jaundice-s01-e01',
-        'info_dict': {
-            'id': 'f779e07c-30c8-459c-8612-5a834ab5e5ba',
-            'ext': 'mp4',
-            'title': 'Humans Vs. Corona',
-            'description': 'md5:ca30a682b4528d02a3eb6d0427dd0f87',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20210830',
-            'series': 'Case Jaundice'
-        },
-        'params': {'skip_download': True},
-    }, {  # Free video
-        'url': 'https://www.hoichoi.tv/videos/1549072415320-six-episode-02-hindi',
-        'info_dict': {
-            'id': 'b41fa1ce-aca6-47b6-b208-283ff0a2de30',
-            'ext': 'mp4',
-            'title': 'Woman in red - Hindi',
-            'description': 'md5:9d21edc1827d32f8633eb67c2054fc31',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20211006',
-            'series': 'Six (Hindi)'
-        },
-        'params': {'skip_download': True},
-    }, {  # Free episode
-        'url': 'https://www.hoichoi.tv/shows/watch-asian-paints-moner-thikana-online-season-1-episode-1',
-        'info_dict': {
-            'id': '1f45d185-8500-455c-b88d-13252307c3eb',
-            'ext': 'mp4',
-            'title': 'Jisshu Sengupta',
-            'description': 'md5:ef6ffae01a3d83438597367400f824ed',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20211004',
-            'series': 'Asian Paints Moner Thikana'
-        },
-        'params': {'skip_download': True},
-    }, {  # Free series
-        'url': 'https://www.hoichoi.tv/shows/watch-moner-thikana-bengali-web-series-online',
-        'playlist_mincount': 5,
-        'info_dict': {
-            'id': 'watch-moner-thikana-bengali-web-series-online',
-        },
-    }, {  # Premium series
-        'url': 'https://www.hoichoi.tv/shows/watch-byomkesh-bengali-web-series-online',
-        'playlist_mincount': 14,
-        'info_dict': {
-            'id': 'watch-byomkesh-bengali-web-series-online',
-        },
-    }, {  # Premium movie
-        'url': 'https://www.hoichoi.tv/movies/detective-2020',
-        'only_matching': True
     }]
 
     @staticmethod
@@ -310,6 +235,81 @@ class ViewLiftIE(ViewLiftBaseIE):
     }, {
         'url': 'https://www.marquee.tv/watch/sadlerswells-sacredmonsters',
         'only_matching': True,
+    }, {  # Free film with langauge code
+        'url': 'https://www.hoichoi.tv/bn/films/title/shuyopoka',
+        'info_dict': {
+            'id': '7a7a9d33-1f4c-4771-9173-ee4fb6dbf196',
+            'ext': 'mp4',
+            'title': 'Shuyopoka',
+            'description': 'md5:e28f2fb8680096a69c944d37c1fa5ffc',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'upload_date': '20211006',
+            'series': None
+        },
+        'params': {'skip_download': True},
+    }, {  # Free film
+        'url': 'https://www.hoichoi.tv/films/title/dadu-no1',
+        'info_dict': {
+            'id': '0000015b-b009-d126-a1db-b81ff3780000',
+            'ext': 'mp4',
+            'title': 'Dadu No.1',
+            'description': 'md5:605cba408e51a79dafcb824bdeded51e',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'upload_date': '20210827',
+            'series': None
+        },
+        'params': {'skip_download': True},
+    }, {  # Free episode
+        'url': 'https://www.hoichoi.tv/webseries/case-jaundice-s01-e01',
+        'info_dict': {
+            'id': 'f779e07c-30c8-459c-8612-5a834ab5e5ba',
+            'ext': 'mp4',
+            'title': 'Humans Vs. Corona',
+            'description': 'md5:ca30a682b4528d02a3eb6d0427dd0f87',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'upload_date': '20210830',
+            'series': 'Case Jaundice'
+        },
+        'params': {'skip_download': True},
+    }, {  # Free video
+        'url': 'https://www.hoichoi.tv/videos/1549072415320-six-episode-02-hindi',
+        'info_dict': {
+            'id': 'b41fa1ce-aca6-47b6-b208-283ff0a2de30',
+            'ext': 'mp4',
+            'title': 'Woman in red - Hindi',
+            'description': 'md5:9d21edc1827d32f8633eb67c2054fc31',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'upload_date': '20211006',
+            'series': 'Six (Hindi)'
+        },
+        'params': {'skip_download': True},
+    }, {  # Free episode
+        'url': 'https://www.hoichoi.tv/shows/watch-asian-paints-moner-thikana-online-season-1-episode-1',
+        'info_dict': {
+            'id': '1f45d185-8500-455c-b88d-13252307c3eb',
+            'ext': 'mp4',
+            'title': 'Jisshu Sengupta',
+            'description': 'md5:ef6ffae01a3d83438597367400f824ed',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'upload_date': '20211004',
+            'series': 'Asian Paints Moner Thikana'
+        },
+        'params': {'skip_download': True},
+    }, {  # Free series
+        'url': 'https://www.hoichoi.tv/shows/watch-moner-thikana-bengali-web-series-online',
+        'playlist_mincount': 5,
+        'info_dict': {
+            'id': 'watch-moner-thikana-bengali-web-series-online',
+        },
+    }, {  # Premium series
+        'url': 'https://www.hoichoi.tv/shows/watch-byomkesh-bengali-web-series-online',
+        'playlist_mincount': 14,
+        'info_dict': {
+            'id': 'watch-byomkesh-bengali-web-series-online',
+        },
+    }, {  # Premium movie
+        'url': 'https://www.hoichoi.tv/movies/detective-2020',
+        'only_matching': True
     }]
 
     @classmethod
