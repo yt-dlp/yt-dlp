b9dccc4bff25040d818b49caa9e18ee6f3f6c43c
pukkandan <pukkandan.ytdlp@gmail.com>
2021-11-05 00:57:56 +0530

    Update yt_dlp/extractor/breitbart.py

--- yt_dlp/extractor/breitbart.py
+++ yt_dlp/extractor/breitbart.py
@@ -18,18 +18,16 @@ class BreitBartIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        video_title = self._og_search_title(
-            webpage, default=None) or self._html_search_regex(
-            r'(?s)<title>(.*?)</title>', webpage, 'video title')
-        video_description = self._og_search_description(webpage)
-        video_thumbnail = self._og_search_thumbnail(webpage)
-
-        age_limit = self._rta_search(webpage)
-        info_dict = {'id': video_id, 'title': video_title, 'description': video_description,
-                     'thumbnail': video_thumbnail, 'age_limit': age_limit,
-                     'formats': self._extract_m3u8_formats(f'https://cdn.jwplayer.com/manifests/{video_id}.m3u8',
-                                                           video_id, ext='mp4')}
-
-        self._sort_formats(info_dict['formats'])
-
-        return info_dict
+        formats = self._extract_m3u8_formats(f'https://cdn.jwplayer.com/manifests/{video_id}.m3u8', video_id, ext='mp4')
+        self._sort_formats(formats)
+        return {
+            'id': video_id,
+            'title': self._og_search_title(
+                webpage, default=None) or self._html_search_regex(
+                r'(?s)<title>(.*?)</title>', webpage, 'video title'),
+            'description': self._og_search_description(webpage),
+            'thumbnail': self._og_search_thumbnail(webpage),
+            'age_limit': self._rta_search(webpage),
+            'thumbnail': video_thumbnail,
+            'formats': formats
+        }
