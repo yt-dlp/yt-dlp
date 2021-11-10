e2e48d38da30ca0550b11f67568442aa814839ce
u-spec-png <54671367+u-spec-png@users.noreply.github.com>
2021-11-10 13:58:44 +0000

    Update yt_dlp/extractor/tokentube.py
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/extractor/tokentube.py
+++ yt_dlp/extractor/tokentube.py
@@ -92,7 +92,8 @@ class TokentubeIE(InfoExtractor):
             r'<a\s*class="place-left"[^>]+>(.+?)</a>',
             webpage, 'uploader', fatal=False)
 
-        description = clean_html(get_element_by_class('p-d-txt', webpage)) or self._html_search_meta(('og:description', 'description', 'twitter:description'), webpage)
+        description = (clean_html(get_element_by_class('p-d-txt', webpage))
+                       or self._html_search_meta(('og:description', 'description', 'twitter:description'), webpage))
 
         if description and description.endswith('Category'):
             description = description[:-8]
