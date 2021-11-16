0852bb25bd2e3639da57b021dce35ac634258d52
zulaport <70630440+zulaport@users.noreply.github.com>
2021-11-15 18:57:29 -0800

    Apply suggestions from code review
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/extractor/stripchat.py
+++ yt_dlp/extractor/stripchat.py
@@ -20,11 +20,8 @@ class StripchatIE(InfoExtractor):
             'id': 'feel_me',
             'ext': 'mp4',
             'title': 're:^feel_me [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$',
-            'description': 're:^.*$',
+            'description': str,
             'is_live': True,
-        },
-        'params': {
-            'skip_download': True,
         },
         'skip': 'Room is offline',
     }
@@ -46,28 +43,19 @@ class StripchatIE(InfoExtractor):
         if not data:
             raise ExtractorError('Unable to find configuration for stream.')
 
-        show = try_get(data, lambda x: x['viewCam']['show'], dict)
-        if show:
-            raise ExtractorError(self._ROOM_PRIVATE, expected=True)
-
-        is_live = try_get(data, lambda x: x['viewCam']['model']['isLive'], bool)
-        if not is_live:
-            raise ExtractorError(self._ROOM_OFFLINE, expected=True)
+        if try_get(data, lambda x: x['viewCam']['show'], dict):
+            raise ExtractorError('Model is in private show', expected=True)
+        elif not try_get(data, lambda x: x['viewCam']['model']['isLive'], bool):
+            raise ExtractorError('Model is offline', expected=True)
 
         server = try_get(data, lambda x: x['viewCam']['viewServers']['flashphoner-hls'], compat_str)
         host = try_get(data, lambda x: x['config']['data']['hlsStreamHost'], compat_str)
         model_id = try_get(data, lambda x: x['viewCam']['model']['id'], int)
 
-        m3u8_url = 'https://b-%s.%s/hls/%d/%d.m3u8' % (server, host, model_id, model_id)
-
         formats = self._extract_m3u8_formats(
-            m3u8_url,
-            video_id,
-            ext='mp4',
-            entry_protocol='m3u8_native',
-            m3u8_id='hls',
-            fatal=False,
-            live=True)
+            'https://b-%s.%s/hls/%d/%d.m3u8' % (server, host, model_id, model_id),
+            video_id, ext='mp4', m3u8_id='hls', fatal=False, live=True)
+        self._sort_formats(formats)
 
         return {
             'id': video_id,
