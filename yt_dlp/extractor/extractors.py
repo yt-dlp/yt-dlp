77fcc6515852bc2e1c6960a6e010ab2ff1caf1ee
Ashish Gupta <39122144+Ashish0804@users.noreply.github.com>
2021-11-20 14:55:14 +0530

    [CozyTV] Add extractor (#1727)
    Authored by: Ashish0804

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -293,6 +293,7 @@ from .commonprotocols import (
 from .condenast import CondeNastIE
 from .contv import CONtvIE
 from .corus import CorusIE
+from .cozytv import CozyTVIE
 from .cracked import CrackedIE
 from .crackle import CrackleIE
 from .crooksandliars import CrooksAndLiarsIE
