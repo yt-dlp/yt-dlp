d319e829a4f255345b12baec63ca29ef6c3640ff
Henrik Heimbuerger <henrik@heimbuerger.de>
2021-11-25 19:59:20 +0100

    [nebula] Factor out _login method

--- yt_dlp/extractor/nebula.py
+++ yt_dlp/extractor/nebula.py
@@ -86,7 +86,7 @@ class NebulaBaseIE(InfoExtractor):
             # if 401 or 403, attempt credential re-auth and retry
             if exc.cause and isinstance(exc.cause, urllib.error.HTTPError) and exc.cause.code in (401, 403):
                 self.to_screen(f'Reauthenticating to Nebula and retrying, because last {auth_type} call resulted in error {exc.cause.code}')
-                self._real_initialize()
+                self._login()
                 return inner_call()
             else:
                 raise
@@ -148,10 +148,13 @@ class NebulaBaseIE(InfoExtractor):
             'creator': episode['channel_title'],
         }
 
-    def _real_initialize(self):
+    def _login(self):
         self._nebula_api_token = self._retrieve_nebula_api_token()
         self._nebula_bearer_token = self._fetch_nebula_bearer_token()
         self._zype_access_token = self._fetch_zype_access_token()
+
+    def _real_initialize(self):
+        self._login()
 
 
 class NebulaIE(NebulaBaseIE):
