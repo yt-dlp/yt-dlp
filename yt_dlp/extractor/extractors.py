bf57cfa8b7a64f5b8382fa17a74af8b9aa2ffa55
Deer-Spangle <60626596+Deer-Spangle@users.noreply.github.com>
2021-11-28 05:04:06 +0000

    [RedGifs] Add Search and User extractors (#1808)
    Authored by: Deer-Spangle

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1216,7 +1216,11 @@ from .redbulltv import (
     RedBullIE,
 )
 from .reddit import RedditIE
-from .redgifs import RedGifsIE
+from .redgifs import (
+    RedGifsIE,
+    RedGifsSearchIE,
+    RedGifsUserIE,
+)
 from .redtube import RedTubeIE
 from .regiotv import RegioTVIE
 from .rentv import (
