69c85ce3712275ef36ae4b64ade6d2624b8ab05c
Jertzukka <jertzukka@gmail.com>
2021-12-01 12:19:05 +0200

    Formatting, proposed fixes

--- yt_dlp/extractor/gofile.py
+++ yt_dlp/extractor/gofile.py
@@ -9,7 +9,7 @@ class GofileIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?gofile\.io/d/(?P<id>[^/]+)'
     _TESTS = [{
         'url': 'https://gofile.io/d/AMZyDw',
-        'md5': '57c04f65556ddbecf9697528274c3212',
+        'md5': '774879ad31bc62c23269da8bf490d025',
         'info_dict': {
             'id': 'de571ac1-5edc-42e2-8ec2-bdac83ad4a31',
             'filesize': 928116,
@@ -23,24 +23,20 @@ class GofileIE(InfoExtractor):
 
         # Create guest account
         accountdata = self._download_json_handle('https://api.gofile.io/createAccount', 'Gofile', note='Getting a new guest account')
-        status = accountdata[0]['status']
         accountToken = accountdata[0]['data']['token']
-        print("[Gofile] Status:", status, "accountToken:", accountToken)
 
         # Get file list
-        requesturl = f"https://api.gofile.io/getContent?contentId={file_id}&token={accountToken}&websiteToken=websiteToken&cache=true"
+        requesturl = f'https://api.gofile.io/getContent?contentId={file_id}&token={accountToken}&websiteToken=websiteToken&cache=true'
         filelist = self._download_json_handle(requesturl, 'Gofile', note='Getting filelist')
 
         # Create all entries
         def entries():
             contents = filelist[0]['data']['contents']
-            key_list = [key for key in contents]
 
-            for key in key_list:
+            for _, file in contents.items():
                 try:
-                    file = contents[key]
                     filedata = {
-                        'id': file_id,
+                        'id': file['id'],
                         'title': file['name'],
                         'url': file['directLink'],
                         'filesize': file['size'],
@@ -48,7 +44,7 @@ class GofileIE(InfoExtractor):
                     }
                     yield filedata
                 except ExtractorError as e:
-                    print(e)
+                    raise ExtractorError(e)
 
         # Set accountToken to allow downloads
         self._set_cookie('gofile.io', 'accountToken', accountToken)
