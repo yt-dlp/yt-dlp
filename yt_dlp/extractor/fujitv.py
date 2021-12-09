c4ae1e0ab6eecbce7dd16276e4ac5e62a28c030a
YuenSzeHong <40477634+YuenSzeHong@users.noreply.github.com>
2021-12-09 21:09:23 +0800

    reverted changes on fujitv.py

--- yt_dlp/extractor/fujitv.py
+++ yt_dlp/extractor/fujitv.py
@@ -5,19 +5,19 @@ from .common import InfoExtractor
 
 
 class FujiTVFODPlus7IE(InfoExtractor):
-    _VALID_URL = r'https?://fod\.fujitv\.co\.jp/title/[0-9a-z]{4}/(?P<id>[0-9a-z]+)'
+    _VALID_URL = r'https?://i\.fod\.fujitv\.co\.jp/title/[0-9a-z]{4}/(?P<id>[0-9a-z]+)'
     _BASE_URL = 'http://i.fod.fujitv.co.jp/'
     _BITRATE_MAP = {
+        300: (320, 180),
         800: (640, 360),
         1200: (1280, 720),
         2000: (1280, 720),
-        4000: (1920, 1080),
     }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         formats = self._extract_m3u8_formats(
-            self._BASE_URL + 'abr/tv_android/%s.m3u8' % video_id, video_id, 'mp4')
+            self._BASE_URL + 'abr/pc_html5/%s.m3u8' % video_id, video_id, 'mp4')
         for f in formats:
             wh = self._BITRATE_MAP.get(f.get('tbr'))
             if wh:
