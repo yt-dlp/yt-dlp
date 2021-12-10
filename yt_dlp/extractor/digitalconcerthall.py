c715e49114c24cb27d99ce27095e4f0e0d299c4f
Robert Jacobson <teridon@gmail.com>
2021-12-09 19:41:38 -0500

    do not use ExtractorError for login errors

--- yt_dlp/extractor/digitalconcerthall.py
+++ yt_dlp/extractor/digitalconcerthall.py
@@ -37,9 +37,8 @@ class DigitalConcertHallIE(InfoExtractor):
 
     def _login(self):
         username, password = self._get_login_info()
-        if username is None:
-            raise ExtractorError('No login info available, needed for using %s.' % self.IE_NAME, expected=True)
-            return
+        if not username:
+            self.raise_login_required()
         token_response = self._download_json(
             self._OAUTH_URL,
             None, 'Obtaining token', errnote='Unable to obtain token', data=urlencode_postdata({
@@ -56,24 +55,17 @@ class DigitalConcertHallIE(InfoExtractor):
         try:
             self._download_json(
                 self._OAUTH_URL,
-                None, 'Logging in', errnote='Unable to login', data=urlencode_postdata({
+                None, note='Logging in', errnote='Unable to login', data=urlencode_postdata({
                     'grant_type': 'password',
                     'username': username,
                     'password': password,
                 }), headers={
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Referer': 'https://www.digitalconcerthall.com/en/login',
+                    'Referer': 'https://www.digitalconcerthall.com',
                     'Authorization': 'Bearer ' + self._ACCESS_TOKEN
                 })
-        except ExtractorError as e:
-            msg = 'Unable to login'
-            if isinstance(e.cause, compat_HTTPError) and e.cause.code == 401:
-                resp = self._parse_json(e.cause.read().decode(), None, fatal=False)
-                if resp:
-                    error = resp.get('extra_info') or resp.get('error_description') or resp.get('error')
-                    if error:
-                        msg += ': ' + error
-            raise ExtractorError('Unable to log in: ' + msg)
+        except:
+            self.raise_login_required(msg='Login info incorrect')
 
     def _real_initialize(self):
         self._login()
