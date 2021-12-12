744b8075519b4daeb31cd73aa8c0003e11c8ba0e
YuenSzeHong <szehong26@gmail.com>
2021-12-12 11:53:20 +0800

    added programme name to title

--- yt_dlp/extractor/fujitv.py
+++ yt_dlp/extractor/fujitv.py
@@ -19,7 +19,7 @@ class FujiTVFODPlus7IE(InfoExtractor):
         'url': 'https://fod.fujitv.co.jp/title/5d40/5d40810075',
         'info_dict': {
             'id': '5d40810075',
-            'title': '#1317 『おっちゃんのまほうカード』の巻／『まるちゃん おばけ屋敷にいく』の巻',
+            'title': 'ちびまる子ちゃん　#1317 『おっちゃんのまほうカード』の巻／『まるちゃん おばけ屋敷にいく』の巻',
             'description': '【原作35周年！あなたの好きな“神回”さくらももこ原作まつり】\n明日から夏休み。まる子は、一学期の荷物をいっきにひきずりながら歩いていた。下校途中の木の下で、おじさんが不思議なカードを売っているのを見かけたまる子とお姉ちゃんは、どうしても欲しくなり…／お母さんにデパートでやっているおばけ屋敷に連れていってとせが むまる子。あっさり却下されるが、お父さんが実は自分も行きたかったと連れて行ってくれることに。いよいよおばけ屋敷に入ったまる子と父ヒロシ。次々あらわれるおばけに二人の運命は…？',
             'ext': 'mp4',
             'format_id': '4000',
@@ -46,7 +46,7 @@ class FujiTVFODPlus7IE(InfoExtractor):
 
         return {
             'id': video_id,
-            'title': json_info.get('ep_title'),
+            'title': f"{json_info.get('lu_title')}　{json_info.get('ep_title')}",
             'description': json_info.get('ep_description'),
             'formats': formats,
             'thumbnail': self._BASE_URL + 'pc/image/wbtn/wbtn_%s.jpg' % video_id,
