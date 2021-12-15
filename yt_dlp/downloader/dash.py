1a80e8d94a89a0d11585cf6f68f0ce1f98f5ddb4
pukkandan <pukkandan.ytdlp@gmail.com>
2021-12-15 08:10:16 +0530

    Move `YoutubeDlFromStartDashFD` to dash.py

--- yt_dlp/downloader/dash.py
+++ yt_dlp/downloader/dash.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 from ..downloader import get_suitable_downloader
 from .fragment import FragmentFD
 
-from ..utils import urljoin, time_millis
+from ..utils import (
+    int_or_none,
+    time_millis,
+    traverse_obj,
+    urljoin,
+)
 
 
 class DashSegmentsFD(FragmentFD):
@@ -86,3 +91,36 @@ class DashSegmentsFD(FragmentFD):
     @staticmethod
     def _ignore_lethal_error():
         return False
+
+
+class YoutubeDlFromStartDashFD(DashSegmentsFD):
+
+    FD_NAME = 'ytlivestartdash'
+
+    def _calculate_fragment_count(self, info_dict):
+        return True, None
+
+    def _get_fragments(self, info_dict, root_info_dict, ctx):
+        manifest_url = info_dict.get('manifest_url')
+        if not manifest_url:
+            self.report_error('URL for MPD manifest is not known; there is a problem in YoutubeIE code')
+
+        stream_number = info_dict.get('manifest_stream_number', 0)
+
+        download_start_time = ctx.get('start') or (time_millis() / 1000)
+        live_start_time = traverse_obj(root_info_dict, ('__original_infodict', 'release_timestamp'))
+        lack_early = False
+        if live_start_time is not None and download_start_time - live_start_time > 432000:
+            self.report_warning('Starting downloading from recent 120 hours of live, because YouTube does not have the data. Please file an issue if you think it is wrong.', only_once=True)
+            lack_early = True
+
+        ie = self.ydl.get_info_extractor('Youtube')
+        return ie._manifest_fragments(manifest_url, stream_number, lack_early=lack_early)
+
+    @staticmethod
+    def _accept_live():
+        return True
+
+    @staticmethod
+    def _ignore_lethal_error():
+        return True
