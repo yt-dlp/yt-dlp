14edf7bf5f9732304ef474a288bd1bd23f0ef828
nixxo <nixxo@protonmail.com>
2021-12-30 20:44:49 +0100

    [mediaset] added an extra class to recognize more urls
    Suggested and written by: @dirkf

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -779,6 +779,7 @@ from .mediaite import MediaiteIE
 from .mediaklikk import MediaKlikkIE
 from .mediaset import (
     MediasetIE,
+    MediasetClipIE,
     MediasetShowIE,
 )
 from .mediasite import (
