bd6ba5fb2107e3a7a26b30b0394bd6f6e76255ca
coletdjnz <coletdjnz@protonmail.com>
2021-12-31 13:31:02 +1300

    Add tests and cleanup

--- yt_dlp/extractor/mainstreaming.py
+++ yt_dlp/extractor/mainstreaming.py
@@ -2,51 +2,109 @@
 import re
 
 from .common import InfoExtractor
-from ..compat import compat_urllib_parse_unquote
-from ..utils import js_to_json, traverse_obj, base_url, urljoin, try_get, parse_duration, int_or_none, HEADRequest, \
-    parse_qs
+
+from ..utils import (
+    int_or_none,
+    js_to_json,
+    parse_duration,
+    traverse_obj,
+    try_get,
+    urljoin
+)
 
 
 class MainStreamingIE(InfoExtractor):
     _VALID_URL = 'https?://(?:webtools-?)?(?P<host>[A-Za-z0-9-]*\.msvdn.net)/(?:embed|amp_embed|content)/(?P<id>\w+)'
-    # examples: https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/MfuWmzL2lGkA?autoPlay=false
-    # https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/HVvPMzy?autoplay=true&skinid=6c2d8b44-9903-493c-bf85-ec27e4d04684&T=1639390944 live DVR (playlist.m3u8?DVR), content type = 20. has live source id.
-  # live video (/live/id/playlist.m3u8): https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/zPnHWY2?autoPlay=true&skinId=6c2d8b44-9903-493c-bf85-ec27e4d04684, content type = 20, has live source id, dvr is disabled
-    # https://www.lacplay.it/ gives the actual m3u8 url. but we can use the id and extract more formats
-    # https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/uAsH3m14QWGx?autoPlay=true&skinId=6c2d8b44-9903-493c-bf85-ec27e4d04684 from lacplay.it
-        # one site hit the api directly https://webtools-859c1818ed614cc5b0047439470927b0.msvdn.net/api/v2/content/tDoFkZD3T1Lw
-    # https://video.milanofinanza.it and milanofinanza.it, youtg.net
-    #
+    IE_DESC = 'MainStreaming Player'
+    # https://video.milanofinanza.it and milanofinanza.it, youtg.net, https://www.lactv.it
 
     _TESTS = [
-        # Live stream offline, has alternative content id
         {
-            'url': 'https://webtools-e18da6642b684f8aa9ae449862783a56.msvdn.net/embed/53EN6GxbWaJC'
-        },
-        # Content id is a playlist
-        {
-            'url': 'https://webtools-e18da6642b684f8aa9ae449862783a56.msvdn.net/embed/WDAF1KOWUpH3'
-        },
-        # Video
-        {
-            'url': 'https://webtools-859c1818ed614cc5b0047439470927b0.msvdn.net/embed/tDoFkZD3T1Lw'
-        },
-        # /embed url without webtools prefix
-        {
-            'url': 'https://859c1818ed614cc5b0047439470927b0.msvdn.net/embed/tDoFkZD3T1Lw'
-        },
-        # All the other endpoints documented here: https://webtools.msvdn.net/
-
-        # Livestream (get actial embed)
-        {
-            'url': 'https://www.lactv.it/diretta-tv/'
+            'note': 'Live stream offline, has alternative content id',
+            'url': 'https://webtools-e18da6642b684f8aa9ae449862783a56.msvdn.net/embed/53EN6GxbWaJC',
+            'info_dict': {
+                'id': '53EN6GxbWaJC',
+                'title': 'Diretta homepage 2021-12-31 12:00',
+                'description': '',
+                'live_status': 'was_live',
+                'ext': 'mp4',
+                'thumbnail': r're:https?://[A-Za-z0-9-]*\.msvdn.net/image/\w+/poster',
+            },
+            'expected_warnings': [
+                'Ignoring alternative content ID: WDAF1KOWUpH3',
+                'MainStreaming said: Live event is OFFLINE'
+            ],
+            'skip': 'live stream offline'
+        }, {
+            'note': 'playlist',
+            'url': 'https://webtools-e18da6642b684f8aa9ae449862783a56.msvdn.net/embed/WDAF1KOWUpH3',
+            'info_dict': {
+                'id': 'WDAF1KOWUpH3',
+                'title': 'Playlist homepage',
+            },
+            'playlist_mincount': 8,
+        }, {
+            'note': 'livestream',
+            'url': 'https://webtools-859c1818ed614cc5b0047439470927b0.msvdn.net/embed/tDoFkZD3T1Lw',
+            'info_dict': {
+                'id': 'tDoFkZD3T1Lw',
+                'title': r're:Class CNBC Live \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
+                'live_status': 'is_live',
+                'ext': 'mp4',
+                'thumbnail': r're:https?://[A-Za-z0-9-]*\.msvdn.net/image/\w+/poster',
+            },
+            'skip': 'live stream'
+        }, {
+            'note': 'video',
+            'url': 'https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/EUlZfGWkGpOd?autoPlay=false',
+            'info_dict': {
+                'id': 'EUlZfGWkGpOd',
+                'title': 'La Settimana ',
+                'description': '03 Ottobre ore 02:00',
+                'ext': 'mp4',
+                'live_status': 'not_live',
+                'thumbnail': r're:https?://[A-Za-z0-9-]*\.msvdn.net/image/\w+/poster',
+                'duration': 1512
+            }
+        }, {
+            'note': 'video without webtools- prefix',
+            'url': 'https://f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/MfuWmzL2lGkA?autoplay=false&T=1635860445',
+            'info_dict': {
+                'id': 'MfuWmzL2lGkA',
+                'title': 'TG Mattina',
+                'description': '06 Ottobre ore 08:00',
+                'ext': 'mp4',
+                'live_status': 'not_live',
+                'thumbnail': r're:https?://[A-Za-z0-9-]*\.msvdn.net/image/\w+/poster',
+                'duration': 789.04  # TODO: this should be rounded?
+            }
+        }, {
+            # TODO: can't get the title test working
+            'note': 'always-on livestream with DVR',
+            'url': 'https://webtools-f5842579ff984c1c98d63b8d789673eb.msvdn.net/embed/HVvPMzy',
+            'info_dict': {
+                'id': 'HVvPMzy',
+                'title': r're:^Diretta LaC News24 \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
+                'description': 'canale all news',
+                'live_status': 'is_live',
+                'ext': 'mp4',
+                'thumbnail': r're:https?://[A-Za-z0-9-]*\.msvdn.net/image/\w+/poster',
+            },
+            'params': {
+                'skip_download': True,
+            },
+        }, {
+            'note': 'no host',
+            'url': 'https://webtools.msvdn.net/embed/MfuWmzL2lGkA',
+            'only_matching': True
+        }, {
+            'url': 'https://859c1818ed614cc5b0047439470927b0.msvdn.net/amp_embed/tDoFkZD3T1Lw',
+            'only_matching': True
+        }, {
+            'url': 'https://859c1818ed614cc5b0047439470927b0.msvdn.net/content/tDoFkZD3T1Lw#',
+            'only_matching': True
         }
-
-
     ]
-    # /api/v2/ must be from a hostname that starts with webtools
-    # webtools embed api can be from either
-    # format urls (m3u8 etc.) must be from non-webtools hostname
 
     @staticmethod
     def _extract_urls(webpage):
@@ -54,7 +112,6 @@ class MainStreamingIE(InfoExtractor):
             r'<iframe[^>]+?src=["\']?(?P<url>%s)["\']?' % MainStreamingIE._VALID_URL, webpage)
         if mobj:
             return [group[0] for group in mobj]
-        # TODO: we could extract the thumbnails (e.g. for https://video.milanofinanza.it/video/pnrr-le-sfide-del-2022-LPwjYU4lOOR4) and get the id that way
 
     def _playlist_entries(self, host, playlist_content):
         for entry in playlist_content:
@@ -76,7 +133,7 @@ class MainStreamingIE(InfoExtractor):
 
     def _call_api(self, host: str, path: str, item_id: str, query=None, note='Downloading API JSON', fatal=False):
         # JSON API, does not appear to be documented
-        return self._call_webtools_api(host, '/api/v2/'+path, item_id, query, note, fatal)
+        return self._call_webtools_api(host, '/api/v2/' + path, item_id, query, note, fatal)
 
     def _call_webtools_api(self, host: str, path: str, item_id: str, query=None, note='Downloading webtools API JSON', fatal=False):
         # webtools docs: https://webtools.msvdn.net/
@@ -103,13 +160,13 @@ class MainStreamingIE(InfoExtractor):
         title = content_info.get('title') or video_id
         description = traverse_obj(content_info, 'longDescription', 'shortDescription', expected_type=str, get_all=False)
         is_live = False
-
+        was_live = False
         if content_info.get('drmEnabled'):
             self.report_drm(video_id)
 
         alternative_content_id = content_info.get('alternativeContentID')
         if alternative_content_id:
-            self.report_warning(f'Found alternative content ID: {alternative_content_id}')
+            self.report_warning(f'Ignoring alternative content ID: {alternative_content_id}')
 
         content_type = int_or_none(content_info.get('contentType'))
         format_base_url = None
@@ -123,6 +180,8 @@ class MainStreamingIE(InfoExtractor):
             heartbeat = self._call_api(host, f'heartbeat/{video_id}', video_id, note='Checking stream status') or {}
             if heartbeat.get('heartBeatUp') is False:
                 self.raise_no_formats(f'MainStreaming said: {heartbeat.get("responseMessage")}', expected=True)
+                is_live = False
+                was_live = True
                 # TODO: if stream does not exist, grab alternative content id?
 
         # Playlist
@@ -142,7 +201,7 @@ class MainStreamingIE(InfoExtractor):
             mpd_formats, mpd_subs = self._extract_mpd_formats_and_subtitles(format_base_url % 'manifest.mpd', video_id=video_id, fatal=False)
 
             subtitles = self._merge_subtitles(m3u8_subs, mpd_subs)
-            formats.extend(m3u8_formats+mpd_formats)
+            formats.extend(m3u8_formats + mpd_formats)
 
         self._sort_formats(formats)
         # TODO: subtitles (in subtitlesPath)
@@ -153,8 +212,9 @@ class MainStreamingIE(InfoExtractor):
             'description': description,
             'formats': formats,
             'is_live': is_live,
+            'was_live': was_live,
             'duration': parse_duration(content_info.get('duration')),
             'tags': content_info.get('tags'),
             'subtitles': subtitles,
-            'thumbnail': f'https://{self._get_webtools_host(host)}/image/{video_id}/poster'  # Note: does not always exist
+            'thumbnail': f'https://{self._get_webtools_host(host)}/image/{video_id}/poster'  # may not always exist?
         }
