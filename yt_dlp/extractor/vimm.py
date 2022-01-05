5d4e5a6e8aca1c0ea5f6544b0c42051dad7cf0e7
pukkandan <pukkandan.ytdlp@gmail.com>
2022-01-05 15:20:06 +0530

    Apply suggestions from code review

--- yt_dlp/extractor/vimm.py
+++ yt_dlp/extractor/vimm.py
@@ -11,13 +11,15 @@ class VimmIE(InfoExtractor):
             'ext': 'mp4',
             'title': 're:^calimeatwagon [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$',
             'live_status': 'is_live',
-        }
+        }, {'skip': 'Live'}
     }]
 
     def _real_extract(self, url):
         channel_id = self._match_id(url)
 
-        formats = self._extract_m3u8_formats(f'https://www.vimm.tv/hls/{channel_id}.m3u8', channel_id, 'mp4', m3u8_id='hls', live=True)
+        formats = self._extract_m3u8_formats(
+            f'https://www.vimm.tv/hls/{channel_id}.m3u8', channel_id, 'mp4', m3u8_id='hls', live=True)
+        self._sort_formats(formats)
 
         return {
             'id': channel_id,
