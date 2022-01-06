06e3b7793d43018ddb9f903f05c15c8da2623369
Moises Lima <mozlima@gmail.com>
2022-01-06 01:19:56 -0300

    [Pornez] use _parse_html5_media_entries

--- yt_dlp/extractor/pornez.py
+++ yt_dlp/extractor/pornez.py
@@ -1,12 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
-import re
 from .common import InfoExtractor
-from ..utils import (
-    extract_attributes,
-    int_or_none,
-    mimetype2ext
-)
+from ..utils import int_or_none
 
 
 class PornezIE(InfoExtractor):
@@ -26,35 +21,23 @@ class PornezIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-        iframe_src = self._html_search_regex(r'<iframe src="([^"]+)"', webpage, 'iframe', fatal=True)
-        title = self._html_search_meta(['name', 'twitter:title', 'og:title'], webpage, 'title', default=None) or self._search_regex(r'<h1>(.*?)</h1>', webpage, 'title')
+        iframe_src = self._html_search_regex(
+            r'<iframe[^>]+src="(https?://pornez\.net/player/\?[^"]+)"', webpage, 'iframe', fatal=True)
+        title = self._html_search_meta(['name', 'twitter:title', 'og:title'], webpage, 'title', default=None)
+        if title is None:
+            title = self._search_regex(r'<h1>(.*?)</h1>', webpage, 'title', fatal=True)
         thumbnail = self._html_search_meta(['thumbnailUrl'], webpage, 'title', default=None)
         webpage = self._download_webpage(iframe_src, video_id)
-        formats = []
-        video_elements = self._search_regex(r'(?s)<video id="hls-video"[^>]*>(.*?)</video>', webpage, 'video elements', fatal=True)
+        entries = self._parse_html5_media_entries(iframe_src, webpage, video_id)[0]
+        for format in entries['formats']:
+            height = self._search_regex(r'_(\d+)\.m3u8', format['url'], 'height')
+            format['format_id'] = '%sp' % height
+            format['height'] = int_or_none(height)
 
-        for source in re.findall(r'<source[^>]+>', video_elements):
-            attributes = extract_attributes(source)
-            format_url = attributes.get('src', None)
-            format_type = attributes.get('type', None)
-            format_title = attributes.get('title', None)
-            height = int_or_none(self._search_regex(r'(\d+)', format_title, 'height', default=None))
-            if format_url is None or format_type is None or format_title is None or height is None:
-                continue
-            format_ext = mimetype2ext(format_type)
-            if format_ext == 'm3u8':
-                format_ext = 'mp4'
-            formats.append({
-                'format_id': format_title,
-                'url': format_url,
-                'ext': format_ext,
-                'height': int_or_none(height)
-            })
-        self._sort_formats(formats)
-        return {
+        entries.update({
             'id': video_id,
             'title': title,
             'thumbnail': thumbnail,
-            'formats': formats,
             'age_limit': 18
-        }
+        })
+        return entries
