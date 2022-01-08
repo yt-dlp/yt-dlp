724c935285b6deeb9f8bbb0db4b1b0bcda6a5019
YuenSzeHong <40477634+YuenSzeHong@users.noreply.github.com>
2022-01-08 10:16:07 +0800

    Update fujitv.py
    added empty dictionary assignment to `json_info` and replaced print to self._report_warning for the token extraction

--- yt_dlp/extractor/fujitv.py
+++ yt_dlp/extractor/fujitv.py
@@ -37,9 +37,12 @@ class FujiTVFODPlus7IE(InfoExtractor):
         # where the thumbnail is a img element with id of 'thumbnail'
         formats = self._extract_m3u8_formats(
             self._BASE_URL + 'abr/tv_android/%s.m3u8' % video_id, video_id, 'mp4')
+        json_info={}
         if not self._get_cookies(url).get('CT'):
             token = self._get_cookies(url).get('CT').value
             json_info = self._download_json('https://fod-sp.fujitv.co.jp/apps/api/episode/detail/?ep_id=%s&is_premium=false' % video_id, video_id, headers={'x-authorization': f'Bearer {token}'}, fatal=False)
+        else: 
+            self._report_warning("Unable to extract token cookie, video information is unavailable")
         for f in formats:
             wh = self._BITRATE_MAP.get(f.get('tbr'))
             if wh:
