e118ff4899886e6b623040ca19b41040cc5c911b
pukkandan <pukkandan.ytdlp@gmail.com>
2022-01-10 22:41:23 +0530

    Update yt_dlp/extractor/noodlemagazine.py

--- yt_dlp/extractor/noodlemagazine.py
+++ yt_dlp/extractor/noodlemagazine.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from .common import InfoExtractor
 from ..utils import (
     parse_duration,
     parse_count,
     unified_strdate
 )
-from .common import InfoExtractor
 
 
 class NoodleMagazineIE(InfoExtractor):
