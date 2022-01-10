c19c6b3b1f7430bdb1046235afd55773f0fa7a77
YuenSzeHong <szehong26@gmail.com>
2022-01-10 17:20:38 +0800

    Update fujitv.py
    fixed function name for giving warning and arranged operation oder

--- yt_dlp/extractor/fujitv.py
+++ yt_dlp/extractor/fujitv.py
@@ -32,14 +32,14 @@ class FujiTVFODPlus7IE(InfoExtractor):
         video_id = self._match_id(url)
         series_id = self._match_valid_url(url).group('sid')
         self._download_webpage(url, video_id)
-        formats = self._extract_m3u8_formats(
-            self._BASE_URL + 'abr/tv_android/%s.m3u8' % video_id, video_id, 'mp4')
         json_info = {}
         if not self._get_cookies(url).get('CT'):
             token = self._get_cookies(url).get('CT').value
             json_info = self._download_json('https://fod-sp.fujitv.co.jp/apps/api/episode/detail/?ep_id=%s&is_premium=false' % video_id, video_id, headers={'x-authorization': f'Bearer {token}'}, fatal=False)
         else:
-            self._report_warning('Unable to extract token cookie, video information is unavailable')
+            self.report_warning('Unable to extract token cookie, video information is unavailable')
+        formats = self._extract_m3u8_formats(
+            self._BASE_URL + 'abr/tv_android/%s.m3u8' % video_id, video_id, 'mp4')
         for f in formats:
             wh = self._BITRATE_MAP.get(f.get('tbr'))
             if wh:
