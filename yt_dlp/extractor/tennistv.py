8185852e764fd5ffd335f3019a4b4795cd46e57d
zenerdi0de <zenerdiodesteam@gmail.com>
2022-01-13 16:30:07 +0530

    sort formats

--- yt_dlp/extractor/tennistv.py
+++ yt_dlp/extractor/tennistv.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from .common import InfoExtractor
 
+from ..compat import compat_urlparse
+
 from ..utils import (
     ExtractorError,
     unified_timestamp,
@@ -10,8 +12,6 @@ from ..utils import (
     random_uuidv4,
     update_url_query
 )
-
-from ..compat import compat_urlparse
 
 
 class TennisTVIE(InfoExtractor):
@@ -59,25 +59,28 @@ class TennisTVIE(InfoExtractor):
             }
 
             login_page = self._download_webpage(update_url_query(BASE_URL, {'client_id': 'tennis-tv-web',
-                        'redirect_uri': 'https://tennistv.com',
-                        'response_mode': 'fragment',
-                        'response_type': 'code',
-                        'scope': 'openid'}),
-                        '')
+                                                'redirect_uri': 'https://tennistv.com',
+                                                                            'response_mode': 'fragment',
+                                                                            'response_type': 'code',
+                                                                            'scope': 'openid'}),
+                                                '')
             post_url = self._html_search_regex(r'action=["\'](.+?)["\']\s+?method=["\']post["\']', login_page, None)
 
-            self._download_webpage(post_url, None, 'Sending login data', 'Unable to send login data',
-                                   data=urlencode_postdata(login_form), headers=self.headers)
+            temp_page = self._download_webpage(post_url, None, 'Sending login data', 'Unable to send login data',
+                                               data=urlencode_postdata(login_form), headers=self.headers)
+
+            if 'Your username or password was incorrect' in temp_page:
+                raise ExtractorError('Your username or password was incorrect', expected=True)
 
             _, handle = self._download_webpage_handle(update_url_query(BASE_URL, {'client_id': 'tennis-tv-web',
-                            'redirect_uri': 'https://www.tennistv.com/resources/v1.1.10/html/silent-check-sso.html',
-                            'state': random_uuidv4(),
-                            'response_mode': 'fragment',
-                            'response_type': 'code',
-                            'scope': 'openid',
-                            'nonce': random_uuidv4(),
-                            'prompt': 'none'}), '',
-                            headers=self.headers)
+                                                                                  'redirect_uri': 'https://www.tennistv.com/resources/v1.1.10/html/silent-check-sso.html',
+                                                                                  'state': random_uuidv4(),
+                                                                                  'response_mode': 'fragment',
+                                                                                  'response_type': 'code',
+                                                                                  'scope': 'openid',
+                                                                                  'nonce': random_uuidv4(),
+                                                                                  'prompt': 'none'}), '',
+                                                      headers=self.headers)
 
             self.token_util(None, {
                 'code': compat_urlparse.parse_qs(handle.geturl()).get('code')[-1],  # There can be multiple, always use the last one
@@ -106,6 +109,10 @@ class TennisTVIE(InfoExtractor):
     def _real_initialize(self):
         self._login()
 
+    def _download_session_json(self, video_id, entryid,):
+        return self._download_json(f'https://atppayments.streamamg.com/api/v1/session/ksession/?lang=en&apijwttoken={self.ACCESS_TOKEN}&entryId={entryid}',
+                                   video_id, 'Downloading ksession token', 'Failed to download ksession token', headers=self.headers)
+
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
@@ -115,31 +122,35 @@ class TennisTVIE(InfoExtractor):
 
         entryid = self._search_regex(r'data-entry-id=["\']([^"\']+)', webpage, 'entryID')
 
-        session_json = self._download_json(f'https://atppayments.streamamg.com/api/v1/session/ksession/?lang=en&apijwttoken={self.ACCESS_TOKEN}&entryId={entryid}',
-                                           video_id, 'Downloading ksession token', 'Failed to download ksession token', headers=self.headers)
+        session_json = self._download_session_json(video_id, entryid)
 
         k_session = session_json.get('KSession')
         if k_session is None:
 
-            k_session = self.token_util(video_id, {
+            # retry with fresh tokens
+            self.token_util(video_id, {
                 'grant_type': 'refresh_token',
                 'refresh_token': self.REFRESH_TOKEN,
                 'client_id': 'tennis-tv-web'
             })
+            k_session = self._download_session_json(video_id, entryid).get('KSession')
+
             if k_session is None:
-                raise ExtractorError('Failed to get KSession, possible premium video', expected=True)
+                raise ExtractorError('Failed to get KSession, possibly a premium video', expected=True)
 
         if session_json.get('ErrorMessage'):
             self.to_screen(session_json['ErrorMessage'])
 
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(f'https://open.http.mp.streamamg.com/p/{self.PARTNER_ID}/sp/{self.PARTNER_ID}00/playManifest/entryId/{entryid}/format/applehttp/protocol/https/a.m3u8?ks={k_session}', video_id)
 
+        self._sort_formats(formats)
+
         return {
             'id': video_id,
             'title': title,
             'description': self._html_search_regex(r'<span itemprop="description" content=["\'](.+?)["\']>', webpage, 'description', fatal=False) or self._og_search_description(webpage),
             'formats': formats,
-            'thumbnail': f'https://open.http.mp.streamamg.com/p/{self.PARTNER_ID}/sp/{self.PARTNER_ID}00/thumbnail/entry_id/{entryid}/version/100001/height/1080',
+            'thumbnail': f'https://open.http.mp.streamamg.com/p/{self.PARTNER_ID}/sp/{self.PARTNER_ID}00/thumbnail/entry_id/{entryid}/version/100001/height/1920',
             'timestamp': unified_timestamp(self._html_search_regex(r'<span itemprop="description" content=["\'](.+?)["\']>', webpage, 'upload time')),
             'subtitles': subtitles,
         }
