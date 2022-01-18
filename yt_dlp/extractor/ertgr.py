bd94f2a66f5d8a61d4976cd1e490fcfc9164a3f3
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2022-01-18 19:43:06 +0200

    Move _parse_age_rating to a staticmethod

--- yt_dlp/extractor/ertgr.py
+++ yt_dlp/extractor/ertgr.py
@@ -113,18 +113,19 @@ class ERTFlixIE(ERTFlixBaseIE):
     },
     ]
 
+    @staticmethod
+    def _parse_age_rating(info_dict):
+        return parse_age_limit(
+            info_dict.get('AgeRating')
+            or (info_dict.get('IsAdultContent') and 18)
+            or (info_dict.get('IsKidsContent') and 0))
+
     def _extract_series(self, video_id):
         media_info = self._call_api(video_id, method='Tile/GetSeriesDetails', id=video_id)
 
-        def _parse_age_rating(info_dict):
-            return parse_age_limit(
-                info_dict.get('AgeRating')
-                or (info_dict.get('IsAdultContent') and 18)
-                or (info_dict.get('IsKidsContent') and 0))
-
         series = try_get(media_info, lambda x: x['Series'], dict) or {}
         series_info = {
-            'age_limit': _parse_age_rating(series),
+            'age_limit': self._parse_age_rating(series),
             'title': series.get('Title'),
             'description': dict_get(series, ('ShortDescription', 'TinyDescription', )),
         }
@@ -158,7 +159,7 @@ class ERTFlixIE(ERTFlixBaseIE):
                         'description': episode.get('ShortDescription'),
                         'timestamp': parse_iso8601(episode.get('PublishDate')),
                         'episode_number': n,
-                        'age_limit': _parse_age_rating(episode),
+                        'age_limit': self._parse_age_rating(episode),
                         'url': 'ertflix:%s' % (codename, ),
                     }
                     info.update(season_info)
