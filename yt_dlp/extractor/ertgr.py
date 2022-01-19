0c68be7d876e5df6d7d8c67cd3700ad3d1c951a7
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2022-01-19 10:05:02 +0200

    Support filtering series with optional season parameters

--- yt_dlp/extractor/ertgr.py
+++ yt_dlp/extractor/ertgr.py
@@ -11,6 +11,7 @@ from ..utils import (
     dict_get,
     int_or_none,
     merge_dicts,
+    parse_qs,
     parse_age_limit,
     parse_iso8601,
     str_or_none,
@@ -170,7 +171,7 @@ class ERTFlixIE(ERTFlixBaseIE):
             or (info_dict.get('IsAdultContent') and 18)
             or (info_dict.get('IsKidsContent') and 0))
 
-    def _extract_series(self, video_id):
+    def _extract_series(self, video_id, season_titles=None, season_numbers=None):
         media_info = self._call_api(video_id, method='Tile/GetSeriesDetails', id=video_id)
 
         series = try_get(media_info, lambda x: x['Series'], dict) or {}
@@ -179,9 +180,16 @@ class ERTFlixIE(ERTFlixBaseIE):
             'title': series.get('Title'),
             'description': dict_get(series, ('ShortDescription', 'TinyDescription', )),
         }
+        if season_numbers:
+            season_titles = season_titles or []
+            for season in try_get(series, lambda x: x['Seasons'], list) or []:
+                if season.get('SeasonNumber') in season_numbers:
+                    season_titles.append(season.get('Title'))
 
-        def gen_episode(m_info):
+        def gen_episode(m_info, season_titles):
             for episode_group in try_get(m_info, lambda x: x['EpisodeGroups'], list) or []:
+                if season_titles and episode_group.get('Title') not in season_titles:
+                    continue
                 episodes = try_get(episode_group, lambda x: x['Episodes'], list)
                 if not episodes:
                     continue
@@ -202,13 +210,25 @@ class ERTFlixIE(ERTFlixBaseIE):
                     info.update(season_info)
                     yield info
 
-        result = self.playlist_result(list(gen_episode(media_info)), playlist_id=video_id)
+        result = self.playlist_result(list(gen_episode(media_info, season_titles)), playlist_id=video_id)
         result.update(series_info)
         return result
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        if video_id.startswith('ser'):
-            return self._extract_series(video_id)
+        if video_id.startswith('ser.'):
+            param_season = parse_qs(url).get('season', [None])
+            param_season = [
+                (have_number, int_or_none(v) if have_number else str_or_none(v))
+                for have_number, v in
+                [(int_or_none(ps) is not None, ps) for ps in param_season]
+                if v is not None
+            ]
+            season_kwargs = {
+                k: [v for is_num, v in param_season if is_num is c] or None
+                for k, c in
+                [('season_titles', False), ('season_numbers', True)]
+            }
+            return self._extract_series(video_id, **season_kwargs)
 
         return self._extract_episode(self._call_api_get_tiles(video_id))
