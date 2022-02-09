637cfc458b998858e3a82dbcb8c7f8df4de51683
Tom <tomaviv57@gmail.com>
2022-02-09 11:54:49 +0200

    The recording used for testing is no longer available

--- yt_dlp/extractor/zoom.py
+++ yt_dlp/extractor/zoom.py
@@ -19,13 +19,7 @@ class ZoomIE(InfoExtractor):
     IE_NAME = 'zoom'
     _VALID_URL = r'(?P<base_url>https?://(?:[^.]+\.)?zoom.us/)rec(?:ording)?/(?:play|share)/(?P<id>[A-Za-z0-9_.-]+)'
     _TEST = {
-        'url': 'https://economist.zoom.us/rec/play/dUk_CNBETmZ5VA2BwEl-jjakPpJ3M1pcfVYAPRsoIbEByGsLjUZtaa4yCATQuOL3der8BlTwxQePl_j0.EImBkXzTIaPvdZO5',
-        'md5': 'ab445e8c911fddc4f9adc842c2c5d434',
-        'info_dict': {
-            'id': 'dUk_CNBETmZ5VA2BwEl-jjakPpJ3M1pcfVYAPRsoIbEByGsLjUZtaa4yCATQuOL3der8BlTwxQePl_j0.EImBkXzTIaPvdZO5',
-            'ext': 'mp4',
-            'title': 'China\'s "two sessions" and the new five-year plan',
-        }
+        # Need to find publicly available zoom recordings
     }
 
     def _real_extract(self, url):
