eeb2da4663438b99ed77764ba8f23e92e8bc082f
Tom <tomaviv57@gmail.com>
2022-02-09 11:37:30 +0200

    Added support for screen cast stream

--- yt_dlp/extractor/zoom.py
+++ yt_dlp/extractor/zoom.py
@@ -1,15 +1,21 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import os.path
+from typing import Union, Any
 
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
     int_or_none,
+    str_or_none,
     js_to_json,
     parse_filesize,
     urlencode_postdata,
     urljoin,
+)
+from ..compat import (
+    compat_urlparse,
 )
 
 
@@ -51,27 +57,48 @@ class ZoomIE(InfoExtractor):
                 raise ExtractorError(validation['errorMessage'], expected=True)
             webpage = self._download_webpage(url, play_id)
 
+        data: dict[str, Union[None, str, int, list, bool, dict[str, Any]]]
         data = self._parse_json(self._search_regex(
             r'(?s)window\.__data__\s*=\s*({.+?});',
             webpage, 'data'), play_id, js_to_json)
 
         subtitles = {}
-        for _type in ('transcript', 'cc'):
+        for _type in ('transcript', 'cc', 'chapter'):
             if data.get('%sUrl' % _type):
                 subtitles[_type] = [{
                     'url': urljoin(base_url, data['%sUrl' % _type]),
                     'ext': 'vtt',
                 }]
 
-        return {
-            'id': play_id,
-            'title': data['topic'],
-            'url': data['viewMp4Url'],
-            'subtitles': subtitles,
+        formats: list[dict[str, Union[None, str, int]]] = []
+
+        if data.get('shareMp4Url'):
+            formats.append({
+                'format_note': 'Screen share stream',
+                'url': str_or_none(data.get('shareMp4Url')),
+                'width': int_or_none(data.get('shareResolvtionsWidth')),
+                'height': int_or_none(data.get('shareResolvtionsHeight')),
+                'format_id': str_or_none(data.get('shareVideoId')),
+                'ext': 'mp4',
+            })
+
+        formats.append({
+            'format_note': 'Camera stream',
+            'url': str_or_none(data.get('viewMp4Url')),
             'width': int_or_none(data.get('viewResolvtionsWidth')),
             'height': int_or_none(data.get('viewResolvtionsHeight')),
+            'format_id': str_or_none(data.get('recordingId')),
+            'ext': 'mp4',
+            'filesize_approx': parse_filesize(data.get('fileSize')),
+        })
+
+        return {
+            'id': play_id,
+            'title': data.get('topic'),
+            'url': data.get('viewMp4Url'),
+            'subtitles': subtitles,
+            'formats': formats,
             'http_headers': {
                 'Referer': base_url,
             },
-            'filesize_approx': parse_filesize(data.get('fileSize')),
         }
