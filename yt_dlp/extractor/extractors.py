c82f051dbbe6ba6a6c56ccdbaa079cbf83912fe1
chris <6024426+iw0nderhow@users.noreply.github.com>
2022-02-13 22:40:50 +0100

    [ruv.is] Add extractor (#2665)
    Closes: #2122
    
    Authored by: iw0nderhow

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1374,7 +1374,10 @@ from .megatvcom import (
 )
 from .rutv import RUTVIE
 from .ruutu import RuutuIE
-from .ruv import RuvIE
+from .ruv import (
+    RuvIE,
+    RuvSpilaIE
+)
 from .safari import (
     SafariIE,
     SafariApiIE,
