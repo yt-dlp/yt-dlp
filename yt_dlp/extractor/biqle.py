d510bc166f5f1dad610db5279f8d46b8151cb5e2
4t14s <216170+Bricio@users.noreply.github.com>
2022-02-12 23:03:43 -0300

    After first review. Altered import order Altered thumbnail test result to regex Removed write_debug Alter url code to not use + operator.

--- yt_dlp/extractor/biqle.py
+++ yt_dlp/extractor/biqle.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import re
+
 from .common import InfoExtractor
 from .vk import VKIE
 from ..compat import compat_b64decode
@@ -8,9 +10,9 @@ from ..compat import compat_b64decode
 from ..utils import (
     int_or_none,
     js_to_json,
+    traverse_obj,
     unified_timestamp,
 )
-import re
 
 
 class BIQLEIE(InfoExtractor):
@@ -25,7 +27,7 @@ class BIQLEIE(InfoExtractor):
             'description': 'Forsaken By Hope Studio Clip — Смотреть онлайн',
             'upload_date': '19700101',
             'timestamp': 0,
-            'thumbnail': 'https://sun9-86.userapi.com/impf/7vN3ACwSTgChP96OdOfzFjUCzFR6ZglDQgWsIw/KPaACiVJJxM.jpg?size=800x450&quality=96&keep_aspect_ratio=1&background=000000&sign=b48ea459c4d33dbcba5e26d63574b1cb&type=video_thumb',
+            'thumbnail': r're:https://[^/]+/impf/7vN3ACwSTgChP96OdOfzFjUCzFR6ZglDQgWsIw/KPaACiVJJxM\.jpg\?size=800x450&quality=96&keep_aspect_ratio=1&background=000000&sign=b48ea459c4d33dbcba5e26d63574b1cb&type=video_thumb',
         },
     }, {
         'url': 'http://biqle.org/watch/-44781847_168547604',
@@ -37,7 +39,7 @@ class BIQLEIE(InfoExtractor):
             'description': 'Ребенок в шоке от автоматической мойки — Смотреть онлайн',
             'timestamp': 1396633454,
             'upload_date': '20140404',
-            'thumbnail': 'https://sun9-36.userapi.com/c535507/u190034692/video/l_b84df002.jpg',
+            'thumbnail': r're:https://[^/]+/c535507/u190034692/video/l_b84df002\.jpg',
         },
     }]
 
@@ -48,17 +50,13 @@ class BIQLEIE(InfoExtractor):
         title = self._search_regex(
             r'<meta.*itemprop ?= ?"name".*content ?= ?"([^"]+)".*/>',
             webpage, 'Title', default='Empty Title', fatal=False)
-        self.write_debug('title: %s' % title)
         uploadDate = self._search_regex(
             r'<meta.*itemprop ?= ?"uploadDate".*content ?= ?"([^"]+)".*/?>',
             webpage, 'Upload Date', fatal=False)
-        self.write_debug('Upload Date: %s' % uploadDate)
         timestamp = unified_timestamp(uploadDate)
-        self.write_debug('timestamp: %s' % timestamp)
         description = self._search_regex(
             r'<meta.*itemprop ?= ?"description".*content ?= ?"([^"]+)".*/>',
             webpage, 'Description', fatal=False)
-        self.write_debug('description: %s' % description)
 
         globalEmbed_url = self._search_regex(
             r'<script.+?window.globEmbedUrl = \'((?:https?:)?//(?:daxab\.com|dxb\.to|[^/]+/player)/[^\']+)\'.*?></script>',
@@ -68,7 +66,6 @@ class BIQLEIE(InfoExtractor):
             webpage, 'Hash', flags=re.DOTALL)
 
         embed_url = globalEmbed_url + hash
-        self.write_debug('embed_url: %s' % embed_url)
 
         if VKIE.suitable(embed_url):
             return self.url_result(embed_url, VKIE.ie_key(), video_id)
@@ -79,10 +76,8 @@ class BIQLEIE(InfoExtractor):
         globParams = self._parse_json(self._search_regex(
             r'<script id="globParams">.*window.globParams = ([^;]+);[^<]+</script>',
             embed_page, 'Global Parameters', flags=re.DOTALL), video_id, transform_source=js_to_json)
-        self.write_debug('globParams: %s' % globParams)
         hostName = compat_b64decode(globParams['server'][::-1]).decode()
         server = 'https://%s/method/video.get/' % hostName
-        self.write_debug('server: %s' % server)
 
         item = self._download_json(
             server + video_id, video_id,
@@ -98,15 +93,14 @@ class BIQLEIE(InfoExtractor):
             if f_id == 'external':
                 return self.url_result(f_url)
             ext, height = f_id.split('_')
-            if globParams['video']['partial']['quality'].get(height) is not None:
+            if traverse_obj(globParams, ('video', 'partial', 'quality', height)) is not None:
                 formats.append({
                     'format_id': height + 'p',
-                    'url': f_url.replace('https://', 'https://%s/' % hostName) + '&videos=%s' % video_id + '&extra_key=%s' % globParams['video']['partial']['quality'][height],
+                    'url': f'https://{hostName}/{f_url[8:]}&videos={video_id}&extra_key={globParams["video"]["partial"]["quality"][height]}',
                     'height': int_or_none(height),
                     'ext': ext,
                 })
         self._sort_formats(formats)
-        self.write_debug('formats: %s' % formats)
 
         thumbnails = []
         for k, v in item.items():
