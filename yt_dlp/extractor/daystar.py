d6a2aca5144ed83427cb5a17cddd923b3e520ca7
hatienloi261299 <loiht.b17vt220@stu.ptit.edu.vn>
2022-03-03 09:21:39 +0700

    Update convention

--- yt_dlp/extractor/daystar.py
+++ yt_dlp/extractor/daystar.py
@@ -4,52 +4,46 @@ from ..utils import js_to_json, urljoin
 
 class DaystarClipIE(InfoExtractor):
     IE_NAME = 'daystar:clip'
-    _VALID_URL = r'https?:\/\/player\.daystar\.tv\/(?P<id>\w+)($|\W)'
-    _DOMAIN = 'https://www.lightcast.com/embed/'
-    _TEST = {
+    _VALID_URL = r'https?://player\.daystar\.tv/(?P<id>\w+)'
+    _TESTS = [{
         'url': 'https://player.daystar.tv/0MTO2ITM',
         'info_dict': {
             'id': '0MTO2ITM',
             'ext': 'mp4',
             'title': 'The Dark World of COVID Pt. 1 | Aaron Siri',
-            'description': 'Is the fear of the pandemic being used to suppress and erode your constitutional '
-                           'freedoms? Attorney Aaron Siri exposes the truth about what’s happening and how you can '
-                           'join the fight to preserve your liberties. (J2167)',
+            'description': 'a420d320dda734e5f29458df3606c5f4',
             'thumbnail': r're:^https?://.+\.jpg',
         },
-    }
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
         webpage = self._download_webpage(url, video_id=video_id)
 
-        title = self._html_search_meta(['og:title', 'twitter:title'], webpage, fatal=True)
-        description = self._html_search_meta(['og:description', 'twitter:description'], webpage, fatal=True)
-
-        src_iframe = self._search_regex(r'\<iframe.*?src=\"(.*?)\"', webpage, 'src iframe')
+        src_iframe = self._search_regex(r'\<iframe[^>]+src="([^"]+)"', webpage, 'src iframe')
 
         webpage_iframe = self._download_webpage(src_iframe.replace('player.php', 'config2.php'), video_id=video_id,
                                                 headers={'Referer': src_iframe})
 
-        sources = self._parse_json(
-            js_to_json(self._search_regex(r'sources\:.*?(\[.*?\])', webpage_iframe, 'm3u8 source')), video_id=video_id)
-
-        thumbnail = self._search_regex(r'image\:.*?\"(.*?)\"', webpage_iframe, 'thumbnail')
+        sources = self._parse_json(self._search_regex(r'sources\:[^>](\[.*?\])', webpage_iframe, 'm3u8 source'),
+                                   video_id=video_id, transform_source=js_to_json)
 
         formats = []
 
         for source in sources:
             file = source.get('file')
-            if source.get('type') == 'm3u8':
-                formats.extend(self._extract_m3u8_formats(urljoin(self._DOMAIN, file), video_id, 'mp4', fatal=False,
-                                                          headers={'Referer': src_iframe}))
+            if file and source.get('type') == 'm3u8':
+                fmts, subs = self._extract_m3u8_formats_and_subtitles(urljoin('https://www.lightcast.com/embed/', file),
+                                                                      video_id, 'mp4', fatal=False,
+                                                                      headers={'Referer': src_iframe})
+                formats.extend(fmts)
         self._sort_formats(formats)
 
         return {
             'id': video_id,
-            'title': title,
-            'description': description,
-            'thumbnail': thumbnail,
+            'title': self._html_search_meta(['og:title', 'twitter:title'], webpage, fatal=True),
+            'description': self._html_search_meta(['og:description', 'twitter:description'], webpage, fatal=True),
+            'thumbnail': self._search_regex(r'image\:[^>]\"([^"]+)\"', webpage_iframe, 'thumbnail'),
             'formats': formats,
         }
