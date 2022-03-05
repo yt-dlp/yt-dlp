d1cedef47ea88da73a33899afc0bd438fc61e6a6
hatienloi261299 <loiht.b17vt220@stu.ptit.edu.vn>
2022-03-05 09:19:46 +0700

    Update code follow convention

--- yt_dlp/extractor/fptplay.py
+++ yt_dlp/extractor/fptplay.py
@@ -7,20 +7,22 @@ import urllib.parse
 
 from .common import InfoExtractor
 from ..utils import (
-    traverse_obj,
     ExtractorError,
-    int_or_none,
+    traverse_obj,
 )
 
 
 class FptplayIE(InfoExtractor):
-    _VALID_URL = r'https?://fptplay\.vn/(?P<type>(?:xem-video))/(?P<slug>.*?)(?:\/(?P<episode>.*?)(?:\/|$)|$)'
+    _VALID_URL = r'https?://fptplay\.vn/(?P<type>(?:xem-video))/([^/]+\-(?P<id>\w+))(?:\/|$)(?:tap-(?P<episode>[^/]+)|$)'
+    _GEO_COUNTRIES = ['VN']
+    IE_NAME = 'fptplay'
+    IE_DESC = 'fptplay.vn'
     _TESTS = [{
         'url': 'https://fptplay.vn/xem-video/nhan-duyen-dai-nhan-xin-dung-buoc-621a123016f369ebbde55945',
         'info_dict': {
             'id': '621a123016f369ebbde55945',
             'ext': 'mp4',
-            'title': 'Nhân Duyên Đại Nhân Xin Dừng Bước - Ms. Cupid In Love - tap-1',
+            'title': 'Nhân Duyên Đại Nhân Xin Dừng Bước - Ms. Cupid In Love - 1',
             'description': '23cf7d1ce0ade8e21e76ae482e6a8c6c',
         },
     }, {
@@ -30,65 +32,41 @@ class FptplayIE(InfoExtractor):
         'url': 'https://fptplay.vn/xem-video/nha-co-chuyen-hi-alls-well-ends-well-1997-6218995f6af792ee370459f0',
         'only_matching': True,
     }]
-    _GEO_COUNTRIES = ['VN']
-    IE_NAME = 'fptplay'
-    IE_DESC = 'fptplay.vn'
 
     def _real_extract(self, url):
-        type_url, slug, episode = self._match_valid_url(url).group('type', 'slug', 'episode')
-        if episode and int_or_none(episode.split('-')[-1]):
-            ep = int(int_or_none(episode.split('-')[-1]))
-        else:
-            ep = 0
-
-        video_id = slug.split('-')[-1]
-
+        type_url, video_id, episode = self._match_valid_url(url).group('type', 'id', 'episode')
+        if not episode:
+            episode = 0
         webpage = self._download_webpage(url, video_id=video_id)
-
-        api = self.get_api_with_st_token(video_id, ep)
-
-        info = self._download_json(api, video_id=video_id)
-
-        url_m3u8 = traverse_obj(info, ('data', 'url'))
-
-        if not url_m3u8:
-            raise ExtractorError('Can not extract data.')
-
-        formats, subtitles = self._extract_m3u8_formats_and_subtitles(url_m3u8, video_id, 'mp4', fatal=False)
-
+        info = self._download_json(self.get_api_with_st_token(video_id, episode), video_id=video_id)
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(traverse_obj(info, ('data', 'url')), video_id,
+                                                                      'mp4')
+        self._sort_formats(formats)
         return {
             'id': video_id,
-            'title': f"{self._html_search_meta(['og:title', 'twitter:title'], webpage, fatal=True)}{f' - {episode}' if episode else ''}",
-            'description': self._html_search_meta(['og:description', 'twitter:description'], webpage, fatal=True),
+            'title': f'{self._html_search_meta(["og:title", "twitter:title"], webpage)}{f" - {episode}" if episode else ""}',
+            'description': self._html_search_meta(['og:description', 'twitter:description'], webpage),
             'formats': formats,
             'subtitles': subtitles,
         }
 
-    def get_api_with_st_token(self, video_id, ep):
-        path = f'stream/vod/{video_id}/{ep}/auto_vip'
-        suffix = '/api/v6.2_w/'
-
+    def get_api_with_st_token(self, video_id, episode):
+        path = f'/api/v6.2_w/stream/vod/{video_id}/{episode}/auto_vip'
         timestamp = int(time.time()) + 10800
-        token = f'WEBv6Dkdsad90dasdjlALDDDS{timestamp}{suffix}{path}'
-
         m = hashlib.md5()
-        m.update(token.encode())
-
+        m.update(f'WEBv6Dkdsad90dasdjlALDDDS{timestamp}{path}'.encode())
         st_token = self.encrypt(m.hexdigest())
-
-        return f"https://api.fptplay.net{suffix}{path}?{urllib.parse.urlencode({'st': st_token, 'e': timestamp})}"
+        return f'https://api.fptplay.net{path}?{urllib.parse.urlencode({"st": st_token, "e": timestamp})}'
 
     def encrypt(self, string):
         n = []
         t = string.upper()
-        r = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-        for o in range(int(len(t) / 2)):
-            i = t[2 * o:2 * o + 2]
-            num = '0x%s' % i
-            n.append(int(num, 16))
+        r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+        for o in range(len(t) // 2):
+            n.append(int(f'0x{t[2 * o: 2 * o + 2]}', 16))
 
         def convert(e):
-            t = ""
+            t = ''
             n = 0
             i = [0, 0, 0]
             a = [0, 0, 0, 0]
@@ -119,7 +97,7 @@ class FptplayIE(InfoExtractor):
                     t += r[a[o]]
                 n += 1
                 while n < 3:
-                    t += "="
+                    t += ''
                     n += 1
             return t
 
