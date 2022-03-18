ffdd3a667cd737493bd38f9887b4ad7b314b2a85
nobody <nobody>
2022-03-17 23:50:50 -0500

    [nitter] General cleanup

--- yt_dlp/extractor/nitter.py
+++ yt_dlp/extractor/nitter.py
@@ -125,23 +125,23 @@ class NitterIE(InfoExtractor):
 
     INSTANCES = NON_HTTP_INSTANCES + HTTP_INSTANCES + DEAD_INSTANCES
 
-    _INSTANCES_RE = '(?:' + '|'.join([re.escape(instance) for instance in INSTANCES]) + ')'
-    _VALID_URL = r'https?://%(instance)s/(?P<uploader_id>.+)/status/(?P<id>[0-9]+)(#.)?' % {'instance': _INSTANCES_RE}
+    _INSTANCES_RE = f'(?:{"|".join([re.escape(instance) for instance in INSTANCES])})'
+    _VALID_URL = fr'https?://{_INSTANCES_RE}/(?P<uploader_id>.+)/status/(?P<id>[0-9]+)(#.)?'
     current_instance = random.choice(HTTP_INSTANCES)
 
     _TESTS = [
         {
             # GIF (wrapped in mp4)
-            'url': 'https://%s/firefox/status/1314279897502629888#m' % current_instance,
+            'url': f'https://{current_instance}/firefox/status/1314279897502629888#m',
             'info_dict': {
                 'id': '1314279897502629888',
                 'ext': 'mp4',
-                'title': 'Firefox 🔥 - You know the old saying, if you see something say something. Now you actually can with the YouTube regrets extension. \n\nReport harmful YouTube recommendations so others can avoid watching them. ➡️ https://mzl.la/3iFIiyg\n\n#UnfckTheInternet',
-                'description': 'You know the old saying, if you see something say something. Now you actually can with the YouTube regrets extension. \n\nReport harmful YouTube recommendations so others can avoid watching them. ➡️ https://mzl.la/3iFIiyg\n\n#UnfckTheInternet',
+                'title': 'md5:7890a9277da4639ab624dd899424c5d8',
+                'description': 'md5:5fea96a4d3716c350f8b95b21b3111fe',
                 'thumbnail': r're:^https?://.*\.jpg$',
                 'uploader': 'Firefox 🔥',
                 'uploader_id': 'firefox',
-                'uploader_url': 'https://%s/firefox' % current_instance,
+                'uploader_url': f'https://{current_instance}/firefox',
                 'upload_date': '20201008',
                 'timestamp': 1602183720,
                 'like_count': int,
@@ -149,7 +149,7 @@ class NitterIE(InfoExtractor):
                 'comment_count': int,
             },
         }, {  # normal video
-            'url': 'https://%s/Le___Doc/status/1299715685392756737#m' % current_instance,
+            'url': f'https://{current_instance}/Le___Doc/status/1299715685392756737#m',
             'info_dict': {
                 'id': '1299715685392756737',
                 'ext': 'mp4',
@@ -158,7 +158,7 @@ class NitterIE(InfoExtractor):
                 'thumbnail': r're:^https?://.*\.jpg$',
                 'uploader': 're:^Le *Doc',
                 'uploader_id': 'Le___Doc',
-                'uploader_url': 'https://%s/Le___Doc' % current_instance,
+                'uploader_url': f'https://{current_instance}/Le___Doc',
                 'upload_date': '20200829',
                 'timestamp': 1598711340,
                 'view_count': int,
@@ -167,16 +167,16 @@ class NitterIE(InfoExtractor):
                 'comment_count': int,
             },
         }, {  # video embed in a "Streaming Political Ads" box
-            'url': 'https://%s/mozilla/status/1321147074491092994#m' % current_instance,
+            'url': f'https://{current_instance}/mozilla/status/1321147074491092994#m',
             'info_dict': {
                 'id': '1321147074491092994',
                 'ext': 'mp4',
-                'title': "Mozilla - Are you being targeted with weird, ominous or just plain annoying political ads while streaming your favorite shows?\n\nThis isn't a real political ad, but if you're watching streaming TV in the U.S., chances are you've seen quite a few. \n\nLearn more ➡️ https://mzl.la/StreamingAds",
-                'description': "Are you being targeted with weird, ominous or just plain annoying political ads while streaming your favorite shows?\n\nThis isn't a real political ad, but if you're watching streaming TV in the U.S., chances are you've seen quite a few. \n\nLearn more ➡️ https://mzl.la/StreamingAds",
+                'title': 'md5:8290664aabb43b9189145c008386bf12',
+                'description': 'md5:9cf2762d49674bc416a191a689fb2aaa',
                 'thumbnail': r're:^https?://.*\.jpg$',
                 'uploader': 'Mozilla',
                 'uploader_id': 'mozilla',
-                'uploader_url': 'https://%s/mozilla' % current_instance,
+                'uploader_url': f'https://{current_instance}/mozilla',
                 'upload_date': '20201027',
                 'timestamp': 1603820940,
                 'view_count': int,
@@ -186,16 +186,16 @@ class NitterIE(InfoExtractor):
             },
             'expected_warnings': ['Ignoring subtitle tracks found in the HLS manifest'],
         }, {  # not the first tweet but main-tweet
-            'url': 'https://%s/firefox/status/1354848277481414657#m' % current_instance,
+            'url': f'https://{current_instance}/firefox/status/1354848277481414657#m',
             'info_dict': {
                 'id': '1354848277481414657',
                 'ext': 'mp4',
-                'title': 'Firefox 🔥 - Happy #DataPrivacyDay! We\'ve got a few tips (and some friendly suggestions, you know, like updating your passwords every once in a while) to leveling up your privacy in less than a minute right here:',
-                'description': 'Happy #DataPrivacyDay! We\'ve got a few tips (and some friendly suggestions, you know, like updating your passwords every once in a while) to leveling up your privacy in less than a minute right here:',
+                'title': 'md5:bef647f03bd1c6b15b687ea70dfc9700',
+                'description': 'md5:5efba25e2f9dac85ebcd21160cb4341f',
                 'thumbnail': r're:^https?://.*\.jpg$',
                 'uploader': 'Firefox 🔥',
                 'uploader_id': 'firefox',
-                'uploader_url': 'https://%s/firefox' % current_instance,
+                'uploader_url': f'https://{current_instance}/firefox',
                 'upload_date': '20210128',
                 'timestamp': 1611855960,
                 'view_count': int,
@@ -209,18 +209,18 @@ class NitterIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         parsed_url = compat_urlparse.urlparse(url)
-        base_url = '%s://%s' % (parsed_url.scheme, parsed_url.netloc)
+        base_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
 
         self._set_cookie(parsed_url.netloc, 'hlsPlayback', 'on')
         full_webpage = self._download_webpage(url, video_id)
 
+        webpage = full_webpage
         main_tweet_start = full_webpage.find('class="main-tweet"')
         if main_tweet_start > 0:
             webpage = full_webpage[main_tweet_start:]
-        if not webpage:
-            webpage = full_webpage
 
-        video_url = '%s%s' % (base_url, self._html_search_regex(r'(?:<video[^>]+data-url|<source[^>]+src)="([^"]+)"', webpage, 'video url'))
+        video_url = '{}{}'.format(base_url, self._html_search_regex(
+            r'(?:<video[^>]+data-url|<source[^>]+src)="([^"]+)"', webpage, 'video url'))
         ext = determine_ext(video_url)
 
         if ext == 'unknown_video':
@@ -231,24 +231,18 @@ class NitterIE(InfoExtractor):
                 'ext': ext
             }]
 
-        title = self._og_search_description(full_webpage)
-        if not title:
-            title = self._html_search_regex(r'<div class="tweet-content[^>]+>([^<]+)</div>', webpage, 'title')
+        title = self._og_search_description(full_webpage) or self._html_search_regex(
+            r'<div class="tweet-content[^>]+>([^<]+)</div>', webpage, 'title', fatal=False)
         description = title
 
-        mobj = self._match_valid_url(url)
-        uploader_id = (
-            mobj.group('uploader_id')
-            or self._html_search_regex(r'<a class="fullname"[^>]+title="([^"]+)"', webpage, 'uploader name', fatal=False)
-        )
+        uploader_id = (self._html_search_regex(
+            r'<a class="username"[^>]+title="@([^"]+)"', webpage, 'uploader id', fatal=False)
+            or self._match_valid_url(url).group('uploader_id'))
 
-        if uploader_id:
-            uploader_url = '%s/%s' % (base_url, uploader_id)
-
-        uploader = self._html_search_regex(r'<a class="fullname"[^>]+title="([^"]+)"', webpage, 'uploader name', fatal=False)
-
+        uploader = self._html_search_regex(
+            r'<a class="fullname"[^>]+title="([^"]+)"', webpage, 'uploader name', fatal=False)
         if uploader:
-            title = '%s - %s' % (uploader, title)
+            title = f'{uploader} - {title}'
 
         counts = {
             f'{x[0]}_count': self._html_search_regex(
@@ -258,33 +252,30 @@ class NitterIE(InfoExtractor):
         }
         counts = {field: 0 if count == '' else parse_count(count) for field, count in counts.items()}
 
-        thumbnail = self._html_search_meta('og:image', full_webpage, 'thumbnail url')
-        if not thumbnail:
-            thumbnail = '%s%s' % (base_url, self._html_search_regex(r'<video[^>]+poster="([^"]+)"', webpage, 'thumbnail url', fatal=False))
-            thumbnail = remove_end(thumbnail, '%3Asmall')
+        thumbnail = (
+            self._html_search_meta('og:image', full_webpage, 'thumbnail url')
+            or remove_end('{}{}'.format(base_url, self._html_search_regex(
+                r'<video[^>]+poster="([^"]+)"', webpage, 'thumbnail url', fatal=False)), '%3Asmall'))
 
-        thumbnails = []
-        thumbnail_ids = ('thumb', 'small', 'large', 'medium', 'orig')
-        for id in thumbnail_ids:
-            thumbnails.append({
-                'id': id,
-                'url': thumbnail + '%3A' + id,
-            })
+        thumbnails = [
+            {'id': id, 'url': f'{thumbnail}%3A{id}'}
+            for id in ('thumb', 'small', 'large', 'medium', 'orig')
+        ]
 
-        date = self._html_search_regex(r'<span[^>]+class="tweet-date"[^>]*><a[^>]+title="([^"]+)"', webpage, 'upload date', default='').replace('·', '')
-        upload_date = unified_strdate(date)
-        timestamp = unified_timestamp(date)
+        date = self._html_search_regex(
+            r'<span[^>]+class="tweet-date"[^>]*><a[^>]+title="([^"]+)"',
+            webpage, 'upload date', default='').replace('·', '')
 
         return {
             'id': video_id,
             'title': title,
             'description': description,
             'uploader': uploader,
-            'timestamp': timestamp,
+            'timestamp': unified_timestamp(date),
             'uploader_id': uploader_id,
-            'uploader_url': uploader_url,
+            'uploader_url': f'{base_url}/{uploader_id}',
             'formats': formats,
             'thumbnails': thumbnails,
             'thumbnail': thumbnail,
-            'upload_date': upload_date,
+            'upload_date': unified_strdate(date),
         } | counts
