bdd60588b05a1cabd853858125522b920065cad8
nyuszika7h <nyuszika7h@gmail.com>
2022-03-28 05:23:44 +0200

    [viki] Don't attempt to modify URLs with signature (#3222)
    Closes #1379
    Authored by: nyuszika7h

--- yt_dlp/extractor/viki.py
+++ yt_dlp/extractor/viki.py
@@ -261,7 +261,7 @@ class VikiIE(VikiBaseIE):
         mpd_content = self._download_webpage(mpd_url, video_id, note='Downloading initial MPD manifest')
         mpd_url = self._search_regex(
             r'(?mi)<BaseURL>(http.+.mpd)', mpd_content, 'new manifest', default=mpd_url)
-        if 'mpdhd_high' not in mpd_url:
+        if 'mpdhd_high' not in mpd_url and 'sig=' not in mpd_url:
             # Modify the URL to get 1080p
             mpd_url = mpd_url.replace('mpdhd', 'mpdhd_high')
         formats = self._extract_mpd_formats(mpd_url, video_id)
