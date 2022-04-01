195204a1ce1c19a47cdb5d586061836d51515119
Lesmiscore <nao20010128@gmail.com>
2022-04-01 21:24:57 +0900

    fix

--- yt_dlp/extractor/tver.py
+++ yt_dlp/extractor/tver.py
@@ -7,6 +7,7 @@ from ..compat import compat_str
 from ..utils import (
     int_or_none,
     smuggle_url,
+    str_or_none,
     traverse_obj,
 )
 
@@ -19,32 +20,25 @@ class TVerIE(InfoExtractor):
         'only_matching': True,
     }]
     BRIGHTCOVE_URL_TEMPLATE = 'http://players.brightcove.net/%s/default_default/index.html?videoId=%s'
-    _PATH_MAP = {
-        'episodes': 'episode',
-    }
 
     def _real_extract(self, url):
-        path, video_id = self._match_valid_url(url).groups()
-        if path == 'lp':
-            webpage = self._download_webpage(url, video_id)
-            redirect_path = self._search_regex(r'to_href="([^"]+)', webpage, 'redirect path')
-            path, video_id = self._match_valid_url(f'https://tver.jp{redirect_path}').groups()
-        api_response = self._download_json(
-            f'https://statics.tver.jp/content/{self._PATH_MAP.get(path, path)}/{video_id}.json', video_id,
+        video_id = self._match_id(url)
+        video_info = self._download_json(
+            f'https://statics.tver.jp/content/episode/{video_id}.json', video_id,
             query={'v': '5'}, headers={
                 'Origin': 'https://tver.jp',
                 'Referer': 'https://tver.jp/',
             })
-        p_id = traverse_obj(api_response, ('video', 'accountID'))
-        r_id = 'ref:' + traverse_obj(api_response, ('video', 'videoRefID'))
+        p_id = traverse_obj(video_info, ('video', 'accountID'))
+        r_id = 'ref:' + traverse_obj(video_info, ('video', 'videoRefID'))
         bc_url = smuggle_url(
             self.BRIGHTCOVE_URL_TEMPLATE % (p_id, r_id),
             {'geo_countries': ['JP']})
 
         return {
             '_type': 'url_transparent',
-            'description': traverse_obj(api_response, 'description', expected_type=compat_str),
-            'episode_number': int_or_none(traverse_obj(api_response, ('main', 'ext', 'episode_number'), expected_type=compat_str)),
+            'title': str_or_none(video_info.get('title')),
+            'description': str_or_none(video_info.get('description')),
             'url': bc_url,
             'ie_key': 'BrightcoveNew',
         }
