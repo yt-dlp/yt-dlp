2c94cbf920e1e8b6e9e52c936c390ed89d5f7521
Lesmiscore <nao20010128@gmail.com>
2022-04-01 23:14:12 +0900

    fixfixfix

--- yt_dlp/extractor/tver.py
+++ yt_dlp/extractor/tver.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from .common import InfoExtractor
 from ..utils import (
+    ExtractorError,
     smuggle_url,
     str_or_none,
     traverse_obj,
@@ -11,32 +12,59 @@ from ..utils import (
 
 
 class TVerIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?tver\.jp/(?P<path>corner|episodes?|feature|lp|tokyo2020/video)/(?P<id>[a-zA-Z0-9]+)'
-    # videos are only available for 7 days
+    _VALID_URL = r'https?://(?:www\.)?tver\.jp/(?:(?:lp|corner|series|episodes?|feature|tokyo2020/video)/)+(?P<id>[a-zA-Z0-9]+)'
+    _NEW_URL_COMPONENT = ('series', 'episodes')
     _TESTS = [{
+        'skip': 'videos are only available for 7 days',
         'url': 'https://tver.jp/episodes/ephss8yveb',
+        'info_dict': {
+            'title': '#44　料理と値段と店主にびっくり　オモてなしすぎウマい店　2時間SP',
+            'description': '【宮城】極厚とんかつ定食５００円　マル秘女性歌手大ファン店主\n【福岡】学生感動パワー店主！！名物パワー定食って！？\n【埼玉】暴れん坊そば名人！！弟子５０人に！？師弟愛シーズン３',
+        },
+        'add_ie': ['BrightcoveNew'],
+    }, {
+        'skip': 'videos are only available for 7 days',
+        'url': 'https://tver.jp/lp/episodes/ep6f16g26p',
+        'info_dict': {
+            # sorry but this is "correct"
+            'title': '4月11日(月)23時06分 ~ 放送予定',
+            'description': '吉祥寺の格安シェアハウスに引っ越して来た高校教師の安彦聡（増田貴久）や、元ファッション誌編集長の大庭桜（田中みな実）など6人。鍵が掛かった部屋に絶対入らないことが絶対ルール。奇妙な共同生活が今始まる！　テレビ東京にて4月11日(月)夜11時6分放送スタート！',
+        },
+        'add_ie': ['BrightcoveNew'],
+    }, {
+        'url': 'https://tver.jp/corner/f0103888',
+        'only_matching': True,
+    }, {
+        'url': 'https://tver.jp/lp/f0033031',
         'only_matching': True,
     }]
     BRIGHTCOVE_URL_TEMPLATE = 'http://players.brightcove.net/%s/default_default/index.html?videoId=%s'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
+        if 'episodes' not in url:
+            # NOTE: episode/ is an old URL.
+            webpage = self._download_webpage(
+                url, video_id, note='Resolving to new URL')
+            video_id = self._match_id(self._search_regex(r'canonical"\s*href="(https?://tver\.jp/.+?)"', webpage, 'url regex'))
         video_info = self._download_json(
             f'https://statics.tver.jp/content/episode/{video_id}.json', video_id,
             query={'v': '5'}, headers={
                 'Origin': 'https://tver.jp',
                 'Referer': 'https://tver.jp/',
             })
-        p_id = traverse_obj(video_info, ('video', 'accountID'))
-        r_id = 'ref:' + traverse_obj(video_info, ('video', 'videoRefID'))
-        bc_url = smuggle_url(
-            self.BRIGHTCOVE_URL_TEMPLATE % (p_id, r_id),
-            {'geo_countries': ['JP']})
+        p_id = video_info['video']['accountID']
+        r_id = traverse_obj(video_info, ('video', ('videoRefID', 'videoID')), get_all=False)
+        if not r_id:
+            raise ExtractorError('Failed to extract reference ID for Brightcove')
+        if not r_id.isdigit():
+            r_id = f'ref:{r_id}'
 
         return {
             '_type': 'url_transparent',
             'title': str_or_none(video_info.get('title')),
             'description': str_or_none(video_info.get('description')),
-            'url': bc_url,
+            'url': smuggle_url(
+                self.BRIGHTCOVE_URL_TEMPLATE % (p_id, r_id), {'geo_countries': ['JP']}),
             'ie_key': 'BrightcoveNew',
         }
