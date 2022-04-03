ce0f94a075961e4059f69fcc57622ca91249e0ab
hatienl0i261299 <loiht.b17vt220@stu.ptit.edu.vn>
2022-04-03 13:04:15 +0700

    [Imdb] Improve extractor

--- yt_dlp/extractor/imdb.py
+++ yt_dlp/extractor/imdb.py
@@ -7,8 +7,9 @@ import re
 from .common import InfoExtractor
 from ..utils import (
     determine_ext,
+    int_or_none,
+    js_to_json,
     mimetype2ext,
-    parse_duration,
     qualities,
     try_get,
     url_or_none,
@@ -28,6 +29,17 @@ class ImdbIE(InfoExtractor):
             'title': 'No. 2',
             'description': 'md5:87bd0bdc61e351f21f20d2d7441cb4e7',
             'duration': 152,
+            'thumbnail': r're:^https?://.+\.jpg',
+        }
+    }, {
+        'url': 'https://www.imdb.com/video/vi3516832537',
+        'info_dict': {
+            'id': '3516832537',
+            'ext': 'mp4',
+            'title': 'Paul: U.S. Trailer #1',
+            'description': 'md5:17fcc4fe11ec29b4399be9d4c5ef126c',
+            'duration': 153,
+            'thumbnail': r're:^https?://.+\.jpg',
         }
     }, {
         'url': 'http://www.imdb.com/video/_/vi2524815897',
@@ -52,7 +64,20 @@ class ImdbIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        data = self._download_json(
+        webpage = self._download_webpage(f'https://www.imdb.com/video/vi{video_id}', video_id)
+
+        info = self._parse_json(
+            self._search_regex(r'(?s)<script\s+id=\"__NEXT_DATA__\"\s+type=\"application/json\"[^>]*>(.*?)\</script\>',
+                               webpage, 'json metadata'), video_id=video_id, transform_source=js_to_json)
+
+        video_info = try_get(info, lambda x: x['props']['pageProps']['videoPlaybackData']['video']) or {}
+
+        title = try_get(video_info, lambda x: x['name']['value']) or try_get(
+            video_info, lambda x: x['primaryTitle']['titleText']['text']) or self._html_search_meta(
+            ['og:title', 'twitter:title'], webpage) or self._html_search_regex(
+            r'<title>(.+?)</title>', webpage, 'title', default=None)
+
+        data = video_info.get('playbackURLs') or try_get(self._download_json(
             'https://www.imdb.com/ve/data/VIDEO_PLAYBACK_DATA', video_id,
             query={
                 'key': base64.b64encode(json.dumps({
@@ -60,11 +85,11 @@ class ImdbIE(InfoExtractor):
                     'subType': 'FORCE_LEGACY',
                     'id': 'vi%s' % video_id,
                 }).encode()).decode(),
-            })[0]
+            }), lambda x: x[0]['videoLegacyEncodings'])
 
         quality = qualities(('SD', '480p', '720p', '1080p'))
-        formats = []
-        for encoding in data['videoLegacyEncodings']:
+        formats, subtitles = [], {}
+        for encoding in data:
             if not encoding or not isinstance(encoding, dict):
                 continue
             video_url = url_or_none(encoding.get('url'))
@@ -73,11 +98,13 @@ class ImdbIE(InfoExtractor):
             ext = mimetype2ext(encoding.get(
                 'mimeType')) or determine_ext(video_url)
             if ext == 'm3u8':
-                formats.extend(self._extract_m3u8_formats(
+                fmts, subs = self._extract_m3u8_formats_and_subtitles(
                     video_url, video_id, 'mp4', entry_protocol='m3u8_native',
-                    preference=1, m3u8_id='hls', fatal=False))
+                    preference=1, m3u8_id='hls', fatal=False)
+                subtitles = self._merge_subtitles(subtitles, subs)
+                formats.extend(fmts)
                 continue
-            format_id = encoding.get('definition')
+            format_id = try_get(encoding, lambda x: x['displayName']['value']) or encoding.get('definition')
             formats.append({
                 'format_id': format_id,
                 'url': video_url,
@@ -86,33 +113,15 @@ class ImdbIE(InfoExtractor):
             })
         self._sort_formats(formats)
 
-        webpage = self._download_webpage(
-            'https://www.imdb.com/video/vi' + video_id, video_id)
-        video_metadata = self._parse_json(self._search_regex(
-            r'args\.push\(\s*({.+?})\s*\)\s*;', webpage,
-            'video metadata'), video_id)
-
-        video_info = video_metadata.get('VIDEO_INFO')
-        if video_info and isinstance(video_info, dict):
-            info = try_get(
-                video_info, lambda x: x[list(video_info.keys())[0]][0], dict)
-        else:
-            info = {}
-
-        title = self._html_search_meta(
-            ['og:title', 'twitter:title'], webpage) or self._html_search_regex(
-            r'<title>(.+?)</title>', webpage, 'title',
-            default=None) or info['videoTitle']
-
         return {
             'id': video_id,
             'title': title,
             'alt_title': info.get('videoSubTitle'),
             'formats': formats,
-            'description': info.get('videoDescription'),
-            'thumbnail': url_or_none(try_get(
-                info, lambda x: x['videoSlate']['source'])),
-            'duration': parse_duration(info.get('videoRuntime')),
+            'description': try_get(video_info, lambda x: x['description']['value']),
+            'thumbnail': url_or_none(try_get(video_info, lambda x: x['thumbnail']['url'])),
+            'duration': int_or_none(try_get(video_info, lambda x: x['runtime']['value'])),
+            'subtitles': subtitles,
         }
 
 
