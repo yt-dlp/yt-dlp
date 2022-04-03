a69b052d9a27322de63c9a5f5578ff102dd6ffd9
pukkandan <pukkandan.ytdlp@gmail.com>
2022-04-03 20:39:52 +0530

    cleanup

--- yt_dlp/extractor/elonet.py
+++ yt_dlp/extractor/elonet.py
@@ -2,11 +2,7 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..utils import (
-    determine_ext,
-    try_get,
-)
-from ..compat import compat_str
+from ..utils import determine_ext
 
 
 class ElonetIE(InfoExtractor):
@@ -17,12 +13,10 @@ class ElonetIE(InfoExtractor):
             'id': '107867',
             'ext': 'mp4',
             'title': 'Valkoinen peura',
-            'description': 'Valkoinen peura (1952) on Erik Blombergin ohjaama ja yhdessä Mirjami Kuosmasen kanssa käsikirjoittama tarunomainen kertomus valkoisen peuran hahmossa lii...',
             'thumbnail': r're:^https?://elonet\.finna\.fi/Cover/Show\?id=kavi\.elonet_elokuva_107867.+',
+            'description': 'md5:bded4201c9677fab10854884fe8f7312',
         },
-        'params': {
-            'skip_download': 'dash',
-        },
+        'params': {'skip_download': 'dash'},
     }, {
         # DASH with subtitles
         'url': 'https://elonet.finna.fi/Record/kavi.elonet_elokuva_116539',
@@ -30,12 +24,10 @@ class ElonetIE(InfoExtractor):
             'id': '116539',
             'ext': 'mp4',
             'title': 'Minulla on tiikeri',
-            'description': 'Pienellä pojalla, joka asuu kerrostalossa, on kotieläimenä tiikeri. Se on kuitenkin salaisuus. Kerrostalon räpätäti on Kotilaisen täti, joka on aina vali...',
             'thumbnail': r're:^https?://elonet\.finna\.fi/Cover/Show\?id=kavi\.elonet_elokuva_116539.+',
+            'description': 'md5:5ab72b3fe76d3414e46cc8f277104419',
         },
-        'params': {
-            'skip_download': 'dash',
-        },
+        'params': {'skip_download': 'dash'},
     }, {
         # Page with multiple videos, download the main one
         'url': 'https://elonet.finna.fi/Record/kavi.elonet_elokuva_117396',
@@ -43,48 +35,34 @@ class ElonetIE(InfoExtractor):
             'id': '117396',
             'ext': 'mp4',
             'title': 'Sampo',
-            'description': 'Aleksandr Ptushkon ohjaama, neuvostoliittolais-suomalainen yhteistuotanto Sampo (1959) on Kalevalan tarustoon pohjautuva fantasiaelokuva. Pohjolan emäntä...',
             'thumbnail': r're:^https?://elonet\.finna\.fi/Cover/Show\?id=kavi\.elonet_elokuva_117396.+',
+            'description': 'md5:ec69572a5b054d0ecafe8086b1fa96f7',
         },
-        'params': {
-            'skip_download': 'dash',
-        },
+        'params': {'skip_download': 'dash'},
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        title = self._html_search_regex(
-            r'<meta .*property="og&#x3A;title" .*content="(.+?)"', webpage, 'title')
-        description = self._html_search_regex(
-            r'<meta .*property="og&#x3A;description" .*content="(.+?)"', webpage, 'description')
-        thumbnail = self._html_search_regex(
-            r'<meta .*property="og&#x3A;image" .*content="(.+?)"', webpage, 'thumbnail')
-
-        json_s = self._html_search_regex(
-            r'id=\'video-data\'[^>]+data-video-sources="(.+?)"', webpage, 'json')
-        src = try_get(
-            self._parse_json(json_s, video_id),
-            lambda x: x[0]["src"], compat_str)
-
-        formats = []
-        subtitles = {}
-
+        src = self._parse_json(self._html_search_regex(
+            r'id=\'video-data\'[^>]+data-video-sources="([^"]+)"', webpage, 'json'), video_id)[0]['src']
         ext = determine_ext(src)
+
         if ext == 'm3u8':
             formats, subtitles = self._extract_m3u8_formats_and_subtitles(src, video_id, fatal=False)
         elif ext == 'mpd':
             formats, subtitles = self._extract_mpd_formats_and_subtitles(src, video_id, fatal=False)
         else:
-            self.report_warning('Unknown streaming format %s' % ext)
+            formats, subtitles = [], {}
+            self.raise_no_formats(f'Unknown streaming format {ext}')
         self._sort_formats(formats)
 
         return {
             'id': video_id,
-            'title': title,
-            'description': description,
-            'thumbnail': thumbnail,
+            'title': self._og_search_title(webpage),
+            'description': self._og_search_description(webpage),
+            'thumbnail': self._og_search_thumbnail(webpage),
             'formats': formats,
             'subtitles': subtitles,
         }
