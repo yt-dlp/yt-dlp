0682bceaf7803c69e343e41e8c64cfa78bbd4b5a
Panatexxa <panatexxa@home>
2022-04-04 21:45:17 +0200

    Address review
    - fix coding style
    - remove comments
    - simplify _VALID_URL
    - set `display_id` instead of `id`
    - ask DailymotionIE for correct string
    - remove height and width

--- yt_dlp/extractor/moviepilot.py
+++ yt_dlp/extractor/moviepilot.py
@@ -1,31 +1,31 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from .dailymotion import DailymotionIE
 from .common import InfoExtractor
 from ..utils import (
-    int_or_none,
-    parse_duration,
     parse_iso8601,
     unescapeHTML,
 )
 
+import re
+
 
 class MoviepilotIE(InfoExtractor):
-    _IE_NAME = "moviepilot"
-    _IE_DESC = "Moviepilot trailer"
-    _VALID_URL = r'https?://(?:www\.)?moviepilot\.de/movies/(?P<id>[^/]+)/?.*'
+    _IE_NAME = 'moviepilot'
+    _IE_DESC = 'Moviepilot trailer'
+    _VALID_URL = r'https?://(?:www\.)?moviepilot\.de/movies/(?P<id>[^/]+)'
 
     _TESTS = [{
         'url': 'https://www.moviepilot.de/movies/interstellar-2/',
         'info_dict': {
-            # 'id': 'interstellar-2',
             'id': 'x7xdut5',
+            'display_id': 'interstellar-2',
             'ext': 'mp4',
             'title': 'Interstellar',
             'thumbnail': 'https://s1.dmcdn.net/v/SaXev1VvzitVZMFsR/x720',
             'timestamp': 1400491705,
             'description': 'md5:7dfc5c1758e7322a7346934f1f0c489c',
-            # coming from Dailymotion
             'uploader': 'Moviepilot',
             'like_count': int,
             'view_count': int,
@@ -36,7 +36,7 @@ class MoviepilotIE(InfoExtractor):
             'tags': ['Alle Trailer', 'Movie', 'Third Party'],
         },
     }, {
-        'url': 'https://www.moviepilot.de/movies/interstellar-2',
+        'url': 'https://www.moviepilot.de/movies/interstellar-2/trailer',
         'only_matching': True,
     }, {
         'url': 'https://www.moviepilot.de/movies/interstellar-2/kinoprogramm/berlin',
@@ -45,13 +45,12 @@ class MoviepilotIE(InfoExtractor):
         'url': 'https://www.moviepilot.de/movies/queen-slim/trailer',
         'info_dict': {
             'id': 'x7xj6o7',
-            # 'id': 'queen-slim',
-            'title': 'Queen & Slim',  # test escaping
+            'display_id': 'queen-slim',
+            'title': 'Queen & Slim',
             'ext': 'mp4',
             'thumbnail': 'https://s2.dmcdn.net/v/SbUM71WtomSjVmI_q/x720',
             'timestamp': 1571838685,
             'description': 'md5:73058bcd030aa12d991e4280d65fbebe',
-            # coming from Dailymotion
             'uploader': 'Moviepilot',
             'like_count': int,
             'view_count': int,
@@ -73,8 +72,8 @@ class MoviepilotIE(InfoExtractor):
         'url': 'https://www.moviepilot.de/movies/muellers-buero/',
         'info_dict': {
             'id': 'x7xcw1i',
-            # 'id': 'muellers-buero',
-            'title': 'Müllers Büro',  # test umlauts
+            'display_id': 'muellers-buero',
+            'title': 'Müllers Büro',
             'ext': 'mp4',
             'description': 'md5:57501251c05cdc61ca314b7633e0312e',
             'timestamp': 1287584475,
@@ -96,26 +95,25 @@ class MoviepilotIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        # normalize url
-        url = f"https://www.moviepilot.de/movies/{video_id}/trailer"
+        webpage = self._download_webpage(f'https://www.moviepilot.de/movies/{video_id}/trailer', video_id)
 
-        webpage = self._download_webpage(url, video_id)
+        # oriented on parse_duration()
+        d = re.match(r'P(?P<hours>[0-9]+)H(?P<mins>[0-9]{2})M(?P<secs>[0-9]{2})S',
+                     self._get_property("duration", webpage, fatal=False))
+        if d:
+            hours, mins, secs = d.groups()
+            duration = float(hours) * 60 * 60 + float(mins) * 60 + float(secs)
+        else:
+            duration = None
 
-        return {'_type': 'url_transparent',
-                'ie_key': 'Dailymotion',
-                'id': video_id,
-                # Currently, the movie title is extracted, e.g. "Interstellar" or "Müllers Büro"
-                # The actual trailer title is: "Interstellar - Trailer 2 (Deutsch) HD" or
-                # "MÃ¼llers BÃ¼ro - Trailer (Deutsch)", umlauts are really broken here.
-                # I prefer to keep the actual movie title or is there an extra metadata field for that?
-                'title': self._og_search_title(webpage),
-                'url': self._get_property("embedURL", webpage),
-                'thumbnail': self._get_property("thumbnailURL", webpage, fatal=False),
-                # The duration is something like "P00H02M20S" but parse_duration cannot parse this
-                # should I write an extra function for that or should parse_duration be able to parse this?
-                # 'duration': parse_duration(self._get_property("duration", webpage, fatal=False))
-                'description': unescapeHTML(self._get_property('description', webpage, fatal=False)),
-                'timestamp': parse_iso8601(self._get_property('uploadDate', webpage, fatal=False),
-                                           delimiter=" "),
-                'height': int_or_none(self._get_property('height', webpage, fatal=False)),
-                'width': int_or_none(self._get_property('width', webpage, fatal=False))}
+        return {
+            '_type': 'url_transparent',
+            'ie_key': DailymotionIE.ie_key(),
+            'display_id': video_id,
+            'title': self._og_search_title(webpage),
+            'url': self._get_property('embedURL', webpage),
+            'thumbnail': self._get_property('thumbnailURL', webpage, fatal=False),
+            'description': unescapeHTML(self._get_property('description', webpage, fatal=False)),
+            'duration': duration,
+            'timestamp': parse_iso8601(self._get_property('uploadDate', webpage, fatal=False), delimiter=' ')
+        }
