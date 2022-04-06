2ba385ccd2c58b27fbb30671f741ff28db9d8efe
Lesmiscore <nao20010128@gmail.com>
2022-04-06 17:18:29 +0900

    sort formats

--- yt_dlp/extractor/twitcasting.py
+++ yt_dlp/extractor/twitcasting.py
@@ -184,11 +184,11 @@ class TwitCastingIE(InfoExtractor):
                 'formats': formats
             }
         elif len(m3u8_urls) == 1:
+            formats = self._extract_m3u8_formats(
+                m3u8_urls[0], video_id, 'mp4', headers=self._M3U8_HEADERS)
             infodict = {
-                'id': video_id,
                 # No problem here since there's only one manifest
-                'formats': self._extract_m3u8_formats(
-                    m3u8_urls[0], video_id, 'mp4', headers=self._M3U8_HEADERS),
+                'formats': formats,
             }
         else:
             infodict = {
