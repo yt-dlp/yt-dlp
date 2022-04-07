22fba53fbd903cd42b0f4ef24c539a4f818fd6e9
pukkandan <pukkandan.ytdlp@gmail.com>
2022-04-07 15:46:53 +0530

    [FfmpegMetadata] Write id3v1 tags

--- yt_dlp/postprocessor/ffmpeg.py
+++ yt_dlp/postprocessor/ffmpeg.py
@@ -769,6 +769,9 @@ class FFmpegMetadataPP(FFmpegPostProcessor):
             if value is not None and mobj:
                 metadata[mobj.group('i') or 'common'][mobj.group('key')] = value
 
+        # Write id3v1 metadata also since Windows Explorer can't handle id3v2 tags
+        yield ('-write_id3v1', '1')
+
         for name, value in metadata['common'].items():
             yield ('-metadata', f'{name}={value}')
 
