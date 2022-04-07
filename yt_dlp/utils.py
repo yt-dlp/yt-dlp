bc8d34934d5e93dd67509b3a0bb479222332fe21
pukkandan <pukkandan.ytdlp@gmail.com>
2022-04-07 11:26:12 +0530

    better comments

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -2229,10 +2229,10 @@ class locked_file(object):
         writable = any(f in mode for f in 'wax+')
         readable = any(f in mode for f in 'r+')
         flags = functools.reduce(operator.ior, (
-            getattr(os, 'O_CLOEXEC', 0),  # win32 does not have O_CLOEXEC
-            getattr(os, 'O_BINARY', 0),  # UNIX don't have O_BINARY and O_NOINHERIT
-            getattr(os, 'O_NOINHERIT', 0),
-            os.O_CREAT if writable else 0,  # do not pass O_TRUNC
+            getattr(os, 'O_CLOEXEC', 0),  # UNIX only
+            getattr(os, 'O_BINARY', 0),  # Windows only
+            getattr(os, 'O_NOINHERIT', 0),  # Windows only
+            os.O_CREAT if writable else 0,  # O_TRUNC only after locking
             os.O_APPEND if 'a' in mode else 0,
             os.O_EXCL if 'x' in mode else 0,
             os.O_RDONLY if not writable else os.O_RDWR if readable else os.O_WRONLY,
