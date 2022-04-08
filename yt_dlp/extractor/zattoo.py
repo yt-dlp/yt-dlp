2b83b0a996b86bb2f116eeeef76e9ea542937cc1
Alexander Seiler <seileralex@gmail.com>
2022-04-08 04:08:28 +0200

    Fix extraction of recordings

--- yt_dlp/extractor/zattoo.py
+++ yt_dlp/extractor/zattoo.py
@@ -253,7 +253,7 @@ class ZattooBaseIE(ZattooPlatformBaseIE):
 
     @staticmethod
     def _create_valid_url(match, qs, base_re=None):
-        match_base = fr'|{base_re}/(?P<vid1>{match})' if base_re else ''
+        match_base = fr'|{base_re}/(?P<vid1>{match})' if base_re else '(?P<vid1>)'
         return rf'''(?x)https?://(?:www\.)?zattoo\.com/(?:
             [^?#]+\?(?:[^#]+&)?{qs}=(?P<vid2>{match})
             {match_base}
