79079ec839e87bd7c19969d0848247378e1e9a08
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2022-04-10 01:19:23 +0530

    Update yt_dlp/extractor/masters.py
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/extractor/masters.py
+++ yt_dlp/extractor/masters.py
@@ -22,7 +22,7 @@ class MastersIE(InfoExtractor):
     _CONTENT_API_URL = "https://www.masters.com/relatedcontent/rest/v2/masters_v1/en/content/masters_v1_{video_id}_en"
 
     def _real_extract(self, url):
-        video_id, upload_date = self._match_id(url), self._match_valid_url(url).group('date')
+        video_id, upload_date = self._match_valid_url(url).group('id', 'date')
         content_resp = self._download_json(
             f'https://www.masters.com/relatedcontent/rest/v2/masters_v1/en/content/masters_v1_{video_id}_en',
             video_id)
