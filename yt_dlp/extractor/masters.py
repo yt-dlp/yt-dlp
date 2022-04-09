293d8b61ad87194ccd10cde1af0cd29e2c322d29
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2022-04-10 01:49:47 +0530

    [masters] changed coding convention

--- yt_dlp/extractor/masters.py
+++ yt_dlp/extractor/masters.py
@@ -26,15 +26,17 @@ class MastersIE(InfoExtractor):
         content_resp = self._download_json(
             f'https://www.masters.com/relatedcontent/rest/v2/masters_v1/en/content/masters_v1_{video_id}_en',
             video_id)
-        formats = self._extract_m3u8_formats(traverse_obj(content_resp, ('media', 'm3u8')), video_id, 'mp4')
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(traverse_obj(content_resp, ('media', 'm3u8')), video_id, 'mp4')
         self._sort_formats(formats)
 
-        thumbnails = [{'id': name, 'url': url} for name, url in traverse_obj(content_resp, ('images', 0)) or []]
+        thumbnail_data = traverse_obj(content_resp, ('images', 0))
+        thumbnails = [{'id': name, 'url': thumbnail_data.get(name)} for name in thumbnail_data or []]
 
         return {
             'id': video_id,
             'title': content_resp.get('title'),
             'formats': formats,
+            'subtitles': subtitles,
             'upload_date': unified_strdate(upload_date),
             'thumbnails': thumbnails,
         }
