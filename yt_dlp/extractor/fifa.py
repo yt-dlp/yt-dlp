e6dc97d81879c26cc7adbcde5dceff4faab295c6
4t14s <216170+Bricio@users.noreply.github.com>
2022-04-12 23:14:05 -0300

    After first review.

--- yt_dlp/extractor/fifa.py
+++ yt_dlp/extractor/fifa.py
@@ -20,7 +20,7 @@ class FifaIE(InfoExtractor):
             'thumbnail': 'https://digitalhub.fifa.com/transform/fa6f0b3e-a2e9-4cf7-9f32-53c57bcb7360/2006_Final_ITA_FRA',
             'duration': 8164,
         },
-        'expected_warnings': ['The stream has AES-128 encryption and neither ffmpeg nor pycryptodomex are available; Decryption will be performed natively, but will be extremely slow'],
+        'params': {'skip_download': 'm3u8'},
     }, {
         'url': 'https://www.fifa.com/fifaplus/pt/watch/1cg5r5Qt6Qt12ilkDgb1sV',
         'info_dict': {
@@ -34,7 +34,7 @@ class FifaIE(InfoExtractor):
             'release_timestamp': 1404777600,
             'release_date': '20140708',
         },
-        'expected_warnings': ['The stream has AES-128 encryption and neither ffmpeg nor pycryptodomex are available; Decryption will be performed natively, but will be extremely slow'],
+        'params': {'skip_download': 'm3u8'},
     }, {
         'url': 'https://www.fifa.com/fifaplus/fr/watch/3C6gQH9C2DLwzNx7BMRQdp',
         'info_dict': {
@@ -46,62 +46,61 @@ class FifaIE(InfoExtractor):
             'duration': 28,
             'thumbnail': 'https://digitalhub.fifa.com/transform/f9301391-f8d9-48b5-823e-c093ac5e3e11/CG_MEN_1986_JOSIMAR',
         },
-        'expected_warnings': ['The stream has AES-128 encryption and neither ffmpeg nor pycryptodomex are available; Decryption will be performed natively, but will be extremely slow'],
+        'params': {'skip_download': 'm3u8'},
     }]
 
     def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        video_id = mobj.group('id')
-        locale = mobj.group('locale')
+        video_id, locale = self._match_valid_url(url).group('id', 'locale')
         webpage = self._download_webpage(url, video_id)
 
-        preconnect_link = self._search_regex(r'<link[^>]+?rel\s*=\s*"preconnect"[^>]+href\s*=\s*"([^"]+)"', webpage, 'Preconnect Link')
+        preconnect_link = self._search_regex(r'<link[^>]+rel\s*=\s*"preconnect"[^>]+href\s*=\s*"([^"]+)"', webpage, 'Preconnect Link')
 
-        json_data = self._download_json(f'{preconnect_link}/video/GetVideoPlayerData/{video_id}',
-                                        video_id, 'Downloading Video Player Data', query={'includeIdents': True, 'locale': locale})
+        json_data = self._download_json(
+            f'{preconnect_link}/video/GetVideoPlayerData/{video_id}', video_id,
+            'Downloading Video Player Data', query={'includeIdents': True, 'locale': locale})
 
         video_details = self._download_json(f'{preconnect_link}/sections/videoDetails/{video_id}', video_id, 'Downloading Video Details', fatal=False)
 
         preplay_parameters = self._download_json(
             f'{preconnect_link}/video/GetVerizonPreplayParameters', video_id, 'Downloading Preplay Parameters', query={
                 'entryId': video_id,
-                'assetId': json_data.get('verizonAssetId'),
+                'assetId': json_data['verizonAssetId'],
                 'useExternalId': False,
-                'requiresToken': json_data.get('requiresToken'),
+                'requiresToken': json_data['requiresToken'],
                 'adConfig': 'fifaplusvideo',
                 'prerollAds': True,
-                'adVideoId': json_data.get('externalVerizonAssetId'),
-                'preIdentId': json_data.get('preIdentId'),
-                'postIdentId': json_data.get('postIdentId'),
+                'adVideoId': json_data['externalVerizonAssetId'],
+                'preIdentId': json_data['preIdentId'],
+                'postIdentId': json_data['postIdentId'],
             })
 
-        cid = f'{json_data.get("preIdentId")},{json_data.get("verizonAssetId")},{json_data.get("postIdentId")}'
+        cid = f'{json_data["preIdentId"]},{json_data["verizonAssetId"]},{json_data["postIdentId"]}'
         content_data = self._download_json(
             f'https://content.uplynk.com/preplay/{cid}/multiple.json', video_id, 'Downloading Content Data', query={
-                'v': preplay_parameters.get('preplayAPIVersion'),
-                'tc': preplay_parameters.get('tokenCheckAlgorithmVersion'),
-                'rn': preplay_parameters.get('randomNumber'),
-                'exp': preplay_parameters.get('tokenExpirationDate'),
-                'ct': preplay_parameters.get('contentType'),
+                'v': preplay_parameters['preplayAPIVersion'],
+                'tc': preplay_parameters['tokenCheckAlgorithmVersion'],
+                'rn': preplay_parameters['randomNumber'],
+                'exp': preplay_parameters['tokenExpirationDate'],
+                'ct': preplay_parameters['contentType'],
                 'cid': cid,
-                'mbtracks': preplay_parameters.get('tracksAssetNumber'),
-                'ad': preplay_parameters.get('adConfiguration'),
-                'ad.preroll': int(preplay_parameters.get('adPreroll')),
-                'ad.cmsid': preplay_parameters.get('adCMSSourceId'),
-                'ad.vid': preplay_parameters.get('adSourceVideoID'),
-                'sig': preplay_parameters.get('signature'),
+                'mbtracks': preplay_parameters['tracksAssetNumber'],
+                'ad': preplay_parameters['adConfiguration'],
+                'ad.preroll': int(preplay_parameters['adPreroll']),
+                'ad.cmsid': preplay_parameters['adCMSSourceId'],
+                'ad.vid': preplay_parameters['adSourceVideoID'],
+                'sig': preplay_parameters['signature'],
             })
 
         formats = self._extract_m3u8_formats(
-            content_data.get('playURL'), video_id, note='Downloading m3u8 Information')
+            content_data['playURL'], video_id, note='Downloading m3u8 Information')
 
         return {
             'id': video_id,
-            'title': json_data.get('title'),
+            'title': json_data['title'],
             'description': json_data.get('description'),
             'duration': int_or_none(json_data.get('duration')),
             'release_timestamp': unified_timestamp(video_details.get('dateOfRelease')),
-            'categories': [video_details.get('videoCategory'), video_details.get('videoSubcategory')],
+            'categories': traverse_obj(video_details, (('videoCategory', 'videoSubcategory'))),
             'thumbnail': traverse_obj(video_details, ('backgroundImage', 'src')),
             'formats': formats,
         }
