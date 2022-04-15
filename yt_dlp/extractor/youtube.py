d4d313a8a3d54bd872adf106a0b065696d6ad9a2
coletdjnz <coletdjnz@protonmail.com>
2022-04-15 15:51:32 +1200

    No need to support RLTD playlist in YoutubeStoriesIE

--- yt_dlp/extractor/youtube.py
+++ yt_dlp/extractor/youtube.py
@@ -5824,7 +5824,7 @@ class YoutubeHistoryIE(YoutubeFeedsInfoExtractor):
 class YoutubeStoriesIE(InfoExtractor):
     IE_DESC = 'YouTube channel stories; "ytstories:" prefix'
     IE_NAME = 'youtube:stories'
-    _VALID_URL = r'ytstories:(?:RLTD|UC)(?P<id>[A-Za-z0-9_-]{21}[AQgw])$'
+    _VALID_URL = r'ytstories:UC(?P<id>[A-Za-z0-9_-]{21}[AQgw])$'
     _TESTS = [{
         'url': 'ytstories:UCwFCb4jeqaKWnciAYM-ZVHg',
         'only_matching': True,
