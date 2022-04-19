62f6f1cbf253240a026a70538b5b58945563fc90
pukkandan <pukkandan.ytdlp@gmail.com>
2022-04-20 02:25:33 +0530

    Don't imply `-s` for later stages of `-O`

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -3013,7 +3013,7 @@ def qualities(quality_ids):
     return q
 
 
-POSTPROCESS_WHEN = {'pre_process', 'after_filter', 'before_dl', 'after_move', 'post_process', 'after_video', 'playlist'}
+POSTPROCESS_WHEN = ('pre_process', 'after_filter', 'before_dl', 'after_move', 'post_process', 'after_video', 'playlist')
 
 
 DEFAULT_OUTTMPL = {
