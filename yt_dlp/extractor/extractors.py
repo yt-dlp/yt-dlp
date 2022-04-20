298dde16d70ad1d2eb7687874103f12cac626583
FraFraFra-LongD <85188920+FraFraFra-LongD@users.noreply.github.com>
2022-04-20 09:27:28 +0200

    Update extractors.py

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1674,6 +1674,7 @@ from .theweatherchannel import TheWeatherChannelIE
 from .thisamericanlife import ThisAmericanLifeIE
 from .thisav import ThisAVIE
 from .thisoldhouse import ThisOldHouseIE
+from .thisvid import ThisVidIE
 from .threespeak import (
     ThreeSpeakIE,
     ThreeSpeakUserIE,
