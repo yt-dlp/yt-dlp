de7dd6872d4e7505e9bb297b162279167ba8bbf5
Evan Spensley <94762716+evansp@users.noreply.github.com>
2022-04-21 17:20:47 -0400

    Apply suggestions from code review
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/postprocessor/embedthumbnail.py
+++ yt_dlp/postprocessor/embedthumbnail.py
@@ -80,14 +80,11 @@ class EmbedThumbnailPP(FFmpegPostProcessor):
         original_thumbnail = thumbnail_filename = info['thumbnails'][idx]['filepath']
 
         thumbnail_ext = os.path.splitext(thumbnail_filename)[1][1:]
-        if info['ext'] not in ['mkv', 'mka']:
-            # Convert unsupported thumbnail formats to PNG (see #25687, #25717)
-            # Original behavior was to convert to JPG, but since JPG is a lossy
-            # format, there will be some additional data loss.
-            # PNG, on the other hand, is lossless.
-            if thumbnail_ext not in ('jpg', 'jpeg', 'png'):
-                thumbnail_filename = convertor.convert_thumbnail(thumbnail_filename, 'png')
-                thumbnail_ext = 'png'
+        # Convert unsupported thumbnail formats (see #25687, #25717)
+        # PNG is preferred since JPEG is lossy
+        if info['ext'] not in ('mkv', 'mka') and thumbnail_ext not in ('jpg', 'jpeg', 'png'):
+            thumbnail_filename = convertor.convert_thumbnail(thumbnail_filename, 'png')
+            thumbnail_ext = 'png'
 
         mtime = os.stat(encodeFilename(filename)).st_mtime
 
@@ -103,7 +100,7 @@ class EmbedThumbnailPP(FFmpegPostProcessor):
         elif info['ext'] in ['mkv', 'mka']:
             options = list(self.stream_copy_opts())
 
-            mimetype = 'image/%s' % ('jpeg' if thumbnail_ext == 'jpg' else thumbnail_ext)
+            mimetype = 'image/%s' % ('jpeg' if thumbnail_ext in ('jpg', 'jpeg') else thumbnail_ext)
             old_stream, new_stream = self.get_stream_number(
                 filename, ('tags', 'mimetype'), mimetype)
             if old_stream is not None:
