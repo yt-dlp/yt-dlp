5b614baa84d128e5127c341f5d634dbf5c535ec8
pukkandan <pukkandan.ytdlp@gmail.com>
2022-04-24 06:26:40 +0530

    refactor

--- yt_dlp/extractor/icareus.py
+++ yt_dlp/extractor/icareus.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-from __future__ import unicode_literals
-
 import re
 
 from .common import InfoExtractor
@@ -9,9 +6,11 @@ from ..utils import (
     determine_ext,
     get_element_by_class,
     int_or_none,
+    merge_dicts,
     parse_bitrate,
     parse_resolution,
     remove_end,
+    str_or_none,
     urlencode_postdata,
     url_or_none,
 )
@@ -82,31 +81,65 @@ class IcareusIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        base_url, maybe_id = self._match_valid_url(url).groups()
-        page = self._download_webpage(url, maybe_id)
-        video_id = self._search_regex(
-            r"_icareus\['itemId'\]\s*=\s*'(\d+)'", page, 'video_id')
-        api_base = self._search_regex(
-            r'var\s+publishingServiceURL\s*=\s*"(http[^"]+)";', page, 'api_base')
-        organization_id = self._search_regex(
-            r"_icareus\['organizationId'\]\s*=\s*'(\d+)'", page, 'organization_id')
-        token = self._search_regex(
-            r"_icareus\['token'\]\s*=\s*'([a-f0-9]+)'", page, 'token')
-        token2 = self._search_regex(
-            r"""data\s*:\s*{action:"getAsset".*?token:'([a-f0-9]+)'}""", page,
-            'token2', default=None, fatal=False)
-        livestream_title = get_element_by_class(
-            'unpublished-info-item future-event-title', page)
-        metad = self._search_json_ld(page, video_id, default=None)
+        base_url, temp_id = self._match_valid_url(url).groups()
+        webpage = self._download_webpage(url, temp_id)
 
-        info = {
-            'id': video_id,
-            'title': None
+        video_id = self._search_regex(r"_icareus\['itemId'\]\s*=\s*'(\d+)'", webpage, 'video_id')
+        organization_id = self._search_regex(r"_icareus\['organizationId'\]\s*=\s*'(\d+)'", webpage, 'organization_id')
+
+        assets = self._download_json(
+            self._search_regex(r'var\s+publishingServiceURL\s*=\s*"(http[^"]+)";', webpage, 'api_base'),
+            video_id, data=urlencode_postdata({
+                'version': '03',
+                'action': 'getAssetPlaybackUrls',
+                'organizationId': organization_id,
+                'assetId': video_id,
+                'token': self._search_regex(r"_icareus\['token'\]\s*=\s*'([a-f0-9]+)'", webpage, 'icareus_token'),
+            }))
+
+        subtitles = {
+            remove_end(sdesc.split(' ')[0], ':'): [{'url': url_or_none(surl)}]
+            for _, sdesc, surl in assets.get('subtitles') or []
         }
-        if metad:
-            info.update(metad)
-        elif token2:
-            metad = self._download_json(
+
+        formats = [{
+            'format': item.get('name'),
+            'format_id': 'audio',
+            'vcodec': 'none',
+            'url': url_or_none(item['url']),
+            'tbr': int_or_none(self._search_regex(
+                r'\((\d+)\s*k\)', item.get('name') or '', 'audio bitrate', default=None)),
+        } for item in assets.get('audio_urls') or [] if url_or_none(item.get('url'))]
+
+        for item in assets.get('urls') or []:
+            video_url = url_or_none(item.get('url'))
+            if video_url is None:
+                continue
+            ext = determine_ext(video_url)
+            if ext == 'm3u8':
+                fmts, subs = self._extract_m3u8_formats_and_subtitles(
+                    video_url, video_id, 'mp4', m3u8_id='hls', fatal=False)
+                formats.extend(fmts)
+                self._merge_subtitles(subs, target=subtitles)
+            else:
+                fmt = item.get('name')
+                formats.append({
+                    'url': video_url,
+                    'format': fmt,
+                    'tbr': parse_bitrate(fmt),
+                    'format_id': str_or_none(item.get('id')),
+                    **parse_resolution(fmt),
+                })
+
+        info, token, live_title = self._search_json_ld(webpage, video_id, default={}), None, None
+        if not info:
+            token = self._search_regex(
+                r'data\s*:\s*{action:"getAsset".*?token:\'([a-f0-9]+)\'}', webpage, 'token', default=None, fatal=False)
+            if not token:
+                live_title = get_element_by_class('unpublished-info-item future-event-title', webpage)
+
+        if token:
+            metadata = self._download_json(
                 f'{base_url}/icareus-suite-api-portlet/publishing',
                 video_id, fatal=False, data=urlencode_postdata({
                     'version': '03',
@@ -115,84 +148,33 @@ class IcareusIE(InfoExtractor):
                     'assetId': video_id,
                     'languageId': 'en_US',
                     'userId': '0',
-                    'token': token2,
-                }))
-            if metad:
-                info.update({
-                    'title': metad.get('name'),
-                    'description': metad.get('description'),
-                    'timestamp': int_or_none(metad.get('date'), scale=1000),
-                    'duration': int_or_none(metad.get('duration')),
-                    'thumbnail': url_or_none(metad.get('thumbnailMedium')),
-                })
-        elif livestream_title:  # Recorded livestream
-            info.update({
-                'title': livestream_title,
-                'description': get_element_by_class(
-                    'unpublished-info-item future-event-description', page),
+                    'token': token,
+                })) or {}
+            info = {
+                'title': metadata.get('name'),
+                'description': metadata.get('description'),
+                'timestamp': int_or_none(metadata.get('date'), scale=1000),
+                'duration': int_or_none(metadata.get('duration')),
+                'thumbnail': url_or_none(metadata.get('thumbnailMedium')),
+            }
+        elif live_title:  # Recorded livestream
+            info = {
+                'title': live_title,
+                'description': get_element_by_class('unpublished-info-item future-event-description', webpage),
                 'timestamp': int_or_none(self._search_regex(
-                    r"var startEvent\s*=\s*(\d+);", page, 'uploadDate',
-                    fatal=False), scale=1000),
-            })
+                    r'var startEvent\s*=\s*(\d+);', webpage, 'uploadDate', fatal=False), scale=1000),
+            }
 
-        if info['title'] is None:
-            self.report_warning("Could not extract metadata", video_id)
+        thumbnails = info.get('thumbnails') or [{
+            'url': url_or_none(info.get('thumbnail') or assets.get('thumbnail'))
+        }]
 
-        info['description'] = clean_html(info.get('description'))
-
-        jsond = self._download_json(
-            api_base, video_id, data=urlencode_postdata({
-                'version': '03',
-                'action': 'getAssetPlaybackUrls',
-                'organizationId': organization_id,
-                'assetId': video_id,
-                'token': token,
-            }))
-
-        if not any(x in info.keys() for x in ('thumbnail', 'thumbnails')):
-            info['thumbnail'] = url_or_none(jsond.get('thumbnail'))
-
-        formats = []
-        for item in jsond.get('urls') or []:
-            video_url = url_or_none(item.get('url'))
-            if video_url is None:
-                continue
-            ext = determine_ext(video_url)
-            if ext == 'm3u8':
-                formats.extend(self._extract_m3u8_formats(
-                    video_url, video_id, 'mp4',
-                    entry_protocol='m3u8_native', m3u8_id='hls',
-                    fatal=False))
-            else:
-                fmt = item.get('name')
-                fd = {
-                    'url': video_url,
-                    'format': fmt,
-                    'tbr': parse_bitrate(fmt),
-                    'format_id': str(item.get('id', '')) or None,
-                }
-                fd.update(parse_resolution(fmt))
-                formats.append(fd)
-
-        formats.extend({
-            'format': item.get('name'),
-            'format_id': 'audio',
-            'vcodec': 'none',
-            'url': url_or_none(item.get('url')),
-            'tbr': int_or_none(self._search_regex(
-                r"\((\d+)\s*k\)", item.get('name', ''), 'audio bitrate',
-                default=None)),
-        } for item in jsond.get('audio_urls') or []
-            if url_or_none(item.get('url')) is not None)
-
-        subtitles = {
-            remove_end(sdesc.split(' ')[0], ':'): [{'url': url_or_none(surl)}]
-            for scode, sdesc, surl in jsond.get('subtitles') or []
-        }
-
-        info.update({
+        self._sort_formats(formats)
+        return merge_dicts({
+            'id': video_id,
+            'title': None,
             'formats': formats,
             'subtitles': subtitles,
-        })
-
-        return info
+            'description': clean_html(info.get('description')),
+            'thumbnails': thumbnails if thumbnails[0]['url'] else None,
+        }, info)
