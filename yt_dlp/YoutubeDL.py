94aade999d04c49166ed79a2056be8e4731029d5
Lesmiscore <nao20010128@gmail.com>
2022-04-27 16:33:56 +0900

    use groups

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -2149,7 +2149,7 @@ class YoutubeDL:
                 mobj = re.match(
                     r'(?P<m>merge)?all(?P<t>video|audio)?(?P<l>lang)?', format_spec)
                 if mobj:
-                    mg, tp, la = mobj.groups()
+                    mg, tp, la = mobj.group('m', 't', 'l')
                     def selector_function(ctx):
                         formats = ctx['formats'][::-1]
 
