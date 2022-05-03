eb84801a9b607b30a62fe3d38865dee27c2bc602
pukkandan <pukkandan.ytdlp@gmail.com>
2022-05-03 21:51:26 +0530

    fix issue from merging 069a38f07335f2531d2aad0b69a024580fac2fc1

--- yt_dlp/extractor/tiktok.py
+++ yt_dlp/extractor/tiktok.py
@@ -142,27 +142,6 @@ class TikTokBaseIE(InfoExtractor):
             })
         return subtitles
 
-    def _get_SIGI_STATE(self, video_id, html):
-        try:
-            sigi_data = get_element_by_id('SIGI_STATE', html)
-            if not sigi_data:
-                raise ExtractorError('No element with \'SIGI_STATE\' id was found')
-        except (ValueError, ExtractorError) as err:
-            if isinstance(err, ExtractorError):
-                self.write_debug(err.orig_msg)
-            sigi_data = self._search_regex(
-                r'''(?s)
-                    <script\s[^>]*?\b
-                        id\s*=\s*(?P<q>"|'|\b)
-                        sigi-persisted-data(?P=q)[^>]*>[^=]*=\s*(?P<json>{.+?})\s*(?:;[^<]+)?
-                    </script
-                ''', html, 'sigi data', default='{}', group='json')
-        try:
-            state = self._parse_json(sigi_data, video_id)
-        except TypeError as err:
-            raise ExtractorError('No SIGI data was found') from err
-        return state if isinstance(state, dict) else {}
-
     def _parse_aweme_video_app(self, aweme_detail):
         aweme_id = aweme_detail['aweme_id']
         video_info = aweme_detail['video']
