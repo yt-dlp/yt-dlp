a9bfcfae27e2ce79c8d88bde304c0674d3fcadfd
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-05-11 20:22:18 +0300

    [goodgame] Update extractor

--- yt_dlp/extractor/goodgame.py
+++ yt_dlp/extractor/goodgame.py
@@ -1,56 +1,49 @@
 from .common import InfoExtractor
-from ..utils import clean_html, traverse_obj
+from ..utils import (
+    clean_html,
+    traverse_obj,
+    ExtractorError,
+)
 
 
 class GoodGameIE(InfoExtractor):
     IE_NAME = 'goodgame:stream'
     _VALID_URL = r'https?://goodgame\.ru/channel/(?P<id>[a-zA-Z0-9_]+)'
     _TESTS = [{
-        'url': 'https://goodgame.ru/channel/Kochevnik',
-        'info_dict': {
-            'id': 'Kochevnik',
-            'ext': 'mp4',
-            'title': 'Кочетятр',
-            'description': '',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'live_status': 'not_live',
-        },
-        'params': {
-            'skip_download': 'm3u8',
-        },
-        'skip': 'HTTP Error 404: Not Found',
-    }, {
         'url': 'https://goodgame.ru/channel/LampaRPG',
         'info_dict': {
-            'id': 'LampaRPG',
+            'id': '156878',
             'ext': 'mp4',
-            'title': r're:Рейтинговые герои.*$',
-            'description': str,
+            'title': r're:Рейтинговые герои \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
+            'description': 'md5:ca5faa69b5d9215afdc65caeca68e1f6',
             'thumbnail': r're:^https?://.*\.jpg$',
+            'is_live': True,
             'live_status': 'is_live',
+            'view_count': int,
         },
-        'params': {
-            'skip_download': 'm3u8',
-        },
+        'params': {'skip_download': 'm3u8'},
     }]
 
     def _real_extract(self, url):
-        channel_id = self._match_id(url)
+        channel_name = self._match_id(url)
 
-        response = self._download_json(f'https://api2.goodgame.ru/v2/streams/{channel_id}', channel_id)
+        response = self._download_json(f'https://api2.goodgame.ru/v2/streams/{channel_name}', channel_name)
+
+        if response.get('status') != 'Live':
+            raise ExtractorError(f'{channel_name} is offline', expected=True)
 
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(
             f'https://hls.goodgame.ru/manifest/{response.get("id")}_master.m3u8',
-            channel_id, 'mp4', m3u8_id='hls', live=True)
+            channel_name, 'mp4', m3u8_id='hls', live=True)
         self._sort_formats(formats)
 
         return {
-            'id': channel_id,
+            'id': traverse_obj(response, ('channel', 'gg_player_src')) or channel_name,
             'formats': formats,
             'subtitles': subtitles,
             'title': traverse_obj(response, ('channel', 'title')),
             'description': clean_html(traverse_obj(response, ('channel', 'description'))),
             'thumbnail': traverse_obj(response, ('channel', 'thumb')),
             'is_live': True,
-            'live_status': 'is_live' if response.get('status') == 'Live' else 'not_live',
+            'view_count': int(response.get('viewers', 0)),
         }
