d4df085a23700ae7ed31522a2857a8f4459da928
Filip Hedman <filip@allears.ai>
2022-05-11 14:50:33 +0200

    [drtv] Add test for dr.dk lyd episode

--- yt_dlp/extractor/drtv.py
+++ yt_dlp/extractor/drtv.py
@@ -121,6 +121,24 @@ class DRTVIE(InfoExtractor):
     }, {
         'url': 'https://www.dr.dk/drtv/program/jagten_220924',
         'only_matching': True,
+    }, {
+        'url': 'https://www.dr.dk/lyd/p4aarhus/regionale-nyheder-ar4/regionale-nyheder-2022-05-05-12-30-3',
+        'info_dict': {
+            'id': "urn:dr:mu:programcard:6265cb2571401424d0360113",
+            'title': "Regionale nyheder",
+            'ext': 'mp4',
+            'duration': 120.043,
+            'series': 'P4 Østjylland regionale nyheder',
+            'timestamp': 1651746600,
+            'season': 'Regionale nyheder',
+            'release_year': 0,
+            'season_id': 'urn:dr:mu:bundle:61c26889539f0201586b73c5',
+            'description': '',
+            'upload_date': '20220505',
+        },
+        'params': {
+            'skip_download': True,
+        },
     }]
 
     def _real_extract(self, url):
