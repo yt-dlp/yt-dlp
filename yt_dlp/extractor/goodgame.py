e3484fb6408b635b0efe289dcae7a903a71adf62
pukkandan <pukkandan.ytdlp@gmail.com>
2022-05-15 22:32:56 +0530

    cleanup

--- yt_dlp/extractor/goodgame.py
+++ yt_dlp/extractor/goodgame.py
@@ -11,27 +11,27 @@ class GoodGameIE(InfoExtractor):
     IE_NAME = 'goodgame:stream'
     _VALID_URL = r'https?://goodgame\.ru/channel/(?P<id>\w+)'
     _TESTS = [{
-        'url': 'https://goodgame.ru/channel/Pomi',
+        'url': 'https://goodgame.ru/channel/Pomi/#autoplay',
         'info_dict': {
-            'id': '1644',
+            'id': 'pomi',
             'ext': 'mp4',
-            'title': r're:Clem vs Special \(bo3\) Wardi Spring EU \- playoff \(финальный день\) \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
-            'channel': 'Clem vs Special (bo3) Wardi Spring EU - playoff (финальный день)',
-            'channel_id': 'Pomi',
-            'channel_url': 'https://goodgame.ru/channel/Pomi',
-            'description': 'md5:7753f17d161c06af196401b0d5dae61e',
+            'title': r're:Reynor vs Special \(1/2,bo3\) Wardi Spring EU \- playoff \(финальный день\) \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
+            'channel_id': '1644',
+            'channel': 'Pomi',
+            'channel_url': 'https://goodgame.ru/channel/Pomi/',
+            'description': 'md5:4a87b775ee7b2b57bdccebe285bbe171',
             'thumbnail': r're:^https?://.*\.jpg$',
             'live_status': 'is_live',
             'view_count': int,
         },
         'params': {'skip_download': 'm3u8'},
-        # 'skip': 'May not be online',
+        'skip': 'May not be online',
     }]
 
     def _real_extract(self, url):
         channel_name = self._match_id(url)
         response = self._download_json(f'https://api2.goodgame.ru/v2/streams/{channel_name}', channel_name)
-        player_id = traverse_obj(response, ('channel', 'gg_player_src'))
+        player_id = response['channel']['gg_player_src']
 
         formats, subtitles = [], {}
         if response.get('status') == 'Live':
@@ -43,15 +43,16 @@ class GoodGameIE(InfoExtractor):
 
         self._sort_formats(formats)
         return {
-            'id': str_or_none(response.get('id')),
+            'id': player_id,
             'formats': formats,
             'subtitles': subtitles,
             'title': traverse_obj(response, ('channel', 'title')),
-            'channel': traverse_obj(response, ('channel', 'title')),
-            'channel_id': channel_name,
-            'channel_url': f'https://goodgame.ru/channel/{channel_name}',
+            'channel': channel_name,
+            'channel_id': str_or_none(traverse_obj(response, ('channel', 'id'))),
+            'channel_url': response.get('url'),
             'description': clean_html(traverse_obj(response, ('channel', 'description'))),
             'thumbnail': traverse_obj(response, ('channel', 'thumb')),
-            'is_live': True,
+            'is_live': bool(formats),
             'view_count': int_or_none(response.get('viewers')),
+            'age_limit': 18 if traverse_obj(response, ('channel', 'adult')) else None,
         }
