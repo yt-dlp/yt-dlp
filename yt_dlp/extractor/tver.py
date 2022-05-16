46596c79b23c486981823890913a9af785cbf4df
pukkandan <pukkandan.ytdlp@gmail.com>
2022-05-15 19:28:56 -0700

    Apply suggestions from code review

--- yt_dlp/extractor/tver.py
+++ yt_dlp/extractor/tver.py
@@ -119,21 +119,15 @@ class TVerSeriesIE(TVerBaseIE):
     def _entries(self, series_id):
         season_json = self._download_json(
             f'https://service-api.tver.jp/api/v1/callSeriesSeasons/{series_id}',
-            series_id, headers={
-                'x-tver-platform-type': 'web'
-            })
-        for season in traverse_obj(season_json, ('result', 'contents', lambda _, s: s['type'] == 'season')):
-            season_id = traverse_obj(season, ('content', 'id'))
+            series_id, headers={'x-tver-platform-type': 'web'})
+        seasons = traverse_obj(season_json, ('result', 'contents', lambda _, s: s['type'] == 'season', 'content', 'id'))
+        for season_id in seasons:
             episode_json = self._download_json(
                 f'https://platform-api.tver.jp/service/api/v1/callSeasonEpisodes/{season_id}',
-                season_id, query={
+                season_id, headers={'x-tver-platform-type': 'web'}, query={
                     'platform_uid': self._PLATFORM_UID,
                     'platform_token': self._PLATFORM_TOKEN,
-                }, headers={
-                    'x-tver-platform-type': 'web'
                 })
-            episodes = traverse_obj(episode_json, ('result', 'contents'))
-            for episode in episodes:
-                if episode.get('type') == 'episode':
-                    video_id = traverse_obj(episode, ('content', 'id'))
-                    yield self.url_result(f'https://tver.jp/episodes/{video_id}', TVerIE, video_id)
+            episodes = traverse_obj(episode_json, ('result', 'contents', lambda _, e: e['type'] == 'episode', 'content', 'id')
+            for video_id in episodes:
+                yield self.url_result(f'https://tver.jp/episodes/{video_id}', TVerIE, video_id)
