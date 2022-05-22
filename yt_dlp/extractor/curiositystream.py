532d9a924551da1d1bfdcf47ec4e2c4ffb217bf0
monnef <mkucera22@gmail.com>
2022-05-22 09:55:58 +0200

    [couriositystream] Add getting auth from browser cookie.

--- yt_dlp/extractor/curiositystream.py
+++ yt_dlp/extractor/curiositystream.py
@@ -23,6 +23,11 @@ class CuriosityStreamBaseIE(InfoExtractor):
 
     def _call_api(self, path, video_id, query=None):
         headers = {}
+        if not self._auth_token:
+            cookie = self._get_cookies("https://curiositystream.com").get("auth_token")
+            if cookie:
+                self.to_screen("Using auth info from cookie.")
+                self._auth_token = cookie.value
         if self._auth_token:
             headers['X-Auth-Token'] = self._auth_token
         result = self._download_json(
