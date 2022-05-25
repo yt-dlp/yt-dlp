a0cb60d1e41c2206a79d281a8c4120b733ee4094
HobbyistDev <tesutonihon4@gmail.com>
2022-05-25 18:36:47 +0900

    Change display id as regular var (not in self)

--- yt_dlp/extractor/netverse.py
+++ yt_dlp/extractor/netverse.py
@@ -15,26 +15,26 @@ class NetverseBaseIE(InfoExtractor):
     }
 
     def get_required_json(self, url, data=None):
-        self.display_id, sites_type = self._match_valid_url(url).group('display_id', 'type')
+        display_id, sites_type = self._match_valid_url(url).group('display_id', 'type')
 
         json_data = self._download_json(
-            f'https://api.netverse.id/medias/api/v2/{self._ENDPOINTS[sites_type]}/{self.display_id}',
-            self.display_id, data=data)
+            f'https://api.netverse.id/medias/api/v2/{self._ENDPOINTS[sites_type]}/{display_id}',
+            display_id, data=data)
 
         if json_data.get('error'):
             raise ExtractorError(f'{self.IE_NAME} said: {json_data.get("message")}')
 
-        return json_data
+        return display_id, json_data
 
-    def _call_metadata_api_from_video_url(self, dailymotion_url, req_file_type='video', query={}):
+    def _call_metadata_api_from_video_url(self, dailymotion_url, display_id, req_file_type='video', query={}):
         access_id = urlsplit(dailymotion_url).path.split('/')[-1]
         required_query = {
             'embedder': 'https://www.netverse.id',
-            **query,
+            **query,        
         }
         metadata_json = self._download_json(
             f'https://www.dailymotion.com/player/metadata/{req_file_type}/{access_id}',
-            self.display_id, query=required_query)
+            display_id, query=required_query)
         return access_id, metadata_json
 
 
@@ -101,7 +101,7 @@ class NetverseIE(NetverseBaseIE):
         }]
 
     def _real_extract(self, url):
-        program_json = self.get_required_json(url=url)
+        display_id, program_json = self.get_required_json(url=url)
 
         videos = traverse_obj(program_json, ('response', 'videos'))
         video_url = videos.get('dailymotion_url')
@@ -109,7 +109,7 @@ class NetverseIE(NetverseBaseIE):
 
         # actually the video itself in dailymotion, but in private
         # Maybe need to refactor
-        access_id, real_video_json = self._call_metadata_api_from_video_url(video_url)
+        access_id, real_video_json = self._call_metadata_api_from_video_url(video_url, display_id)
         video_id = real_video_json.get('id')
 
         # For m3u8
@@ -120,7 +120,7 @@ class NetverseIE(NetverseBaseIE):
             video_url = format.get('url')
             if video_url is None:
                 continue
-            fmt, sub = self._extract_m3u8_formats_and_subtitles(video_url, video_id=self.display_id)
+            fmt, sub = self._extract_m3u8_formats_and_subtitles(video_url, video_id=display_id)
             video_format.extend(fmt)
             self._merge_subtitles(sub, target=subtitles)
 
@@ -155,7 +155,7 @@ class NetversePlaylistIE(NetverseBaseIE):
     }
 
     def _real_extract(self, url):
-        playlist_data = self.get_required_json(url)
+        display_id, playlist_data = self.get_required_json(url)
         webseries_info = traverse_obj(playlist_data, ('response', 'webseries_info'))
         videos = traverse_obj(playlist_data, ('response', 'related', 'data'))
 
@@ -165,10 +165,12 @@ class NetversePlaylistIE(NetverseBaseIE):
 
         entries = []
         for video in videos:
-            video_url = f'https://www.netverse.id/video/{video.get("slug")}'
-
-            entry = self.url_result(video_url, NetverseIE)
-            entries.append(entry)
+            vid_url = video.get('slug')
+            
+            if vid_url is not None:
+                video_url = f'https://www.netverse.id/video/{vid_url}'
+                entry = self.url_result(video_url, NetverseIE)
+                entries.append(entry)
 
         return self.playlist_result(
             entries, webseries_info.get('video_webseries_detail_id'), webseries_info.get('title')
