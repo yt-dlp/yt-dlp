76ce346031d41325b71774961561ea7754724bef
Lesmiscore <nao20010128@gmail.com>
2022-06-01 01:02:25 +0900

    add test for new url format

--- yt_dlp/extractor/vevo.py
+++ yt_dlp/extractor/vevo.py
@@ -147,6 +147,9 @@ class VevoIE(VevoBaseIE):
     }, {
         'url': 'https://embed.vevo.com/?isrc=USH5V1923499&partnerId=4d61b777-8023-4191-9ede-497ed6c24647&partnerAdCode=',
         'only_matching': True,
+    }, {
+        'url': 'ttps://tv.vevo.com/watch/artist/janet-jackson/US0450100550',
+        'only_matching': True,
     }]
     _VERSIONS = {
         0: 'youtube',  # only in AuthenticateVideo videoVersions
