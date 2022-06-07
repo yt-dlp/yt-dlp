9e2ed6ee52f198eae29b985ac907b044f7ec7bc9
Daniel Lindholm <daniel.lindholm@allears.ai>
2022-06-07 14:30:54 +0200

    [expressen] Update Expressen Web TV info extractor to work with the latest version of the website

--- yt_dlp/extractor/expressen.py
+++ yt_dlp/extractor/expressen.py
@@ -19,9 +19,10 @@ class ExpressenIE(InfoExtractor):
                     '''
     _TESTS = [{
         'url': 'https://www.expressen.se/tv/ledare/ledarsnack/ledarsnack-om-arbetslosheten-bland-kvinnor-i-speciellt-utsatta-omraden/',
-        'md5': '2fbbe3ca14392a6b1b36941858d33a45',
+        'md5': 'deb2ca62e7b1dcd19fa18ba37523f66e',
         'info_dict': {
-            'id': '8690962',
+            'id': 'ba90f5a9-78d1-4511-aa02-c177b9c99136',
+            'display_id': 'ledarsnack-om-arbetslosheten-bland-kvinnor-i-speciellt-utsatta-omraden',
             'ext': 'mp4',
             'title': 'Ledarsnack: Om arbetslösheten bland kvinnor i speciellt utsatta områden',
             'description': 'md5:f38c81ff69f3de4d269bbda012fcbbba',
@@ -64,7 +65,7 @@ class ExpressenIE(InfoExtractor):
                 display_id, transform_source=unescapeHTML)
 
         info = extract_data('video-tracking-info')
-        video_id = info['videoId']
+        video_id = info['contentId']
 
         data = extract_data('article-data')
         stream = data['stream']
