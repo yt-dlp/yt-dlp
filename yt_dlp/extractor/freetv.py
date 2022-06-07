f97fe2895ea25f42e5184f5a44884bda39a3a47a
pukkandan <pukkandan.ytdlp@gmail.com>
2022-06-07 18:54:11 +0530

    cleanup

--- yt_dlp/extractor/freetv.py
+++ yt_dlp/extractor/freetv.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 from .common import InfoExtractor
@@ -11,13 +12,11 @@ from ..utils import (
 
 
 class FreeTvBaseIE(InfoExtractor):
-    def _get_api_response(self, content_id, postdata, resource_type):
-        response = self._download_json(
+    def _get_api_response(self, content_id, resource_type, postdata):
+        return self._download_json(
             'https://www.freetv.com/wordpress/wp-admin/admin-ajax.php',
             content_id, data=urlencode_postdata(postdata),
-            note=f'Downloading {content_id} {resource_type} JSON')
-
-        return response['data']
+            note=f'Downloading {content_id} {resource_type} JSON')['data']
 
 
 class FreeTvMoviesIE(FreeTvBaseIE):
@@ -43,19 +42,12 @@ class FreeTvMoviesIE(FreeTvBaseIE):
     }]
 
     def _extract_video(self, content_id, action='olyott_video_play'):
-        api_response = self._get_api_response(
-            content_id, resource_type='video',
-            postdata={
-                'action': action,
-                'contentID': content_id,
-            })
+        api_response = self._get_api_response(content_id, 'video', {
+            'action': action,
+            'contentID': content_id,
+        })
 
-        video_id = api_response['displayMeta']['contentID']
-        video_url = traverse_obj(api_response, ('displayMeta', 'streamURLVideo'), expected_type=str)
-
-        if determine_ext(video_url) != 'm3u8':
-            self.raise_no_formats('Manifest was not found', video_id=video_id)
-
+        video_id, video_url = api_response['displayMeta']['contentID'], api_response['displayMeta']['streamURLVideo']
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(video_url, video_id, 'mp4')
         self._sort_formats(formats)
 
@@ -71,13 +63,12 @@ class FreeTvMoviesIE(FreeTvBaseIE):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
 
-        content_id = self._search_regex(
-            (r'class=["\'][^>]+postid-(?P<video_id>\d+)',
-             r'<link[^>]+freetv.com/\?p=(?P<video_id>\d+)'
-             r'<div[^>]+data-params=["\'][^>]+post_id=(?P<video_id>\d+)'),
-            webpage, 'video_id', group='video_id')
-
-        return self._extract_video(content_id)
+        return self._extract_video(
+            self._search_regex((
+                r'class=["\'][^>]+postid-(?P<video_id>\d+)',
+                r'<link[^>]+freetv.com/\?p=(?P<video_id>\d+)',
+                r'<div[^>]+data-params=["\'][^>]+post_id=(?P<video_id>\d+)',
+            ), webpage, 'video id', group='video_id'))
 
 
 class FreeTvIE(FreeTvBaseIE):
@@ -110,68 +101,45 @@ class FreeTvIE(FreeTvBaseIE):
     },
     ]
 
-    def _extract_series_season(self, season_id, series_title, action='olyott_get_dynamic_series_content'):
-        api_response = self._get_api_response(
-            season_id, resource_type='series',
-            postdata={
-                'action': action,
-                'contentID': season_id,
-                'type': 'list',
-                'perPage': '1000',
-            })
+    def _extract_series_season(self, season_id, series_title):
+        episodes = self._get_api_response(season_id, 'series', {
+            'contentID': season_id,
+            'action': 'olyott_get_dynamic_series_content',
+            'type': 'list',
+            'perPage': '1000',
+        })['1']
 
-        season_episodes = traverse_obj(api_response, '1')
+        for episode in episodes:
+            video_id = str(episode['contentID'])
+            formats, subtitles = self._extract_m3u8_formats_and_subtitles(episode['streamURL'], video_id, 'mp4')
+            self._sort_formats(formats)
 
-        entries = []
-        for episode in season_episodes:
-            video_id = str_or_none(episode.get('contentID'))
-            video_url = episode.get('streamURL')
-
-            if determine_ext(video_url) == 'm3u8':
-                formats, subtitles = self._extract_m3u8_formats_and_subtitles(
-                    video_url, video_id, 'mp4', fatal=False)
-                self._sort_formats(formats)
-
-                entries.append({
-                    'id': video_id,
-                    'title': episode.get('fullTitle'),
-                    'description': episode.get('description'),
-                    'formats': formats,
-                    'subtitles': subtitles,
-                    'thumbnail': episode.get('thumbnail'),
-                    'series': series_title,
-                    'series_id': traverse_obj(episode, ('contentMeta', 'displayMeta', 'seriesID')),
-                    'season_id': traverse_obj(episode, ('contentMeta', 'displayMeta', 'seasonID')),
-                    'season_number': int_or_none(
-                        traverse_obj(episode, ('contentMeta', 'displayMeta', 'seasonNum'))),
-                    'episode_number': int_or_none(
-                        traverse_obj(episode, ('contentMeta', 'displayMeta', 'episodeNum'))),
-                })
-            else:
-                self.report_warning('Manifest was not found', video_id)
-
-        return entries
-
-    def _extract_all_series_seasons(self, season_ids, series_title):
-        entries = []
-        for season_id in season_ids:
-            entries.extend(self._extract_series_season(season_id, series_title))
-
-        return entries
+            yield {
+                'id': video_id,
+                'title': episode.get('fullTitle'),
+                'description': episode.get('description'),
+                'formats': formats,
+                'subtitles': subtitles,
+                'thumbnail': episode.get('thumbnail'),
+                'series': series_title,
+                'series_id': traverse_obj(episode, ('contentMeta', 'displayMeta', 'seriesID')),
+                'season_id': traverse_obj(episode, ('contentMeta', 'displayMeta', 'seasonID')),
+                'season_number': traverse_obj(
+                    episode, ('contentMeta', 'displayMeta', 'seasonNum'), expected_type=int_or_none),
+                'episode_number': traverse_obj(
+                    episode, ('contentMeta', 'displayMeta', 'episodeNum'), expected_type=int_or_none),
+            }
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
 
-        title = self._html_search_regex(
-            r'<h1[^>]+class=["\']synopis[^>]>(?P<title>[^<]+)',
-            webpage, 'title', fatal=False, group='title')
+        title = self._html_search_regex(r'<h1[^>]+class=["\']synopis[^>]>([^<]+)', webpage, 'title', fatal=False),
         description = self._html_search_regex(
-            r'<div[^>]+class=["\']+synopis content[^>]><p>(?P<description>[^<]+)',
-            webpage, 'description', fatal=False, group='description')
-
-        season_ids = re.findall(r'<option[^>]+value=["\'](?P<content_id>\d+)["\']', webpage)
+            r'<div[^>]+class=["\']+synopis content[^>]><p>([^<]+)', webpage, 'description', fatal=False)
 
         return self.playlist_result(
-            self._extract_all_series_seasons(season_ids, title),
-            playlist_id=display_id, playlist_title=title, playlist_description=description)
+            itertools.chain(
+                self._extract_series_season(season_id, title)
+                for season_id in re.findall(r'<option[^>]+value=["\'](\d+)["\']', webpage)),
+            display_id, title, description)
