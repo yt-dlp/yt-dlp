a2549253463fa3e3304e1024623652a7883f3e3b
Rainwangpupil <Rainwang_20220102@163.com>
2022-06-07 11:41:04 +0800

    Since pytho.org now has some new versions, I added some code
    These Python versions are
    1.Python 3.8
    2.Python 3.9
    3.Python 3.10

--- setup.py
+++ setup.py
@@ -140,6 +140,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
