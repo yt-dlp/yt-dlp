37fca281b7d1d5aecfc53d773b86d14bd73659ce
pukkandan <pukkandan.ytdlp@gmail.com>
2022-06-11 04:41:37 +0530

    rewrite

--- yt_dlp/extractor/franceculture.py
+++ yt_dlp/extractor/franceculture.py
@@ -3,12 +3,13 @@ from ..utils import int_or_none, parse_duration, unified_strdate
 
 
 class FranceCultureIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?radiofrance\.fr/franceculture/podcasts/(?:[^/]+/)*(?P<id>[^/?#&]+)'
+    _VALID_URL = r'https?://(?:www\.)?radiofrance\.fr/franceculture/podcasts/(?:[^?#]+/)?(?P<display_id>[^?#]+)-(?P<id>\d+)($|[?#])'
     _TESTS = [
         {
             'url': 'https://www.radiofrance.fr/franceculture/podcasts/science-en-questions/la-physique-d-einstein-aiderait-elle-a-comprendre-le-cerveau-8440487',
             'info_dict': {
-                'id': 'la-physique-d-einstein-aiderait-elle-a-comprendre-le-cerveau-8440487',
+                'id': '8440487',
+                'display_id': 'la-physique-d-einstein-aiderait-elle-a-comprendre-le-cerveau',
                 'ext': 'mp3',
                 'title': 'La physique d’Einstein aiderait-elle à comprendre le cerveau ?',
                 'description': 'Existerait-il un pont conceptuel entre la physique de l’espace-temps et les neurosciences ?',
@@ -21,37 +22,27 @@ class FranceCultureIE(InfoExtractor):
     ]
 
     def _real_extract(self, url):
-        display_id = self._match_id(url)
+        video_id, display_id = self._match_valid_url(url).group('id', 'display_id')
         webpage = self._download_webpage(url, display_id)
 
-        info = {
-            'id': display_id,
+        # _search_json_ld doesn't correctly handle this. See https://github.com/yt-dlp/yt-dlp/pull/3874#discussion_r891903846
+        video_data = self._search_json('', webpage, 'audio data', display_id, contains_pattern=r'\s*"@type"\s*:\s*"AudioObject"\s*.+')
+
+        return {
+            'id': video_id,
+            'display_id': display_id,
+            'url': video_data['contentUrl'],
+            'ext': video_data.get('encodingFormat'),
+            'vcodec': 'none' if video_data.get('encodingFormat') == 'mp3' else None,
+            'duration': parse_duration(video_data.get('duration')),
             'title': self._html_search_regex(
                 r'(?s)<h1[^>]*itemprop="[^"]*name[^"]*"[^>]*>(.+?)</h1>', webpage, 'title',
                 default=self._og_search_title(webpage)),
-            'description': self._html_search_regex(r'(?s)<meta name="description" content="([^"]+)',
-                                                   webpage, 'description', default=None),
+            'description': self._html_search_regex(
+                r'(?s)<meta name="description" content="([^"]+)', webpage, 'description', default=None),
             'thumbnail': self._og_search_thumbnail(webpage),
-            'uploader': self._html_search_regex(r'(?s)<span class="author">(.*?)</span>', webpage,
-                                                'uploader', default=None),
-            'upload_date': unified_strdate(self._search_regex(r'"datePublished":"([^"]+)', webpage,
-                                           'timestamp', fatal=False))
-        }
-
-        video_data = self._parse_json(
-            self._search_regex(  # type: ignore
-                r'({"@type":"AudioObject","contentUrl":"[^"]+","duration":"[^"]+","encodingFormat":"mp3","potentialAction":{"@type":"Action","name":"ListenAction"}})',
-                webpage, 'video data'), display_id)
-        video_url = video_data['contentUrl']
-        ext = video_data['encodingFormat']
-
-        duration = parse_duration(video_data['duration'])
-
-        return {
-            'display_id': display_id,
-            'url': video_url,
-            'ext': ext,
-            'vcodec': 'none' if ext == 'mp3' else None,
-            'duration': int_or_none(duration),
-            **info,
+            'uploader': self._html_search_regex(
+                r'(?s)<span class="author">(.*?)</span>', webpage, 'uploader', default=None),
+            'upload_date': unified_strdate(self._search_regex(
+                r'"datePublished":"([^"]+)', webpage, 'timestamp', fatal=False))
         }
