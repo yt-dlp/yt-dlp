d5882ff0d0627e85c68678de02b27ee77771ba67
pukkandan <pukkandan.ytdlp@gmail.com>
2022-06-12 00:47:10 +0530

    cleanup

--- yt_dlp/extractor/mirrorcouk.py
+++ yt_dlp/extractor/mirrorcouk.py
@@ -1,4 +1,5 @@
 from .common import InfoExtractor
+from ..utils import unescapeHTML
 
 
 class MirrorCoUKIE(InfoExtractor):
@@ -9,86 +10,79 @@ class MirrorCoUKIE(InfoExtractor):
             'id': 'voyyS7SV',
             'ext': 'mp4',
             'title': 'Love Island: Gemma Owen enters the villa',
-            'description': "Love Island: Michael Owen's daughter Gemma Owen enters the villa.",
+            'description': 'Love Island: Michael Owen\'s daughter Gemma Owen enters the villa.',
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/voyyS7SV/poster.jpg?width=720',
             'display_id': '27163139',
             'timestamp': 1654547895,
             'duration': 57.0,
             'upload_date': '20220606',
         },
-        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/3am/celebrity-news/michael-jacksons-son-blankets-new-25344890',
         'info_dict': {
             'id': 'jyXpdvxp',
             'ext': 'mp4',
-            "title": "Michael Jackson’s son Bigi calls for action on climate change",
-            "description": "Michael Jackson's son, Bigi Jackson, calls on world leaders to take action against climate change, saying 'we have some work to do.'",
+            'title': 'Michael Jackson’s son Bigi calls for action on climate change',
+            'description': 'md5:d39ceaba2b7a615b4ca6557e7bc40222',
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/jyXpdvxp/poster.jpg?width=720',
             'display_id': '25344890',
             'timestamp': 1635749907,
             'duration': 56.0,
             'upload_date': '20211101',
         },
-        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/sport/football/news/antonio-conte-next-tottenham-manager-25346042',
         'info_dict': {
             'id': 'q6FkKa4p',
             'ext': 'mp4',
-            "title": "Nuno sacked by Tottenham after fifth Premier League defeat of the season",
-            "description": "Nuno Espirito Santo has been sacked as Tottenham boss after only four months in charge.",
+            'title': 'Nuno sacked by Tottenham after fifth Premier League defeat of the season',
+            'description': 'Nuno Espirito Santo has been sacked as Tottenham boss after only four months in charge.',
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/q6FkKa4p/poster.jpg?width=720',
             'display_id': '25346042',
             'timestamp': 1635763157,
             'duration': 40.0,
             'upload_date': '20211101',
         },
-        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/3am/celebrity-news/johnny-depp-splashes-50k-curry-27160737',
         'info_dict': {
             'id': 'IT0oa1nH',
             'ext': 'mp4',
-            "title": "Johnny Depp Leaves The Grand Hotel in Birmingham",
-            "description": "Johnny Depp Leaves The Grand Hotel in Birmingham.",
-            'thumbnail': "https://cdn.jwplayer.com/v2/media/IT0oa1nH/poster.jpg?width=720",
+            'title': 'Johnny Depp Leaves The Grand Hotel in Birmingham',
+            'description': 'Johnny Depp Leaves The Grand Hotel in Birmingham.',
+            'thumbnail': 'https://cdn.jwplayer.com/v2/media/IT0oa1nH/poster.jpg?width=720',
             'display_id': '27160737',
             'timestamp': 1654524120,
             'duration': 65.0,
             'upload_date': '20220606',
         },
-        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/tv/tv-news/love-islands-liam-could-first-27162602',
         'info_dict': {
             'id': 'EaPr5Z2j',
             'ext': 'mp4',
-            "title": "Love Island: Davide reveals plot twist after receiving text",
-            "description": "Love Island: Davide reveals plot twist after receiving text",
-            'thumbnail': "https://cdn.jwplayer.com/v2/media/EaPr5Z2j/poster.jpg?width=720",
+            'title': 'Love Island: Davide reveals plot twist after receiving text',
+            'description': 'Love Island: Davide reveals plot twist after receiving text',
+            'thumbnail': 'https://cdn.jwplayer.com/v2/media/EaPr5Z2j/poster.jpg?width=720',
             'display_id': '27162602',
             'timestamp': 1654552597,
             'duration': 23.0,
             'upload_date': '20220606',
         },
-        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/news/uk-news/william-kate-sent-message-george-27160572',
         'info_dict': {
             'id': 'ygtceXIu',
             'ext': 'mp4',
-            "title": "Prince William and Kate arrive in Wales with George and Charlotte",
-            "description": "Prince William and Kate Middleton arrive in Wales with children Prince George and Princess Charlotte.",
-            'thumbnail': "https://cdn.jwplayer.com/v2/media/ygtceXIu/poster.jpg?width=720",
+            'title': 'Prince William and Kate arrive in Wales with George and Charlotte',
+            'description': 'Prince William and Kate Middleton arrive in Wales with children Prince George and Princess Charlotte.',
+            'thumbnail': 'https://cdn.jwplayer.com/v2/media/ygtceXIu/poster.jpg?width=720',
             'display_id': '27160572',
             'timestamp': 1654349678,
             'duration': 106.0,
             'upload_date': '20220604',
         },
-        'add_ie': ['JWPlatform'],
-    },
-    ]
+    }]
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
