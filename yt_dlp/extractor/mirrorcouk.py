4fc6d0c5ed4596e6ef35635b1c6f6f091b9a7e78
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-06-11 12:04:42 -0400

    [MirrorCoUK] extractor fixes

--- yt_dlp/extractor/mirrorcouk.py
+++ yt_dlp/extractor/mirrorcouk.py
@@ -11,10 +11,12 @@ class MirrorCoUKIE(InfoExtractor):
             'title': 'Love Island: Gemma Owen enters the villa',
             'description': "Love Island: Michael Owen's daughter Gemma Owen enters the villa.",
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/voyyS7SV/poster.jpg?width=720',
+            'display_id': '27163139',
             'timestamp': 1654547895,
             'duration': 57.0,
             'upload_date': '20220606',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/3am/celebrity-news/michael-jacksons-son-blankets-new-25344890',
         'info_dict': {
@@ -23,10 +25,12 @@ class MirrorCoUKIE(InfoExtractor):
             "title": "Michael Jackson’s son Bigi calls for action on climate change",
             "description": "Michael Jackson's son, Bigi Jackson, calls on world leaders to take action against climate change, saying 'we have some work to do.'",
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/jyXpdvxp/poster.jpg?width=720',
+            'display_id': '25344890',
             'timestamp': 1635749907,
             'duration': 56.0,
             'upload_date': '20211101',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/sport/football/news/antonio-conte-next-tottenham-manager-25346042',
         'info_dict': {
@@ -35,10 +39,12 @@ class MirrorCoUKIE(InfoExtractor):
             "title": "Nuno sacked by Tottenham after fifth Premier League defeat of the season",
             "description": "Nuno Espirito Santo has been sacked as Tottenham boss after only four months in charge.",
             'thumbnail': 'https://cdn.jwplayer.com/v2/media/q6FkKa4p/poster.jpg?width=720',
+            'display_id': '25346042',
             'timestamp': 1635763157,
             'duration': 40.0,
             'upload_date': '20211101',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/3am/celebrity-news/johnny-depp-splashes-50k-curry-27160737',
         'info_dict': {
@@ -47,10 +53,12 @@ class MirrorCoUKIE(InfoExtractor):
             "title": "Johnny Depp Leaves The Grand Hotel in Birmingham",
             "description": "Johnny Depp Leaves The Grand Hotel in Birmingham.",
             'thumbnail': "https://cdn.jwplayer.com/v2/media/IT0oa1nH/poster.jpg?width=720",
+            'display_id': '27160737',
             'timestamp': 1654524120,
             'duration': 65.0,
             'upload_date': '20220606',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/tv/tv-news/love-islands-liam-could-first-27162602',
         'info_dict': {
@@ -59,10 +67,12 @@ class MirrorCoUKIE(InfoExtractor):
             "title": "Love Island: Davide reveals plot twist after receiving text",
             "description": "Love Island: Davide reveals plot twist after receiving text",
             'thumbnail': "https://cdn.jwplayer.com/v2/media/EaPr5Z2j/poster.jpg?width=720",
+            'display_id': '27162602',
             'timestamp': 1654552597,
             'duration': 23.0,
             'upload_date': '20220606',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     }, {
         'url': 'https://www.mirror.co.uk/news/uk-news/william-kate-sent-message-george-27160572',
         'info_dict': {
@@ -71,16 +81,24 @@ class MirrorCoUKIE(InfoExtractor):
             "title": "Prince William and Kate arrive in Wales with George and Charlotte",
             "description": "Prince William and Kate Middleton arrive in Wales with children Prince George and Princess Charlotte.",
             'thumbnail': "https://cdn.jwplayer.com/v2/media/ygtceXIu/poster.jpg?width=720",
+            'display_id': '27160572',
             'timestamp': 1654349678,
             'duration': 106.0,
             'upload_date': '20220604',
-        }
+        },
+        'add_ie': ['JWPlatform'],
     },
     ]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
         jwplayer_id = self._search_regex(
             r'<img[^>]+?\s*?\s*c?\s*lass?\s*=?\s*["\']video-thumbnail["\']?\s*src=?\s*["\']https:\/\/cdn\.jwplayer\.com\/v2\/media\/(\w+)\/', webpage, 'data id')
-        return self._parse_jwplayer_data(self._download_json(f"https://cdn.jwplayer.com/v2/media/{jwplayer_id}?page_domain=www.mirror.co.uk", video_id))
+
+        return {
+            '_type': 'url_transparent',
+            'display_id': display_id,
+            'url': f"https://cdn.jwplayer.com/v2/media/{jwplayer_id}?page_domain=www.mirror.co.uk",
+            'ie_key': 'JWPlatform'
+        }
