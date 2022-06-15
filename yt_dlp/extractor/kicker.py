a5a96467c10edaf3dd11c6844320ef662cf47352
HobbyistDev <tesutonihon4@gmail.com>
2022-06-15 15:22:21 +0900

    [kicker] Fix test

--- yt_dlp/extractor/kicker.py
+++ yt_dlp/extractor/kicker.py
@@ -1,35 +1,41 @@
 from .common import InfoExtractor
 from .dailymotion import DailymotionIE
-from ..utils import (
-    extract_attributes,
-    get_element_html_by_class,
-    get_element_by_id,
-    get_elements_html_by_attribute,
-    get_elements_by_attribute,
-    get_element_html_by_attribute
-)
 
 
 class KickerIE(InfoExtractor):
     _VALID_URL = r'https://www.kicker.de/(?P<video_slug>[\w-]+)/video'
-    _TESTS = {
+    _TEST = {
         'url': 'https://www.kicker.de/pogba-dembel-co-die-top-11-der-abloesefreien-spieler-905049/video',
-        # 'info_dict': {
-            # 'id': '',
-            # 'title': '',
-        # }
+        'info_dict': {
+            'id': 'km04mrK0DrRAVxy2GcA',
+            'title': 'Pogba, Dembelé & Co.: Die Top 11 der ablösefreien Spieler | kicker.tv Hintergrund | Video - kicker',
+            'ext': 'mp4',
+            'duration': 350,
+            'description': 'md5:a5a3dd77dbb6550dbfb997be100b9998',
+            'uploader_id': 'x2dfupo',
+            'timestamp': 1654677626,
+            'like_count': int,
+            'uploader': 'kicker.de',
+            'view_count': int,
+            'ie': 'Dailymotion',
+            'age_limit': 0,
+            'thumbnail': r're:https://s\d+.dmcdn.net/v/T-x741YeYAx8aSZ0Z/x1080',
+            'tags': ['published', 'category.InternationalSoccer'],
+            'upload_date': '20220608'
+        }
     }
-    
+
     def _real_extract(self, url):
         video_slug = self._match_valid_url(url).group('video_slug')
-        
+
         webpage = self._download_webpage(url, video_slug)
         dailymotion_video_id = self._search_regex(
-            r'data-dmprivateid=\"(?P<video_id>\w+)\"', webpage, 
-            'video_id', group = 'video_id')
-        
+            r'data-dmprivateid=\"(?P<video_id>\w+)\"', webpage,
+            'video_id', group='video_id')
+
         return {
             '_type': 'url_transparent',
             'ie': DailymotionIE.ie_key(),
-            'url': f'https://www.dailymotion.com/video/{dailymotion_video_id}'
+            'url': f'https://www.dailymotion.com/video/{dailymotion_video_id}',
+            'title': self._html_extract_title(webpage),
         }
