dcef0de959c40a24a8192a736b479558f010773e
Pritam Das <49360491+pritam20ps05@users.noreply.github.com>
2022-06-15 17:34:35 +0530

    under-indent fix

--- yt_dlp/extractor/instagram.py
+++ yt_dlp/extractor/instagram.py
@@ -376,8 +376,8 @@ class InstagramIE(InstagramBaseIE):
             video_id, 
             headers={
                 'accept': ('text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,'
-                'image/avif,image/webp,image/apng,*/*;q=0.8,'
-                'application/signed-exchange;v=b3;q=0.9'),
+                            'image/avif,image/webp,image/apng,*/*;q=0.8,'
+                            'application/signed-exchange;v=b3;q=0.9'),
                 'authority': 'www.instagram.com',
                 'referer': 'https://www.instagram.com',
                 'user-agent': ('Mozilla/5.0 (X11; Linux x86_64) '
