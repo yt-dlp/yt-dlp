99d10bf60796a90d2ca421ec63f1208b15ae5f48
pukkandan <pukkandan.ytdlp@gmail.com>
2022-06-15 17:59:35 +0530

    [cleanup, extractor] Rename `extractors.py` to `_extractors.py`
    This should be considered part of the next commit,
    but is separated so that `git` can detect the renaming better

--- yt_dlp/extractor/__init__.py
+++ yt_dlp/extractor/__init__.py
@@ -11,7 +11,7 @@ if not os.environ.get('YTDLP_NO_LAZY_EXTRACTORS'):
         _LAZY_LOADER = True
 
 if not _LAZY_LOADER:
-    from .extractors import *  # noqa: F403
+    from ._extractors import *  # noqa: F403
     _ALL_CLASSES = [  # noqa: F811
         klass
         for name, klass in globals().items()
