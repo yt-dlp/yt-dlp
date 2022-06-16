8e48bb5e89afc62c4a3e96031f7242dd7a5a605d
HobbyistDev <tesutonihon4@gmail.com>
2022-06-16 11:37:50 +0900

    [dailywire] Added DailyWirePodcastIE

--- yt_dlp/extractor/dailywire.py
+++ yt_dlp/extractor/dailywire.py
@@ -60,3 +60,38 @@ class DailyWireIE(InfoExtractor):
             'is_live': episode_info.get('isLive'),
             'creator': f'{episode_info.get("first_name")} {episode_info.get("last_name")}'
         }
+
+
+class DailyWirePodcastIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)dailywire(?:\.com)/(?:podcasts)/(?P<podcaster>[\w-]+/(?P<slug>[\w-]+))'
+    _TESTS = [{
+        'url': 'https://www.dailywire.com/podcasts/morning-wire/get-ready-for-recession-6-15-22',
+        'info_dict': {
+            'id': 'cl4f01d0w8pbe0a98ydd0cfn1',
+            'title': 'Get Ready for Recession | 6.15.22',
+            'ext': 'm4a',
+            'description': 'The S\u0026P 500 officially slides into a bear market and recession is ahead, the January 6 committee says Trump knew he lost, and the PGA suspends 17 players who’ve joined a new Saudi league. Get the facts first on Morning Wire. ',
+            'thumbnail': 'https://daily-wire-production.imgix.net/podcasts/ckx4otgd71jm508699tzb6hf4-1639506575562.jpg',
+        }
+    }]
+
+    def _real_extract(self, url):
+        slug = self._match_valid_url(url).group('slug')
+        webpage = self._download_webpage(url, slug)
+
+        json_data = self._search_nextjs_data(webpage, slug)
+        episode_info = json_data['props']['pageProps']['episodeData']['episode']
+
+        audio_id = episode_info.get('audioMuxPlaybackId') or episode_info.get('VUsAipTrBVSgzw73SpC2DAJD401TYYwEp')
+        formats = {
+            'url': f'https://stream.media.dailywire.com/{audio_id}/audio.m4a',
+        }
+        self._sort_formats(formats)
+        return {
+            'id': episode_info['id'],
+            'title': episode_info.get('title'),
+            'duration': float_or_none(episode_info.get('duration')),
+            'thumbnail': episode_info.get('thumbnail'),
+            'description': episode_info.get('description'),
+
+        }
