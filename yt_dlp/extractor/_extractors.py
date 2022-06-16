0e45fd3f6fd1c2ce89243904dc21a6def97a1b3d
thatDudo <you@example.com>
2022-06-16 15:56:11 +0200

    Finalizing merge

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -31,7 +31,10 @@ from .adobetv import (
     AdobeTVChannelIE,
     AdobeTVVideoIE,
 )
-from .adultswim import AdultSwimIE
+from .adultswim import (
+    AdultSwimIE,
+    AdultSwimStreamIE,
+)
 from .aenetworks import (
     AENetworksIE,
     AENetworksCollectionIE,
