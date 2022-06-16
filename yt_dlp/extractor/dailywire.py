9d17a3dd8b69072b5eeb3db7becc2575bbefddbd
HobbyistDev <tesutonihon4@gmail.com>
2022-06-16 10:54:33 +0900

    [dailywire] Added duration to metadata, improve format grabber, fix title in test

--- yt_dlp/extractor/dailywire.py
+++ yt_dlp/extractor/dailywire.py
@@ -1,16 +1,19 @@
 from .common import InfoExtractor
+from ..utils import float_or_none
 
 
 class DailyWireIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)dailywire(?:\.com)/(?:episode)/(?P<episode_name>[\w-]+)'
     _TESTS = [{
+        # need no-check-certificate
         'url': 'https://www.dailywire.com/episode/1-fauci',
         'info_dict': {
             'id': 'ckzsl50xwqpy508502drqpb12',
             'ext': 'mp4',
-            'title': '1-fauci',
+            'title': '1. Fauci',
         }
     }, {
+        # need no-check-certificate
         'url': 'https://www.dailywire.com/episode/ep-124-bill-maher',
         'info_dict': {
             'id': 'cl355p7u74t900894oxp1bnae',
@@ -27,16 +30,17 @@ class DailyWireIE(InfoExtractor):
         episode_info = json_data['props']['pageProps']['episodeData']['episode']
 
         formats, subtitle = [], {}
+
         for segment in episode_info['segments']:
             subs = {}
-            if segment.get('audio') == 'Access Denied' and segment.get('video') == 'Access Denied':
+            if segment.get('audio') in ('Access Denied', None) and segment.get('video') in ('Access Denied', None):
                 continue
-            if segment.get('video') != 'Access Denied':
+            if segment.get('video') not in ('Access Denied', None):
                 format_, subs = self._extract_m3u8_formats_and_subtitles(
                     segment.get('audio') or segment.get('video'), slug
                 )
                 formats.extend(format_)
-            if segment.get('audio') != 'Access Denied':
+            if segment.get('audio') not in ('Access Denied', None):
                 format_ = {
                     'url': segment.get('audio'),
                 }
@@ -52,6 +56,7 @@ class DailyWireIE(InfoExtractor):
             'subtitles': subtitle,
             'description': episode_info.get('description'),
             'thumbnail': episode_info.get('image'),
+            'duration': float_or_none(episode_info.get('duration')),
             'is_live': episode_info.get('isLive'),
             'creator': f'{episode_info.get("first_name")} {episode_info.get("last_name")}'
         }
