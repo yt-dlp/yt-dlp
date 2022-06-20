737e1169aedef945bb9a76bb31fb211d2dfe36f0
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-06-20 19:59:37 -0400

    more fixes

--- yt_dlp/extractor/scrolller.py
+++ yt_dlp/extractor/scrolller.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import (determine_ext, KNOWN_EXTENSIONS, int_or_none)
+from ..utils import (determine_ext, int_or_none)
 import json
 
 
@@ -9,46 +9,46 @@ class ScrolllerIE(InfoExtractor):
         'url': 'https://scrolller.com/a-helping-hand-1k9pxikxkw',
         'info_dict': {
             'id': 'a-helping-hand-1k9pxikxkw',
-            'ext': "mp4",
+            'ext': 'mp4',
             'thumbnail': 'https://zepto.scrolller.com/a-helping-hand-3ty9q8x094-540x960.jpg',
             'title': 'A helping hand',
-            "age_limit": 0,
+            'age_limit': 0,
         }
     }, {
         'url': 'https://scrolller.com/tigers-chasing-a-drone-c5d1f2so6j',
         'info_dict': {
-            "id": "tigers-chasing-a-drone-c5d1f2so6j",
-            'ext': "mp4",
+            'id': 'tigers-chasing-a-drone-c5d1f2so6j',
+            'ext': 'mp4',
             'thumbnail': 'https://zepto.scrolller.com/tigers-chasing-a-drone-az9pkpguwe-540x303.jpg',
-            "title": "Tigers chasing a drone",
-            "age_limit": 0,
+            'title': 'Tigers chasing a drone',
+            'age_limit': 0,
         }
     }, {
         'url': 'https://scrolller.com/baby-rhino-smells-something-9chhugsv9p',
         'info_dict': {
-            "id": "baby-rhino-smells-something-9chhugsv9p",
-            'ext': "mp4",
-            "thumbnail": 'https://atto.scrolller.com/hmm-whats-that-smell-bh54mf2c52-300x224.jpg',
-            "title": "Baby rhino smells something",
-            "age_limit": 0,
+            'id': 'baby-rhino-smells-something-9chhugsv9p',
+            'ext': 'mp4',
+            'thumbnail': 'https://atto.scrolller.com/hmm-whats-that-smell-bh54mf2c52-300x224.jpg',
+            'title': 'Baby rhino smells something',
+            'age_limit': 0,
         }
     }, {
         'url': 'https://scrolller.com/its-all-fun-and-games-cco8jjmoh7',
         'info_dict': {
-            "id": "its-all-fun-and-games-cco8jjmoh7",
-            'ext': "mp4",
+            'id': 'its-all-fun-and-games-cco8jjmoh7',
+            'ext': 'mp4',
             'thumbnail': 'https://atto.scrolller.com/its-all-fun-and-games-3amk9vg7m3-540x649.jpg',
-            "title": "It\'s all fun and games...",
-            "age_limit": 0,
+            'title': 'It\'s all fun and games...',
+            'age_limit': 0,
         }
     }, {
         'url': 'https://scrolller.com/may-the-force-be-with-you-octokuro-yeytg1fs7a',
         'info_dict': {
-            "id": "may-the-force-be-with-you-octokuro-yeytg1fs7a",
-            'ext': "mp4",
+            'id': 'may-the-force-be-with-you-octokuro-yeytg1fs7a',
+            'ext': 'mp4',
             'thumbnail': 'https://thumbs2.redgifs.com/DarkStarchyNautilus-poster.jpg',
-            "title": "May the force be with you (Octokuro)",
-            "age_limit": 18,
+            'title': 'May the force be with you (Octokuro)',
+            'age_limit': 18,
         }
     }
     ]
@@ -59,7 +59,7 @@ class ScrolllerIE(InfoExtractor):
         query = {
             'query':
             '''{
-                getSubredditPost(url:"/%s"){
+                getSubredditPost(url:'/%s'){
                     id
                     title
                     isNsfw
@@ -73,23 +73,24 @@ class ScrolllerIE(InfoExtractor):
         }
 
         video_data = self._download_json(
-            "https://api.scrolller.com/api/v2/graphql", video_id, data=json.dumps(query).encode(), headers={'Content-Type': 'application/json'})['data']['getSubredditPost']
+            'https://api.scrolller.com/api/v2/graphql', video_id, data=json.dumps(query).encode(),
+            headers={'Content-Type': 'application/json'})['data']['getSubredditPost']
 
         thumbnails = []
         formats = []
 
-        for source in video_data.get("mediaSources") or []:
-            if determine_ext(source.get("url")) not in KNOWN_EXTENSIONS:
+        for source in video_data['mediaSources']:
+            if determine_ext(source.get('url')) not in ('.jpg', '.png'):
                 thumbnails.append({
-                    "url": source.get("url"),
-                    "width": int_or_none(source.get("width")),
-                    "height": int_or_none(source.get("height")),
+                    'url': source.get('url'),
+                    'width': int_or_none(source.get('width')),
+                    'height': int_or_none(source.get('height')),
                 })
             else:
                 formats.append({
-                    "url": source.get("url"),
-                    "width": int_or_none(source.get("width")),
-                    "height": int_or_none(source.get("height")),
+                    'url': source.get('url'),
+                    'width': int_or_none(source.get('width')),
+                    'height': int_or_none(source.get('height')),
                 })
 
         if not formats:
@@ -98,9 +99,9 @@ class ScrolllerIE(InfoExtractor):
         self._sort_formats(formats)
 
         return {
-            "id": video_id,
-            "title": video_data.get("title"),
-            "thumbnails": thumbnails,
-            "formats": formats,
-            "age_limit": 18 if video_data.get("isNsfw") else 0
+            'id': video_id,
+            'title': video_data.get('title'),
+            'thumbnails': thumbnails,
+            'formats': formats,
+            'age_limit': 18 if video_data.get('isNsfw') else 0
         }
