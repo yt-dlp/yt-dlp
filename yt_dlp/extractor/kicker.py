bf8603b7f7ddff679aafb93bd91bfb2a6c7a4d6d
HobbyistDev <tesutonihon4@gmail.com>
2022-06-20 12:42:22 +0900

    Change to match_id, change to url_result

--- yt_dlp/extractor/kicker.py
+++ yt_dlp/extractor/kicker.py
@@ -8,7 +8,7 @@ class KickerIE(InfoExtractor):
         'url': 'https://www.kicker.de/pogba-dembel-co-die-top-11-der-abloesefreien-spieler-905049/video',
         'info_dict': {
             'id': 'km04mrK0DrRAVxy2GcA',
-            'title': 'md5:2cde755b10d80c3e5084d11a4387f020',
+            'title': 'md5:b91d145bac5745ac58d5479d8347a875',
             'ext': 'mp4',
             'duration': 350,
             'description': 'md5:a5a3dd77dbb6550dbfb997be100b9998',
@@ -45,16 +45,19 @@ class KickerIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        video_slug = self._match_valid_id(url)
+        video_slug = self._match_id(url)
 
         webpage = self._download_webpage(url, video_slug)
         dailymotion_video_id = self._search_regex(
             r'data-dmprivateid=\"(?P<video_id>\w+)\"', webpage,
             'video_id', group='video_id')
 
-        return {
-            '_type': 'url_transparent',
-            'ie': DailymotionIE.ie_key(),
-            'url': f'https://www.dailymotion.com/video/{dailymotion_video_id}',
-            'title': self._html_extract_title(webpage),
-        }
+        # return {
+            # '_type': 'url_transparent',
+            # 'ie': DailymotionIE.ie_key(),
+            # 'url': f'https://www.dailymotion.com/video/{dailymotion_video_id}',
+            # 'title': self._html_extract_title(webpage),
+        # }
+        return self.url_result(
+            f'https://www.dailymotion.com/video/{dailymotion_video_id}',
+            ie='Dailymotion', video_title=self._html_extract_title(webpage))
