0c55abdef963cf9d68eb1ca2cf4ef59833468b50
HobbyistDev <tesutonihon4@gmail.com>
2022-06-22 22:58:06 +0900

    lint

--- yt_dlp/extractor/steamcommunity.py
+++ yt_dlp/extractor/steamcommunity.py
@@ -7,25 +7,25 @@ class SteamCommunityBroadcastIE(InfoExtractor):
         'url': 'https://steamcommunity.com/broadcast/watch/76561199037072858',
         'only_matching': True,
     }]
-    
+
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
         json_data = self._download_json(
-            f'https://steamcommunity.com/broadcast/getbroadcastmpd/',
+            'https://steamcommunity.com/broadcast/getbroadcastmpd/',
             video_id, query={'steamid': f'{video_id}'}
         )
         formats = []
         mpd_formats, mpd_subs = self._extract_mpd_formats_and_subtitles(json_data['url'], video_id)
         format_, subs = self._extract_m3u8_formats_and_subtitles(json_data['hls_url'], video_id)
-        
+
         formats.extend(format_)
         formats.extend(mpd_formats)
-        
+
         uploader_json = self._download_json(
-            f'https://steamcommunity.com/actions/ajaxresolveusers',
+            'https://steamcommunity.com/actions/ajaxresolveusers',
             video_id, query={'steamids': f'{video_id}'})[0]  # assume the data only one
-        
+
         # TODO: get chat from 'view_url_template' in https://steamcommunity.com/broadcast/getchatinfo?steamid={video_id}
         # the chat is using '0' as first chat id and then changed based on '47639818'
         # the chat need requested regulary, i think https://github.com/yt-dlp/yt-dlp/pull/3048 can help
@@ -38,6 +38,5 @@ class SteamCommunityBroadcastIE(InfoExtractor):
             'view_count': json_data.get('num_view'),
             'uploader': uploader_json.get('persona_name'),
             'uploader_id': uploader_json.get('accountid'),
-            
+
         }
-        
