7166544d6e4c9a106d677760bace16950b3be14e
HobbyistDev <tesutonihon4@gmail.com>
2022-06-23 20:26:48 +0900

    add metadata:`categories`, set `clientId` in `query`

--- yt_dlp/extractor/premiershiprugby.py
+++ yt_dlp/extractor/premiershiprugby.py
@@ -13,14 +13,15 @@ class PremiershipRugbyIE(InfoExtractor):
             'thumbnail': 'https://open.http.mp.streamamg.com/p/3000914/sp/300091400/thumbnail/entry_id/0_mbkb7ldt//width/960/height/540/type/1/quality/75',
             'duration': 6093.0,
             'tags': ['video'],
+            'categories': ['Full Match', 'Harlequins', 'Newcastle Falcons', 'gallaher premiership'],
         }
     }]
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
         json_data = self._download_json(
-            f'https://article-cms-api.incrowdsports.com/v2/articles/slug/{display_id}?clientId=PRL',
-            display_id)['data']['article']
+            f'https://article-cms-api.incrowdsports.com/v2/articles/slug/{display_id}',
+            display_id, query={'clientId': 'PRL'})['data']['article']
 
         formats, subs = self._extract_m3u8_formats_and_subtitles(
             json_data['heroMedia']['content']['videoLink'], display_id)
@@ -33,4 +34,5 @@ class PremiershipRugbyIE(InfoExtractor):
             'thumbnail': traverse_obj(json_data, ('heroMedia', 'content', 'videoThumbnail')),
             'duration': int_or_none(traverse_obj(json_data, ('heroMedia', 'content', 'metadata', 'msDuration')), scale=1000),
             'tags': json_data.get('tags'),
+            'categories': traverse_obj(json_data, ('categories', ..., 'text')),
         }
