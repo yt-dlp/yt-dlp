21ba67cc3329be87106d30777766c724407aff67
coletdjnz <coletdjnz@protonmail.com>
2022-07-06 18:28:37 +1200

    Fix windows registry for ftp proxies

--- yt_dlp/compat/urllib/request.py
+++ yt_dlp/compat/urllib/request.py
@@ -12,7 +12,8 @@ if compat_os_name == 'nt':
     # On older python versions, proxies are extracted from Windows registry erroneously. [1]
     # If the https proxy in the registry does not have a scheme, urllib will incorrectly add https:// to it. [2]
     # It is unlikely that the user has actually set it to be https, so we should be fine to safely downgrade
-    # it to http on these older python versions to avoid issues.
+    # it to http on these older python versions to avoid issues
+    # This also applies for ftp proxy type, as ftp:// proxy scheme is not supported.
     # 1: https://github.com/python/cpython/issues/86793
     # 2: https://github.com/python/cpython/blob/51f1ae5ceb0673316c4e4b0175384e892e33cc6e/Lib/urllib/request.py#L2683-L2698
     from urllib.request import getproxies_environment, getproxies_registry
@@ -25,8 +26,9 @@ if compat_os_name == 'nt':
         ):
             return proxies
 
-        if 'https' in proxies and proxies['https'].startswith(f'https://'):
-            proxies['https'] = 'http' + proxies['https'][5:]
+        for scheme in ('https', 'ftp'):
+            if scheme in proxies and proxies[scheme].startswith(f'{scheme}://'):
+                proxies[scheme] = 'http' + proxies[scheme][5:]
 
         return proxies
 
