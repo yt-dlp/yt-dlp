098428a903b699b307867a4976c16141d2ab2e4b
pukkandan <pukkandan.ytdlp@gmail.com>
2022-07-10 15:37:09 +0530

    Apply suggestions from code review

--- yt_dlp/extractor/syvdk.py
+++ yt_dlp/extractor/syvdk.py
@@ -1,9 +1,9 @@
 from .common import InfoExtractor
-from ..utils import try_get
+from ..utils import traverse_obj
 
 
 class SYVDKIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?24syv\.dk/episode/(?P<id>[a-zA-Z0-9-]+)'
+    _VALID_URL = r'https?://(?:www\.)?24syv\.dk/episode/(?P<id>[\w-]+)'
 
     _TESTS = [{
         'url': 'https://24syv.dk/episode/isabella-arendt-stiller-op-for-de-konservative-2',
@@ -20,14 +20,14 @@ class SYVDKIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-        info_data = self._search_nextjs_data(webpage, video_id)["props"]["pageProps"]["episodeDetails"][0]
+        info_data = self._search_nextjs_data(webpage, video_id)['props']['pageProps']['episodeDetails'][0]
 
         return {
             'id': str(info_data['id']),
-            'display_id': video_id,
-            'title': try_get(info_data, lambda x: x['title']['rendered']),
-            'description': try_get(info_data, lambda x: x['details']['post_title']),
             'vcodec': 'none',
             'ext': 'mp3',
-            'url': info_data['details']['enclosure']
+            'url': info_data['details']['enclosure'],
+            'display_id': video_id,
+            'title': traverse_obj(info_data, ('title', 'rendered')),
+            'description': traverse_obj(info_data, ('details', 'post_title')),
         }
