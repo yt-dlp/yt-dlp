8f30f5a1472bcde3b38cd9d16a0015d6809f2947
pukkandan <pukkandan.ytdlp@gmail.com>
2022-07-11 04:44:32 +0530

    Update yt_dlp/extractor/theholetv.py

--- yt_dlp/extractor/theholetv.py
+++ yt_dlp/extractor/theholetv.py
@@ -20,8 +20,10 @@ class TheHoleTvExtractorIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        player_attrs = extract_attributes(self._search_regex(r'(<div[^>]*\bdata-controller="player"[^>]*>)', webpage, 'video player'))
-        formats, subtitles = self._extract_m3u8_formats_and_subtitles(player_attrs['data-player-source-value'], video_id)
+        player_attrs = extract_attributes(self._search_regex(
+            r'(<div[^>]*\bdata-controller="player"[^>]*>)', webpage, 'video player'))
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(
+            player_attrs['data-player-source-value'], video_id, 'mp4')
         self._sort_formats(formats)
 
         return {
