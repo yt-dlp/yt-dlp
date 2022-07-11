8c42d04e7ffbd06e5da8f76a5e17ab75acfd5b2c
HobbyistDev <tesutonihon4@gmail.com>
2022-07-11 14:28:07 +0900

    [Detik20] Apply fix from review, fix tests

--- yt_dlp/extractor/detik.py
+++ yt_dlp/extractor/detik.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import int_or_none, str_or_none
+from ..utils import int_or_none, merge_dicts, str_or_none
 
 
 class Detik20IE(InfoExtractor):
@@ -16,7 +16,7 @@ class Detik20IE(InfoExtractor):
             'description': 'md5:ac18dcee5b107abbec1ed46e0bf400e3',
             'title': 'Zulhas Klaim Sukses Turunkan Harga Migor Jawa-Bali',
             'tags': ['zulkifli hasan', 'menteri perdagangan', 'minyak goreng'],
-            'timestamp': 1657014348,
+            'timestamp': 1657039548,
             'upload_date': '20220705'
         }
     }, {
@@ -31,7 +31,7 @@ class Detik20IE(InfoExtractor):
             'title': 'Ahli: Level PPKM Jadi Payung Strategi Protokol Kesehatan',
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/07/05/Ahli-_Level_PPKM_Jadi_Payung_Strat_jOgUMCN-20220705182313-custom.jpg?w=650&q=80',
             'description': 'md5:4eb825a9842e6bdfefd66f47b364314a',
-            'timestamp': 1657020055,
+            'timestamp': 1657045255,
         }
     }, {
         # otobuzz
@@ -40,7 +40,7 @@ class Detik20IE(InfoExtractor):
             'id': '220704093',
             'ext': 'mp4',
             'tags': ['cicilan mobil', 'mitsubishi pajero sport', 'mitsubishi', 'pajero sport'],
-            'timestamp': 1656926321,
+            'timestamp': 1656951521,
             'duration': 83,
             'upload_date': '20220704',
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/07/04/5d6187e402ec4a91877755a5886ff5b6-20220704161859-0s.jpg?w=650&q=80',
@@ -56,7 +56,7 @@ class Detik20IE(InfoExtractor):
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/07/04/6b172c6fb564411996ea145128315630-20220704090746-0s.jpg?w=650&q=80',
             'title': 'Crash-crash Horor di Paruh Pertama MotoGP 2022',
             'description': 'md5:fbcc6687572ad7d16eb521b76daa50e4',
-            'timestamp': 1656900391,
+            'timestamp': 1656925591,
             'duration': 107,
             'tags': ['marc marquez', 'fabio quartararo', 'francesco bagnaia', 'motogp crash', 'motogp 2022'],
             'upload_date': '20220704',
@@ -69,8 +69,8 @@ class Detik20IE(InfoExtractor):
             'ext': 'mp4',
             'title': '24 Tahun Reformasi dan Alarm Demokrasi dari Filipina',
             'upload_date': '20220518',
-            'timestamp': 1652888623,
-            'duration': 7385,
+            'timestamp': 1652913823,
+            'duration': 185.0,
             'tags': ['politik', 'adu perspektif', 'indonesia', 'filipina', 'demokrasi'],
             'description': 'md5:8eaaf440b839c3d02dca8c9bbbb099a9',
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/05/18/adpers_18_mei_compressed-20220518230458-custom.jpg?w=650&q=80',
@@ -81,12 +81,12 @@ class Detik20IE(InfoExtractor):
         'info_dict': {
             'id': '220703032',
             'ext': 'mp4',
-            'timestamp': 1656799238,
+            'timestamp': 1656824438,
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/07/02/SOSOK_BGBJ-20220702191138-custom.jpg?w=650&q=80',
             'title': 'Resa Boenard Si \'Princess Bantar Gebang\'',
             'description': 'md5:84ea66306a0285330de6a13fc6218b78',
             'tags': ['sosok', 'sosok20d', 'bantar gebang', 'bgbj', 'resa boenard', 'bantar gebang bgbj', 'bgbj bantar gebang', 'sosok bantar gebang', 'sosok bgbj', 'bgbj resa boenard'],
-            'upload_date': '20220702',
+            'upload_date': '20220703',
             'duration': 650,
         }
     }, {
@@ -96,12 +96,12 @@ class Detik20IE(InfoExtractor):
             'id': '220603135',
             'ext': 'mp4',
             'description': 'md5:4771fe101aa303edb829c59c26f9e7c6',
-            'timestamp': 1654279105,
+            'timestamp': 1654304305,
             'title': 'Merasakan Bus Imut Tanpa Pengemudi, Muter-muter di Kawasan BSD City',
             'tags': ['viral', 'autonomous vehicle', 'electric', 'shuttle bus'],
             'thumbnail': 'https://cdnv.detik.com/videoservice/AdminTV/2022/06/03/VIRAL_BUS_NO_SUPIR-20220604004707-custom.jpg?w=650&q=80',
             'duration': 593,
-            'upload_date': '20220603'
+            'upload_date': '20220604',
         }
     }]
 
@@ -109,19 +109,14 @@ class Detik20IE(InfoExtractor):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
         json_ld_data = self._search_json_ld(webpage, display_id)
-
+        
         video_url = self._html_search_regex(
             r'videoUrl\s*:\s*"(?P<video_url>[^"]+)', webpage, 'videoUrl')
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(video_url, display_id, ext='mp4')
 
-        return {
+        return merge_dicts(json_ld_data, {
             'id': self._html_search_meta('video_id', webpage),
             'formats': formats,
             'subtitles': subtitles,
-            'title': json_ld_data.get('name') or self._og_search_title(webpage) or self._html_extract_title(webpage),
-            'description': json_ld_data.get('description') or self._og_search_description(webpage) or self._html_search_meta('description', webpage),
-            'thumbnail': json_ld_data.get('thumbnailUrl') or self._og_search_thumbnail(webpage),
             'tags': str_or_none(json_ld_data.get('keywords') or self._html_search_meta(['keywords', 'keyword', 'dtk:keywords'], webpage), '').split(','),
-            'duration': int_or_none(self._html_search_meta('duration', webpage)),
-            'timestamp': int_or_none(self._html_search_meta('dtk:publishdateunix', webpage), 1000)
-        }
+        })
