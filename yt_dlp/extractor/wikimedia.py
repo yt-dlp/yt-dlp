1de83b846b60164250f99ff2467fb455b8174606
pukkandan <pukkandan.ytdlp@gmail.com>
2022-07-13 20:34:32 +0530

    rewrite

--- yt_dlp/extractor/wikimedia.py
+++ yt_dlp/extractor/wikimedia.py
@@ -1,81 +1,56 @@
-from .common import InfoExtractor
-
 import re
 
+from .common import InfoExtractor
 from ..utils import (
     clean_html,
-    determine_ext,
-    ExtractorError,
     get_element_by_class,
     parse_qs,
-    urljoin)
+    remove_start,
+    unescapeHTML,
+    urljoin,
+)
 
 
 class WikimediaIE(InfoExtractor):
     IE_NAME = 'wikimedia.org'
     _API_BASE_URL = 'https://commons.wikimedia.org/'
-    _VALID_URL = 'https://commons.wikimedia.org/wiki/File:(?P<id>[^/]+)'
-    _TEST = {
+    _VALID_URL = r'https://commons.wikimedia.org/wiki/File:(?P<id>[^/]+)\.\w+'
+    _TESTS = [{
         'url': 'https://commons.wikimedia.org/wiki/File:Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm',
         'info_dict': {
             'url': 're:https?://upload.wikimedia.org/wikipedia',
-            'description': 'Deutsch:  Beschreibung auf der Seite: "Im Verlauf der Erdgeschichte glich das Klima einer Achterbahnfahrt. Die „Fieberkurve“ unseres Planeten zeigt die globalen Temperaturschwankungen bis heute – rekonstruiert anhand von historischen Klimadaten."\nZu Wikimedia Commons hochgeladen von: PantheraLeo1359531.\nHinweise zur Weiterverwendung: https://www.zdf.de/dokumentation/terra-x/terra-x-creative-commons-cc-100.html.\nVereinfachender Verlauf in der Geschichte der Erde, für die Zukunft spätestens ab dem Jahr 2050 mit spekulativem Verlauf in der Prognose (ausgeprägtes Global-warming-Szenario ist dargestellt).English:  Climate change, Temperature in history of Earth, Video of Terra X.',
             'ext': 'webm',
             'id': 'Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS',
             'title': 'Die Temperaturkurve der Erde (ZDF, Terra X) 720p HD 50FPS.webm - Wikimedia Commons',
-            'license': 'This file is licensed under the Creative Commons Attribution 4.0 International license. You are free: to share – to copy, distribute and transmit the work to remix – to adapt the work Under the following conditions: attribution – You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use. https://creativecommons.org/licenses/by/4.0CC BY 4.0 Creative Commons Attribution 4.0 truetrue',
-            'author': 'ZDF/Terra X/Gruppe 5/Luise Wagner, Jonas Sichert, Andreas Hougardy',
-            'subtitles': {
-                'de': [{
-                    'ext': 'vtt',
-                    'url': 're:https?://commons.wikimedia.org/w/api.php'
-                }],
-                'en-gb': [{
-                    'ext': 'vtt',
-                    'url': 're:https?://commons.wikimedia.org/w/api.php'
-                }],
-                'nl': [{
-                    'ext': 'vtt',
-                    'url': 're:https?://commons.wikimedia.org/w/api.php'
-                }],
-                'en': [{
-                    'ext': 'vtt',
-                    'url': 're:https?://commons.wikimedia.org/w/api.php'
-                }]
-            }}}
+            'description': 'md5:7cd84f76e7081f1be033d0b155b4a460',
+            'license': 'Creative Commons Attribution 4.0 International',
+            'uploader': 'ZDF/Terra X/Gruppe 5/Luise Wagner, Jonas Sichert, Andreas Hougardy',
+            'subtitles': 'count:4'
+        }
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        ext = determine_ext(url, None)
-        if not ext:
-            raise ExtractorError('The URL does not contain a video', expected=True)
         webpage = self._download_webpage(url, video_id)
-        video_url = self._html_search_regex('<source [^>]*src="([^"]+)"', webpage, 'video URL')
-        license = get_element_by_class('layouttemplate licensetpl mw-content-ltr', webpage)
-        license = clean_html(license)
-        description = get_element_by_class('description', webpage)
-        description = clean_html(description)
-        author = self._html_search_regex(r'>\s*Author\s*</td>\s*<td\b[^>]*>\s*([^<]+)\s*</td>', webpage, 'video author',
-                                         default=None)
-        video_id = video_id.replace('.' + ext, '')
 
         subtitles = {}
-        for sub in re.findall(r"\bsrc\s*=\s*[\"\'](\/w\/api(.*?)[\s\"])\b", webpage):
-            sub = sub[0].replace('"', '')
-            sub = urljoin('https://commons.wikimedia.org', sub).replace('amp;', '').strip()
+        for sub in set(re.findall(r'\bsrc\s*=\s*["\'](/w/api[^"]+)["\']', webpage)):
+            sub = urljoin('https://commons.wikimedia.org', unescapeHTML(sub))
             qs = parse_qs(sub)
             lang = qs.get('lang', [None])[-1]
             sub_ext = qs.get('trackformat', [None])[-1]
-            if not lang or not sub_ext:
-                continue
-            subtitles.setdefault(lang, []).append({'ext': sub_ext, 'url': sub})
+            if lang and sub_ext:
+                subtitles.setdefault(lang, []).append({'ext': sub_ext, 'url': sub})
 
-        return {'url': video_url,
-                'description': description,
-                'ext': ext,
-                'id': video_id,
-                'title': self._og_search_title(webpage).replace('File:', ''),
-                'license': license,
-                'author': author,
-                'subtitles': subtitles
-                }
+        return {
+            'id': video_id,
+            'url': self._html_search_regex(r'<source\s[^>]*\bsrc="([^"]+)"', webpage, 'video URL'),
+            'description': clean_html(get_element_by_class('description', webpage)),
+            'title': remove_start(self._og_search_title(webpage), 'File:'),
+            'license': self._html_search_regex(
+                r'licensed under(?: the)? (.+?) license',
+                get_element_by_class('licensetpl', webpage), 'license', fatal=False),
+            'uploader': self._html_search_regex(
+                r'>\s*Author\s*</td>\s*<td\b[^>]*>\s*([^<]+)\s*</td>', webpage, 'video author', default=None),
+            'subtitles': subtitles,
+        }
