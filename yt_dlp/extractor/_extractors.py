5f2da312fa66d6f001ca4d8d79ee281b9b62e9ed
HobbyistDev <105957301+HobbyistDev@users.noreply.github.com>
2022-07-14 15:24:27 +0900

    [extractor/rtl.lu] Add extractor (#4222)
    Closes #1721
    Authored by: HobbyistDev

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1444,7 +1444,13 @@ from .rottentomatoes import RottenTomatoesIE
 from .rozhlas import RozhlasIE
 from .rtbf import RTBFIE
 from .rte import RteIE, RteRadioIE
-from .rtlnl import RtlNlIE
+from .rtlnl import (
+    RtlNlIE,
+    RTLLuTeleVODIE, 
+    RTLLuArticleIE,
+    RTLLuLiveIE,
+    RTLLuRadioIE,
+)
 from .rtl2 import (
     RTL2IE,
     RTL2YouIE,
