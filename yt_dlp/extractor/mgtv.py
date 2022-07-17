1765c6039e131744a84180ba10a7a9c87565421b
pukkandan <pukkandan.ytdlp@gmail.com>
2022-07-18 05:02:30 +0530

    [extractor/MangoTV] Fix subtitle languages
    Closes #4315

--- yt_dlp/extractor/mgtv.py
+++ yt_dlp/extractor/mgtv.py
@@ -137,14 +137,15 @@ class MGTVIE(InfoExtractor):
             url_sub = sub.get('url')
             if not url_sub:
                 continue
-            locale = sub.get('captionCountrySimpleName')
+            locale = sub.get('captionSimpleName') or 'en'
             sub = self._download_json(f'{domain}{url_sub}', video_id, fatal=False,
                                       note=f'Download subtitle for locale {sub.get("name")} ({locale})') or {}
             sub_url = url_or_none(sub.get('info'))
             if not sub_url:
                 continue
-            subtitles.setdefault(locale or 'en', []).append({
+            subtitles.setdefault(locale.lower(), []).append({
                 'url': sub_url,
+                'name': sub.get('name'),
                 'ext': 'srt'
             })
         return subtitles
