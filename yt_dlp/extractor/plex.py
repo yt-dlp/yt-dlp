08a06415a9c41c8ba40bfc6c80510d4298d4a970
HobbyistDev <105957301+HobbyistDev@users.noreply.github.com>
2022-07-18 06:35:46 +0900

    Apply suggestions from code review
    Co-authored-by: pukkandan <pukkandan.ytdlp@gmail.com>

--- yt_dlp/extractor/plex.py
+++ yt_dlp/extractor/plex.py
@@ -66,7 +66,7 @@ class PlexWatchBaseIE(InfoExtractor):
 
 
 class PlexWatchMovieIE(PlexWatchBaseIE):
-    _VALID_URL = r'https?://watch\.plex\.tv/(?:\w+/)?(?:country/\w+/)?(?P<sites_type>movie)/(?P<id>[\w-]+)[?/#&]?'
+    _VALID_URL = r'https?://watch\.plex\.tv/(?:\w+/)?(?:country/\w+/)?(?P<sites_type>movie)/(?P<id>[\w-]+)'
     _TESTS = [{
         'url': 'https://watch.plex.tv/movie/bowery-at-midnight',
         'info_dict': {
@@ -170,7 +170,7 @@ class PlexWatchLiveIE(PlexWatchBaseIE):
             display_id, 'live')
 
         return {
-            'id': nextjs_json["id"],
+            'id': nextjs_json['id'],
             'display_id': display_id,
             'title': traverse_obj(media_json, ('MediaContainer', 'MediaSubscription', 0, 'title')),
             'formats': formats,
