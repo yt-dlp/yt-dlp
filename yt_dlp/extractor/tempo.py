c4cd33f0474fbbfbe8f95decfca83218689107f9
HobbyistDev <tesutonihon4@gmail.com>
2022-07-27 20:09:57 +0900

    [Tempo.co] lint

--- yt_dlp/extractor/tempo.py
+++ yt_dlp/extractor/tempo.py
@@ -4,7 +4,7 @@ from ..utils import int_or_none, parse_iso8601, str_or_none, traverse_obj
 
 class TempoIE(InfoExtractor):
     _VALID_URL = r'https?://video\.tempo\.co/\w+/\d+/(?P<id>[\w-]+)'
-    _TESTS =[{
+    _TESTS = [{
         'url': 'https://video.tempo.co/read/30058/anies-baswedan-ajukan-banding-putusan-ptun-batalkan-ump-dki',
         'info_dict': {
             'id': '2144438',
@@ -19,34 +19,34 @@ class TempoIE(InfoExtractor):
             'tags': ['Anies Baswedan', ' PTUN', ' PTUN | Pengadilan Tata Usaha Negara', ' PTUN Batalkan UMP DKI', ' UMP DKI'],
         }
     }]
-    
+
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
-        
+
         player_key, widget_id = self._search_regex(
             r'<ivs-player\s*[^>]+data-ivs-key\s*=\s*"(?P<player_key>[\w]+)[^>]+\bdata-ivs-wid="(?P<widget_id>[\w-]+)',
             webpage, 'player_key, widget_id', group=('player_key', 'widget_id'))
-        
+
         json_ld_data = self._search_json_ld(webpage, display_id)
-        
+
         json_data = self._download_json(
             f'https://ivxplayer.ivideosmart.com/prod/widget/{widget_id}',
             display_id, query={'key': player_key})
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(
             json_data['player']['video_url'], display_id, ext='mp4')
-            
+
         return {
             'id': str(json_data['ivx']['id']),
             'display_id': display_id,
             'formats': formats,
             'subtitles': subtitles,
-            'title': (self._html_search_meta('twitter:title', webpage) or self._og_search_title(webpage) 
+            'title': (self._html_search_meta('twitter:title', webpage) or self._og_search_title(webpage)
                       or traverse_obj(json_data, ('ivx', 'name'))),
             'duration': int_or_none(traverse_obj(json_data, ('ivx', 'duration'))),
-            'thumbnail': (self._html_search_meta('twitter:image:src', webpage) or self._og_search_thumbnail(webpage) 
+            'thumbnail': (self._html_search_meta('twitter:image:src', webpage) or self._og_search_thumbnail(webpage)
                           or traverse_obj(json_data, ('ivx', 'thumbnail_url'))),
-            'description': (json_ld_data.get('description') or self._html_search_meta(['description', 'twitter:description'], webpage) 
+            'description': (json_ld_data.get('description') or self._html_search_meta(['description', 'twitter:description'], webpage)
                             or self._og_search_description(webpage)),
             'timestamp': parse_iso8601(traverse_obj(json_data, ('ivx', 'created_at'))),
             'tags': str_or_none(self._html_search_meta('keywords', webpage), '').split(','),
