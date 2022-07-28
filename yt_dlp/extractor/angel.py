f60d1c655958abd9245cebea7177a8b5880f8747
Juhmer Tena <juhmer@att.net>
2022-07-28 01:23:59 -0700

    prioritize og:image property for thumbnail

--- yt_dlp/extractor/angel.py
+++ yt_dlp/extractor/angel.py
@@ -12,9 +12,9 @@ class AngelIE(InfoExtractor):
         'info_dict': {
             'id': '2f3d0382-ea82-4cdc-958e-84fbadadc710',
             'ext': 'mp4',
-            'title': 'Tuttle Twins S1 E1: When Laws Give You Lemons',
+            'title': 'Tuttle Twins Season 1, Episode 1: When Laws Give You Lemons',
             'description': 'md5:73b704897c20ab59c433a9c0a8202d5e',
-            'thumbnail': r're:^https?://images.angelstudios.com/.*\.jpeg$',
+            'thumbnail': r're:^https?://images.angelstudios.com/image/upload/angel-app/.*$',
             'duration': 1359.0
         }
     }, {
@@ -23,9 +23,9 @@ class AngelIE(InfoExtractor):
         'info_dict': {
             'id': '8dfb714d-bca5-4812-8125-24fb9514cd10',
             'ext': 'mp4',
-            'title': 'The Chosen S1 E1: I Have Called You By Name',
+            'title': 'The Chosen Season 1, Episode 1: I Have Called You By Name',
             'description': 'md5:aadfb4827a94415de5ff6426e6dee3be',
-            'thumbnail': r're:^https?://images.angelstudios.com/.*\.jpeg$',
+            'thumbnail': r're:^https?://images.angelstudios.com/image/upload/angel-app/.*$',
             'duration': 3276.0
         }
     }]
@@ -39,14 +39,18 @@ class AngelIE(InfoExtractor):
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(
             json_ld.pop('url'), video_id, note='Downloading HD m3u8 information')
 
-        return merge_dicts(json_ld, {
+        info_dict = {
             'id': video_id,
-            'title': self._og_search_title,
+            'title': self._og_search_title(webpage),
             'description': self._og_search_description(webpage),
-            'thumbnails': [{
-                # Second group has unnecessary data about transformations of the thumbnail
-                'url': re.sub(r'(/upload)/.+(/angel-app/.+)$', r'\1\2.jpeg', item['url'])
-            } for item in json_ld.pop('thumbnails') or [] if url_or_none(item.get('url'))],
             'formats': formats,
             'subtitles': subtitles
-        })
+        }
+
+        # Angel uses cloudinary in the background and supports image transformations.
+        # We remove these transformations and return the source file
+        base_thumbnail_url = url_or_none(self._og_search_thumbnail(webpage)) or json_ld.pop('thumbnails')
+        if base_thumbnail_url:
+            info_dict['thumbnail'] = re.sub(r'(/upload)/.+(/angel-app/.+)$', r'\1\2', base_thumbnail_url)
+
+        return merge_dicts(info_dict, json_ld)
