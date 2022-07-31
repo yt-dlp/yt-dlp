98a60600b22959ff9e644084c0b67672aaf6fbf6
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2022-07-31 23:47:32 +0000

    [extractors/holodex] Add extractor (#4434)
    Closes #726 
    Authored by: sqrtNOT, pukkandan

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -641,6 +641,7 @@ from .hidive import HiDiveIE
 from .historicfilms import HistoricFilmsIE
 from .hitbox import HitboxIE, HitboxLiveIE
 from .hitrecord import HitRecordIE
+from .holodex import HolodexIE
 from .hotnewhiphop import HotNewHipHopIE
 from .hotstar import (
     HotStarIE,
