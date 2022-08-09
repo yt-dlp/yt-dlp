7a6de82c96ff6efa8215b3d22bdafc5caa1002f8
palewire <b@palewi.re>
2022-08-09 11:59:49 -0700

    [truth] added new extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1792,6 +1792,7 @@ from .trovo import (
 )
 from .trueid import TrueIDIE
 from .trunews import TruNewsIE
+from .truth import TruthIE
 from .trutv import TruTVIE
 from .tube8 import Tube8IE
 from .tubetugraz import TubeTuGrazIE, TubeTuGrazSeriesIE
