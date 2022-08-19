e409f8233c8b58c9e95410fe2dbcd03f8b79f044
elyse0 <26639800+elyse0@users.noreply.github.com>
2022-08-19 02:39:56 -0500

    Do not import ctypes globally

--- yt_dlp/jsinterp.py
+++ yt_dlp/jsinterp.py
@@ -6,8 +6,6 @@ import math
 import operator
 import re
 
-from ctypes import c_int32
-
 from .utils import (
     NO_DEFAULT,
     ExtractorError,
@@ -284,7 +282,8 @@ class JSInterpreter:
         try:
             # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#binary_bitwise_operators
             if op in ('&', '|', '^', '~', '<<', '>>', '>>', '>>>'):
-                return c_int32(_OPERATORS[op](left_val, right_val)).value
+                import ctypes
+                return ctypes.c_int32(_OPERATORS[op](left_val, right_val)).value
 
             return _OPERATORS[op](left_val, right_val)
         except Exception as e:
