6d6dd396deebcc76e9c7ecc88e620bc661744609
columndeeply <columndeeply@proton.me>
2022-08-31 07:31:27 +0200

    Add default values for optional fields

--- yt_dlp/extractor/prankcast.py
+++ yt_dlp/extractor/prankcast.py
@@ -31,33 +31,49 @@ class PrankCastIE(InfoExtractor):
         url = broadcast_url + recording_hash + ".mp3"
 
         # Get dates
-        start_date = json_info['props']['pageProps']['ssr_data_showreel']['start_date'].replace('Z', '')
-        end_date = json_info['props']['pageProps']['ssr_data_showreel']['end_date'].replace('Z', '')
+        start_date = None
+        if 'start_date' in json_info['props']['pageProps']['ssr_data_showreel']:
+            start_date = json_info['props']['pageProps']['ssr_data_showreel']['start_date'].replace('Z', '')
+
+        end_date = None
+        if 'end_date' in json_info['props']['pageProps']['ssr_data_showreel']:
+            end_date = json_info['props']['pageProps']['ssr_data_showreel']['end_date'].replace('Z', '')
 
         # Get broadcast date
-        upload_date = start_date.split('T')[0].replace('-', '')
+        upload_date = None
+        if start_date is not None:
+            upload_date = start_date.split('T')[0].replace('-', '')
 
         # Get broadcast title
         broadcast_title = json_info['props']['pageProps']['ssr_data_showreel']['broadcast_title']
 
         # Get author (AKA show host)
-        uploader = json_info['props']['pageProps']['ssr_data_showreel']['user_name']
+        uploader = None
+        if 'user_name' in json_info['props']['pageProps']['ssr_data_showreel']:
+            uploader = json_info['props']['pageProps']['ssr_data_showreel']['user_name']
 
         # Get the co-hosts/guests
-        guests = [uploader]
-        for guest in json.loads(json_info['props']['pageProps']['ssr_data_showreel']['guests_json']):
-            guests.append(guest['name'])
+        if uploader is not None:
+            guests = [uploader]
+        else:
+            guests = []
+
+        if 'guests_json' in json_info['props']['pageProps']['ssr_data_showreel']:
+            for guest in json.loads(json_info['props']['pageProps']['ssr_data_showreel']['guests_json']):
+                guests.append(guest['name'])
 
         # Parse the duration of the stream
-        duration = datetime.fromisoformat(end_date) - datetime.fromisoformat(start_date)
-        parsed_duration = int(parse_duration(str(duration)))
+        parsed_duration = None
+        if start_date is not None and end_date is not None:
+            duration = datetime.fromisoformat(end_date) - datetime.fromisoformat(start_date)
+            parsed_duration = int(parse_duration(str(duration)))
 
         return {
             'id': video_id,
             'title': broadcast_title,
+            'url': url,
             'upload_date': upload_date,
             'uploader': uploader,
-            'url': url,
             'duration': parsed_duration,
             'cast': ', '.join(guests)
         }
