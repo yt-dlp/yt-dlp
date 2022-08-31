99ba0a8c713b7f27ece9f791940979ff7f5d9f2e
pukkandan <pukkandan.ytdlp@gmail.com>
2022-08-31 22:10:54 +0530

    cleanup

--- yt_dlp/extractor/epoch.py
+++ yt_dlp/extractor/epoch.py
@@ -3,12 +3,11 @@ from .common import InfoExtractor
 
 class EpochIE(InfoExtractor):
     _VALID_URL = r'https?://www.theepochtimes\.com/[\w-]+_(?P<id>\d+).html'
-    _YOUMAKER_MANIFEST_URL = 'http://vs1.youmaker.com/assets/{0}/playlist.m3u8'
     _TESTS = [
         {
             'url': 'https://www.theepochtimes.com/they-can-do-audio-video-physical-surveillance-on-you-24h-365d-a-year-rex-lee-on-intrusive-apps_4661688.html',
             'info_dict': {
-                'id': '4661688',
+                'id': 'a3dd732c-4750-4bc8-8156-69180668bda1',
                 'ext': 'mp4',
                 'title': '‘They Can Do Audio, Video, Physical Surveillance on You 24H/365D a Year’: Rex Lee on Intrusive Apps',
             }
@@ -16,7 +15,7 @@ class EpochIE(InfoExtractor):
         {
             'url': 'https://www.theepochtimes.com/the-communist-partys-cyberattacks-on-america-explained-rex-lee-talks-tech-hybrid-warfare_4342413.html',
             'info_dict': {
-                'id': '4342413',
+                'id': '276c7f46-3bbf-475d-9934-b9bbe827cf0a',
                 'ext': 'mp4',
                 'title': 'The Communist Party’s Cyberattacks on America Explained; Rex Lee Talks Tech Hybrid Warfare',
             }
@@ -24,7 +23,7 @@ class EpochIE(InfoExtractor):
         {
             'url': 'https://www.theepochtimes.com/kash-patel-a-6-year-saga-of-government-corruption-from-russiagate-to-mar-a-lago_4690250.html',
             'info_dict': {
-                'id': '4690250',
+                'id': 'aa9ceecd-a127-453d-a2de-7153d6fd69b6',
                 'ext': 'mp4',
                 'title': 'Kash Patel: A ‘6-Year-Saga’ of Government Corruption, From Russiagate to Mar-a-Lago',
             }
@@ -32,20 +31,16 @@ class EpochIE(InfoExtractor):
     ]
 
     def _real_extract(self, url):
-
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-        title = self._html_extract_title(webpage)
-        youmaker_video_id = self._search_regex(
-            r'data-trailer="[\w-]+" data-id="([\w-]+)"', webpage, 'url')
 
+        youmaker_video_id = self._search_regex(r'data-trailer="[\w-]+" data-id="([\w-]+)"', webpage, 'url')
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(
-            self._YOUMAKER_MANIFEST_URL.format(youmaker_video_id), video_id, 'mp4', entry_protocol='m3u8_native',
-            m3u8_id='hls', fatal=False)
+            f'http://vs1.youmaker.com/assets/{youmaker_video_id}/playlist.m3u8', video_id, 'mp4', m3u8_id='hls')
 
         return {
-            'id': video_id,
+            'id': youmaker_video_id,
             'formats': formats,
             'subtitles': subtitles,
-            'title': title
+            'title': self._html_extract_title(webpage)
         }
