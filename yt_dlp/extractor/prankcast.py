9178afd08f61225eb59a04746598322abb6268d3
columndeeply <columndeeply@proton.me>
2022-09-05 19:46:29 +0200

    Inline url return and use list comprehension to get the cast

--- yt_dlp/extractor/prankcast.py
+++ yt_dlp/extractor/prankcast.py
@@ -23,28 +23,12 @@ class PrankCastIE(InfoExtractor):
         # Extract the JSON
         json_info = self._search_nextjs_data(webpage, video_id)['props']['pageProps']['ssr_data_showreel']
 
-        # Get the broadcast URL and the recording hash.
-        # The full URL is {broadcast_url}/{recording_hash}.mp3
-        broadcast_url = json_info.get('broadcast_url')
-        recording_hash = json_info.get('recording_hash')
-        url = broadcast_url + recording_hash + ".mp3"
-
         # Get author (AKA show host)
         uploader = json_info.get('user_name')
 
         # Get the co-hosts/guests
-        if uploader != '':
-            guests = [uploader]
-        else:
-            guests = []
-
-        guests_json = json_info.get('guests_json')
-        if guests_json is not None:
-            guests_json = self._parse_json(guests_json, video_id)
-            for guest in guests_json:
-                guest_name = guest.get('name')
-                if guest_name is not None:
-                    guests.append(guest_name)
+        guests_json = self._parse_json(json_info.get('guests_json') or '{}', video_id)
+        guests = [] if not uploader else [uploader] + [x.get('name') for x in guests_json if x.get('name') is not None]
 
         # Get dates
         start_date = parse_iso8601(json_info.get('start_date'))
@@ -59,7 +43,7 @@ class PrankCastIE(InfoExtractor):
             'id': video_id,
             'title': json_info.get('broadcast_title') or self._og_search_title(webpage),
             'display_id': display_id,
-            'url': url,
+            'url': f'{json_info["broadcast_url"]}{json_info["recording_hash"]}.mp3',
             'timestamp': start_date,
             'uploader': uploader,
             'channel_id': json_info.get('user_id'),
