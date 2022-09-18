166934042d44a3ae32e0306115c51473fb87c4c6
itachi-19 <sulabh.biswas.0157@gmail.com>
2022-09-19 01:03:35 +0530

    Mxplayer API fixes

--- yt_dlp/extractor/mxplayer.py
+++ yt_dlp/extractor/mxplayer.py
@@ -4,124 +4,89 @@ from ..utils import try_get
 
 
 class MxplayerIE(InfoExtractor):
+    #_VALID_URL = r'https?://(?:www\.)?mxplayer\.in/(?P<type>movie|show/[-\w]+/[-\w]+)/(?P<display_id>[-\w]+)-(?P<id>\w+)'
     _VALID_URL = r'https?://(?:www\.)?mxplayer\.in/(?P<type>movie|show/[-\w]+/[-\w]+)/(?P<display_id>[-\w]+)-(?P<id>\w+)'
     _TESTS = [{
-        'url': 'https://www.mxplayer.in/show/watch-my-girlfriend-is-an-alien-hindi-dubbed/season-1/episode-1-online-9d2013d31d5835bb8400e3b3c5e7bb72',
+        'url': 'https://www.mxplayer.in/movie/watch-kitne-door-kitne-paas-movie-online-a9e9c76c566205955f70d8b2cb88a6a2',
         'info_dict': {
-            'id': '9d2013d31d5835bb8400e3b3c5e7bb72',
+            'id': 'a9e9c76c566205955f70d8b2cb88a6a2',
+            'display_id': 'watch-kitne-door-kitne-paas-movie-online',
+            'title': 'Kitne Door Kitne Paas',
+            'duration': 8458,
             'ext': 'mp4',
-            'title': 'Episode 1',
-            'description': 'md5:62ed43eb9fec5efde5cf3bd1040b7670',
-            'season_number': 1,
-            'episode_number': 1,
-            'duration': 2451,
-            'season': 'Season 1',
-            'series': 'My Girlfriend Is An Alien (Hindi Dubbed)',
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/9d2013d31d5835bb8400e3b3c5e7bb72/en/16x9/320x180/9562f5f8df42cad09c9a9c4e69eb1567_1920x1080.webp',
-            'episode': 'Episode 1'
+            'description': 'Jatin and Karishma, who meet each other on a plane to India, are set to marry the partners chosen by their respective parents. However, they find themselves attracted to each other.',
         },
         'params': {
             'format': 'bv',
             'skip_download': True,
         },
     }, {
-        'url': 'https://www.mxplayer.in/movie/watch-knock-knock-hindi-dubbed-movie-online-b9fa28df3bfb8758874735bbd7d2655a?watch=true',
+        'url': 'https://www.mxplayer.in/show/watch-ek-thi-begum-hindi/season-2/game-of-power-online-5e5305c28f1409847cdc4520b6ad77cf',
         'info_dict': {
-            'id': 'b9fa28df3bfb8758874735bbd7d2655a',
+            'id': '5e5305c28f1409847cdc4520b6ad77cf',
+            'display_id': 'game-of-power-online',
+            'title': 'Game Of Power',
+            'duration': 1845,
             'ext': 'mp4',
-            'title': 'Knock Knock (Hindi Dubbed)',
-            'description': 'md5:b195ba93ff1987309cfa58e2839d2a5b',
-            'season_number': 0,
-            'episode_number': 0,
-            'duration': 5970,
-            'season': 'Season 0',
-            'series': None,
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/b9fa28df3bfb8758874735bbd7d2655a/en/16x9/320x180/test_pic1588676032011.webp',
-            'episode': 'Episode 0'
+            'description': 'Cops arrest Ashraf assuming a prostitute. But she gets away with her new identity, Leela Paswan. Maqsood dumps Nana Mhatre and appoints Shaqeel Ansari as a new Bombay chief. For society, he is a sophisticated businessman. The rift between the new chief minister, Yashwant Patil and Shaqeel Ansari begins with a failed business deal. Ashwin Surve, a daredevil young gangster challenges established gangs of Maqsood & Bhai Chavan. He wants to enter in the drug business. But the only supplier Nari Khan supplies stuff only to Maqsood gang and no one else. ACP Qureshi arrests Bhai Chavan and turns his focus on people close to Zaheer and Ashraf. Ashraf faces a narrow escape from Qureshi and decides to take up her new identity beyond just fake documents.',
+            'series': 'Ek Thi Begum (Hindi)',
+            'season': 'Season 2',
+            'season_number': 2,
+            'episode': 'Episode 2',
+            'episode_number': 2,
         },
         'params': {
             'format': 'bv',
             'skip_download': True,
         },
-    }, {
-        'url': 'https://www.mxplayer.in/show/watch-shaitaan/season-1/the-infamous-taxi-gang-of-meerut-online-45055d5bcff169ad48f2ad7552a83d6c',
-        'info_dict': {
-            'id': '45055d5bcff169ad48f2ad7552a83d6c',
-            'ext': 'mp4',
-            'title': 'The infamous taxi gang of Meerut',
-            'description': 'md5:033a0a7e3fd147be4fb7e07a01a3dc28',
-            'season_number': 1,
-            'episode_number': 1,
-            'duration': 2332,
-            'season': 'Season 1',
-            'series': 'Shaitaan',
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/45055d5bcff169ad48f2ad7552a83d6c/en/16x9/320x180/voot_8e7d5f8d8183340869279c732c1e3a43.webp',
-            'episode': 'Episode 1'
-        },
-        'params': {
-            'format': 'best',
-            'skip_download': True,
-        },
-    }, {
-        'url': 'https://www.mxplayer.in/show/watch-aashram/chapter-1/duh-swapna-online-d445579792b0135598ba1bc9088a84cb',
-        'info_dict': {
-            'id': 'd445579792b0135598ba1bc9088a84cb',
-            'ext': 'mp4',
-            'title': 'Duh Swapna',
-            'description': 'md5:35ff39c4bdac403c53be1e16a04192d8',
-            'season_number': 1,
-            'episode_number': 3,
-            'duration': 2568,
-            'season': 'Chapter 1',
-            'series': 'Aashram',
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/d445579792b0135598ba1bc9088a84cb/en/4x3/1600x1200/test_pic1624819307993.webp',
-            'episode': 'Episode 3'
-        },
-        'params': {
-            'format': 'bv',
-            'skip_download': True,
-        },
-    }, {
-        'url': 'https://www.mxplayer.in/show/watch-dangerous/season-1/chapter-1-online-5a351b4f9fb69436f6bd6ae3a1a75292',
-        'info_dict': {
-            'id': '5a351b4f9fb69436f6bd6ae3a1a75292',
-            'ext': 'mp4',
-            'title': 'Chapter 1',
-            'description': 'md5:233886b8598bc91648ac098abe1d288f',
-            'season_number': 1,
-            'episode_number': 1,
-            'duration': 1305,
-            'season': 'Season 1',
-            'series': 'Dangerous',
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/5a351b4f9fb69436f6bd6ae3a1a75292/en/4x3/1600x1200/test_pic1624706302350.webp',
-            'episode': 'Episode 1'
-        },
-        'params': {
-            'format': 'bv',
-            'skip_download': True,
-        },
-    }, {
-        'url': 'https://www.mxplayer.in/movie/watch-the-attacks-of-2611-movie-online-0452f0d80226c398d63ce7e3ea40fa2d',
-        'info_dict': {
-            'id': '0452f0d80226c398d63ce7e3ea40fa2d',
-            'ext': 'mp4',
-            'title': 'The Attacks of 26/11',
-            'description': 'md5:689bacd29e97b3f31eaf519eb14127e5',
-            'season_number': 0,
-            'episode_number': 0,
-            'duration': 6085,
-            'season': 'Season 0',
-            'series': None,
-            'thumbnail': 'https://qqcdnpictest.mxplay.com/pic/0452f0d80226c398d63ce7e3ea40fa2d/en/16x9/320x180/00c8955dab5e5d340dbde643f9b1f6fd_1920x1080.webp',
-            'episode': 'Episode 0'
-        },
-        'params': {
-            'format': 'best',
-            'skip_download': True,
-        },
-    }]
+    },]
 
     def _real_extract(self, url):
+        video_type, display_id, video_id = self._match_valid_url(url).groups()
+        if 'show' in video_type: video_type = 'episode'
+        API_URL = f'https://api.mxplay.com/v1/web/detail/video?type={video_type}&id={video_id}'
+        STREAM_URL = 'https://llvod.mxplay.com/{}'
+        data_json = self._download_json(API_URL, display_id)
+        print(API_URL)
+        formats = []
+        subtitles = {}
+
+        from pprint import pprint
+
+        description = data_json['description']
+        series, season, season_number, episode_number = None, None, None, None
+
+        if video_type == 'episode':
+            series = data_json['container']['container']['title']
+            season = data_json['container']['title']
+            season_number = int(season.split()[1])
+            episode_number = data_json['sequence']
+
+        for key in 'dash', 'hls':
+            playlist_url = STREAM_URL.format(data_json['stream'][key]['high'])
+            print(playlist_url)
+            if key == 'dash':
+                frmt, subs = self._extract_mpd_formats_and_subtitles(playlist_url, display_id, fatal=False)
+            else:
+                frmt, subs = self._extract_m3u8_formats_and_subtitles(playlist_url, display_id, fatal=False)
+            formats.extend(frmt)
+            subtitles = self._merge_subtitles(subtitles, subs)
+        self._sort_formats(formats)
+
+        return {
+            'id': video_id,
+            'display_id': display_id,
+            'title': data_json['title'],
+            'duration': data_json['duration'],
+            'description': description,
+            'formats': formats,
+            'subtitles': subtitles,
+            'series': series,
+            'season': season,
+            'season_number': season_number,
+            'episode_number': episode_number,
+        }
+        '''
         type, display_id, video_id = self._match_valid_url(url).groups()
         type = 'movie_film' if type == 'movie' else 'tvshow_episode'
         API_URL = 'https://androidapi.mxplay.com/v1/detail/'
@@ -174,6 +139,7 @@ class MxplayerIE(InfoExtractor):
             'formats': formats,
             'subtitles': subtitles,
         }
+    '''
 
 
 class MxplayerShowIE(InfoExtractor):
