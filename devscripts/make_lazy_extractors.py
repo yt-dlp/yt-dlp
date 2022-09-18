d53c4a27e080cd5aea31e57382bc6536f9fc1900
John Sanabria <john.sanabria@correounivalle.edu.co>
2022-09-17 19:32:56 -0500

    fixing os.rename -> shutil.move

--- devscripts/make_lazy_extractors.py
+++ devscripts/make_lazy_extractors.py
@@ -3,6 +3,7 @@
 # Allow direct execution
 import os
 import sys
+import shutil
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -50,12 +51,14 @@ def get_all_ies():
     PLUGINS_DIRNAME = 'ytdlp_plugins'
     BLOCKED_DIRNAME = f'{PLUGINS_DIRNAME}_blocked'
     if os.path.exists(PLUGINS_DIRNAME):
-        os.rename(PLUGINS_DIRNAME, BLOCKED_DIRNAME)
+        #os.rename(PLUGINS_DIRNAME, BLOCKED_DIRNAME)
+        shutil.move(PLUGINS_DIRNAME, BLOCKED_DIRNAME)
     try:
         from yt_dlp.extractor.extractors import _ALL_CLASSES
     finally:
         if os.path.exists(BLOCKED_DIRNAME):
-            os.rename(BLOCKED_DIRNAME, PLUGINS_DIRNAME)
+            #os.rename(BLOCKED_DIRNAME, PLUGINS_DIRNAME)
+            shutil.move(BLOCKED_DIRNAME, PLUGINS_DIRNAME)
     return _ALL_CLASSES
 
 
