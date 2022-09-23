1555baad8a490330f688bf69e8272789ed1fa6e4
HobbyistDev <tesutonihon4@gmail.com>
2022-09-23 21:05:24 +0900

    [extractor/unscripted] Add UnscriptedNewsVideo Extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1966,6 +1966,7 @@ from .drooble import DroobleIE
 from .umg import UMGDeIE
 from .unistra import UnistraIE
 from .unity import UnityIE
+from .unscripted import UnscriptedNewsVideoIE
 from .uol import UOLIE
 from .uplynk import (
     UplynkIE,
