f90e5ea04642c24bd351369b31b5436da7588db2
doublecouponday <mail@samueljenks.me>
2022-09-30 23:38:52 +1300

    feat: microsoft embed

--- yt_dlp/extractor/microsoftembed.py
+++ yt_dlp/extractor/microsoftembed.py
@@ -1,11 +1,18 @@
 from .common import InfoExtractor
-
+from ..utils import (
+    traverse_obj,
+    unified_timestamp,
+    int_or_none,
+)
 
 class MicrosoftEmbedIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?microsoft\.com/en-us/videoplayer/embed/(?P<id>[a-z0-9A-Z]+)'
     _TESTS = [{
         'url': 'https://www.microsoft.com/en-us/videoplayer/embed/RWL07e',
-        'only_matching': True
+        'info_dict': {
+            'id': 'RWL07e',
+            'title': '...'
+        }
     }]
     _API_URL = 'https://prod-video-cms-rt-microsoft-com.akamaized.net/vhs/api/videos/'
 
@@ -17,22 +24,31 @@ class MicrosoftEmbedIE(InfoExtractor):
             video_id)
 
         formats = []
-        streams = metadata['streams']
+        for source_type, source in metadata['streams'].items():
+            stream_url = source['url']
 
-        for key, value in streams.items():
-            stream_url = value['url']
-
-            if key in ('smoothStreaming', 'apple_HTTP_Live_Streaming', 'mPEG_DASH'):
-                formats.extend(self._extract_ism_formats(stream_url, video_id))
-
+            if source_type == 'smoothStreaming':
+                formats.extend(self._extract_ism_formats(stream_url, video_id, 'mss'))
+            elif source_type in ('apple_HTTP_Live_Streaming', 'mPEG_DASH'):
+                formats.extend(self._extract_m3u8_formats(stream_url, video_id))
+            else: 
+                formats.append({
+                    'format_id': source_type,
+                    'url': stream_url
+                    # height...
+                    #width ...
+                })
             # else:
             #     formats[key] = value
-
+        # TODO:
+        # - extract subtitles (see expected return format in common.py)
+        # - extract thumbnails (see expected return format in common.py)
         output = {
             'id': video_id,
-            'title': metadata['snippet']['title'],
-            'thumbnails': metadata['snippet']['thumbnails'],
-            'timestamp': metadata['snippet']['activeStartDate'],
-            'formats': formats
+            'title': traverse_obj('snippet', 'title'),
+            #'thumbnails': metadata['snippet']['thumbnails'],  # will need to extract into thumbnail list ofrmat
+            'timestamp': metadata['snippet']['activeStartDate'],  # use unified_timestamp from utils
+            'formats': formats,
+            'age_limit': int_or_none(traverse_obj('snippet', 'minimumAge')) or 0
         }
         return output
