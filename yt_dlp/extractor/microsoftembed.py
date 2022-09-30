db1fbf00ce2d7edc3041abdc6d2acfcb4f98ef79
doublecouponday <mail@samueljenks.me>
2022-09-30 23:52:49 +1300

    fix: datetime formatting

--- yt_dlp/extractor/microsoftembed.py
+++ yt_dlp/extractor/microsoftembed.py
@@ -29,26 +29,36 @@ class MicrosoftEmbedIE(InfoExtractor):
 
             if source_type == 'smoothStreaming':
                 formats.extend(self._extract_ism_formats(stream_url, video_id, 'mss'))
-            elif source_type in ('apple_HTTP_Live_Streaming', 'mPEG_DASH'):
+            elif source_type == 'apple_HTTP_Live_Streaming':
                 formats.extend(self._extract_m3u8_formats(stream_url, video_id))
+            elif source_type == 'mPEG_DASH':
+                formats.extend(self._extract_mpd_formats(stream_url, video_id))
             else: 
                 formats.append({
                     'format_id': source_type,
-                    'url': stream_url
-                    # height...
-                    #width ...
+                    'url': stream_url,
+                    'height': source.get('heightPixels'),
+                    'width': source.get('widthPixels')
                 })
-            # else:
-            #     formats[key] = value
+
+        self._sort_formats(formats)
+        
+        timestamp = traverse_obj(
+            metadata, ('snippet', 'activeStartDate'))
+
+        age_limit = int_or_none(
+            traverse_obj(metadata, ('snippet', 'minimumAge'))) or 0
+
         # TODO:
         # - extract subtitles (see expected return format in common.py)
         # - extract thumbnails (see expected return format in common.py)
         output = {
             'id': video_id,
-            'title': traverse_obj('snippet', 'title'),
+            'title': traverse_obj(metadata, ('snippet', 'title')),
             #'thumbnails': metadata['snippet']['thumbnails'],  # will need to extract into thumbnail list ofrmat
-            'timestamp': metadata['snippet']['activeStartDate'],  # use unified_timestamp from utils
+            'timestamp': unified_timestamp(timestamp),  # use unified_timestamp from utils
             'formats': formats,
-            'age_limit': int_or_none(traverse_obj('snippet', 'minimumAge')) or 0
+            'age_limit': age_limit,
+            'subtitles': ''
         }
         return output
