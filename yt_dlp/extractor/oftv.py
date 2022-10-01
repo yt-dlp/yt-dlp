c2cbcba4d3863a33bddf2af3ec0cf6528cad9bb1
doublecouponday <mail@samueljenks.me>
2022-10-01 14:25:56 +1300

    draft: oftv extractor

--- yt_dlp/extractor/oftv.py
+++ yt_dlp/extractor/oftv.py
@@ -1,15 +1,15 @@
 from .common import InfoExtractor
-
+from .zype import ZypeIE
 
 class OfTVIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?yourextractor\.com/watch/(?P<id>[0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?of.tv/video/(?P<id>[0-9a-zA-Z]+)'
     _TESTS = [{
         'url': 'https://of.tv/video/627d7d95b353db0001dadd1a',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+        'md5': '',
         'info_dict': {
             'id': '627d7d95b353db0001dadd1a',
             'ext': 'mp4',
-            'title': 'Video title goes here',
+            'title': '',
             'thumbnail': r're:^https?://.*\.jpg$',
         }
     }]
@@ -17,16 +17,7 @@ class OfTVIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-
-        
-
-        # TODO more code goes here, for example ...
-        title = self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title')
-
-        return {
-            'id': video_id,
-            'title': title,
-            'description': self._og_search_description(webpage),
-            'uploader': self._search_regex(r'<div[^>]+id="uploader"[^>]*>([^<]+)<', webpage, 'uploader', fatal=False),
-            # TODO more properties (see yt_dlp/extractor/common.py)
-        }
+        extraction = ZypeIE.extract_from_webpage(self._downloader, url, webpage)
+        output = list(extraction)
+        print(f'extractor: {output[0]}')
+        return output[0]
