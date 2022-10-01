3ca213dc2aa12bcdf6c8287f049f163092224014
Tejas Deepak Dashpute <54032612+Tejas1020@users.noreply.github.com>
2022-10-01 19:29:12 +0530

    hacktoberfest-2022
    Add this your repo as this can help for dictionary

---
+++ English Dictionary.py
@@ -0,0 +1,27 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("English Dictionary\data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
