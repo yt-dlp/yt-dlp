cc558b8ac1f10c485078f882928cbfa26c1dfe48
doublecouponday <mail@samueljenks.me>
2022-10-01 13:30:14 +1300

    init: oftv extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1222,6 +1222,7 @@ from .nzherald import NZHeraldIE
 from .nzz import NZZIE
 from .odatv import OdaTVIE
 from .odnoklassniki import OdnoklassnikiIE
+from .oftv import OfTVIE
 from .oktoberfesttv import OktoberfestTVIE
 from .olympics import OlympicsReplayIE
 from .on24 import On24IE
