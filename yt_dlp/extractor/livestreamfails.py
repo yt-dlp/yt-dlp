c8ce3141dac8dc72be567af9133921313455d838
invertico <8355966+invertico@users.noreply.github.com>
2022-10-04 19:29:34 +0200

    [livestreamfails] Support alternative URL

--- yt_dlp/extractor/livestreamfails.py
+++ yt_dlp/extractor/livestreamfails.py
@@ -3,21 +3,27 @@ from ..utils import format_field, traverse_obj, unified_timestamp
 
 
 class LivestreamfailsIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?livestreamfails\.com/clip/(?P<id>[0-9]+)'
-    _TESTS = [{
-        'url': 'https://livestreamfails.com/clip/139200',
-        'md5': '8a03aea1a46e94a05af6410337463102',
-        'info_dict': {
-            'id': '139200',
-            'ext': 'mp4',
-            'display_id': 'ConcernedLitigiousSalmonPeteZaroll-O8yo9W2L8OZEKhV2',
-            'title': 'Streamer jumps off a trampoline at full speed',
-            'creator': 'paradeev1ch',
-            'thumbnail': r're:^https?://.+',
-            'timestamp': 1656271785,
-            'upload_date': '20220626',
+    _VALID_URL = r'https?://(?:www\.)?livestreamfails\.com/(clip|post)/(?P<id>[0-9]+)'
+    _TESTS = [
+        {
+            'url': 'https://livestreamfails.com/clip/139200',
+            'md5': '8a03aea1a46e94a05af6410337463102',
+            'info_dict': {
+                'id': '139200',
+                'ext': 'mp4',
+                'display_id': 'ConcernedLitigiousSalmonPeteZaroll-O8yo9W2L8OZEKhV2',
+                'title': 'Streamer jumps off a trampoline at full speed',
+                'creator': 'paradeev1ch',
+                'thumbnail': r're:^https?://.+',
+                'timestamp': 1656271785,
+                'upload_date': '20220626',
+            }
+        },
+        {
+            'url': 'https://livestreamfails.com/post/139200',
+            'only_matching': True,
         }
-    }]
+    ]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
