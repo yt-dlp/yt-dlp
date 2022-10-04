3eda01301ff5a8f36fdae81d634bfd8daa9556c8
Bobstuna <qwertster0@gmail.com>
2022-10-04 15:44:55 +1300

    fix test to use playlist form

--- yt_dlp/extractor/israelnationalnews.py
+++ yt_dlp/extractor/israelnationalnews.py
@@ -7,30 +7,35 @@ class IsraelNationalNewsIE(InfoExtractor):
     _TESTS = [{
         'url': 'https://www.israelnationalnews.com/news/354520',
         'info_dict': {
-            'id': 'jA84wQhVvg8',
-            'title': 'Even CNN Host Is Shocked by How Bad Biden\'s Approval Ratings Have Gotten | DM CLIPS | Rubin Report',
-            'ext': 'mp4',
-            'description': 'md5:b7325a3d00c7596337dc3ae37e32d35c',
-            'channel': 'The Rubin Report',
-            'channel_follower_count': int,
-            'comment_count': int,
-            'categories': ['News & Politics'],
-            'like_count': int,
-            'uploader_url': 'http://www.youtube.com/user/RubinReport',
-            'uploader_id': 'RubinReport',
-            'availability': 'public',
-            'view_count': int,
-            'duration': 240,
-            'thumbnail': 'https://i.ytimg.com/vi_webp/jA84wQhVvg8/maxresdefault.webp',
-            'live_status': 'not_live',
-            'playable_in_embed': True,
-            'age_limit': 0,
-            'tags': 'count:29',
-            'channel_id': 'UCJdKr0Bgd_5saZYqLCa9mng',
-            'channel_url': 'https://www.youtube.com/channel/UCJdKr0Bgd_5saZYqLCa9mng',
-            'upload_date': '20220606',
-            'uploader': 'The Rubin Report',
-        }
+            'id': '354520'
+        },
+        'playlist': [{
+            'info_dict': {
+                'id': 'jA84wQhVvg8',
+                'title': 'Even CNN Host Is Shocked by How Bad Biden\'s Approval Ratings Have Gotten | DM CLIPS | Rubin Report',
+                'ext': 'mp4',
+                'description': 'md5:b7325a3d00c7596337dc3ae37e32d35c',
+                'channel': 'The Rubin Report',
+                'channel_follower_count': int,
+                'comment_count': int,
+                'categories': ['News & Politics'],
+                'like_count': int,
+                'uploader_url': 'http://www.youtube.com/user/RubinReport',
+                'uploader_id': 'RubinReport',
+                'availability': 'public',
+                'view_count': int,
+                'duration': 240,
+                'thumbnail': 'https://i.ytimg.com/vi_webp/jA84wQhVvg8/maxresdefault.webp',
+                'live_status': 'not_live',
+                'playable_in_embed': True,
+                'age_limit': 0,
+                'tags': 'count:29',
+                'channel_id': 'UCJdKr0Bgd_5saZYqLCa9mng',
+                'channel_url': 'https://www.youtube.com/channel/UCJdKr0Bgd_5saZYqLCa9mng',
+                'upload_date': '20220606',
+                'uploader': 'The Rubin Report',
+            }
+        }]
     }]
 
     def _real_extract(self, url):
