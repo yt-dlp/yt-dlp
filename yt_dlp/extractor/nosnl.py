2cc4c0a2226e41c11f1533a5fc632f685871c54e
pukkandan <pukkandan.ytdlp@gmail.com>
2022-10-06 09:22:53 +0530

    fix misunderstanding

--- yt_dlp/extractor/nosnl.py
+++ yt_dlp/extractor/nosnl.py
@@ -49,7 +49,7 @@ class NOSNLArticleIE(InfoExtractor):
         }
     ]
 
-    def _get_video_generator(self, nextjs_json, display_id):
+    def _entries(self, nextjs_json, display_id):
         for item in nextjs_json['items']:
             if item.get('type') == 'video':
                 formats, subtitle = self._extract_m3u8_formats_and_subtitles(
@@ -83,7 +83,7 @@ class NOSNLArticleIE(InfoExtractor):
         nextjs_json = self._search_nextjs_data(webpage, display_id)['props']['pageProps']['data']
         return {
             '_type': 'playlist',
-            '_entries': self._get_video_generator(nextjs_json, display_id),
+            'entries': self._entries(nextjs_json, display_id),
             'id': str(nextjs_json['id']),
             'title': nextjs_json.get('title') or self._html_search_meta(['title', 'og:title', 'twitter:title'], webpage),
             'description': (nextjs_json.get('description')
