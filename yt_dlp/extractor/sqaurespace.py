11bb5933701cd05a43e9a12e509bfe0343feca54
coletdjnz <coletdjnz@protonmail.com>
2022-10-08 18:52:17 +1300

    combine

--- yt_dlp/extractor/sqaurespace.py
+++ yt_dlp/extractor/sqaurespace.py
@@ -12,6 +12,8 @@ class SqaurespaceGenericPassthroughIE(InfoExtractor):
     _VALID_URL = False
 
     def _extract_from_webpage(self, url, webpage):
+        combined = ''
         for html in get_elements_html_by_class('sqs-video-wrapper', webpage):
             data = urllib.parse.unquote(extract_attributes(html).get('data-html'))
-            yield from self._extract_generic_embeds(url, data, note=data)
+            combined += data
+        yield from self._extract_generic_embeds(url, combined)
