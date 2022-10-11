93d99b73dbe20855afe9ad962b1b8e00a19c52b4
doublecouponday <mail@samueljenks.me>
2022-10-12 11:04:05 +1300

    fix: touched __main__ file

--- yt_dlp/extractor/aeonco.py
+++ yt_dlp/extractor/aeonco.py
@@ -1,35 +1,49 @@
 from .common import InfoExtractor
+from .vimeo import VimeoIE
 
 
 class AeonCoIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?yourextractor\.com/watch/(?P<id>[0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?aeon\.co/videos/(?P<id>[^/]+)'
     _TESTS = [{
-        'url': 'https://yourextractor.com/watch/42',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+        'url': 'https://aeon.co/videos/raw-solar-storm-footage-is-the-punk-rock-antidote-to-sleek-james-webb-imagery',
+        'md5': 'e5884d80552c9b6ea8d268a258753362',
         'info_dict': {
-            'id': '42',
+            'id': '1284717',
             'ext': 'mp4',
-            'title': 'Video title goes here',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            # TODO more properties, either as:
-            # * A value
-            # * MD5 checksum; start the string with md5:
-            # * A regular expression; start the string with re:
-            # * Any Python type, e.g. int or float
+            'title': 'Brilliant Noise',
+            'thumbnail': 'https://i.vimeocdn.com/video/21006315-1a1e49da8b07fd908384a982b4ba9ff0268c509a474576ebdf7b1392f4acae3b-d_960',
+            'video_id': 'raw-solar-storm-footage-is-the-punk-rock-antidote-to-sleek-james-webb-imagery',
+            'uploader': 'Semiconductor',
+            'uploader_id': 'semiconductor',
+            'uploader_url': 'https://vimeo.com/semiconductor',
+            'duration': 348
+        }
+    }, {
+        'url': 'https://aeon.co/videos/dazzling-timelapse-shows-how-microbes-spoil-our-food-and-sometimes-enrich-it',
+        'md5': '4e5f3dad9dbda0dbfa2da41a851e631e',
+        'info_dict': {
+            'id': '728595228',
+            'ext': 'mp4',
+            'title': 'Wrought',
+            'thumbnail': 'https://i.vimeocdn.com/video/1484618528-c91452611f9a4e4497735a533da60d45b2fe472deb0c880f0afaab0cd2efb22a-d_1280',
+            'video_id': 'dazzling-timelapse-shows-how-microbes-spoil-our-food-and-sometimes-enrich-it',
+            'uploader': 'Biofilm Productions',
+            'uploader_id': 'user140352216',
+            'uploader_url': 'https://vimeo.com/user140352216',
+            'duration': 1344
         }
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-
-        # TODO more code goes here, for example ...
-        title = self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title')
+        vimeo_id = self._search_regex(r'hosterId\":\w*\"(?P<id>[0-9]+)', webpage, 'id')
+        vimeo_url = f'https://player.vimeo.com/video/{vimeo_id}'
+        referrer_url = VimeoIE._smuggle_referrer(vimeo_url, "https://aeon.co")
 
         return {
-            'id': video_id,
-            'title': title,
-            'description': self._og_search_description(webpage),
-            'uploader': self._search_regex(r'<div[^>]+id="uploader"[^>]*>([^<]+)<', webpage, 'uploader', fatal=False),
-            # TODO more properties (see yt_dlp/extractor/common.py)
+            'video_id': video_id,
+            '_type': 'url_transparent',
+            'url': referrer_url,
+            'ie_key': 'Vimeo',
         }
