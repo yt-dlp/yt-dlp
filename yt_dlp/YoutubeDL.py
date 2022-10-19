812dc3aae1282d971e91f46958dbf8646dfa3cdb
bashonly <bashonly@bashonly.com>
2022-10-19 12:44:42 -0500

    Fix to retain previous `-F -s` behavior

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -649,12 +649,12 @@ class YoutubeDL:
         else:
             self.params['nooverwrites'] = not self.params['overwrites']
 
-        if self.params.get('simulate') is None and any([
+        if self.params.get('simulate') is None and any((
             self.params.get('list_thumbnails'),
             self.params.get('listformats'),
             self.params.get('listsubtitles'),
-        ]):
-            self.params['simulate'] = True
+        )):
+            self.params['simulate'] = 'list_only'
 
         self.params.setdefault('forceprint', {})
         self.params.setdefault('print_to_file', {})
@@ -2649,8 +2649,7 @@ class YoutubeDL:
         # The pre-processors may have modified the formats
         formats = self._get_formats(info_dict)
 
-        list_only = self.params.get('simulate') and (
-            self.params.get('list_thumbnails') or self.params.get('listformats') or self.params.get('listsubtitles'))
+        list_only = self.params.get('simulate') == 'list_only'
         interactive_format_selection = not list_only and self.format_selector == '-'
         if self.params.get('list_thumbnails'):
             self.list_thumbnails(info_dict)
