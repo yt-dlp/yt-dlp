dfd03bc89026f26018358165c0eb7a17f8dae442
Lauren N. Liberda <lauren@selfisekai.rocks>
2022-10-24 18:24:22 +0200

    [polsatgo] support subtitles, client choosing (fixes geo restrictions outside PL)

--- yt_dlp/extractor/polsatgo.py
+++ yt_dlp/extractor/polsatgo.py
@@ -13,22 +13,54 @@ from ..utils import (
 class PolsatGoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?polsat(?:box)?go\.pl/.+/(?P<id>[0-9a-fA-F]+)(?:[/#?]|$)'
     _TESTS = [{
-        'url': 'https://polsatgo.pl/wideo/seriale/swiat-wedlug-kiepskich/5024045/sezon-1/5028300/swiat-wedlug-kiepskich-odcinek-88/4121',
+        'url': 'https://polsatgo.pl/wideo/seriale/swiat-wedlug-kiepskich/5024025/sezon-1/5024197/swiat-wedlug-kiepskich-odcinek-1/4208/ogladaj',
         'info_dict': {
-            'id': '4121',
+            'id': '4208',
             'ext': 'mp4',
-            'title': 'Świat według Kiepskich - Odcinek 88',
+            'title': 'Świat według Kiepskich - Odcinek 1',
             'age_limit': 12,
+        },
+        'params': {
+            'listsubtitles': True,
         },
     }]
 
-    def _extract_formats(self, sources, video_id):
+    _CLIENTS = {
+        'android_pg': {
+            'name': 'android_pg',
+            'domain': 'b2c-mobile.redefine.pl',
+            'client': {
+                'deviceType': 'mobile',
+                'application': 'native',
+                'os': 'android',
+                'build': 10003,
+                'widevine': False,
+                'portal': 'pg',
+                'player': 'cpplayer',
+            },
+        },
+        'web_pg': {
+            'name': 'web_pg',
+            'domain': 'b2c.redefine.pl',
+            'client': {
+                'application': 'firefox',
+                'build': 2170000,
+                'deviceType': 'pc',
+                'os': 'linux',
+                'player': 'html',
+                'portal': 'pg'
+            }
+        }
+    }
+
+    def _extract_formats(self, sources, video_id, client):
         for source in sources or []:
             if not source.get('id'):
                 continue
             url = url_or_none(self._call_api(
                 'drm', video_id, 'getPseudoLicense',
-                {'mediaId': video_id, 'sourceId': source['id']}).get('url'))
+                {'mediaId': video_id, 'sourceId': source['id']},
+                client).get('url'))
             if not url:
                 continue
             yield {
@@ -38,38 +70,44 @@ class PolsatGoIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        media = self._call_api('navigation', video_id, 'prePlayData', {'mediaId': video_id})['mediaItem']
+        client_name = self._configuration_arg('player_client', default='web_pg')
+        media = self._call_api(
+            'navigation', video_id, 'prePlayData', {'mediaId': video_id}, client_name)['mediaItem']
 
         formats = list(self._extract_formats(
-            try_get(media, lambda x: x['playback']['mediaSources']), video_id))
+            try_get(media, lambda x: x['playback']['mediaSources']), video_id, client_name))
+        self._sort_formats(formats)
 
         return {
             'id': video_id,
             'title': media['displayInfo']['title'],
             'formats': formats,
-            'age_limit': int_or_none(media['displayInfo']['ageGroup'])
+            'age_limit': int_or_none(media['displayInfo']['ageGroup']),
+            'subtitles': self.extract_subtitles(video_id, client_name),
         }
 
-    def _call_api(self, endpoint, media_id, method, params):
+    def _get_subtitles(self, video_id, client_name):
+        subtitles = {}
+        for subtitle in self._call_api(
+                'navigation', video_id, 'getMedia', {'mediaId': video_id}, client_name)['subtitles']:
+            subtitles.setdefault(subtitle['name'], []).append({
+                'url': subtitle['src'],
+            })
+        return subtitles
+
+    def _call_api(self, endpoint, media_id, method, params, client_name):
+        client = self._CLIENTS[client_name]
         rand_uuid = str(uuid4())
         res = self._download_json(
-            f'https://b2c-mobile.redefine.pl/rpc/{endpoint}/', media_id,
-            note=f'Downloading {method} JSON metadata',
+            f'https://{client["domain"]}/rpc/{endpoint}/', media_id,
+            note=f'Downloading {method} JSON metadata as {client_name}',
             data=json.dumps({
                 'method': method,
                 'id': '2137',
                 'jsonrpc': '2.0',
                 'params': {
                     **params,
-                    'userAgentData': {
-                        'deviceType': 'mobile',
-                        'application': 'native',
-                        'os': 'android',
-                        'build': 10003,
-                        'widevine': False,
-                        'portal': 'pg',
-                        'player': 'cpplayer',
-                    },
+                    'userAgentData': client['client'],
                     'deviceId': {
                         'type': 'other',
                         'value': rand_uuid,
