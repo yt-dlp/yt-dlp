e8fdb46bdb6faeca07389399d59a1366a1e08342
pukkandan <pukkandan.ytdlp@gmail.com>
2022-10-25 14:38:55 +0530

    Out of scope

--- test/test_download.py
+++ test/test_download.py
@@ -16,7 +16,6 @@ import urllib.error
 
 from test.helper import (
     assertGreaterEqual,
-    assertLessEqual,
     expect_info_dict,
     expect_warnings,
     get_params,
@@ -129,7 +128,7 @@ def generator(test_case, tname):
         params['outtmpl'] = tname + '_' + params['outtmpl']
         if is_playlist and 'playlist' not in test_case:
             params.setdefault('extract_flat', 'in_playlist')
-            params.setdefault('playlistend', max(test_case.get('playlist_mincount', -1), test_case.get('playlist_maxcount', -1)))
+            params.setdefault('playlistend', test_case.get('playlist_mincount'))
             params.setdefault('skip_download', True)
 
         ydl = YoutubeDL(params, auto_init=False)
@@ -193,14 +192,6 @@ def generator(test_case, tname):
                     test_case['playlist_mincount'],
                     'Expected at least %d in playlist %s, but got only %d' % (
                         test_case['playlist_mincount'], test_case['url'],
-                        len(res_dict['entries'])))
-            if 'playlist_maxcount' in test_case:
-                assertLessEqual(
-                    self,
-                    len(res_dict['entries']),
-                    test_case['playlist_maxcount'],
-                    'Expected at most %d in playlist %s, but got %d' % (
-                        test_case['playlist_maxcount'], test_case['url'],
                         len(res_dict['entries'])))
             if 'playlist_count' in test_case:
                 self.assertEqual(
