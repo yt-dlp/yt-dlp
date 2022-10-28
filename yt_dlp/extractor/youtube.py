afbab60ce823a68ef86cb58112b5c45ab5fac65a
coletdjnz <coletdjnz@protonmail.com>
2022-10-28 19:49:34 +1300

    extract microformats unlisted privacy setting

--- yt_dlp/extractor/youtube.py
+++ yt_dlp/extractor/youtube.py
@@ -4765,7 +4765,7 @@ class YoutubeTabBaseInfoExtractor(YoutubeBaseInfoExtractor):
         @param data: response
         """
         sidebar_renderer = self._extract_sidebar_info_renderer(data, 'playlistSidebarPrimaryInfoRenderer') or {}
-        playlist_header_renderer = traverse_obj(data, ('header', 'playlistHeaderRenderer'))
+        playlist_header_renderer = traverse_obj(data, ('header', 'playlistHeaderRenderer')) or {}
         player_header_privacy = playlist_header_renderer.get('privacy')
 
         badges = self._extract_badges(sidebar_renderer)
@@ -4776,6 +4776,9 @@ class YoutubeTabBaseInfoExtractor(YoutubeBaseInfoExtractor):
                 ..., 'privacyForm', 'dropdownFormFieldRenderer', 'dropdown', 'dropdownRenderer', 'entries',
                 lambda _, v: v['privacyDropdownItemRenderer']['isSelected'], 'privacyDropdownItemRenderer', 'icon', 'iconType'),
             get_all=False, expected_type=str)
+
+        microformats_is_unlisted = traverse_obj(
+            data, ('microformat', 'microformatDataRenderer', 'unlisted'), expected_type=bool)
 
         return (
             'public' if (
@@ -4790,7 +4793,8 @@ class YoutubeTabBaseInfoExtractor(YoutubeBaseInfoExtractor):
                 is_unlisted=(
                     self._has_badge(badges, BadgeType.AVAILABILITY_UNLISTED)
                     or player_header_privacy == 'UNLISTED' if player_header_privacy is not None
-                    else privacy_setting_icon == 'PRIVACY_UNLISTED' if privacy_setting_icon is not None else None),
+                    else privacy_setting_icon == 'PRIVACY_UNLISTED' if privacy_setting_icon is not None
+                    else microformats_is_unlisted if microformats_is_unlisted is not None else None),
                 needs_subscription=self._has_badge(badges, BadgeType.AVAILABILITY_SUBSCRIPTION) or None,
                 needs_premium=self._has_badge(badges, BadgeType.AVAILABILITY_PREMIUM) or None,
                 needs_auth=False))
