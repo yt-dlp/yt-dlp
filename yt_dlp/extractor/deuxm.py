95fbae8600c2c72b6f7d4839fd93d63c3f181781
CrankDatSouljaBoy <75489748+CrankDatSouljaBoy@users.noreply.github.com>
2022-10-31 01:43:21 +0100

    [extractor/deuxm] fix hash, regex for thumbnail test

--- yt_dlp/extractor/deuxm.py
+++ yt_dlp/extractor/deuxm.py
@@ -7,12 +7,12 @@ class DeuxMIE(InfoExtractor):
 
     _TESTS = [{
         'url': 'https://2m.ma/fr/replay/single/6351d439b15e1a613b3debe8',
-        'md5': '7ab69a170493c7b7c3febe2ad965c9fd',
+        'md5': '5f761f04c9d686e553b685134dca5d32',
         'info_dict': {
             'id': '6351d439b15e1a613b3debe8',
             'ext': 'mp4',
             'title': 'Grand Angle : Jeudi 20 Octobre 2022',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/21/16663630861663508ea560dc8fbf20c43b3e330ff4.png'
+            'thumbnail': r're:^https?://2msoread-ww.amagi.tv/mediasfiles/videos/images/.*\.png$',
         }
     }, {
         'url': 'https://2m.ma/fr/replay/single/635c0aeab4eec832622356da',
@@ -21,7 +21,6 @@ class DeuxMIE(InfoExtractor):
             'id': '635c0aeab4eec832622356da',
             'ext': 'mp4',
             'title': 'Journal  Amazigh : Vendredi 28 Octobre 2022',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/28/16669764903.png'
         }
     }]
 
@@ -43,13 +42,13 @@ class DeuxMNewsIE(InfoExtractor):
 
     _TESTS = [{
         'url': 'https://2m.ma/fr/news/Kan-Ya-Mkan-d%C3%A9poussi%C3%A8re-l-histoire-du-phare-du-Cap-Beddouza-20221028',
-        'md5': 'f434e2552b1bd1004fc04737b59c03a9',
+        'md5': '43d5e693a53fa0b71e8a5204c7d4542a',
         'info_dict': {
             'id': '635c5d1233b83834e35b282e',
             'ext': 'mp4',
             'title': 'Kan Ya Mkan d\u00e9poussi\u00e8re l\u2019histoire du phare du Cap Beddouza',
             'description': 'md5:99dcf29b82f1d7f2a4acafed1d487527',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/27/1666869608ba-kane-ya-mkane-27-10-2022.png'
+            'thumbnail': r're:^https?://2msoread-ww.amagi.tv/mediasfiles/videos/images/.*\.png$',
         }
     }, {
         'url': 'https://2m.ma/fr/news/Interview-Casablanca-hors-des-sentiers-battus-avec-Abderrahim-KASSOU-Replay--20221017',
@@ -58,8 +57,7 @@ class DeuxMNewsIE(InfoExtractor):
             'id': '634d9e108b70d40bc51a844b',
             'ext': 'mp4',
             'title': 'Interview: Casablanca hors des sentiers battus avec Abderrahim KASSOU (Replay) ',
-            'description': 'md5:3b8e78111de9fcc6ef7f7dd6cff2430c',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/17/1666031205e8b5613cf7fd24cc10a116844b23bfd4.png'
+            'description': 'md5:3b8e78111de9fcc6ef7f7dd6cff2430c'
         }
     }]
 
