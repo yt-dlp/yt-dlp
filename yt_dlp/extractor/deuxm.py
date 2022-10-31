d22ea0bf55989bb10da0ed06ff144c222367a39b
CrankDatSouljaBoy <75489748+CrankDatSouljaBoy@users.noreply.github.com>
2022-10-31 01:09:11 +0100

    [extractor/deuxm] reformat code

--- yt_dlp/extractor/deuxm.py
+++ yt_dlp/extractor/deuxm.py
@@ -1,8 +1,5 @@
 from .common import InfoExtractor
-from ..utils import (
-    url_or_none,
-    traverse_obj
-)
+from ..utils import url_or_none
 
 
 class DeuxMIE(InfoExtractor):
@@ -10,6 +7,7 @@ class DeuxMIE(InfoExtractor):
 
     _TESTS = [{
         'url': 'https://2m.ma/fr/replay/single/6351d439b15e1a613b3debe8',
+        'md5': '7ab69a170493c7b7c3febe2ad965c9fd',
         'info_dict': {
             'id': '6351d439b15e1a613b3debe8',
             'ext': 'mp4',
@@ -18,6 +16,7 @@ class DeuxMIE(InfoExtractor):
         }
     }, {
         'url': 'https://2m.ma/fr/replay/single/635c0aeab4eec832622356da',
+        'md5': 'ad6af2f5e4d5b2ad2194a84b6e890b4c',
         'info_dict': {
             'id': '635c0aeab4eec832622356da',
             'ext': 'mp4',
@@ -28,9 +27,6 @@ class DeuxMIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        info = self._download_json(
-            'https://2m.ma/api/watchDetail/%s' % (video_id),
-            video_id, "Downloading media JSON") or {}
         video = self._download_json(
             f'https://2m.ma/api/watchDetail/{video_id}', video_id)['response']['News']
         return {
@@ -43,40 +39,39 @@ class DeuxMIE(InfoExtractor):
 
 
 class DeuxMNewsIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?2m\.ma/[^/]+/news/(?P<id>(?:.)+)'
+    _VALID_URL = r'https?://(?:www\.)?2m\.ma/(?P<lang>(?:[a-z])+)/news/(?P<id>(?:.)+)'
 
     _TESTS = [{
-        'url': 'https://2m.ma/fr/news/Sahara-la-r%C3%A9solution-2654-de-l-ONU-une-confirmation-de-l-appui-%C3%A0-l-initiative-d-autonomie-Vid%C3%A9o--20221028',
+        'url': 'https://2m.ma/fr/news/Kan-Ya-Mkan-d%C3%A9poussi%C3%A8re-l-histoire-du-phare-du-Cap-Beddouza-20221028',
+        'md5': 'f434e2552b1bd1004fc04737b59c03a9',
         'info_dict': {
-            'id': '635c4aad346a31673b1aaec3',
+            'id': '635c5d1233b83834e35b282e',
             'ext': 'mp4',
-            'title': 'Sahara : la r\u00e9solution 2654 de l\u0027ONU, une confirmation de l\u0027appui \u00e0 l\u0027initiative d\u0027autonomie (Vid\u00e9o)',
-            'description': 'md5:a240fb360871e6ffd37e1fadd813e42c',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/28/16669845857-1.png'
+            'title': 'Kan Ya Mkan d\u00e9poussi\u00e8re l\u2019histoire du phare du Cap Beddouza',
+            'description': 'md5:99dcf29b82f1d7f2a4acafed1d487527',
+            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/27/1666869608ba-kane-ya-mkane-27-10-2022.png'
         }
     }, {
-        'url': 'https://2m.ma/fr/news/La-3%C3%A8me-conf%C3%A9rence-des-MD-Talks-Des-Ambassadeurs-et-des-Consuls-louent-le-potentiel-%C3%A9nerg%C3%A9tique-de-la-r%C3%A9gion-de-Guelmim-Oued-Noun--20221022',
+        'url': 'https://2m.ma/fr/news/Interview-Casablanca-hors-des-sentiers-battus-avec-Abderrahim-KASSOU-Replay--20221017',
+        'md5': '7aca29f02230945ef635eb8290283c0c',
         'info_dict': {
-            'id': '635405760ed0533c940e7e82',
+            'id': '634d9e108b70d40bc51a844b',
             'ext': 'mp4',
-            'title': 'MD Talks: Des diplomates louent le potentiel \u00e9nerg\u00e9tique de la r\u00e9gion de Guelmim-Oued Noun ',
-            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/22/16664504981-Reportage---MD-TALKS.png'
+            'title': 'Interview: Casablanca hors des sentiers battus avec Abderrahim KASSOU (Replay) ',
+            'description': 'md5:3b8e78111de9fcc6ef7f7dd6cff2430c',
+            'thumbnail': 'https://2msoread-ww.amagi.tv/mediasfiles/videos/images/2022/10/17/1666031205e8b5613cf7fd24cc10a116844b23bfd4.png'
         }
     }]
 
     def _real_extract(self, url):
-        lang = self._search_regex(r'https?://(?:www\.)?2m\.ma/(?P<lang>(?:[a-z])+)/.*', url, 'lang')
-        news_id = self._match_id(url)
-        info = self._download_json(
-            'https://2m.ma/api/articlesByUrl?lang=%s&url=/news/%s' % (lang, news_id),
-            news_id, "Downloading media JSON") or {}
-        article = traverse_obj(info, ('response', 'article'))[0]
-        title = article.get('title')
-        thumbnail = url_or_none(article.get('cover'))
+        lang = self._search_regex(self._VALID_URL, url, 'lang')
+        article_name = self._match_id(url)
+        video = self._download_json(
+            f'https://2m.ma/api/articlesByUrl?lang={lang}&url=/news/{article_name}', article_name)['response']['article'][0]
         return {
-            'id': article.get('id'),
-            'title': title,
-            'url': article.get('image')[0],
-            'description': article.get('content'),
-            'thumbnail': thumbnail
+            'id': video.get('id'),
+            'title': video.get('title'),
+            'url': video['image'][0],
+            'description': video.get('content'),
+            'thumbnail': url_or_none(video.get('cover')),
         }
