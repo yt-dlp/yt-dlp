72123f3058bae2101f786e98390c218ffe1d9a17
Erik Slovák <erik.slovak7@gmail.com>
2022-11-03 20:25:16 +0100

    [markiza] Fix extractor for tvnoviny

--- yt_dlp/extractor/markiza.py
+++ yt_dlp/extractor/markiza.py
@@ -1,9 +1,7 @@
-import re
 
 from .common import InfoExtractor
 from ..compat import compat_str
 from ..utils import (
-    orderedSet,
     parse_duration,
     try_get,
 )
@@ -68,55 +66,41 @@ class MarkizaIE(InfoExtractor):
 
 
 class MarkizaPageIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?(?:(?:[^/]+\.)?markiza|tvnoviny)\.sk/(?:[^/]+/)*(?P<id>\d+)_'
+    _VALID_URL = r'https?://(?:www\.)?(?:(?:[^/]+\.)?markiza|tvnoviny)\.sk/(?:[^/]+/)*(?P<id>\d+)-'
     _TESTS = [{
-        'url': 'http://www.markiza.sk/soubiz/zahranicny/1923705_oteckovia-maju-svoj-den-ti-slavni-nie-su-o-nic-menej-rozkosni',
-        'md5': 'ada4e9fad038abeed971843aa028c7b0',
+        'url': 'https://tvnoviny.sk/domace/clanok/141815-byvaly-sportovec-udajne-vyrabal-mast-z-marihuany-sud-mu-vymeral-20-rocny-trest-a-vzal-aj-rodinny-dom',
+        'md5': '74fdfc216f91de4d9aa4780a5c742720',
         'info_dict': {
-            'id': '139355',
+            'id': '141815',
+            'title': 'Bývalý športovec údajne vyrábal masť z marihuany. Súd mu vymeral 20-ročný trest a vzal aj rodinný dom | TVNOVINY.sk',
             'ext': 'mp4',
-            'title': 'Oteckovia 110',
-            'description': 'md5:d41d8cd98f00b204e9800998ecf8427e',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'duration': 2604,
-        },
-        'params': {
-            'skip_download': True,
         },
     }, {
-        'url': 'http://dajto.markiza.sk/filmy-a-serialy/1774695_frajeri-vo-vegas',
-        'only_matching': True,
+        'url': 'https://tvnoviny.sk/domace/clanok/144055-robert-z-kosic-dostal-najnizsi-mozny-trest-za-to-co-spravil-je-to-aj-tak-vela-tvrdia-blizki',
+        'info_dict': {
+            'id': '144055',
+            'title': 'Róbert z Košíc dostal najnižší možný trest. Za to, čo spravil, je to aj tak veľa, tvrdia blízki | TVNOVINY.sk',
+            'ext': 'mp4',
+        },
     }, {
-        'url': 'http://superstar.markiza.sk/aktualne/1923870_to-je-ale-telo-spevacka-ukazala-sexy-postavicku-v-bikinach',
-        'only_matching': True,
-    }, {
-        'url': 'http://hybsa.markiza.sk/aktualne/1923790_uzasna-atmosfera-na-hybsa-v-poprade-superstaristi-si-prve-koncerty-pred-davom-ludi-poriadne-uzili',
-        'only_matching': True,
-    }, {
-        'url': 'http://doma.markiza.sk/filmy/1885250_moja-vysnivana-svadba',
-        'only_matching': True,
-    }, {
-        'url': 'http://www.tvnoviny.sk/domace/1923887_po-smrti-manzela-ju-cakalo-poriadne-prekvapenie',
-        'only_matching': True,
+        'url': 'https://tvnoviny.sk/domace/clanok/338907-preco-sa-mnozia-utoky-tinedzerov-podla-psychologiciek-je-za-tym-rastuca-frustracia',
+        'info_dict': {
+            'id': '338907',
+            'title': 'Prečo sa množia útoky tínedžerov? Podľa psychologičiek je za tým rastúca frustrácia | TVNOVINY.sk',
+            'ext': 'mp4',
+        },
     }]
 
-    @classmethod
-    def suitable(cls, url):
-        return False if MarkizaIE.suitable(url) else super(MarkizaPageIE, cls).suitable(url)
-
     def _real_extract(self, url):
-        playlist_id = self._match_id(url)
-
-        webpage = self._download_webpage(
-            # Downloading for some hosts (e.g. dajto, doma) fails with 500
-            # although everything seems to be OK, so considering 500
-            # status code to be expected.
-            url, playlist_id, expected_status=500)
-
-        entries = [
-            self.url_result('http://videoarchiv.markiza.sk/video/%s' % video_id)
-            for video_id in orderedSet(re.findall(
-                r'(?:initPlayer_|data-entity=["\']|id=["\']player_)(\d+)',
-                webpage))]
-
-        return self.playlist_result(entries, playlist_id)
+        video_id = self._match_id(url)
+        webpage = self._download_webpage(url, video_id)
+        iframe_url = self._html_search_regex(r'\"(https://media.cms.markiza.sk/embed/[\w]*?)(?:\?[\w\W]*?)?\"', webpage, 'iframe url')
+        iframe_website = self._download_webpage(iframe_url, video_id)
+        playlist_url = self._search_regex(r'\"src\":\"(https:\\\/\\\/[\w\W]*?playlist\.m3u8)', iframe_website, 'playlist url')
+        stripped_playlist_url = playlist_url.replace("\\", "")
+        formats = self._extract_m3u8_formats(stripped_playlist_url, video_id, 'mp4')
+        return {
+            'id': video_id,
+            'title': self._og_search_title(webpage),
+            'formats': formats,
+        }
