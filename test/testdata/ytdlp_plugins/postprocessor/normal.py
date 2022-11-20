30cee8bdd0b6853099c227e998b8e29b362e95a0
coletdjnz <coletdjnz@protonmail.com>
2022-11-20 20:59:08 +1300

    Remove comments from test plugins

--- test/testdata/ytdlp_plugins/postprocessor/normal.py
+++ test/testdata/ytdlp_plugins/postprocessor/normal.py
@@ -1,26 +1,21 @@
-# ⚠ Don't use relative imports
 from yt_dlp.postprocessor.common import PostProcessor
 
 
-# ℹ️ See the docstring of yt_dlp.postprocessor.common.PostProcessor
 class NormalPluginPP(PostProcessor):
     def __init__(self, downloader=None, **kwargs):
-        # ⚠ Only kwargs can be passed from the CLI, and all argument values will be string
-        # Also, "downloader", "when" and "key" are reserved names
         super().__init__(downloader)
         self._kwargs = kwargs
 
-    # ℹ️ See docstring of yt_dlp.postprocessor.common.PostProcessor.run
     def run(self, info):
-        if info.get('_type', 'video') != 'video':  # PP was called for playlist
+        if info.get('_type', 'video') != 'video':
             self.to_screen(f'Post-processing playlist {info.get("id")!r} with {self._kwargs}')
-        elif info.get('filepath'):  # PP was called after download (default)
+        elif info.get('filepath'):
             filepath = info.get('filepath')
             self.to_screen(f'Post-processed {filepath!r} with {self._kwargs}')
-        elif info.get('requested_downloads'):  # PP was called after_video
+        elif info.get('requested_downloads'):
             filepaths = [f.get('filepath') for f in info.get('requested_downloads')]
             self.to_screen(f'Post-processed {filepaths!r} with {self._kwargs}')
-        else:  # PP was called before actual download
+        else:
             filepath = info.get('_filename')
             self.to_screen(f'Pre-processed {filepath!r} with {self._kwargs}')
-        return [], info  # return list_of_files_to_delete, info_dict
+        return [], info
