9d52bf65ff38386a70493ce152f0883476b0709b
Elan Ruusamäe <glen@pld-linux.org>
2022-11-22 20:09:57 +0200

    [extractor/kanal2] Add extractor (#5575)
    Authored by: glensc, pukkandan, bashonly

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -820,6 +820,7 @@ from .joj import JojIE
 from .jwplatform import JWPlatformIE
 from .kakao import KakaoIE
 from .kaltura import KalturaIE
+from .kanal2 import Kanal2IE
 from .karaoketv import KaraoketvIE
 from .karrierevideos import KarriereVideosIE
 from .keezmovies import KeezMoviesIE
