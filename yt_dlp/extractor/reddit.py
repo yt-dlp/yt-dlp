d97ffae04ef5c0d979697c71676b6214d82bf3ee
pukkandan <pukkandan.ytdlp@gmail.com>
2022-11-30 11:43:13 +0530

    isort

--- yt_dlp/extractor/reddit.py
+++ yt_dlp/extractor/reddit.py
@@ -4,12 +4,12 @@ from urllib.parse import urlparse
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
-    int_or_none,
     float_or_none,
+    int_or_none,
+    traverse_obj,
     try_get,
     unescapeHTML,
     url_or_none,
-    traverse_obj,
 )
 
 
