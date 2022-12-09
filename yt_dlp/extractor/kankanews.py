3c1e1927878ca3efa964c748df462a8c4eff953d
synthpop123 <2020393267@qq.com>
2022-12-09 14:41:52 +0800

    [extractor/kankanews] Fix suggested changes

--- yt_dlp/extractor/kankanews.py
+++ yt_dlp/extractor/kankanews.py
@@ -2,13 +2,13 @@ import time
 import random
 import string
 import hashlib
+import urllib.parse
 
 from .common import InfoExtractor
-from ..utils import traverse_obj
 
 
 class KankaNewsIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?kankanews\.com/a/[0-9]+\-[0-9]+\-[0-9]+/[0-9]+\.shtml.*'
+    _VALID_URL = r'https?://(?:www\.)?kankanews\.com/a/\d+\-\d+\-\d+/(?P<id>\d+)\.shtml'
     _TESTS = [{
         'url': 'https://www.kankanews.com/a/2022-11-08/00310276054.shtml?appid=1088227',
         'md5': '05e126513c74b1258d657452a6f4eef9',
@@ -22,26 +22,27 @@ class KankaNewsIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        webpage = self._download_webpage(url, None)
-        video_id = self._search_regex(r'omsid="(\d+)"', webpage, 'video_id')
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
+        video_id = self._search_regex(r'omsid\s*=\s*"(\d+)"', webpage, 'video id')
 
-        params = [
-            'nonce=' + str(''.join(random.choices(string.ascii_letters + string.digits, k=8)).lower()),
-            'omsid=' + str(video_id),
-            'platform=pc',
-            'timestamp=' + str(int(time.time())),
-            'version=1.0'
-        ]
-        tmp = '&'.join(params) + '&28c8edde3d61a0411511d3b1866f0636'
-        tmp = hashlib.md5(tmp.encode()).hexdigest()
-        sign = hashlib.md5(tmp.encode()).hexdigest()
-        params.append(('sign' + '=' + sign))
-        meta = self._download_json('https://api-app.kankanews.com/kankan/pc/getvideo?' + '&'.join(params), video_id)
-        meta = traverse_obj(meta, ('result', 'video'))
+        params = {
+            'nonce': ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)),
+            'omsid': video_id,
+            'platform': 'pc',
+            'timestamp': int(time.time()),
+            'version': '1.0',
+        }
+        params['sign'] = hashlib.md5((hashlib.md5((
+            urllib.parse.urlencode(params) + '&28c8edde3d61a0411511d3b1866f0636'
+        ).encode()).hexdigest()).encode()).hexdigest()
+
+        meta = self._download_json('https://api-app.kankanews.com/kankan/pc/getvideo',
+                                   video_id, query=params)['result']['video']
 
         return {
             'id': video_id,
-            'url': meta.get('videourl'),
-            'title': self._search_regex(r'g\.title="(.+?)"', webpage, 'title'),
-            'thumbnail': meta.get('titlepic'),
+            'url': meta['videourl'],
+            'title': self._search_regex(r'g\.title\s*=\s*"([^"]+)"', webpage, 'title'),
+            'thumbnail': meta['titlepic'],
         }
