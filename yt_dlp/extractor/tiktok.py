a46a0f9ca4101e811f06eaf052c5623513c34059
bashonly <bashonly@bashonly.com>
2022-12-19 11:42:31 -0600

    Cleanup

--- yt_dlp/extractor/tiktok.py
+++ yt_dlp/extractor/tiktok.py
@@ -11,6 +11,7 @@ from ..utils import (
     HEADRequest,
     LazyList,
     UnsupportedError,
+    UserNotLive,
     get_element_by_id,
     get_first,
     int_or_none,
@@ -927,28 +928,26 @@ class TikTokLiveIE(InfoExtractor):
         webpage = self._download_webpage(url, uploader, headers={'User-Agent': 'User-Agent:Mozilla/5.0'})
         room_id = self._html_search_regex(r'snssdk\d*://live\?room_id=(\d+)', webpage, 'room ID', default=None)
         if not room_id:
-            raise ExtractorError('The user is not currently live', expected=True)
-        video_js_data = self._download_json(
+            raise UserNotLive(video_id=uploader)
+        live_info = traverse_obj(self._download_json(
             'https://www.tiktok.com/api/live/detail/', room_id, query={
                 'aid': '1988',
                 'roomID': room_id,
-            })
+            }), 'LiveRoomInfo', expected_type=dict, default={})
+
+        if 'status' not in live_info:
+            raise ExtractorError('Unexpected response from TikTok API')
         # status = 2 if live else 4
-        is_live = traverse_obj(video_js_data, ('LiveRoomInfo', 'status'), expected_type=int, default=4) == 2
-        if not is_live:
-            raise ExtractorError('The user is not currently live', expected=True)
-        live_url = traverse_obj(video_js_data, ('LiveRoomInfo', 'liveUrl'), expected_type=url_or_none)
-        if not live_url:
-            raise ExtractorError('No stream URL found')
+        if not int_or_none(live_info['status']) == 2:
+            raise UserNotLive(video_id=uploader)
 
         return {
             'id': room_id,
-            'title': (traverse_obj(video_js_data, ('LiveRoomInfo', 'title'), expected_type=str)
-                      or self._html_search_meta(['og:title', 'twitter:title'], webpage, default='')),
-            'uploader': traverse_obj(video_js_data, ('LiveRoomInfo', 'ownerInfo', 'uniqueId')) or uploader,
-            'uploader_id': traverse_obj(video_js_data, ('LiveRoomInfo', 'ownerInfo', 'id')),
-            'creator': traverse_obj(video_js_data, ('LiveRoomInfo', 'ownerInfo', 'nickname')),
-            'concurrent_view_count': traverse_obj(video_js_data, ('LiveRoomInfo', 'liveRoomStats', 'userCount'), expected_type=int),
-            'formats': self._extract_m3u8_formats(live_url, room_id, 'mp4', live=is_live),
-            'is_live': is_live,
+            'title': live_info.get('title') or self._html_search_meta(['og:title', 'twitter:title'], webpage, default=''),
+            'uploader': uploader,
+            'uploader_id': traverse_obj(live_info, ('ownerInfo', 'id')),
+            'creator': traverse_obj(live_info, ('ownerInfo', 'nickname')),
+            'concurrent_view_count': traverse_obj(live_info, ('liveRoomStats', 'userCount'), expected_type=int),
+            'formats': self._extract_m3u8_formats(live_info['liveUrl'], room_id, 'mp4', live=True),
+            'is_live': True,
         }
