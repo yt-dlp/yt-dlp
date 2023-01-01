e3a5190859807e44eead5011956abe905a970de5
bashonly <bashonly@bashonly.com>
2023-01-01 11:37:16 -0600

    [extractor/iqiyi] Fix `Iq` JS regex

--- yt_dlp/extractor/iqiyi.py
+++ yt_dlp/extractor/iqiyi.py
@@ -527,11 +527,13 @@ class IqIE(InfoExtractor):
         webpack_js_url = self._proto_relative_url(self._search_regex(
             r'<script src="((?:https?)?//stc.iqiyipic.com/_next/static/chunks/webpack-\w+\.js)"', webpage, 'webpack URL'))
         webpack_js = self._download_webpage(webpack_js_url, video_id, note='Downloading webpack JS', errnote='Unable to download webpack JS')
-        webpack_map1, webpack_map2 = [self._parse_json(js_map, video_id, transform_source=js_to_json) for js_map in self._search_regex(
-            r'\(({[^}]*})\[\w+\][^\)]*\)\s*\+\s*["\']\.["\']\s*\+\s*({[^}]*})\[\w+\]\+["\']\.js', webpack_js, 'JS locations', group=(1, 2))]
-        for module_index in reversed(list(webpack_map2.keys())):
+        webpack_map = self._parse_json(self._search_regex(
+            r'["\']\s*\+\s*(\{\s*(?:\d+\s*:\s*["\'][\da-f]+["\']\s*,?\s*)+})\[\w+\]\+["\']\.js', webpack_js, 'JS locations'),
+            video_id, transform_source=js_to_json)
+
+        for module_index in reversed(list(webpack_map.keys())):
             module_js = self._download_webpage(
-                f'https://stc.iqiyipic.com/_next/static/chunks/{webpack_map1.get(module_index, module_index)}.{webpack_map2[module_index]}.js',
+                f'https://stc.iqiyipic.com/_next/static/chunks/{module_index}.{webpack_map[module_index]}.js',
                 video_id, note=f'Downloading #{module_index} module JS', errnote='Unable to download module JS', fatal=False) or ''
             if 'vms request' in module_js:
                 self.cache.store('iq', 'player_js', module_js)
@@ -545,7 +547,7 @@ class IqIE(InfoExtractor):
     def _update_bid_tags(self, webpage, video_id):
         extracted_bid_tags = self._parse_json(
             self._search_regex(
-                r'arguments\[1\][^,]*,\s*function\s*\([^\)]*\)\s*{\s*"use strict";?\s*var \w=({.+}})\s*,\s*\w\s*=\s*{\s*getNewVd',
+                r'function\s*\([^)]*\)\s*\{\s*"use strict";?\s*var \w\s*=\s*(\{\s*\d+\s*:\s*\{\s*nbid\s*:.+}\s*})\s*,\s*\w\s*=\s*\{\s*getNewVd',
                 self._extract_vms_player_js(webpage, video_id), 'video tags', default=''),
             video_id, transform_source=js_to_json, fatal=False)
         if not extracted_bid_tags:
