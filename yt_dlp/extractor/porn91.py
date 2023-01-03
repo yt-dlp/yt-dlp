0dec4d7f79403c45653a65ffe9a992c70b211b45
pmitchell86 <121815598+pmitchell86@users.noreply.github.com>
2023-01-02 16:11:53 -0800

    [91porn] fix title & comment extraction

--- yt_dlp/extractor/porn91.py
+++ yt_dlp/extractor/porn91.py
@@ -1,4 +1,7 @@
 from .common import InfoExtractor
+from ..compat import (
+    compat_urllib_parse_unquote
+)
 from ..utils import (
     parse_duration,
     int_or_none,
@@ -10,17 +13,40 @@ class Porn91IE(InfoExtractor):
     IE_NAME = '91porn'
     _VALID_URL = r'(?:https?://)(?:www\.|)91porn\.com/.+?\?viewkey=(?P<id>[\w\d]+)'
 
-    _TEST = {
+    _TESTS = [{
         'url': 'http://91porn.com/view_video.php?viewkey=7e42283b4f5ab36da134',
-        'md5': '7fcdb5349354f40d41689bd0fa8db05a',
+        'md5': 'd869db281402e0ef4ddef3c38b866f86',
         'info_dict': {
             'id': '7e42283b4f5ab36da134',
             'title': '18岁大一漂亮学妹，水嫩性感，再爽一次！',
             'ext': 'mp4',
             'duration': 431,
+            'comment_count': 94,
             'age_limit': 18,
         }
-    }
+    }, {
+        'url': 'https://91porn.com/view_video.php?viewkey=23a2ab9f50a9bcdc410a',
+        'md5': '4bf9f0f108368a3af6ae7be4973e38d6',
+        'info_dict': {
+            'id': '23a2ab9f50a9bcdc410a',
+            'title': '“老师下面都湿了，快给我”',
+            'ext': 'mp4',
+            'duration': 617,
+            'comment_count': 19,
+            'age_limit': 18,
+        }
+    }, {
+        'url': 'https://91porn.com/view_video.php?viewkey=726186267387ffe1e5e6',
+        'md5': 'b0067d158495566fe3d20bafd89e36d1',
+        'info_dict': {
+            'id': '726186267387ffe1e5e6',
+            'title': '见过卖老婆的，那你见过卖亲闺女的吗？',
+            'ext': 'mp4',
+            'duration': 244,
+            'comment_count': 13,
+            'age_limit': 18,
+        }
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
@@ -33,28 +59,31 @@ class Porn91IE(InfoExtractor):
             raise ExtractorError('91 Porn says: Daily limit 10 videos exceeded', expected=True)
 
         title = self._search_regex(
-            r'<div id="viewvideo-title">([^<]+)</div>', webpage, 'title')
-        title = title.replace('\n', '')
+            r'<title\s?[^>]*>([^<]+)</title>',
+            webpage, 'title')
+        title = title.replace('\n', '').replace('Chinese homemade video', '').strip()
 
         video_link_url = self._search_regex(
-            r'<textarea[^>]+id=["\']fm-video_link[^>]+>([^<]+)</textarea>',
+            r'document\.write\(\s*strencode2\s*\((\s*(?:\"[^\"]+\")|(?:\'[^\']+\'))\s*\)\s*\)\s*;?',
             webpage, 'video link')
-        videopage = self._download_webpage(video_link_url, video_id)
 
-        info_dict = self._parse_html5_media_entries(url, videopage, video_id)[0]
+        video_link_url = compat_urllib_parse_unquote(video_link_url)
+        video_link_url = self._search_regex(
+            r"src=\'([^\']+)\'", video_link_url, 'video link')
 
         duration = parse_duration(self._search_regex(
-            r'时长:\s*</span>\s*(\d+:\d+)', webpage, 'duration', fatal=False))
+            r'时长:\s*<span\s?[^>]*>\s*(\d+:\d+)\s*</span>',
+            webpage, 'duration', fatal=False))
 
         comment_count = int_or_none(self._search_regex(
-            r'留言:\s*</span>\s*(\d+)', webpage, 'comment count', fatal=False))
+            r'留言:\s*<span\s?[^>]*>\s*(\d+)\s*</span>', webpage, 'comment count', fatal=False))
 
-        info_dict.update({
+        return {
             'id': video_id,
+            'url': video_link_url,
+            'ext': 'mp4',
             'title': title,
             'duration': duration,
             'comment_count': comment_count,
-            'age_limit': self._rta_search(webpage),
-        })
-
-        return info_dict
+            'age_limit': 18
+        }
