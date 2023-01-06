4879a2450550b8a2346b7571adfe722a2a319e84
pmitchell86 <121815598+pmitchell86@users.noreply.github.com>
2023-01-06 05:17:20 +0000

    extract ext, description & view_count

--- yt_dlp/extractor/porn91.py
+++ yt_dlp/extractor/porn91.py
@@ -2,8 +2,10 @@ import urllib.parse
 
 from .common import InfoExtractor
 from ..utils import (
+    determine_ext,
     parse_duration,
     int_or_none,
+    parse_iso8601,
     remove_end,
     ExtractorError,
 )
@@ -18,9 +20,12 @@ class Porn91IE(InfoExtractor):
         'info_dict': {
             'id': '7e42283b4f5ab36da134',
             'title': '18岁大一漂亮学妹，水嫩性感，再爽一次！',
+            'description': '想看我拍新的系列都请帮我加精跟5星好评哦！希望大家鼎力支持，谢过了。我再重申，这次是朋友介绍安排的漂亮学生，费用不低，不过胜在年轻听话，水嫩性感，很超值的女生（6分05有91验证）。PS:本人强壮耐久，事业型男，愿意结交江浙沪的漂亮学妹，加Q：2889560495，语音验证性别，欢迎女生约我，或者靠谱男来一起泡美眉。',
             'ext': 'mp4',
             'duration': 431,
+            'timestamp': None,
             'comment_count': int,
+            'view_count': int,
             'age_limit': 18,
         }
     }, {
@@ -29,9 +34,12 @@ class Porn91IE(InfoExtractor):
         'info_dict': {
             'id': '23a2ab9f50a9bcdc410a',
             'title': '“老师下面都湿了，快给我”',
-            'ext': 'mp4',
+            'description': '疫情开放  炮友出圈',
+            'ext': 'm3u8',
             'duration': 617,
+            'timestamp': None,
             'comment_count': int,
+            'view_count': int,
             'age_limit': 18,
         }
     }, {
@@ -40,11 +48,14 @@ class Porn91IE(InfoExtractor):
         'info_dict': {
             'id': '726186267387ffe1e5e6',
             'title': '见过卖老婆的，那你见过卖亲闺女的吗？',
-            'ext': 'mp4',
+            'description': '疫情当下，如何约炮？',
+            'ext': 'm3u8',
             'duration': 244,
+            'timestamp': None,
             'comment_count': int,
+            'view_count': int,
             'age_limit': 18,
-        }
+       }
     }]
 
     def _real_extract(self, url):
@@ -66,18 +77,30 @@ class Porn91IE(InfoExtractor):
         video_link_url = self._search_regex(
             r"src=\'([^\']+)\'", urllib.parse.unquote(video_link_url), 'unquoted video link')
 
+        timestamp = parse_iso8601(self._search_regex(
+            r'<span\s+class=["\']title-yakov["\']>(\d{4}-\d{2}-\d{2})</span>', webpage, 'timestamp', fatal=False))
+
+        description = self._search_regex(
+            r'<span\s+class=["\']more title["\']>([^<]*)<br', webpage, 'description', fatal=False).strip()
+
         duration = parse_duration(self._search_regex(
             r'时长:\s*<span[^>]*>\s*(\d+:\d+)\s*</span>', webpage, 'duration', fatal=False))
 
         comment_count = int_or_none(self._search_regex(
             r'留言:\s*<span[^>]*>\s*(\d+)\s*</span>', webpage, 'comment count', fatal=False))
 
+        view_count = int_or_none(self._search_regex(
+            r'热度:\s*<span[^>]*>\s*(\d+)\s*</span>', webpage, 'comment count', fatal=False))
+
         return {
             'id': video_id,
             'url': video_link_url,
-            'ext': 'mp4',
+            'ext': determine_ext(video_link_url),
             'title': title,
+            'timestamp': timestamp,
+            'description': description,
             'duration': duration,
             'comment_count': comment_count,
-            'age_limit': 18
+            'view_count': view_count,
+            'age_limit': 18,
         }
