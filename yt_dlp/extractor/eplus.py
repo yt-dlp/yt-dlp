e7b8389b21fb80d25407a5f25a3086911a9c54dc
pukkandan <pukkandan.ytdlp@gmail.com>
2023-01-07 01:22:21 +0530

    cleanup

--- yt_dlp/extractor/eplus.py
+++ yt_dlp/extractor/eplus.py
@@ -11,7 +11,7 @@ from ..utils import (
 
 class EplusIbIE(InfoExtractor):
     IE_NAME = 'eplus:inbound'
-    IE_DESC = 'e+ (イープラス). Only works for the link starts with "live.eplus.jp/ex/player?ib="'
+    IE_DESC = 'e+ (イープラス)'
     _VALID_URL = r'https?://live\.eplus\.jp/ex/player\?ib=(?P<id>([A-Za-z\d]|%2B|%2F){86}(%3D){2})'
     _TESTS = [{
         # simple fake url
@@ -36,50 +36,41 @@ class EplusIbIE(InfoExtractor):
         self.to_screen(f'delivery_status = {delivery_status}, archive_mode = {archive_mode}')
 
         if delivery_status == 'PREPARING':
-            raise UserNotLive('This event has not started yet.')
+            raise UserNotLive('This event has not started yet')
         elif delivery_status == 'STARTED':
-            # FFmpeg is unable to update HTTP request headers during download.
+            # FIXME: HTTP request headers need to be updated to continue download
             self.report_warning(
-                'Due to technical limitations, the download will be interrupted after one hour.')
+                'Due to technical limitations, the download will be interrupted after one hour')
             live_status = 'is_live'
         elif delivery_status == 'STOPPED':
-            # check archive status
-            errmsg = 'This event has been ended. '
             if archive_mode == 'ON':  # I didn't see values other than "ON"
-                raise UserNotLive(errmsg + 'The archive has not been generated yet.')
-            else:
-                raise ExtractorError(errmsg + 'No archive for this event.', expected=True)
+                raise UserNotLive(
+                    'This event has ended, but the archive has not been generated yet')
+            raise ExtractorError(
+                'This event has ended and there is no archive for this event', expected=True)
         elif delivery_status == 'WAIT_CONFIRM_ARCHIVED':
-            raise UserNotLive('The archive will be available shortly.')
+            raise UserNotLive(
+                'This event has ended, and the archive will be available shortly')
         elif delivery_status == 'CONFIRMED_ARCHIVE':
-            # The archive may be available.
-            # TODO: how to update HTTP request headers for "HlsFD" during download?
+            # FIXME: HTTP request headers need to be updated to continue download
             self.report_warning(
-                'Due to technical limitations, the download will be interrupted after one hour. You can restart to continue the download.')
+                'Due to technical limitations, the download will be interrupted after one hour. '
+                'You can restart to continue the download')
             live_status = 'was_live'
         else:
-            raise ExtractorError(f'Unknown delivery_status: {delivery_status}', expected=False)
+            raise ExtractorError(f'Unknown delivery_status: {delivery_status}')
 
-        # TODO: if there are multiple playlists, tell me.
         m3u8_playlist_urls = self._html_search_regex(
             r'var listChannels = (.+);\n', webpage, 'listChannels', default=None)
         if not m3u8_playlist_urls:
-            raise ExtractorError(
-                'Could not find the playlist URL. This event may not be accessible.', expected=True)
+            self.raise_no_formats(
+                'Could not find the playlist URL. This event may not be accessible', expected=True)
 
         return {
-            # mandatory metafields
-
             'id': data_json['app_id'],
-            'title': data_json['app_name'],
-            'formats': self._extract_m3u8_formats(m3u8_url=traverse_obj(self._parse_json(
-                m3u8_playlist_urls, video_id), 0), video_id=video_id),
-
-            # optional metafields
-
+            'title': data_json.get('app_name'),
+            'formats': self._extract_m3u8_formats(m3u8_playlist_url, video_id),
             'live_status': live_status,
             'description': traverse_obj(data_json, 'content'),
-            'timestamp': calendar.timegm((datetime.datetime.strptime(
-                traverse_obj(data_json, 'event_datetime'), '%Y%m%d%H%M')
-                - datetime.timedelta(hours=9)).timetuple()),
+            'timestamp': try_call(lambda: unified_timestamp(data_json['event_datetime']) - 32400),
         }
