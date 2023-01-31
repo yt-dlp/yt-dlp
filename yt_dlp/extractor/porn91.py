6a7a5518e466d3e73feccab51138929154450c38
pmitchell86 <121815598+pmitchell86@users.noreply.github.com>
2023-01-31 16:41:30 +0000

    Remove _get_formats_and_subtitle(). It works better when the ext is hard-coded to mp4

--- yt_dlp/extractor/porn91.py
+++ yt_dlp/extractor/porn91.py
@@ -1,7 +1,6 @@
 import urllib.parse
 from .common import InfoExtractor
 from ..utils import (
-    determine_ext,
     int_or_none,
     parse_duration,
     remove_end,
@@ -35,7 +34,7 @@ class Porn91IE(InfoExtractor):
             'id': '7ef0cf3d362c699ab91c',
             'title': '真实空乘，冲上云霄第二部',
             'description': 'md5:618bf9652cafcc66cd277bd96789baea',
-            'ext': 'm3u8',
+            'ext': 'mp4',
             'duration': 248,
             'upload_date': '20221119',
             'comment_count': int,
@@ -64,14 +63,11 @@ class Porn91IE(InfoExtractor):
         video_link_url = self._search_regex(
             r'src=["\']([^"\']+)["\']', urllib.parse.unquote(video_link_url), 'unquoted video link')
 
-        formats, subtitles = self._get_formats_and_subtitle(video_link_url, video_id)
-
         return {
             'id': video_id,
             'url': video_link_url,
             'title': remove_end(self._html_extract_title(webpage).replace('\n', ''), 'Chinese homemade video').strip(),
-            'formats': formats,
-            'subtitles': subtitles,
+            'ext': 'mp4',
             'upload_date': unified_strdate(self._search_regex(
                 r'<span\s+class=["\']title-yakov["\']>(\d{4}-\d{2}-\d{2})</span>', webpage, 'upload_date', fatal=False)),
             'description': self._html_search_regex(
@@ -84,14 +80,3 @@ class Porn91IE(InfoExtractor):
                 r'热度:\s*<span[^>]*>\s*(\d+)\s*</span>', webpage, 'view count', fatal=False)),
             'age_limit': 18,
         }
-
-    def _get_formats_and_subtitle(self, video_link_url, video_id):
-        formats, subtitles = [], {}
-        ext = determine_ext(video_link_url)
-        if ext == 'm3u8':
-            fmts, subs = self._extract_m3u8_formats_and_subtitles(video_link_url, video_id)
-            formats.extend(fmts)
-            self._merge_subtitles(subs, target=subtitles)
-        else:
-            formats.append({'url': video_link_url, 'ext': ext})
-        return formats, subtitles
