7f122d0356d88fdc05467a0ff2813e6951b431b3
HobbyistDev <tesutonihon4@gmail.com>
2023-02-14 17:43:27 +0900

    change episode_json -> episode, initial_state_json -> initial_state

--- yt_dlp/extractor/viu.py
+++ yt_dlp/extractor/viu.py
@@ -498,9 +498,9 @@ class ViuOTTNewIE(ViuOTTNewBaseIE):
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
-        episode_json = traverse_obj(list(filter(
+        episode = traverse_obj(list(filter(
             lambda x: x.get('@type') in ('TVEpisode', 'Movie'), self._yield_json_ld(webpage, display_id))), 0) or {}
-        initial_state_json = self._search_json(
+        initial_state = self._search_json(
             r'window\.__INITIAL_STATE__\s*=', webpage, 'initial state',
             display_id)['content']['clipDetails']
         video_data = self._download_json(
@@ -508,7 +508,7 @@ class ViuOTTNewIE(ViuOTTNewBaseIE):
             headers={'Authorization': ViuOTTNewBaseIE._TOKEN, **self._HEADERS, 'ccode': 'ID'})
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(video_data['playUrl'], display_id)
 
-        for key, url in initial_state_json.items():
+        for key, url in initial_state.items():
             lang, ext = self._search_regex(
                 r'^subtitle_(?P<lang>[\w-]+)_(?P<ext>\w+)$', key, 'subtitle metadata',
                 default=(None, None), group=('lang', 'ext'))
@@ -521,22 +521,22 @@ class ViuOTTNewIE(ViuOTTNewBaseIE):
                 if ext == 'vtt':
                     subtitles[lang].append({
                         'ext': 'srt',
-                        'url': f'{remove_end(initial_state_json[key], "vtt")}srt',
+                        'url': f'{remove_end(initial_state[key], "vtt")}srt',
                     })
 
         return {
             'id': display_id,
-            'title': (traverse_obj(initial_state_json, 'title', 'display_title')
-                      or episode_json.get('name')),
-            'description': initial_state_json.get('description') or episode_json.get('description'),
-            'duration': initial_state_json.get('duration'),
-            'thumbnail': traverse_obj(episode_json, ('image', 'url')),
-            'timestamp': unified_timestamp(episode_json.get('dateCreated')),
+            'title': (traverse_obj(initial_state, 'title', 'display_title')
+                      or episode.get('name')),
+            'description': initial_state.get('description') or episode.get('description'),
+            'duration': initial_state.get('duration'),
+            'thumbnail': traverse_obj(episode, ('image', 'url')),
+            'timestamp': unified_timestamp(episode.get('dateCreated')),
             'formats': formats,
             'subtitles': subtitles,
-            'episode_number': (traverse_obj(initial_state_json, 'episode_no', 'episodeno', expected_type=int_or_none)
-                               or int_or_none(episode_json.get('episodeNumber'))),
-            'cast': traverse_obj(episode_json, ('actor', ..., 'name'), default=None),
+            'episode_number': (traverse_obj(initial_state, 'episode_no', 'episodeno', expected_type=int_or_none)
+                               or int_or_none(episode.get('episodeNumber'))),
+            'cast': traverse_obj(episode, ('actor', ..., 'name'), default=None),
             'age_limit': self._AGE_RATINGS_MAPPER.get(
-                initial_state_json.get('internal_age_rating'))
+                initial_state.get('internal_age_rating'))
         }
