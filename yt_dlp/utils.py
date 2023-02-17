b25d6cb96337d479bdcb41768356da414c3aa835
Alex Ionescu <aaionescu@protonmail.com>
2023-02-17 04:29:32 +0100

    [utils] Fix race condition in `make_dir` (#6089)
    Authored by: aionescu

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -5370,8 +5370,8 @@ def random_uuidv4():
 def make_dir(path, to_screen=None):
     try:
         dn = os.path.dirname(path)
-        if dn and not os.path.exists(dn):
-            os.makedirs(dn)
+        if dn:
+            os.makedirs(dn, exist_ok=True)
         return True
     except OSError as err:
         if callable(to_screen) is not None:
