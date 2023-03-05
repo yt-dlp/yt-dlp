4f1c02db6dccded21e98c34114919a538c9d5981
NewUserHa <32261870+NewUserHa@users.noreply.github.com>
2023-03-06 01:31:21 +0800

    Update YoutubeDL.py

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -3472,6 +3472,10 @@ class YoutubeDL:
         if self.params.get('keepvideo', False):
             for f in files_to_delete:
                 infodict['__files_to_move'].setdefault(f, '')
+        elif self.params.get('keepsubs', False):
+            for f in files_to_delete:
+                if f.endswith('.vtt'):
+                    infodict['__files_to_move'].setdefault(f, '')
         else:
             self._delete_downloaded_files(
                 *files_to_delete, info=infodict, msg='Deleting original file %s (pass -k to keep)')
