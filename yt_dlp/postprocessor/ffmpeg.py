e84161a5dfecefc540f7a0103def6b3e49b25d8c
bashonly <bashonly@bashonly.com>
2023-03-06 15:38:26 -0600

    Improve implementation

--- yt_dlp/postprocessor/ffmpeg.py
+++ yt_dlp/postprocessor/ffmpeg.py
@@ -302,10 +302,10 @@ class FFmpegPostProcessor(PostProcessor):
             None)
         return num, len(streams)
 
-    def _fixup_chapters(self, chapters, filepath):
-        if chapters and chapters[-1]['end_time'] is None:
-            chapters[-1]['end_time'] = self._get_real_video_duration(filepath)
-        return chapters
+    def _fixup_chapters(self, info):
+        last_chapter = traverse_obj(info, ('chapters', -1, {dict})) or {}
+        if 'end_time' in last_chapter and last_chapter['end_time'] is None:
+            info['chapters'][-1]['end_time'] = self._get_real_video_duration(info['filepath'])
 
     def _get_real_video_duration(self, filepath, fatal=True):
         try:
@@ -683,12 +683,12 @@ class FFmpegMetadataPP(FFmpegPostProcessor):
 
     @PostProcessor._restrict_to(images=False)
     def run(self, info):
+        self._fixup_chapters(info)
         filename, metadata_filename = info['filepath'], None
         files_to_delete, options = [], []
         if self._add_chapters and info.get('chapters'):
             metadata_filename = replace_extension(filename, 'meta')
-            options.extend(self._get_chapter_opts(
-                self._fixup_chapters(info['chapters'], filename), metadata_filename))
+            options.extend(self._get_chapter_opts(info['chapters'], metadata_filename))
             files_to_delete.append(metadata_filename)
         if self._add_metadata:
             options.extend(self._get_metadata_opts(info))
@@ -1046,12 +1046,13 @@ class FFmpegSplitChaptersPP(FFmpegPostProcessor):
 
     @PostProcessor._restrict_to(images=False)
     def run(self, info):
-        if not info.get('chapters'):
+        self._fixup_chapters(info)
+        chapters = info.get('chapters') or []
+        if not chapters:
             self.to_screen('Chapter information is unavailable')
             return [], info
 
         in_file = info['filepath']
-        chapters = self._fixup_chapters(info['chapters'], in_file)
         if self._force_keyframes and len(chapters) > 1:
             in_file = self.force_keyframes(in_file, (c['start_time'] for c in chapters))
         self.to_screen('Splitting video by chapters; %d chapters found' % len(chapters))
