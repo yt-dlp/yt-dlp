cbf6377efd96b89bc77d951dc87f30dc7947de6f
pukkandan <pukkandan.ytdlp@gmail.com>
2023-03-10 13:19:17 +0530

    Apply suggestions from code review

--- yt_dlp/extractor/cbc.py
+++ yt_dlp/extractor/cbc.py
@@ -202,8 +202,7 @@ class CBCPlayerIE(InfoExtractor):
 
 class CBCGemIE(InfoExtractor):
     IE_NAME = 'gem.cbc.ca'
-    # /media/ is an old URL style, new URLs don't have that, see #6395
-    _VALID_URL = r'https?://gem\.cbc\.ca/(?:media/|)(?P<id>[0-9a-z-]+/s[0-9]+[a-z][0-9]+)'
+    _VALID_URL = r'https?://gem\.cbc\.ca/(?:media/)?(?P<id>[0-9a-z-]+/s[0-9]+[a-z][0-9]+)'
     _TESTS = [{
         # This is a normal, public, TV show video
         'url': 'https://gem.cbc.ca/media/schitts-creek/s06e01',
@@ -247,8 +246,6 @@ class CBCGemIE(InfoExtractor):
         'params': {'format': 'bv'},
         'skip': 'Geo-restricted to Canada',
     }, {
-        # New URL format without /media/
-        # https://github.com/yt-dlp/yt-dlp/issues/6395
         'url': 'https://gem.cbc.ca/nadiyas-family-favourites/s01e01',
         'only_matching': True,
     }]
