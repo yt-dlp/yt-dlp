7f3c03fa22a2630ab76687a06ef5b12702e44dd8
Marcel <flashdagger@googlemail.com>
2023-03-10 22:20:56 +0100

    [bibeltv] apply suggested changes

--- yt_dlp/extractor/bibeltv.py
+++ yt_dlp/extractor/bibeltv.py
@@ -1,19 +1,22 @@
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
-    LazyList,
     clean_html,
     determine_ext,
+    format_field,
+    int_or_none,
     js_to_json,
+    orderedSet,
     parse_iso8601,
-    try_get
+    traverse_obj,
+    try_get,
+    url_or_none,
 )
 
 
 class BibelTVIE(InfoExtractor):
     IE_DESC = 'BibelTV'
     _VALID_URL = r'https?://(?:www\.)?bibeltv\.de/mediathek/videos/(?:crn/)?(?P<id>\d+)'
-    _RETURN_TYPE = 'video'
 
     _GEO_COUNTRIES = ['AT', 'CH', 'DE']
     _GEO_BYPASS = False
@@ -50,8 +53,9 @@ class BibelTVIE(InfoExtractor):
     def _extract_formats_and_subtitles(self, data, crn_id, *, is_live=False):
         formats = []
         subtitles = {}
-        for media in data:
-            media_url = media['src']
+        for media_url in traverse_obj(data, (..., 'src', {url_or_none})):
+            if not media_url:
+                continue
             media_ext = determine_ext(media_url)
             if media_ext == 'm3u8':
                 m3u8_formats, m3u8_subs = self._extract_m3u8_formats_and_subtitles(
@@ -63,73 +67,68 @@ class BibelTVIE(InfoExtractor):
                 formats.extend(mpd_formats)
                 subtitles.update(mpd_subs)
             elif media_ext == 'mp4':
-                formats.append({'url': media['src']})
+                formats.append({'url': media_url})
             else:
                 self.report_warning(f'Unknown format {media_ext!r}')
 
         return formats, subtitles
 
     @staticmethod
-    def _extract_url_info(data):
-        crn_id = data['crn']
-
+    def _extract_base_info(data):
         return {
-            '_type': 'url',
-            'id': crn_id,
-            'url': format_field(data, 'slug', 'https://www.bibeltv.de/mediathek/videos/%s'),
+            'id': data['crn'],
             'title': data.get('title'),
             'description': data.get('description'),
-            'duration': try_get(data, lambda x: x['duration'] / 1000),
+            'duration': int_or_none(data.get('duration'), scale=1000),
             'timestamp': parse_iso8601(data.get('schedulingStart')),
-            'thumbnails': [{'url': url} for url in {image['url'] for image in data.get('images', ())}],
+            'thumbnails': orderedSet(
+                traverse_obj(data, ('images', ..., {'url': ('url', {url_or_none})}))),
             'season_number': data.get('seasonNumber'),
             'episode_number': data.get('episodeNumber'),
         }
 
-    def _extract_video(self, data):
+    def _extract_url_info(self, data):
+        return {
+            '_type': 'url',
+            'url': format_field(data, 'slug', 'https://www.bibeltv.de/mediathek/videos/%s'),
+            **self._extract_base_info(data),
+        }
+
+    def _extract_video_info(self, data):
         video_id = data['id']
         crn_id = data['crn']
 
         json_data = self._download_json(
-            f'{self.API_URL}/video/{video_id}', crn_id, headers={'Authorization': self.AUTH_TOKEN}
-        )
+            f'{self.API_URL}/video/{video_id}', crn_id, headers={'Authorization': self.AUTH_TOKEN})
         if json_data.get('status') != 'success':
             raise ExtractorError('Failed to load JSON metadata')
 
         formats, subtitles = self._extract_formats_and_subtitles(
-            try_get(json_data, lambda x: x['video']['videoUrls'], list) or [], crn_id)
+            traverse_obj(json_data, ('video', 'videoUrls', ...)), crn_id)
 
         return {
-            'id': crn_id,
-            'title': data.get('title'),
-            'description': data.get('description'),
-            'duration': try_get(data, lambda x: x['duration'] / 1000),
-            'timestamp': parse_iso8601(data.get('schedulingStart')),
-            'thumbnails': [{'url': url} for url in {image['url'] for image in data.get('images', ())}],
+            '_type': 'video',
+            **self._extract_base_info(data),
             'formats': formats,
             'subtitles': subtitles,
-            'season_number': data.get('seasonNumber'),
-            'episode_number': data.get('episodeNumber'),
             'view_count': data.get('viewCount'),
             'like_count': data.get('likeCount'),
         }
 
     def _real_extract(self, url):
         crn_id = self._match_id(url)
-        webpage = self._download_webpage(url, crn_id)
-        nextjs_data = self._search_nextjs_data(webpage, crn_id)
-        video_data = try_get(nextjs_data,
-                             lambda x: x['props']['pageProps']['videoPageData']['videos'][0], dict)
+        video_data = traverse_obj(
+            self._search_nextjs_data(self._download_webpage(url, crn_id), crn_id),
+            ('props', 'pageProps', 'videoPageData', 'videos', 0, {dict}))
         if not video_data:
             raise ExtractorError('Missing video data.')
 
-        return self._extract_video(video_data)
+        return self._extract_video_info(video_data)
 
 
 class BibelTvSerienIE(BibelTVIE):
     IE_DESC = 'BibelTV'
     _VALID_URL = r'https?://(?:www\.)?bibeltv\.de/mediathek/serien/(?:crn/)?(?P<id>\d+)'
-    _RETURN_TYPE = 'playlist'
 
     IE_NAME = 'bibeltv:serien'
 
@@ -147,18 +146,17 @@ class BibelTvSerienIE(BibelTVIE):
         crn_id = self._match_id(url)
         webpage = self._download_webpage(url, crn_id)
         nextjs_data = self._search_nextjs_data(webpage, crn_id)
-        serie_data = try_get(nextjs_data,
-                             lambda x: x['props']['pageProps']['seriePageData'], dict)
-        if not serie_data:
+        series_data = try_get(nextjs_data, lambda x: x['props']['pageProps']['seriePageData'], dict)
+        if not series_data:
             raise ExtractorError('Missing series data.')
 
-        video_data = serie_data.get('videos', ())
+        video_data = traverse_obj(series_data, ('videos', ..., {dict}))
 
         return self.playlist_result(
-            LazyList(map(self._extract_url_info, video_data)),
+            list(map(self._extract_url_info, video_data)),
             playlist_id=crn_id,
-            playlist_title=serie_data.get('title'),
-            playlist_description=clean_html(serie_data.get('description')),
+            playlist_title=series_data.get('title'),
+            playlist_description=clean_html(series_data.get('description')),
             playlist_count=len(video_data),
         )
 
@@ -192,7 +190,7 @@ class BibelTvLiveIE(BibelTVIE):
             raise ExtractorError(f'Missing livestream data for {stream_id!r}')
 
         formats, subtitles = self._extract_formats_and_subtitles(
-            stream_data.get('src', ()), stream_id, is_live=True)
+            traverse_obj(stream_data, ('src', ...)), stream_id, is_live=True)
 
         return {
             'id': stream_id,
