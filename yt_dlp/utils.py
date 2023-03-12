92fcc679b116b32a5d2c64e0c9463542abe46c0c
observeroftime02 <43228249+observeroftime02@users.noreply.github.com>
2023-03-11 18:47:17 -0800

    [utils] Update Chrome Version
    Added latest Google Chrome version to the list of Chrome Versions used for useragent strings.

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -75,7 +75,6 @@ compiled_regex_type = type(re.compile(''))
 def random_user_agent():
     _USER_AGENT_TPL = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
     _CHROME_VERSIONS = (
-        '103.0.5060.131',
         '103.0.5060.132',
         '103.0.5060.133',
         '103.0.5060.134',
@@ -113,7 +112,8 @@ def random_user_agent():
         '106.0.5197.0',
         '107.0.5304.107',
         '107.0.5304.123',
-        '108.0.5359.125',  # Current chrome version on Win10x64
+        '108.0.5359.125',
+        '111.0.5563.64',  # Current chrome version on Win10x64
     )
     return _USER_AGENT_TPL % random.choice(_CHROME_VERSIONS)
 
