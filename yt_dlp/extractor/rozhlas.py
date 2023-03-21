cd7093b012c555c191fbd50a2e9c5276922d6649
bashonly <bashonly@bashonly.com>
2023-03-21 14:25:33 -0500

    Apply suggestion from code review

--- yt_dlp/extractor/rozhlas.py
+++ yt_dlp/extractor/rozhlas.py
@@ -170,20 +170,17 @@ class RozhlasVltavaIE(InfoExtractor):
     def _extract_video(self, entry):
         formats = []
         audio_id = entry['meta']['ga']['contentId']
-        for audio in traverse_obj(entry, ('audioLinks', ..., {dict})):
-            audio_url = audio.get('url')
-            if not url_or_none(audio_url):
-                continue
+        for audio in traverse_obj(entry, ('audioLinks', lambda _, v: url_or_none(v['url']))):
             ext = audio.get('variant')
             if ext == 'dash':
                 formats.extend(self._extract_mpd_formats(
-                    audio_url, audio_id, mpd_id=ext, fatal=False))
+                    audio['url'], audio_id, mpd_id=ext, fatal=False))
             elif ext == 'hls':
                 formats.extend(self._extract_m3u8_formats(
-                    audio_url, audio_id, 'm4a', m3u8_id=ext, fatal=False))
+                    audio['url'], audio_id, 'm4a', m3u8_id=ext, fatal=False))
             else:
                 formats.append({
-                    'url': audio_url,
+                    'url': audio['url'],
                     'ext': ext,
                     'format_id': ext,
                     'abr': int_or_none(audio.get('bitrate')),
