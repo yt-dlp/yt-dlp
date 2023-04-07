f0affc6fe5458efb016cbf8097ea53e3debcd4ba
sian1468 <58017832+sian1468@users.noreply.github.com>
2023-04-07 16:19:41 +0700

    [extractor/line] Remove live.line.me

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -943,10 +943,6 @@ from .limelight import (
     LimelightChannelIE,
     LimelightChannelListIE,
 )
-from .line import (
-    LineLiveIE,
-    LineLiveChannelIE,
-)
 from .linkedin import (
     LinkedInIE,
     LinkedInLearningIE,
