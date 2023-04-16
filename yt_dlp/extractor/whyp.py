2da4a7feae5fe1ab127b4d6a1b0096b880b04aa8
bashonly <88596187+bashonly@users.noreply.github.com>
2023-04-16 16:56:21 +0000

    Cleanup

--- yt_dlp/extractor/whyp.py
+++ yt_dlp/extractor/whyp.py
@@ -1,31 +1,36 @@
 from .common import InfoExtractor
-from ..utils import traverse_obj, float_or_none, str_or_none, url_or_none
+from ..utils import (
+    float_or_none,
+    str_or_none,
+    traverse_obj,
+    url_or_none,
+)
 
 
 class WhypIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?whyp\.it/tracks/(?P<id>\d+)'
-    _TEST = {
+    _TESTS = [{
         'url': 'https://www.whyp.it/tracks/18337/home-page-example-track-b4kq7',
         'md5': 'c1187b42ebf8605284e3dc92aeb33d16',
         'info_dict': {
             'url': 'https://cdn.whyp.it/50eb17cc-e9ff-4e18-b89b-dc9206a95cb1.mp3',
             'id': '18337',
             'title': 'Home Page Example Track',
-            'description': 'Just an example track for the home page!\n\nCredit: https://file-examples.com/index.php/sample-audio-files/sample-mp3-download/',
+            'description': 'md5:bd758000fb93f3159339c852b5b9133c',
             'ext': 'mp3',
             'duration': 52.82,
             'uploader': 'Brad',
             'uploader_id': '1',
             'thumbnail': 'https://cdn.whyp.it/a537bb36-3373-4c61-96c8-27fc1b2f427a.jpg',
-            'vcodec': 'none',
-        }
-    }
+        },
+    }, {
+        'url': 'https://www.whyp.it/tracks/18337',
+        'only_matching': True,
+    }]
 
     def _real_extract(self, url):
         unique_id = self._match_id(url)
-
         webpage = self._download_webpage(url, unique_id)
-
         data = self._search_nuxt_data(webpage, unique_id)['rawTrack']
 
         return {
