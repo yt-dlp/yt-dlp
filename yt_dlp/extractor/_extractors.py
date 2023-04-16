2c566ed14101673c651c08c306c30fa5b4010b85
CoryTibbettsDev <70112527+CoryTibbettsDev@users.noreply.github.com>
2023-04-16 17:26:37 +0000

    [extractor/whyp] Add extractor (#6803)
    Authored by: CoryTibbettsDev

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2295,6 +2295,7 @@ from .weibo import (
     WeiboMobileIE
 )
 from .weiqitv import WeiqiTVIE
+from .whyp import WhypIE
 from .wikimedia import WikimediaIE
 from .willow import WillowIE
 from .wimtv import WimTVIE
