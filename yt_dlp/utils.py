a5e7b43947242418bd1d2d207541fb702bd6fe40
CapacitorSet <capacitorset@gmail.com>
2023-04-23 19:53:22 +0200

    [utils] `FormatSorter`: add exact_match type

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -6444,6 +6444,8 @@ class FormatSorter:
             in_list = self._get_field_setting(field, 'in_list')
             not_in_list = self._get_field_setting(field, 'not_in_list')
             value = 0 if ((in_list is None or value in in_list) and (not_in_list is None or value not in not_in_list)) else -1
+        elif type == 'exact_match':
+            value = int(value == limit)
         elif type == 'ordered':
             value = self._resolve_field_value(field, value, True)
 
@@ -6461,7 +6463,7 @@ class FormatSorter:
                 else (-1, value, 0))
 
     def _calculate_field_preference(self, format, field):
-        type = self._get_field_setting(field, 'type')  # extractor, boolean, ordered, field, multiple
+        type = self._get_field_setting(field, 'type')  # extractor, boolean, ordered, field, multiple, exact_match
         get_value = lambda f: format.get(self._get_field_setting(f, 'field'))
         if type == 'multiple':
             type = 'field'  # Only 'field' is allowed in multiple for now
