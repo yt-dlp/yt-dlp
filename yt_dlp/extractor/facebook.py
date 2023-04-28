4b9bbe6d720551bcf8e0524ca257e481176f5f3b
ringus1 <ringus1@users.noreply.github.com>
2023-04-28 12:10:14 +0200

    Formatting

--- yt_dlp/extractor/facebook.py
+++ yt_dlp/extractor/facebook.py
@@ -391,11 +391,10 @@ class FacebookIE(InfoExtractor):
             title = get_first(media, ('title', 'text'))
             description = get_first(media, ('creation_story', 'comet_sections', 'message', 'story', 'message', 'text'))
             uploader_data = (
-                get_first(post, (..., ..., lambda k, v: k == "owner" and v.get("name")), expected_type=dict)
+                get_first(post, (..., ..., lambda k, v: k == 'owner' and v.get('name')), expected_type=dict)
                 or get_first(media, 'owner')
                 or get_first(post, ('node', 'actors', ...))
-                or {}
-            )
+                or {})
 
             page_title = title or self._html_search_regex((
                 r'<h2\s+[^>]*class="uiHeaderTitle"[^>]*>(?P<content>[^<]*)</h2>',
@@ -422,19 +421,10 @@ class FacebookIE(InfoExtractor):
                 thumbnail = None
             view_count = parse_count(
                 self._search_regex(
-                    (
-                        r'\bviewCount\s*:\s*["\']([\d,.]+)',
-                        r'video_view_count":\s*(\d+)',
-                    ),
-                    webpage,
-                    'view count',
-                    default=None
-                )
-            )
+                    (r'\bviewCount\s*:\s*["\']([\d,.]+)', r'video_view_count":\s*(\d+)',), webpage, 'view count', default=None))
             live_view_count = (
                 get_first(post, ('video', 'liveViewerCount'), expected_type=int)
-                or get_first(post, (..., ..., "attachments", ..., "media", "liveViewerCount"), expected_type=int)
-            )
+                or get_first(post, (..., ..., 'attachments', ..., 'media', 'liveViewerCount'), expected_type=int))
             info_dict = {
                 'description': description,
                 'uploader': uploader,
