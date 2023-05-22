b86c96d29b06c7b48555becdc800eacd68015ba4
pukkandan <pukkandan.ytdlp@gmail.com>
2023-05-22 16:56:54 +0530

    Rewrite

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -2724,24 +2724,23 @@ class YoutubeDL:
             return info_dict
 
         format_selector = self.format_selector
-        if format_selector is None:
-            req_format = self._default_format_spec(info_dict, download=download)
-            self.write_debug('Default format spec: %s' % req_format)
-            format_selector = self.build_format_selector(req_format)
-
         while True:
             if interactive_format_selection:
-                req_format = input(
-                    self._format_screen('\nEnter format selector (press ENTER for default, or CTRL+C to quit): ', self.Styles.EMPHASIS))
-
-                if req_format.strip() == '':
-                    req_format = self._default_format_spec(info_dict, download=download)
-                    self.write_debug('Nothing selected, using default format spec: %s' % req_format)
+                req_format = input(' '.join((
+                    self._format_screen('\nEnter format selector', self.Styles.EMPHASIS),
+                    '(Press ENTER for default, or Ctrl+C to quit)',
+                    self._format_screen(': ', self.Styles.EMPHASIS))
+                ))
                 try:
-                    format_selector = self.build_format_selector(req_format)
+                    format_selector = self.build_format_selector(req_format) if req_format else None
                 except SyntaxError as err:
                     self.report_error(err, tb=False, is_error=False)
                     continue
+
+            if format_selector is None:
+                req_format = self._default_format_spec(info_dict, download=download)
+                self.write_debug(f'Default format spec: {req_format}')
+                format_selector = self.build_format_selector(req_format)
 
             formats_to_download = list(format_selector({
                 'formats': formats,
