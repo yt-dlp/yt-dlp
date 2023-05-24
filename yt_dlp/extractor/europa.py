759a7875d58c00c61613efb2975b51151abfe564
HobbyistDev <tesutonihon4@gmail.com>
2023-05-24 17:00:39 +0900

    [europarl] remove broken test

--- yt_dlp/extractor/europa.py
+++ yt_dlp/extractor/europa.py
@@ -109,33 +109,6 @@ class EuroParlWebstreamIE(InfoExtractor):
             'skip_download': True,
         }
     }, {
-        'url': 'https://multimedia.europarl.europa.eu/pl/webstreaming/eu-cop27-un-climate-change-conference-in-sharm-el-sheikh-egypt-ep-delegation-meets-with-ngo-represen_20221114-1600-SPECIAL-OTHER',
-        'info_dict': {
-            'id': 'a8428de8-b9cd-6a2e-11e4-3805d9c9ff5c',
-            'ext': 'mp4',
-            'release_timestamp': 1668434400,
-            'release_date': '20221114',
-            'title': 'md5:d3550280c33cc70e0678652e3d52c028',
-        },
-        'params': {
-            'skip_download': True,
-        },
-        'skip': 'Webpage Not Found'
-    }, {
-        # embed webpage
-        'url': 'https://webstreaming.europarl.europa.eu/ep/embed/embed.html?event=20220914-0900-PLENARY&language=en&autoplay=true&logo=true',
-        'info_dict': {
-            'id': 'bcaa1db4-76ef-7e06-8da7-839bd0ad1dbe',
-            'ext': 'mp4',
-            'title': 'Plenary session',
-            'release_date': '20220914',
-            'release_timestamp': 1663137900,
-        },
-        'params': {
-            'skip_download': True,
-        },
-        'skip': 'Can\'t redirect without using js'
-    }, {
         # live webstream
         'url': 'https://multimedia.europarl.europa.eu/en/webstreaming/euroscola_20221115-1000-SPECIAL-EUROSCOLA',
         'info_dict': {
