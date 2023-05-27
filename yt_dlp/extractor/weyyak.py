2c4aac7469f9fd682ca019eeb816ca25f49747b0
bashonly <88596187+bashonly@users.noreply.github.com>
2023-05-27 18:18:02 +0000

    Extract all available metadata

--- yt_dlp/extractor/weyyak.py
+++ yt_dlp/extractor/weyyak.py
@@ -1,5 +1,12 @@
 from .common import InfoExtractor
-from ..utils import float_or_none, traverse_obj
+from ..utils import (
+    float_or_none,
+    int_or_none,
+    parse_age_limit,
+    traverse_obj,
+    unified_timestamp,
+    url_or_none,
+)
 
 
 class WeyyakIE(InfoExtractor):
@@ -13,6 +20,16 @@ class WeyyakIE(InfoExtractor):
                 'ext': 'mp4',
                 'title': 'Ribat Al Hob',
                 'duration': 2771,
+                'alt_title': 'رباط الحب',
+                'season': 'Season 1',
+                'season_number': 1,
+                'episode': 'Episode 49',
+                'episode_number': 49,
+                'timestamp': 1485907200,
+                'upload_date': '20170201',
+                'thumbnail': r're:^https://content\.weyyak\.com/.+/poster-image',
+                'categories': ['Drama', 'Thrillers', 'Romance'],
+                'tags': 'count:8',
             },
         },
         {
@@ -23,6 +40,15 @@ class WeyyakIE(InfoExtractor):
                 'ext': 'mp4',
                 'title': '8 Seconds',
                 'duration': 6490,
+                'alt_title': '8 ثواني',
+                'description': 'md5:45b83a155c30b49950624c7e99600b9d',
+                'age_limit': 15,
+                'release_year': 2015,
+                'timestamp': 1683106031,
+                'upload_date': '20230503',
+                'thumbnail': r're:^https://content\.weyyak\.com/.+/poster-image',
+                'categories': ['Drama', 'Social'],
+                'cast': ['Ceylin Adiyaman', 'Esra Inal'],
             },
         },
     ]
@@ -31,17 +57,30 @@ class WeyyakIE(InfoExtractor):
         video_id, lang, type_ = self._match_valid_url(url).group('id', 'lang', 'type')
 
         path = 'episode/' if type_ == 'episode' else 'contents/moviedetails?contentkey='
-        video_info = self._download_json(
-            f'https://msapifo-prod-me.weyyak.z5.com/v1/{lang}/{path}{video_id}', video_id)
-        video_details = self._download_json(
-            f'https://api-weyyak.akamaized.net/get_info/{video_info["data"]["video_id"]}',
-            video_id, 'Extracting video details')
-        formats, subtitles = self._extract_m3u8_formats_and_subtitles(video_details['url_video'], video_id)
+        data = self._download_json(
+            f'https://msapifo-prod-me.weyyak.z5.com/v1/{lang}/{path}{video_id}', video_id)['data']
+        m3u8_url = self._download_json(
+            f'https://api-weyyak.akamaized.net/get_info/{data["video_id"]}',
+            video_id, 'Extracting video details')['url_video']
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, video_id)
 
         return {
             'id': video_id,
-            'title': traverse_obj(video_info, ('data', 'title')),
-            'duration': float_or_none(video_details.get('duration')),
             'formats': formats,
             'subtitles': subtitles,
+            **traverse_obj(data, {
+                'title': ('title', {str}),
+                'alt_title': ('translated_title', {str}),
+                'description': ('synopsis', {str}),
+                'duration': ('length', {float_or_none}),
+                'age_limit': ('age_rating', {parse_age_limit}),
+                'season_number': ('season_number', {int_or_none}),
+                'episode_number': ('episode_number', {int_or_none}),
+                'thumbnail': ('imagery', 'thumbnail', {url_or_none}),
+                'categories': ('genres', ..., {str}),
+                'tags': ('tags', ..., {str}),
+                'cast': (('main_actor', 'main_actress'), {str}),
+                'timestamp': ('insertedAt', {unified_timestamp}),
+                'release_year': ('production_year', {int_or_none}),
+            }),
         }
