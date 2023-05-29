f0c0913df5ed5fc745bc024b7800c91f3b7344aa
pukkandan <pukkandan.ytdlp@gmail.com>
2023-05-29 09:44:56 +0530

    Remove _legacy_real_extract

--- yt_dlp/extractor/rai.py
+++ yt_dlp/extractor/rai.py
@@ -584,7 +584,8 @@ class RaiIE(RaiBaseIE):
         'only_matching': True,
     }]
 
-    def _extract_from_content_id(self, content_id, url):
+    def _real_extract(self, url):
+        content_id = self._match_id(url)
         media = self._download_json(
             f'https://www.rai.tv/dl/RaiTV/programmi/media/ContentItem-{content_id}.html?json',
             content_id, 'Downloading video JSON', fatal=False, expected_status=404)
@@ -620,84 +621,6 @@ class RaiIE(RaiBaseIE):
             'upload_date': unified_strdate(media.get('date')),
             'duration': parse_duration(media.get('length')),
             'subtitles': self._extract_subtitles(url, media),
-            **relinker_info
-        }
-
-    def _real_extract(self, url):
-        content_info = self._extract_from_content_id(self._match_id(url), url)
-        return content_info or self._legacy_real_extract(url)
-
-    def _legacy_real_extract(self, url):
-        # this code seems to be no longer useful. renamed to _legacy and created
-        # a new _real_extract that gets info directly from the video_id
-        # and uses _legacy only if it fails to retrieve infos
-        self.to_screen('Using legacy extractor')
-        video_id = self._match_id(url)
-
-        webpage = self._download_webpage(url, video_id)
-
-        content_item_id = None
-
-        content_item_url = self._html_search_meta(
-            ('og:url', 'og:video', 'og:video:secure_url', 'twitter:url',
-             'twitter:player', 'jsonlink'), webpage, default=None)
-        if content_item_url:
-            content_item_id = self._search_regex(
-                rf'ContentItem-({self._UUID_RE})', content_item_url,
-                'content item id', default=None)
-
-        if not content_item_id:
-            content_item_id = self._search_regex(
-                rf'''(?x)
-                    (?:
-                        (?:initEdizione|drawMediaRaiTV)\(|
-                        <(?:[^>]+\bdata-id|var\s+uniquename)=|
-                        <iframe[^>]+\bsrc=
-                    )
-                    (["\'])
-                    (?:(?!\1).)*\bContentItem-(?P<id>{self._UUID_RE})
-                ''',
-                webpage, 'content item id', default=None, group='id')
-
-        content_item_ids = set()
-        if content_item_id:
-            content_item_ids.add(content_item_id)
-        if video_id not in content_item_ids:
-            content_item_ids.add(video_id)
-
-        for content_item_id in content_item_ids:
-            try:
-                return self._extract_from_content_id(content_item_id, url)
-            except GeoRestrictedError:
-                raise
-            except ExtractorError:
-                pass
-
-        relinker_url = self._proto_relative_url(self._search_regex(
-            r'''(?x)
-                (?:
-                    var\s+videoURL|
-                    mediaInfo\.mediaUri
-                )\s*=\s*
-                ([\'"])
-                (?P<url>
-                    (?:https?:)?
-                    //mediapolis(?:vod)?\.rai\.it/relinker/relinkerServlet\.htm\?
-                    (?:(?!\1).)*\bcont=(?:(?!\1).)+)\1
-            ''',
-            webpage, 'relinker URL', group='url'))
-
-        relinker_info = self._extract_relinker_info(
-            urljoin(url, relinker_url), video_id)
-
-        title = self._search_regex(
-            r'var\s+videoTitolo\s*=\s*([\'"])(?P<title>[^\'"]+)\1',
-            webpage, 'title', group='title',
-            default=None) or self._og_search_title(webpage)
-
-        return {
-            'id': video_id,
-            'title': title,
             **relinker_info
         }
 
