af3f553355c2220ca5c4af620126d48a1e40067a
bashonly <bashonly@bashonly.com>
2023-06-14 11:07:21 -0500

    Cleanup tests part 2

--- yt_dlp/extractor/rheinmaintv.py
+++ yt_dlp/extractor/rheinmaintv.py
@@ -8,50 +8,47 @@ class RheinMainTVIE(InfoExtractor):
         'url': 'https://www.rheinmaintv.de/sendungen/beitrag-video/auf-dem-weg-zur-deutschen-meisterschaft/vom-07.11.2022/',
         'info_dict': {
             'id': 'auf-dem-weg-zur-deutschen-meisterschaft-vom-07.11.2022',
-            'display_id': 'auf-dem-weg-zur-deutschen-meisterschaft',
-            'title': 'Auf dem Weg zur Deutschen Meisterschaft',
+            'ext': 'ismv',
             'alt_title': 'Auf dem Weg zur Deutschen Meisterschaft',
-            'description': 'Die Lateinformation der FG Rhein-Main fährt am kommenden Wochenende zur Deutschen Meisterschaft nach Bremen. Im Training holen sich die Tänzerinnen und Tänzer den letzten Schliff.',
-            'ext': 'mp4',
-            'subtitles': {},
-            'thumbnail': 'https://rmtvmedia0.blob.core.windows.net/a0631411-b183-484b-ab6a-4e37a5831402/FormationsgemeinschaftRheinMain.jpg?sv=2016-05-31&sr=c&sig=iYdkgclsnXr2vDF%2B93jKTq702tsaWMtdxeeJHjV8iK4%3D&st=2022-11-07T17%3A44%3A17Z&se=3022-11-08T17%3A44%3A17Z&sp=r',
+            'title': 'Auf dem Weg zur Deutschen Meisterschaft',
+            'upload_date': '20221108',
+            'view_count': int,
+            'display_id': 'auf-dem-weg-zur-deutschen-meisterschaft',
+            'thumbnail': r're:^https://.+\.jpg',
+            'description': 'md5:48c59b74192bc819a9b34af1d5ed1eb9',
             'timestamp': 1667933057,
             'duration': 243.0,
-            'view_count': int,
-            'upload_date': '20221108'
-        }
+        },
     }, {
         'url': 'https://www.rheinmaintv.de/sendungen/beitrag-video/formationsgemeinschaft-rhein-main-bei-den-deutschen-meisterschaften/vom-14.11.2022/',
         'info_dict': {
             'id': 'formationsgemeinschaft-rhein-main-bei-den-deutschen-meisterschaften-vom-14.11.2022',
-            'display_id': 'formationsgemeinschaft-rhein-main-bei-den-deutschen-meisterschaften',
+            'ext': 'ismv',
             'title': 'Formationsgemeinschaft Rhein-Main bei den Deutschen Meisterschaften',
-            'alt_title': 'Formationsgemeinschaft Rhein-Main bei den Deutschen Meisterschaften',
-            'description': 'Die Lateinformation wollte bei den Deutschen Meisterschaften in die Zwischenrunde. Leider schaffte es das Team nicht.',
-            'ext': 'mp4',
-            'subtitles': {},
-            'thumbnail': 'https://rmtvmedia0.blob.core.windows.net/b43ca3fa-39b4-4129-9512-116c342b9a05/04_Latein.jpg?sv=2016-05-31&sr=c&sig=I6TEvoc8M2fzN6PwvIuPZ1VxxL06GnvxrjMTo7C3ys0%3D&st=2022-11-14T14%3A30%3A14Z&se=3022-11-15T14%3A30%3A14Z&sp=r',
             'timestamp': 1668526214,
-            'duration': 345.0,
+            'display_id': 'formationsgemeinschaft-rhein-main-bei-den-deutschen-meisterschaften',
+            'alt_title': 'Formationsgemeinschaft Rhein-Main bei den Deutschen Meisterschaften',
             'view_count': int,
-            'upload_date': '20221115'
+            'thumbnail': r're:^https://.+\.jpg',
+            'duration': 345.0,
+            'description': 'md5:9370ba29526984006c2cba1372e5c5a0',
+            'upload_date': '20221115',
         }
     }, {
         'url': 'https://www.rheinmaintv.de/sendungen/beitrag-video/casino-mainz-bei-den-deutschen-meisterschaften/vom-14.11.2022/',
         'info_dict': {
             'id': 'casino-mainz-bei-den-deutschen-meisterschaften-vom-14.11.2022',
-            'display_id': 'casino-mainz-bei-den-deutschen-meisterschaften',
+            'ext': 'ismv',
             'title': 'Casino Mainz bei den Deutschen Meisterschaften',
-            'alt_title': 'Casino Mainz bei den Deutschen Meisterschaften',
-            'description': 'Die Standardformation aus Mainz hoffte auch auf den Sprung in die Zwischenrunde, doch auch für sie war Schluss nach der Vorrunde.',
-            'ext': 'mp4',
-            'subtitles': {},
-            'thumbnail': 'https://rmtvmedia0.blob.core.windows.net/e1cf7655-91b9-4b7e-b19f-a79852c7b41f/06_Standard.jpg?sv=2016-05-31&sr=c&sig=jQPJSdxCxJMX8yUZkFUPNrv1qISb0pCouK9MzAMSOMc%3D&st=2022-11-14T14%3A50%3A02Z&se=3022-11-15T14%3A50%3A02Z&sp=r',
-            'timestamp': 1668527402,
-            'duration': 348.0,
             'view_count': int,
-            'upload_date': '20221115'
-        }
+            'timestamp': 1668527402,
+            'alt_title': 'Casino Mainz bei den Deutschen Meisterschaften',
+            'upload_date': '20221115',
+            'display_id': 'casino-mainz-bei-den-deutschen-meisterschaften',
+            'duration': 348.0,
+            'thumbnail': r're:^https://.+\.jpg',
+            'description': 'md5:70fc1660eeba96da17199e5bdff4c0aa',
+        },
     }]
 
     def _real_extract(self, url):
