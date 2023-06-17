01aba2519a0884ef17d5f85608dbd2a455577147
pukkandan <pukkandan.ytdlp@gmail.com>
2023-06-18 04:04:52 +0530

    [jsinterp] Fix global object extraction
    Closes #7327

--- yt_dlp/jsinterp.py
+++ yt_dlp/jsinterp.py
@@ -779,7 +779,7 @@ class JSInterpreter:
         obj = {}
         obj_m = re.search(
             r'''(?x)
-                (?<!this\.)%s\s*=\s*{\s*
+                (?<!\.)%s\s*=\s*{\s*
                     (?P<fields>(%s\s*:\s*function\s*\(.*?\)\s*{.*?}(?:,\s*)?)*)
                 }\s*;
             ''' % (re.escape(objname), _FUNC_NAME_RE),
