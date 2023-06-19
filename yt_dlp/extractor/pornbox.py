bdee4777f91732c6eccf182a439876d9608977be
niemands <67282402+niemands@users.noreply.github.com>
2023-06-19 20:32:02 +0200

    [Pornbox] Download free scenes

--- yt_dlp/extractor/pornbox.py
+++ yt_dlp/extractor/pornbox.py
@@ -5,40 +5,55 @@ from ..utils import parse_duration
 class PornboxIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?pornbox\.com/application/watch-page/(?P<id>[0-9]+)'
     _TESTS = [{
-        'url': 'https://pornbox.com/application/watch-page/73480',
-        'md5': '4a4b8db9dfb46a6671e41d5a36dffacf',
+        'url': 'https://pornbox.com/application/watch-page/429019',
+        'md5': '7c2f09cc7be6d9084a9faa7186d395d7',
         'info_dict': {
-            'id': '73480',
+            'id': '429019',
             'ext': 'mp4',
-            'title': 'md5:13c05c869615621a41b521a6c04d0af7',
-            'description': 'md5:13c05c869615621a41b521a6c04d0af7',
-            'uploader': 'VK Studio',
-            'upload_date': '20220617',
+            'title': 'md5:9c5decc32bc4d96c255e328282a9730a',
+            'description': 'md5:c47331c98221bc55c2d02a142a13f562',
+            'uploader': 'Giorgio Grandi',
+            'upload_date': '20230609',
             'age_limit': 18,
-            'availability': 'premium_only',
-            'duration': 2753,
-            'cast': ['Lesya Milk', 'Leo Casanova'],
-            'tags': 'count:48',
+            'availability': 'needs_auth',
+            'duration': 1961,
+            'cast': ['Tina Kay', 'Monika Wild', 'Neeo', 'Mr. Anderson', 'Thomas Lee', 'Angelo Godshack', 'Dylan Brown'],
+            'tags': 'count:40',
             'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
         }
-    },
-        {
-            'url': 'https://pornbox.com/application/watch-page/216045',
-            'md5': '56cb78bb1d8e0d2dad8b75c278c20098',
-            'info_dict': {
-                'id': '216045',
-                'ext': 'mp4',
-                'title': 'md5:3e48528e73a9a2b12f7a2772ed0b26a2',
-                'description': 'md5:3e631dcaac029f15ed434e402d1b06c7',
-                'uploader': 'VK Studio',
-                'upload_date': '20210412',
-                'age_limit': 18,
-                'availability': 'premium_only',
-                'duration': 2710,
-                'cast': ['Bella Grey', 'Oliver Trunk'],
-                'tags': 'count:29',
-                'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
-            }
+    }, {
+        'url': 'https://pornbox.com/application/watch-page/426095',
+        'md5': '0cc35f4d4300bc8e0496aef2a9ec20b9',
+        'info_dict': {
+            'id': '426095',
+            'ext': 'mp4',
+            'title': 'md5:96b05922d74be2be939cf247294f26bd',
+            'uploader': 'Giorgio Grandi',
+            'upload_date': '20230606',
+            'age_limit': 18,
+            'availability': 'needs_auth',
+            'duration': 1767,
+            'cast': ['Tina Kay', 'Neeo', 'Mr. Anderson', 'Thomas Lee', 'Brian Ragnastone', 'Rycky Optimal'],
+            'tags': 'count:37',
+            'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
+        }
+    }, {
+        'url': 'https://pornbox.com/application/watch-page/216045',
+        'md5': '56cb78bb1d8e0d2dad8b75c278c20098',
+        'info_dict': {
+            'id': '216045',
+            'ext': 'mp4',
+            'title': 'md5:3e48528e73a9a2b12f7a2772ed0b26a2',
+            'description': 'md5:3e631dcaac029f15ed434e402d1b06c7',
+            'uploader': 'VK Studio',
+            'upload_date': '20210412',
+            'age_limit': 18,
+            'availability': 'premium_only',
+            'duration': 2710,
+            'cast': ['Bella Grey', 'Oliver Trunk'],
+            'tags': 'count:29',
+            'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
+        }
     }]
 
     def _real_extract(self, url):
@@ -70,11 +85,9 @@ class PornboxIE(InfoExtractor):
         metadata = {
             'id': video_id,
             'title': public_data.get('scene_name').strip(),
-            'description': public_data.get('small_description').strip(),
             'uploader': public_data.get('studio'),
             'upload_date': date,
             'age_limit': 18,
-            'availability': 'premium_only',
             'duration': parse_duration(public_data.get('runtime')),
             'cast': cast,
             'tags': tags,
@@ -82,7 +95,15 @@ class PornboxIE(InfoExtractor):
             'subtitles': subtitles
         }
 
-        if not public_data.get('is_purchased'):
+        if description := public_data.get('small_description'):
+            metadata['description'] = description.strip()
+
+        if public_data.get('price').get('is_available_for_free'):
+            metadata['availability'] = 'needs_auth'
+        else:
+            metadata['availability'] = 'premium_only'
+
+        if (not public_data.get('is_purchased')) or (not public_data.get('price').get('is_available_for_free')):
             self.raise_login_required('You are either not logged in or do not have access to this scene',
                                       metadata_available=True, method='cookies')
             return metadata
