f32b4b56d101d5f15a1a60f989aa0e4fb5e27606
c-basalt <117849907+c-basalt@users.noreply.github.com>
2023-06-19 21:49:59 -0400

    improve format extraction

--- yt_dlp/extractor/vkplay.py
+++ yt_dlp/extractor/vkplay.py
@@ -18,14 +18,24 @@ class VKPlayIE(InfoExtractor):
         initial_state_json = self._search_regex(r'id="initial-state"[^>]*>([^<]+)</script>', webpage, 'initial_state')
         initial_state = self._parse_json(initial_state_json, video_id)
         record_info = traverse_obj(initial_state, ('record', 'currentRecord', 'data'))
+        playurls = traverse_obj(record_info, ('data', 0, 'playerUrls', ..., {
+            'url': ('url', {url_or_none}),
+            'format_id': ('type', {str_or_none}),
+        }))
+        formats = []
+        for playurl in playurls:
+            if not playurl.get('url', None):
+                continue
+            elif '.m3u8' in playurl['url']:
+                formats.extend(self._extract_m3u8_formats(playurl['url'], video_id))
+            else:
+                formats.append(playurl)
+
         meta = {
             'id': video_id,
+            'formats': formats,
             **traverse_obj(record_info, {
                 'title': ('title', {str}),
-                'formats': ('data', 0, 'playerUrls', lambda _, i: i['url'], {
-                    'url': ('url', {url_or_none}),
-                    'format_id': ('type', {str_or_none}),
-                }),
                 'thumbnail': ('previewUrl', {url_or_none}),
                 'release_timestamp': ('startTime'),
                 'uploader': ('blog', 'owner', 'name', {str_or_none}),
