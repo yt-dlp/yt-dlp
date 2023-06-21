c372b198ca963dc8a86ea5dc2df7e5e101c7307f
niemands <67282402+niemands@users.noreply.github.com>
2023-06-21 20:11:51 +0200

    Cleanup

--- yt_dlp/extractor/pornbox.py
+++ yt_dlp/extractor/pornbox.py
@@ -1,5 +1,12 @@
 from .common import InfoExtractor
-from ..utils import parse_duration
+from ..utils import (
+    parse_duration,
+    traverse_obj,
+    str_or_none,
+    strip_or_none,
+    parse_iso8601,
+    qualities
+)
 
 
 class PornboxIE(InfoExtractor):
@@ -13,6 +20,7 @@ class PornboxIE(InfoExtractor):
             'title': 'md5:9c5decc32bc4d96c255e328282a9730a',
             'description': 'md5:c47331c98221bc55c2d02a142a13f562',
             'uploader': 'Giorgio Grandi',
+            'timestamp': 1686348000,
             'upload_date': '20230609',
             'age_limit': 18,
             'availability': 'needs_auth',
@@ -29,6 +37,7 @@ class PornboxIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'md5:96b05922d74be2be939cf247294f26bd',
             'uploader': 'Giorgio Grandi',
+            'timestamp': 1686088800,
             'upload_date': '20230606',
             'age_limit': 18,
             'availability': 'needs_auth',
@@ -36,6 +45,9 @@ class PornboxIE(InfoExtractor):
             'cast': ['Tina Kay', 'Neeo', 'Mr. Anderson', 'Thomas Lee', 'Brian Ragnastone', 'Rycky Optimal'],
             'tags': 'count:37',
             'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
+        },
+        'params': {
+            'skip_download': True
         }
     }, {
         'url': 'https://pornbox.com/application/watch-page/216045',
@@ -46,14 +58,21 @@ class PornboxIE(InfoExtractor):
             'title': 'md5:3e48528e73a9a2b12f7a2772ed0b26a2',
             'description': 'md5:3e631dcaac029f15ed434e402d1b06c7',
             'uploader': 'VK Studio',
+            'timestamp': 1618264800,
             'upload_date': '20210412',
             'age_limit': 18,
             'availability': 'premium_only',
             'duration': 2710,
             'cast': ['Bella Grey', 'Oliver Trunk'],
             'tags': 'count:29',
-            'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$'
-        }
+            'thumbnail': r're:^https?://cdn-image\.gtflixtv\.com.*\.jpg.*$',
+            'subtitles': 'count:6'
+        },
+        'params': {
+            'skip_download': True,
+            'ignore_no_formats_error': True
+        },
+        'skip': 'Only for subscribers'
     }]
 
     def _real_extract(self, url):
@@ -61,19 +80,12 @@ class PornboxIE(InfoExtractor):
 
         public_data = self._download_json(f'https://pornbox.com/contents/{video_id}', video_id)
 
-        if public_data.get('studios') is not None:
-            date = public_data.get('studios')[0].get('release_date') or public_data.get('publish_date')
-        else:
-            date = public_data.get('publish_date')
-        date = date[:10].replace('-', '')
+        date = traverse_obj(public_data, ('studios', 'release_date'), 'publish_date')
         cast = []
         for m in public_data.get('models') or []:
             cast.append(m.get('model_name'))
         for m in public_data.get('male_models') or []:
             cast.append(m.get('model_name'))
-        tags = []
-        for t in public_data.get('niches') or []:
-            tags.append(t.get('niche'))
 
         subtitles = {}
         for country_code in public_data.get('subtitles') or []:
@@ -85,56 +97,58 @@ class PornboxIE(InfoExtractor):
         metadata = {
             'id': video_id,
             'title': public_data.get('scene_name').strip(),
+            'description': strip_or_none(public_data.get('small_description')),
             'uploader': public_data.get('studio'),
-            'upload_date': date,
+            'timestamp': parse_iso8601(date),
             'age_limit': 18,
             'duration': parse_duration(public_data.get('runtime')),
             'cast': cast,
-            'tags': tags,
-            'thumbnail': public_data.get('player_poster'),
-            'subtitles': subtitles
+            'tags': traverse_obj(public_data, ('niches', ..., 'niche'), default=[]),
+            'thumbnail': str_or_none(public_data.get('player_poster')),
+            'subtitles': subtitles,
         }
 
-        if description := public_data.get('small_description'):
-            metadata['description'] = description.strip()
-
-        if public_data.get('price').get('is_available_for_free'):
+        is_free_scene = traverse_obj(public_data, ('price', 'is_available_for_free'), default=False, expected_type=bool)
+        if is_free_scene:
             metadata['availability'] = 'needs_auth'
         else:
             metadata['availability'] = 'premium_only'
 
-        if (not public_data.get('is_purchased')) or (not public_data.get('price').get('is_available_for_free')):
+        if (not public_data.get('is_purchased')) or (not is_free_scene):
             self.raise_login_required('You are either not logged in or do not have access to this scene',
                                       metadata_available=True, method='cookies')
             return metadata
 
-        medias = public_data.get('medias')
-        full_video = next(filter(lambda x: x.get('title') == 'Full video', medias), None)
-        if full_video is None:
+        media_id = traverse_obj(public_data, ('medias', lambda _, v: v['title'] == 'Full video', 'media_id'),
+                                default=[], expected_type=int)
+        if not media_id:
             self.raise_no_formats(msg='Could not find stream id', video_id=video_id)
-        media_id = full_video.get('media_id')
+
+        # traverse_obj branches, therefore media_id is a list
+        media_id = media_id[0]
 
         headers = {
-            "Accept": "*/*",
-            "Accept-Language": "en-US;q=0.7,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br",
-            "X-Requested-With": "XMLHttpRequest",
-            "DNT": "1",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin"
+            'Accept': '*/*',
+            'Accept-Language': 'en-US;q=0.7,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin'
         }
         stream_data = self._download_json(f'https://pornbox.com/media/{media_id}/stream', video_id=video_id,
                                           headers=headers, note='Getting manifest urls')
-        qualities = stream_data.get('qualities')
-        qualities.sort(key=lambda x: x.get('bitrate'))
+
+        get_quality = qualities(['web', 'vga', 'hd', '1080p', '4k', '8k'])
         formats = []
-        for q in qualities:
+        for q in stream_data.get('qualities') or []:
             formats.append({
                 'url': q.get('src'),
                 'vbr': int(q.get('bitrate')) // 1000,
-                'format_id': q.get('quality')
+                'format_id': q.get('quality'),
+                'quality': get_quality(q.get('quality'))
             })
 
         metadata['formats'] = formats
