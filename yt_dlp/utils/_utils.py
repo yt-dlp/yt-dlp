9b23e79c34030625288fecf84092bb3b712ba913
bashonly <88596187+bashonly@users.noreply.github.com>
2023-06-22 04:56:06 +0000

    revert change to _utils.py

--- yt_dlp/utils/_utils.py
+++ yt_dlp/utils/_utils.py
@@ -3502,8 +3502,7 @@ def get_compatible_ext(*, vcodecs, acodecs, vexts, aexts, preferences=None):
         },
     }
 
-    sanitize_codec = functools.partial(
-        try_get, getter=lambda x: x[0].split('.')[0].replace('0', '').lower())
+    sanitize_codec = functools.partial(try_get, getter=lambda x: x[0].split('.')[0].replace('0', ''))
     vcodec, acodec = sanitize_codec(vcodecs), sanitize_codec(acodecs)
 
     for ext in preferences or COMPATIBLE_CODECS.keys():
