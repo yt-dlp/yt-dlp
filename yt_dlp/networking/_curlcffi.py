10115d5127d6d2c30f5e9e76540ac9eefcb05bd2
coletdjnz <coletdjnz@protonmail.com>
2023-07-14 11:13:31 +1200

    more

--- yt_dlp/networking/_curlcffi.py
+++ yt_dlp/networking/_curlcffi.py
@@ -1,4 +1,5 @@
 from .common import RequestHandler, Request, Response, register
+from .director import Preference, register_preference
 
 from ..dependencies import curl_cffi
 
@@ -8,8 +9,18 @@ if curl_cffi is None:
 
 @register
 class CurlCFFIRH(RequestHandler):
-    RH_NAME = 'curl-impersonate (cffi)'
+    RH_NAME = 'curl-impersonate (curl_cffi)'
 
     def _send(self, request: Request):
         pass
 
+
+@register_preference
+class CurlCFFIPrefernce(Preference):
+    RH_KEY = CurlCFFIRH.RH_KEY
+
+    def _get_preference(self, request: Request, handler: RequestHandler) -> int:
+        if not request.extensions.get('impersonate'):
+            return -1000
+        else:
+            return 1000
