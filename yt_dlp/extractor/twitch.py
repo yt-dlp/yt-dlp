43fd8ad02f35f65c0d0132dc67841567e55b1125
mpeter50 <83356418+mpeter50@users.noreply.github.com>
2023-07-14 14:47:51 +0200

    fix setting the subtitle in the infodict

--- yt_dlp/extractor/twitch.py
+++ yt_dlp/extractor/twitch.py
@@ -627,7 +627,7 @@ class TwitchVodIE(TwitchBaseIE):
                 'url': update_url_query(f'https://api.twitch.tv/v5/videos/{vod_id}/comments',
                                         {'client_id': self._CLIENT_ID}),
                 'ext': 'json',
-            }]},
+            }]}
             info['__post_extractor'] = lambda: {'subtitles': self.extract_subtitles(vod_id)}
 
         return info
