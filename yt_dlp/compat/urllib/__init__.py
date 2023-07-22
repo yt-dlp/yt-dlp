a250b247334ce9f641e709cbb64974da6034a2b3
pukkandan <pukkandan.ytdlp@gmail.com>
2023-07-22 17:56:53 +0530

    [compat] Ensure submodules are imported correctly
    Closes #7663

--- yt_dlp/compat/urllib/__init__.py
+++ yt_dlp/compat/urllib/__init__.py
@@ -1,6 +1,9 @@
 # flake8: noqa: F405
 from urllib import *  # noqa: F403
 
+del request
+from . import request  # noqa: F401
+
 from ..compat_utils import passthrough_module
 
 passthrough_module(__name__, 'urllib')
