bd730470f2ebafcf102804ba7ef326df6087b43b
bashonly <bashonly@bashonly.com>
2023-07-22 13:32:10 -0500

    Cleanup

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -3298,7 +3298,7 @@ class YoutubeDL:
                     fd = get_suitable_downloader(info_dict, self.params, to_stdout=temp_filename == '-')
                     if fd not in [FFmpegFD, DashSegmentsFD] and 'no-direct-merge' not in self.params['compat_opts'] and (
                             info_dict.get('section_start') or info_dict.get('section_end')):
-                        msg = (f'This format cannot be partially downloaded {fd}' if FFmpegFD.available()
+                        msg = ('This format cannot be partially downloaded' if FFmpegFD.available()
                                else 'You have requested downloading the video partially, but ffmpeg is not installed')
                         self.report_error(f'{msg}. Aborting')
                         return
