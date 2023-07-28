a15fcd299e767a510debd8dc1646fe863b96ce0e
nnoboa <90611593+nnoboa@users.noreply.github.com>
2023-07-28 14:52:07 -0400

    [ie/Wimbledon] Add extractor (#7551)
    Closes #7462
    Authored by: nnoboa

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2363,6 +2363,7 @@ from .weyyak import WeyyakIE
 from .whyp import WhypIE
 from .wikimedia import WikimediaIE
 from .willow import WillowIE
+from .wimbledon import WimbledonIE
 from .wimtv import WimTVIE
 from .whowatch import WhoWatchIE
 from .wistia import (
