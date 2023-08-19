4987115c155c99633ee93a2c3c99dd359d612652
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2023-08-19 19:51:55 +0800

    [ie/PIAULIZAPortal] Add extractor
    https://ulizaportal.jp/
    https://t.pia.jp/streaming
    
    For video platform "ULIZA" operated by the PIA Corporation.
    Tickets purchased at "PIA LIVE STREAM" are also used for "ULIZA".

---
+++ yt_dlp/extractor/piaulizaportal.py
@@ -0,0 +1,60 @@
+from .common import InfoExtractor
+from ..utils import (
+    ExtractorError,
+    int_or_none,
+    parse_qs,
+    time_seconds,
+    traverse_obj,
+)
+
+
+class PIAULIZAPortalIE(InfoExtractor):
+    IE_DESC = 'ulizaportal.jp - PIA LIVE STREAM'
+    _VALID_URL = r'https://ulizaportal\.jp/pages/(?P<id>[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})(\?expires=(?P<expires>\d+).*)?'
+    _TESTS = [{
+        'url': 'https://ulizaportal.jp/pages/005f18b7-e810-5618-cb82-0987c5755d44',
+        'info_dict': {
+            'id': '005f18b7-e810-5618-cb82-0987c5755d44',
+            'title': 'プレゼンテーションプレイヤーのサンプル',
+            'live_status': 'not_live',
+        },
+        'params': {
+            'skip_download': True,
+            'ignore_no_formats_error': True,
+        },
+    }, {
+        'url': 'https://ulizaportal.jp/pages/005e1b23-fe93-5780-19a0-98e917cc4b7d?expires=4102412400&signature=f422a993b683e1068f946caf406d211c17d1ef17da8bef3df4a519502155aa91&version=1',
+        'info_dict': {
+            'id': '005e1b23-fe93-5780-19a0-98e917cc4b7d',
+            'title': '【確認用】視聴サンプルページ（ULIZA）',
+            'live_status': 'not_live',
+        },
+        'params': {
+            'skip_download': True,
+            'ignore_no_formats_error': True,
+        },
+    }]
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+
+        expires = int_or_none(traverse_obj(self._match_valid_url(url), ('expires')))
+        if expires and expires <= time_seconds():
+            raise ExtractorError('The link is expired.', video_id=video_id, expected=True)
+
+        webpage = self._download_webpage(url, video_id)
+
+        player_data = self._download_webpage(self._search_regex(
+            r'<script [^>]*\bsrc="(https://player-api\.p\.uliza\.jp/v1/players/(?:[^"]*))"[^>]*>',
+            webpage, 'player data'), video_id, headers={'Referer': 'https://ulizaportal.jp/'},
+            note='Fetching player data', errnote='Unable to fetch player data')
+
+        m3u8_url = self._search_regex(
+            r'https://vms-api.p.uliza.jp/v1/prog-index.m3u8[^"]+', player_data, 'm3u8 url', group=(0))
+
+        return {
+            'id': video_id,
+            'title': self._html_extract_title(webpage),
+            'formats': self._extract_m3u8_formats(m3u8_url, video_id),
+            'live_status': 'is_live' if traverse_obj(parse_qs(m3u8_url), ('live', 0)) == 'true' else 'not_live',
+        }
