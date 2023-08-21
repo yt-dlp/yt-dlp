e43c8096bdf0b2b7e66da303e79d10407116fd83
Nathan Touzé <60022007+nathantouze@users.noreply.github.com>
2023-08-21 15:01:56 +0200

    Update dropbox links
    Since dropbox updated their share links around July this year, I had to make an update it to keep using this extractor. 
    
    This new regex handles old and new link formats.

--- yt_dlp/extractor/dropbox.py
+++ yt_dlp/extractor/dropbox.py
@@ -12,7 +12,7 @@ from ..utils import (
 
 
 class DropboxIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?dropbox[.]com/sh?/(?P<id>[a-zA-Z0-9]{15})/.*'
+    _VALID_URL = r'https?://(?:www\.)?dropbox\.com/(?:scl/fi/|e/scl/fi/|s/|sh/)(?P<id>[^/]+)/.*'
     _TESTS = [
         {
             'url': 'https://www.dropbox.com/s/nelirfsxnmcfbfh/youtube-dl%20test%20video%20%27%C3%A4%22BaW_jenozKc.mp4?dl=0',
