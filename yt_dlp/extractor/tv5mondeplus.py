dde9c933814d0a1373fd5d59e4129d1944a304b9
bashonly <bashonly@bashonly.com>
2023-08-25 08:00:48 -0500

    Partially revert 91761a56e80e5f159b2eff0dba1c285b71e48e4d

--- yt_dlp/extractor/tv5mondeplus.py
+++ yt_dlp/extractor/tv5mondeplus.py
@@ -96,49 +96,43 @@ class TV5MondePlusIE(InfoExtractor):
             r'(<[^>]+class="video_player_loader"[^>]+>)',
             webpage, 'video player loader'))
 
+        video_id = None
         video_files = self._parse_json(
             vpl_data['data-broadcast'], display_id)
         formats = []
-        video_id = None
-
-        def process_video_files(v):
-            nonlocal video_id
-            for video_file in v:
-                v_url = video_file.get('url')
+        for video_file in video_files:
+            v_url = video_file.get('url')
+            if not v_url:
+                continue
+            if video_file.get('type') == 'application/deferred':
+                d_param = urllib.parse.quote(v_url)
+                token = video_file.get('token')
+                if not token:
+                    continue
+                deferred_json = self._download_json(
+                    f'https://api.tv5monde.com/player/asset/{d_param}/resolve?condenseKS=true', display_id,
+                    note='Downloading deferred info', headers={'Authorization': f'Bearer {token}'}, fatal=False)
+                v_url = traverse_obj(deferred_json, (0, 'url', {url_or_none}))
                 if not v_url:
                     continue
-                if video_file.get('type') == 'application/deferred':
-                    d_param = urllib.parse.quote(v_url)
-                    token = video_file.get('token')
-                    if not token:
-                        continue
-                    deferred_json = self._download_json(
-                        f'https://api.tv5monde.com/player/asset/{d_param}/resolve?condenseKS=true', display_id,
-                        note='Downloading deferred info', headers={'Authorization': f'Bearer {token}'}, fatal=False)
-                    v_url = traverse_obj(deferred_json, (0, 'url', {url_or_none}))
-                    if not v_url:
-                        continue
-                    # data-guid from the webpage isn't stable, use the material id from the json urls
-                    video_id = self._search_regex(
-                        r'materials/([\da-zA-Z]{10}_[\da-fA-F]{7})/', v_url, 'video id',
-                        default=display_id)
-                    process_video_files(deferred_json)
+                # data-guid from the webpage isn't stable, use the asset id from the json urls
+                video_id = self._search_regex(
+                    r'assets/([\d]{9}_[\da-fA-F]{7})/materials', v_url, 'video id',
+                    default=display_id)
 
-                video_format = video_file.get('format') or determine_ext(v_url)
-                if video_format == 'm3u8':
-                    formats.extend(self._extract_m3u8_formats(
-                        v_url, display_id, 'mp4', 'm3u8_native',
-                        m3u8_id='hls', fatal=False))
-                elif video_format == 'mpd':
-                    formats.extend(self._extract_mpd_formats(
-                        v_url, display_id, fatal=False))
-                else:
-                    formats.append({
-                        'url': v_url,
-                        'format_id': video_format,
-                    })
-
-        process_video_files(video_files)
+            video_format = video_file.get('format') or determine_ext(v_url)
+            if video_format == 'm3u8':
+                formats.extend(self._extract_m3u8_formats(
+                    v_url, display_id, 'mp4', 'm3u8_native',
+                    m3u8_id='hls', fatal=False))
+            elif video_format == 'mpd':
+                formats.extend(self._extract_mpd_formats(
+                    v_url, display_id, fatal=False))
+            else:
+                formats.append({
+                    'url': v_url,
+                    'format_id': video_format,
+                })
 
         metadata = self._parse_json(
             vpl_data['data-metadata'], display_id)
