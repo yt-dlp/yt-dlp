9e2084d587dd447b69449620362aa13576baf1b1
Moritz Barsnick <barsnick@gmx.net>
2023-08-31 13:49:57 +0200

    [ie/Bild.de] also handle available HLS URLs
    The site sometimes delivers both HLS and a static file. Previously, the
    presence of HLS broke even the handling of the static file.
    
    Fixes https://github.com/yt-dlp/yt-dlp/issues/7951

--- yt_dlp/extractor/bild.py
+++ yt_dlp/extractor/bild.py
@@ -6,7 +6,7 @@ from ..utils import (
 
 
 class BildIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?bild\.de/(?:[^/]+/)+(?P<display_id>[^/]+)-(?P<id>\d+)(?:,auto=true)?\.bild\.html'
+    _VALID_URL = r'https?://(?:www\.)?bild\.de/(?:[^/]+/)+(?P<display_id>[^/]+)-(?P<id>\d+)(?:,auto=true)?\.bild\.html?'
     IE_DESC = 'Bild.de'
     _TEST = {
         'url': 'http://www.bild.de/video/clip/apple-ipad-air/das-koennen-die-neuen-ipads-38184146.bild.html',
@@ -27,11 +27,21 @@ class BildIE(InfoExtractor):
         video_data = self._download_json(
             url.split('.bild.html')[0] + ',view=json.bild.html', video_id)
 
+        formats = []
+        for src in video_data['clipList'][0]['srces']:
+            if src['type'] == 'application/x-mpegURL':
+                formats.extend(
+                    self._extract_m3u8_formats(
+                        src['src'], video_id, 'mp4',
+                        entry_protocol='m3u8_native', m3u8_id='hls', fatal=False))
+            elif src['type'] == 'video/mp4':
+                formats.append({'url': src['src'], 'format_id': 'http-mp4'})
+
         return {
             'id': video_id,
             'title': unescapeHTML(video_data['title']).strip(),
             'description': unescapeHTML(video_data.get('description')),
-            'url': video_data['clipList'][0]['srces'][0]['src'],
+            'formats': formats,
             'thumbnail': video_data.get('poster'),
             'duration': int_or_none(video_data.get('durationSec')),
         }
