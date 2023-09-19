0925f0fde7adc98dcb7d39cc0a0d69235a35fd05
Simon Sawicki <contact@grub4k.xyz>
2023-09-19 02:39:05 +0200

    not not not not

--- yt_dlp/extractor/pr0gramm.py
+++ yt_dlp/extractor/pr0gramm.py
@@ -102,7 +102,7 @@ class Pr0grammIE(InfoExtractor):
         error = traverse_obj(data, ('error', {str}))
         if error:
             if error in ('nsfwRequired', 'nsflRequired', 'nsfpRequired'):
-                if self._is_logged_in:
+                if not self._is_logged_in:
                     self.raise_login_required(method='cookies')
                 raise ExtractorError(f'Unverified account cannot access NSFW/NSFL ({error})', expected=True)
             raise ExtractorError(f'API returned: {error}', expected=True)
