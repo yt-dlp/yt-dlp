7609c6726b4dc676de52a753d34617516ffdeee0
pasha <pasha.syd04@gmail.com>
2023-09-19 09:21:27 -0700

    Revert "update mojevideo extractor"
    This reverts commit 14ca868f2362e080102f9e60a2920653190f41c1.

--- yt_dlp/extractor/mojevideo.py
+++ yt_dlp/extractor/mojevideo.py
@@ -10,18 +10,15 @@ class MojevideoIE(InfoExtractor):
     def _real_extract(self, url):
         webpage = self._download_webpage(url, 1)
 
-        v_id = re.search(r'vId=(\d+)', webpage).group(1)
-        v_ex = re.search(r"vEx='(\d+)'", webpage).group(1)
-        v_hash = re.search(r'vHash=\[([^\]]+)', webpage).group(1).split(",")[0].replace("'", "")
+        vId = re.search(r'vId=(\d+)', webpage).group(1)
+        vEx = re.search(r"vEx='(\d+)'", webpage).group(1)
+        vHash = re.search(r'vHash=\[([^\]]+)', webpage).group(1).split(",")[0].replace("'", "")
 
 
         info = {}
-        video_url = "https://cache01.mojevideo.sk/securevideos69/" + v_id + ".mp4?md5=" + v_hash + "&expires=" + v_ex
+        video_url = ""
         if video_url:
-            info = {
-                'id' : v_id,
-                'url': video_url
-            }
+            info = {url: video_url}
         if not info:
             raise ExtractorError('No videos found on webpage', expected=True)
 
