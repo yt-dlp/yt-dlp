5d67ae7c6d3696729ce12b8cf40b18d7a59b1f4b
trainman261 <trainman261@users.noreply.github.com>
2023-09-29 21:51:04 +0200

    Update yt_dlp/extractor/theplatform.py
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/theplatform.py
+++ yt_dlp/extractor/theplatform.py
@@ -163,7 +163,7 @@ class ThePlatformIE(ThePlatformBaseIE, AdobePassIE):
             # rtmp download
             'skip_download': True,
         },
-        'skip': 'cnet no longer seems to use theplatform, at least not in this video',
+        'skip': 'CNet no longer uses ThePlatform',
     }, {
         'url': 'https://player.theplatform.com/p/D6x-PC/pulse_preview/embed/select/media/yMBg9E8KFxZD',
         'info_dict': {
