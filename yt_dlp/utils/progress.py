8e9aa95c3ba6ade15608d9392a78b62878d9e3b5
Simon Sawicki <contact@grub4k.xyz>
2023-10-01 18:17:33 +0200

    Use exponential moving average

--- yt_dlp/utils/progress.py
+++ yt_dlp/utils/progress.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import bisect
-import operator
 import threading
 import time
 
@@ -9,14 +8,14 @@ import time
 class ProgressCalculator:
     # Time to calculate the speed over (in nanoseconds)
     SAMPLING_WINDOW = 1_000_000_000
-    # Time to smooth the speed over (in nanoseconds)
-    SMOOTHING_WINDOW = 100_000_000
+    # Factor for the exponential moving average (from 0 = prev to 1 = current)
+    SMOOTHING_FACTOR = 0.3
 
     def __init__(self, initial: int):
         self.downloaded = initial or 0
 
         self.elapsed: float = 0
-        self.speed: int | None = None
+        self.speed: int = 0
         self.eta: float | None = None
 
         self._total = None
@@ -26,9 +25,8 @@ class ProgressCalculator:
         self._lock = threading.Lock()
         self._thread_sizes: dict[int, int] = {}
 
-        self._downloaded = _DataPoints()
-        self._downloaded.add_point(self._start_time, self.downloaded)
-        self._speeds = _DataPoints()
+        self._times = [self._start_time]
+        self._downloaded = [self.downloaded]
 
     @property
     def total(self):
@@ -68,56 +66,25 @@ class ProgressCalculator:
         if self.total is not None and self.downloaded > self.total:
             self._total = self.downloaded
 
-        self._downloaded.add_point(current_time, self.downloaded)
+        self._times.append(current_time)
+        self._downloaded.append(self.downloaded)
 
         if current_time <= self._last_update:
             return
         self._last_update = current_time
 
-        self._downloaded.trim(current_time - self.SAMPLING_WINDOW)
-        download_time, download_bytes = self._downloaded.ranges()
+        offset = bisect.bisect_left(self._times, current_time - self.SAMPLING_WINDOW)
+        del self._times[:offset]
+        del self._downloaded[:offset]
+        download_time = current_time - self._times[0]
         if not download_time:
             return
-        speed = round(download_bytes * 1_000_000_000 / download_time)
+        downloaded_bytes = self.downloaded - self._downloaded[0]
+        speed = round(downloaded_bytes * 1_000_000_000 / download_time)
 
-        self._speeds.add_point(current_time, speed)
-        self._speeds.trim(current_time - self.SMOOTHING_WINDOW)
-        speed_time = self._speeds.ranges()[0] or 1
-
-        weights = tuple(1 + (point - current_time) / speed_time for point in self._speeds.times)
-        # Same as `statistics.fmean(self.data_points, weights)`, but weights is >=3.11
-        self.speed = int(sum(map(operator.mul, self._speeds.values, weights)) / sum(weights))
+        self.speed = int(self.SMOOTHING_FACTOR * speed + (1 - self.SMOOTHING_FACTOR) * self.speed)
 
         if not self.total:
             self.eta = None
         elif self.speed:
             self.eta = (self.total - self.downloaded) / self.speed
-
-
-class _DataPoints:
-    def __init__(self):
-        self.times: list[int] = []
-        self.values: list[int] = []
-
-    def add_point(self, time: int, value: int):
-        """Add a point to the dataset"""
-        self.times.append(time)
-        self.values.append(value)
-
-    def trim(self, start_time: int):
-        """Remove expired data points"""
-        offset = bisect.bisect_left(self.times, start_time)
-        del self.times[:offset]
-        del self.values[:offset]
-
-        return offset
-
-    def ranges(self):
-        """Return the range of both the time and data"""
-        if not self.times:
-            return 0, 0
-
-        return self.times[-1] - self.times[0], self.values[-1] - self.values[0]
-
-    def __len__(self):
-        return len(self.times)
