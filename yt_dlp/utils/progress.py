8c8420df3279a5f0ff3bd5c0897edcd42ca94c32
Simon Sawicki <contact@grub4k.xyz>
2023-10-01 13:36:15 +0200

    Use downloaded bytes as measurement for speed

--- yt_dlp/utils/progress.py
+++ yt_dlp/utils/progress.py
@@ -29,9 +29,7 @@ class ProgressCalculator:
         self._last_update = self._start_time
 
         self._times: list[float] = []
-        self._speeds: list[int] = []
-
-        self._thread_updates: dict[int, float] = {}
+        self._downloaded: list[int] = []
         self._thread_sizes: dict[int, int] = {}
 
     @property
@@ -52,7 +50,6 @@ class ProgressCalculator:
         current_thread = threading.get_ident()
         with self._lock:
             self._thread_sizes[current_thread] = 0
-            self._thread_updates[current_thread] = TIME_PROVIDER()
 
     def update(self, size: int | None):
         if not size:
@@ -61,39 +58,45 @@ class ProgressCalculator:
         current_thread = threading.get_ident()
 
         with self._lock:
-            current_time = TIME_PROVIDER()
-            last_size = self._thread_sizes.get(current_thread) or 0
-            last_update = self._thread_updates.get(current_thread) or self._start_time
-            chunk = size - last_size
-            print(f'    [{threading.get_ident()}] {last_update} -> {current_time} ({chunk}B)')
-
-            update_time = self._update(chunk, current_time, last_update)
-            if update_time:
-                self._thread_updates[current_thread] = current_time
+            last_size = self._thread_sizes.get(current_thread, 0)
             self._thread_sizes[current_thread] = size
+            self._update(size - last_size)
 
-    def _update(self, size, current_time, last_update):
+    def _update(self, size):
+        current_time = TIME_PROVIDER()
+
         self.downloaded += size
         self.elapsed = current_time - self._start_time
         if self.total is not None and self.downloaded > self.total:
             self._total = self.downloaded
 
         self._times.append(current_time)
-        self._speeds.append(size / (current_time - last_update))
+        self._downloaded.append(self.downloaded)
 
-        if current_time - self.UPDATE_TIME <= last_update:
+        if current_time - self.UPDATE_TIME <= self._last_update:
             return
         self._last_update = current_time
 
         offset = bisect.bisect_left(self._times, current_time - self.WINDOW_SIZE)
         del self._times[:offset]
-        del self._speeds[:offset]
+        del self._downloaded[:offset]
+        assert self._times, "A current entry should have been added above"
+        assert self._downloaded, "A current entry should have been added above"
 
-        weights = tuple(1 + (point - current_time) / self.WINDOW_SIZE for point in self._times)
-        # Same as `statistics.fmean(self.data_points, weights)`, but weights is >=3.11
-        self.speed = sum(map(float.__mul__, self._speeds, weights)) / sum(weights)
+        time_delta = self._times[-1] - self._times[0]
+        if not time_delta:
+            return
+
+        downloaded_bytes = self._downloaded[-1] - self._downloaded[0]
+        speed = downloaded_bytes / time_delta
+
+        # TODO: Add linear moving average
+        # weights = tuple(1 + (point - current_time) / self.WINDOW_SIZE for point in self._times)
+        # # Same as `statistics.fmean(self.data_points, weights)`, but weights is >=3.11
+        # self.speed = sum(map(float.__mul__, self._downloaded, weights)) / sum(weights)
+        self.speed = speed
 
         if not self.total:
             self.eta = None
-        else:
+        elif self.speed:
             self.eta = (self.total - self.downloaded) / self.speed
