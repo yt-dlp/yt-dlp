53b9ed7cffadfcb0fd901b37c8e87b28c99967f7
LoserFox <57448027+LoserFox@users.noreply.github.com>
2023-11-06 00:51:40 +0800

    update

--- yt_dlp/extractor/neteasemusic.py
+++ yt_dlp/extractor/neteasemusic.py
@@ -142,6 +142,10 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
             'subtitles': {'lyrics': [{'ext': 'lrc'}]},
             "duration": 256,
             'thumbnail': r're:^http.*\.jpg',
+            'album': '偶像练习生 表演曲目合集',
+            'average_rating': r'[0-9]+',
+            'album_artist': '偶像练习生',
+
         },
     }, {
         'note': 'No lyrics.',
@@ -155,6 +159,9 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
             'timestamp': 1202745600,
             'duration': 263,
             'thumbnail': r're:^http.*\.jpg',
+            'album': 'Piano Solos Vol. 2',
+            'album_artist': '',
+            'average_rating':r'[0-9]+',
         },
     }, {
         'url': 'https://y.music.163.com/m/song?app_version=8.8.45&id=95670&uct2=sKnvS4+0YStsWkqsPhFijw%3D%3D&dlt=0846',
@@ -171,6 +178,8 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
             'duration': 268,
             'alt_title': '伴唱:现代人乐队 合唱:总政歌舞团',
             'thumbnail': r're:^http.*\.jpg',
+            'average_rating': r'[0-9]+',
+            'album_artist': '侯牧人',
         },
     }, {
         'url': 'http://music.163.com/#/song?id=32102397',
@@ -186,6 +195,9 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
             'subtitles': {'lyrics': [{'ext': 'lrc'}]},
             'duration': 199,
             'thumbnail': r're:^http.*\.jpg',
+            'album': 'Bad Blood',
+            'average_rating':r'[0-9]+',
+            'album_artist': 'Taylor Swift',
         },
         'skip': 'Blocked outside Mainland China',
     }, {
@@ -203,6 +215,9 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
             'duration': 229,
             'alt_title': '说出愿望吧(Genie)',
             'thumbnail': r're:^http.*\.jpg',
+            'average_rating':r'[0-9]+',
+            'album':'Oh!',
+            'album_artist':'少女时代',
         },
         'skip': 'Blocked outside Mainland China',
     }]
@@ -259,8 +274,8 @@ class NetEaseMusicIE(NetEaseMusicBaseIE):
                 'title': ('name', {str}),
                 'timestamp': ('album', 'publishTime', {self.kilo_or_none}),
                 'duration': ('duration', {self.kilo_or_none}),
-                'album': ('album', 'name', {url_or_none}),
-                'thumbnail': ('album', 'picUrl', {str_or_none}),
+                'album': ('album', 'name', {str_or_none}),
+                'thumbnail': ('album', 'picUrl', {url_or_none}),
                 'average_rating': ('score', {int_or_none}),
             }),
         }
