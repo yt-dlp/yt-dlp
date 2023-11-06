f8c9d92df60daccfe874f40bbce3a87d6e5585b4
SirElderling <decluttered@protonmail.com>
2023-11-06 06:30:44 +0000

    [RadioComercial] Add extractor - fix tests formatting

--- yt_dlp/extractor/radiocomercial.py
+++ yt_dlp/extractor/radiocomercial.py
@@ -18,64 +18,59 @@ from ..utils.traversal import traverse_obj
 
 class RadioComercialIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?radiocomercial\.pt/podcasts/[^/]+/\D*(?P<season>\d+)/(?P<id>[\w-]+)'
-    _TESTS = [
-        {
-            'url': 'https://radiocomercial.pt/podcasts/o-homem-que-mordeu-o-cao/t6/taylor-swift-entranhando-se-que-nem-uma-espada-no-ventre-dos-fas',
-            'md5': '5f4fe8e485b29d2e8fd495605bc2c7e4',
-            'info_dict': {
-                'id': 'taylor-swift-entranhando-se-que-nem-uma-espada-no-ventre-dos-fas',
-                'ext': 'mp3',
-                'title': 'Taylor Swift entranhando-se que nem uma espada no ventre dos fãs.',
-                'description': None,
-                'release_date': '20231025',
-                'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
-                'season': 6
-            }
+    _TESTS = [{
+        'url': 'https://radiocomercial.pt/podcasts/o-homem-que-mordeu-o-cao/t6/taylor-swift-entranhando-se-que-nem-uma-espada-no-ventre-dos-fas',
+        'md5': '5f4fe8e485b29d2e8fd495605bc2c7e4',
+        'info_dict': {
+            'id': 'taylor-swift-entranhando-se-que-nem-uma-espada-no-ventre-dos-fas',
+            'ext': 'mp3',
+            'title': 'Taylor Swift entranhando-se que nem uma espada no ventre dos fãs.',
+            'description': None,
+            'release_date': '20231025',
+            'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
+            'season': 6
+        }
+    }, {
+        'url': 'https://radiocomercial.pt/podcasts/convenca-me-num-minuto/t3/convenca-me-num-minuto-que-os-lobisomens-existem',
+        'md5': '47e96c273aef96a8eb160cd6cf46d782',
+        'info_dict': {
+            'id': 'convenca-me-num-minuto-que-os-lobisomens-existem',
+            'ext': 'mp3',
+            'title': 'Convença-me num minuto que os lobisomens existem',
+            'description': None,
+            'release_date': '20231026',
+            'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
+            'season': 3
+        }
+    }, {
+        'url': 'https://radiocomercial.pt/podcasts/inacreditavel-by-ines-castel-branco/t2/o-desastre-de-aviao',
+        'md5': '69be64255420fec23b7259955d771e54',
+        'info_dict': {
+            'id': 'o-desastre-de-aviao',
+            'ext': 'mp3',
+            'title': 'O desastre de avião',
+            'description': 'md5:8a82beeb372641614772baab7246245f',
+            'release_date': '20231101',
+            'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
+            'season': 2
         },
-        {
-            'url': 'https://radiocomercial.pt/podcasts/convenca-me-num-minuto/t3/convenca-me-num-minuto-que-os-lobisomens-existem',
-            'md5': '47e96c273aef96a8eb160cd6cf46d782',
-            'info_dict': {
-                'id': 'convenca-me-num-minuto-que-os-lobisomens-existem',
-                'ext': 'mp3',
-                'title': 'Convença-me num minuto que os lobisomens existem',
-                'description': None,
-                'release_date': '20231026',
-                'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
-                'season': 3
-            }
+        'params': {
+            # inconsistant md5
+            'skip_download': True,
         },
-        {
-            'url': 'https://radiocomercial.pt/podcasts/inacreditavel-by-ines-castel-branco/t2/o-desastre-de-aviao',
-            'md5': '69be64255420fec23b7259955d771e54',
-            'info_dict': {
-                'id': 'o-desastre-de-aviao',
-                'ext': 'mp3',
-                'title': 'O desastre de avião',
-                'description': 'md5:8a82beeb372641614772baab7246245f',
-                'release_date': '20231101',
-                'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
-                'season': 2
-            },
-            'params': {
-                # inconsistant md5
-                'skip_download': True,
-            },
-        },
-        {
-            'url': 'https://radiocomercial.pt/podcasts/tnt-todos-no-top/2023/t-n-t-29-de-outubro',
-            'md5': '91d32d4d4b1407272068b102730fc9fa',
-            'info_dict': {
-                'id': 't-n-t-29-de-outubro',
-                'ext': 'mp3',
-                'title': 'T.N.T 29 de outubro',
-                'description': None,
-                'release_date': '20231029',
-                'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
-                'season': 2023
-            }
-        },
-    ]
+    }, {
+        'url': 'https://radiocomercial.pt/podcasts/tnt-todos-no-top/2023/t-n-t-29-de-outubro',
+        'md5': '91d32d4d4b1407272068b102730fc9fa',
+        'info_dict': {
+            'id': 't-n-t-29-de-outubro',
+            'ext': 'mp3',
+            'title': 'T.N.T 29 de outubro',
+            'description': None,
+            'release_date': '20231029',
+            'thumbnail': r're:https://radiocomercial.pt/upload/[^.]+.jpg',
+            'season': 2023
+        }
+    }]
 
     def _real_extract(self, url):
         video_id, season = self._match_valid_url(url).group('id', 'season')
@@ -95,32 +90,28 @@ class RadioComercialIE(InfoExtractor):
 class RadioComercialPlaylistIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?radiocomercial\.pt/podcasts/(?P<id>[\w-]+)(?:\D*(?P<season>\d+))?'
     _PAGE_SIZE = 19
-    _TESTS = [
-        {
-            'url': 'https://radiocomercial.pt/podcasts/convenca-me-num-minuto/t3',
-            'info_dict': {
-                'id': 'convenca-me-num-minuto',
-                'title': 'Convença-me num Minuto - Temporada 3',
-            },
-            'playlist_mincount': 32
+    _TESTS = [{
+        'url': 'https://radiocomercial.pt/podcasts/convenca-me-num-minuto/t3',
+        'info_dict': {
+            'id': 'convenca-me-num-minuto',
+            'title': 'Convença-me num Minuto - Temporada 3',
         },
-        {
-            'url': 'https://radiocomercial.pt/podcasts/o-homem-que-mordeu-o-cao',
-            'info_dict': {
-                'id': 'o-homem-que-mordeu-o-cao',
-                'title': 'O Homem Que Mordeu o Cão',
-            },
-            'playlist_mincount': 19
+        'playlist_mincount': 32
+    }, {
+        'url': 'https://radiocomercial.pt/podcasts/o-homem-que-mordeu-o-cao',
+        'info_dict': {
+            'id': 'o-homem-que-mordeu-o-cao',
+            'title': 'O Homem Que Mordeu o Cão',
         },
-        {
-            'url': 'https://radiocomercial.pt/podcasts/as-minhas-coisas-favoritas',
-            'info_dict': {
-                'id': 'as-minhas-coisas-favoritas',
-                'title': 'As Minhas Coisas Favoritas',
-            },
-            'playlist_mincount': 131
-        }
-    ]
+        'playlist_mincount': 19
+    }, {
+        'url': 'https://radiocomercial.pt/podcasts/as-minhas-coisas-favoritas',
+        'info_dict': {
+            'id': 'as-minhas-coisas-favoritas',
+            'title': 'As Minhas Coisas Favoritas',
+        },
+        'playlist_mincount': 131
+    }]
 
     def _fetch_page(self, podcast, season, page):
         page += 1
