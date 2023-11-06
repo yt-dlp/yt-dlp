e687d70de2547753234941448bcbb9632a76d0de
SirElderling <decluttered@protonmail.com>
2023-11-06 06:59:05 +0000

    [ZenPorn] Add extractor - fix tests formatting

--- yt_dlp/extractor/zenporn.py
+++ yt_dlp/extractor/zenporn.py
@@ -13,68 +13,63 @@ from ..utils import (
 class ZenPornIE(InfoExtractor):
     IE_DESC = 'ZenPorn'
     _VALID_URL = r'https?://(?:www\.)?zenporn\.com/video/(?P<id>\d+)'
-    _TESTS = [
-        {
-            'url': 'https://zenporn.com/video/15627016/desi-bhabi-ki-chudai',
-            'md5': '07bd576b5920714d74975c054ca28dee',
-            'info_dict': {
-                'id': '15627016',
-                'extr_id': '9563799',
-                'ext': 'mp4',
-                'title': 'md5:669eafd3bbc688aa29770553b738ada2',
-                'description': '',
-                'thumbnail': 'md5:2fc044a19bab450fef8f1931e7920a18',
-                'post_date': '20230925',
-                'uploader': 'md5:9fae59847f1f58d1da8f2772016c12f3',
-                'age_limit': 18
-            }
-        },
-        {
-            'url': 'https://zenporn.com/video/15570701',
-            'md5': 'acba0d080d692664fcc8c4e5502b1a67',
-            'info_dict': {
-                'id': '15570701',
-                'extr_id': '2297875',
-                'ext': 'mp4',
-                'title': 'md5:47aebdf87644ec91e8b1a844bc832451',
-                'description': '',
-                'thumbnail': 'https://tn.manysex.com/contents/videos_screenshots/2297000/2297875/480x270/1.jpg',
-                'post_date': '20230921',
-                'uploader': 'Lois Clarke',
-                'age_limit': 18
-            }
-        },
-        {
-            'url': 'https://zenporn.com/video/8531117/amateur-students-having-a-fuck-fest-at-club/',
-            'md5': '67411256aa9451449e4d29f3be525541',
-            'info_dict': {
-                'id': '8531117',
-                'extr_id': '12791908',
-                'ext': 'mp4',
-                'title': 'Amateur students having a fuck fest at club',
-                'description': '',
-                'thumbnail': 'https://tn.txxx.tube/contents/videos_screenshots/12791000/12791908/288x162/1.jpg',
-                'post_date': '20191005',
-                'uploader': 'Jackopenass',
-                'age_limit': 18
-            }
-        },
-        {
-            'url': 'https://zenporn.com/video/15872038/glad-you-came/',
-            'md5': '296ccab437f5bac6099433768449d8e1',
-            'info_dict': {
-                'id': '15872038',
-                'extr_id': '111585',
-                'ext': 'mp4',
-                'title': 'Glad You Came',
-                'description': '',
-                'thumbnail': 'https://vpim.m3pd.com/contents/videos_screenshots/111000/111585/480x270/1.jpg',
-                'post_date': '20231024',
-                'uploader': 'Martin Rudenko',
-                'age_limit': 18
-            }
+    _TESTS = [{
+        'url': 'https://zenporn.com/video/15627016/desi-bhabi-ki-chudai',
+        'md5': '07bd576b5920714d74975c054ca28dee',
+        'info_dict': {
+            'id': '15627016',
+            'extr_id': '9563799',
+            'ext': 'mp4',
+            'title': 'md5:669eafd3bbc688aa29770553b738ada2',
+            'description': '',
+            'thumbnail': 'md5:2fc044a19bab450fef8f1931e7920a18',
+            'post_date': '20230925',
+            'uploader': 'md5:9fae59847f1f58d1da8f2772016c12f3',
+            'age_limit': 18
         }
-    ]
+    }, {
+        'url': 'https://zenporn.com/video/15570701',
+        'md5': 'acba0d080d692664fcc8c4e5502b1a67',
+        'info_dict': {
+            'id': '15570701',
+            'extr_id': '2297875',
+            'ext': 'mp4',
+            'title': 'md5:47aebdf87644ec91e8b1a844bc832451',
+            'description': '',
+            'thumbnail': 'https://tn.manysex.com/contents/videos_screenshots/2297000/2297875/480x270/1.jpg',
+            'post_date': '20230921',
+            'uploader': 'Lois Clarke',
+            'age_limit': 18
+        }
+    }, {
+        'url': 'https://zenporn.com/video/8531117/amateur-students-having-a-fuck-fest-at-club/',
+        'md5': '67411256aa9451449e4d29f3be525541',
+        'info_dict': {
+            'id': '8531117',
+            'extr_id': '12791908',
+            'ext': 'mp4',
+            'title': 'Amateur students having a fuck fest at club',
+            'description': '',
+            'thumbnail': 'https://tn.txxx.tube/contents/videos_screenshots/12791000/12791908/288x162/1.jpg',
+            'post_date': '20191005',
+            'uploader': 'Jackopenass',
+            'age_limit': 18
+        }
+    }, {
+        'url': 'https://zenporn.com/video/15872038/glad-you-came/',
+        'md5': '296ccab437f5bac6099433768449d8e1',
+        'info_dict': {
+            'id': '15872038',
+            'extr_id': '111585',
+            'ext': 'mp4',
+            'title': 'Glad You Came',
+            'description': '',
+            'thumbnail': 'https://vpim.m3pd.com/contents/videos_screenshots/111000/111585/480x270/1.jpg',
+            'post_date': '20231024',
+            'uploader': 'Martin Rudenko',
+            'age_limit': 18
+        }
+    }]
 
     def _gen_info_url(self, ext_domain, extr_id, lifetime=86400):
         """ This function is a reverse engineering from the website javascript """
