f2f93629f6e3c07769fc79a81732693326cdee58
Elan Ruusamäe <glen@pld-linux.org>
2023-11-07 21:15:27 +0200

    Fill extra info obj rather change default title

--- yt_dlp/extractor/duoplay.py
+++ yt_dlp/extractor/duoplay.py
@@ -15,24 +15,22 @@ class DuoplayIE(InfoExtractor):
         'info_dict': {
             'id': '4312',
             'ext': 'mp4',
-            'title': 'Siberi võmm - S02E12 - Operatsioon "Öö"',
+            'title': 'Siberi võmm - Operatsioon "Öö"',
             'thumbnail': r're:https://.+\.jpg(?:\?c=\d+)?$',
             'description': 'md5:8ef98f38569d6b8b78f3d350ccc6ade8',
             'upload_date': '20170523',
             'timestamp': 1495567800,
+            'series': 'Siberi võmm',
+            'season': 'Season 2',
+            'season_number': 2,
+            'episode': 'Operatsioon "Öö"',
+            'episode_number': 12,
         },
     }]
 
     def _real_extract(self, url):
         def decode_quot(s: str):
             return s.replace("&quot;", '"')
-
-        # Format as "SXXEXX"
-        def season_episode(o):
-            return 'S%02dE%02d' % (
-                traverse_obj(o, 'season_id'),
-                traverse_obj(0, 'episode_nr'),
-            )
 
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
@@ -42,9 +40,13 @@ class DuoplayIE(InfoExtractor):
 
         return {
             'id': video_id,
-            'title': join_nonempty(traverse_obj(ep, 'title'), season_episode(ep), traverse_obj(ep, 'subtitle'), delim=' - '),
+            'title': join_nonempty(traverse_obj(ep, 'title'), traverse_obj(ep, 'subtitle'), delim=' - '),
             'description': traverse_obj(ep, 'synopsis'),
             'thumbnail': traverse_obj(ep, ('images', 'original')),
             'formats': self._extract_m3u8_formats(manifest_url, video_id, 'mp4'),
             'timestamp': unified_timestamp(traverse_obj(ep, 'airtime') + ' +0200'),
+            'series': traverse_obj(ep, 'title'),
+            'season_number': traverse_obj(ep, 'season_id'),
+            'episode': traverse_obj(ep, 'subtitle'),
+            'episode_number': traverse_obj(ep, 'episode_nr'),
         }
