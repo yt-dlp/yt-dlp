8cb4fbb4f1e14b970e93351cb666b585f31eb005
Elan Ruusamäe <glen@pld-linux.org>
2023-11-07 21:14:59 +0200

    Include season-episode in title

--- yt_dlp/extractor/duoplay.py
+++ yt_dlp/extractor/duoplay.py
@@ -15,7 +15,7 @@ class DuoplayIE(InfoExtractor):
         'info_dict': {
             'id': '4312',
             'ext': 'mp4',
-            'title': 'Siberi võmm / Operatsioon "Öö"',
+            'title': 'Siberi võmm - S02E12 - Operatsioon "Öö"',
             'thumbnail': r're:https://.+\.jpg(?:\?c=\d+)?$',
             'description': 'md5:8ef98f38569d6b8b78f3d350ccc6ade8',
             'upload_date': '20170523',
@@ -27,6 +27,13 @@ class DuoplayIE(InfoExtractor):
         def decode_quot(s: str):
             return s.replace("&quot;", '"')
 
+        # Format as "SXXEXX"
+        def season_episode(o):
+            return 'S%02dE%02d' % (
+                traverse_obj(o, 'season_id'),
+                traverse_obj(0, 'episode_nr'),
+            )
+
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
         manifest_url = self._search_regex(r'<video-player[^>]+manifest-url="([^"]+)"', webpage, 'video-player')
@@ -35,7 +42,7 @@ class DuoplayIE(InfoExtractor):
 
         return {
             'id': video_id,
-            'title': join_nonempty(traverse_obj(ep, 'title'), traverse_obj(ep, 'subtitle'), delim=' / '),
+            'title': join_nonempty(traverse_obj(ep, 'title'), season_episode(ep), traverse_obj(ep, 'subtitle'), delim=' - '),
             'description': traverse_obj(ep, 'synopsis'),
             'thumbnail': traverse_obj(ep, ('images', 'original')),
             'formats': self._extract_m3u8_formats(manifest_url, video_id, 'mp4'),
