cb865736dd70a9bb05b506ffd0d68b3ceaea6f56
Elan Ruusamäe <glen@pld-linux.org>
2023-11-08 20:16:23 +0200

    Update test data

--- yt_dlp/extractor/jupiter.py
+++ yt_dlp/extractor/jupiter.py
@@ -8,22 +8,21 @@ from ..utils import (
 class JupiterIE(InfoExtractor):
     _VALID_URL = r'https://jupiter\.err\.ee/(?P<id>\d+)/'
     _TESTS = [{
-        'note': 'Siberi võmm S02E12',
+        'note': 'S01E06: Impulss',
         'url': 'https://jupiter.err.ee/1609145945/impulss',
         'md5': '1ff59d535310ac9c5cf5f287d8f91b2d',
         'info_dict': {
-            'id': '4312',
+            'id': '1609145945',
             'ext': 'mp4',
-            'title': 'Operatsioon "Öö"',
-            'thumbnail': r're:https://.+\.jpg(?:\?c=\d+)?$',
-            'description': 'md5:8ef98f38569d6b8b78f3d350ccc6ade8',
-            'upload_date': '20170523',
-            'timestamp': 1495567800,
-            'series': 'Siberi võmm',
-            'season': 'Season 2',
-            'season_number': 2,
-            'episode': 'Operatsioon "Öö"',
-            'episode_number': 12,
+            'title': 'Loteriipilet hooldekodusse',
+            'description': 'md5:d1770e868afffd5d42b886283574941e',
+            'upload_date': '20231107',
+            'timestamp': 1699380000,
+            'series': 'Impulss',
+            'season': 'Season 1',
+            'season_number': 1,
+            'episode': 'Loteriipilet hooldekodusse',
+            'episode_number': 6,
         },
     }]
 
