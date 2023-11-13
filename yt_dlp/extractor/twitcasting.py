f44afe8be6b710cbd1ff53cf7123f0cecd3e4a81
JC-Chung <52159296+JC-Chung@users.noreply.github.com>
2023-11-14 00:00:33 +0800

    Update twitcasting.py

--- yt_dlp/extractor/twitcasting.py
+++ yt_dlp/extractor/twitcasting.py
@@ -248,10 +248,13 @@ class TwitCastingLiveIE(InfoExtractor):
             'Pass "https://twitcasting.tv/{0}/show" to download the history'.format(uploader_id))
 
         webpage = self._download_webpage(url, uploader_id)
-        is_live = 'data-is-onlive="true"' in webpage
+        is_live = self._search_regex(
+            (r'(data-is-onlive="true")', # public live
+             r'(?s)(<span\s*class="tw-movie-thumbnail2-badge"\s*data-status="live">\s*LIVE)'), # protected live
+            webpage, 'is live?', default=None)
         current_live = self._search_regex(
-            (r'data-type="movie" data-id="(\d+)">',
-             r'tw-sound-flag-open-link" data-id="(\d+)" style=',
+            (r'data-type="movie" data-id="(\d+)">', # no available?
+             r'tw-sound-flag-open-link" data-id="(\d+)" style=', # no available?
              r'data-movie-id="(\d+)"',),
             webpage, 'current live ID', default=None)
         if is_live and not current_live:
@@ -259,14 +262,11 @@ class TwitCastingLiveIE(InfoExtractor):
             webpage = self._download_webpage(
                 f'https://twitcasting.tv/{uploader_id}/show/', uploader_id,
                 note='Downloading live history')
-            is_live = self._search_regex(
-                (r'(?s)(<span\s*class="tw-movie-thumbnail-badge"\s*data-status="live">\s*LIVE)',
-                 r'(data-is-onlive="true")'),
-                webpage, 'is live?', default=None)
+            is_live = self._search_regex(r'(?s)(<span\s*class="tw-movie-thumbnail2-badge"\s*data-status="live">\s*LIVE)', webpage, 'is live?', default=None)
             if is_live:
                 # get the first live; running live is always at the first
                 current_live = self._search_regex(
-                    r'(?s)<a\s+class="tw-movie-thumbnail"\s*href="/[^/]+/movie/(?P<video_id>\d+)"\s*>.+?</a>',
+                    r'(?s)<a\s+class="tw-movie-thumbnail2"\s*href="/[^/]+/movie/(?P<video_id>\d+)"\s*>.+?</a>',
                     webpage, 'current live ID 2', default=None, group='video_id')
         if not current_live:
             raise UserNotLive(video_id=uploader_id)
