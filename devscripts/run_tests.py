dd549927539a828ff45eaa83c5b032e42a798b16
Simon Sawicki <contact@grub4k.xyz>
2023-12-06 13:19:09 +0100

    Fix lint lol

--- devscripts/run_tests.py
+++ devscripts/run_tests.py
@@ -10,39 +10,40 @@ IE_TEST_PATTERN = re.compile(r'IE(_all|_\d+)?$')
 
 
 def parse_args():
-	parser = argparse.ArgumentParser(description='Run selected yt-dlp tests')
-	parser.add_argument(
-		'test', help='An extractor test, or one of "test", "core" or "download"', nargs='*')
-	parser.add_argument(
-		'-k', help='Run a test matching the expression. Same as "pytest -k"', metavar='EXPRESSION')
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description='Run selected yt-dlp tests')
+    parser.add_argument(
+        'test', help='An extractor test, or one of "test", "core" or "download"', nargs='*')
+    parser.add_argument(
+        '-k', help='Run a test matching the expression. Same as "pytest -k"', metavar='EXPRESSION')
+    return parser.parse_args()
 
 
 def run_tests(*tests, pattern=None):
-	command = ['pytest', '--tb', 'short']
+    command = ['pytest', '--tb', 'short']
 
-	if pattern:
-		command.extend(['-k', pattern])
+    if pattern:
+        command.extend(['-k', pattern])
 
-	for test in tests:
-		if test == 'core':
-			command.extend(['-m', 'not download'])
-		elif test == 'download':
-			command.extend(['-m', 'download'])
-		else:
-			test = IE_TEST_PATTERN.sub(r'\1', test)
-			command.append(f'test/test_download.py::TestDownload::test_{test}')
+    for test in tests:
+        if test == 'core':
+            command.extend(['-m', 'not download'])
+        elif test == 'download':
+            command.extend(['-m', 'download'])
+        else:
+            test = IE_TEST_PATTERN.sub(r'\1', test)
+            command.append(f'test/test_download.py::TestDownload::test_{test}')
 
-	if not command:
-		command.extend(['-m', 'not download'])
+    if not command:
+        command.extend(['-m', 'not download'])
 
-	print(f'Running {command}')
-	os.chdir(Path(__file__).parent.parent)
-	try:
-		subprocess.run(command)
-	except FileNotFoundError:
-		print('"pytest" needs to be installed to run the tests', file=sys.stderr)
+    print(f'Running {command}')
+    os.chdir(Path(__file__).parent.parent)
+    try:
+        subprocess.run(command)
+    except FileNotFoundError:
+        print('"pytest" needs to be installed to run the tests', file=sys.stderr)
+
 
 if __name__ == '__main__':
-	args = parse_args()
-	run_tests(*args.test, pattern=args.k)
+    args = parse_args()
+    run_tests(*args.test, pattern=args.k)
