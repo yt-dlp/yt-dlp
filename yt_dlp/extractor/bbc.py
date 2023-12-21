961ac675ee07a870603e7f6d54de8313666c0a13
Moritz Barsnick <barsnick@gmx.net>
2023-12-21 13:21:10 +0100

    [ie/bbc] amend fixes from review
    Review in https://github.com/yt-dlp/yt-dlp/pull/8321

--- yt_dlp/extractor/bbc.py
+++ yt_dlp/extractor/bbc.py
@@ -320,11 +320,11 @@ class BBCCoUkIE(InfoExtractor):
         formats, subtitles = [], {}
         for media_set in self._MEDIA_SETS:
             try:
-                fmts, sttls = self._download_media_selector_url(
+                fmts, subs = self._download_media_selector_url(
                     self._MEDIA_SELECTOR_URL_TEMPL % (media_set, programme_id), programme_id)
                 formats.extend(fmts)
-                if sttls:
-                    subtitles = self._merge_subtitles(subtitles, sttls)
+                if subs:
+                    self._merge_subtitles(subs, target=subtitles)
             except BBCCoUkIE.MediaSelectionError as e:
                 if e.id in ('notukerror', 'geolocation', 'selectionunavailable'):
                     last_exception = e
@@ -332,8 +332,7 @@ class BBCCoUkIE(InfoExtractor):
                 self._raise_extractor_error(e)
         if last_exception:
             if formats or subtitles:
-                self.report_warning(
-                    '%s returned error: %s' % (self.IE_NAME, last_exception.id))
+                self.report_warning(f'{self.IE_NAME} returned error: {last_exception.id}')
             else:
                 self._raise_extractor_error(last_exception)
         return formats, subtitles
