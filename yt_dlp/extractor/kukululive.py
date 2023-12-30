55b3e113952a334a5ed0e12947ac4741dbaa2a67
DmitryScaletta <dmitryscaletta@mail.ru>
2023-12-30 17:36:20 +0300

    [KukuluLive] Fix extracting live stream formats

--- yt_dlp/extractor/kukululive.py
+++ yt_dlp/extractor/kukululive.py
@@ -10,6 +10,52 @@ class KukuluLiveIE(InfoExtractor):
         'only_matching': True,
     }]
 
+    def _get_quality_meta(self, video_id, desc, code, force_h264=''):
+        description = desc if force_h264 == '' else f'{desc} (force_h264)'
+        qs = self._download_webpage(
+            'https://live.erinn.biz/live.player.fplayer.php', video_id,
+            query={
+                'hash': video_id,
+                'action': f'get{code}liveByAjax',
+                'force_h264': force_h264,
+            },
+            note=f'Downloading {description} quality metadata',
+            errnote=f'Unable to download {description} quality metadata'
+        )
+        return compat_parse_qs(qs)
+
+    def _add_quality_formats(self, formats, quality_meta):
+        vcodec = quality_meta.get('vcodec')[0]
+        quality = quality_meta.get('now_quality')[0]
+        quality_priority = {
+            'high': 3,
+            'h264': 2,
+            'low': 1,
+        }
+        formats.extend([
+            {
+                'format_id': quality,
+                'url': quality_meta.get('hlsaddr')[0],
+                'ext': 'mp4',
+                'vcodec': vcodec,
+                'quality': quality_priority[quality],
+            },
+            {
+                'format_id': f'{quality}-rtmp',
+                'url': quality_meta.get('streamaddr')[0],
+                'ext': 'mp4',
+                'vcodec': vcodec,
+                'quality': quality_priority[quality] - 5,
+            },
+            {
+                'format_id': f'{quality}-audioonly',
+                'url': quality_meta.get('hlsaddr_audioonly')[0],
+                'ext': 'aac',
+                'vcodec': 'none',
+                'quality': quality_priority[quality] - 10,
+            },
+        ])
+
     def _real_extract(self, url):
         video_id = self._match_id(url)
         html = self._download_webpage(
@@ -29,38 +75,16 @@ class KukuluLiveIE(InfoExtractor):
 
         if is_live_stream:
             qualities = [
-                ('hevc', 'getForceLowliveByAjax'),
-                ('h264', 'getZliveByAjax'),
+                ('high', 'Z'),
+                ('low', 'ForceLow'),
             ]
             formats = []
-            for (codec, action) in qualities:
-                qs = self._download_webpage(
-                    'https://live.erinn.biz/live.player.fplayer.php', video_id,
-                    query={'hash': video_id, 'action': action},
-                    note=f'Downloading {codec} metadata',
-                    errnote=f'Unable to download {codec} metadata'
-                )
-                quality_meta = compat_parse_qs(qs)
-                vcodec = quality_meta.get('vcodec')[0]
-                quality = quality_meta.get('now_quality')[0]
-                formats.append({
-                    'format_id': quality,
-                    'url': quality_meta.get('hlsaddr')[0],
-                    'ext': 'mp4',
-                    'vcodec': vcodec,
-                })
-                formats.append({
-                    'format_id': f'{quality}-audioonly',
-                    'url': quality_meta.get('hlsaddr_audioonly')[0],
-                    'ext': 'aac',
-                    'vcodec': 'none',
-                })
-                formats.append({
-                    'format_id': f'{quality}-rtmp',
-                    'url': quality_meta.get('streamaddr')[0],
-                    'ext': 'mp4',
-                    'vcodec': 'vcodec',
-                })
+            for (desc, code) in qualities:
+                quality_meta = self._get_quality_meta(video_id, desc, code)
+                self._add_quality_formats(formats, quality_meta)
+                if desc == 'high' and quality_meta.get('vcodec')[0] == 'HEVC':
+                    h264_meta = self._get_quality_meta(video_id, desc, code, force_h264='1')
+                    self._add_quality_formats(formats, h264_meta)
 
             return {
                 'id': str(video_id),
