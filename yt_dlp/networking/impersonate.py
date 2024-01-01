d644aeb2c344b542284dc3b79eb2223f9b8fc0e2
coletdjnz <coletdjnz@protonmail.com>
2024-01-01 14:38:04 +1300

    Only remove user agent if it matches.
    In some cases we may want to provide a custom user agent

--- yt_dlp/networking/impersonate.py
+++ yt_dlp/networking/impersonate.py
@@ -131,7 +131,6 @@ class ImpersonateRequestHandler(RequestHandler, ABC):
         headers = self._merge_headers(request.headers)
         if self._get_request_target(request) is not None:
             # remove all headers present in std_headers
-            headers.pop('User-Agent', None)
             for header in std_headers:
                 if header in headers and std_headers[header] == headers[header]:
                     headers.pop(header, None)
