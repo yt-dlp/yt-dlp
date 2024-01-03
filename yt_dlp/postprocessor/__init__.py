2598790093e47121a21937e527ecfb91393f59d7
Lev Plyusnin <me@llistochek.ru>
2024-01-03 15:16:10 +0700

    Revert MutagenMetadataPP

--- yt_dlp/postprocessor/__init__.py
+++ yt_dlp/postprocessor/__init__.py
@@ -30,7 +30,6 @@ from .metadataparser import (
 )
 from .modify_chapters import ModifyChaptersPP
 from .movefilesafterdownload import MoveFilesAfterDownloadPP
-from .mutagenmetadata import MutagenMetadataPP
 from .sponskrub import SponSkrubPP
 from .sponsorblock import SponsorBlockPP
 from .xattrpp import XAttrMetadataPP
