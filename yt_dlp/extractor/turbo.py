db51f0f9bb5d122d6619a97d050918dc0fbbce9f
Maxence <max@23.tf>
2024-01-03 21:15:15 -0500

    Fix turbo extractor

--- yt_dlp/extractor/turbo.py
+++ yt_dlp/extractor/turbo.py
@@ -1,64 +1,54 @@
 import re
 
 from .common import InfoExtractor
-from ..compat import compat_str
 from ..utils import (
-    ExtractorError,
-    int_or_none,
-    qualities,
-    xpath_text,
+    get_element_html_by_class,
 )
 
 
 class TurboIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?turbo\.fr/videos-voiture/(?P<id>[0-9]+)-'
-    _API_URL = 'http://www.turbo.fr/api/tv/xml.php?player_generique=player_generique&id={0:}'
+    _VALID_URL = r'https?://(?:www\.)?turbo\.fr/((?P<playlist>[_\d\w]+).xml)?'
+    _API_URL = 'https://www.viously.com/video/hls/{0:}/index.m3u8'
     _TEST = {
         'url': 'http://www.turbo.fr/videos-voiture/454443-turbo-du-07-09-2014-renault-twingo-3-bentley-continental-gt-speed-ces-guide-achat-dacia.html',
-        'md5': '33f4b91099b36b5d5a91f84b5bcba600',
+        'md5': '37a6c3381599381ff53a7e1e0575c0bc',
         'info_dict': {
-            'id': '454443',
+            'id': 'F_xQzS2jwb3',
             'ext': 'mp4',
-            'duration': 3715,
-            'title': 'Turbo du 07/09/2014 : Renault Twingo 3, Bentley Continental GT Speed, CES, Guide Achat Dacia... ',
+            'title': 'Turbo du 07/09/2014 : Renault Twingo 3, Bentley Continental GT Speed, CES, Guide Achat Dacia...',
             'description': 'Turbo du 07/09/2014 : Renault Twingo 3, Bentley Continental GT Speed, CES, Guide Achat Dacia...',
-            'thumbnail': r're:^https?://.*\.jpg$',
         }
     }
 
-    def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        video_id = mobj.group('id')
+    def _entries(self, playlist):
+        items = playlist.findall('./channel/item')
+        for item in items:
+            if item is None or item.find('./link').text is None:
+                continue
+            yield self._extract_video(item.find('./link').text)
 
-        webpage = self._download_webpage(url, video_id)
-
-        playlist = self._download_xml(self._API_URL.format(video_id), video_id)
-        item = playlist.find('./channel/item')
-        if item is None:
-            raise ExtractorError('Playlist item was not found', expected=True)
-
-        title = xpath_text(item, './title', 'title')
-        duration = int_or_none(xpath_text(item, './durate', 'duration'))
-        thumbnail = xpath_text(item, './visuel_clip', 'thumbnail')
-        description = self._html_search_meta('description', webpage)
-
-        formats = []
-        get_quality = qualities(['3g', 'sd', 'hq'])
-        for child in item:
-            m = re.search(r'url_video_(?P<quality>.+)', child.tag)
-            if m:
-                quality = compat_str(m.group('quality'))
-                formats.append({
-                    'format_id': quality,
-                    'url': child.text,
-                    'quality': get_quality(quality),
-                })
-
+    def _extract_video(self, url):
+        webpage = self._download_webpage(url, None)
+        viously_player = get_element_html_by_class('viously-player-wrapper', webpage)
+        video_id = self._html_search_regex(r'id="([-_\w]+)"', viously_player, 'video_id')
+        title = self._html_extract_title(webpage)
         return {
             'id': video_id,
             'title': title,
-            'duration': duration,
-            'thumbnail': thumbnail,
-            'description': description,
-            'formats': formats,
+            'description': title,
+            'formats': self._extract_m3u8_formats(self._API_URL.format(video_id), video_id),
         }
+
+    def _real_extract(self, url):
+        mobj = self._match_valid_url(url)
+        playlist_id = mobj.group('playlist')
+
+        if playlist_id and self._yes_playlist(playlist_id, None):
+            playlist = self._download_xml(url, playlist_id)
+            return self.playlist_result(
+                self._entries(playlist),
+                playlist_id,
+                playlist_title=playlist.find('./channel/title').text,
+            )
+
+        return self._extract_video(url)
