5803f6116a9338d9f63cc26c27f6f56d73d76976
sepro <4618135+seproDev@users.noreply.github.com>
2024-01-06 04:16:46 +0100

    Revert unrelated changes

--- yt_dlp/extractor/jiosaavn.py
+++ yt_dlp/extractor/jiosaavn.py
@@ -77,7 +77,7 @@ class JioSaavnSongIE(JioSaavnBaseIE):
 
 
 class JioSaavnAlbumIE(JioSaavnBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?(?:jio)?saavn\.com/(?:s/playlist|album)/[^/?#]+/(?P<id>[^/?#]+)'
+    _VALID_URL = r'https?://(?:www\.)?(?:jio)?saavn\.com/album/[^/?#]+/(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.jiosaavn.com/album/96/buIOjYZDrNA_',
         'info_dict': {
