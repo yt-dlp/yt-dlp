965935fc32d8110efe4db877a3c1907a25091ee4
bashonly <88596187+bashonly@users.noreply.github.com>
2024-01-14 22:00:07 +0000

    [ie/getcourseru] Cleanup

--- yt_dlp/extractor/getcourseru.py
+++ yt_dlp/extractor/getcourseru.py
@@ -3,9 +3,7 @@ from ..utils import int_or_none, traverse_obj, url_or_none
 
 
 class GetCourseRuIE(InfoExtractor):
-    _NETRC_MACHINE = 'getcourseru'
     _VALID_URL = r'https?://[^.]+\.getcourse\.ru/sign-player/\?json=(?P<id>[^#]+)'
-
     _TESTS = [{
         'url': 'http://player02.getcourse.ru/sign-player/?json=eyJ2aWRlb19oYXNoIjoiMTkwYmRmOTNmMWIyOTczNTMwOTg1M2E3YTE5ZTI0YjMiLCJ1c2VyX2lkIjozNTk1MjUxODMsInN1Yl9sb2dpbl91c2VyX2lkIjpudWxsLCJsZXNzb25faWQiOm51bGwsImlwIjoiNDYuMTQyLjE4Mi4yNDciLCJnY19ob3N0IjoiYWNhZGVteW1lbC5vbmxpbmUiLCJ0aW1lIjoxNzA1MjY2Njc2LCJwYXlsb2FkIjoidV8zNTk1MjUxODMiLCJ1aV9sYW5ndWFnZSI6InJ1IiwiaXNfaGF2ZV9jdXN0b21fc3R5bGUiOnRydWV9&s=0c5e0c6aef19da36add3135a162b8eba&vh-static-feature=zigzag',
         'info_dict': {
@@ -13,15 +11,12 @@ class GetCourseRuIE(InfoExtractor):
             'title': '190bdf93f1b29735309853a7a19e24b3',
             'ext': 'mp4',
             'duration': 1693
-            # note: the original URL is necessary to obtain an up-to-date URL, because the URL is always changing
         },
-        'skip': 'Requires authentication',
-        'note': 'This extractor is used by AcademyMel extractor, which has a login feature'
+        'skip': 'JWT expired',
     }]
 
     def _real_extract(self, url):
         webpage = self._download_webpage(url, None, 'Downloading player page')
-
         window_configs = self._search_json(
             r'window\.configs\s*=', webpage, 'config', None)
         video_id = str(window_configs['videoId'])
