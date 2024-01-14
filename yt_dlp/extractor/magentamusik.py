40b2c4ee473fa741f5abdc04a69fa8f7299bf4b3
sepro <4618135+seproDev@users.noreply.github.com>
2024-01-14 21:07:34 +0100

    Apply review suggestions

--- yt_dlp/extractor/magentamusik.py
+++ yt_dlp/extractor/magentamusik.py
@@ -1,41 +1,61 @@
 from .common import InfoExtractor
+from ..utils import ExtractorError, int_or_none, join_nonempty, url_or_none
+from ..utils.traversal import traverse_obj
 
 
 class MagentaMusikIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?magentamusik\.de/(?P<id>.*)'
+    _VALID_URL = r'https?://(?:www\.)?magentamusik\.de/(?P<id>[^/?#]+)'
 
     _TESTS = [{
         'url': 'https://www.magentamusik.de/marty-friedman-woa-2023-9208205928595409235',
         'md5': 'd82dd4748f55fc91957094546aaf8584',
         'info_dict': {
             'id': '9208205928595409235',
+            'display_id': 'marty-friedman-woa-2023-9208205928595409235',
             'ext': 'mp4',
             'title': 'Marty Friedman: W:O:A 2023',
-            'duration': 2760
+            'alt_title': 'Konzert vom: 05.08.2023 13:00',
+            'duration': 2760,
+            'categories': ['Musikkonzert'],
+            'release_year': 2023,
+            'location': 'Deutschland',
         }
     }]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
-        video_id = self._html_search_regex(r'"assetId":"[^\d]+([0-9]+)"', webpage, 'video_id')
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
+        player_config = self._search_json(
+            r'data-js-element="o-video-player__config">', webpage, 'player config', display_id, fatal=False)
+        if not player_config:
+            raise ExtractorError('No video found', expected=True)
 
-        json = self._download_json("https://wcps.t-online.de/cvss/magentamusic/vodplayer/v3/player/58935/%s/Main%%20Movie" % video_id, video_id)
-        xml_url = json['content']['feature']['representations'][0]['contentPackages'][0]['media']['href']
-        metadata = json['content']['feature'].get('metadata')
-        title = None
-        if metadata:
-            title = metadata.get('title')
-            duration = metadata.get('runtimeInSeconds')
-        xml = self._download_xml(xml_url, video_id)
-        final_url = xml[0][0][0].attrib['src']
+        asset_id = player_config.get('assetId')
+        if not asset_id:
+            raise ExtractorError('No assetId found in player config')
+        asset_details = self._download_json(
+            f'https://wcps.t-online.de/cvss/magentamusic/vodclient/v2/assetdetails/58938/{asset_id}',
+            display_id, note='Downloading asset details')
 
-        formats = self._extract_m3u8_formats(
-            final_url, video_id, ext='mp4')
+        video_id = traverse_obj(asset_details, ('content', 'partnerInformation', ..., 'reference'), get_all=False)
+
+        vod_data = self._download_json(
+            f'https://wcps.t-online.de/cvss/magentamusic/vodclient/v2/player/58935/{video_id}/Main%20Movie', video_id)
+        smil_url = traverse_obj(
+            vod_data, ('content', 'feature', 'representations', ...,
+                       'contentPackages', ..., 'media', 'href', {url_or_none}), get_all=False)
 
         return {
             'id': video_id,
-            'title': title,
-            'formats': formats,
-            'duration': duration
+            'display_id': display_id,
+            'formats': self._extract_smil_formats(smil_url, video_id),
+            **traverse_obj(vod_data, ('content', 'feature', 'metadata', {
+                'title': 'title',
+                'alt_title': 'originalTitle',
+                'description': 'longDescription',
+                'duration': ('runtimeInSeconds', {int_or_none}),
+                'location': ('countriesOfProduction', {list}, {lambda x: join_nonempty(*x, delim=', ')}),
+                'release_year': ('yearOfProduction', {int_or_none}),
+                'categories': ('mainGenre', {str}, {lambda x: x and [x]}),
+            })),
         }
