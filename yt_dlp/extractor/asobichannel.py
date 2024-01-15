5d799a48b3f4917d9c10ec9f59fcdcbbba738379
sepro <4618135+seproDev@users.noreply.github.com>
2024-01-15 13:04:30 +0100

    Rework live videos

--- yt_dlp/extractor/asobichannel.py
+++ yt_dlp/extractor/asobichannel.py
@@ -1,7 +1,6 @@
-import datetime
-
 from .common import InfoExtractor
-from ..utils import ExtractorError, clean_html, merge_dicts, parse_iso8601, traverse_obj
+from ..utils import ExtractorError, clean_html, merge_dicts, parse_iso8601
+from ..utils.traversal import traverse_obj
 
 
 class AsobiChannelBaseIE(InfoExtractor):
@@ -13,7 +12,7 @@ class AsobiChannelBaseIE(InfoExtractor):
             'title': 'title',
             'description': ('body', {clean_html}),
             'thumbnail': ('contents', 'video_thumb', 'url'),
-            'release_timestamp': ('publishedAt', {parse_iso8601}),
+            'timestamp': ('publishedAt', {parse_iso8601}),
             'modified_timestamp': ('updatedAt', {parse_iso8601}),
             'channel': ('channel', 'name'),
             'channel_id': ('channel', 'id'),
@@ -34,8 +33,8 @@ class AsobiChannelIE(AsobiChannelBaseIE):
             'title': 'アイドルマスター ミリオンライブ！ 765プロch 原っぱ通信 #1',
             'description': 'md5:b930bd2199c9b2fd75951ce4aaa7efd2',
             'thumbnail': 'https://images.microcms-assets.io/assets/d2420de4b9194e11beb164f99edb1f95/a8e6f84119f54eb9ab4ce16729239905/%E3%82%B5%E3%83%A0%E3%83%8D%20(1).png',
-            'release_timestamp': 1697098247,
-            'release_date': '20231012',
+            'timestamp': 1697098247,
+            'upload_date': '20231012',
             'modified_timestamp': 1698381162,
             'modified_date': '20231027',
             'channel': 'アイドルマスター',
@@ -52,8 +51,8 @@ class AsobiChannelIE(AsobiChannelBaseIE):
             'thumbnail': 'https://images.microcms-assets.io/assets/d2420de4b9194e11beb164f99edb1f95/20e5c1d6184242eebc2512a5dec59bf0/P1_%E5%8E%9F%E3%81%A3%E3%81%B1%E3%82%B5%E3%83%A0%E3%83%8D.png',
             'modified_timestamp': 1697797125,
             'modified_date': '20231020',
-            'release_timestamp': 1697261769,
-            'release_date': '20231014',
+            'timestamp': 1697261769,
+            'upload_date': '20231014',
             'channel': 'アイドルマスター',
             'channel_id': 'idolmaster',
         },
@@ -79,28 +78,27 @@ class AsobiChannelIE(AsobiChannelBaseIE):
         }
 
     def _process_live(self, video_id, metadata):
-        now_ts = datetime.datetime.now().timestamp()
-
         content_id = metadata['contents']['video_id']
-
-        live_start = traverse_obj(metadata, ('period', 'start', {parse_iso8601}))
-        if live_start is None:
-            live_status = None
-        elif now_ts < live_start:
-            # live_status should be 'is_upcoming', but stream is not available at this point
-            self.raise_no_formats(f'Live stream will be available at {live_start}', expected=True)
-        else:
-            live_status = 'is_live'
-
         event_data = self._download_json(
             f'https://survapi.channel.or.jp/ex/events/{content_id}?embed=channel', video_id,
             headers=self._survapi_header, note='Downloading event data')
 
-        live_url = event_data['data']['Channel']['Custom_live_url']
+        player_type = traverse_obj(event_data, ('data', 'Player_type'))
+        if player_type == 'poster':
+            self.raise_no_formats('Live event has not yet started', expected=True)
+            live_status = 'is_upcoming'
+            formats = []
+        elif player_type == 'player':
+            live_status = 'is_live'
+            formats = self._extract_m3u8_formats(
+                event_data['data']['Channel']['Custom_live_url'], video_id, live=True)
+        else:
+            raise ExtractorError('Unexpected player type')
 
         return {
+            'release_timestamp': traverse_obj(metadata, ('period', 'start', {parse_iso8601})),
             'live_status': live_status,
-            'formats': self._extract_m3u8_formats(live_url, video_id, live=True),
+            'formats': formats,
         }
 
     def _real_extract(self, url):
