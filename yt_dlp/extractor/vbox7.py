cb45e613e7b4bcfbdbfac911feddc509806249d7
sepro <4618135+seproDev@users.noreply.github.com>
2024-01-29 08:24:47 +0100

    Adjust base url extraction

--- yt_dlp/extractor/vbox7.py
+++ yt_dlp/extractor/vbox7.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import int_or_none
+from ..utils import ExtractorError, base_url, int_or_none, url_basename
 from ..utils.traversal import traverse_obj
 
 
@@ -64,17 +64,14 @@ class Vbox7IE(InfoExtractor):
             'https://www.vbox7.com/aj/player/item/options', video_id,
             query={'vid': video_id})['options']
 
-        if '.mp4' in data['src']:
-            base_url = data['src'].rpartition('_')[0]
-        else:
-            base_url = data['src'].rpartition('.')[0]
+        fmt_base = base_url(data['src']) + url_basename(data['src']).rsplit('.', 1)[0].rsplit('_', 1)[0]
 
         formats = self._extract_m3u8_formats(
-            f'{base_url}.m3u8', video_id, m3u8_id='hls', fatal=False)
+            f'{fmt_base}.m3u8', video_id, m3u8_id='hls', fatal=False)
         # TODO: Add MPD formats, when dash range support is added
         for res in traverse_obj(data, ('resolutions', lambda _, v: v != 0, {int})):
             formats.append({
-                'url': f'{base_url}_{res}.mp4',
+                'url': f'{fmt_base}_{res}.mp4',
                 'format_id': f'http-{res}',
                 'height': res,
             })
