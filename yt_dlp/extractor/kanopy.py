6775f35cf1c9b6e2608cd7c9dda9c859fc0d724a
BGarber42 <BGarber42@gmail.com>
2024-02-07 06:11:26 +0100

    Match domainId of user memberships to passed url, handle hls + dash

--- yt_dlp/extractor/kanopy.py
+++ yt_dlp/extractor/kanopy.py
@@ -1,37 +1,22 @@
 import json
 
 from .common import InfoExtractor
-from ..utils import ExtractorError, try_get
+from ..utils import ExtractorError, traverse_obj, try_call, url_or_none
 
 
 class KanopyIE(InfoExtractor):
-    _API_BASE_URL = "https://www.kanopy.com/kapi/"
-    _NETRC_MACHINE = "kanopy"
-    _VALID_URL = r"https?://(?:www\.)?kanopy.com/(?P<lang>\w{2})/(?P<institution>.*?)/watch/video/(?P<id>[0-9]+)"
+    _API_BASE_URL = 'https://www.kanopy.com/kapi/'
+    _NETRC_MACHINE = 'kanopy'
+    _VALID_URL = r'https?://(?:www\.)?kanopy.com/(?P<lang>\w{2})/(?P<institution>.*?)/watch/video/(?P<id>[0-9]+)'
     _TESTS = [
         {
-            "url": "https://www.kanopy.com/en/pleasantvalley/watch/video/114603",
-            "info_dict": {
-                "id": "114603",
-                "ext": "m3u8",
-                "title": "The Blue Kite",
-                "description": "md5:6163af52ae92627ae7c58906991d3792",
+            'url': 'https://www.kanopy.com/en/pleasantvalley/watch/video/114603',
+            'info_dict': {
+                'id': '114603',
+                'ext': 'mp4',
+                'title': 'The Blue Kite',
+                'description': 'md5:6163af52ae92627ae7c58906991d3792',
             },
-        },
-        {
-            "url": "https://www.kanopy.com/en/pleasantvalley/watch/video/14020947",
-            "info_dict": {
-                "id": "14020947",
-                "ext": "mpd",
-                "title": "The Whale",
-                "description": "TBD",
-            },
-            "params": {
-                "ignore_no_formats_error": True,
-                "skip_download": True,
-            },
-            "expected_warnings": ["This video is DRM protected"],
-            "expected_exception": "DownloadError",
         },
     ]
     _LOGIN_REQUIRED = True
@@ -39,9 +24,12 @@ class KanopyIE(InfoExtractor):
     _USER_ID = None
 
     headers = {
-        "content-type": "application/json",
-        "x-version": "web/prod/4.3.0/2024-01-08-15-13-05",
+        'content-type': 'application/json',
+        'x-version': 'web/prod/4.3.0/2024-01-08-15-13-05',
     }
+
+    def _match_membership(self, memberships, matcher):
+        return next((d for d in memberships if d['subdomain'] == matcher), None)
 
     def _real_initialize(self):
         if not self._ACCESS_TOKEN and not self._USER_ID:
@@ -51,69 +39,89 @@ class KanopyIE(InfoExtractor):
         self.report_login()
         try:
             access_json = self._download_json(
-                self._API_BASE_URL + "login",
+                self._API_BASE_URL + 'login',
                 None,
-                "Logging in to site using credentials",
-                "Unable to log in",
+                'Logging in to site using credentials',
+                'Unable to log in',
                 fatal=False,
                 headers=self.headers,
                 data=json.dumps(
                     {
-                        "credentialType": "email",
-                        "emailUser": {"email": username, "password": password},
+                        'credentialType': 'email',
+                        'emailUser': {'email': username, 'password': password},
                     }
                 ).encode(),
             )
-            self._ACCESS_TOKEN = try_get(access_json, lambda x: x["jwt"])
+            self._ACCESS_TOKEN = try_call(lambda: access_json['jwt'])
             if self._ACCESS_TOKEN is None:
-                self.report_warning("Failed to get Access token")
+                self.report_warning('Failed to get Access token')
             else:
-                self.headers.update({"Authorization": "Bearer %s" % self._ACCESS_TOKEN})
-                self._USER_ID = try_get(access_json, lambda x: x["userId"])
+                self.headers.update({'Authorization': 'Bearer %s' % self._ACCESS_TOKEN})
+                self._USER_ID = try_call(lambda: access_json['userId'])
         except ExtractorError as e:
             self.report_warning(e)
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
+        lang, institution, video_id = self._match_valid_url(url).groups()
 
-        memberships = self._download_json(
-            self._API_BASE_URL + f"memberships?userId={self._USER_ID}",
-            None,
-            headers=self.headers,
+        memberships = traverse_obj(
+            self._download_json(
+                self._API_BASE_URL + f'memberships?userId={self._USER_ID}',
+                None,
+                headers=self.headers,
+            ),
+            ('list'),
+            list,
         )
 
+        domain_id = try_call(
+            lambda: self._match_membership(memberships, institution)['domainId']
+        )
+        if not domain_id:
+            raise ExtractorError(f'Could not match {institution} to a membership')
+
         video_info = self._download_json(
-            self._API_BASE_URL
-            + f"videos/{video_id}?domainId={memberships['list'][0]['domainId']}",
+            self._API_BASE_URL + f'videos/{video_id}?domainId={domain_id}',
             video_id,
             headers=self.headers,
         )
 
         streams = self._download_json(
-            self._API_BASE_URL + "plays",
+            self._API_BASE_URL + 'plays',
             video_id,
             headers=self.headers,
             data=json.dumps(
                 {
-                    "domainId": memberships["list"][0]["domainId"],
-                    "userId": self._USER_ID,
-                    "videoId": video_id,
+                    'domainId': domain_id,
+                    'userId': self._USER_ID,
+                    'videoId': video_id,
                 }
             ).encode(),
         )
 
-        manifests = streams["manifests"]
-        drm_free = bool(
-            list(filter(lambda x: "drm" in x and x["drm"] == "none", manifests))
-        )
-
-        self.write_debug(f"Params: {self._downloader.params}")
-        if not drm_free:
-            self.report_drm(video_id)
+        formats, subtitles = [], {}
+        for manifest in traverse_obj(
+            streams, ('manifests', lambda _, v: url_or_none(v['url']))
+        ):
+            manifest_type = manifest.get('manifestType')
+            if manifest_type == 'hls':
+                fmts, subs = self._extract_m3u8_formats_and_subtitles(
+                    manifest['url'], video_id, m3u8_id='hls', fatal=False
+                )
+            elif manifest_type == 'dash':
+                fmts, subs = self._extract_mpd_formats_and_subtitles(
+                    manifest['url'], video_id, mpd_id='dash', fatal=False
+                )
+            else:
+                self.report_warning(f'Unsupported manifest type: {manifest_type!r}')
+                continue
+            formats.extend(fmts)
+            self._merge_subtitles(subs, target=subtitles)
 
         return {
-            "id": video_id,
-            "title": video_info["video"]["title"],
-            "description": video_info["video"]["descriptionHtml"],
-            "url": streams["manifests"][0]["url"],
+            'id': video_id,
+            'title': video_info['video']['title'],
+            'description': video_info['video']['descriptionHtml'],
+            'formats': formats,
+            'subtitles': subtitles,
         }
