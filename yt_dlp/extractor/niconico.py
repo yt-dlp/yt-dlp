70a7854e2fbcfcdb833948f7b2529de2b8a255aa
bashonly <88596187+bashonly@users.noreply.github.com>
2024-02-17 21:59:34 +0000

    Apply suggestions from code review

--- yt_dlp/extractor/niconico.py
+++ yt_dlp/extractor/niconico.py
@@ -471,7 +471,7 @@ class NiconicoIE(InfoExtractor):
         }
 
     def _get_subtitles(self, video_id, api_data):
-        comments_info = traverse_obj(api_data, ('comment', 'nvComment'))
+        comments_info = traverse_obj(api_data, ('comment', 'nvComment', {dict})) or {}
         danmaku = traverse_obj(self._download_json(
             f'{comments_info.get("server")}/v1/threads', video_id, data=json.dumps({
                 'additionals': {},
