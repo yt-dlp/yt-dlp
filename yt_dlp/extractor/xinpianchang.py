a5f6bd55d816fdbc87aa42a2754fd6f35e7533e9
sepro <4618135+seproDev@users.noreply.github.com>
2024-02-18 06:49:38 +0100

    [ie/Xinpianchang] Mark broken

--- yt_dlp/extractor/xinpianchang.py
+++ yt_dlp/extractor/xinpianchang.py
@@ -8,6 +8,7 @@ from ..utils import (
 
 
 class XinpianchangIE(InfoExtractor):
+    _WORKING = False
     _VALID_URL = r'https?://www\.xinpianchang\.com/(?P<id>[^/]+?)(?:\D|$)'
     IE_NAME = 'xinpianchang'
     IE_DESC = 'xinpianchang.com'
