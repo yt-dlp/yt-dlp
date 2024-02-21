7b9f56ade5aefbed689a503112eda9e5fa2b0566
HobbyistDev <tesutonihon4@gmail.com>
2024-02-21 18:39:21 +0800

    reformat `product_detail_json`

--- yt_dlp/extractor/viu.py
+++ yt_dlp/extractor/viu.py
@@ -294,7 +294,9 @@ class ViuOTTIE(InfoExtractor):
             next_js_data = self._search_nextjs_data(webpage, video_id)['props']
             runtime_info = next_js_data['initialState']['app']['runtimeInfo']
 
-            product_detail_json = traverse_obj(next_js_data, ('pageProps', 'fallback', lambda k, v: v if re.match(r'@"PRODUCT_DETAIL"[^:]+', k) else None), get_all=False)
+            product_detail_json = traverse_obj(
+                next_js_data, ('pageProps', 'fallback', lambda k, v: v if re.match(r'@"PRODUCT_DETAIL"[^:]+', k) else None), 
+                get_all=False)
             current_product_info = traverse_obj(product_detail_json, ('data', 'current_product'))
             current_product_subtitle_info = current_product_info.get('subtitle')
 
