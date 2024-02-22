0bae4e828a9ed2af5cf7dbfeb75367669542e458
sepro <4618135+seproDev@users.noreply.github.com>
2024-02-22 22:33:04 +0100

    Address review comments

--- yt_dlp/extractor/cnbc.py
+++ yt_dlp/extractor/cnbc.py
@@ -1,114 +1,97 @@
-import json
 from .common import InfoExtractor
-from ..utils import traverse_obj, unified_strdate, url_or_none
+from ..utils import int_or_none, parse_iso8601, str_or_none, url_or_none
+from ..utils.traversal import traverse_obj
 
 
 class CNBCVideoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?cnbc\.com/video/(?:[^/]+/)+(?P<id>[^./?#&]+)\.html'
 
-    _TESTS = [
-        {
-            'url': 'https://www.cnbc.com/video/2023/12/07/mcdonalds-just-unveiled-cosmcsits-new-spinoff-brand.html',
-            'info_dict': {
-                'author': 'Sean Conlon',
-                'channel': 'News Videos',
-                'description': 'McDonald\'s unveiled its new spinoff brand known as CosMc\'s this week, announcing that the new brand\'s first location will open in Bolingbrook, Illinois. CosMc\'s drive-thru focused menu features brand new lemonades and teas, blended beverages, and cold coffee, as well as a small lineup of food. The burger chain first revealed it was creating CosMc\'s as a spinoff during its second-quarter earnings call in July.',
-                'duration': 65.0,
-                'ext': 'mp4',
-                'id': 'mcdonalds-just-unveiled-cosmcsits-new-spinoff-brand',
-                'title': 'Here\'s a first look at McDonald\'s new spinoff brand, CosMc\'s',
-                'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107344192-1701894812493-CosMcsskyHero_2336x1040_hero-desktop.jpg?v=1701894855',
-                'thumbnails': [
-                    {
-                        'url': 'https://image.cnbcfm.com/api/v1/image/107344192-1701894812493-CosMcsskyHero_2336x1040_hero-desktop.jpg?v=1701894855'
-                    }
-                ],
-                'timestamp': 1701977810,
-                'upload_date': '20231207',
-            },
-            'expected_warnings': ['Unable to download f4m manifest'],
+    _TESTS = [{
+        'url': 'https://www.cnbc.com/video/2023/12/07/mcdonalds-just-unveiled-cosmcsits-new-spinoff-brand.html',
+        'info_dict': {
+            'ext': 'mp4',
+            'id': '107344774',
+            'display_id': 'mcdonalds-just-unveiled-cosmcsits-new-spinoff-brand',
+            'modified_timestamp': 1702053483,
+            'timestamp': 1701977810,
+            'channel': 'News Videos',
+            'upload_date': '20231207',
+            'description': 'md5:882c001d85cb43d7579b514307b3e78b',
+            'release_timestamp': 1701977375,
+            'modified_date': '20231208',
+            'release_date': '20231207',
+            'duration': 65,
+            'author': 'Sean Conlon',
+            'title': 'Here\'s a first look at McDonald\'s new spinoff brand, CosMc\'s',
+            'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107344192-1701894812493-CosMcsskyHero_2336x1040_hero-desktop.jpg?v=1701894855',
         },
-        {
-            'url': 'https://www.cnbc.com/video/2023/12/08/jim-cramer-shares-his-take-on-seattles-tech-scene.html',
-            'info_dict': {
-                'author': 'Jim Cramer',
-                'channel': 'Mad Money with Jim Cramer',
-                'description': '\'Mad Money\' host Jim Cramer returns from his trip to the West Coast with insight into the tech scene in Seattle, WA.',
-                'duration': 299.0,
-                'ext': 'mp4',
-                'id': 'jim-cramer-shares-his-take-on-seattles-tech-scene',
-                'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107345481-1702079431MM-B-120823.jpg?v=1702079430',
-                'thumbnails': [
-                    {
-                        'url': 'https://image.cnbcfm.com/api/v1/image/107345481-1702079431MM-B-120823.jpg?v=1702079430'
-                    }
-                ],
-                'timestamp': 1702080139,
-                'title': 'Jim Cramer shares his take on Seattle\'s tech scene',
-                'upload_date': '20231208',
-            },
-            'expected_warnings': ['Unable to download f4m manifest'],
+        'expected_warnings': ['Unable to download f4m manifest'],
+    }, {
+        'url': 'https://www.cnbc.com/video/2023/12/08/jim-cramer-shares-his-take-on-seattles-tech-scene.html',
+        'info_dict': {
+            'author': 'Jim Cramer',
+            'channel': 'Mad Money with Jim Cramer',
+            'description': 'md5:72925be21b952e95eba51178dddf4e3e',
+            'duration': 299.0,
+            'ext': 'mp4',
+            'id': '107345451',
+            'display_id': 'jim-cramer-shares-his-take-on-seattles-tech-scene',
+            'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107345481-1702079431MM-B-120823.jpg?v=1702079430',
+            'timestamp': 1702080139,
+            'title': 'Jim Cramer shares his take on Seattle\'s tech scene',
+            'release_date': '20231208',
+            'upload_date': '20231209',
+            'modified_timestamp': 1702080139,
+            'modified_date': '20231209',
+            'release_timestamp': 1702073551,
         },
-        {
-            'url': 'https://www.cnbc.com/video/2023/12/08/the-epicenter-of-ai-is-in-seattle-says-jim-cramer.html',
-            'info_dict': {
-                'author': 'Jim Cramer',
-                'channel': 'Mad Money with Jim Cramer',
-                'description': '\'Mad Money\' host Jim Cramer returns from his trip to the West Coast with insight into the tech scene in Seattle, WA.',
-                'duration': 113.0,
-                'ext': 'mp4',
-                'id': 'the-epicenter-of-ai-is-in-seattle-says-jim-cramer',
-                'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107345486-Screenshot_2023-12-08_at_70339_PM.png?v=1702080248',
-                'thumbnails': [
-                    {
-                        'url': 'https://image.cnbcfm.com/api/v1/image/107345486-Screenshot_2023-12-08_at_70339_PM.png?v=1702080248'
-                    }
-                ],
-                'timestamp': 1702080535,
-                'title': 'The epicenter of AI is in Seattle, says Jim Cramer',
-                'upload_date': '20231208',
-            },
-            'expected_warnings': ['Unable to download f4m manifest'],
+        'expected_warnings': ['Unable to download f4m manifest'],
+    }, {
+        'url': 'https://www.cnbc.com/video/2023/12/08/the-epicenter-of-ai-is-in-seattle-says-jim-cramer.html',
+        'info_dict': {
+            'author': 'Jim Cramer',
+            'channel': 'Mad Money with Jim Cramer',
+            'description': 'md5:72925be21b952e95eba51178dddf4e3e',
+            'duration': 113.0,
+            'ext': 'mp4',
+            'id': '107345474',
+            'display_id': 'the-epicenter-of-ai-is-in-seattle-says-jim-cramer',
+            'thumbnail': 'https://image.cnbcfm.com/api/v1/image/107345486-Screenshot_2023-12-08_at_70339_PM.png?v=1702080248',
+            'timestamp': 1702080535,
+            'title': 'The epicenter of AI is in Seattle, says Jim Cramer',
+            'release_timestamp': 1702077347,
+            'modified_timestamp': 1702080535,
+            'release_date': '20231208',
+            'upload_date': '20231209',
+            'modified_date': '20231209',
         },
-    ]
+        'expected_warnings': ['Unable to download f4m manifest'],
+    }]
 
     def _real_extract(self, url):
-        display_id = self._match_valid_url(url).group('id')
+        display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
-        metadata = json.loads(
-            self._html_search_regex(
-                r'window\.__s_data=(\{.*?\});', webpage, 'video JSON data.'))
+        data = self._search_json(r'window\.__s_data=', webpage, 'video data', display_id)
 
-        link = traverse_obj(
-            metadata,
-            ('page', 'page', 'layout', 1, 'columns', 0, 'modules', 0, 'data', 'encodings', 0, 'url'), expected_type=url_or_none)
-
-        upload_date = traverse_obj(
-            metadata,
-            ('page', 'page', 'layout', 1, 'columns', 0, 'modules', 0, 'data', 'uploadDate'))
+        player_data = traverse_obj(data, (
+            'page', 'page', 'layout', ..., 'columns', ..., 'modules',
+            lambda _, v: v['name'] == 'clipPlayer', 'data', {dict}), get_all=False)
 
         return {
-            'author': self._html_search_regex(
-                r'<meta name="parsely-author" content="([^<]+)"',
-                webpage,
-                'video author',
-                default=None),
-            'channel': self._html_search_regex(
-                r'<div class="ClipPlayer-clipPlayerIntroSection"><[^>]*>([^<]+)<',
-                webpage,
-                'channel name',
-                fatal=False),
-            'formats': self._extract_akamai_formats(link, display_id),
             'id': display_id,
-            'upload_date': unified_strdate(upload_date) if upload_date else None,
-            'url': link,
-            **traverse_obj(
-                self._search_json_ld(webpage, display_id, default={}),
-                {
-                    'title': 'title',
-                    'description': 'description',
-                    'duration': 'duration',
-                    'timestamp': 'timestamp',
-                    'thumbnails': 'thumbnails',
-                }),
+            'display_id': display_id,
+            'formats': self._extract_akamai_formats(player_data['playbackURL'], display_id),
+            **self._search_json_ld(webpage, display_id, fatal=False),
+            **traverse_obj(player_data, {
+                'id': ('id', {str_or_none}),
+                'title': ('title', {str}),
+                'description': ('description', {str}),
+                'author': ('author', ..., 'name', {str}),
+                'timestamp': ('datePublished', {parse_iso8601}),
+                'release_timestamp': ('uploadDate', {parse_iso8601}),
+                'modified_timestamp': ('dateLastPublished', {parse_iso8601}),
+                'thumbnail': ('thumbnail', {url_or_none}),
+                'duration': ('duration', {int_or_none}),
+                'channel': ('section', 'title', {str}),
+            }, get_all=False),
         }
