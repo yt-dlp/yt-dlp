f55ee1c352e191ffeb0a0cccb77bd7efc1839ef1
sepro <4618135+seproDev@users.noreply.github.com>
2024-02-23 15:47:35 +0100

    Address review comment

--- yt_dlp/extractor/nerdcubed.py
+++ yt_dlp/extractor/nerdcubed.py
@@ -15,23 +15,24 @@ class NerdCubedFeedIE(InfoExtractor):
         'playlist_mincount': 5500,
     }
 
-    def _entries(self, feed):
-        for feed_entry in traverse_obj(feed, ('videos', lambda _, v: v['id'])):
-            yield self.url_result(
-                'https://www.youtube.com/watch?v=' + feed_entry['id'], YoutubeIE,
-                **traverse_obj(feed_entry, {
-                    'id': ('id', {str}),
-                    'title': ('title', {str}),
-                    'description': ('description', {str}),
-                    'timestamp': ('publishedAt', {parse_iso8601}),
-                    'channel': ('source', 'name', {str}),
-                    'channel_id': ('source', 'id', {str}),
-                    'channel_url': ('source', 'url', {str}),
-                    'thumbnail': ('thumbnail', 'source', {url_or_none}),
-                }), url_transparent=True)
+    def _extract_video(self, feed_entry):
+        return self.url_result(
+            f'https://www.youtube.com/watch?v={feed_entry["id"]}', YoutubeIE,
+            **traverse_obj(feed_entry, {
+                'id': ('id', {str}),
+                'title': ('title', {str}),
+                'description': ('description', {str}),
+                'timestamp': ('publishedAt', {parse_iso8601}),
+                'channel': ('source', 'name', {str}),
+                'channel_id': ('source', 'id', {str}),
+                'channel_url': ('source', 'url', {str}),
+                'thumbnail': ('thumbnail', 'source', {url_or_none}),
+            }), url_transparent=True)
 
     def _real_extract(self, url):
         video_id = 'nerdcubed-feed'
         feed = self._download_json('https://www.nerdcubed.co.uk/_/cdn/videos.json', video_id)
 
-        return self.playlist_result(self._entries(feed), video_id, 'nerdcubed.co.uk feed')
+        return self.playlist_result(
+            map(self._extract_video, traverse_obj(feed, ('videos', lambda _, v: v['id']))),
+            video_id, 'nerdcubed.co.uk feed')
