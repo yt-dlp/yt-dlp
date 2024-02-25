3b72f03f4ae88b712afb5683abcc39f21834a85e
SirElderling <decluttered@protonmail.com>
2024-02-25 11:02:17 +0000

    [ZenPorn] - additional adjustments based on PR review

--- yt_dlp/extractor/zenporn.py
+++ yt_dlp/extractor/zenporn.py
@@ -18,13 +18,13 @@ class ZenPornIE(InfoExtractor):
         'url': 'https://zenporn.com/video/15627016/desi-bhabi-ki-chudai',
         'md5': '07bd576b5920714d74975c054ca28dee',
         'info_dict': {
-            'id': '15627016',
-            'extr_id': '9563799',
+            'id': '9563799',
+            'display_id': '15627016',
             'ext': 'mp4',
             'title': 'md5:669eafd3bbc688aa29770553b738ada2',
             'description': '',
             'thumbnail': 'md5:2fc044a19bab450fef8f1931e7920a18',
-            'post_date': '20230925',
+            'upload_date': '20230925',
             'uploader': 'md5:9fae59847f1f58d1da8f2772016c12f3',
             'age_limit': 18
         }
@@ -32,13 +32,13 @@ class ZenPornIE(InfoExtractor):
         'url': 'https://zenporn.com/video/15570701',
         'md5': 'acba0d080d692664fcc8c4e5502b1a67',
         'info_dict': {
-            'id': '15570701',
-            'extr_id': '2297875',
+            'id': '2297875',
+            'display_id': '15570701',
             'ext': 'mp4',
             'title': 'md5:47aebdf87644ec91e8b1a844bc832451',
             'description': '',
             'thumbnail': 'https://mstn.nv7s.com/contents/videos_screenshots/2297000/2297875/480x270/1.jpg',
-            'post_date': '20230921',
+            'upload_date': '20230921',
             'uploader': 'Lois Clarke',
             'age_limit': 18
         }
@@ -46,13 +46,13 @@ class ZenPornIE(InfoExtractor):
         'url': 'https://zenporn.com/video/8531117/amateur-students-having-a-fuck-fest-at-club/',
         'md5': '67411256aa9451449e4d29f3be525541',
         'info_dict': {
-            'id': '8531117',
-            'extr_id': '12791908',
+            'id': '12791908',
+            'display_id': '8531117',
             'ext': 'mp4',
             'title': 'Amateur students having a fuck fest at club',
             'description': '',
             'thumbnail': 'https://tn.txxx.tube/contents/videos_screenshots/12791000/12791908/288x162/1.jpg',
-            'post_date': '20191005',
+            'upload_date': '20191005',
             'uploader': 'Jackopenass',
             'age_limit': 18
         }
@@ -60,13 +60,13 @@ class ZenPornIE(InfoExtractor):
         'url': 'https://zenporn.com/video/15872038/glad-you-came/',
         'md5': '296ccab437f5bac6099433768449d8e1',
         'info_dict': {
-            'id': '15872038',
-            'extr_id': '111585',
+            'id': '111585',
+            'display_id': '15872038',
             'ext': 'mp4',
             'title': 'Glad You Came',
             'description': '',
             'thumbnail': 'https://vpim.m3pd.com/contents/videos_screenshots/111000/111585/480x270/1.jpg',
-            'post_date': '20231024',
+            'upload_date': '20231024',
             'uploader': 'Martin Rudenko',
             'age_limit': 18
         }
@@ -93,36 +93,33 @@ class ZenPornIE(InfoExtractor):
 
         ext_domain, extr_id = self._search_regex(
             r'https://(?P<ext_domain>[\w.-]+\.\w{3})/embed/(?P<extr_id>\d+)/',
-            webpage, 'embed info', group=('ext_domain', 'extr_id'), fatal=True)
+            webpage, 'embed info', group=('ext_domain', 'extr_id'))
 
         info_json = self._download_json(
             self._gen_info_url(ext_domain, extr_id), video_id,
-            note='Downloading video info JSON', fatal=True)
+            note='Downloading video info JSON')
 
         video_json = self._download_json(
             f'https://{ext_domain}/api/videofile.php', video_id, query={
                 'video_id': extr_id,
                 'lifetime': 8640000,
-            }, note='Downloading video metadata JSON', fatal=True)
+            }, note='Downloading video metadata JSON')
 
-        encoded_url = video_json[0]['video_url']
-
-        decoded_url = self._decode_video_url(encoded_url)
+        decoded_url = self._decode_video_url(video_json[0]['video_url'])
         if not decoded_url:
             raise ExtractorError('Unable to decode the video url')
 
         return {
-            'id': video_id,
-            'extr_id': extr_id,
-            'ext': traverse_obj(
-                video_json, (..., 'format', {determine_ext}), get_all=False),
+            'id': extr_id,
+            'display_id': video_id,
+            'ext': traverse_obj(video_json, (0, 'format', {determine_ext})),
             'url': f'https://{ext_domain}{decoded_url}',
             'age_limit': 18,
             **traverse_obj(info_json, ('video', {
                 'title': ('title', {str}),
                 'description': ('description', {str}),
                 'thumbnail': ('thumb', {url_or_none}),
-                'post_date': ('post_date', {unified_strdate}),
+                'upload_date': ('post_date', {unified_strdate}),
                 'uploader': ('user', 'username', {str}),
             })),
         }
