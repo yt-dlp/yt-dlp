c7522c0f04decee907bc16788c73c78094a3799f
sepro <4618135+seproDev@users.noreply.github.com>
2024-03-03 00:20:32 +0100

    Additional cleanup

--- yt_dlp/extractor/dailymotion.py
+++ yt_dlp/extractor/dailymotion.py
@@ -48,31 +48,33 @@ class DailymotionBaseInfoExtractor(InfoExtractor):
     def _get_token(self, xid):
         cookies = self._get_dailymotion_cookies()
         token = self._get_cookie_value(cookies, 'access_token') or self._get_cookie_value(cookies, 'client_token')
-        if not token:
-            data = {
-                'client_id': 'f1a362d288c1b98099c7',
-                'client_secret': 'eea605b96e01c796ff369935357eca920c5da4c5',
-            }
-            username, password = self._get_login_info()
-            if username:
-                data.update({
-                    'grant_type': 'password',
-                    'password': password,
-                    'username': username,
-                })
-            else:
-                data['grant_type'] = 'client_credentials'
-            try:
-                token = self._download_json(
-                    'https://graphql.api.dailymotion.com/oauth/token',
-                    None, 'Downloading Access Token',
-                    data=urlencode_postdata(data))['access_token']
-            except ExtractorError as e:
-                if isinstance(e.cause, HTTPError) and e.cause.status == 400:
-                    raise ExtractorError(self._parse_json(
-                        e.cause.response.read().decode(), xid)['error_description'], expected=True)
-                raise
-            self._set_dailymotion_cookie('access_token' if username else 'client_token', token)
+        if token:
+            return token
+
+        data = {
+            'client_id': 'f1a362d288c1b98099c7',
+            'client_secret': 'eea605b96e01c796ff369935357eca920c5da4c5',
+        }
+        username, password = self._get_login_info()
+        if username:
+            data.update({
+                'grant_type': 'password',
+                'password': password,
+                'username': username,
+            })
+        else:
+            data['grant_type'] = 'client_credentials'
+        try:
+            token = self._download_json(
+                'https://graphql.api.dailymotion.com/oauth/token',
+                None, 'Downloading Access Token',
+                data=urlencode_postdata(data))['access_token']
+        except ExtractorError as e:
+            if isinstance(e.cause, HTTPError) and e.cause.status == 400:
+                raise ExtractorError(self._parse_json(
+                    e.cause.response.read().decode(), xid)['error_description'], expected=True)
+            raise
+        self._set_dailymotion_cookie('access_token' if username else 'client_token', token)
         return token
 
     def _call_api(self, object_type, xid, object_fields, note, filter_extra=None):
@@ -409,7 +411,7 @@ class DailymotionSearchIE(DailymotionPlaylistBaseIE):
         },
         'playlist_mincount': 90,
     }]
-_SEARCH_QUERY = 'query SEARCH_QUERY( $query: String! $page: Int $limit: Int ) { search { videos( query: $query first: $limit page: $page ) { edges { node { xid } } } } } '
+    _SEARCH_QUERY = 'query SEARCH_QUERY( $query: String! $page: Int $limit: Int ) { search { videos( query: $query first: $limit page: $page ) { edges { node { xid } } } } } '
 
     def _call_search_api(self, term, page, note):
         if not self._HEADERS.get('Authorization'):
