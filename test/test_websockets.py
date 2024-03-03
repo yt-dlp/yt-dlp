59c352e7e2b4ddb689c3560b6fb2a8715bd046bc
sepro <4618135+seproDev@users.noreply.github.com>
2024-03-03 03:02:26 +0100

    [test:websockets] Reduce timeout

--- test/test_websockets.py
+++ test/test_websockets.py
@@ -192,8 +192,8 @@ class TestWebsSocketRequestHandlerConformance:
 
     @pytest.mark.parametrize('handler', ['Websockets'], indirect=True)
     @pytest.mark.parametrize('params,extensions', [
-        ({'timeout': 0.00001}, {}),
-        ({}, {'timeout': 0.00001}),
+        ({'timeout': 1e-100}, {}),
+        ({}, {'timeout': 1e-100}),
     ])
     def test_timeout(self, handler, params, extensions):
         with handler(**params) as rh:
