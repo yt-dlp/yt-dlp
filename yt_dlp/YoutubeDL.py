c94c595d859f00468cb7818f7f246a1b376dd8ac
bashonly <bashonly@protonmail.com>
2024-03-08 10:43:42 -0600

    cleanup
    Authored by: bashonly

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -3577,7 +3577,7 @@ class YoutubeDL:
                 self.report_warning(f'The info failed to download: {e}; trying with URL {webpage_url}')
                 self.download([webpage_url])
             except ExtractorError as e:
-                self.report_error(str(e), e.format_traceback())
+                self.report_error(e)
         return self._download_retcode
 
     @staticmethod
