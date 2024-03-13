c7a048bfc66e59bce24ee40b673c7c6b33e877c0
bashonly <bashonly@protonmail.com>
2024-03-13 04:14:36 -0500

    [devscripts] `update-changelog.py`: Simplify
    Authored by: bashonly

--- devscripts/update-changelog.py
+++ devscripts/update-changelog.py
@@ -11,14 +11,6 @@ from devscripts.utils import read_file, read_version, write_file
 
 # Always run after devscripts/update-version.py, and run before `make doc|pypi-files|tar|all`
 
-# If changing HEADER value, must apply identical change to Changelog.md in the same commit
-HEADER = '''# Changelog
-
-<!--
-# To create a release, dispatch the https://github.com/yt-dlp/yt-dlp/actions/workflows/release.yml workflow on master
--->
-'''
-
 if __name__ == '__main__':
-    changelog = read_file('Changelog.md')[len(HEADER):]
-    write_file('Changelog.md', f'{HEADER}\n### {read_version()}\n{create()}\n{changelog}')
+    header, sep, changelog = read_file('Changelog.md').partition('\n### ')
+    write_file('Changelog.md', f'{header}{sep}{read_version()}\n{create()}\n{sep}{changelog}')
