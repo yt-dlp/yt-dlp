006e13d823ec2e50493da939a3d1e4904c2e525a
coletdjnz <coletdjnz@protonmail.com>
2024-03-17 09:22:55 +1300

    Update yt_dlp/options.py
    Co-authored-by: Simon Sawicki <accounts@grub4k.xyz>

--- yt_dlp/options.py
+++ yt_dlp/options.py
@@ -518,8 +518,9 @@ def create_parser():
     network.add_option(
         '--impersonate',
         metavar='CLIENT[:[VERSION][:OS[:OS_VERSION]]]', dest='impersonate', default=None,
-        help='Client to impersonate for requests. E.g. chrome, chrome:110, chrome::android.'
-             ' Pass in an empty string (--impersonate="") to impersonate any client.',
+        help=(
+            'Client to impersonate for requests. E.g. chrome, chrome:110, chrome::android. '
+            'Pass in an empty string (--impersonate="") to impersonate any client.'),
     )
     network.add_option(
         '--list-impersonate-targets',
