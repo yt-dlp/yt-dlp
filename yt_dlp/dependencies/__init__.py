2a3bc465513ff257beda06dcae11bee966c04f23
coletdjnz <coletdjnz@protonmail.com>
2024-03-17 14:55:48 +1300

    move back to curlcffi module

--- yt_dlp/dependencies/__init__.py
+++ yt_dlp/dependencies/__init__.py
@@ -76,8 +76,6 @@ else:
 
 try:
     import curl_cffi
-    if curl_cffi.__version__ != '0.5.10':
-        curl_cffi._yt_dlp__version = f'{curl_cffi.__version__} (unsupported)'
 except ImportError:
     curl_cffi = None
 
