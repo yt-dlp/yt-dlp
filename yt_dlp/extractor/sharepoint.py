521702a9fb0ad6ad6fd58b00c7c043a6a376fb15
bashonly <88596187+bashonly@users.noreply.github.com>
2024-03-19 16:54:09 +0000

    cleanup

--- yt_dlp/extractor/sharepoint.py
+++ yt_dlp/extractor/sharepoint.py
@@ -1,12 +1,5 @@
 import json
-
-from urllib.parse import (
-    parse_qs,
-    urlencode,
-    urljoin,
-    urlparse,
-    urlunparse,
-)
+import urllib.parse
 
 from .common import InfoExtractor
 from ..utils.traversal import traverse_obj
@@ -14,7 +7,6 @@ from ..utils.traversal import traverse_obj
 
 class SharePointIE(InfoExtractor):
     _VALID_URL = r'https?://[\w-]+\.sharepoint\.com/:v:/g/(?:[^/?#]+/){2}(?P<id>[^/?#]+)'
-
     _TESTS = [
         {
             'url': 'https://lut-my.sharepoint.com/:v:/g/personal/juha_eerola_student_lab_fi/EUrAmrktb4ZMhUcY9J2PqMEBD_9x_l0DyYWVgAvp-TTOMw?e=ZpQOOw',
@@ -34,36 +26,31 @@ class SharePointIE(InfoExtractor):
 
         video = self._search_json(r'g_fileInfo\s*=', webpage, 'player config', video_id)
 
-        parsed_url = urlparse(video['.transformUrl'])
-        base_media_url = urlunparse(parsed_url._replace(
-            path=urljoin(f'{parsed_url.path}/', '../videomanifest'),
-            query=urlencode(
-                {
-                    **parse_qs(parsed_url.query),
-                    'cTag': video['.ctag'],
-                    'action': 'Access',
-                    'part': 'index',
-                }, doseq=True,
-            ),
-        ))
+        parsed_url = urllib.parse.urlparse(video['.transformUrl'])
+        base_media_url = urllib.parse.urlunparse(parsed_url._replace(
+            path=urllib.parse.urljoin(f'{parsed_url.path}/', '../videomanifest'),
+            query=urllib.parse.urlencode({
+                **urllib.parse.parse_qs(parsed_url.query),
+                'cTag': video['.ctag'],
+                'action': 'Access',
+                'part': 'index',
+            }, doseq=True)))
 
         # The player is configured with a number of options that are then added to the format URLs,
         # but we always get all formats back no matter what options we send
-        hls_formats = self._extract_m3u8_formats(
-            f'{base_media_url}&format=hls', video_id, m3u8_id='hls', fatal=False)
-        hls_formats.extend(self._extract_m3u8_formats(
-            f'{base_media_url}&format=hls-vnext', video_id, m3u8_id='hls-vnext', fatal=False))
-        for hls_format in hls_formats:
+        formats = []
+        for hls_type in ('hls', 'hls-vnext'):
+            formats.extend(self._extract_m3u8_formats(
+                base_media_url, video_id, m3u8_id=hls_type, query={'format': hls_type}, fatal=False))
+        for hls_format in formats:
             hls_format['quality'] = -2
-        formats = self._extract_mpd_formats(
-            f'{base_media_url}&format=dash', video_id, mpd_id='dash', fatal=False)
-        formats.extend(hls_formats)
+        formats.extend(self._extract_mpd_formats(
+            base_media_url, video_id, mpd_id='dash', query={'format': 'dash'}, fatal=False))
 
         return {
             'id': video_id,
             'formats': formats,
             'title': video.get('name'),
             'duration': traverse_obj(
-                video, ('MediaServiceFastMetadata', {json.loads}, 'media', 'duration', {lambda x: x / 10000000})
-            ),
+                video, ('MediaServiceFastMetadata', {json.loads}, 'media', 'duration', {lambda x: x / 10000000})),
         }
