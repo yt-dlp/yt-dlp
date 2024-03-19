cf13ba97a4f31f5d3609bc9fb93bcac095c670e2
bashonly <88596187+bashonly@users.noreply.github.com>
2024-03-19 14:28:57 +0000

    fixup

--- yt_dlp/extractor/loom.py
+++ yt_dlp/extractor/loom.py
@@ -1,5 +1,10 @@
 from .common import InfoExtractor
-from ..utils import int_or_none, parse_iso8601
+from ..utils import (
+    int_or_none,
+    parse_iso8601,
+    str_or_none,
+    url_or_none,
+)
 from ..utils.traversal import traverse_obj
 
 
@@ -14,6 +19,9 @@ class LoomIE(InfoExtractor):
             'title': 'A Ruler for Windows - 28 March 2022',
             'uploader': 'wILLIAM PIP',
             'upload_date': '20220328',
+            'timestamp': 1648454238,
+            'duration': 27,
+            'uploader_id': '13180341',
         }
     }, {
         'url': 'https://www.loom.com/share/c43a642f815f4378b6f80a889bb73d8d',
@@ -24,14 +32,18 @@ class LoomIE(InfoExtractor):
             'title': 'Lilah Nielsen Intro Video',
             'uploader': 'Lilah Nielsen',
             'upload_date': '20200826',
+            'timestamp': 1598480716,
+            'duration': 20,
+            'uploader_id': '4827100',
         }
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-        metadata = self._search_json(
-            r'window\.loomSSRVideo\s*=', webpage, 'metadata', video_id, fatal=False)
+        metadata = traverse_obj(
+            self._search_json(r'window\.__APOLLO_STATE__\s*=', webpage, 'metadata', video_id, fatal=False),
+            (lambda k, _: k.endswith(f'Video:{video_id}'), {dict}), get_all=False)
         video_url = self._download_json(
             f'https://www.loom.com/api/campaigns/sessions/{video_id}/transcoded-url',
             video_id, 'Downloading video url',  data=b'')['url']
@@ -41,11 +53,12 @@ class LoomIE(InfoExtractor):
             'url': video_url,
             **traverse_obj(metadata, {
                 'title': ('name', {str}),
-                'uploader': ('owner_full_name', {str}),
+                'description': ('description', {str}),
+                'uploader': ('owner', 'display_name', {str}),
+                'uploader_id': ('owner_id', {str_or_none}),
                 'timestamp': ('createdAt', {parse_iso8601}),
-                'view_count': ('total_views', {int_or_none}),
                 'width': ('video_properties', 'width', {int_or_none}),
                 'height': ('video_properties', 'height', {int_or_none}),
-                'filesize': ('video_properties', 'byte_size', {int_or_none}),
+                'duration': ('video_properties', 'duration', {int_or_none}),
             }),
         }
