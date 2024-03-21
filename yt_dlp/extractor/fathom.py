635c031bd91f3bca92ca8506f0ac2d99fb9df0ed
src-tinkerer <overfill0600@proton.me>
2024-03-21 12:46:45 +0330

    [ie/fathom] Refactor and use common functions

--- yt_dlp/extractor/fathom.py
+++ yt_dlp/extractor/fathom.py
@@ -1,12 +1,12 @@
-from datetime import datetime
-import time
 import json
-
 from .common import InfoExtractor
 from ..utils import (
-    get_element_html_by_id,
     extract_attributes,
+    float_or_none,
+    get_element_html_by_id,
+    parse_iso8601,
 )
+from ..utils.traversal import traverse_obj
 
 
 class FathomIE(InfoExtractor):
@@ -19,7 +19,7 @@ class FathomIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'eCom Inucbator - Coaching Session',
             'duration': 8125.380507,
-            'timestamp': 1699036314.0,
+            'timestamp': 1699048914,
             'upload_date': '20231103',
         },
     }, {
@@ -30,34 +30,26 @@ class FathomIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Jon, Lawrence, Neman chat about practice',
             'duration': 3571.517847,
-            'timestamp': 1698921000.0,
+            'timestamp': 1698933600,
             'upload_date': '20231102',
         },
     }]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
 
-        div_app = get_element_html_by_id('app', webpage)
-        attrs = extract_attributes(div_app)
-        data = json.loads(attrs["data-page"])
+        props = traverse_obj(
+            get_element_html_by_id('app', webpage), ({extract_attributes}, 'data-page', {json.loads}, 'props'))
 
-        video_id = str(data['props']['call']['id'])
-        title = data['props']['head']['title']
-        duration = data['props']['duration']
-        upload_date = datetime.fromisoformat(data['props']['call']['started_at'])
-        timestamp = time.mktime(upload_date.timetuple())
-
-        source_url = data['props']['call']['video_url']
-
-        formats = self._extract_m3u8_formats(
-            source_url, video_id, 'mp4', entry_protocol='m3u8_native', fatal=False)
+        video_id = str(props['call']['id'])
 
         return {
             'id': video_id,
-            'title': title,
-            'duration': duration,
-            'timestamp': timestamp,
-            'formats': formats,
+            'formats': self._extract_m3u8_formats(props['call']['video_url'], video_id, 'mp4'),
+            **traverse_obj(props, {
+                'title': ('head', 'title', {str}),
+                'duration': ('duration', {float_or_none}),
+                'timestamp': ('call', 'started_at', {parse_iso8601}),
+            }),
         }
