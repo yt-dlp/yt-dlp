c121ca87529b228b2131257ab3e4d75c188cba3b
Offert4324 <104715700+Offert4324@users.noreply.github.com>
2024-03-23 23:17:46 +0900

    Update medici.py

--- yt_dlp/extractor/medici.py
+++ yt_dlp/extractor/medici.py
@@ -1,67 +1,90 @@
+from urllib.parse import unquote
 from .common import InfoExtractor
 from ..utils import (
     unified_strdate,
-    update_url_query,
-    urlencode_postdata,
+    str_or_none,
+    traverse_obj,
 )
 
 
 class MediciIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?medici\.tv/#!/(?P<id>[^?#&]+)'
-    _TEST = {
-        'url': 'http://www.medici.tv/#!/daniel-harding-frans-helmerson-verbier-festival-music-camp',
-        'md5': '004c21bb0a57248085b6ff3fec72719d',
-        'info_dict': {
-            'id': '3059',
-            'ext': 'flv',
-            'title': 'Daniel Harding conducts the Verbier Festival Music Camp \u2013 With Frans Helmerson',
-            'description': 'md5:322a1e952bafb725174fd8c1a8212f58',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20170408',
+    _VALID_URL = r'https?://(?:www\.)?(?:medici|edu\.medici)\.tv/(?:(?:[a-zA-Z0-9-_\.]+)/)*(?:#!/)?(?P<id>[^?#&]+)'
+    _TESTS = [
+        {
+            'url': 'https://www.medici.tv/en/operas/thomas-ades-the-exterminating-angel-calixto-bieito-opera-bastille-paris',
+            'md5': 'd483f74e7a7a9eac0dbe152ab189050d',
+            'info_dict': {
+                'id': '8032',
+                'ext': 'mp4',
+                'title': 'Thomas Adès\'s The Exterminating Angel',
+                'thumbnail': r're:^https?://medicitv-a\.imgix\.net/movie/[^?]+\.jpg(?:\?[^?]+)?',
+                'description': 'md5:708ae6350dadc604225b4a6e32482bab',
+                'upload_date': '20240304',
+            },
         },
-    }
+        {
+            'url': 'https://edu.medici.tv/en/operas/wagner-lohengrin-paris-opera-kirill-serebrennikov-piotr-beczala-kwangchul-youn-johanni-van-oostrum',
+            'md5': '4ef3f4079a6e1c617584463a9eb84f99',
+            'info_dict': {
+                'id': '7900',
+                'ext': 'mp4',
+                'title': 'Wagner\'s Lohengrin',
+                'thumbnail': r're:^https?://medicitv-a\.imgix\.net/movie/[^?]+\.jpg(?:\?[^?]+)?',
+                'description': 'md5:a384a62937866101f86902f21752cd89',
+                'upload_date': '20231017',
+            },
+        },
+    ]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        # Sets csrftoken cookie
         self._download_webpage(url, video_id)
+        cookies = self._get_cookies(url)
 
-        MEDICI_URL = 'http://www.medici.tv/'
+        if "edu" in url:
+            request_url = f'https://api.medici.tv/edu-satie/edito/movie-file/{video_id}/'
+            source_url = 'https://edu.medici.tv'
+        else:
+            request_url = f'https://api.medici.tv/satie/edito/movie-file/{video_id}/'
+            source_url = 'https://www.medici.tv'
+
+        mAuth_cookie = cookies.get('auth._token.mAuth')
+        if not mAuth_cookie:
+            self.to_screen('[auth] Can\'t find a token for authorization. Only previews may be downloaded.')
+            token = ''
+        else:
+            token = unquote(str_or_none(mAuth_cookie.value, ''))
 
         data = self._download_json(
-            MEDICI_URL, video_id,
-            data=urlencode_postdata({
-                'json': 'true',
-                'page': '/%s' % video_id,
-                'timezone_offset': -420,
-            }), headers={
-                'X-CSRFToken': self._get_cookies(url)['csrftoken'].value,
-                'X-Requested-With': 'XMLHttpRequest',
-                'Referer': MEDICI_URL,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            })
+            request_url, video_id,
+            headers={
+                'Authorization': token,
+                'Device-Type': 'web',
+                'Origin': source_url,
+                'Referer': source_url + '/',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'en',
+            }
+        )
 
-        video = data['video']['videos']['video1']
+        id = str_or_none(data.get('id'))
+        title = data.get('title')
+        description = data.get('subtitle')
 
-        title = video.get('nom') or data['title']
+        m3u8_url = traverse_obj(data, ('video', 'video_url'), expected_type=str)
 
-        video_id = video.get('id') or video_id
-        formats = self._extract_f4m_formats(
-            update_url_query(video['url_akamai'], {
-                'hdcore': '3.1.0',
-                'plugin=aasp': '3.1.0.43.124',
-            }), video_id, f4m_id='hds')
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, video_id, ext='mp4')
 
-        description = data.get('meta_description')
-        thumbnail = video.get('url_thumbnail') or data.get('main_image')
-        upload_date = unified_strdate(data['video'].get('date'))
+        thumbnail = data.get('picture')
+        upload_date = unified_strdate(data.get('date_publish'))
 
         return {
-            'id': video_id,
+            'id': id,
             'title': title,
             'description': description,
             'thumbnail': thumbnail,
             'upload_date': upload_date,
             'formats': formats,
+            'subtitles': subtitles,
         }
