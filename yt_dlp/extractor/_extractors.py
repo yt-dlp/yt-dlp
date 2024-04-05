b85f7e72a83d776bc7d21dcb9d323fe0df62c2c4
bashonly <bashonly@protonmail.com>
2024-04-05 18:42:52 -0500

    [ie/jiosaavn] Support playlists
    Authored by: bashonly

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -876,6 +876,7 @@ from .jeuxvideo import JeuxVideoIE
 from .jiosaavn import (
     JioSaavnSongIE,
     JioSaavnAlbumIE,
+    JioSaavnPlaylistIE,
 )
 from .jove import JoveIE
 from .joj import JojIE
