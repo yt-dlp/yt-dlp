383b125feb3dbdbe89f380c2b4329447ad059b19
bashonly <bashonly@protonmail.com>
2024-04-07 15:36:28 -0500

    revert to `ValueError`
    Authored by: bashonly

--- yt_dlp/extractor/jiosaavn.py
+++ yt_dlp/extractor/jiosaavn.py
@@ -2,7 +2,6 @@ import functools
 
 from .common import InfoExtractor
 from ..utils import (
-    ExtractorError,
     format_field,
     int_or_none,
     js_to_json,
@@ -23,9 +22,9 @@ class JioSaavnBaseIE(InfoExtractor):
     def requested_bitrates(self):
         requested_bitrates = self._configuration_arg('bitrate', ['128', '320'], ie_key='JioSaavn')
         if invalid_bitrates := set(requested_bitrates) - self._VALID_BITRATES:
-            raise ExtractorError(
+            raise ValueError(
                 f'Invalid bitrate(s): {", ".join(invalid_bitrates)}. '
-                + f'Valid bitrates are: {", ".join(sorted(self._VALID_BITRATES, key=int))}', expected=True)
+                + f'Valid bitrates are: {", ".join(sorted(self._VALID_BITRATES, key=int))}')
         return requested_bitrates
 
     def _extract_formats(self, song_data):
