5b5891e48effdd1c7fc7307ec9c46cbcda1dd755
Ludovico Gerardi <gerardiludo@gmail.com>
2024-04-11 17:44:21 +0200

    [hytale] Use CloudflareStreamIE explicitly
    Since CloudflareStreamIE is always used as the "final" extractor,
    making explicit use of it doesn't require yt-dlp to find the correct
    one itself.

--- yt_dlp/extractor/hytale.py
+++ yt_dlp/extractor/hytale.py
@@ -49,7 +49,7 @@ class HytaleIE(InfoExtractor):
         entries = [
             self.url_result(
                 f'https://cloudflarestream.com/{video_hash}/manifest/video.mpd?parentOrigin=https%3A%2F%2Fhytale.com',
-                title=self._titles.get(video_hash), url_transparent=True)
+                ie='CloudflareStream', title=self._titles.get(video_hash), url_transparent=True)
             for video_hash in re.findall(
                 r'<stream\s+class\s*=\s*"ql-video\s+cf-stream"\s+src\s*=\s*"([a-f0-9]{32})"',
                 webpage)
