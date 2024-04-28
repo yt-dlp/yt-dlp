f2b7821c18604799438cab1e8234ac0856fef210
bashonly <bashonly@protonmail.com>
2024-04-28 16:38:28 -0500

    remove pyinstaller name arg
    Authored by: bashonly

--- bundle/docker/static/entrypoint.sh
+++ bundle/docker/static/entrypoint.sh
@@ -5,7 +5,7 @@ source ~/.local/share/pipx/venvs/pyinstaller/bin/activate
 python -m devscripts.install_deps --include secretstorage
 python -m devscripts.make_lazy_extractors
 python devscripts/update-version.py -c "${channel}" -r "${origin}" "${version}"
-python -m bundle.pyinstaller --name yt-dlp_linux
+python -m bundle.pyinstaller
 deactivate
 
 source ~/.local/share/pipx/venvs/staticx/bin/activate
