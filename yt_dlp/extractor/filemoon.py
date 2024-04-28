fb267df8a602b2d4e78ec7a41856385e3e018e38
mrsnsercan <130498730+mrsnsercan@users.noreply.github.com>
2024-04-28 23:46:53 +0300

    Update filemoon.py

--- yt_dlp/extractor/filemoon.py
+++ yt_dlp/extractor/filemoon.py
@@ -1,23 +1,44 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import re
+
 from .common import InfoExtractor
-from ..utils import decode_packed_codes, try_get
+from ..utils import (
+    decode_packed_codes,
+    js_to_json,
+)
 
 
-class FilemoonIE(InfoExtractor):
-    _VALID_URL = r'https?://filemoon\.sx/[e,d]/(?P<id>[^\/$]+)' 
-    _SITE_URL = 'https://filemoon.sx'
-
-    def _real_extract(self, url):
-        videoid = self._match_id(url)
-        _url = f"{self._SITE_URL}/d/{videoid}"
-        webpage = try_get(ie._download_webpage(_url, videoid), lambda x: re.sub('[\t\n]', '', x))
-        packed = self._search_regex(r'<script data-cfasync=[^>]+>eval\((.+)\)</script>', webpage, 'packed code')
-        unpacked = decode_packed_codes(packed)
-        m3u8_url = try_get(re.search(r'file:"(?P<url>[^"]+)"', unpacked), lambda x: x.group('url'))        
-        formats = self._extract_m3u8_formats(m3u8_url, videoid, ext="mp4", entry_protocol='m3u8_native', m3u8_id="hls"
-        title = self._html_extract_title(webpage)
-        return {
-            'id': videoid,
-            'title': title,
-            'formats': formats,
+class FileMoonIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?filemoon\.sx/./(?P<id>\w+)'
+    _TEST = {
+        'url': 'https://filemoon.sx/e/dw40rxrzruqz',
+        'md5': '5a713742f57ac4aef29b74733e8dda01',
+        'info_dict': {
+            'id': 'dw40rxrzruqz',
+            'title': 'dw40rxrzruqz',
             'ext': 'mp4'
         }
+    }
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+
+        webpage = self._download_webpage(url, video_id)
+        matches = re.findall(r'(?s)(eval.*?)</script>', webpage)
+        packed = matches[-1]
+        unpacked = decode_packed_codes(packed)
+        jwplayer_sources = self._parse_json(
+            self._search_regex(
+                r'(?s)player\s*\.\s*setup\s*\(\s*\{\s*sources\s*:\s*(.*?])', unpacked, 'jwplayer sources'),
+            video_id, transform_source=js_to_json)
+
+        formats = self._parse_jwplayer_formats(jwplayer_sources, video_id)
+
+        return {
+            'id': video_id,
+            'title': self._generic_title(url) or video_id,
+            'formats': formats
+        }
+        
