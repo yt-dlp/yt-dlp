582510fdcb5b52c696748b872737b1303fa42c2e
Simon Sawicki <contact@grub4k.xyz>
2024-04-28 20:16:51 +0200

    Cleanup

--- bundle/docker/static/entrypoint.sh
+++ bundle/docker/static/entrypoint.sh
@@ -5,10 +5,10 @@ source ~/.local/share/pipx/venvs/pyinstaller/bin/activate
 python -m devscripts.install_deps --include secretstorage
 python -m devscripts.make_lazy_extractors
 python devscripts/update-version.py -c "${channel}" -r "${origin}" "${version}"
-python -m bundle.pyinstaller --name yt-dlp_static
+python -m bundle.pyinstaller --name yt-dlp_linux
 deactivate
 
 source ~/.local/share/pipx/venvs/staticx/bin/activate
-staticx /yt-dlp/dist/yt-dlp_static /build/yt-dlp_static
+staticx /yt-dlp/dist/yt-dlp_linux /build/yt-dlp_linux
 deactivate
-chmod +rx /build/yt-dlp_static
+chmod +rx /build/yt-dlp_linux
