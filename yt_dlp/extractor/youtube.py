4cf46e29c1c3dc8efb53cadc36578733cd0ede26
pukkandan <pukkandan.ytdlp@gmail.com>
2024-05-12 23:18:48 +0530

    Update yt_dlp/extractor/youtube.py
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/youtube.py
+++ yt_dlp/extractor/youtube.py
@@ -3648,7 +3648,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
 
     def _get_requested_clients(self, url, smuggled_data):
         requested_clients = []
-        default = ['ios', 'web']
+        default = ['ios', 'android', 'web']
         allowed_clients = sorted(
             (client for client in INNERTUBE_CLIENTS.keys() if client[:1] != '_'),
             key=lambda client: INNERTUBE_CLIENTS[client]['priority'], reverse=True)
