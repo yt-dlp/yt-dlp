e81cd887306c4b32a617ff62d924b3cc0f38c058
feederbox826 <feederbox826@users.noreply.github.com>
2024-05-13 18:40:54 -0400

    revert renaming username to email

--- yt_dlp/extractor/pornhub.py
+++ yt_dlp/extractor/pornhub.py
@@ -77,8 +77,8 @@ class PornHubBaseIE(InfoExtractor):
         # on both sites so that we have to identify them as the same video.
         # For that purpose we have to keep both in the same extractor
         # but under different netrc machines.
-        email, password = self._get_login_info(netrc_machine=site)
-        if email is None:
+        username, password = self._get_login_info(netrc_machine=site)
+        if username is None:
             return
 
         login_url = 'https://www.%s/%slogin' % (host, 'premium/' if 'premium' in host else '')
@@ -97,7 +97,7 @@ class PornHubBaseIE(InfoExtractor):
         login_form = self._hidden_inputs(login_page)
 
         login_form.update({
-            'email': email,
+            'email': username,
             'password': password,
         })
 
