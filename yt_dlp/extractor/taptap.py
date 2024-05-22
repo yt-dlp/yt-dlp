bf460863b20edab9505ce2a146ffdda6869698aa
c-basalt <117849907+c-basalt@users.noreply.github.com>
2024-05-22 01:52:43 -0400

    Apply suggestions from code review
    Co-authored-by: sepro <4618135+seproDev@users.noreply.github.com>

--- yt_dlp/extractor/taptap.py
+++ yt_dlp/extractor/taptap.py
@@ -16,7 +16,7 @@ class TapTapBaseIE(InfoExtractor):
     _VIDEO_API = 'https://www.taptap.cn/webapiv2/video-resource/v1/multi-get'
     _INFO_API = None
     _INFO_QUERY_KEY = 'id'
-    _DATA_PATH = ('data')
+    _DATA_PATH = None
     _ID_PATH = None
     _META_PATH = None
 
@@ -24,8 +24,8 @@ class TapTapBaseIE(InfoExtractor):
         query = {**query, 'X-UA': self._X_UA.format(uuid=uuid.uuid4())}
         return self._download_json(url, video_id, query=query, **kwargs)['data']
 
-    def _extract_video(self, video_id, is_intl=False):
-        video_data = self._get_api(self._VIDEO_API, video_id, query={'video_ids': video_id})['data']['list'][0]
+    def _extract_video(self, video_id):
+        video_data = self._get_api(self._VIDEO_API, video_id, query={'video_ids': video_id})['list'][0]
 
         video_url = traverse_obj(video_data, ('play_url', ('url_h265', 'url'), {url_or_none}))[0]
         formats = self._extract_m3u8_formats(video_url, video_id)
@@ -69,7 +69,9 @@ class TapTapMomentIE(TapTapBaseIE):
     _ID_PATH = ('moment', 'topic', (('videos', ...), 'pin_video'), 'video_id')
     _META_PATH = ('moment', {
         'timestamp': ('created_time', {int_or_none}),
+        'modified_timestamp': ('edited_time', {int_or_none}),
         'uploader': ('author', 'user', 'name', {str}),
+        'uploader_id': ('author', 'user', 'id', {int}, {str_or_none}),
         'title': ('topic', 'title', {str}),
         'description': ('topic', 'summary', {str}),
     })
@@ -81,7 +83,10 @@ class TapTapMomentIE(TapTapBaseIE):
             'description': 'md5:cf66f7819d413641b8b28c8543f4ecda',
             'timestamp': 1633453402,
             'upload_date': '20211005',
+            'modified_timestamp': 1633453402,
+            'modified_date': '20211005',
             'uploader': '乌酱',
+            'uploader_id': '532896',
         },
         'playlist_count': 1,
         'playlist': [{
@@ -93,10 +98,14 @@ class TapTapMomentIE(TapTapBaseIE):
                 'duration': 66,
                 'timestamp': 1633453402,
                 'upload_date': '20211005',
+                'modified_timestamp': 1633453402,
+                'modified_date': '20211005',
                 'uploader': '乌酱',
+                'uploader_id': '532896',
                 'thumbnail': r're:^https?://.*\.(png|jpg)',
             }
-        }]
+        }],
+        'params': {'skip_download': 'm3u8'},
     }, {
         'url': 'https://www.taptap.cn/moment/521630629209573493',
         'info_dict': {
@@ -105,7 +114,10 @@ class TapTapMomentIE(TapTapBaseIE):
             'description': 'md5:2c81245da864428c904d53ae4ad2182b',
             'timestamp': 1711425600,
             'upload_date': '20240326',
+            'modified_timestamp': 1711425600,
+            'modified_date': '20240326',
             'uploader': '崩坏：星穹铁道',
+            'uploader_id': '414732580',
         },
         'playlist_count': 1,
         'playlist': [{
@@ -117,10 +129,29 @@ class TapTapMomentIE(TapTapBaseIE):
                 'duration': 173,
                 'timestamp': 1711425600,
                 'upload_date': '20240326',
+                'modified_timestamp': 1711425600,
+                'modified_date': '20240326',
                 'uploader': '崩坏：星穹铁道',
+                'uploader_id': '414732580',
                 'thumbnail': r're:^https?://.*\.(png|jpg)',
             }
-        }]
+        }],
+        'params': {'skip_download': 'm3u8'},
+    }, {
+        'url': 'https://www.taptap.cn/moment/540493587511511299',
+        'playlist_count': 2,
+        'info_dict': {
+            'id': '540493587511511299',
+            'title': '中式民俗解谜《纸嫁衣7》、新系列《纸不语》公布！',
+            'description': 'md5:d60842350e686ddb242291ddfb8e39c9',
+            'timestamp': 1715920200,
+            'upload_date': '20240517',
+            'modified_timestamp': 1715942225,
+            'modified_date': '20240517',
+            'uploader': 'TapTap 编辑',
+            'uploader_id': '7159244',
+        },
+        'params': {'skip_download': 'm3u8'},
     }]
 
 
@@ -158,25 +189,53 @@ class TapTapAppIE(TapTapBaseIE):
                 'duration': 295,
                 'thumbnail': r're:^https?://.*\.(png|jpg)',
             }
-        }]
+        }],
+        'params': {'skip_download': 'm3u8'},
     }]
 
 
-class TapTapAppIntlIE(TapTapAppIE, TapTapIntlBase):
+class TapTapAppIntlIE(TapTapIntlBase):
     _VALID_URL = r'https?://www\.taptap\.io/app/(?P<id>\d+)'
     _INFO_API = 'https://www.taptap.io/webapiv2/i/app/v5/detail'
-    _DATA_PATH = ('data', 'app')
+    _DATA_PATH = 'app'
+    _ID_PATH = (('app_videos', 'videos'), ..., 'video_id')
+    _META_PATH = {
+        'title': ('title', {str}),
+        'description': ('description', 'text', {str}, {clean_html}),
+    }
+    _TESTS = [{
+        'url': 'https://www.taptap.io/app/233287',
+        'info_dict': {
+            'id': '233287',
+            'title': '《虹彩六號 M》',
+            'description': 'md5:418285f9c15347fc3cf3e3a3c649f182',
+        },
+        'playlist_count': 1,
+        'playlist': [{
+            'info_dict': {
+                'id': '2149708997',
+                'ext': 'mp4',
+                'title': '《虹彩六號 M》',
+                'description': 'md5:418285f9c15347fc3cf3e3a3c649f182',
+                'duration': 78,
+                'thumbnail': r're:^https?://.*\.(png|jpg)',
+            }
+        }],
+        'params': {'skip_download': 'm3u8'},
+    }]
 
 
-class TapTapPostIntlIE(TapTapAppIntlIE):
+class TapTapPostIntlIE(TapTapIntlBase):
     _VALID_URL = r'https?://www\.taptap\.io/post/(?P<id>\d+)'
     _INFO_API = 'https://www.taptap.io/webapiv2/creation/post/v1/detail'
     _INFO_QUERY_KEY = 'id_str'
-    _DATA_PATH = ('data', 'post')
+    _DATA_PATH = 'post'
     _ID_PATH = ((('videos', ...), 'pin_video'), 'video_id')
     _META_PATH = {
         'timestamp': ('published_time', {int_or_none}),
+        'modified_timestamp': ('edited_time', {int_or_none}),
         'uploader': ('user', 'name', {str}),
+        'uploader_id': ('user', 'id', {int}, {str_or_none}),
         'title': ('title', {str}),
         'description': ('list_fields', 'summary', {str}),
     }
@@ -188,7 +247,10 @@ class TapTapPostIntlIE(TapTapAppIntlIE):
             'description': 'md5:f7717c13f6d3108e22db7303e6690bf7',
             'timestamp': 1614664951,
             'upload_date': '20210302',
+            'modified_timestamp': 1614664951,
+            'modified_date': '20210302',
             'uploader': 'TapTap Editor',
+            'uploader_id': '80224473',
         },
         'playlist_count': 1,
         'playlist': [{
@@ -200,8 +262,12 @@ class TapTapPostIntlIE(TapTapAppIntlIE):
                 'duration': 122,
                 'timestamp': 1614664951,
                 'upload_date': '20210302',
+                'modified_timestamp': 1614664951,
+                'modified_date': '20210302',
                 'uploader': 'TapTap Editor',
+                'uploader_id': '80224473',
                 'thumbnail': r're:^https?://.*\.(png|jpg)',
             }
-        }]
+        }],
+        'params': {'skip_download': 'm3u8'},
     }]
