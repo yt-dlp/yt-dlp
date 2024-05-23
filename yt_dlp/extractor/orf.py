c0072b6d0a815544cfac4146954710019641a3b0
sepro <4618135+seproDev@users.noreply.github.com>
2024-05-23 06:22:53 +0200

    Update yt_dlp/extractor/orf.py

--- yt_dlp/extractor/orf.py
+++ yt_dlp/extractor/orf.py
@@ -605,7 +605,7 @@ class ORFONIE(InfoExtractor):
         api_json = self._download_json(
             f'https://api-tvthek.orf.at/api/v4.3/public/episode/encrypted/{encrypted_id}', video_id)
 
-        if api_json.get('is_drm_protected'):
+        if traverse_obj(api_json, 'is_drm_protected'):
             self.report_drm(video_id)
 
         formats, subtitles = [], {}
