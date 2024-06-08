d73bce492877b9768b40f32b6f12e4c9d8dd0d86
hui1601 <hui1601@naver.com>
2024-06-08 23:48:01 +0900

    [soopglobal] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1874,6 +1874,10 @@ from .sonyliv import (
     SonyLIVIE,
     SonyLIVSeriesIE,
 )
+from .soopglobal import (
+    SoopGlobalLiveIE,
+    SoopGlobalVodIE,
+)
 from .soundcloud import (
     SoundcloudEmbedIE,
     SoundcloudIE,
