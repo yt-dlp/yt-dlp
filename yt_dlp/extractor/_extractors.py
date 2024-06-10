28445dbfbfb28f86f5b92eff3134e7ce0a16a957
ajnar <alvear.devs@gmail.com>
2024-06-10 20:59:18 -0300

    [MercadoLibre] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1111,6 +1111,7 @@ from .megatvcom import (
 )
 from .meipai import MeipaiIE
 from .melonvod import MelonVODIE
+from .mercadolibre import MercadoLibreIE
 from .metacritic import MetacriticIE
 from .mgtv import MGTVIE
 from .microsoftembed import MicrosoftEmbedIE
