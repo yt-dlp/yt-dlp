496748848ec14d589a6dd04dd0f86f621dd9e9d9
bashonly <bashonly@protonmail.com>
2024-06-13 11:38:35 -0500

    [cookies] imports cleanup
    Authored by: bashonly

--- yt_dlp/cookies.py
+++ yt_dlp/cookies.py
@@ -2,6 +2,7 @@ import base64
 import collections
 import contextlib
 import datetime as dt
+import functools
 import glob
 import hashlib
 import http.cookiejar
@@ -24,8 +25,6 @@ from .aes import (
     aes_gcm_decrypt_and_verify_bytes,
     unpad_pkcs7,
 )
-
-from .compat import functools  # isort: split
 from .compat import compat_os_name
 from .dependencies import (
     _SECRETSTORAGE_UNAVAILABLE_REASON,
