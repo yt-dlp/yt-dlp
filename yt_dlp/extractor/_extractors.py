96c8ce3b4ffef59aae06f3ff809e26997999b8ea
DinhHuy2010 <huy04052010@outlook.com>
2024-06-13 16:48:38 +0700

    add vtv.vn

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2372,7 +2372,7 @@ from .vrt import (
     VrtNUIE,
 )
 from .vtm import VTMIE
-from .vtvgo import VTVgoVideoIE
+from .vtvgo import VTVgoVideoIE, VTVvnIE
 from .vuclip import VuClipIE
 from .vvvvid import (
     VVVVIDIE,
--- yt_dlp/extractor/vtvgo.py
+++ yt_dlp/extractor/vtvgo.py
@@ -1,10 +1,12 @@
+from urllib.parse import urlparse
+
 from .common import InfoExtractor
 
 
 class VTVgoVideoIE(InfoExtractor):
     _VALID_URL = [
-        r'https://vtvgo\.vn/(kho-video|tin-tuc)/(?P<id>.*)\.html',
-        r'https://vtvgo.vn/digital/detail\.php\?(?:digital_id=\d+&)?content_id=(?P<id>\d+)',
+        r'(?:https://)?(?:www\.)?vtvgo\.vn/(kho-video|tin-tuc)/(?P<id>.*)\.html',
+        r'(?:https://)?(?:www\.)?vtvgo\.vn/digital/detail\.php\?(?:digital_id=\d+&)?content_id=(?P<id>\d+)',
     ]
     _TESTS = [
         {
@@ -66,6 +68,32 @@ class VTVgoVideoIE(InfoExtractor):
             'id': video_id,
             'title': self._og_search_title(webpage, default=None),
             'description': self._og_search_description(webpage, default=None),
-            'thumbnail': self._og_search_property('image', webpage, 'thumbnail', default=None),
+            'thumbnail': self._og_search_thumbnail(webpage, default=None),
+            'formats': formats,
+        }
+
+class VTVvnIE(InfoExtractor):
+    _VALID_URL = r'(?:https://)?(?:www\.)?vtv\.vn/video/(?P<id>.+)\.htm'
+    _TESTS = [{
+        'url': 'https://vtv.vn/video/thoi-su-20h-vtv1-12-6-2024-680411.htm',
+        'info_dict': {
+            'id': 'thoi-su-20h-vtv1-12-6-2024-680411',
+            'ext': 'mp4',
+            'title': 'Thời sự 20h VTV1 - 12/6/2024 - Video đã phát trên VTV1 | VTV.VN',
+            'thumbnail': 'https://cdn-images.vtv.vn/zoom/600_315/66349b6076cb4dee98746cf1/2024/06/12/thumb/1206-ts-20h-02929741475480320806760.mp4/thumb0.jpg',
+        },
+    }]
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+        webpage = self._download_webpage(url, video_id)
+        data_vid = self._html_search_regex(r'(\"vtv\.mediacdn\.vn/.+\.mp4\")', webpage, 'data-vid', group=1)
+        parsed = urlparse(f'https://{data_vid[1:][:-1]}') # type: ignore
+        m3u8_url = f'https://cdn-videos.vtv.vn/{parsed.path}/master.m3u8'
+        formats = self._extract_m3u8_formats(m3u8_url, video_id, 'mp4')
+        return {
+            'id': video_id,
+            'title': self._og_search_title(webpage, default=None),
+            'thumbnail': self._og_search_thumbnail(webpage, default=None),
             'formats': formats,
         }
