f36c4af8a1ceae7af8342e9cf2fe4541420b3534
Moritz Krüger <moritz.krueger@fn.de>
2024-07-10 01:11:42 +0200

    Minor formatting fix (https://github.com/yt-dlp/yt-dlp/issues/10407)

--- yt_dlp/extractor/tiktok.py
+++ yt_dlp/extractor/tiktok.py
@@ -1462,8 +1462,8 @@ class TikTokLiveIE(TikTokBaseIE):
             # first traverse_obj appears to be deprecated, but remains for now to ensure backwards compatability
             room_id = (traverse_obj(data, ('UserModule', 'users', ..., 'roomId', {str_or_none}), get_all=False)
                        or self._search_regex(r'snssdk\d*://live\?room_id=(\d+)', webpage, 'room ID', default=None)
-                       or room_id
-                       or (traverse_obj(data, ('LiveRoom', "liveRoomUserInfo", "user", "roomId"), get_all=False)))
+                       or (traverse_obj(data, ('LiveRoom', "liveRoomUserInfo", "user", "roomId"), get_all=False))
+                       or room_id)
             uploader = uploader or traverse_obj(
                 data, ('LiveRoom', 'liveRoomUserInfo', 'user', 'uniqueId'),
                 ('UserModule', 'users', ..., 'uniqueId'), get_all=False, expected_type=str)
