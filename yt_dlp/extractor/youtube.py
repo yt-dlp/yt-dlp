97a85de82f16c84cdac77c0abb6e7c5c709b37cf
sepro <4618135+seproDev@users.noreply.github.com>
2024-07-13 21:22:10 +0200

    [ie/youtube] Ignore initial player response on poToken experiment
    Authored by: seproDev

--- yt_dlp/extractor/youtube.py
+++ yt_dlp/extractor/youtube.py
@@ -1294,6 +1294,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         '401': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'vcodec': 'av01.0.12M.08'},
     }
     _SUBTITLE_FORMATS = ('json3', 'srv1', 'srv2', 'srv3', 'ttml', 'vtt')
+    _POTOKEN_EXPERIMENTS = ('51217476', '51217102')
 
     _GEO_BYPASS = False
 
@@ -3703,8 +3704,15 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
     def _extract_player_responses(self, clients, video_id, webpage, master_ytcfg, smuggled_data):
         initial_pr = None
         if webpage:
-            initial_pr = self._search_json(
-                self._YT_INITIAL_PLAYER_RESPONSE_RE, webpage, 'initial player response', video_id, fatal=False)
+            experiments = traverse_obj(master_ytcfg, (
+                'WEB_PLAYER_CONTEXT_CONFIGS', ..., 'serializedExperimentIds', {str}, {lambda x: x.split(',')}, ..., {str}))
+            if all(x in experiments for x in self._POTOKEN_EXPERIMENTS):
+                self.report_warning(
+                    'Webpage contains broken formats (poToken experiment detected). Ignoring initial player response')
+                master_ytcfg = self._get_default_ytcfg()
+            else:
+                initial_pr = self._search_json(
+                    self._YT_INITIAL_PLAYER_RESPONSE_RE, webpage, 'initial player response', video_id, fatal=False)
 
         prs = []
         if initial_pr and not self._invalid_player_response(initial_pr, video_id):
