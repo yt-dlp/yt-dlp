9de688435c6e65a22fdd58b0522ff02df2cc1aa0
grqx_wsl <173253225+grqx@users.noreply.github.com>
2024-07-24 00:17:07 +1200

    clean up, format code, change audio GenericIE fallback rules, clarify variable names, delete debug output

--- yt_dlp/extractor/germanupa.py
+++ yt_dlp/extractor/germanupa.py
@@ -6,9 +6,8 @@ from ..utils import traverse_obj
 
 
 class GermanupaIE(InfoExtractor):
-    _VALID_URL = r'https?://germanupa\.de/(?!media\b)[\w-]+/(?P<id>[\w-]+)'
-    _IFRAME_RE = r'<iframe[^>]+data-src\s*?=\s*?([\'"])(?P<url>https://germanupa\.de/media/oembed\?url=(?:(?!\1).)+)\1'
-    _LOGIN_REQUIRED_RE = r'<div[^>]+class\s*?=\s*?([\'"])(?:(?!\1).)*login-wrapper(?:(?!\1).)*\1'
+    IE_DESC = 'germanupa.de'
+    _VALID_URL = r'https?://germanupa\.de/mediathek/(?P<id>[\w-]+)'
     _TESTS = [{
         'url': 'https://germanupa.de/mediathek/4-figma-beratung-deine-sprechstunde-fuer-figma-fragen',
         'info_dict': {
@@ -56,22 +55,23 @@ class GermanupaIE(InfoExtractor):
         },
         'skip': 'login required',
     }]
+    _IFRAME_RE = r'<iframe[^>]+data-src\s*?=\s*?([\'"])(?P<url>https://germanupa\.de/media/oembed\?url=(?:(?!\1).)+)\1'
+    _LOGIN_REQUIRED_RE = r'<div[^>]+class\s*?=\s*?([\'"])(?:(?!\1).)*login-wrapper(?:(?!\1).)*\1'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        embed_url = self._search_regex(self._IFRAME_RE, webpage, 'iframe embed', default=None,
-                                       group='url')
-        real_url = traverse_obj(urllib.parse.parse_qs(urllib.parse.urlparse(embed_url).query),
-                                ('url', 0), None)
-        if not real_url:
-            if not self._search_regex(self._LOGIN_REQUIRED_RE, webpage, 'login required div',
-                                      default=None):
-                return self.url_result(url, 'Generic')  # Fall back to generic to extract audio
-            self.raise_login_required('This video is only available for members')
-        self.to_screen(embed_url)
-        return self.url_result(
-            VimeoIE._smuggle_referrer(
-                real_url.replace('https://vimeo.com/', 'https://player.vimeo.com/video/'), url),
-            VimeoIE)
+        embed_url = self._search_regex(self._IFRAME_RE, webpage, 'iframe embed', None, group='url')
+        if not embed_url:
+            return self.url_result(url, 'Generic')  # Fall back to generic to extract audio
+
+        param_url = traverse_obj(urllib.parse.parse_qs(urllib.parse.urlparse(embed_url).query),
+                                 ('url', 0), None)
+        if not param_url:
+            if self._search_regex(self._LOGIN_REQUIRED_RE, webpage, 'login wrapper', default=None):
+                self.raise_login_required('This video is only available for members')
+            return self.url_result(url)  # Fall back
+
+        real_url = param_url.replace('https://vimeo.com/', 'https://player.vimeo.com/video/')
+        return self.url_result(VimeoIE._smuggle_referrer(real_url, url), VimeoIE)
