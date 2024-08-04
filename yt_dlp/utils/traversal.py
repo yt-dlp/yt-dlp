949019fa6580ee47ef75f9ea6f0426f759fcf4ab
Simon Sawicki <contact@grub4k.xyz>
2024-08-04 03:11:56 +0200

    [utils] `traversal`: Add `subs_list_to_dict` helper

--- yt_dlp/utils/traversal.py
+++ yt_dlp/utils/traversal.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
+import collections
 import collections.abc
 import contextlib
+import functools
 import http.cookies
 import inspect
 import itertools
 import re
+import typing
 import xml.etree.ElementTree
 
 from ._utils import (
@@ -14,6 +19,7 @@ from ._utils import (
     deprecation_warning,
     is_iterable_like,
     try_call,
+    url_or_none,
     variadic,
 )
 
@@ -308,6 +314,48 @@ def require(name, /, *, expected=False):
 
 class _RequiredError(ExtractorError):
     pass
+
+
+@typing.overload
+def subs_list_to_dict(*, ext: str | None = None) -> collections.abc.Callable[[list[dict]], dict[str, list[dict]]]: ...
+
+
+@typing.overload
+def subs_list_to_dict(subs: list[dict] | None, /, *, ext: str | None = None) -> dict[str, list[dict]]: ...
+
+
+def subs_list_to_dict(subs: list[dict] | None = None, /, *, ext=None):
+    """
+    Convert subtitles from a traversal into a subtitle dict.
+    The path should have an `all` immediately before this function.
+
+    Arguments:
+    `ext`      The default value for `ext` in the subtitle dict
+
+    In the dict you can set the following additional items:
+    `id`       The subtitle id to sort the dict into
+    `quality`  The sort order for each subtitle
+    """
+    if subs is None:
+        return functools.partial(subs_list_to_dict, ext=ext)
+
+    result = collections.defaultdict(list)
+
+    for sub in subs:
+        if not url_or_none(sub.get('url')) and not sub.get('data'):
+            continue
+        sub_id = sub.pop('id', None)
+        if sub_id is None:
+            continue
+        if ext is not None and not sub.get('ext'):
+            sub['ext'] = ext
+        result[sub_id].append(sub)
+    result = dict(result)
+
+    for subs in result.values():
+        subs.sort(key=lambda x: x.pop('quality', 0) or 0)
+
+    return result
 
 
 def get_first(obj, *paths, **kwargs):
