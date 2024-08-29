80f4885fb0d35577b5428924f3ba9bc3184fca1d
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2024-08-29 18:03:17 +0000

    [ie/underline] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2243,6 +2243,10 @@ from .ufctv import (
 from .ukcolumn import UkColumnIE
 from .uktvplay import UKTVPlayIE
 from .umg import UMGDeIE
+from .underline import (
+    UnderlinePosterIE,
+    UnderlinePosterListIE,
+)
 from .unistra import UnistraIE
 from .unity import UnityIE
 from .unsupported import (
