e23d2a34e2086889525de0ccdf0c20492abb45ad
bashonly <88596187+bashonly@users.noreply.github.com>
2024-09-13 23:04:00 +0000

    minutia

--- yt_dlp/extractor/huya.py
+++ yt_dlp/extractor/huya.py
@@ -140,7 +140,6 @@ class HuyaLiveIE(InfoExtractor):
 
 class HuyaVideoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?huya\.com/video/play/(?P<id>\d+)\.html'
-
     IE_NAME = 'huya:video'
     IE_DESC = '虎牙视频'
 
