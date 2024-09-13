fa83d0b36bc43d30fe9241c1e923f4614864b758
naglis <827324+naglis@users.noreply.github.com>
2024-09-13 23:23:19 +0000

    [ie/LnkGo] Remove extractor (#10904)
    Authored by: naglis

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1041,10 +1041,7 @@ from .livestream import (
     LivestreamShortenerIE,
 )
 from .livestreamfails import LivestreamfailsIE
-from .lnkgo import (
-    LnkGoIE,
-    LnkIE,
-)
+from .lnk import LnkIE
 from .loom import (
     LoomFolderIE,
     LoomIE,
