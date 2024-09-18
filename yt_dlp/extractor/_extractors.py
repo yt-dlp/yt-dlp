8393e10f118f33926bce55317abdf01cfbca80e5
sepro <sepro@sepr0.com>
2024-09-18 03:08:39 +0200

    [ie/SnapchatSpotlight] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1879,6 +1879,7 @@ from .slideshare import SlideshareIE
 from .slideslive import SlidesLiveIE
 from .slutload import SlutloadIE
 from .smotrim import SmotrimIE
+from .snapchat import SnapchatSpotlightIE
 from .snotr import SnotrIE
 from .sohu import (
     SohuIE,
