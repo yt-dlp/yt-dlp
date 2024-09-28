1b868594657f74cb257098d5048ea7a1e8fc2fdc
Alexandre Villela (SleX) <alexandre.villela@hotmart.com>
2024-09-28 13:32:43 -0300

    fix: set referer in download video

--- yt_dlp/extractor/seleniumpagerendering.py
+++ yt_dlp/extractor/seleniumpagerendering.py
@@ -21,76 +21,89 @@ DEFAULT_CONFIGURATION = {
 
 
 class SeleniumPageRenderingIE(InfoExtractor):
-    def _download_webpage(self, url_or_request, display_id, *_args, **_kwargs):
+    def _download_webpage(self, url_or_request, display_id, headers=None, *_args, **_kwargs):
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
         from selenium.webdriver.common.by import By
         from selenium.webdriver.support.ui import WebDriverWait
         from selenium.webdriver.support import expected_conditions
+        from selenium.common.exceptions import SessionNotCreatedException
 
-        chrome_options = Options()
-        for arq in DEFAULT_CONFIGURATION.get('chrome_options'):
-            chrome_options.add_argument(arq)
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.execute_cdp_cmd('Network.enable', {})
-        if DEFAULT_CONFIGURATION.get('headers'):
-            driver.execute_cdp_cmd(
-                'Network.setExtraHTTPHeaders', {'headers': DEFAULT_CONFIGURATION.get('headers')}
-            )
-        driver.get(url_or_request)
-
-        driver.implicitly_wait(DEFAULT_CONFIGURATION.get('implicitly_wait') or 10)
-        # Wait for the page to render and the video to appear
-        src = 'UnsupportedError'
-        for _ in range(3):
+        for _r in range(3):
+            driver = None
             try:
-                wait = WebDriverWait(driver, 5)
-                src = wait.until(
-                    expected_conditions.visibility_of_element_located(
-                        (By.CSS_SELECTOR, 'video')
+                chrome_options = Options()
+                for arq in DEFAULT_CONFIGURATION.get('chrome_options'):
+                    chrome_options.add_argument(arq)
+                driver = webdriver.Chrome(options=chrome_options)
+                driver.execute_cdp_cmd('Network.enable', {})
+                if headers:
+                    driver.execute_cdp_cmd(
+                        'Network.setExtraHTTPHeaders', {'headers': headers}
                     )
-                ).find_element(By.CSS_SELECTOR, 'source').get_attribute('src')
+                driver.get(url_or_request)
+
+                driver.implicitly_wait(DEFAULT_CONFIGURATION.get('implicitly_wait') or 10)
+                # Wait for the page to render and the video to appear
+                src = 'UnsupportedError'
+                for _ in range(3):
+                    try:
+                        wait = WebDriverWait(driver, 5)
+                        src = wait.until(
+                            expected_conditions.visibility_of_element_located(
+                                (By.CSS_SELECTOR, 'video')
+                            )
+                        ).find_element(By.CSS_SELECTOR, 'source').get_attribute('src')
+                        if display_id not in src:
+                            raise UnsupportedError(url)
+                        break
+                    except Exception:
+                        pass
                 if display_id not in src:
-                    raise UnsupportedError(url)
-                break
-            except Exception:
-                pass
-        if display_id not in src:
-            raise UnsupportedError(url)
-        html_modificado = driver.page_source
-        driver.quit()
-        return html_modificado
+                    raise UnsupportedError(url_or_request)
+                return driver.page_source
+            except SessionNotCreatedException:
+                time.sleep()
+            finally:
+                try:
+                    if driver:
+                        driver.quit()
+                except Exception:
+                    pass
+        raise UnsupportedError(url_or_request)
+
+
+    def _og_search_thumbnail(self, html, **kargs):
+        if not 'data-poster="' in html:
+            return kargs.get('default')
+        return html.split('data-poster="')[1].split('"')[0]
+
+    def _get_headers(self, url):
+        headers = DEFAULT_CONFIGURATION.get('headers')
+        if headers:
+            return headers
+        url, smuggled_data = unsmuggle_url(url, {})
+        return filter_dict({
+            'Accept-Encoding': 'identity',
+            'Referer': smuggled_data.get('referer'),
+        })
 
     def _real_extract(self, url):
         original_url = url
         display_id = self._match_id(url)
-        video_id = self._generic_id(url)
-        url, smuggled_data = unsmuggle_url(url, {})
-        full_response = self._request_webpage(url, display_id, headers=filter_dict({
-            'Accept-Encoding': 'identity',
-            'Referer': smuggled_data.get('referer'),
-        }))
-        for _ in range(3):
-            webpage = self._download_webpage(url, display_id)
-            info_dict = {
-                'id': display_id,
-                'title': self._generic_title(url),
-                'timestamp': unified_timestamp(full_response.headers.get('Last-Modified')),
-            }
-
-            info_dict.update({
-                'id': display_id,
-                'display_id': display_id,
-                'title': self._generic_title('', webpage, default='video'),
-                'description': self._og_search_description(webpage, default=None),
-                'thumbnail': self._og_search_thumbnail(webpage, default=None),
-                'age_limit': self._rta_search(webpage),
-            })
-
-            embeds = list(
-                self._extract_generic_embeds(original_url, webpage, urlh=full_response, info_dict=info_dict))
-            if len(embeds) == 1:
-                return merge_dicts(embeds[0], info_dict)
-            elif embeds:
-                return self.playlist_result(embeds, **info_dict)
+        headers = self._get_headers(url)
+        full_response = self._request_webpage(url, display_id, headers=headers)
+        webpage = self._download_webpage(url, display_id, headers)
+        info_dict = {
+            'id': display_id,
+            'title': self._generic_title('', webpage, default='video'),
+            'thumbnail': self._og_search_thumbnail(webpage, default=None),
+            'timestamp': unified_timestamp(full_response.headers.get('Last-Modified')),
+        }
+        embeds = list(
+            self._extract_generic_embeds(original_url, webpage, urlh=full_response, info_dict=info_dict))
+        if len(embeds) == 1:
+            return merge_dicts(info_dict, embeds[0])
+        elif embeds:
+            return self.playlist_result(embeds, **info_dict)
         raise UnsupportedError(url)
