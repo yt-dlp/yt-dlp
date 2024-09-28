e1b9000a747ab4f692568e92d13d0069c87a4c1a
MellowKyler <dewerdk@gmail.com>
2024-09-28 18:30:32 -0500

    Support non-did:plc DIDs.
    Improve readability by assigning variables for
    common traversal paths.
    
    Additional None-type handling.

--- yt_dlp/extractor/bluesky.py
+++ yt_dlp/extractor/bluesky.py
@@ -14,13 +14,13 @@ class BlueskyIE(InfoExtractor):
             'upload_date': '20240921',
             'description': 'OMG WE HAVE VIDEOS NOW',
             'thumbnail': r're:^https://video.bsky.app/watch/.*\.jpg$',
-            'alt-title': None,
+            'alt_title': None,
             'uploader': 'Blu3Blu3Lilith',
             'channel': 'blu3blue.bsky.social',
             'uploader_id': 'did:plc:pzdr5ylumf7vmvwasrpr5bf2',
             'channel_id': 'did:plc:pzdr5ylumf7vmvwasrpr5bf2',
             'uploader_url': 'https://bsky.app/profile/blu3blue.bsky.social',
-            'channel_url': 'https://bsky.app/profile/blu3blue.bsky.social',
+            'channel_url': 'https://bsky.app/profile/did:plc:pzdr5ylumf7vmvwasrpr5bf2',
             'timestamp': 1726940605,
             'like_count': int,
             'repost_count': int,
@@ -40,13 +40,13 @@ class BlueskyIE(InfoExtractor):
             'upload_date': '20240911',
             'description': r're:^Bluesky now has video!',
             'thumbnail': r're:^https://video.bsky.app/watch/.*\.jpg$',
-            'alt-title': 'Bluesky video feature announcement',
+            'alt_title': 'Bluesky video feature announcement',
             'uploader': 'Bluesky',
             'channel': 'bsky.app',
             'uploader_id': 'did:plc:z72i7hdynmk6r22z27h6tvur',
             'channel_id': 'did:plc:z72i7hdynmk6r22z27h6tvur',
             'uploader_url': 'https://bsky.app/profile/bsky.app',
-            'channel_url': 'https://bsky.app/profile/bsky.app',
+            'channel_url': 'https://bsky.app/profile/did:plc:z72i7hdynmk6r22z27h6tvur',
             'timestamp': 1726074716,
             'like_count': int,
             'repost_count': int,
@@ -66,13 +66,13 @@ class BlueskyIE(InfoExtractor):
             'upload_date': '20240914',
             'description': r're:^alright.\nthis was .. a tiny bit of a pain.',
             'thumbnail': r're:^https://video.bsky.app/watch/.*\.jpg$',
-            'alt-title': r're:^me making a goofy little test video',
+            'alt_title': r're:^me making a goofy little test video',
             'uploader': 'clockwork banana',
             'channel': 'clockworkbanana.fun',
             'uploader_id': 'did:plc:3tndo2mqg2vgpxnpyrxiol6p',
             'channel_id': 'did:plc:3tndo2mqg2vgpxnpyrxiol6p',
             'uploader_url': 'https://bsky.app/profile/clockworkbanana.fun',
-            'channel_url': 'https://bsky.app/profile/clockworkbanana.fun',
+            'channel_url': 'https://bsky.app/profile/did:plc:3tndo2mqg2vgpxnpyrxiol6p',
             'timestamp': 1726353835,
             'like_count': int,
             'repost_count': int,
@@ -92,13 +92,13 @@ class BlueskyIE(InfoExtractor):
             'upload_date': '20240922',
             'description': '',
             'thumbnail': r're:^https://video.bsky.app/watch/.*\.jpg$',
-            'alt-title': None,
+            'alt_title': None,
             'uploader': 'maeve',
             'channel': 'souris.moe',
             'uploader_id': 'did:plc:tj7g244gl5v6ai6cm4f4wlqp',
             'channel_id': 'did:plc:tj7g244gl5v6ai6cm4f4wlqp',
             'uploader_url': 'https://bsky.app/profile/souris.moe',
-            'channel_url': 'https://bsky.app/profile/souris.moe',
+            'channel_url': 'https://bsky.app/profile/did:plc:tj7g244gl5v6ai6cm4f4wlqp',
             'timestamp': 1727003838,
             'like_count': int,
             'repost_count': int,
@@ -108,29 +108,51 @@ class BlueskyIE(InfoExtractor):
             'subtitles': 'count:0',
             'comments': list,
         },
+    }, {
+        'url': 'https://bsky.app/profile/de1.pds.tentacle.expert/post/3l3w4tnezek2e',
+        'md5': '1af9c7fda061cf7593bbffca89e43d1c',
+        'info_dict': {
+            'id': '3l3w4tnezek2e',
+            'ext': 'mp4',
+            'title': 'clean: ""',
+            'upload_date': '20240911',
+            'description': '',
+            'thumbnail': r're:^https://video.bsky.app/watch/.*\.jpg$',
+            'alt_title': None,
+            'uploader': 'clean',
+            'channel': 'de1.pds.tentacle.expert',
+            'uploader_id': 'did:web:de1.tentacle.expert',
+            'channel_id': 'did:web:de1.tentacle.expert',
+            'uploader_url': 'https://bsky.app/profile/de1.pds.tentacle.expert',
+            'channel_url': 'https://bsky.app/profile/did:web:de1.tentacle.expert',
+            'timestamp': 1726098823,
+            'like_count': int,
+            'repost_count': int,
+            'comment_count': int,
+            'webpage_url': 'https://bsky.app/profile/de1.pds.tentacle.expert/post/3l3w4tnezek2e',
+            'tags': 'count:1',
+            'subtitles': 'count:0',
+            'comments': list,
+        },
     }]
 
     def _get_comments(self, meta):
-        yield self.traverse_replies(
-            meta.get('thread'),
-            (traverse_obj(meta, ('thread', 'post', 'record', 'reply', 'root', 'uri'))
-             or traverse_obj(meta, ('thread', 'post', 'uri'))))
+        yield self.traverse_replies(meta, traverse_obj(meta, ('post', 'uri'), default=''))
 
     def traverse_replies(self, thread_node, root_uri):
-        parent_uri = traverse_obj(thread_node, ('post', 'record', 'reply', 'parent', 'uri'))
-        parent_id = 'root' if parent_uri == root_uri else parent_uri
-        author_handle = traverse_obj(thread_node, ('post', 'author', 'handle'))
-        author_did = traverse_obj(thread_node, ('post', 'author', 'did'))
+        post = thread_node.get('post')
+        parent_uri = traverse_obj(post, ('record', 'reply', 'parent', 'uri'))
+        author_did = traverse_obj(post, ('author', 'did'), default='')
         yield {
-            'id': traverse_obj(thread_node, ('post', 'uri')),
-            'text': traverse_obj(thread_node, ('post', 'record', 'text')),
-            'timestamp': parse_iso8601(traverse_obj(thread_node, ('post', 'record', 'createdAt'))),
-            'parent': parent_id,
-            'like_count': traverse_obj(thread_node, ('post', 'likeCount')),
-            'author': traverse_obj(thread_node, ('post', 'author', 'displayName')),
+            'id': post.get('uri'),
+            'text': traverse_obj(post, ('record', 'text')),
+            'timestamp': parse_iso8601(traverse_obj(post, ('record', 'createdAt'))),
+            'parent': 'root' if parent_uri == root_uri else parent_uri,
+            'like_count': post.get('likeCount'),
+            'author': traverse_obj(post, ('author', 'displayName')),
             'author_id': author_did,
-            'author_thumbnail': traverse_obj(thread_node, ('post', 'author', 'avatar'), expected_type=url_or_none),
-            'author_url': f'https://bsky.app/profile/{author_handle}',
+            'author_thumbnail': traverse_obj(post, ('author', 'avatar'), expected_type=url_or_none),
+            'author_url': f'https://bsky.app/profile/{traverse_obj(post, ('author', 'handle'))}',
             'author_is_uploader': 'Yes' if author_did in root_uri else 'No',
         }
         if replies := thread_node.get('replies'):
@@ -142,7 +164,7 @@ class BlueskyIE(InfoExtractor):
     def _real_extract(self, url):
         handle, video_id = self._match_valid_url(url).groups()
         did = handle if handle.startswith('did:') else self._download_json(
-            (f'https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle={handle}'),
+            (f'https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle={handle}'),
             video_id, expected_status=200).get('did')
 
         getcomments = self.get_param('getcomments', False)
@@ -152,26 +174,26 @@ class BlueskyIE(InfoExtractor):
             query={'uri': f'at://{did}/app.bsky.feed.post/{video_id}',
                    'depth': 1000 if getcomments else 0,
                    'parentHeight': 1000 if getcomments else 0},
-            expected_status=200)
+            expected_status=200).get('thread')
+        post, record_embed = meta.get('post'), traverse_obj(meta, ('post', 'record', 'embed'))
 
         formats, subs = self._extract_m3u8_formats_and_subtitles(
-            traverse_obj(meta, ('thread', 'post', 'embed', 'playlist')),
+            traverse_obj(post, ('embed', 'playlist')),
             video_id, 'mp4', 'm3u8_native', m3u8_id='hls', fatal=False,
             note='Downloading HD m3u8 information', errnote='Unable to download HD m3u8 information')
-        blob_cid = (traverse_obj(meta, ('thread', 'post', 'embed', 'cid'))
-                    or traverse_obj(meta, ('thread', 'post', 'record', 'embed', 'video', 'ref', '$link')))
+        blob_cid = traverse_obj(post, ('embed', 'cid'), ('record', 'embed', 'video', 'ref', '$link'))
         formats.append({
             'format_id': 'blob',
             'url': f'https://bsky.social/xrpc/com.atproto.sync.getBlob?did={did}&cid={blob_cid}',
-            'ext': mimetype2ext(traverse_obj(meta, ('thread', 'post', 'record', 'embed', 'video', 'mimeType')), 'mp4'),
-            'width': traverse_obj(meta, ('thread', 'post', 'record', 'embed', 'aspectRatio', 'width'), expected_type=int_or_none),
-            'height': traverse_obj(meta, ('thread', 'post', 'record', 'embed', 'aspectRatio', 'height'), expected_type=int_or_none),
-            'filesize': traverse_obj(meta, ('thread', 'post', 'record', 'embed', 'video', 'size'), expected_type=int_or_none),
+            'ext': mimetype2ext(traverse_obj(record_embed, ('video', 'mimeType')), 'mp4'),
+            'width': traverse_obj(record_embed, ('aspectRatio', 'width'), expected_type=int_or_none),
+            'height': traverse_obj(record_embed, ('aspectRatio', 'height'), expected_type=int_or_none),
+            'filesize': traverse_obj(record_embed, ('video', 'size'), expected_type=int_or_none),
         })
 
-        handle = traverse_obj(meta, ('thread', 'post', 'author', 'handle'))
-        uploader = traverse_obj(meta, ('thread', 'post', 'author', 'displayName'))
-        description = traverse_obj(meta, ('thread', 'post', 'record', 'text'))
+        handle = traverse_obj(post, ('author', 'handle'))
+        uploader = traverse_obj(post, ('author', 'displayName'))
+        description = traverse_obj(post, ('record', 'text'))
         extractor = self.extract_comments(meta)
 
         return {
@@ -179,21 +201,20 @@ class BlueskyIE(InfoExtractor):
             'title': f'{uploader}: "{description}"',
             'formats': formats,
             'description': description,
-            'thumbnail': traverse_obj(meta, ('thread', 'post', 'embed', 'thumbnail'), expected_type=url_or_none),
-            'alt-title': traverse_obj(meta, ('thread', 'post', 'record', 'alt'), ('thread', 'post', 'embed', 'alt')),
+            'thumbnail': traverse_obj(post, ('embed', 'thumbnail'), expected_type=url_or_none),
+            'alt_title': traverse_obj(post, ('embed', 'alt'), ('record', 'embed', 'alt')),
             'uploader': uploader,
             'channel': handle,
             'uploader_id': did,
             'channel_id': did,
             'uploader_url': f'https://bsky.app/profile/{handle}',
-            'channel_url': f'https://bsky.app/profile/{handle}',
-            'timestamp': parse_iso8601(traverse_obj(meta, ('thread', 'post', 'record', 'createdAt'))),
-            'like_count': traverse_obj(meta, ('thread', 'post', 'likeCount')),
-            'repost_count': traverse_obj(meta, ('thread', 'post', 'repostCount')),
-            'comment_count': traverse_obj(meta, ('thread', 'post', 'replyCount')),
+            'channel_url': f'https://bsky.app/profile/{did}',
+            'timestamp': parse_iso8601(traverse_obj(post, ('record', 'createdAt'))),
+            'like_count': post.get('likeCount'),
+            'repost_count': post.get('repostCount'),
+            'comment_count': post.get('replyCount'),
             'webpage_url': url,
-            'tags': (traverse_obj(meta, ('thread', 'post', 'labels'), expected_type=list)
-                     + traverse_obj(meta, ('thread', 'post', 'record', 'langs'), expected_type=list)),
+            'tags': post.get('labels', []) + traverse_obj(post, ('record', 'langs'), default=[]),
             'comments': [] if not extractor else [*(extractor().get('comments'))[0]][1:],
             'subtitles': self._merge_subtitles(subs),
         }
