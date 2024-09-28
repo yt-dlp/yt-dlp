9a948f313fd448669f186a1eb156d05f78c7c11d
Alexandre Villela (SleX) <alexandre.villela@hotmart.com>
2024-09-28 11:00:17 -0300

    fix: validates if the correct video was loaded

--- yt_dlp/extractor/seleniumpagerendering.py
+++ yt_dlp/extractor/seleniumpagerendering.py
@@ -1,3 +1,5 @@
+import time
+
 from yt_dlp.utils import filter_dict, unsmuggle_url, unified_timestamp, UnsupportedError
 
 from .common import InfoExtractor
@@ -5,7 +7,7 @@ from ..utils import merge_dicts
 
 DEFAULT_CONFIGURATION = {
     'chrome_options': [
-        '--headless',
+        # '--headless',
         '--disable-gpu',
         '--disable-extensions',
         '--proxy-server="direct://"',
@@ -19,9 +21,12 @@ DEFAULT_CONFIGURATION = {
 
 
 class SeleniumPageRenderingIE(InfoExtractor):
-    def _download_webpage(self, url_or_request, *_args, **_kwargs):
+    def _download_webpage(self, url_or_request, display_id, *_args, **_kwargs):
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions
 
         chrome_options = Options()
         for arq in DEFAULT_CONFIGURATION.get('chrome_options'):
@@ -33,7 +38,25 @@ class SeleniumPageRenderingIE(InfoExtractor):
                 'Network.setExtraHTTPHeaders', {'headers': DEFAULT_CONFIGURATION.get('headers')}
             )
         driver.get(url_or_request)
+
         driver.implicitly_wait(DEFAULT_CONFIGURATION.get('implicitly_wait') or 10)
+        # Wait for the page to render and the video to appear
+        src = 'UnsupportedError'
+        for _ in range(3):
+            try:
+                wait = WebDriverWait(driver, 5)
+                src = wait.until(
+                    expected_conditions.visibility_of_element_located(
+                        (By.CSS_SELECTOR, 'video')
+                    )
+                ).find_element(By.CSS_SELECTOR, 'source').get_attribute('src')
+                if display_id not in src:
+                    raise UnsupportedError(url)
+                break
+            except Exception:
+                pass
+        if display_id not in src:
+            raise UnsupportedError(url)
         html_modificado = driver.page_source
         driver.quit()
         return html_modificado
@@ -43,30 +66,31 @@ class SeleniumPageRenderingIE(InfoExtractor):
         display_id = self._match_id(url)
         video_id = self._generic_id(url)
         url, smuggled_data = unsmuggle_url(url, {})
-        full_response = self._request_webpage(url, video_id, headers=filter_dict({
+        full_response = self._request_webpage(url, display_id, headers=filter_dict({
             'Accept-Encoding': 'identity',
             'Referer': smuggled_data.get('referer'),
         }))
-        webpage = self._download_webpage(url, video_id)
+        for _ in range(3):
+            webpage = self._download_webpage(url, display_id)
+            info_dict = {
+                'id': display_id,
+                'title': self._generic_title(url),
+                'timestamp': unified_timestamp(full_response.headers.get('Last-Modified')),
+            }
 
-        info_dict = {
-            'id': video_id,
-            'title': self._generic_title(url),
-            'timestamp': unified_timestamp(full_response.headers.get('Last-Modified')),
-        }
+            info_dict.update({
+                'id': display_id,
+                'display_id': display_id,
+                'title': self._generic_title('', webpage, default='video'),
+                'description': self._og_search_description(webpage, default=None),
+                'thumbnail': self._og_search_thumbnail(webpage, default=None),
+                'age_limit': self._rta_search(webpage),
+            })
 
-        info_dict.update({
-            'id': video_id,
-            'display_id': display_id,
-            'title': self._generic_title('', webpage, default='video'),
-            'description': self._og_search_description(webpage, default=None),
-            'thumbnail': self._og_search_thumbnail(webpage, default=None),
-            'age_limit': self._rta_search(webpage),
-        })
-
-        embeds = list(self._extract_generic_embeds(original_url, webpage, urlh=full_response, info_dict=info_dict))
-        if len(embeds) == 1:
-            return merge_dicts(embeds[0], info_dict)
-        elif embeds:
-            return self.playlist_result(embeds, **info_dict)
+            embeds = list(
+                self._extract_generic_embeds(original_url, webpage, urlh=full_response, info_dict=info_dict))
+            if len(embeds) == 1:
+                return merge_dicts(embeds[0], info_dict)
+            elif embeds:
+                return self.playlist_result(embeds, **info_dict)
         raise UnsupportedError(url)
