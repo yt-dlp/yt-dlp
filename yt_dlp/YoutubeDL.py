33a29ca5d2870dca3071e99a06882a08c3766e52
Simon Sawicki <contact@grub4k.xyz>
2024-10-13 03:38:09 +0200

    [cookies] Fix compatibility for Python <=3.9 in traceback
    Authored by: Grub4K

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -4120,7 +4120,8 @@ class YoutubeDL:
                 self.params.get('cookiefile'), self.params.get('cookiesfrombrowser'), self)
         except CookieLoadError as error:
             cause = error.__context__
-            self.report_error(str(cause), tb=''.join(traceback.format_exception(cause)))
+            # compat: <=py3.9: `traceback.format_exception` has a different signature
+            self.report_error(str(cause), tb=''.join(traceback.format_exception(None, cause, cause.__traceback__)))
             raise
 
     @property
