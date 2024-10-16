62f09c4bfd16884d8e02cc0fdd89cfa96ad5759c
sepro <sepro@sepr0.com>
2024-10-16 23:29:16 +0200

    Improve regex further
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/funk.py
+++ yt_dlp/extractor/funk.py
@@ -3,7 +3,7 @@ from .nexx import NexxIE
 
 
 class FunkIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:(?:www|origin|play)\.)?funk\.net/(?:channel|playlist)/[^/]+/(?P<display_id>[0-9a-z-]+)-(?P<id>\d+)'
+    _VALID_URL = r'https?://(?:(?:www|origin|play)\.)?funk\.net/(?:channel|playlist)/[^/?#]+/(?P<display_id>[0-9a-z-]+)-(?P<id>\d+)'
     _TESTS = [{
         'url': 'https://www.funk.net/channel/ba-793/die-lustigsten-instrumente-aus-dem-internet-teil-2-1155821',
         'md5': '8610449476156f338761a75391b0017d',
