ef9acc949e1fdc8f0d49a5d8717c475f769b7347
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2024-10-17 15:17:33 +0000

    BRIGHTCOVE_URL_TEMPLATE

--- yt_dlp/extractor/drtalks.py
+++ yt_dlp/extractor/drtalks.py
@@ -1,9 +1,7 @@
-import functools
-import urllib.parse
 
 from .brightcove import BrightcoveNewIE
 from .common import InfoExtractor
-from ..utils import extract_attributes
+from ..utils import extract_attributes, smuggle_url
 
 
 class DrTalksIE(InfoExtractor):
@@ -14,7 +12,7 @@ class DrTalksIE(InfoExtractor):
         'info_dict': {
             'id': '1758074870279626053',
             'title': 'Free Access Day 1 - Events at DrTalks',
-            'thumbnail': 're:https://event.drtalks.com/wp-content/uploads/.+',
+            'thumbnail': r're:https://event.drtalks.com/wp-content/uploads/.+',
         },
         'playlist_mincount': 11,
         'params': {
@@ -25,7 +23,7 @@ class DrTalksIE(InfoExtractor):
         'info_dict': {
             'id': '1747611460188466596',
             'title': 'The BioEnergetics Summit',
-            'thumbnail': 're:https://event.drtalks.com/wp-content/uploads/.+',
+            'thumbnail': r're:https://event.drtalks.com/wp-content/uploads/.+',
         },
         'playlist_mincount': 8,
         'params': {
@@ -38,19 +36,19 @@ class DrTalksIE(InfoExtractor):
         'url': 'https://event.drtalks.com/medicine-of-mindset-summit/free-access-day-1/',
         'only_matching': True,
     }]
+    BRIGHTCOVE_URL_TEMPLATE = 'http://players.brightcove.net/%s/%s_%s/index.html?playlistId=%s'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
-        player_attrs = extract_attributes(self._search_regex(r'(<video-js[^>]+>)', webpage, 'player'))
-
-        playlist_url = functools.reduce(urllib.parse.urljoin, [
-            'https://players.brightcove.net/',
-            f'{player_attrs["data-account"]}/',
-            f'{player_attrs["data-player"]}_{player_attrs["data-embed"]}/',
-            f'index.html?playlistId={player_attrs["data-playlist-id"]}',
-        ])
+        player_attrs = extract_attributes(self._search_regex(
+            r'(<video-js[^>]+\bid=(["\'])myPlayerID\2[^>]*>)', webpage, 'player'))
+        bc_url = smuggle_url(self.BRIGHTCOVE_URL_TEMPLATE % (
+            player_attrs.get('data-account', '6314452011001'),
+            player_attrs.get('data-player', 'f3rfrCUjm'),
+            player_attrs.get('data-embed', 'default'),
+            player_attrs['data-playlist-id']), {'source_url': url})
 
         return self.url_result(
-            playlist_url, BrightcoveNewIE.ie_key(), video_id, self._og_search_title(webpage),
+            bc_url, BrightcoveNewIE.ie_key(), video_id, self._og_search_title(webpage),
             url_transparent=True, thumbnail=self._og_search_thumbnail(webpage))
