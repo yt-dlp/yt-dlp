fa922039a87fdb090e0c1a4ffd25e88ca2077ea7
sepro <sepro@sepr0.com>
2024-10-20 23:13:06 +0200

    Fix import order

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -362,7 +362,10 @@ from .ccc import (
 )
 from .ccma import CCMAIE
 from .cctv import CCTVIE
-from .cda import CDAIE, CDAFolderIE
+from .cda import (
+    CDAIE,
+    CDAFolderIE,
+)
 from .cellebrite import CellebriteIE
 from .ceskatelevize import CeskaTelevizeIE
 from .cgtn import CGTNIE
