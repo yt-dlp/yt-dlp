8f1f89f2c8935eebd8b6f0947bf1a50fc4890cbf
Paweł Kotiuk <kotiuk@zohomail.eu>
2024-10-20 23:16:22 +0200

    [ie/cda] Support folders (#10786)
    Closes #5429
    Authored by: pktiuk

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -363,7 +363,10 @@ from .ccc import (
 )
 from .ccma import CCMAIE
 from .cctv import CCTVIE
-from .cda import CDAIE
+from .cda import (
+    CDAIE,
+    CDAFolderIE,
+)
 from .cellebrite import CellebriteIE
 from .ceskatelevize import CeskaTelevizeIE
 from .cgtn import CGTNIE
