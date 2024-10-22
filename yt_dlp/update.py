8bad937211863b7f298a8d29b214fa0a5b684dd9
bashonly <bashonly@protonmail.com>
2024-10-22 13:09:43 -0500

    [build] Use Ubuntu 20.04 and Python 3.9 for Linux ARM builds (#8638)
    Authored by: bashonly

--- yt_dlp/update.py
+++ yt_dlp/update.py
@@ -158,13 +158,6 @@ def _get_system_deprecation():
             return EXE_MSG_TMPL.format('Windows 7/Server 2008 R2', 'issues/10086', STOP_MSG)
         return None
 
-    # Temporary until aarch64/armv7l build flow is bumped to Ubuntu 20.04 and Python 3.9
-    elif variant in ('linux_aarch64_exe', 'linux_armv7l_exe'):
-        libc_ver = version_tuple(os.confstr('CS_GNU_LIBC_VERSION').partition(' ')[2])
-        if libc_ver < (2, 31):
-            return EXE_MSG_TMPL.format('system glibc version < 2.31', 'pull/8638', STOP_MSG)
-        return None
-
     return f'Support for Python version {major}.{minor} has been deprecated. {PYTHON_MSG}'
 
 
@@ -357,7 +350,8 @@ class Updater:
                     continue
 
                 self._report_error(
-                    f'yt-dlp cannot be updated to {resolved_tag} since you are on an older Python version', True)
+                    f'yt-dlp cannot be updated to {resolved_tag} since you are on an older Python version '
+                    'or your operating system is not compatible with the requested build', True)
                 return None
 
         return resolved_tag
