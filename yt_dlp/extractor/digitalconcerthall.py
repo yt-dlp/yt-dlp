b65e014c3cc7cd5eff354e59c8ee354e6ccbcbd9
bashonly <bashonly@protonmail.com>
2024-10-30 11:11:53 -0500

    [ie/digitalconcerthall] Support login with access/refresh tokens
    Authored by: bashonly

--- yt_dlp/extractor/digitalconcerthall.py
+++ yt_dlp/extractor/digitalconcerthall.py
@@ -1,7 +1,10 @@
+import time
+
 from .common import InfoExtractor
 from ..networking.exceptions import HTTPError
 from ..utils import (
     ExtractorError,
+    jwt_decode_hs256,
     parse_codecs,
     try_get,
     url_or_none,
@@ -13,9 +16,6 @@ from ..utils.traversal import traverse_obj
 class DigitalConcertHallIE(InfoExtractor):
     IE_DESC = 'DigitalConcertHall extractor'
     _VALID_URL = r'https?://(?:www\.)?digitalconcerthall\.com/(?P<language>[a-z]+)/(?P<type>film|concert|work)/(?P<id>[0-9]+)-?(?P<part>[0-9]+)?'
-    _OAUTH_URL = 'https://api.digitalconcerthall.com/v2/oauth2/token'
-    _USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15'
-    _ACCESS_TOKEN = None
     _NETRC_MACHINE = 'digitalconcerthall'
     _TESTS = [{
         'note': 'Playlist with only one video',
@@ -69,48 +69,132 @@ class DigitalConcertHallIE(InfoExtractor):
         'params': {'skip_download': 'm3u8'},
         'playlist_count': 1,
     }]
+    _LOGIN_HINT = ('Use  --username token --password ACCESS_TOKEN  where ACCESS_TOKEN'
+                   'is the `access_token_production` from your browser local storage')
+    _REFRESH_HINT = 'or else use a refresh_token with  --username refresh --password REFRESH_TOKEN'
+    _OAUTH_URL = 'https://api.digitalconcerthall.com/v2/oauth2/token'
+    _CLIENT_ID = 'dch.webapp'
+    _CLIENT_SECRET = '2ySLN+2Fwb'
+    _USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15'
+    _OAUTH_HEADERS = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Origin': 'https://www.digitalconcerthall.com',
+        'Referer': 'https://www.digitalconcerthall.com/',
+        'User-Agent': _USER_AGENT,
+    }
+    _real_access_token = None
+    _access_token_expiry = 0
+    _refresh_token = None
+
+    @property
+    def _access_token_is_expired(self):
+        return self._access_token_expiry - 30 <= int(time.time())
+
+    @property
+    def _access_token(self):
+        if not self._real_access_token:
+            # return an initial access token that can be used to auth with password or refresh_token
+            return self._download_json(
+                self._OAUTH_URL, None, 'Obtaining initial token', 'Unable to obtain initial token',
+                data=urlencode_postdata({
+                    'affiliate': 'none',
+                    'grant_type': 'device',
+                    'device_vendor': 'unknown',
+                    # device_model 'Safari' gets split streams of 4K/HEVC video and lossless/FLAC audio,
+                    # but this is no longer effective since actual login is not possible anymore
+                    'device_model': 'unknown',
+                    'app_id': self._CLIENT_ID,
+                    'app_distributor': 'berlinphil',
+                    'app_version': '1.95.0',
+                    'client_secret': self._CLIENT_SECRET,
+                }), headers=self._OAUTH_HEADERS)['access_token']
+
+        if self._access_token_is_expired:
+            if not self._refresh_token:
+                raise ExtractorError(
+                    'Access token has expired. Get a new access_token from your browser '
+                    f'and try again, {self._REFRESH_HINT}', expected=True)
+            self._fetch_new_tokens()
+
+        return self._real_access_token
+
+    @_access_token.setter
+    def _access_token(self, value):
+        self._real_access_token = value
+        self._access_token_expiry = traverse_obj(value, ({jwt_decode_hs256}, 'exp', {int})) or 0
+
+    def _set_tokens(self, auth_data):
+        self._access_token = auth_data['access_token']
+        if refresh_token := traverse_obj(auth_data, ('refresh_token', {str})):
+            self.write_debug('New refresh token granted')
+            self._refresh_token = refresh_token
+        self.cache.store(self._NETRC_MACHINE, 'tokens', {
+            'access_token': self._real_access_token,
+            'refresh_token': self._refresh_token,
+        })
+
+    def _fetch_new_tokens(self):
+        try:
+            response = self._download_json(
+                self._OAUTH_URL, None, 'Refreshing token', 'Unable to refresh token',
+                data=urlencode_postdata({
+                    'grant_type': 'refresh_token',
+                    'refresh_token': self._refresh_token,
+                    'client_id': self._CLIENT_ID,
+                    'client_secret': self._CLIENT_SECRET,
+                }), headers={
+                    **self._OAUTH_HEADERS,
+                    'Authorization': f'Bearer {self._real_access_token or self._access_token}',
+                })
+        except ExtractorError as e:
+            if isinstance(e.cause, HTTPError) and e.cause.status == 401:
+                self._refresh_token = None
+                self._set_tokens({'access_token': None})
+                raise ExtractorError('Your tokens have been invalidated', expected=True)
+            raise
+        self._set_tokens(response)
 
     def _perform_login(self, username, password):
-        login_token = self._download_json(
-            self._OAUTH_URL,
-            None, 'Obtaining token', errnote='Unable to obtain token', data=urlencode_postdata({
-                'affiliate': 'none',
-                'grant_type': 'device',
-                'device_vendor': 'unknown',
-                # device_model 'Safari' gets split streams of 4K/HEVC video and lossless/FLAC audio
-                'device_model': 'unknown' if self._configuration_arg('prefer_combined_hls') else 'Safari',
-                'app_id': 'dch.webapp',
-                'app_distributor': 'berlinphil',
-                'app_version': '1.84.0',
-                'client_secret': '2ySLN+2Fwb',
-            }), headers={
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                'User-Agent': self._USER_AGENT,
-            })['access_token']
-        try:
-            login_response = self._download_json(
-                self._OAUTH_URL,
-                None, note='Logging in', errnote='Unable to login', data=urlencode_postdata({
-                    'grant_type': 'password',
-                    'username': username,
-                    'password': password,
-                }), headers={
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                    'Referer': 'https://www.digitalconcerthall.com',
-                    'Authorization': f'Bearer {login_token}',
-                    'User-Agent': self._USER_AGENT,
-                })
-        except ExtractorError as error:
-            if isinstance(error.cause, HTTPError) and error.cause.status == 401:
-                raise ExtractorError('Invalid username or password', expected=True)
-            raise
-        self._ACCESS_TOKEN = login_response['access_token']
+        if username not in ('refresh', 'token'):
+            raise ExtractorError(
+                'Login with username and password is no longer supported '
+                f'for this site. {self._LOGIN_HINT}, {self._REFRESH_HINT}', expected=True)
+        self.report_login()
+
+        # Try first with passed refresh_token since user may want to override the cache with it
+        if username == 'refresh':
+            self._refresh_token = password
+            self._fetch_new_tokens()
+            return
+
+        # Try cached access_token
+        cached_tokens = self.cache.load(self._NETRC_MACHINE, 'tokens', default={})
+        self._access_token = cached_tokens.get('access_token')
+        self._refresh_token = cached_tokens.get('refresh_token')
+        if not self._access_token_is_expired:
+            return
+        if self._real_access_token:
+            self.write_debug('Cached access token has expired, invalidating')
+            self._access_token = None
+        # Try cached refresh_token
+        if self._refresh_token:
+            try:
+                return self._fetch_new_tokens()
+            except ExtractorError:
+                # Do not raise for caached tokens; invalidate and continue
+                self._access_token = None
+
+        # username is 'token'
+        if not traverse_obj(password, {jwt_decode_hs256}):
+            raise ExtractorError(
+                f'The access token passed to yt-dlp is not valid. {self._LOGIN_HINT}', expected=True)
+        self._access_token = password
+        self._set_tokens({'access_token': self._access_token})
 
     def _real_initialize(self):
-        if not self._ACCESS_TOKEN:
-            self.raise_login_required(method='password')
+        if not self._real_access_token:
+            self.raise_login_required(f'{self._LOGIN_HINT}, {self._REFRESH_HINT}', method='none')
 
     def _entries(self, items, language, type_, **kwargs):
         for item in items:
@@ -118,7 +202,7 @@ class DigitalConcertHallIE(InfoExtractor):
             stream_info = self._download_json(
                 self._proto_relative_url(item['_links']['streams']['href']), video_id, headers={
                     'Accept': 'application/json',
-                    'Authorization': f'Bearer {self._ACCESS_TOKEN}',
+                    'Authorization': f'Bearer {self._access_token}',
                     'Accept-Language': language,
                     'User-Agent': self._USER_AGENT,
                 })
@@ -150,15 +234,23 @@ class DigitalConcertHallIE(InfoExtractor):
         language, type_, video_id, part = self._match_valid_url(url).group('language', 'type', 'id', 'part')
         if not language:
             language = 'en'
-
         api_type = 'concert' if type_ == 'work' else type_
-        vid_info = self._download_json(
-            f'https://api.digitalconcerthall.com/v2/{api_type}/{video_id}', video_id, headers={
-                'Accept': 'application/json',
-                'Accept-Language': language,
-                'User-Agent': self._USER_AGENT,
-                'Authorization': f'Bearer {self._ACCESS_TOKEN}',
-            })
+
+        try:
+            vid_info = self._download_json(
+                f'https://api.digitalconcerthall.com/v2/{api_type}/{video_id}', video_id, headers={
+                    'Accept': 'application/json',
+                    'Accept-Language': language,
+                    'User-Agent': self._USER_AGENT,
+                    'Authorization': f'Bearer {self._access_token}',
+                })
+        except ExtractorError as error:
+            if isinstance(error.cause, HTTPError) and error.cause.status == 401:
+                self.report_warning('Access token has been invalidated')
+                self._fetch_new_tokens()
+            else:
+                raise
+
         videos = [vid_info] if type_ == 'film' else traverse_obj(vid_info, ('_embedded', ..., ...))
 
         if type_ == 'work':
