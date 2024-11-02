50b4b87a3d6f87ef26b86581ba65054693138ace
sepro <sepro@sepr0.com>
2024-11-02 08:32:23 +0100

    Oops
    Co-authored-by: Mozi <29089388+pzhlkj6612@users.noreply.github.com>

--- yt_dlp/extractor/radioradicale.py
+++ yt_dlp/extractor/radioradicale.py
@@ -62,7 +62,7 @@ class RadioRadicaleIE(InfoExtractor):
             subtitles = {}
 
             for m3u8_url in traverse_obj(video, ('sources', ..., 'src', {url_or_none})):
-                fmts, subs = self._extract_m3u8_formats(m3u8_url, video_id)
+                fmts, subs = self._extract_m3u8_formats_and_subtitles(m3u8_url, video_id)
                 formats.extend(fmts)
                 self._merge_subtitles(subs, target=subtitles)
             for sub in traverse_obj(video, ('subtitles', ..., lambda _, v: url_or_none(v['src']))):
