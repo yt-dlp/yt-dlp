cf058306be85d73ff5377695082158feee9bc00e
Hugo <hugov2002@gmail.com>
2024-11-08 20:10:19 +0100

    Capture group for the _DOMAIN_RE section of the _VALID_URL
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/cloudflarestream.py
+++ yt_dlp/extractor/cloudflarestream.py
@@ -63,7 +63,8 @@ class CloudflareStreamIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        domain = 'bytehighway.net' if 'bytehighway.net/' in url else 'cloudflarestream.com'
+        if domain != 'bytehighway.net':
+            domain = 'cloudflarestream.com'
         base_url = f'https://{domain}/{video_id}/'
         if '.' in video_id:
             video_id = self._parse_json(base64.urlsafe_b64decode(
