d367d424e5c2bca8914607ad095e235cfb6b838e
rroller <ronnie@ronnieroller.com>
2024-11-17 19:25:38 -0800

    [ie/streamff] Add extractor for streamff

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1977,6 +1977,7 @@ from .storyfire import (
 )
 from .streamable import StreamableIE
 from .streamcz import StreamCZIE
+from .streamff import StreamffIE
 from .streetvoice import StreetVoiceIE
 from .stretchinternet import StretchInternetIE
 from .stripchat import StripchatIE
