434bd53b375a803344c416ea2bdba6707bf83c9d
Audrey <45548254+tntmod54321@users.noreply.github.com>
2024-11-23 02:11:54 -0500

    attempt #2 at not treating ddl urls as m3u8

--- yt_dlp/extractor/bannedvideo.py
+++ yt_dlp/extractor/bannedvideo.py
@@ -254,7 +254,7 @@ class BannedVideoIE(BannedVideoBaseIE):
             'url': video_info.get('directUrl'),
             'ext': 'mp4',
         }] if url_or_none(video_info.get('directUrl')) else []
-        if video_info.get('streamUrl') and not re.search(r'\.mp4$', video_info.get('streamUrl') or ''):
+        if video_info.get('streamUrl') and not video_info.get('streamUrl') == video_info.get('directUrl'):
             formats.extend(self._extract_m3u8_formats(
                 video_info.get('streamUrl'), video_id, 'mp4',
                 entry_protocol='m3u8_native', m3u8_id='hls',
