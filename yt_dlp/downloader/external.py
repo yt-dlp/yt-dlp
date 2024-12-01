0b382a7d72e647681e6022e01d953af4e4b7758f
tcely <tcely@users.noreply.github.com>
2024-12-01 17:32:05 -0500

    Remove continue from aria2c
    This doesn't actually work, or make sense, without configuring the stream selector to use `inorder` instead of `default` for the algorithm.
    
    Ideally, `aria2c` would write the needed information for resuming downloads in its own file before exiting.
    Otherwise, it's more reliable to download everything again.

--- yt_dlp/downloader/external.py
+++ yt_dlp/downloader/external.py
@@ -293,7 +293,7 @@ class Aria2cFD(ExternalFD):
         return super()._call_downloader(tmpfilename, info_dict)
 
     def _make_cmd(self, tmpfilename, info_dict):
-        cmd = [self.exe, '-c', '--no-conf',
+        cmd = [self.exe, '--no-conf',
                '--console-log-level=warn', '--summary-interval=0', '--download-result=hide',
                '--http-accept-gzip=true', '--file-allocation=none', '-x16', '-j16', '-s16']
         if 'fragments' in info_dict:
