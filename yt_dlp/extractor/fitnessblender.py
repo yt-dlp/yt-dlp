f1ace898a5d93fb4ba0cbb3c49c53b1edf540dee
Son Phan Trung <phantrungson17@gmail.com>
2024-12-25 19:55:47 +0700

    add fitness blender

---
+++ yt_dlp/extractor/fitnessblender.py
@@ -0,0 +1,15 @@
+from .brightcove import BrightcoveNewIE
+from .common import InfoExtractor
+
+
+class FitnessBlenderIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?fitnessblender\.com/videos/[\w-]+/(?P<tc>T)?(?P<id>\d+)'
+    def _real_extract(self, url):
+        video_id = self._match_valid_url(url).group('id')
+
+        account_id = '6036648099001'
+        player_id = 'skIgx8kLxj'
+
+        return self.url_result(
+            f'https://players.brightcove.net/{account_id}/{player_id}_default/index.html?videoId={video_id}',
+            BrightcoveNewIE)
