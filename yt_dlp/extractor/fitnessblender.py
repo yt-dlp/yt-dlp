b0c13586f203774c59c7d3fc074d42f29de4ff24
Son Phan Trung <phantrungson17@gmail.com>
2025-01-01 20:07:26 +0700

    add TESTS

--- yt_dlp/extractor/fitnessblender.py
+++ yt_dlp/extractor/fitnessblender.py
@@ -4,6 +4,16 @@ from .common import InfoExtractor
 
 class FitnessBlenderIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?fitnessblender\.com/videos/[\w-]+/(?P<tc>T)?(?P<id>\d+)'
+    _TESTS = [{
+        'url': 'https://www.fitnessblender.com/page/fb-plus-player-test',
+        'info_dict': {
+            'id': '6076568195001',
+            'ext': 'mp4',
+            'title': 'Sports Endurance Workout - Stamina, Speed, and Agility Workout',
+            'thumbnail': r're:^https://.+\.jpg',
+        },
+        'params': {'skip_download': 'm3u8'},
+    }]
     def _real_extract(self, url):
         video_id = self._match_valid_url(url).group('id')
 
