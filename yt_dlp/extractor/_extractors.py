af2c821d74049b519895288aca23cee81fc4b049
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-01-20 23:26:05 +0800

    [ie/piramidetv] Add extractors (#10777)
    Closes #10706, Closes #10708
    Authored by: kclauhk, HobbyistDev, seproDev
    
    Co-authored-by: HobbyistDev <tesutonihon4@gmail.com>
    Co-authored-by: sepro <sepro@sepr0.com>

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1541,6 +1541,10 @@ from .pinterest import (
     PinterestCollectionIE,
     PinterestIE,
 )
+from .piramidetv import (
+    PiramideTVChannelIE,
+    PiramideTVIE,
+)
 from .pixivsketch import (
     PixivSketchIE,
     PixivSketchUserIE,
