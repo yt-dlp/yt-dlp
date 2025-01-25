a53515b56f3cadb0917e1e1c1e67647011d8ddff
bashonly <bashonly@protonmail.com>
2025-01-25 15:03:24 -0600

    cleanup
    Authored by: bashonly

--- yt_dlp/extractor/soundcloud.py
+++ yt_dlp/extractor/soundcloud.py
@@ -665,6 +665,11 @@ class SoundcloudPlaylistBaseIE(SoundcloudBaseIE):
                     'playlistId': playlist_id,
                     'playlistSecretToken': token,
                 }, headers=self._HEADERS)
+        album_info = traverse_obj(playlist, {
+            'album': ('title', {str}),
+            'album_artist': ('user', 'username', {str}),
+            'album_type': ('set_type', {str}),
+        })
         entries = []
         for track in tracks:
             track_id = str_or_none(track.get('id'))
@@ -676,26 +681,15 @@ class SoundcloudPlaylistBaseIE(SoundcloudBaseIE):
                 if token:
                     url += '?secret_token=' + token
             entries.append(self.url_result(
-                url,
-                SoundcloudIE.ie_key(),
-                track_id,
-                url_transparent=True,
-                **traverse_obj(playlist, {
-                    'album': ('title', {str}),
-                    'album_artist': ('user', 'username', {str}),
-                    'album_type': ('set_type', {str}),
-                })),
-            )
+                url, SoundcloudIE.ie_key(), track_id, url_transparent=True, **album_info))
         return self.playlist_result(
             entries, playlist_id,
             playlist.get('title'),
             playlist.get('description'),
+            **album_info,
             **traverse_obj(playlist, {
                 'uploader': ('user', 'username', {str}),
                 'uploader_id': ('user', 'id', {str_or_none}),
-                'album': ('title', {str}),
-                'album_artist': ('user', 'username', {str}),
-                'album_type': ('set_type', {str}),
             }),
         )
 
