e51235bf937345e322e388c7104f6223f3891eeb
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-02-09 18:51:40 +0100

    Use URL results for data extraction

--- yt_dlp/extractor/digiview.py
+++ yt_dlp/extractor/digiview.py
@@ -1,5 +1,5 @@
 from .youtube import YoutubeIE
-from ..utils import int_or_none, urlencode_postdata
+from ..utils import clean_html, int_or_none, traverse_obj, url_or_none, urlencode_postdata
 
 
 class DigiviewIE(YoutubeIE):
@@ -25,31 +25,22 @@ class DigiviewIE(YoutubeIE):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        webpage_data = self._download_json(
+        video_data = self._download_json(
             'https://ladigitale.dev/digiview/inc/recuperer_video.php', video_id,
             data=urlencode_postdata({'id': video_id}))
 
-        youtube_ie = YoutubeIE()
-        youtube_ie.set_downloader(self._downloader)
-        info = youtube_ie._real_extract(webpage_data['videoId'])
-
-        # replace the YouTube metadata by the Digiview one
-        info['title'] = webpage_data.get('titre') or info['title']
-        info['description'] = webpage_data.get('description') or info['description']
-
-        ffmpeg_args = []
-
-        start_time = int_or_none(webpage_data.get('debut'))
-        if start_time is not None and start_time != 0:
-            ffmpeg_args.extend(['-ss', str(start_time)])
-
-        end_time = int_or_none(webpage_data.get('fin'))
-        if end_time is not None and end_time != info['duration']:
-            ffmpeg_args.extend(['-t', str(end_time - (start_time or 0))])
-
-        if ffmpeg_args and self._downloader:
-            # cut the video if specified in the Digiview webpage
-            ppargs = self._downloader.params.get('postprocessor_args')
-            ppargs.setdefault('merger', []).extend(ffmpeg_args)
-
-        return info
+        clip_id = video_data['videoId']
+        return {
+            '_type': 'url_transparent',
+            'url': f'https://www.youtube.com/watch?v={clip_id}',
+            'ie_key': YoutubeIE.ie_key(),
+            'id': clip_id,
+            **traverse_obj(video_data, {
+                'section_start': ('debut', {int_or_none}),
+                'section_end': ('fin', {int_or_none}),
+                'description': ('description', {clean_html}),
+                'title': ('titre', {str}),
+                'thumbnail': ('vignette', {url_or_none}),
+                'view_count': ('vues', {int_or_none}),
+            }),
+        }
