2df5c07325d41210048fb5fde089fd8ff9bb4508
Zhan Ziyuan <154674404+ZhanZiyuan@users.noreply.github.com>
2025-02-15 19:25:12 +0800

    Moved `import yt_dlp`.
    Moved `import yt_dlp` in `__main__.py`.

--- yt_dlp/__main__.py
+++ yt_dlp/__main__.py
@@ -6,12 +6,12 @@
 import sys
 from pathlib import Path
 
-import yt_dlp
-
 if __package__ is None and not getattr(sys, 'frozen', False):
     # direct call of __main__.py
     path = Path(__file__).resolve()
     sys.path.insert(0, str(path.parent.parent))
+
+import yt_dlp
 
 if __name__ == '__main__':
 
