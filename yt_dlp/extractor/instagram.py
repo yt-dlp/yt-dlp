6660d09f094c5d5ad0dc5ebb800f4cff497483bd
bashonly <88596187+bashonly@users.noreply.github.com>
2025-02-23 07:10:08 +0000

    Apply suggestions from code review

--- yt_dlp/extractor/instagram.py
+++ yt_dlp/extractor/instagram.py
@@ -689,7 +689,7 @@ class InstagramTagIE(InstagramPlaylistBaseIE):
 
 
 class InstagramStoryIE(InstagramBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?instagram\.com/stories/(?P<user>[^/]+)(?:/(?P<id>\d+))?'
+    _VALID_URL = r'https?://(?:www\.)?instagram\.com/stories/(?P<user>[^/?#]+)(?:/(?P<id>\d+))?'
     IE_NAME = 'instagram:story'
 
     _TESTS = [{
@@ -710,7 +710,7 @@ class InstagramStoryIE(InstagramBaseIE):
     def _real_extract(self, url):
         username, story_id = self._match_valid_url(url).group('user', 'id')
         if username == 'highlights' and not story_id:  # story id is only mandatory for highlights
-            raise ExtractorError('Missing highlight id')
+            raise ExtractorError('Input URL is missing a highlight ID', expected=True)
         display_id = story_id or username
         story_info = self._download_webpage(url, display_id)
         user_info = self._search_json(r'"user":', story_info, 'user info', display_id, fatal=False)
@@ -730,6 +730,7 @@ class InstagramStoryIE(InstagramBaseIE):
             display_id, errnote=False, fatal=False, headers=self._API_HEADERS), 'reels')
         if not videos:
             self.raise_login_required('You need to log in to access this content')
+        user_info = traverse_obj(videos, (user_id, 'user', {dict})) or {}
 
         full_name = traverse_obj(videos, (f'highlight:{story_id}', 'user', 'full_name'), (user_id, 'user', 'full_name'))
         story_title = traverse_obj(videos, (f'highlight:{story_id}', 'title'))
@@ -739,8 +740,8 @@ class InstagramStoryIE(InstagramBaseIE):
         highlights = traverse_obj(videos, (f'highlight:{story_id}', 'items'), (user_id, 'items'))
         info_data = []
         for highlight in highlights:
-            if user_info := traverse_obj(videos, (user_id, 'user')):
-                highlight['user'].update(user_info)
+            highlight.setdefault('user', {})
+            highlight['user'].update(user_info)
             highlight_data = self._extract_product(highlight)
             if highlight_data.get('formats'):
                 info_data.append({
