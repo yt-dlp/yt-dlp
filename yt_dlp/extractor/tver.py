c5e35a35f7fc59c068fa2dd77e53cba7971a278c
fireattack <human.peng@gmail.com>
2025-03-10 11:09:27 +0800

    Fix date might be last year; rename to broadcast_date; add an actual release_timestamp

--- yt_dlp/extractor/tver.py
+++ yt_dlp/extractor/tver.py
@@ -148,16 +148,17 @@ class TVerIE(InfoExtractor):
         onair_label = str_or_none(episode_content.get('broadcastDateLabel'))
 
         if mobj := re.search(r'(\d+)月(\d+)日', onair_label):
-            year = dt.datetime.now(dt.timezone(dt.timedelta(hours=9))).year
             month, day = map(int, mobj.groups())
-            release_year = year
-            release_date = dt.datetime(year, month, day).strftime('%Y%m%d')
+            now = dt.datetime.now(dt.timezone(dt.timedelta(hours=9)))
+            year = now.year - 1 if now.replace(month=month, day=day) > now else now.year
+            broadcast_year = year
+            broadcast_date = dt.datetime(year, month, day).strftime('%Y%m%d')
         elif mobj := re.search(r'(\d{4})年放送', onair_label):
-            release_year = int(mobj.group(1))
-            release_date = None
+            broadcast_year = int(mobj.group(1))
+            broadcast_date = None
         else:
-            release_year = None
-            release_date = None
+            broadcast_year = None
+            broadcast_date = None
 
         thumbnails = [
             {
@@ -189,6 +190,7 @@ class TVerIE(InfoExtractor):
             'url': smuggle_url(
                 self.BRIGHTCOVE_URL_TEMPLATE % (p_id, r_id), {'geo_countries': ['JP']}),
             'ie_key': 'BrightcoveNew',
-            'release_year': release_year,
-            'release_date': release_date,
+            'release_timestamp': traverse_obj(video_info, ('viewStatus', 'startAt')),
+            'broadcast_year': broadcast_year,
+            'broadcast_date': broadcast_date,
         }
