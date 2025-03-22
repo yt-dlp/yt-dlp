fbecb9b50a1d16c43d7becee15018e7643c0d20a
bashonly <bashonly@protonmail.com>
2025-03-22 15:52:32 -0500

    [ie/9now.com.au] Fix extractor
    Authored by: bashonly

--- yt_dlp/extractor/ninenow.py
+++ yt_dlp/extractor/ninenow.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
@@ -9,26 +12,33 @@ from ..utils import (
     unified_strdate,
     unified_timestamp,
 )
+from ..utils.traversal import traverse_obj
 
 
 class NineNowIE(InfoExtractor):
     IE_NAME = '9now.com.au'
-    _VALID_URL = r'https?://(?:www\.)?9now\.com\.au/(?:[^/]+/){2}(?P<id>[^/?#]+)'
+    _VALID_URL = r'https?://(?:www\.)?9now\.com\.au/(?:[^/]+/){2}(?P<id>(?P<type>clip|episode)-[^/?#]+)'
     _GEO_COUNTRIES = ['AU']
     _TESTS = [{
         # clip
-        'url': 'https://www.9now.com.au/afl-footy-show/2016/clip-ciql02091000g0hp5oktrnytc',
-        'md5': '17cf47d63ec9323e562c9957a968b565',
+        'url': 'https://www.9now.com.au/today/season-2025/clip-cm8hw9h5z00080hquqa5hszq7',
         'info_dict': {
-            'id': '16801',
+            'id': '6370295582112',
             'ext': 'mp4',
-            'title': 'St. Kilda\'s Joey Montagna on the potential for a player\'s strike',
-            'description': 'Is a boycott of the NAB Cup "on the table"?',
+            'title': 'Would Karl Stefanovic be able to land a plane?',
+            'description': 'The Today host\'s skills are put to the test with the latest simulation tech.',
             'uploader_id': '4460760524001',
-            'upload_date': '20160713',
-            'timestamp': 1468421266,
+            'duration': 197.376,
+            'tags': ['clip', 'flights', 'technology', 'karl stefanovic'],
+            'season': 'Season 2025',
+            'season_number': 2025,
+            'timestamp': 1742507988,
+            'upload_date': '20250320',
+            'release_date': '20250320',
         },
-        'skip': 'Only available in Australia',
+        'params': {
+            'skip_download': True,
+        },
     }, {
         # episode
         'url': 'https://www.9now.com.au/afl-footy-show/2016/episode-19',
@@ -41,7 +51,7 @@ class NineNowIE(InfoExtractor):
         # episode of series
         'url': 'https://www.9now.com.au/lego-masters/season-3/episode-3',
         'info_dict': {
-            'id': '6249614030001',
+            'id': '6308830406112',
             'title': 'Episode 3',
             'ext': 'mp4',
             'season_number': 3,
@@ -50,17 +60,27 @@ class NineNowIE(InfoExtractor):
             'uploader_id': '4460760524001',
             'timestamp': 1619002200,
             'upload_date': '20210421',
+            'duration': 3574.085,
+            'thumbnail': r're:https?://.+/.+\.jpg',
+            'tags': ['episode'],
+            'season': 'Season 3',
+            'episode': 'Episode 3',
+            'release_date': '20210421',
         },
-        'expected_warnings': ['Ignoring subtitle tracks'],
         'params': {
             'skip_download': True,
         },
     }]
     BRIGHTCOVE_URL_TEMPLATE = 'http://players.brightcove.net/4460760524001/default_default/index.html?videoId=%s'
 
-    def _real_extract(self, url):
-        display_id = self._match_id(url)
-        webpage = self._download_webpage(url, display_id)
+    # XXX: For parsing next.js v15+ data; see also yt_dlp.extractor.francetv and yt_dlp.extractor.goplay
+    def _find_json(self, s):
+        return self._search_json(
+            r'\w+\s*:\s*', s, 'next js data', None, contains_pattern=r'\[(?s:.+)\]', default=None)
+
+    def _old_extract_common_data(self, webpage, display_id):
+        self.write_debug('Falling back to the old method for extracting common data')
+
         page_data = self._parse_json(self._search_regex(
             r'window\.__data\s*=\s*({.*?});', webpage,
             'page data', default='{}'), display_id, fatal=False)
@@ -77,25 +97,35 @@ class NineNowIE(InfoExtractor):
             cache = page_data.get(kind, {}).get(f'{kind}Cache', {})
             if not cache:
                 continue
-            common_data = {
+            return {
                 'episode': (cache.get(current_key) or next(iter(cache.values())))[kind],
                 'season': (cache.get(current_key) or next(iter(cache.values()))).get('season', None),
             }
-            break
-        else:
-            raise ExtractorError('Unable to find video data')
 
-        if not self.get_param('allow_unplayable_formats') and try_get(common_data, lambda x: x['episode']['video']['drm'], bool):
+    def _real_extract(self, url):
+        display_id, video_type = self._match_valid_url(url).group('id', 'type')
+        webpage = self._download_webpage(url, display_id)
+
+        common_data = traverse_obj(
+            re.findall(r'<script[^>]*>\s*self\.__next_f\.push\(\s*(\[.+?\])\s*\);?\s*</script>', webpage),
+            (..., {json.loads}, ..., {self._find_json},
+             lambda _, v: v['payload'][video_type]['slug'] == display_id,
+             'payload', any)) or self._old_extract_common_data(webpage, display_id)
+        if not common_data:
+            raise ExtractorError('Unable to extract video data')
+
+        if traverse_obj(common_data, (video_type, 'video', 'drm', {bool})):
             self.report_drm(display_id)
+
         brightcove_id = try_get(
-            common_data, lambda x: x['episode']['video']['brightcoveId'], str) or 'ref:{}'.format(common_data['episode']['video']['referenceId'])
+            common_data, lambda x: x[video_type]['video']['brightcoveId'], str) or 'ref:{}'.format(common_data[video_type]['video']['referenceId'])
         video_id = str_or_none(try_get(common_data, lambda x: x['episode']['video']['id'])) or brightcove_id
 
-        title = try_get(common_data, lambda x: x['episode']['name'], str)
+        title = try_get(common_data, lambda x: x[video_type]['name'], str)
         season_number = try_get(common_data, lambda x: x['season']['seasonNumber'], int)
         episode_number = try_get(common_data, lambda x: x['episode']['episodeNumber'], int)
         timestamp = unified_timestamp(try_get(common_data, lambda x: x['episode']['airDate'], str))
-        release_date = unified_strdate(try_get(common_data, lambda x: x['episode']['availability'], str))
+        release_date = unified_strdate(try_get(common_data, lambda x: x[video_type]['availability'], str))
         thumbnails_data = try_get(common_data, lambda x: x['episode']['image']['sizes'], dict) or {}
         thumbnails = [{
             'id': thumbnail_id,
@@ -110,8 +140,8 @@ class NineNowIE(InfoExtractor):
                 {'geo_countries': self._GEO_COUNTRIES}),
             'id': video_id,
             'title': title,
-            'description': try_get(common_data, lambda x: x['episode']['description'], str),
-            'duration': float_or_none(try_get(common_data, lambda x: x['episode']['video']['duration'], float), 1000),
+            'description': try_get(common_data, lambda x: x[video_type]['description'], str),
+            'duration': float_or_none(try_get(common_data, lambda x: x[video_type]['video']['duration'], float), 1000),
             'thumbnails': thumbnails,
             'ie_key': 'BrightcoveNew',
             'season_number': season_number,
