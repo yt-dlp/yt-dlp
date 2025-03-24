7f5c6f2fb67a3df66bae24da5c95c2dd73894489
Tahasanul Abraham <tahasanul.ibrahim@gmail.com>
2025-03-24 22:30:32 +0100

    [xvideos] Support profiles, searches, channels and favourites

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2503,7 +2503,7 @@ from .xinpianchang import XinpianchangIE
 from .xminus import XMinusIE
 from .xnxx import XNXXIE
 from .xstream import XstreamIE
-from .xvideos import (    
+from .xvideos import (
     XVideosChannelIE,
     XVideosIE,
     XVideosPlaylistIE,
