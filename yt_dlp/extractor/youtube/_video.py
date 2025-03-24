2a555d80dceb25486eff11170142651333a06637
bashonly <bashonly@protonmail.com>
2025-03-24 11:57:17 -0500

    [ie/youtube] Fix nsig extraction for player `363db69b`
    Closes #12724
    
    Authored by: bashonly

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -2179,7 +2179,10 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                 \'use\s+strict\';\s*
                 (?P<code>
                     var\s+(?P<name>[a-zA-Z0-9_$]+)\s*=\s*
-                    (?P<value>"(?:[^"\\]|\\.)+"\.split\("[^"]+"\))
+                    (?P<value>
+                        (?P<q1>["\'])(?:(?!(?P=q1)).)+(?P=q1)
+                        \.split\((?P<q2>["\'])(?:(?!(?P=q1)).)+(?P=q2)\)
+                    )
                 )[;,]
             ''', jscode, 'global variable', group=('code', 'name', 'value'), default=(None, None, None))
 
