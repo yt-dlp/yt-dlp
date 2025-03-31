bfaa5b5a8a2da0fbd5fdaab21011a87d7a19dbee
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-03-31 07:43:53 +0500

    search and extract from json

--- yt_dlp/extractor/francaisfacile.py
+++ yt_dlp/extractor/francaisfacile.py
@@ -1,18 +1,24 @@
 from .common import InfoExtractor
+from ..utils import float_or_none, url_or_none
+from ..utils.traversal import traverse_obj
 
 
 class FrancaisFacileIE(InfoExtractor):
-    _VALID_URL = r'https?:\/\/francaisfacile\.rfi\.fr\/fr\/(actualit%C3%A9|podcasts\/[^\/]+)\/(?P<id>\d+)-.*'
+    _VALID_URL = r'https?://francaisfacile\.rfi\.fr/fr/(actualit%C3%A9|podcasts/[^/#?]+)/(?P<id>\d+)-'
     IE_NAME = 'francaisfacile'
     _TESTS = [
         {
-            'url': 'https://francaisfacile.rfi.fr/fr/podcasts/un-mot-une-histoire/20250317-le-mot-de-david-foenkinos-peut-%C3%AAtre',
-            'md5': 'db83c2cc2589b4c24571c6b6cf14f5f1',
+            'url': 'https://francaisfacile.rfi.fr/fr/actualit%C3%A9/20250305-r%C3%A9concilier-les-jeunes-avec-la-lecture-gr%C3%A2ce-aux-r%C3%A9seaux-sociaux',
+            'md5': '4f33674cb205744345cc835991100afa',
             'info_dict': {
-                'id': '20250317',
-                'title': 'Le mot de David Foenkinos: «peut-être» - Un mot, une histoire',
-                'url': 'https://aod-fle.akamaized.net/fle/sounds/fr/2025/03/17/4ca6cbbe-0315-11f0-a85b-005056a97652.mp3',
+                'id': '20250305',
+                'title': 'Réconcilier les jeunes avec la lecture grâce aux réseaux sociaux',
+                'url': 'https://aod-fle.akamaized.net/fle/sounds/fr/2025/03/05/6b6af52a-f9ba-11ef-a1f8-005056a97652.mp3',
                 'ext': 'mp3',
+                'description': 'md5:b903c63d8585bd59e8cc4d5f80c4272d',
+                'duration': 103.15,
+                'timestamp': 1741177984,
+                'upload_date': '20250305',
             },
         },
         {
@@ -23,16 +29,24 @@ class FrancaisFacileIE(InfoExtractor):
                 'title': 'Argentine: le sac d\'un alpiniste retrouvé 40 ans après sa mort',
                 'url': 'https://aod-fle.akamaized.net/fle/sounds/fr/2025/03/07/8edf4082-fb46-11ef-8a37-005056bf762b.mp3',
                 'ext': 'mp3',
+                'description': 'md5:7fd088fbdf4a943bb68cf82462160dca',
+                'duration': 117.74,
+                'timestamp': 1741352789,
+                'upload_date': '20250307',
             },
         },
         {
-            'url': 'https://francaisfacile.rfi.fr/fr/actualit%C3%A9/20250305-r%C3%A9concilier-les-jeunes-avec-la-lecture-gr%C3%A2ce-aux-r%C3%A9seaux-sociaux',
-            'md5': '4f33674cb205744345cc835991100afa',
+            'url': 'https://francaisfacile.rfi.fr/fr/podcasts/un-mot-une-histoire/20250317-le-mot-de-david-foenkinos-peut-%C3%AAtre',
+            'md5': 'db83c2cc2589b4c24571c6b6cf14f5f1',
             'info_dict': {
-                'id': '20250305',
-                'title': 'Réconcilier les jeunes avec la lecture grâce aux réseaux sociaux',
-                'url': 'https://aod-fle.akamaized.net/fle/sounds/fr/2025/03/05/6b6af52a-f9ba-11ef-a1f8-005056a97652.mp3',
+                'id': '20250317',
+                'title': 'Le mot de David Foenkinos: «peut-être» - Un mot, une histoire',
+                'url': 'https://aod-fle.akamaized.net/fle/sounds/fr/2025/03/17/4ca6cbbe-0315-11f0-a85b-005056a97652.mp3',
                 'ext': 'mp3',
+                'description': 'md5:3fe35fae035803df696bfa7af2496e49',
+                'duration': 198.96,
+                'timestamp': 1742210897,
+                'upload_date': '20250317',
             },
         },
     ]
@@ -40,11 +54,16 @@ class FrancaisFacileIE(InfoExtractor):
     def _real_extract(self, url):
         audio_id = self._match_id(url)
         webpage = self._download_webpage(url, audio_id, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:135.0) Gecko/20100101 Firefox/135.0'})
-        title = self._html_extract_title(webpage, fatal=True)
-        audio_url = self._html_search_regex(r'(?P<url>https://aod-fle\.akamaized\.net/fle/sounds/fr/.+?\.mp3)', webpage, 'audio URL', group='url')
+        data = self._search_json(
+            r'<script[^>]+\bdata-media-id=[^>]+\btype="application/json"[^>]*>',
+            webpage, 'audio data', audio_id)
         return {
             'id': audio_id,
-            'title': title,
-            'url': audio_url,
-            'ext': 'mp3',
+            'title': self._html_extract_title(webpage),
+            **self._search_json_ld(webpage, audio_id, fatal=False),
+            **traverse_obj(data, {
+                'title': ('title', {str}),
+                'url': ('sources', ..., 'url', {url_or_none}, any),
+                'duration': ('sources', ..., 'duration', {float_or_none}, any),
+            }),
         }
