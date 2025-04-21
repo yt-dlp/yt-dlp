337bb8fce957d802248cc4ca4e4b20f2b9d642ab
Sergey B (Troex Nevelin) <436912+troex@users.noreply.github.com>
2025-04-21 14:36:46 +0200

    Update yt_dlp/extractor/getcourseru.py
    thanks, confirm it's now downloading with proper name and doesn't cause an 'tuple' error
    
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/getcourseru.py
+++ yt_dlp/extractor/getcourseru.py
@@ -8,7 +8,7 @@ from ..utils.traversal import traverse_obj
 
 
 class GetCourseRuPlayerIE(InfoExtractor):
-    _VALID_URL = r'https?://(player02\.getcourse\.ru|cf-api-2\.vhcdn\.com)/sign-player/?\?(?:[^#]+&)?json=[^#&]+'
+    _VALID_URL = r'https?://(?:player02\.getcourse\.ru|cf-api-2\.vhcdn\.com)/sign-player/?\?(?:[^#]+&)?json=[^#&]+'
     _EMBED_REGEX = [rf'<iframe[^>]+\bsrc=[\'"](?P<url>{_VALID_URL}[^\'"]*)']
     _TESTS = [{
         'url': 'http://player02.getcourse.ru/sign-player/?json=eyJ2aWRlb19oYXNoIjoiMTkwYmRmOTNmMWIyOTczNTMwOTg1M2E3YTE5ZTI0YjMiLCJ1c2VyX2lkIjozNTk1MjUxODMsInN1Yl9sb2dpbl91c2VyX2lkIjpudWxsLCJsZXNzb25faWQiOm51bGwsImlwIjoiNDYuMTQyLjE4Mi4yNDciLCJnY19ob3N0IjoiYWNhZGVteW1lbC5vbmxpbmUiLCJ0aW1lIjoxNzA1NDQ5NjQyLCJwYXlsb2FkIjoidV8zNTk1MjUxODMiLCJ1aV9sYW5ndWFnZSI6InJ1IiwiaXNfaGF2ZV9jdXN0b21fc3R5bGUiOnRydWV9&s=354ad2c993d95d5ac629e3133d6cefea&vh-static-feature=zigzag',
