126292524e03431dd4ceda0f53f4ea7af7ac0738
Alan Xiao <alanxiao@axcdev.com>
2025-04-23 20:02:15 -0400

    Update _utils.py
    Resolve string termination issue.

--- yt_dlp/utils/_utils.py
+++ yt_dlp/utils/_utils.py
@@ -167,7 +167,7 @@ JSON_LD_RE = r'(?is)<script[^>]+type=(["\']?)application/ld\+json\1[^>]*>\s*(?P<
 
 NUMBER_RE = r'\d+(?:\.\d+)?'
 
-WINDOWS_RESERVED_NAMES_RE = fr'({'|'.join(
+WINDOWS_RESERVED_NAMES_RE = fr'({"|".join(
     ("CON", "CONOUT$", "CONIN$", "PRN", "AUX", "CLOCK$", "NUL")
     + tuple(f"{name:s}{num:d}" for name, num in itertools.product(("COM", "LPT"), range(0, 10)))
     + tuple(
