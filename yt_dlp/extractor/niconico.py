9a7cd32c6a3c68de8dbf31f710fabdf647aae20b
bashonly <88596187+bashonly@users.noreply.github.com>
2025-04-28 22:28:48 +0000

    minutia

--- yt_dlp/extractor/niconico.py
+++ yt_dlp/extractor/niconico.py
@@ -80,15 +80,15 @@ class NiconicoBaseIE(InfoExtractor):
                 }, data=urlencode_postdata({
                     'otp': self._get_tfa_info('6 digit number shown on app'),
                 }))
-            if self.is_logged_in:
-                return
-            elif 'error-code' in parse_qs(urlh.url):
+            if 'error-code' in parse_qs(urlh.url):
                 err_msg = traverse_obj(mfa, ({find_element(cls='pageMainMsg')}, {clean_html}))
                 self._raise_login_error(err_msg, 'MFA session expired')
             elif 'formError' in mfa:
                 err_msg = traverse_obj(mfa, (
                     {find_element(cls='formError')}, {find_element(tag='div')}, {clean_html}))
                 self._raise_login_error(err_msg, 'MFA challenge failed')
+            elif self.is_logged_in:
+                return
 
         self._raise_login_error(None, 'Unexpected login error', expected=False)
 
