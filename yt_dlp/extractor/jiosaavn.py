42ad4197da432f168a02fb11ddae3bddc71c5125
Subrat Lima <subrat.k.lima@protonmail.com>
2025-05-11 00:39:35 -0500

    [ie/jiosaavn:show] Add extractor (#12803)
    Closes #12766
    
    Authored by: subrat-lima

--- yt_dlp/extractor/jiosaavn.py
+++ yt_dlp/extractor/jiosaavn.py
@@ -5,19 +5,24 @@ import re
 from .common import InfoExtractor
 from ..utils import (
     InAdvancePagedList,
+    ISO639Utils,
     clean_html,
     int_or_none,
     make_archive_id,
     smuggle_url,
+    unified_strdate,
+    unified_timestamp,
     unsmuggle_url,
     url_basename,
     url_or_none,
     urlencode_postdata,
+    urljoin,
 )
 from ..utils.traversal import traverse_obj
 
 
 class JioSaavnBaseIE(InfoExtractor):
+    _URL_BASE_RE = r'https?://(?:www\.)?(?:jio)?saavn\.com'
     _API_URL = 'https://www.jiosaavn.com/api.php'
     _VALID_BITRATES = {'16', '32', '64', '128', '320'}
 
@@ -30,16 +35,20 @@ class JioSaavnBaseIE(InfoExtractor):
                 f'Valid bitrates are: {", ".join(sorted(self._VALID_BITRATES, key=int))}')
         return requested_bitrates
 
-    def _extract_formats(self, song_data):
+    def _extract_formats(self, item_data):
+        # Show/episode JSON data has a slightly different structure than song JSON data
+        if media_url := traverse_obj(item_data, ('more_info', 'encrypted_media_url', {str})):
+            item_data.setdefault('encrypted_media_url', media_url)
+
         for bitrate in self.requested_bitrates:
             media_data = self._download_json(
-                self._API_URL, song_data['id'],
+                self._API_URL, item_data['id'],
                 f'Downloading format info for {bitrate}',
                 fatal=False, data=urlencode_postdata({
                     '__call': 'song.generateAuthToken',
                     '_format': 'json',
                     'bitrate': bitrate,
-                    'url': song_data['encrypted_media_url'],
+                    'url': item_data['encrypted_media_url'],
                 }))
             if not traverse_obj(media_data, ('auth_url', {url_or_none})):
                 self.report_warning(f'Unable to extract format info for {bitrate}')
@@ -52,24 +61,6 @@ class JioSaavnBaseIE(InfoExtractor):
                 'abr': int(bitrate),
                 'vcodec': 'none',
             }
-
-    def _extract_song(self, song_data, url=None):
-        info = traverse_obj(song_data, {
-            'id': ('id', {str}),
-            'title': ('song', {clean_html}),
-            'album': ('album', {clean_html}),
-            'thumbnail': ('image', {url_or_none}, {lambda x: re.sub(r'-\d+x\d+\.', '-500x500.', x)}),
-            'duration': ('duration', {int_or_none}),
-            'view_count': ('play_count', {int_or_none}),
-            'release_year': ('year', {int_or_none}),
-            'artists': ('primary_artists', {lambda x: x.split(', ') if x else None}),
-            'webpage_url': ('perma_url', {url_or_none}),
-        })
-        if webpage_url := info.get('webpage_url') or url:
-            info['display_id'] = url_basename(webpage_url)
-            info['_old_archive_ids'] = [make_archive_id(JioSaavnSongIE, info['display_id'])]
-
-        return info
 
     def _call_api(self, type_, token, note='API', params={}):
         return self._download_json(
@@ -84,6 +75,81 @@ class JioSaavnBaseIE(InfoExtractor):
                 **params,
             })
 
+    @staticmethod
+    def _extract_common_info(data):
+        return traverse_obj(data, {
+            'id': ('id', {str}),
+            'thumbnail': ('image', {url_or_none}, {lambda x: re.sub(r'-\d+x\d+\.', '-500x500.', x)}),
+            'view_count': ('play_count', {int_or_none}),
+            'release_year': ('year', {int_or_none}),
+            'language': ('language', {lambda x: ISO639Utils.short2long(x.casefold()) or 'und'}),
+            'webpage_url': ('perma_url', {url_or_none}),
+        })
+
+    @staticmethod
+    def _extract_song(song_data, url=None):
+        info = JioSaavnBaseIE._extract_common_info(song_data)
+        info.update(traverse_obj(song_data, {
+            'title': ('song', {clean_html}),
+            'album': ('album', {clean_html}),
+            'duration': ('duration', {int_or_none}),
+            'release_date': ('release_date', {unified_strdate}),
+            'channel': ('label', {str}),
+            'channel_id': ('label_id', {str}),
+            'channel_url': ('label_url', {urljoin('https://www.jiosaavn.com/')}),
+            'artists': ('primary_artists', {lambda x: x.split(', ') if x else None}),
+        }))
+        if webpage_url := info.get('webpage_url') or url:
+            info['display_id'] = url_basename(webpage_url)
+            info['_old_archive_ids'] = [make_archive_id(JioSaavnSongIE, info['display_id'])]
+
+        return info
+
+    @staticmethod
+    def _extract_episode(episode_data, url=None):
+        info = JioSaavnBaseIE._extract_common_info(episode_data)
+        info.update(traverse_obj(episode_data, {
+            'title': ('title', {clean_html}),
+            'description': ('more_info', 'description', {str}),
+            'duration': ('more_info', 'duration', {int_or_none}),
+            'timestamp': ('more_info', 'release_time', {unified_timestamp}),
+            'channel': ('more_info', 'label', {str}),
+            'channel_id': ('more_info', 'label_id', {str}),
+            'channel_url': ('more_info', 'label_url', {urljoin('https://www.jiosaavn.com/')}),
+            'series': ('more_info', 'show_title', {str}),
+            'series_id': ('more_info', 'show_id', {str}),
+            'season': ('more_info', 'season_title', {str}),
+            'season_number': ('more_info', 'season_no', {int_or_none}),
+            'season_id': ('more_info', 'season_id', {str}),
+            'episode_number': ('more_info', 'episode_number', {int_or_none}),
+            'cast': ('starring', {lambda x: x.split(', ') if x else None}),
+            'artists': ('more_info', 'artistMap', 'primary_artists', ..., 'name', {str}),
+        }))
+        if webpage_url := info.get('webpage_url') or url:
+            info['display_id'] = url_basename(webpage_url)
+
+        if featured_artists := traverse_obj(episode_data, ('featured_artists', {str})):
+            info.setdefault('artists', []).extend(featured_artists.split(', '))
+
+        return info
+
+    def _extract_jiosaavn_result(self, url, endpoint, response_key, parse_func):
+        url, smuggled_data = unsmuggle_url(url)
+        data = traverse_obj(smuggled_data, ({
+            'id': ('id', {str}),
+            'encrypted_media_url': ('encrypted_media_url', {str}),
+        }))
+
+        if 'id' in data and 'encrypted_media_url' in data:
+            result = {'id': data['id']}
+        else:
+            # only extract metadata if this is not a url_transparent result
+            data = self._call_api(endpoint, self._match_id(url))[response_key][0]
+            result = parse_func(data, url)
+
+        result['formats'] = list(self._extract_formats(data))
+        return result
+
     def _yield_songs(self, playlist_data):
         for song_data in traverse_obj(playlist_data, ('songs', lambda _, v: v['id'] and v['perma_url'])):
             song_info = self._extract_song(song_data)
@@ -96,7 +162,7 @@ class JioSaavnBaseIE(InfoExtractor):
 
 class JioSaavnSongIE(JioSaavnBaseIE):
     IE_NAME = 'jiosaavn:song'
-    _VALID_URL = r'https?://(?:www\.)?(?:jiosaavn\.com/song/[^/?#]+/|saavn\.com/s/song/(?:[^/?#]+/){3})(?P<id>[^/?#]+)'
+    _VALID_URL = JioSaavnBaseIE._URL_BASE_RE + r'(?:/song/[^/?#]+/|/s/song/(?:[^/?#]+/){3})(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.jiosaavn.com/song/leja-re/OQsEfQFVUXk',
         'md5': '3b84396d15ed9e083c3106f1fa589c04',
@@ -106,12 +172,38 @@ class JioSaavnSongIE(JioSaavnBaseIE):
             'ext': 'm4a',
             'title': 'Leja Re',
             'album': 'Leja Re',
-            'thumbnail': r're:https?://c.saavncdn.com/258/Leja-Re-Hindi-2018-20181124024539-500x500.jpg',
+            'thumbnail': r're:https?://.+/.+\.jpg',
             'duration': 205,
             'view_count': int,
             'release_year': 2018,
             'artists': ['Sandesh Shandilya', 'Dhvani Bhanushali', 'Tanishk Bagchi'],
             '_old_archive_ids': ['jiosaavnsong OQsEfQFVUXk'],
+            'channel': 'T-Series',
+            'language': 'hin',
+            'channel_id': '34297',
+            'channel_url': 'https://www.jiosaavn.com/label/t-series-albums/6DLuXO3VoTo_',
+            'release_date': '20181124',
+        },
+    }, {
+        'url': 'https://www.jiosaavn.com/song/chuttamalle/P1FfWjZkQ0Q',
+        'md5': '96296c58d6ce488a417ef0728fd2d680',
+        'info_dict': {
+            'id': 'O94kBTtw',
+            'display_id': 'P1FfWjZkQ0Q',
+            'ext': 'm4a',
+            'title': 'Chuttamalle',
+            'album': 'Devara Part 1 - Telugu',
+            'thumbnail': r're:https?://.+/.+\.jpg',
+            'duration': 222,
+            'view_count': int,
+            'release_year': 2024,
+            'artists': 'count:3',
+            '_old_archive_ids': ['jiosaavnsong P1FfWjZkQ0Q'],
+            'channel': 'T-Series',
+            'language': 'tel',
+            'channel_id': '34297',
+            'channel_url': 'https://www.jiosaavn.com/label/t-series-albums/6DLuXO3VoTo_',
+            'release_date': '20240926',
         },
     }, {
         'url': 'https://www.saavn.com/s/song/hindi/Saathiya/O-Humdum-Suniyo-Re/KAMiazoCblU',
@@ -119,26 +211,50 @@ class JioSaavnSongIE(JioSaavnBaseIE):
     }]
 
     def _real_extract(self, url):
-        url, smuggled_data = unsmuggle_url(url)
-        song_data = traverse_obj(smuggled_data, ({
-            'id': ('id', {str}),
-            'encrypted_media_url': ('encrypted_media_url', {str}),
-        }))
+        return self._extract_jiosaavn_result(url, 'song', 'songs', self._extract_song)
 
-        if 'id' in song_data and 'encrypted_media_url' in song_data:
-            result = {'id': song_data['id']}
-        else:
-            # only extract metadata if this is not a url_transparent result
-            song_data = self._call_api('song', self._match_id(url))['songs'][0]
-            result = self._extract_song(song_data, url)
 
-        result['formats'] = list(self._extract_formats(song_data))
-        return result
+class JioSaavnShowIE(JioSaavnBaseIE):
+    IE_NAME = 'jiosaavn:show'
+    _VALID_URL = JioSaavnBaseIE._URL_BASE_RE + r'/shows/[^/?#]+/(?P<id>[^/?#]{11,})/?(?:$|[?#])'
+    _TESTS = [{
+        'url': 'https://www.jiosaavn.com/shows/non-food-ways-to-boost-your-energy/XFMcKICOCgc_',
+        'md5': '0733cd254cfe74ef88bea1eaedcf1f4f',
+        'info_dict': {
+            'id': 'qqzh3RKZ',
+            'display_id': 'XFMcKICOCgc_',
+            'ext': 'mp3',
+            'title': 'Non-Food Ways To Boost Your Energy',
+            'description': 'md5:26e7129644b5c6aada32b8851c3997c8',
+            'episode': 'Episode 1',
+            'timestamp': 1640563200,
+            'series': 'Holistic Lifestyle With Neha Ranglani',
+            'series_id': '52397',
+            'season': 'Holistic Lifestyle With Neha Ranglani',
+            'season_number': 1,
+            'season_id': '61273',
+            'thumbnail': r're:https?://.+/.+\.jpg',
+            'duration': 311,
+            'view_count': int,
+            'release_year': 2021,
+            'language': 'eng',
+            'channel': 'Saavn OG',
+            'channel_id': '1953876',
+            'episode_number': 1,
+            'upload_date': '20211227',
+        },
+    }, {
+        'url': 'https://www.jiosaavn.com/shows/himesh-reshammiya/Kr8fmfSN4vo_',
+        'only_matching': True,
+    }]
+
+    def _real_extract(self, url):
+        return self._extract_jiosaavn_result(url, 'episode', 'episodes', self._extract_episode)
 
 
 class JioSaavnAlbumIE(JioSaavnBaseIE):
     IE_NAME = 'jiosaavn:album'
-    _VALID_URL = r'https?://(?:www\.)?(?:jio)?saavn\.com/album/[^/?#]+/(?P<id>[^/?#]+)'
+    _VALID_URL = JioSaavnBaseIE._URL_BASE_RE + r'/album/[^/?#]+/(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.jiosaavn.com/album/96/buIOjYZDrNA_',
         'info_dict': {
@@ -158,7 +274,7 @@ class JioSaavnAlbumIE(JioSaavnBaseIE):
 
 class JioSaavnPlaylistIE(JioSaavnBaseIE):
     IE_NAME = 'jiosaavn:playlist'
-    _VALID_URL = r'https?://(?:www\.)?(?:jio)?saavn\.com/(?:s/playlist/(?:[^/?#]+/){2}|featured/[^/?#]+/)(?P<id>[^/?#]+)'
+    _VALID_URL = JioSaavnBaseIE._URL_BASE_RE + r'/(?:s/playlist/(?:[^/?#]+/){2}|featured/[^/?#]+/)(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.jiosaavn.com/s/playlist/2279fbe391defa793ad7076929a2f5c9/mood-english/LlJ8ZWT1ibN5084vKHRj2Q__',
         'info_dict': {
