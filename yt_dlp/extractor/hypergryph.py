1821907d9a766fad7ca816cab06744e380e01795
yuan <yuan@yuandeMacBook-Air.local>
2025-06-03 07:41:43 -0700

    optimize hypergryph extractor

--- yt_dlp/extractor/hypergryph.py
+++ yt_dlp/extractor/hypergryph.py
@@ -5,28 +5,24 @@ from ..utils import js_to_json, traverse_obj
 class MonsterSirenHypergryphMusicIE(InfoExtractor):
     _VALID_URL = r'https?://monster-siren\.hypergryph\.com/music/(?P<id>\d+)'
     _TESTS = [{
-        'url': 'https://monster-siren.hypergryph.com/music/514562',
+        'url': 'https://monster-siren.hypergryph.com/music/779464',
         'info_dict': {
-            'id': '514562',
+            'id': '779464',
             'ext': 'wav',
             'artists': ['塞壬唱片-MSR'],
-            'album': 'Flame Shadow',
-            'title': 'Flame Shadow',
+            'title': '无机物',
         },
     }]
 
     def _real_extract(self, url):
         audio_id = self._match_id(url)
-        webpage = self._download_webpage(url, audio_id)
-        json_data = self._search_json(
-            r'window\.g_initialProps\s*=', webpage, 'data', audio_id, transform_source=js_to_json)
+        json_data = self._download_json(f'https://monster-siren.hypergryph.com/api/song/{audio_id}', audio_id)
 
         return {
             'id': audio_id,
-            'title': traverse_obj(json_data, ('player', 'songDetail', 'name')),
-            'url': traverse_obj(json_data, ('player', 'songDetail', 'sourceUrl')),
+            'title': json_data['data']['name'],
+            'url': json_data['data']['sourceUrl'],
             'ext': 'wav',
             'vcodec': 'none',
-            'artists': traverse_obj(json_data, ('player', 'songDetail', 'artists', ...)),
-            'album': traverse_obj(json_data, ('musicPlay', 'albumDetail', 'name')),
+            'artists': json_data['data']['artists'],
         }
