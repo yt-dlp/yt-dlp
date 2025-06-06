231349786e8c42089c2e079ec94c0ea866c37999
gamer191 <83270075+gamer191@users.noreply.github.com>
2025-06-07 05:32:03 +1000

    [ie/youtube] Extract srt subtitles (#13411)
    Closes #1734
    Authored by: gamer191

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -250,7 +250,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         '400': {'ext': 'mp4', 'height': 1440, 'format_note': 'DASH video', 'vcodec': 'av01.0.12M.08'},
         '401': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'vcodec': 'av01.0.12M.08'},
     }
-    _SUBTITLE_FORMATS = ('json3', 'srv1', 'srv2', 'srv3', 'ttml', 'vtt')
+    _SUBTITLE_FORMATS = ('json3', 'srv1', 'srv2', 'srv3', 'ttml', 'srt', 'vtt')
     _DEFAULT_CLIENTS = ('tv', 'ios', 'web')
     _DEFAULT_AUTHED_CLIENTS = ('tv', 'web')
 
