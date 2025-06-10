6f0e44cffe9c03caa909d1f990c8c07c00ff7a81
tcely <tcely@users.noreply.github.com>
2025-06-10 05:15:45 -0400

    [ie/youtube] use casefold for extractor_key

--- yt_dlp/extractor/youtube/pot/_director.py
+++ yt_dlp/extractor/youtube/pot/_director.py
@@ -364,7 +364,7 @@ def initialize_pot_director(ie):
 
     def get_provider_logger_and_settings(provider, logger_key):
         logger_prefix = f'{logger_key}:{provider.PROVIDER_NAME}'
-        extractor_key = f'{EXTRACTOR_ARG_PREFIX}-{provider.PROVIDER_KEY.lower()}'
+        extractor_key = f'{EXTRACTOR_ARG_PREFIX}-{provider.PROVIDER_KEY}'.casefold()
         return (
             YoutubeIEContentProviderLogger(ie, logger_prefix, log_level=log_level),
             ie.get_param('extractor_args', {}).get(extractor_key, {}))
