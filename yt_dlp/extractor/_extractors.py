8e8e017a3535a31c5973c094719c13a6126d245d
doe1080 <98906116+doe1080@users.noreply.github.com>
2025-06-15 15:04:43 +0900

    [ie/rambler] Add Extractors

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1688,6 +1688,7 @@ from .rai import (
     RaiPlaySoundPlaylistIE,
     RaiSudtirolIE,
 )
+from .rambler import RamblerIE
 from .raywenderlich import (
     RayWenderlichCourseIE,
     RayWenderlichIE,
