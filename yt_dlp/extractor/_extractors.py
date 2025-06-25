889992f6319c27f2ff45f0e0f37374277899ab0b
frank <frank.czr@gmail.com>
2025-06-24 21:58:05 -0500

    [ie/unitednation] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2288,6 +2288,7 @@ from .uliza import (
 )
 from .umg import UMGDeIE
 from .unistra import UnistraIE
+from .unitednation import UnitedNationWebTVExtractorIE
 from .unity import UnityIE
 from .unsupported import (
     KnownDRMIE,
