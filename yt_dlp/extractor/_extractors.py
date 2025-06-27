94d3a683c1f0200c1852b8f325a5c968d4395b21
Khaoklong <chatmongkol.klong@gmail.com>
2025-06-27 21:08:57 +0700

    [Trueidnet] add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2167,6 +2167,10 @@ from .trovo import (
 from .trtcocuk import TrtCocukVideoIE
 from .trtworld import TrtWorldIE
 from .trueid import TrueIDIE
+from .trueidnet import (
+    TrueidnetMovieIE,
+    TrueidnetSeriesIE,
+)
 from .trunews import TruNewsIE
 from .truth import TruthIE
 from .trutv import TruTVIE
