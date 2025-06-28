7c2d5656a1a10d287f9aee0e6071d79fe186b940
bashonly <bashonly@protonmail.com>
2025-06-27 19:31:05 -0500

    refactor
    Authored by: bashonly

--- yt_dlp/extractor/hotstar.py
+++ yt_dlp/extractor/hotstar.py
@@ -260,16 +260,14 @@ class HotStarIE(HotStarBaseIE):
         if video_data.get('drmProtected'):
             self.report_drm(video_id)
 
+        geo_restricted = False
+        formats, subs = [], {}
+        headers = {'Referer': f'{self._BASE_URL}/in'}
         content_type = traverse_obj(
             video_data, (('assetType', 'contentType'), {str}, any)) or self._CONTENT_TYPE[video_type]
 
         # See https://github.com/yt-dlp/yt-dlp/issues/396
-        st = self._download_webpage_handle(f'{self._BASE_URL}/in', video_id)[1].headers.get('x-origin-date')
-
-        geo_restricted = False
-        formats, subs = [], {}
-        headers = {'Referer': f'{self._BASE_URL}/in'}
-
+        st = self._request_webpage(f'{self._BASE_URL}/in', video_id).get_header('x-origin-date')
         watch = self._call_api_v2('pages/watch', video_id, content_type, cookies=cookies, st=st)
         player_config = traverse_obj(watch, (
             'page', 'spaces', 'player', 'widget_wrappers', lambda _, v: v['template'] == 'PlayerWidget',
