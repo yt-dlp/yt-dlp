46a8361a4e62a661401787665099e47c3a3b4886
Clark Andreasen <candreasen@kettleriverconsulting.com>
2025-06-28 21:51:13 -0500

    [Sauceplus] add extractor

---
+++ yt_dlp/extractor/sauceplus.py
@@ -0,0 +1,40 @@
+from .floatplane import FloatplaneBaseIE
+
+
+class SauceplusIE(FloatplaneBaseIE):
+    _VALID_URL = r'https?://(?:(?:www|beta)\.)?sauceplus\.com/post/(?P<id>\w+)'
+    _BASE_URL = 'https://www.sauceplus.com'
+    _HEADERS = {'Origin': _BASE_URL,
+                'Referer': _BASE_URL + '/'}
+    _TESTS = [{
+        'url': 'https://www.sauceplus.com/post/YbBwIa2A5g',
+        'info_dict': {
+            'id': 'eit4Ugu5TL',
+            'ext': 'mp4',
+            'display_id': 'YbBwIa2A5g',
+            'title': 'Scare the Coyote - Episode 3',
+            'description': '',
+            'thumbnail': r're:^https?://.*\.jpe?g$',
+            'duration': 2975,
+            'comment_count': int,
+            'like_count': int,
+            'dislike_count': int,
+            'release_date': '20250627',
+            'release_timestamp': 1750993500,
+            'uploader': 'Scare The Coyote',
+            'uploader_id': '683e0a3269688656a5a49a44',
+            'uploader_url': 'https://www.sauceplus.com/channel/ScareTheCoyote/home',
+            'channel': 'Scare The Coyote',
+            'channel_id': '683e0a326968866ceba49a45',
+            'channel_url': 'https://www.sauceplus.com/channel/ScareTheCoyote/home/main',
+            'availability': 'subscriber_only',
+        },
+        'params': {'skip_download': 'm3u8'},
+    }]
+
+    def _real_initialize(self):
+        if not self._get_cookies(self._BASE_URL).get('__Host-sp-sess'):
+            self.raise_login_required()
+
+    def _real_extract(self, url):
+        return self._base_extract(url, self._BASE_URL, do_impersonate=True)
