yt-dlp/yt-dlp

commit 2ba5391cd68ed4f2415c827d2cecbcbc75ace10b

[ie/youtube] Fix premium formats extraction (#13586)
Fix ff6f94041aeee19c5559e1c1cd693960a1c1dd14

Closes #13545
Authored by: bashonly

yt_dlp/extractor/youtube/_video.py
@@ -2820,10 +2820,6 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
             context['signatureTimestamp'] = sts
         return {
             'playbackContext': {
-                'adPlaybackContext': {
-                    'pyv': True,
-                    'adType': 'AD_TYPE_INSTREAM',
-                },
                 'contentPlaybackContext': context,
             },
             **cls._get_checkok_params(),

yt_dlp/extractor/youtube/_base.py
@@ -63,6 +63,7 @@ INNERTUBE_CLIENTS = {
         'INNERTUBE_CONTEXT_CLIENT_NAME': 1,
         'PO_TOKEN_REQUIRED_CONTEXTS': [_PoTokenContext.GVS],
         'SUPPORTS_COOKIES': True,
+        'PLAYER_PARAMS': '8AEB',
     },
     'web_embedded': {
         'INNERTUBE_CONTEXT': {
@@ -174,6 +175,7 @@ INNERTUBE_CLIENTS = {
         },
         'INNERTUBE_CONTEXT_CLIENT_NAME': 7,
         'SUPPORTS_COOKIES': True,
+        'PLAYER_PARAMS': '8AEB',
     },
     'tv_simply': {
         'INNERTUBE_CONTEXT': {

commit 0e68332bcb9fba87c42805b7a051eeb2bed36206

[ie/youtube] Fix subtitles extraction (#13659)
Fixes regression introduced in 2ba5391cd68ed4f2415c827d2cecbcbc75ace10b

Closes #13654
Authored by: bashonly

yt_dlp/extractor/youtube/_video.py
@@ -3978,7 +3978,9 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         def process_language(container, base_url, lang_code, sub_name, client_name, query):
             lang_subs = container.setdefault(lang_code, [])
             for fmt in self._SUBTITLE_FORMATS:
-                query = {**query, 'fmt': fmt}
+                # xosf=1 results in undesirable text position data for vtt, json3 & srv* subtitles
+                # See: https://github.com/yt-dlp/yt-dlp/issues/13654
+                query = {**query, 'fmt': fmt, 'xosf': []}
                 lang_subs.append({
                     'ext': fmt,
                     'url': urljoin('https://www.youtube.com', update_url_query(base_url, query)),