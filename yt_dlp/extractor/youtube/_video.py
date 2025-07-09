aa9f1f4d577e99897ac16cd19d4e217d688ea75d
coletdjnz <coletdjnz@protonmail.com>
2025-07-09 18:29:54 +1200

    [ie/youtube] Log bad playability statuses of player responses (#13647)
    Authored by: coletdjnz

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -3273,6 +3273,10 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                 # web_creator may work around age-verification for all videos but requires PO token
                 append_client('tv_embedded', 'web_creator')
 
+            status = traverse_obj(pr, ('playabilityStatus', 'status', {str}))
+            if status not in ('OK', 'LIVE_STREAM_OFFLINE', 'AGE_CHECK_REQUIRED', 'AGE_VERIFICATION_REQUIRED'):
+                self.write_debug(f'{video_id}: {client} player response playability status: {status}')
+
         prs.extend(deprioritized_prs)
 
         if skipped_clients:
