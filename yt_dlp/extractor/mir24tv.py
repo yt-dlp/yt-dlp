966f155187029615ad32409b697a9db4a965bdc3
bashonly <88596187+bashonly@users.noreply.github.com>
2025-07-09 22:13:37 +0000

    Apply suggestions from code review

--- yt_dlp/extractor/mir24tv.py
+++ yt_dlp/extractor/mir24tv.py
@@ -3,7 +3,8 @@ from ..utils import parse_qs, url_or_none
 from ..utils.traversal import require, traverse_obj
 
 
-class Mir24tvIE(InfoExtractor):
+class Mir24TvIE(InfoExtractor):
+    IE_NAME = 'mir24.tv'
     _VALID_URL = r'https?://(?:www\.)?mir24\.tv/news/(?P<id>[0-9]+)/[^/?#]+'
     _TESTS = [{
         'url': 'https://mir24.tv/news/16635210/dni-kultury-rossii-otkrylis-v-uzbekistane.-na-prazdnichnom-koncerte-vystupili-zvezdy-rossijskoj-estrada',
@@ -11,7 +12,7 @@ class Mir24tvIE(InfoExtractor):
             'id': '16635210',
             'title': 'Дни культуры России открылись в Узбекистане. На праздничном концерте выступили звезды российской эстрады',
             'ext': 'mp4',
-            'thumbnail': 'https://images.mir24.tv/7ccONWCkXAOdwu64Wyk3JjGdrj67BTmszwf8860pWpU/q:95/w:865/h:500/c:1836:1080:nowe:84:0/rt:fill-down/czM6Ly9taXIyNC10di9tZWRpYS9BcnRpY2xlL2ltYWdlL2I0MGFhOThiLThlYjQtNDYxOC04MjA0LTE4NGI0NGNjY2NjOS9NSVIyNC0xMC4wMy4wN180N180M18wNC5TdGlsbDQ1My1jb3B5LnBuZw.jpg',
+            'thumbnail': r're:https://images\.mir24\.tv/.+\.jpg',
         },
     }]
 
@@ -25,10 +26,12 @@ class Mir24tvIE(InfoExtractor):
 
         m3u8_url = traverse_obj(iframe_url, (
             {parse_qs}, 'source', -1, {self._proto_relative_url}, {url_or_none}, {require('m3u8 URL')}))
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, video_id, 'mp4', m3u8_id='hls')
 
         return {
             'id': video_id,
             'title': self._og_search_title(webpage, default=None) or self._html_extract_title(webpage),
             'thumbnail': self._og_search_thumbnail(webpage, default=None),
-            'formats': self._extract_m3u8_formats(m3u8_url, video_id, 'mp4'),
+            'formats': formats,
+            'subtitles': subtitles,
         }
