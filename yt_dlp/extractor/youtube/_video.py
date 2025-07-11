dcbacbe46c6dcc177e592e6cd34e92c5f77a6a11
coletdjnz <coletdjnz@protonmail.com>
2025-07-12 11:18:17 +1200

    [ie/youtube] Require PO Token for iOS HLS formats

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -256,7 +256,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         '401': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'vcodec': 'av01.0.12M.08'},
     }
     _SUBTITLE_FORMATS = ('json3', 'srv1', 'srv2', 'srv3', 'ttml', 'srt', 'vtt')
-    _DEFAULT_CLIENTS = ('tv', 'ios', 'web')
+    _DEFAULT_CLIENTS = ('tv', 'web')
     _DEFAULT_AUTHED_CLIENTS = ('tv', 'web')
     # Premium does not require POT (except for subtitles)
     _DEFAULT_PREMIUM_CLIENTS = ('tv', 'web')
