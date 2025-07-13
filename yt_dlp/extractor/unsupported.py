d572538c04c67a6358826f21538561f35870ab64
doe1080 <98906116+doe1080@users.noreply.github.com>
2025-07-13 23:43:14 +0200

    [ie/bandaichannel] Remove extractor (#13152)
    Closes #8829
    Authored by: doe1080

--- yt_dlp/extractor/unsupported.py
+++ yt_dlp/extractor/unsupported.py
@@ -53,6 +53,7 @@ class KnownDRMIE(UnsupportedInfoExtractor):
         r'(?:beta\.)?crunchyroll\.com',
         r'viki\.com',
         r'deezer\.com',
+        r'b-ch\.com',
     )
 
     _TESTS = [{
@@ -167,6 +168,9 @@ class KnownDRMIE(UnsupportedInfoExtractor):
         'only_matching': True,
     }, {
         'url': 'http://www.deezer.com/playlist/176747451',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.b-ch.com/titles/8203/001',
         'only_matching': True,
     }]
 
