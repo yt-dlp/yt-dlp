630f3389c33f0f7f6ec97e8917d20aeb4e4078da
Frank Cai <70647872+averageFOSSenjoyer@users.noreply.github.com>
2025-07-13 16:16:01 -0500

    [ie/UnitedNationsWebTv] Add extractor (#13538)
    Closes #2675
    Authored by: averageFOSSenjoyer

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2290,6 +2290,7 @@ from .uliza import (
 )
 from .umg import UMGDeIE
 from .unistra import UnistraIE
+from .unitednations import UnitedNationsWebTvIE
 from .unity import UnityIE
 from .unsupported import (
     KnownDRMIE,
