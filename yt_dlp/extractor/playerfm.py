c4f89ae4931e82eb6b189c01b0012136063e095f
sepro <sepro@sepr0.com>
2025-07-19 01:18:07 +0200

    Fix extractor

--- yt_dlp/extractor/playerfm.py
+++ yt_dlp/extractor/playerfm.py
@@ -1,6 +1,6 @@
 
 from .common import InfoExtractor
-from ..utils import clean_html, int_or_none, url_or_none
+from ..utils import clean_html, clean_podcast_url, int_or_none, str_or_none, url_or_none
 from ..utils.traversal import traverse_obj
 
 
@@ -9,52 +9,64 @@ class PlayerFmIE(InfoExtractor):
     _TESTS = [{
         'url': 'https://player.fm/series/chapo-trap-house/movie-mindset-33-casino-feat-felix',
         'info_dict': {
-            'id': 'movie-mindset-33-casino-feat-felix',
+            'ext': 'mp3',
+            'id': '478606546',
+            'display_id': 'movie-mindset-33-casino-feat-felix',
             'thumbnail': r're:^https://.*\.(jpg|png)',
             'title': 'Movie Mindset 33 - Casino feat. Felix',
             'creators': ['Chapo Trap House'],
             'description': r're:The first episode of this season of Movie Mindset is free .+ we feel about it\.',
             'duration': 6830,
-            'ext': 'mp3',
+            'timestamp': 1745406000,
+            'upload_date': '20250423',
         },
     }, {
-        'url': 'https://player.fm/series/nbc-nightly-news-with-lester-holt/thursday-april-24-2025',
+        'url': 'https://player.fm/series/nbc-nightly-news-with-tom-llamas/thursday-april-17-2025',
         'info_dict': {
-            'id': 'thursday-april-24-2025',
-            'thumbnail': r're:^https://.*\.(jpg|png)',
-            'title': 'Thursday, April 24, 2025',
-            'creators': ['Lester Holt, NBC News'],
-            'description': '<p>Trump urges Putin to \'STOP\' after deadly Russian strike on Ukraine; More than 90,000 people have paid their respects to Pope Francis; Miami shooting leaves three injured; and more on tonight’s broadcast.</p>',
-            'duration': 1250,
             'ext': 'mp3',
+            'id': '477635490',
+            'display_id': 'thursday-april-17-2025',
+            'title': 'Thursday, April 17, 2025',
+            'thumbnail': r're:^https://.*\.(jpg|png)',
+            'duration': 1143,
+            'description': 'md5:4890b8cf9a55a787561cd5d59dfcda82',
+            'creators': ['NBC News'],
+            'timestamp': 1744941374,
+            'upload_date': '20250418',
         },
     }, {
-        'url': 'https://player.fm/series/soccer-101/ep-109-its-kicking-off-how-have-the-rules-for-kickoff-changed-what-are-the-best-approaches-to-getting-the-game-underway-and-how-could-we-improve-on-the-present-system',
+        'url': 'https://player.fm/series/soccer-101/ep-109-its-kicking-off-how-have-the-rules-for-kickoff-changed-what-are-the-best-approaches-to-getting-the-game-underway-and-how-could-we-improve-on-the-present-system-ack3NzL3yibvs4pf',
         'info_dict': {
-            'id': 'ep-109-its-kicking-off-how-have-the-rules-for-kickoff-changed-what-are-the-best-approaches-to-getting-the-game-underway-and-how-could-we-improve-on-the-present-system',
+            'ext': 'mp3',
+            'id': '481418710',
             'thumbnail': r're:^https://.*\.(jpg|png)',
-            'title': '#109 It\'s kicking off! How have the rules for kickoff changed, what are the best approaches to getting the game underway, and how could we improve on the present system?',
+            'title': r're:#109 It\'s kicking off! How have the rules for kickoff changed, .+ the present system\?',
             'creators': ['TSS'],
-            'description': '<p>Ryan is joined by Joe and Taylor to answer the age old question (in your best Jerry Seinfeld impression), "What\'s the deal with kickoff?!" How does a game get underway, how have the rules regarding kickoff evolved since the mid-1800s, what are the different approaches to getting your tactics right from kickoff, how could we improve upon the current system, and much much more!</p><p>The Soccer 101 theme, and plenty of other excellent music, can be found right here: <a href="https://aerialist.bandcamp.com/">https://aerialist.bandcamp.com</a>.</p>',
-            'duration': 1765,
-            'ext': 'mp3',
+            'duration': 1510,
+            'display_id': 'md5:b52ecacaefab891b59db69721bfd9b13',
+            'description': 'md5:52a39e36d08d8919527454f152ad3c25',
+            'timestamp': 1659102055,
+            'upload_date': '20220729',
         },
     }]
 
     def _real_extract(self, url):
         # podcast url is always after last backlash
-        video_id, url = self._match_valid_url(url).group('id', 'url')
-        data = self._download_json(url + '.json', video_id)
+        display_id, url = self._match_valid_url(url).group('id', 'url')
+        data = self._download_json(url + '.json', display_id)
 
         return {
-            'id': video_id,
-            'url': 'https://' + self._search_regex(r'redirect\.mp3/(.+)', data['url'], 'video url'),
+            'display_id': display_id,
             'vcodec': 'none',
             **traverse_obj(data, {
+                'id': ('id', {int}, {str_or_none}),
+                'url': ('url', {clean_podcast_url}),
                 'title': ('title', {str}),
                 'description': ('description', {clean_html}),
                 'duration': ('duration', {int_or_none}),
                 'thumbnail': (('image', ('series', 'image')), 'url', {url_or_none}, any),
+                'filesize': ('size', {int_or_none}),
+                'timestamp': ('publishedAt', {int_or_none}),
                 'creators': ('series', 'author', {str}, filter, all, filter),
             }),
         }
