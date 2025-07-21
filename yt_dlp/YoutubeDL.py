2ac3eb98373d1c31341c5e918c83872c7ff409c6
bashonly <88596187+bashonly@users.noreply.github.com>
2025-07-21 13:41:00 -0500

    Fix `ImpersonateTarget` sanitization (#13791)
    Fix 32809eb2da92c649e540a5b714f6235036026161
    
    Authored by: bashonly

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -3716,6 +3716,8 @@ class YoutubeDL:
                 return {k: filter_fn(v) for k, v in obj.items() if not reject(k, v)}
             elif isinstance(obj, (list, tuple, set, LazyList)):
                 return list(map(filter_fn, obj))
+            elif isinstance(obj, ImpersonateTarget):
+                return str(obj)
             elif obj is None or isinstance(obj, (str, int, float, bool)):
                 return obj
             else:
