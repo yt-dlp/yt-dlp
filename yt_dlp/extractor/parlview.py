41b39e6b24bbbf0af2e194631751d6908da30bce
Barry van Oudtshoorn <bvanoudtshoorn@gmail.com>
2025-07-24 08:57:45 +0800

    [ie/parlview] Implement PR feedback
    Co-authored-by: seproDev <seproDev@users.noreply.github.com>

--- yt_dlp/extractor/parlview.py
+++ yt_dlp/extractor/parlview.py
@@ -1,20 +1,17 @@
-from yt_dlp.utils.traversal import (
-    traverse_obj,
-)
+import re
+
+from yt_dlp.utils._utils import parse_iso8601
 
 from .common import InfoExtractor
 from ..utils import (
-    ExtractorError,
-    join_nonempty,
     parse_duration,
-    unified_timestamp,
     url_or_none,
 )
+from ..utils.traversal import traverse_obj
 
 
 class ParlviewIE(InfoExtractor):
-    _WORKING = True
-    _VALID_URL = r'https?://(?:www\.)?aph\.gov\.au/(?:.*/)?video/(?P<id>\d{5,7})'
+    _VALID_URL = r'https?://(?:www\.)?aph\.gov\.au/(?:.*/)?video/(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.aph.gov.au/News_and_Events/Watch_Read_Listen/ParlView/video/3406614',
         'info_dict': {
@@ -23,39 +20,50 @@ class ParlviewIE(InfoExtractor):
             'title': 'Senate Chamber',
             'description': 'Official Recording of Senate Proceedings from the Australian Parliament',
             'thumbnail': 'https://aphbroadcasting-prod.z01.azurefd.net/vod-storage/vod-logos/SenateParlview06.jpg',
-            'upload_date': '20250326',
-            'uploader': 'Australian Parliament House',
+            'upload_date': '20250325',
+            'duration': 17999,
+            'timestamp': 1742939400,
         },
         'params': {
             'skip_download': True,
         },
     }, {
-        'url': 'https://www.aph.gov.au/News_and_Events/Watch_Read_Listen/ParlView/video/3406614',
-        'only_matching': True,
+        'url': 'https://www.aph.gov.au/News_and_Events/Watch_Read_Listen/ParlView/video/SV1394.dv',
+        'info_dict': {
+            'id': 'SV1394.dv',
+            'ext': 'mp4',
+            'title': 'Senate Select Committee on Uranium Mining and Milling [Part 1]',
+            'description': 'Official Recording of Senate Committee Proceedings from the Australian Parliament',
+            'thumbnail': 'https://aphbroadcasting-prod.z01.azurefd.net/vod-storage/vod-logos/CommitteeThumbnail06.jpg',
+            'upload_date': '19960822',
+            'duration': 14765,
+            'timestamp': 840754200,
+        },
+        'params': {
+            'skip_download': True,
+        },
     }]
-    _API_URL = 'https://vodapi.aph.gov.au/api/search/parlview/%s'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        api_data = self._download_json(self._API_URL % video_id, video_id)
+        api_data = self._download_json(f'https://vodapi.aph.gov.au/api/search/parlview/{video_id}', video_id)
 
         video_details = traverse_obj(api_data, ('videoDetails', {dict}))
-        if not video_details:
-            raise ExtractorError('API request was not successful', expected=traverse_obj(api_data, ('wasSuccessful', {bool})) is False)
 
-        m3u8_url = traverse_obj(video_details, ('files', 'file', 'url', {str}))
-        formats = self._extract_m3u8_formats(m3u8_url, video_id, 'mp4', 'm3u8_native')
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(
+            video_details['files']['file']['url'], video_id, 'mp4')
+
+        DURATION_RE = re.compile(r'(?P<duration>\d+:\d+:\d+):\d+')
 
         return {
             'id': video_id,
             'formats': formats,
-            'uploader': 'Australian Parliament House',
-            ** traverse_obj(video_details, {
+            'subtitles': subtitles,
+            **traverse_obj(video_details, {
                 'title': (('parlViewTitle', 'title'), {str}, any),
                 'description': ('parlViewDescription', {str}),
-                'duration': ('files', 'file', 'duration', {lambda s: ':'.join((s or '').split(':')[:3])}, {parse_duration}),
-                'timestamp': ('recordingFrom', {unified_timestamp}),
+                'duration': ('files', 'file', 'duration', {DURATION_RE.fullmatch}, 'duration', {parse_duration}),
+                'timestamp': ('recordingFrom', {parse_iso8601}),
                 'thumbnail': ('thumbUrl', {url_or_none}),
-                'series': (None, {lambda vd: join_nonempty('eventGroup', 'eventSubGroup', delim=' - ', from_dict=vd)}),
             }),
         }
