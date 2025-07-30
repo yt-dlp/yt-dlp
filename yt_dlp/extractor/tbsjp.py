4f5b9e8d81ee101b56b0dfdf22621c2328819760
garret1317 <garret@airmail.cc>
2025-07-30 17:23:33 +0100

    [ie/TBSJP] pass Origin header in Streaks API request

--- yt_dlp/extractor/tbsjp.py
+++ yt_dlp/extractor/tbsjp.py
@@ -55,7 +55,7 @@ class TBSJPEpisodeIE(TBSJPBaseIE):
 
         return {
             **self._extract_from_streaks_api(
-                'tbs', f'ref:{video_id}', headers={'Referer': 'https://cu.tbs.co.jp/'}),
+                'tbs', f'ref:{video_id}', headers={'Origin': 'https://cu.tbs.co.jp/'}),
             **traverse_obj(episode, {
                 'title': ('title', ..., 'value', {str}, any),
                 'cast': ('credit', ..., 'name', ..., 'value', {clean_html}, any, {lambda x: x.split(',')}, ..., {str.strip}, filter, all, filter),
