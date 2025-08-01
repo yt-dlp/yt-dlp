fb26d5134e26803b9e3c0640ab0f328d549bb363
TheQWERTYCodr <93845040+TheQWERTYCodr@users.noreply.github.com>
2025-08-01 07:40:35 -0400

    update regex to follow coding guidelines
    make unreferenced group non-capturing

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -4200,7 +4200,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                     (?P<album>[^\n]+)\n+
                     (?:℗\s*(?P<release_year>\d{4})[^\n]+\n+)?
                     (?:Released\ on\s*:\s*(?P<release_date>\d{4}-\d{2}-\d{2}))?
-                    (.+?\nArtist\s*:\s*(?P<clean_artist>[^\n]+)\n)?
+                    (?:.+?\nArtist\s*:\s*(?P<clean_artist>[^\n]+)\n)?
                     .+Auto-generated\ by\ YouTube\.\s*$
                 ''', video_description)
             if mobj:
