a9df16db4720d4b7b4514cb3e2ea6e22bd9dac94
garret1317 <garret@airmail.cc>
2025-08-11 08:20:24 +0100

    traverse_obj: allow traversal of dataclasses (for protobug)

--- yt_dlp/utils/traversal.py
+++ yt_dlp/utils/traversal.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import collections
 import collections.abc
 import contextlib
+import dataclasses
 import functools
 import http.cookies
 import inspect
@@ -233,6 +234,8 @@ def traverse_obj(
                 result = list(map(apply_specials, obj.iterfind(xpath)))
             else:
                 result = apply_specials(obj)
+        elif dataclasses.is_dataclass(obj):
+            result = getattr(obj, key)
 
         return branching, result if branching else (result,)
 
