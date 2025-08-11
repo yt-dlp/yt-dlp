60fc70676f56d8ba99867cdd11f67bf868318f3f
bashonly <bashonly@protonmail.com>
2025-08-11 14:48:07 -0500

    [build] Add `linux_dir` builds
    Authored by: bashonly

--- bundle/docker/linux/entrypoint.sh
+++ bundle/docker/linux/entrypoint.sh
@@ -6,6 +6,10 @@ source ~/yt-dlp-build-venv/bin/activate
 python3 -m devscripts.install_deps -o --include build
 python3 -m devscripts.install_deps --include secretstorage --include curl-cffi --include pyinstaller
 python3 -m devscripts.make_lazy_extractors
-python3 devscripts/update-version.py -c "${channel}" -r "${origin}" "${version}"
+python3 devscripts/update-version.py -c "${CHANNEL}" -r "${ORIGIN}" "${VERSION}"
+python3 -m bundle.pyinstaller --onedir
+pushd "./dist/${EXE_NAME}"
+zip -r "/build/${EXE_NAME}.zip" .
+popd
 python3 -m bundle.pyinstaller
-mv dist/* /build/
+mv "./dist/${EXE_NAME}" /build/
