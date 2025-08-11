5e4ceb35cf997af0dbf100e1de37f4e2bcbaa0b7
sepro <sepro@sepr0.com>
2025-08-11 05:18:28 +0200

    [cleanup] Misc (#13852)
    Closes #13815
    Authored by: seproDev, injust, bashonly
    
    Co-authored-by: Justin Su <injustsu@gmail.com>
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/update.py
+++ yt_dlp/update.py
@@ -154,10 +154,14 @@ def _get_system_deprecation():
             'issues/13856', STOP_MSG)
 
     # Temporary until linux_armv7l executable builds are discontinued
-    if variant in ('linux_armv7l_exe'):
+    if variant == 'linux_armv7l_exe':
         return EXE_MSG_TMPL.format(
             f'{variant} (the PyInstaller-bundled executable for the Linux armv7l platform)',
             'issues/13976', STOP_MSG)
+
+    # Temporary until linux_aarch64_exe is built with Python >=3.10 instead of Python 3.9
+    if variant == 'linux_aarch64_exe':
+        return None
 
     if sys.version_info > MIN_RECOMMENDED:
         return None
