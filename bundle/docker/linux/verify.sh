842d829e641832a617922168de2e9ff2aa870ac7
bashonly <bashonly@protonmail.com>
2025-08-15 01:08:59 -0500

    fix verify script for onedir
    Authored by: bashonly

--- bundle/docker/linux/verify.sh
+++ bundle/docker/linux/verify.sh
@@ -3,7 +3,25 @@ set -eu
 
 if [ -n "${TEST_ONEDIR_BUILD:-}" ]; then
     echo "Extracting zip to verify onedir build"
-    "${PYTHON:-python}" -m zipfile -e "/build/${EXE_NAME}.zip" ./
+    if command -v python3 >/dev/null 2>&1; then
+        python3 -m zipfile -e "/build/${EXE_NAME}.zip" ./
+    else
+        echo "Attempting to install unzip"
+        if command -v dnf >/dev/null 2>&1; then
+            dnf -y install --allowerasing unzip
+        elif command -v yum >/dev/null 2>&1; then
+            yum -y install unzip
+        elif command -v apt-get >/dev/null 2>&1; then
+            DEBIAN_FRONTEND=noninteractive apt-get update -qq
+            DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends unzip
+        elif command -v apk >/dev/null 2>&1; then
+            apk add --no-cache unzip
+        else
+            echo "Unsupported image"
+            exit 1
+        fi
+        unzip "/build/${EXE_NAME}.zip" -d ./
+    fi
 else
     echo "Verifying onefile build"
     cp "/build/${EXENAME}" ./
