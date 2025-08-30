65faed21595231cbf91ab5dfc458cab44a1ad0ac
Mr Flamel <148793343+mrFlamel@users.noreply.github.com>
2025-08-30 08:52:36 +0300

    Apply suggestions from code review
    Co-authored-by: N/Ame <173015200+grqz@users.noreply.github.com>

--- yt_dlp/extractor/watchthechosen.py
+++ yt_dlp/extractor/watchthechosen.py
@@ -2,6 +2,7 @@ import datetime
 import json
 
 from .common import InfoExtractor
+from ..utils.traversal import traverse_obj
 
 
 class WatchTheChosenIE(InfoExtractor):
@@ -14,8 +15,7 @@ class WatchTheChosenIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Season 3 Episode 2: Two by Two',
         },
-    },
-        {
+    }, {
         'url': 'https://watch.thechosen.tv/video/184683596189',
         'md5': '52998d02633f7c0e2edc195c6f676a3d',
         'info_dict': {
@@ -23,18 +23,28 @@ class WatchTheChosenIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Season 4 Episode 8: Humble',
         },
-    },
-    ]
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
         # DOWNLOADING METADATA
-        post_data = {'operationName': 'Video', 'variables': {'channelID': '12884901895', 'videoID': video_id},
-                     'query': 'query Video($channelID: ID!, $videoID: ID!) { video(ChannelID: $channelID, VideoID: $videoID) { ...VideoFragment __typename } } fragment VideoFragment on Video { title description updatedAt thumbnail createdAt duration likeCount comments }'}
-        encoded_data = json.dumps(post_data).encode('utf-8')  # Encode post data to bytes (so digestible for _download_json)
-        headers = {'channelid': '12884901895', 'content-type': 'application/json'}  # POST-request headers
-        metadata = self._download_json('https://api.frontrow.cc/query', video_id, data=encoded_data, headers=headers)['data']['video']
+        metadata = traverse_obj(self._download_json(
+            'https://api.frontrow.cc/query', video_id, data=json.dumps({
+                'operationName': 'Video',
+                'variables': {'channelID': '12884901895', 'videoID': video_id},
+                'query': r'''query Video($channelID: ID!, $videoID: ID!) {
+                    video(ChannelID: $channelID, VideoID: $videoID) {
+                        ...VideoFragment __typename
+                    }
+                }
+                fragment VideoFragment on Video {
+                    title description updatedAt thumbnail createdAt duration likeCount comments
+                }''',
+            }).encode(), headers={
+                'channelid': '12884901895',
+                'content-type': 'application/json'
+            }), ('data', 'video'))
 
         # Helper for formatting dates in ISO 8601
         def formatDate(date):
@@ -47,14 +57,16 @@ class WatchTheChosenIE(InfoExtractor):
 
         return {
             'id': video_id,
-            'title': metadata['title'],
-            'description': metadata['description'],
-            'thumbnail': metadata['thumbnail'],
-            'modified_date': formatDate(metadata['updatedAt']),
-            'upload_date': formatDate(metadata['createdAt']),
-            'duration': metadata['duration'],
-            'like_count': metadata['likeCount'],
-            'comment_count': metadata['comments'],
             'formats': formats,
             'subtitles': subtitles,
+            **traverse_obj(metadata, {
+                'title': 'title',
+                'description': 'description',
+                'thumbnail': 'thumbnail',
+                'modified_date': ('updatedAt', {formatDate}),
+                'upload_date': ('createdAt', {formatDate}),
+                'duration': 'duration',
+                'like_count': 'likeCount',
+                'comment_count': 'comments',
+            }),
         }
