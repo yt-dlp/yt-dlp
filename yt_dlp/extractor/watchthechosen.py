b2cbe159f98f839a8a890ea4cf999edbe3429879
MrFlamel <jakob.luhamets@gmail.com>
2025-08-30 09:52:52 +0300

    Added views count and fixed tests

--- yt_dlp/extractor/watchthechosen.py
+++ yt_dlp/extractor/watchthechosen.py
@@ -9,19 +9,35 @@ class WatchTheChosenIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?watch\.thechosen\.tv/video/(?P<id>[0-9]+)'
     _TESTS = [{
         'url': 'https://watch.thechosen.tv/video/184683594325',
-        'md5': '03405a126e90f5e48d449005de7d585e',
+        'md5': 'a7030265262d659b9692a3418b189833',
         'info_dict': {
             'id': '184683594325',
             'ext': 'mp4',
             'title': 'Season 3 Episode 2: Two by Two',
+            'description': 'Jesus is a problem for the Romans. With pilgrims from everywhere setting up a tent city at the gates of Capernaum to see Jesus, tensions rise. Jesus officially sends the twelve apostles on an exciting but dangerous mission, causing Little James to ask why he hasn’t been healed.',
+            'thumbnail': 'https://fastly.frontrowcdn.com/channels/12884901895/VIDEO_THUMBNAIL/184683594325/3b180a4a-797e-403d-8929-ffcd3f29db4d.VIDEO_THUMBNAIL',
+            'modified_date': str,
+            'upload_date': '20231102',
+            'duration': 4212,
+            'like_count': int,
+            'comment_count': int,
+            'view_count': int,
         },
     }, {
         'url': 'https://watch.thechosen.tv/video/184683596189',
-        'md5': '52998d02633f7c0e2edc195c6f676a3d',
+        'md5': '3bbfd4f0718b076481c5b823ec7a6760',
         'info_dict': {
             'id': '184683596189',
             'ext': 'mp4',
             'title': 'Season 4 Episode 8: Humble',
+            'description': 'Jesus refuses to look and act like the Messiah everyone expects—arriving like a lamb instead of a lion and riding a donkey instead of a war horse.',
+            'thumbnail': 'https://fastly.frontrowcdn.com/channels/12884901895/VIDEO_THUMBNAIL/184683596189/a6c070e0-0f8e-4813-a264-85b9944c98ea.VIDEO_THUMBNAIL',
+            'modified_date': str,
+            'upload_date': '20240506',
+            'duration': 5092,
+            'like_count': int,
+            'comment_count': int,
+            'view_count': int,
         },
     }]
 
@@ -39,11 +55,11 @@ class WatchTheChosenIE(InfoExtractor):
                     }
                 }
                 fragment VideoFragment on Video {
-                    title description updatedAt thumbnail createdAt duration likeCount comments
+                    title description updatedAt thumbnail createdAt duration likeCount comments views
                 }''',
             }).encode(), headers={
                 'channelid': '12884901895',
-                'content-type': 'application/json'
+                'content-type': 'application/json',
             }), ('data', 'video'))
 
         # Helper for formatting dates in ISO 8601
@@ -68,5 +84,6 @@ class WatchTheChosenIE(InfoExtractor):
                 'duration': 'duration',
                 'like_count': 'likeCount',
                 'comment_count': 'comments',
+                'view_count': 'views',
             }),
         }
