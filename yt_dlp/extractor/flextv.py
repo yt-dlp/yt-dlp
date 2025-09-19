5c985185ce2d793e2c66eb657d8a974e1d28c41f
sepro <sepro@sepr0.com>
2025-09-20 00:20:45 +0200

    Address review comments

--- yt_dlp/extractor/flextv.py
+++ yt_dlp/extractor/flextv.py
@@ -51,7 +51,7 @@ class FlexTVIE(InfoExtractor):
         for stream in traverse_obj(stream_data, ('sources', ..., {dict})):
             if stream.get('format') == 'ivs' and url_or_none(stream.get('url')):
                 formats.extend(self._extract_m3u8_formats(
-                    stream['url'], channel_id, 'mp4', live=True))
+                    stream['url'], channel_id, 'mp4', live=True, fatal=False, m3u8_id='ivs'))
             for format_type in ['hls', 'flv']:
                 for data in traverse_obj(stream, (
                         'urlDetail', format_type, 'resolution', lambda _, v: url_or_none(v['url']))):
@@ -61,7 +61,6 @@ class FlexTVIE(InfoExtractor):
                         'height': int_or_none(data.get('resolution')),
                         'ext': 'mp4' if format_type == 'hls' else 'flv',
                         'protocol': 'm3u8_native' if format_type == 'hls' else 'http',
-                        'live': True,
                     })
 
         return {
