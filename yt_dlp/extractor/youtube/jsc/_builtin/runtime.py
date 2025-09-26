29e0b8a93e38f29a37d72d990a7693e0beb82824
coletdjnz <coletdjnz@protonmail.com>
2025-09-27 08:58:53 +1200

    disable pre-processed player cache

--- yt_dlp/extractor/youtube/jsc/_builtin/runtime.py
+++ yt_dlp/extractor/youtube/jsc/_builtin/runtime.py
@@ -101,6 +101,9 @@ class JsRuntimeChalBaseJCP(JsChallengeProvider):
         ScriptType.CORE: 'core.min.js',
     }
 
+    # currently disabled as files are large and we do not support rotation
+    _ENABLE_PREPROCESSED_PLAYER_CACHE = False
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._available = True
@@ -125,7 +128,10 @@ class JsRuntimeChalBaseJCP(JsChallengeProvider):
             grouped[request.input.player_url].append(request)
 
         for player_url, grouped_requests in grouped.items():
-            player = self.ie.cache.load(self._CACHE_SECTION, f'player:{player_url}')
+            player = None
+            if self._ENABLE_PREPROCESSED_PLAYER_CACHE:
+                player = self.ie.cache.load(self._CACHE_SECTION, f'player:{player_url}')
+
             if player:
                 cached = True
             else:
@@ -139,7 +145,7 @@ class JsRuntimeChalBaseJCP(JsChallengeProvider):
             if output['type'] == 'error':
                 raise JsChallengeProviderError(output['error'])
 
-            if preprocessed := output.get('preprocessed_player'):
+            if self._ENABLE_PREPROCESSED_PLAYER_CACHE and (preprocessed := output.get('preprocessed_player')):
                 self.ie.cache.store(self._CACHE_SECTION, f'player:{player_url}', preprocessed)
 
             for request, response_data in zip(grouped_requests, output['responses']):
@@ -237,7 +243,6 @@ class JsRuntimeChalBaseJCP(JsChallengeProvider):
         code = load_script(
             self._SCRIPT_FILENAMES[script_type], error_hook=error_hook)
         if code:
-            # TODO: strip internal header comments as to match published version
             return Script(script_type, ScriptVariant.UNMINIFIED, ScriptSource.BUILTIN, self._SUPPORTED_VERSION, code)
         return None
 
