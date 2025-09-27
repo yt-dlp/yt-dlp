3b5588acd48c731e62154ba2a1d5548fdd8ddf71
coletdjnz <coletdjnz@protonmail.com>
2025-09-27 14:31:34 +1200

    Fix downloading npm packages error + bypass --download-ext npm if already cached

--- yt_dlp/extractor/youtube/jsc/_builtin/deno.py
+++ yt_dlp/extractor/youtube/jsc/_builtin/deno.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import contextlib
+import re
 import shlex
 import subprocess
 
@@ -21,7 +21,7 @@ from yt_dlp.extractor.youtube.jsc.provider import (
 )
 from yt_dlp.extractor.youtube.pot._provider import BuiltinIEContentProvider
 from yt_dlp.extractor.youtube.pot.provider import provider_bug_report_message
-from yt_dlp.utils import Popen
+from yt_dlp.utils import Popen, remove_terminal_sequences
 
 
 @register_provider
@@ -29,8 +29,9 @@ class DenoJCP(JsRuntimeChalBaseJCP, BuiltinIEContentProvider):
     PROVIDER_NAME = 'deno'
     JS_RUNTIME_NAME = 'deno'
 
-    _DENO_OPTIONS = ['--no-prompt', '--no-remote']
+    _DENO_BASE_OPTIONS = ['--no-prompt', '--no-remote', '--no-lock', '--node-modules-dir=none', '--no-config']
     DENO_NPM_LIB_FILENAME = 'yt.solver.deno.lib.js'
+    _NPM_PACKAGES_CACHED = False
 
     def _iter_script_sources(self):
         for source, func in super()._iter_script_sources():
@@ -41,23 +42,42 @@ class DenoJCP(JsRuntimeChalBaseJCP, BuiltinIEContentProvider):
     def _deno_npm_source(self, script_type: ScriptType, /) -> Script | None:
         if script_type != ScriptType.LIB:
             return None
-        if 'npm' not in self.ie.get_param('download_ext_components', []):
-            self._report_ext_component_skipped('npm', 'NPM package')
-            return None
         # Deno-specific lib scripts that uses Deno NPM imports
         error_hook = lambda e: self.logger.warning(
             f'Failed to read deno challenge solver lib script: {e}{provider_bug_report_message(self)}')
         code = load_script(
             self.DENO_NPM_LIB_FILENAME, error_hook=error_hook)
-        if code:
-            # TODO: any other permissions we want when not using --no-remote?
-            with contextlib.suppress(ValueError):
-                self._DENO_OPTIONS.remove('--no-remote')
-            return Script(script_type, ScriptVariant.DENO_NPM, ScriptSource.BUILTIN, self._SCRIPT_VERSION, code)
-        return None
+        if not code:
+            return None
+        if 'npm' not in self.ie.get_param('download_ext_components', []):
+            # We may still be able to continue if the npm packages are available/cached
+            self._NPM_PACKAGES_CACHED = self._npm_packages_cached(code)
+            if not self._NPM_PACKAGES_CACHED:
+                self._report_ext_component_skipped('npm', 'NPM package')
+                return None
+        return Script(script_type, ScriptVariant.DENO_NPM, ScriptSource.BUILTIN, self._SCRIPT_VERSION, code)
+
+    def _npm_packages_cached(self, stdin: str) -> bool:
+        # Check if npm packages are cached, so we can run without --download-ext-components npm
+        self.logger.debug('Checking if npm packages are cached')
+        try:
+            self._run_deno(stdin, [*self._DENO_BASE_OPTIONS, '--cached-only'])
+        except JsChallengeProviderError as e:
+            self.logger.trace(f'Deno npm packages not cached: {e}')
+            return False
+        return True
 
     def _run_js_runtime(self, stdin: str, /) -> str:
-        cmd = [self.runtime_info.path, 'run', *self._DENO_OPTIONS, '-']
+        options = [*self._DENO_BASE_OPTIONS]
+        if self._lib_script.variant == ScriptVariant.DENO_NPM and self._NPM_PACKAGES_CACHED:
+            options.append('--cached-only')
+        elif self._lib_script.variant != ScriptVariant.DENO_NPM:
+            options.append('--no-npm')
+            options.append('--cached-only')
+        return self._run_deno(stdin, options)
+
+    def _run_deno(self, stdin, options) -> str:
+        cmd = [self.runtime_info.path, 'run', *options, '-']
         self.logger.debug(f'Running deno: {shlex.join(cmd)}')
         with Popen(
             cmd,
@@ -67,14 +87,19 @@ class DenoJCP(JsRuntimeChalBaseJCP, BuiltinIEContentProvider):
             stderr=subprocess.PIPE,
         ) as proc:
             stdout, stderr = proc.communicate_or_kill(stdin)
-            # TODO: fails when deno needs to download dependencies?
+            stderr = self._clean_stderr(stderr)
             if proc.returncode or stderr:
-                msg = 'Error running deno process'
+                msg = f'Error running deno process (returncode: {proc.returncode})'
                 if stderr:
                     msg = f'{msg}: {stderr}'
                 raise JsChallengeProviderError(msg)
-
         return stdout
+
+    def _clean_stderr(self, stderr):
+        return '\n'.join(
+            line for line in stderr.splitlines()
+            if not re.match(r'^Download\s+https[^\s]+$', remove_terminal_sequences(line))
+        )
 
 
 @register_preference(DenoJCP)
