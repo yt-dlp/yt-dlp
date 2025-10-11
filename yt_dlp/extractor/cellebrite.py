ffc87373c63a17786b3f47f9beac6898f309043b
Sean Ellingham <sean.ellingham@basecreative.co.uk>
2025-10-11 22:42:01 +0100

    Fix dependent extractor tests

--- yt_dlp/extractor/cellebrite.py
+++ yt_dlp/extractor/cellebrite.py
@@ -5,18 +5,6 @@ from ..utils import ExtractorError, make_archive_id, url_basename
 class CellebriteIE(VidyardBaseIE):
     _VALID_URL = r'https?://cellebrite\.com/(?:\w+)?/(?P<id>[\w-]+)'
     _TESTS = [{
-        'url': 'https://cellebrite.com/en/collect-data-from-android-devices-with-cellebrite-ufed/',
-        'info_dict': {
-            'id': 'ZqmUss3dQfEMGpauambPuH',
-            'display_id': '16025876',
-            'ext': 'mp4',
-            'title': 'Ask the Expert: Chat Capture - Collect Data from Android Devices in Cellebrite UFED',
-            'description': 'md5:dee48fe12bbae5c01fe6a053f7676da4',
-            'thumbnail': 'https://cellebrite.com/wp-content/uploads/2021/05/Chat-Capture-1024x559.png',
-            'duration': 455.979,
-            '_old_archive_ids': ['cellebrite 16025876'],
-        },
-    }, {
         'url': 'https://cellebrite.com/en/how-to-lawfully-collect-the-maximum-amount-of-data-from-android-devices/',
         'info_dict': {
             'id': 'QV1U8a2yzcxigw7VFnqKyg',
@@ -24,7 +12,7 @@ class CellebriteIE(VidyardBaseIE):
             'ext': 'mp4',
             'title': 'How to Lawfully Collect the Maximum Amount of Data From Android Devices',
             'description': 'md5:0e943a9ac14c374d5d74faed634d773c',
-            'thumbnail': 'https://cellebrite.com/wp-content/uploads/2022/07/How-to-Lawfully-Collect-the-Maximum-Amount-of-Data-From-Android-Devices.png',
+            'thumbnail': 'https://play.vidyard.com/QV1U8a2yzcxigw7VFnqKyg.jpg',
             'duration': 134.315,
             '_old_archive_ids': ['cellebrite 29018255'],
         },
