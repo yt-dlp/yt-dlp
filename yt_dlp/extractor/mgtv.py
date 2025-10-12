9d72a21c077cdc4301cb352d85209cd6e959a9b9
Henry Qin <root@hq6.me>
2025-10-11 17:09:12 -0700

    Change url to api_url.

--- yt_dlp/extractor/mgtv.py
+++ yt_dlp/extractor/mgtv.py
@@ -93,11 +93,11 @@ class MGTVIE(InfoExtractor):
                 raise ExtractorError(error['msg'], expected=True)
             raise
 
-        url = 'https://pcweb.api.mgtv.com/player/getSource'
+        api_url = 'https://pcweb.api.mgtv.com/player/getSource'
         if self._get_cookies('https://mgtv.com'):
-            url = 'https://tinker.glb.mgtv.com/player/getSource'
+            api_url = 'https://tinker.glb.mgtv.com/player/getSource'
         stream_data = self._download_json(
-            url, video_id, query={
+            api_url, video_id, query={
                 'tk2': tk2,
                 'pm2': api_data['atc']['pm2'],
                 'video_id': video_id,
