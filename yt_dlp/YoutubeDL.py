b4455b066c146353ccf4348762b5939d967ee705
bashonly <bashonly@protonmail.com>
2025-10-15 03:56:38 -0500

    `YoutubeDL.cookiejar`: remove compat comment
    Authored by: bashonly

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -4112,7 +4112,6 @@ class YoutubeDL:
                 self.params.get('cookiefile'), self.params.get('cookiesfrombrowser'), self)
         except CookieLoadError as error:
             cause = error.__context__
-            # compat: <=py3.9: `traceback.format_exception` has a different signature
             self.report_error(str(cause), tb=''.join(traceback.format_exception(None, cause, cause.__traceback__)))
             raise
 
