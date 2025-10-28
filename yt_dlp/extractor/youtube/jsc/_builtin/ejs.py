34595cae673addf210ed0b66b8aa8b36fecf093f
sepro <sepro@sepr0.com>
2025-10-28 19:33:09 +0100

    Clear outdated script cache

--- yt_dlp/extractor/youtube/jsc/_builtin/ejs.py
+++ yt_dlp/extractor/youtube/jsc/_builtin/ejs.py
@@ -214,12 +214,18 @@ class EJSBaseJCP(JsChallengeProvider):
                     self.logger.warning(
                         f'Challenge solver {script_type.value} script version {script.version} '
                         f'is not supported (source: {script.source.value}, variant: {script.variant}, supported version: {self._SCRIPT_VERSION})')
+                    if script.source is ScriptSource.CACHE:
+                        self.logger.debug('Clearing outdated cached script')
+                        self.ie.cache.store(self._CACHE_SECTION, script_type.value, None)
                     continue
                 script_hashes = self._ALLOWED_HASHES[script.type].get(script.variant, [])
                 if script_hashes and script.hash not in script_hashes:
                     self.logger.warning(
                         f'Hash mismatch on challenge solver {script.type.value} script '
                         f'(source: {script.source.value}, variant: {script.variant}, hash: {script.hash})!{provider_bug_report_message(self)}')
+                    if script.source is ScriptSource.CACHE:
+                         self.logger.debug('Clearing invalid cached script')
+                         self.ie.cache.store(self._CACHE_SECTION, script_type.value, None)
                     continue
             self.logger.debug(
                 f'Using challenge solver {script.type.value} script v{script.version} '
