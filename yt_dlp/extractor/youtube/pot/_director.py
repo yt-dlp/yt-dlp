44bc28f29658a66037288e825cfdcd57079cc99c
bashonly <bashonly@protonmail.com>
2025-10-29 17:25:02 -0500

    [ie/youtube] actually make `pot` settings behavior consistent with `_configuration_arg`
    Authored by: bashonly

--- yt_dlp/extractor/youtube/pot/_director.py
+++ yt_dlp/extractor/youtube/pot/_director.py
@@ -39,6 +39,7 @@ from yt_dlp.extractor.youtube.pot.provider import (
     provider_bug_report_message,
 )
 from yt_dlp.utils import bug_reports_message, format_field, join_nonempty
+from yt_dlp.utils.traversal import traverse_obj
 
 if typing.TYPE_CHECKING:
     from yt_dlp.extractor.youtube.pot.cache import CacheProviderPreference
@@ -367,10 +368,14 @@ def initialize_pot_director(ie):
 
     def get_provider_logger_and_settings(provider, logger_key):
         logger_prefix = f'{logger_key}:{provider.PROVIDER_NAME}'
-        extractor_key = f'{EXTRACTOR_ARG_PREFIX}-{provider.PROVIDER_KEY.lower()}'
-        return (
-            YoutubeIEContentProviderLogger(ie, logger_prefix, log_level=log_level),
-            {k.lower(): v for k, v in ie.get_param('extractor_args', {}).get(extractor_key, {}).items()})
+        settings = {
+            k.lower(): v
+            for k, v in traverse_obj(
+                ie.get_param('extractor_args'),
+                (f'{EXTRACTOR_ARG_PREFIX}-{provider.PROVIDER_KEY}', {dict.items}, ...),
+                casesense=False)
+        }
+        return YoutubeIEContentProviderLogger(ie, logger_prefix, log_level=log_level), settings
 
     cache_providers = []
     for cache_provider in _pot_cache_providers.value.values():
