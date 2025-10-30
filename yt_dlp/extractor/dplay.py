65bd6ff7c900edbc5d9d6be93f588b9febab698c
w4grfw <>
2025-10-30 12:28:00 +0100

    new key categories

--- yt_dlp/extractor/dplay.py
+++ yt_dlp/extractor/dplay.py
@@ -1075,6 +1075,7 @@ class DiscoveryNetworksDeIE(DiscoveryPlusBaseIE):
             'creators': ['DMAX'],
             'thumbnail': 'https://eu1-prod-images.disco-api.com/2023/05/09/f72fb510-7992-3b12-af7f-f16a2c22d1e3.jpeg',
             'tags': ['schatzsucher', 'schatz', 'nugget', 'bodenschätze', 'down under', 'australien', 'goldrausch'],
+            'categories': ['Gold', 'Schatzsucher'],
         },
         'params': {'skip_download': 'm3u8'},
     }, {
@@ -1123,27 +1124,7 @@ class DiscoveryNetworksDeIE(DiscoveryPlusBaseIE):
             'creators': ['DMAX'],
             'thumbnail': 'https://eu1-prod-images.disco-api.com/2025/10/14/0bdee68c-a8d8-33d9-9204-16eb61108552.jpeg',
             'tags': [],
-        },
-        'params': {'skip_download': 'm3u8'},
-    }, {
-        'url': 'https://tlc.de/sendungen/evil-gesichter-des-boesen/das-geheimnis-meines-bruders',
-        'info_dict': {
-            'id': '7792288',
-            'ext': 'mp4',
-            'title': 'Das Geheimnis meines Bruders',
-            'description': 'md5:3167550bb582eb9c92875c86a0a20882',
-            'display_id': '7792288',
-            'episode': 'Episode 1',
-            'episode_number': 1,
-            'season': 'Season 1',
-            'season_number': 1,
-            'series': 'Evil - Gesichter des Bösen',
-            'duration': 2626.0,
-            'upload_date': '20240926',
-            'timestamp': 1727388000,
-            'creators': ['TLC'],
-            'thumbnail': 'https://eu1-prod-images.disco-api.com/2024/11/29/e9f3e3ae-74ec-3631-81b7-fc7bbe844741.jpeg',
-            'tags': ['kriminalfall', 'böse', 'verbrecher', 'zusammenleben', 'mord', 'familie', 'ermittler', 'leben', 'fälle', 'opfer', 'tod', 'mörder', 'crime'],
+            'categories': ['DMAX Originals', 'Jobs', 'Blaulicht'],
         },
         'params': {'skip_download': 'm3u8'},
     }, {
@@ -1165,6 +1146,29 @@ class DiscoveryNetworksDeIE(DiscoveryPlusBaseIE):
             'creators': ['TLC'],
             'thumbnail': 'https://eu1-prod-images.disco-api.com/2023/04/05/59941d26-a81b-365f-829f-69d8cd81fd0f.jpeg',
             'tags': [],
+            'categories': ['Paranormal', 'Gruselig!'],
+        },
+        'params': {'skip_download': 'm3u8'},
+    }, {
+        'url': 'https://tlc.de/sendungen/evil-gesichter-des-boesen/das-geheimnis-meines-bruders',
+        'info_dict': {
+            'id': '7792288',
+            'ext': 'mp4',
+            'title': 'Das Geheimnis meines Bruders',
+            'description': 'md5:3167550bb582eb9c92875c86a0a20882',
+            'display_id': '7792288',
+            'episode': 'Episode 1',
+            'episode_number': 1,
+            'season': 'Season 1',
+            'season_number': 1,
+            'series': 'Evil - Gesichter des Bösen',
+            'duration': 2626.0,
+            'upload_date': '20240926',
+            'timestamp': 1727388000,
+            'creators': ['TLC'],
+            'thumbnail': 'https://eu1-prod-images.disco-api.com/2024/11/29/e9f3e3ae-74ec-3631-81b7-fc7bbe844741.jpeg',
+            'tags': ['kriminalfall', 'böse', 'verbrecher', 'zusammenleben', 'mord', 'familie', 'ermittler', 'leben', 'fälle', 'opfer', 'tod', 'mörder', 'crime'],
+            'categories': ['True Crime', 'Mord'],
         },
         'params': {'skip_download': 'm3u8'},
     }]
@@ -1182,9 +1186,12 @@ class DiscoveryNetworksDeIE(DiscoveryPlusBaseIE):
                 'filter[show.slug]': programme,
             }, fatal=False)
         video_id = traverse_obj(meta, ('uid', {str}, {lambda s: s[-7:]}))
+        categories = traverse_obj(meta, ('taxonomies', lambda _, v: v.get('category') == 'genre', 'title', {strip_or_none}))
 
-        return self._get_disco_api_info(
+        disco_api_info = self._get_disco_api_info(
             url, video_id, 'eu1-prod.disco-api.com', realm, country)
+        disco_api_info['categories'] = categories
+        return disco_api_info
 
     def _update_disco_api_headers(self, headers, disco_base, display_id, realm):
         headers.update({
