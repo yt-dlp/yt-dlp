b9f22c71fb3ce957029da591a808f54a76a731bc
sepro <sepro@sepr0.com>
2025-10-31 21:40:26 +0100

    minutia

--- yt_dlp/extractor/ard.py
+++ yt_dlp/extractor/ard.py
@@ -618,61 +618,57 @@ class ARDAudiothekBaseIE(InfoExtractor):
 
 
 class ARDAudiothekIE(ARDAudiothekBaseIE):
-    _VALID_URL = r'''https:?//(?:www\.)?ardaudiothek\.de/episode/(?P<id>urn:ard:(?:episode|section|extra):[a-f0-9]{16})'''
+    _VALID_URL = r'https:?//(?:www\.)?ardaudiothek\.de/episode/(?P<id>urn:ard:(?:episode|section|extra):[a-f0-9]{16})'
 
-    _TESTS = [
-        {
-            'url': 'https://www.ardaudiothek.de/episode/urn:ard:episode:eabead1add170e93/',
-            'info_dict': {
-                'id': 'urn:ard:episode:eabead1add170e93',
-                'ext': 'mp3',
-                'upload_date': '20240717',
-                'duration': 3339,
-                'title': 'CAIMAN CLUB (S04E04): Cash Out',
-                'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:ed64411a07a4b405',
-                'description': 'md5:0e5d127a3832ae59e8bab40a91a5dadc',
-                'display_id': 'urn:ard:episode:eabead1add170e93',
-                'timestamp': 1721181641,
-                'series': '1LIVE Caiman Club',
-                'channel': 'WDR',
-                'episode': 'Episode 4',
-                'episode_number': 4,
-            },
+    _TESTS = [{
+        'url': 'https://www.ardaudiothek.de/episode/urn:ard:episode:eabead1add170e93/',
+        'info_dict': {
+            'id': 'urn:ard:episode:eabead1add170e93',
+            'ext': 'mp3',
+            'upload_date': '20240717',
+            'duration': 3339,
+            'title': 'CAIMAN CLUB (S04E04): Cash Out',
+            'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:ed64411a07a4b405',
+            'description': 'md5:0e5d127a3832ae59e8bab40a91a5dadc',
+            'display_id': 'urn:ard:episode:eabead1add170e93',
+            'timestamp': 1721181641,
+            'series': '1LIVE Caiman Club',
+            'channel': 'WDR',
+            'episode': 'Episode 4',
+            'episode_number': 4,
         },
-        {
-            'url': 'https://www.ardaudiothek.de/episode/urn:ard:section:855c7a53dac72e0a/',
-            'info_dict': {
-                'id': 'urn:ard:section:855c7a53dac72e0a',
-                'ext': 'mp4',
-                'upload_date': '20241231',
-                'duration': 3304,
-                'title': 'Illegaler DDR-Detektiv: Doberschütz und die letzte Staatsjagd (1/2) - Wendezeit',
-                'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:b9b4f1e8b93da4dd',
-                'description': 'md5:3552d571e1959754cff66c1da6c0fdae',
-                'display_id': 'urn:ard:section:855c7a53dac72e0a',
-                'timestamp': 1735629900,
-                'series': 'Auf der Spur – Die ARD Ermittlerkrimis',
-                'channel': 'ARD',
-                'episode': 'Episode 1',
-                'episode_number': 1,
-            },
+    }, {
+        'url': 'https://www.ardaudiothek.de/episode/urn:ard:section:855c7a53dac72e0a/',
+        'info_dict': {
+            'id': 'urn:ard:section:855c7a53dac72e0a',
+            'ext': 'mp4',
+            'upload_date': '20241231',
+            'duration': 3304,
+            'title': 'Illegaler DDR-Detektiv: Doberschütz und die letzte Staatsjagd (1/2) - Wendezeit',
+            'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:b9b4f1e8b93da4dd',
+            'description': 'md5:3552d571e1959754cff66c1da6c0fdae',
+            'display_id': 'urn:ard:section:855c7a53dac72e0a',
+            'timestamp': 1735629900,
+            'series': 'Auf der Spur – Die ARD Ermittlerkrimis',
+            'channel': 'ARD',
+            'episode': 'Episode 1',
+            'episode_number': 1,
         },
-        {
-            'url': 'https://www.ardaudiothek.de/episode/urn:ard:extra:d2fe7303d2dcbf5d/',
-            'info_dict': {
-                'id': 'urn:ard:extra:d2fe7303d2dcbf5d',
-                'ext': 'mp3',
-                'title': 'Trailer: Fanta Vier Forever, Baby!?!',
-                'description': 'md5:b64a586f2e976b8bb5ea0a79dbd8751c',
-                'channel': 'SWR',
-                'duration': 62,
-                'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:48d3c255969be803',
-                'series': 'Fanta Vier Forever, Baby!?!',
-                'timestamp': 1732108217,
-                'upload_date': '20241120',
-            },
+    }, {
+        'url': 'https://www.ardaudiothek.de/episode/urn:ard:extra:d2fe7303d2dcbf5d/',
+        'info_dict': {
+            'id': 'urn:ard:extra:d2fe7303d2dcbf5d',
+            'ext': 'mp3',
+            'title': 'Trailer: Fanta Vier Forever, Baby!?!',
+            'description': 'md5:b64a586f2e976b8bb5ea0a79dbd8751c',
+            'channel': 'SWR',
+            'duration': 62,
+            'thumbnail': 'https://api.ardmediathek.de/image-service/images/urn:ard:image:48d3c255969be803',
+            'series': 'Fanta Vier Forever, Baby!?!',
+            'timestamp': 1732108217,
+            'upload_date': '20241120',
         },
-    ]
+    }]
 
     _QUERY_ITEM = '''\
     query($id: ID!) {
@@ -728,24 +724,21 @@ class ARDAudiothekIE(ARDAudiothekBaseIE):
 
 
 class ARDAudiothekPlaylistIE(ARDAudiothekBaseIE):
-    _VALID_URL = r'''https:?//(?:www\.)?ardaudiothek\.de/sendung/(?P<playlist>[\w-]+)/(?P<id>urn:ard:show:[a-f0-9]{16})'''
+    _VALID_URL = r'https:?//(?:www\.)?ardaudiothek\.de/sendung/(?P<playlist>[\w-]+)/(?P<id>urn:ard:show:[a-f0-9]{16})'
 
-    _TESTS = [
-        {
-            'url': 'https://www.ardaudiothek.de/sendung/mia-insomnia/urn:ard:show:c405aa26d9a4060a/',
-            'info_dict': {
-                'display_id': 'mia-insomnia',
-                'title': 'Mia Insomnia',
-                'id': 'urn:ard:show:c405aa26d9a4060a',
-                'description': 'md5:d9ceb7a6b4d26a4db3316573bb564292',
-            },
-            'playlist_mincount': 37,
+    _TESTS = [{
+        'url': 'https://www.ardaudiothek.de/sendung/mia-insomnia/urn:ard:show:c405aa26d9a4060a/',
+        'info_dict': {
+            'display_id': 'mia-insomnia',
+            'title': 'Mia Insomnia',
+            'id': 'urn:ard:show:c405aa26d9a4060a',
+            'description': 'md5:d9ceb7a6b4d26a4db3316573bb564292',
         },
-        {
-            'url': 'https://www.ardaudiothek.de/sendung/100-berlin/urn:ard:show:4d248e0806ce37bc/',
-            'only_matching': True,
-        },
-    ]
+        'playlist_mincount': 37,
+    }, {
+        'url': 'https://www.ardaudiothek.de/sendung/100-berlin/urn:ard:show:4d248e0806ce37bc/',
+        'only_matching': True,
+    }]
 
     _QUERY_PLAYLIST = '''
     query($id: ID!) {
