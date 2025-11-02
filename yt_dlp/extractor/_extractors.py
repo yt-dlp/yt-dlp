6432c06ddd5bd710f4275ca58f9ec90341ffbb5a
Nikolay Fedorov <40500428+swayll@users.noreply.github.com>
2025-11-02 17:44:18 +0300

    Fix 645:18: COM812 Trailing comma missing

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -642,7 +642,7 @@ from .filmon import (
 from .filmweb import FilmwebIE
 from .firsttv import (
     FirstTVIE,
-    FirstTVLiveIE
+    FirstTVLiveIE,
 )
 from .fivetv import FiveTVIE
 from .flextv import FlexTVIE
