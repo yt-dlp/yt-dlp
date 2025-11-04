1a83dd553062edb3dfa48158b160bc64a4aba142
Eliot <eliot.burgalat@imt-atlantique.net>
2025-11-04 14:40:20 +0100

    fix ruff check

--- yt_dlp/extractor/digiteka.py
+++ yt_dlp/extractor/digiteka.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import int_or_none, determine_ext, traverse_obj
+from ..utils import determine_ext
 
 
 class DigitekaIE(InfoExtractor):
@@ -31,10 +31,10 @@ class DigitekaIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Il est passionné de DS',
             'thumbnail': r're:^https?://.*\.jpg',
-            'duration': 89,  
+            'duration': 89,
             'upload_date': '20251012',
             'timestamp': 1760285363,
-            'uploader_id': '3pz33', 
+            'uploader_id': '3pz33',
         },
         'params': {'skip_download': True},
     }]
@@ -45,42 +45,40 @@ class DigitekaIE(InfoExtractor):
         video_type = mobj.group('embed_type') or mobj.group('site_type')
         if video_type == 'music':
             video_type = 'musique'
-            
-        site_type = mobj.group('site_type')
-        
+
         IFRAME_MD_ID = '01836272'   # Static ID for Ultimedia iframes
         iframe_json_ld_url = (
             f'https://www.ultimedia.com/deliver/generic/iframe/mdtk/{IFRAME_MD_ID}/zone/1/src/{video_id}'
         )
-        
+
         iframe_webpage = self._download_webpage(
             iframe_json_ld_url, video_id, note='Downloading iframe JSON-LD', fatal=False)
-        
+
         with open('webpage.txt', 'w', encoding='utf-8') as f:
-            f.write(iframe_webpage or '')             
-        
+            f.write(iframe_webpage or '')
+
         info = self._search_json_ld(iframe_webpage, video_id, 'VideoObject', fatal=False) or {}
-        video_url = info.get('url') 
+        video_url = info.get('url')
 
         if not video_url:
             self.report_warning('JSON-LD "contentUrl" missing. Checking DtkPlayer JS for MP4 URL.')
-            
+
             video_url = self._search_regex(
                 r'"mp4_404"\s*:\s*"(https?:\\/\\/assets\.digiteka\.com\\/encoded\\/[^"]+\\/mp4\\/[^"]+_404\.mp4)"',
                 iframe_webpage, 'MP4 404 URL', fatal=False)
             if video_url:
                 video_url = video_url.replace('\\/', '/')
-        
+
         if video_url:
             self.to_screen(f'{video_id}: SUCCESS: Using JSON-LD method.')
-            
+
             title = info.get('title') or self._html_search_meta('title', iframe_webpage)
             formats = [{
                 'url': video_url,
-                'ext': determine_ext(video_url, 'mp4'), 
+                'ext': determine_ext(video_url, 'mp4'),
                 'format_id': 'hd',
             }]
-            
+
             return {
                 'id': video_id,
                 'title': title,
@@ -89,10 +87,9 @@ class DigitekaIE(InfoExtractor):
                 'timestamp': info.get('timestamp'),
                 'formats': formats,
             }
-        
 
         self.report_warning('JSON-LD extraction failed. Falling back to original API logic.')
-        
+
         deliver_info = self._download_json(
             f'http://www.ultimedia.com/deliver/video?video={video_id}&topic={video_type}',
             video_id)
@@ -100,9 +97,7 @@ class DigitekaIE(InfoExtractor):
         yt_id = deliver_info.get('yt_id')
         if yt_id:
             return self.url_result(yt_id, 'Youtube')
-        
-        jwconf = deliver_info['jwconf']
-        
+
         return {
             'formats': formats,
         }
--- yt_dlp/extractor/ultimedia.py
+++ yt_dlp/extractor/ultimedia.py
@@ -1,13 +1,20 @@
 from .common import InfoExtractor
-from ..utils import (
-    determine_ext,
-    int_or_none,
-    parse_iso8601,
-    traverse_obj,
-)
+from ..utils import determine_ext
 
-class UltimediaVideo(InfoExtractor):
-    _VALID_URL = r'https?://www\.ultimedia\.com/default/index/videogeneric/id/\?(?P<id>[a-zA-Z0-9]+)'
+
+class AUltimediaVideoIE(InfoExtractor):
+    _VALID_URL = r'''(?x)
+    https?://(?:www\.)?(?:digiteka\.net|ultimedia\.com)/
+    (?:
+        # ... (patterns for 'deliver')
+        |
+        default/index/video
+        (?P<site_type>
+            generic|
+            music
+        )
+        /id
+    )/(?P<id>[\d+a-z]+)'''
     _TESTS = [{
         'url': 'https://www.ultimedia.com/default/index/videogeneric/id/3x5x55k',
         'info_dict': {
@@ -21,21 +28,41 @@ class UltimediaVideo(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        video_url = self._html_search_regex(
-            r'(https?://[^"\']+\.mp4)', webpage, 'video URL', 
-            fatal=False)
-        
-       
-        video_data = self._search_regex(
-            r'var\s+videoData\s*=\s*({.+?});', webpage, 'video data', default=None)
-        sources = None
-        if video_data:
-            try:
-                video_json = self._parse_json(video_data, video_id, fatal=False)
-                sources = video_json.get('sources')
-            except Exception:
-                pass
-        
+        with open('webpage.txt', 'w', encoding='utf-8') as f:
+            f.write(webpage)
 
+        title = self._search_regex(
+            r'<h1>\s*<div[^>]+id=["\']catArticle["\'][^>]*>[^<]+</div>\s*(.+?)\s*</h1>',
+            webpage, 'title', fatal=True)
 
+        description = self._html_search_regex(
+            r'<div[^>]+class=["\']trunk6["\'][^>]*>(.+?)</div>',
+            webpage, 'description', fatal=False)
 
+        IFRAME_MD_ID = '01836272'           # Static ID for Ultimedia iframes
+        iframe_json_ld_url = (
+            f'https://www.ultimedia.com/deliver/generic/iframe/mdtk/{IFRAME_MD_ID}/zone/1/src/{video_id}'
+        )
+
+        self.to_screen(f'{video_id}: Downloading JSON-LD from direct iframe URL: {iframe_json_ld_url}')
+        iframe_webpage = self._download_webpage(iframe_json_ld_url, video_id, note='Downloading iframe content')
+
+        info = self._search_json_ld(iframe_webpage, video_id, 'VideoObject', fatal=True)
+        video_url = info.get('url')
+
+        formats = [{
+            'url': video_url,
+            'ext': determine_ext(video_url, 'mp4'),
+        }]
+
+        return {
+            **info,
+            'id': video_id,
+            'title': title,
+            'description': description,
+            'formats': formats,
+        }
+
+    @staticmethod
+    def ie_key():
+        return 'AUltimeidaVideo'
