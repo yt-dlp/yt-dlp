96aa615e1de5a40423693613c0f85d93f6abe39b
Pierce Brooks <piercebrooks@rocketmail.com>
2025-11-05 12:29:03 -0500

    support mux embeds, plus other adherence to contributor guidance

--- yt_dlp/extractor/mux.py
+++ yt_dlp/extractor/mux.py
@@ -3,8 +3,8 @@ from .common import InfoExtractor
 
 class MuxStreamNewIE(InfoExtractor):
     IE_NAME = 'stream.new'
-    _WORKING = True
-    _VALID_URL = r'https?://stream.new/v/(?P<id>[A-Za-z0-9-]+)(/embed)?'
+    _VALID_URL = r'https?://(?:stream\.new/v|player\.mux\.com)/(?P<id>[A-Za-z0-9-]+)'
+    _EMBED_REGEX = [r'<iframe\b[^>]+\bsrc=["\'](?P<url>(?:https?:)?//(?:stream\.new/v|player\.mux\.com)/(?P<id>[A-Za-z0-9-]+))']
     _TESTS = [{
         'url': 'https://stream.new/v/36d8c00L1vEFwF01aF1cRKQs8nblgegMda8YR9CE3wejk/embed',
         'info_dict': {
@@ -23,15 +23,11 @@ class MuxStreamNewIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        video_url = 'https://stream.mux.com/%s.m3u8' % video_id
-
         formats = self._extract_m3u8_formats(
-            video_url, video_id, 'mp4',
-            entry_protocol='m3u8_native', m3u8_id='hls', fatal=False)
+            f'https://stream.mux.com/{video_id}.m3u8', video_id, 'mp4')
 
         return {
             'id': video_id,
             'title': video_id,
             'formats': formats,
-            '_type': 'video',
         }
