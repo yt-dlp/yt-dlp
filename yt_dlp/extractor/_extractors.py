b705de2186225a0861b4b8280b0e63ad16352e57
e1ven <401330+e1ven@users.noreply.github.com>
2025-11-05 13:33:39 -0400

    Support for multi-season dropout shows

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -537,6 +537,7 @@ from .dropbox import DropboxIE
 from .dropout import (
     DropoutIE,
     DropoutSeasonIE,
+    DropoutShowIE,
 )
 from .drtalks import DrTalksIE
 from .drtuber import DrTuberIE
