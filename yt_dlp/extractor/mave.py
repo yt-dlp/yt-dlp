e2a82b5f87eed9d7ebc974f7b06c07223f166486
Anton Larionov <11796525+anlar@users.noreply.github.com>
2025-11-08 17:27:16 +0100

    [ie/mave] Re-write Mave Digital extractor to use API directly

--- yt_dlp/extractor/mave.py
+++ yt_dlp/extractor/mave.py
@@ -1,25 +1,73 @@
-import re
-
 from .common import InfoExtractor
 from ..utils import (
     clean_html,
     int_or_none,
     parse_iso8601,
-    urljoin,
 )
-from ..utils.traversal import require, traverse_obj
 
 
-class MaveIE(InfoExtractor):
+class MaveBaseIE(InfoExtractor):
+    _API_BASE_URL = 'https://api.mave.digital/v1/website'
+    _API_BASE_STORAGE_URL = 'https://store.cloud.mts.ru/mave/'
+
+    def _load_channel_meta(self, channel_id):
+        return self._download_json(
+            f'{self._API_BASE_URL}/{channel_id}/',
+            channel_id)
+
+    def _load_episode_meta(self, channel_id, episode_code):
+        return self._download_json(
+            f'{self._API_BASE_URL}/{channel_id}/episodes/{episode_code}',
+            episode_code)
+
+    def _create_entry(self, channel_id, channel_meta, episode_meta):
+        display_id = f'{channel_id}-{episode_meta["code"]}'
+
+        reactions = episode_meta.get('reactions')
+
+        return {
+            'display_id': display_id,
+            'channel_id': channel_id,
+            'channel_url': f'https://{channel_id}.mave.digital/',
+            'vcodec': 'none',
+            'thumbnail': self._API_BASE_STORAGE_URL + episode_meta['image'],
+            'url': self._API_BASE_STORAGE_URL + episode_meta['audio'],
+            'id': episode_meta['id'],
+            'title': episode_meta['title'],
+            'description': clean_html(episode_meta['description']),
+            'duration': int_or_none(episode_meta['duration']),
+            'season_number': int_or_none(episode_meta['season']),
+            'episode_number': int_or_none(episode_meta['number']),
+            'view_count': int_or_none(episode_meta['listenings']),
+            'like_count': next(
+                (int(r['count']) for r in reactions
+                 if r['type'] == 'like'),
+                0,
+            ) if reactions else None,
+            'dislike_count': next(
+                (int(r['count']) for r in reactions
+                 if r['type'] == 'dislike'),
+                0,
+            ) if reactions else None,
+            'age_limit': 18 if episode_meta.get('is_explicit') else None,
+            'timestamp': parse_iso8601(episode_meta['publish_date']),
+            'series_id': channel_meta['podcast']['id'],
+            'series': channel_meta['podcast']['title'],
+            'channel': channel_meta['podcast']['title'],
+            'uploader': channel_meta['podcast']['author'],
+        }
+
+
+class MaveIE(MaveBaseIE):
     IE_NAME = 'mave'
-    _VALID_URL = r'https?://(?P<channel>[\w-]+)\.mave\.digital/(?P<id>ep-\d+)'
+    _VALID_URL = r'https?://(?P<channel_id>[\w-]+)\.mave\.digital/ep-(?P<episode_code>\d+)'
     _TESTS = [{
         'url': 'https://ochenlichnoe.mave.digital/ep-25',
         'md5': 'aa3e513ef588b4366df1520657cbc10c',
         'info_dict': {
             'id': '4035f587-914b-44b6-aa5a-d76685ad9bc2',
             'ext': 'mp3',
-            'display_id': 'ochenlichnoe-ep-25',
+            'display_id': 'ochenlichnoe-25',
             'title': 'Между мной и миром: психология самооценки',
             'description': 'md5:4b7463baaccb6982f326bce5c700382a',
             'uploader': 'Самарский университет',
@@ -46,7 +94,7 @@ class MaveIE(InfoExtractor):
         'info_dict': {
             'id': '41898bb5-ff57-4797-9236-37a8e537aa21',
             'ext': 'mp3',
-            'display_id': 'budem-ep-12',
+            'display_id': 'budem-12',
             'title': 'Екатерина Михайлова: "Горе от ума" не про женщин написана',
             'description': 'md5:fa3bdd59ee829dfaf16e3efcb13f1d19',
             'uploader': 'Полина Цветкова+Евгения Акопова',
@@ -69,46 +117,18 @@ class MaveIE(InfoExtractor):
             'upload_date': '20241230',
         },
     }]
-    _API_BASE_URL = 'https://api.mave.digital/'
 
     def _real_extract(self, url):
-        channel_id, slug = self._match_valid_url(url).group('channel', 'id')
-        display_id = f'{channel_id}-{slug}'
-        webpage = self._download_webpage(url, display_id)
-        data = traverse_obj(
-            self._search_nuxt_json(webpage, display_id),
-            ('data', lambda _, v: v['activeEpisodeData'], any, {require('podcast data')}))
+        channel_id, episode_code = self._match_valid_url(url).group(
+            'channel_id', 'episode_code')
 
-        return {
-            'display_id': display_id,
-            'channel_id': channel_id,
-            'channel_url': f'https://{channel_id}.mave.digital/',
-            'vcodec': 'none',
-            'thumbnail': re.sub(r'_\d+(?=\.(?:jpg|png))', '', self._og_search_thumbnail(webpage, default='')) or None,
-            **traverse_obj(data, ('activeEpisodeData', {
-                'url': ('audio', {urljoin(self._API_BASE_URL)}),
-                'id': ('id', {str}),
-                'title': ('title', {str}),
-                'description': ('description', {clean_html}),
-                'duration': ('duration', {int_or_none}),
-                'season_number': ('season', {int_or_none}),
-                'episode_number': ('number', {int_or_none}),
-                'view_count': ('listenings', {int_or_none}),
-                'like_count': ('reactions', lambda _, v: v['type'] == 'like', 'count', {int_or_none}, any),
-                'dislike_count': ('reactions', lambda _, v: v['type'] == 'dislike', 'count', {int_or_none}, any),
-                'age_limit': ('is_explicit', {bool}, {lambda x: 18 if x else None}),
-                'timestamp': ('publish_date', {parse_iso8601}),
-            })),
-            **traverse_obj(data, ('podcast', 'podcast', {
-                'series_id': ('id', {str}),
-                'series': ('title', {str}),
-                'channel': ('title', {str}),
-                'uploader': ('author', {str}),
-            })),
-        }
+        channel_meta = self._load_channel_meta(channel_id)
+        episode_meta = self._load_episode_meta(channel_id, episode_code)
+
+        return self._create_entry(channel_id, channel_meta, episode_meta)
 
 
-class MaveChannelIE(InfoExtractor):
+class MaveChannelIE(MaveBaseIE):
     IE_NAME = 'mave:channel'
     _VALID_URL = r'https?://(?P<id>[\w-]+)\.mave\.digital/?$'
     _TESTS = [{
@@ -127,32 +147,45 @@ class MaveChannelIE(InfoExtractor):
             'description': 'md5:ee36a6a52546b91b487fe08c552fdbb2',
         },
         'playlist_mincount': 20,
+    }, {
+        'url': 'https://geekcity.mave.digital/',
+        'info_dict': {
+            'id': 'geekcity',
+            'title': 'Мужчины в трико',
+            'description': 'md5:4164d425d60a0d97abdce9d1f6f8e049',
+        },
+        'playlist_mincount': 80,
     }]
 
     def _real_extract(self, url):
         channel_id = self._match_id(url)
 
-        webpage = self._download_webpage(url, channel_id)
-
-        nuxt_data = self._search_nuxt_json(webpage, channel_id)
-
-        channel_title = traverse_obj(nuxt_data, (
-            'pinia', 'common', 'podcast', 'podcast', 'title'))
-        channel_description = traverse_obj(nuxt_data, (
-            'pinia', 'common', 'podcast', 'podcast', 'description'))
-
-        episodes_data = traverse_obj(nuxt_data, ('pinia', 'common', 'episodes'))
+        channel_meta = self._load_channel_meta(channel_id)
 
         entries = []
-        for episode in episodes_data[::-1]:
-            episode_id = traverse_obj(episode, 'id')
-            episode_code = 'ep-' + str(traverse_obj(episode, 'code'))
-            if episode_code:
-                entries.append(self.url_result(
-                    f'https://{channel_id}.mave.digital/{episode_code}',
-                    ie=MaveIE.ie_key(),
-                    video_id=episode_id,
-                ))
+        page = 1
 
-        return self.playlist_result(
-            entries, channel_id, channel_title, channel_description)
+        while True:
+            data = self._download_json(
+                f'{self._API_BASE_URL}/{channel_id}/episodes?'
+                f'view=all&page={page}&sort=newest&format=all',
+                channel_id)
+
+            episodes = data.get('episodes', [])
+
+            if not episodes:
+                break
+            else:
+                for episode_meta in data['episodes']:
+                    entries.append(self._create_entry(
+                        channel_id, channel_meta, episode_meta))
+
+                page += 1
+
+        return {
+            '_type': 'playlist',
+            'id': channel_id,
+            'title': channel_meta['podcast']['title'],
+            'description': channel_meta['podcast']['description'],
+            'entries': entries,
+        }
