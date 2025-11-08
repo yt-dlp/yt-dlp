8557bf9b91a94a7f23977d4b6480a37bc06027aa
coletdjnz <coletdjnz@protonmail.com>
2025-11-08 17:25:51 +1300

    test

--- test/test_networking.py
+++ test/test_networking.py
@@ -686,6 +686,8 @@ class TestHTTPRequestHandler(TestRequestHandlerBase):
                 rh, Request(f'http://127.0.0.1:{self.http_port}/headers'))
             assert res.readable()
             assert res.read(1) == b'H'
+            # Ensure we don't close the adaptor yet
+            assert not res.closed
             assert res.read(3) == b'ost'
             assert res.read().decode().endswith('\n\n')
             assert res.read() == b''
