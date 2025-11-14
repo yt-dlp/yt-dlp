e4582162e14fd1c5b76ef8d28bb6b5b55c4605d3
nihil-admirari <50202386+nihil-admirari@users.noreply.github.com>
2025-11-14 15:24:11 +0300

    Improved Bash completions

--- devscripts/bash-completion.py
+++ devscripts/bash-completion.py
@@ -2,7 +2,9 @@
 
 # Allow direct execution
 import os
+import shlex
 import sys
+from itertools import chain
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -13,19 +15,19 @@ BASH_COMPLETION_FILE = 'completions/bash/yt-dlp'
 BASH_COMPLETION_TEMPLATE = 'devscripts/bash-completion.in'
 
 
-def build_completion(opt_parser):
-    opts_flag = []
-    for group in opt_parser.option_groups:
-        for option in group.option_list:
-            # for every long flag
-            opts_flag.append(option.get_opt_string())
+def yt_dlp_flags():
+    for opt in yt_dlp.parseOpts(ignore_config_files=True)[0]._get_all_options():
+        for opt_str in chain(opt._short_opts, opt._long_opts):
+            yield shlex.quote(opt_str)
+
+
+def build_completion():
     with open(BASH_COMPLETION_TEMPLATE) as f:
         template = f.read()
     with open(BASH_COMPLETION_FILE, 'w') as f:
         # just using the special char
-        filled_template = template.replace('{{flags}}', ' '.join(opts_flag))
+        filled_template = template.replace('YT_DLP_FLAGS', ' '.join(sorted(yt_dlp_flags())))
         f.write(filled_template)
 
 
-parser = yt_dlp.parseOpts(ignore_config_files=True)[0]
-build_completion(parser)
+build_completion()
