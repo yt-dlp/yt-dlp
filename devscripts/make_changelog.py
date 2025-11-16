yt-dlp/yt-dlp

commit b52f70170915e895b6867c9bfea0e3dfb593d7a9

[devscripts] `make_changelog`: Extend regex for fix commits
Authored by: bashonly

devscripts/make_changelog.py
@@ -251,7 +251,7 @@ class CommitRange:
         ''', re.VERBOSE | re.DOTALL)
     EXTRACTOR_INDICATOR_RE = re.compile(r'(?:Fix|Add)\s+Extractors?', re.IGNORECASE)
     REVERT_RE = re.compile(r'(?:\[[^\]]+\]\s+)?(?i:Revert)\s+([\da-f]{40})')
-    FIXES_RE = re.compile(r'(?i:(?:bug\s*)?fix(?:es)?(?:\s+bugs?)?(?:\s+in|\s+for)?|Improve)\s+([\da-f]{40})')
+    FIXES_RE = re.compile(r'(?i:(?:bug\s*)?fix(?:es)?(?:\s+(?:bugs?|regression(?:\s+introduced)?))?(?:\s+(?:in|for|from|by))?|Improve)\s+([\da-f]{40})')
     UPSTREAM_MERGE_RE = re.compile(r'Update to ytdl-commit-([\da-f]+)')
 
     def __init__(self, start, end, default_author=None):

commit 751125ad794bccd77cfd1b1cce054925563f47a2

[devscripts] `make_changelog`: Make regex readable
Fix regression introduced in b52f70170915e895b6867c9bfea0e3dfb593d7a9

Authored by: bashonly

devscripts/make_changelog.py
@@ -251,7 +251,13 @@ class CommitRange:
         ''', re.VERBOSE | re.DOTALL)
     EXTRACTOR_INDICATOR_RE = re.compile(r'(?:Fix|Add)\s+Extractors?', re.IGNORECASE)
     REVERT_RE = re.compile(r'(?:\[[^\]]+\]\s+)?(?i:Revert)\s+([\da-f]{40})')
-    FIXES_RE = re.compile(r'(?i:(?:bug\s*)?fix(?:es)?(?:\s+(?:bugs?|regression(?:\s+introduced)?))?(?:\s+(?:in|for|from|by))?|Improve)\s+([\da-f]{40})')
+    FIXES_RE = re.compile(r'''
+        (?i:
+            (?:bug\s*)?fix(?:es)?(?:
+                \s+(?:bugs?|regression(?:\s+introduced)?)
+            )?(?:\s+(?:in|for|from|by))?
+            |Improve
+        )\s+([\da-f]{40})''', re.VERBOSE)
     UPSTREAM_MERGE_RE = re.compile(r'Update to ytdl-commit-([\da-f]+)')
 
     def __init__(self, start, end, default_author=None):