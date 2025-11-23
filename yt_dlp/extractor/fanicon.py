27d502cbef2bb049c1139fb6ef05bf3bc81e1364
doe1080 <98906116+doe1080@users.noreply.github.com>
2025-11-23 22:14:07 +0900

    fix

--- yt_dlp/extractor/fanicon.py
+++ yt_dlp/extractor/fanicon.py
@@ -3,6 +3,7 @@ import itertools
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
+    clean_html,
     determine_ext,
     filter_dict,
     int_or_none,
@@ -10,7 +11,7 @@ from ..utils import (
     str_or_none,
     url_or_none,
 )
-from ..utils.traversal import traverse_obj
+from ..utils.traversal import require, traverse_obj
 
 
 class FaniconBaseIE(InfoExtractor):
@@ -19,7 +20,7 @@ class FaniconBaseIE(InfoExtractor):
 
 
 class FaniconIE(FaniconBaseIE):
-    _VALID_URL = r'https?://fanicon\.net/(?:fanilive|web/fancommunities)/(?P<fc_id>\d+)(?:/livearchive)?/(?P<id>[\da-f]+)(?:/(?P<slug>[\da-f]+))?'
+    _VALID_URL = r'https?://fanicon\.net/(?:fanilive|web/fancommunities)/(?P<fc_id>\d+)(?:/live(?:archive)?)?/(?P<id>[\da-f]+)(?:/(?P<slug>[\da-f]+))?'
     _TESTS = [{
         'url': 'https://fanicon.net/fanilive/4937/1224907/83fd46f739df37d',
         'info_dict': {
@@ -54,21 +55,24 @@ class FaniconIE(FaniconBaseIE):
             'uploader_id': '1147555',
         },
         'skip': 'Only available for subscribers',
+    }, {
+        'url': 'https://fanicon.net/web/fancommunities/5831/live/1790503',
+        'only_matching': True,
     }]
 
     def _parse_tron(self, key, webpage, video_id):
         return self._search_json(
-            rf'window\.tron\.{key}\s*=\s*', webpage, f'{key} info', video_id, default={})
+            rf'window\.tron\.{key}\s*=', webpage, f'{key} info', video_id, default={})
 
     def _real_extract(self, url):
-        fc_id, video_id, slug = self._match_valid_url(url).groups()
+        fc_id, video_id, slug = self._match_valid_url(url).group('fc_id', 'id', 'slug')
         webpage = self._download_webpage(url, video_id)
 
         if slug:
             live_info = self._parse_tron('live', webpage, video_id)
-            file_url = live_info['url']
+            file_url = traverse_obj(live_info, ('url', {url_or_none}, {require('Source URL')}))
             is_live = not traverse_obj(live_info, ('isArchive', {bool}))
-            formats = self._extract_m3u8_formats(file_url, video_id, 'mp4', live=is_live)
+            formats = self._extract_m3u8_formats(file_url, video_id, 'mp4')
 
             metadata = {
                 'display_id': slug,
@@ -86,26 +90,29 @@ class FaniconIE(FaniconBaseIE):
             if error := cards.get('error'):
                 raise ExtractorError(error['message'], expected=True)
 
-            file_url = cards['card']['media']['fileUrl']
-            is_live = traverse_obj(cards, ('card', 'media', 'isOnLive', {bool}))
+            card = cards['card']
+            file_url = traverse_obj(card, (
+                'media', ('fileUrl', 'playbackUrl'), {url_or_none}, any, {require('Source URL')}))
+            is_live = traverse_obj(card, ('media', 'isOnLive', {bool}))
 
-            if not (ext := determine_ext(file_url)):
+            ext = determine_ext(file_url)
+            if not ext:
                 raise ExtractorError('Unexpected file URL')
             elif ext == 'm3u8':
-                formats = self._extract_m3u8_formats(file_url, video_id, 'mp4', live=is_live)
-            elif ext in {'mp3', 'mp4'}:
+                formats = self._extract_m3u8_formats(file_url, video_id, 'mp4')
+            elif ext in ('mp3', 'mp4'):
                 formats = [{'ext': ext, 'url': file_url}]
             else:
                 self.raise_no_formats(f'Unexpected file type: {ext}')
 
-            metadata = traverse_obj(cards, ('card', {
-                'title': ('body', {str.strip}),
-                'comment_count': ('commentCount', {int}),
-                'like_count': ('likeCount', {int}),
+            metadata = traverse_obj(card, {
+                'title': ('body', {clean_html}),
+                'comment_count': ('commentCount', {int_or_none}),
+                'like_count': ('likeCount', {int_or_none}),
                 'modified_timestamp': ('updatedAt', {parse_iso8601}),
-                'tags': ('tags', ..., {str}),
+                'tags': ('tags', ..., {clean_html}, filter, all, filter),
                 'timestamp': ('createdAt', {parse_iso8601}),
-            }))
+            })
 
         return {
             'id': video_id,
@@ -113,11 +120,11 @@ class FaniconIE(FaniconBaseIE):
             'is_live': is_live,
             **metadata,
             **traverse_obj(self._download_json(
-                f'{self._API_BASE}/fanclubs/{fc_id}', fc_id), ('fanclub', {
-                    'channel': ('name', {str}),
+                f'{self._API_BASE}/fanclubs/{fc_id}', fc_id, fatal=False), ('fanclub', {
+                    'channel': ('name', {clean_html}),
                     'channel_id': ('id', {str_or_none}),
                     'thumbnail': ('heroImageUrl', {url_or_none}),
-                    'uploader': ('owners', ..., 'name', {str}, any),
+                    'uploader': ('owners', ..., 'name', {clean_html}, any),
                     'uploader_id': (('ownerId', ('owners', ..., 'id')), {str_or_none}, any),
                 }),
             ),
@@ -139,12 +146,8 @@ class FaniconFCIE(FaniconBaseIE):
         min_id = None
         for page in itertools.count(1):
             cards = self._download_json(
-                f'{self._API_BASE}/cards', fc_id,
-                f'Downloading page {page}', query=filter_dict({
-                    'fanclubId': fc_id,
-                    'maxId': min_id,
-                }),
-            )
+                f'{self._API_BASE}/cards', fc_id, f'Downloading page {page}',
+                query=filter_dict({'fanclubId': fc_id, 'maxId': min_id}))
             if not cards:
                 break
             if error := cards.get('error'):
@@ -159,9 +162,9 @@ class FaniconFCIE(FaniconBaseIE):
                     yield self.url_result(
                         f'{self._BASE_URL}/web/fancommunities/{fc_id}/livearchive/{media["cardId"]}', FaniconIE)
 
-            if not (min_id := traverse_obj(cards, (
-                'cards', ..., 'id', {int_or_none}, filter, all, {min}, {str_or_none},
-            ))):
+            min_id = traverse_obj(cards, (
+                'cards', ..., 'id', {int_or_none}, filter, all, {min}, {str_or_none}))
+            if not min_id:
                 break
 
     def _real_extract(self, url):
