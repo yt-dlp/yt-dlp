04f1fa49ac8d32d3a9a2d6b39ff3a291ae27152a
puteid <100704281+putridambassador121@users.noreply.github.com>
2025-12-06 21:08:49 -0300

    added the changes based on the code review and added multi track downloads

--- yt_dlp/extractor/bnrnews.py
+++ yt_dlp/extractor/bnrnews.py
@@ -3,7 +3,6 @@ from ..utils import (
     ExtractorError,
     int_or_none,
     str_or_none,
-    url_or_none,
     urlhandle_detect_ext,
 )
 from ..utils.traversal import traverse_obj
@@ -22,37 +21,67 @@ class BNRNewsIE(InfoExtractor):
             'url': 'https://bnrnews.bg/api/media/9ce1fff8-f2aa-4d87-90b5-01dff966bb9a',
             'ext': 'mp3',
         },
+    }, {
+        'url': 'https://bnrnews.bg/horizont/post/395494/100-godini-ot-rozhdenieto-na-georgi-partsalev',
+        'info_dict': {
+            'id': '395494',
+            'title': '100 години от рождението на Георги Парцалев',
+            'thumbnail': 'https://bnrnews.bg/api/media/ff6874b7-6085-4bb6-8152-039d79faf833',
+        },
+        'playlist_count': 2,
     }]
 
-    def _make_media_api(self, url_path: str) -> str:
-        return f'https://bnrnews.bg/api/media/{url_path}'
+    def _get_multiple_audio(self, audio_list: list[dict[str, str]]):
+        for audio in audio_list:
+            ext_handle = self._request_webpage(audio.get('url'), audio.get('id'), note='getting audio entry info')
+            ext = urlhandle_detect_ext(ext_handle)
+            if ext == 'unknown_video':
+                raise ExtractorError('error determining file type')
+            yield {
+                'id': audio.get('id'),
+                'title': audio.get('title'),
+                'url': audio.get('url'),
+                'ext': ext,
+                'vcodec': 'none',
+            }
 
-    def _get_single_audio(self, url, video_id):
-        html_page = self._download_webpage(url, video_id, note='Downloading WebPage', errnote='Failed to download page')
-        if html_page:
-            json_body = self._search_nextjs_data(html_page, video_id)
-            if json_body:
-                data_field = traverse_obj(json_body, ('props', 'pageProps', 'data'))
-                if data_field:
-                    audio_info: dict[str, str] = traverse_obj(data_field, ({
-                        'id': ('Id', {int_or_none}, {str_or_none}),
-                        'title': ('Title', {str}),
-                        'thumbnail': ('MainImageInstance', 'Id', {self._make_media_api}, {url_or_none}),
-                        'url': ('Sections', lambda _, v: v.get('SectionType') == 'Audio', 'Audio', 'Id', {self._make_media_api}, {url_or_none}, any),
-                    }))
+    def _get_single_audio(self, url, audio_id):
+        html_page = self._download_webpage(url, audio_id, note='Downloading WebPage', errnote='Failed to download page')
+        json_body = self._search_nextjs_data(html_page, audio_id)
+        data_field = traverse_obj(json_body, ('props', 'pageProps', 'data'))
+        if data_field:
+            audio_info: dict[str, str] = traverse_obj(data_field, ({
+                'id': ('Id', {int_or_none}, {str_or_none}),
+                'title': ('Title', {str}),
+                'thumbnail': ('MainImageInstance', 'Id', {str}, {lambda x: f'https://bnrnews.bg/api/media/{x}'}),
+            }))
 
-                    if traverse_obj(audio_info, 'url'):
-                        ext_handle = self._request_webpage(audio_info.get('url'), video_id)
-                        ext = urlhandle_detect_ext(ext_handle)
-                        if ext == 'unknown_video':
-                            raise ExtractorError('error determining file type')
-                        audio_info['ext'] = ext
-                        return audio_info
-                    else:
-                        raise ExtractorError("there's no audio to download", expected=True)
+            audio_list = traverse_obj(data_field, (
+                'Sections',
+                lambda _, v: v.get('SectionType') == 'Audio',
+                {
+                    'id': ('Id', {int_or_none}, {str_or_none}),
+                    'title': ('Audio', 'Description', {str}),
+                    'url': ('Audio', 'Id', {lambda x: f'https://bnrnews.bg/api/media/{x}'}),
+                },
+                all,
+            ))
+            if audio_list:
+                if len(audio_list) > 1:
+                    return self.playlist_result(self._get_multiple_audio(audio_list),
+                                                playlist_id=audio_id,
+                                                playlist_title=audio_info.get('title'), thumbnail=audio_info.get('thumbnail'),
+                                                )
                 else:
-                    raise ExtractorError("the data field is either empty or it doesn't exist")
+                    ext_handle = self._request_webpage(audio_list[0].get('url'), audio_id, note='getting audio info')
+                    ext = urlhandle_detect_ext(ext_handle)
+                    if ext == 'unknown_video':
+                        raise ExtractorError('error determining file type')
+                    audio_info['ext'] = ext
+                    audio_info['url'] = audio_list[0].get('url')
+                    return audio_info
+            raise ExtractorError("there's no audio to download", expected=True)
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        return self._get_single_audio(url, video_id)
+        audio_id = self._match_id(url)
+        return self._get_single_audio(url, audio_id)
