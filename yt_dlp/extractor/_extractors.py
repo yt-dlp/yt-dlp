2069392ea64431e9058747bf03d89fa0b8e851ca
1-Byte <1-byte@gmx.net>
2025-12-14 13:07:23 +0100

    [ie/swisscom] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2010,6 +2010,7 @@ from .svt import (
     SVTSeriesIE,
 )
 from .swearnet import SwearnetEpisodeIE
+from .swisscom import SwisscomWebcastIE
 from .syvdk import SYVDKIE
 from .sztvhu import SztvHuIE
 from .tagesschau import TagesschauIE
