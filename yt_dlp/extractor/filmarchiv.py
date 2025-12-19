865b3a9b202eefd94414e6ec8e7d19452075ccb6
bashonly <88596187+bashonly@users.noreply.github.com>
2025-12-19 00:28:30 +0000

    Apply suggestions from code review

--- yt_dlp/extractor/filmarchiv.py
+++ yt_dlp/extractor/filmarchiv.py
@@ -9,7 +9,7 @@ from ..utils.traversal import (
 
 class FilmArchivIE(InfoExtractor):
     IE_NAME = 'FILMARCHIV ON'
-    _VALID_URL = r'https?://(?:www\.)?filmarchiv\.at/de/filmarchiv-on/video/(?P<id>[0-9a-zA-Z_]+)'
+    _VALID_URL = r'https?://(?:www\.)?filmarchiv\.at/de/filmarchiv-on/video/(?P<id>f_[0-9a-zA-Z]{5,})'
     _TESTS = [{
         'url': 'https://www.filmarchiv.at/de/filmarchiv-on/video/f_0305p7xKrXUPBwoNE9x6mh',
         'md5': '54a6596f6a84624531866008a77fa27a',
@@ -18,7 +18,7 @@ class FilmArchivIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Der Wurstelprater zur Kaiserzeit',
             'description': 'md5:9843f92df5cc9a4975cee7aabcf6e3b2',
-            'thumbnail': r're:https://cdn.filmarchiv.at/f_0305/p7xKrXUPBwoNE9x6mh[^/]*/poster.jpg$',
+            'thumbnail': r're:https://cdn\.filmarchiv\.at/f_0305/p7xKrXUPBwoNE9x6mh_v1/poster\.jpg',
         },
     }, {
         'url': 'https://www.filmarchiv.at/de/filmarchiv-on/video/f_0306vI3wO0tJIsfrqYFQXF',
@@ -28,7 +28,7 @@ class FilmArchivIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Vor 70 Jahren: Wettgehen der Briefträger in Wien',
             'description': 'md5:b2a2e4230923cd1969d471c552e62811',
-            'thumbnail': r're:https://cdn.filmarchiv.at/f_0306/vI3wO0tJIsfrqYFQXF[^/]*/poster.jpg$',
+            'thumbnail': r're:https://cdn\.filmarchiv\.at/f_0306/vI3wO0tJIsfrqYFQXF_v1/poster\.jpg',
         },
     }]
 
@@ -37,29 +37,19 @@ class FilmArchivIE(InfoExtractor):
         webpage = self._download_webpage(url, media_id)
 
         description = traverse_obj(webpage, (
-            {find_elements(
-                tag='div',
-                attr='class', value=r'[^\'"]*(?<=[\'"\s])border-base-content(?=[\'"\s])[^\'"]*',
-                html=False, regex=True)}, ...,
-            {find_elements(
-                tag='div',
-                attr='class', value=r'[^\'"]*(?<=[\'"\s])prose(?=[\'"\s])[^\'"]*',
-                html=False, regex=True)}, ...,
-            {clean_html}, any,
-        ))
+            {find_elements(tag='div', attr='class', value=r'.*\bborder-base-content\b', regex=True)}, ...,
+            {find_elements(tag='div', attr='class', value=r'.*\bprose\b', html=False, regex=True)}, ...,
+            {clean_html}, any))
 
-        og_img = self._html_search_meta('og:image', webpage, 'image URL', fatal=True)
-        prefix = self._search_regex(
-            r'/videostatic/([^/]+/[^_]+_[^/]+)/poster.jpg', og_img, 'prefix')
-
+        path = '/'.join((media_id[:6], media_id[6:]))
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(
-            f'https://cdn.filmarchiv.at/{prefix}_sv1/playlist.m3u8', media_id)
+            f'https://cdn.filmarchiv.at/{path}_sv1/playlist.m3u8', media_id)
 
         return {
             'id': media_id,
             'title': traverse_obj(webpage, ({find_element(tag='title-div')}, {clean_html})),
             'description': description,
-            'thumbnail': f'https://cdn.filmarchiv.at/{prefix}/poster.jpg',
+            'thumbnail': f'https://cdn.filmarchiv.at/{path}_v1/poster.jpg',
             'formats': formats,
             'subtitles': subtitles,
         }
