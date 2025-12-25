253397824146ca82770c72195ff7aadf3d1d5b20
puteid <100704281+putridambassador121@users.noreply.github.com>
2025-12-25 14:04:42 -0300

    added thumbnail and fixed the tests

--- yt_dlp/extractor/matreska.py
+++ yt_dlp/extractor/matreska.py
@@ -1,41 +1,106 @@
+import json
+
 from .common import InfoExtractor
+from ..utils import (
+    traverse_obj,
+    url_or_none,
+)
 
 
-class MatreskaIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?yourextractor\.com/watch/(?P<id>[0-9]+)'
+class MatreshkaIE(InfoExtractor):
+    _VALID_URL = r'https?://matreshka\.tv/video/(?P<id>[\w]+)(?:\?playlistID=(?P<playlist_id>[\w]+))?'
     _TESTS = [{
-        'url': 'https://yourextractor.com/watch/42',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+        'url': 'https://matreshka.tv/video/HwKAY4Id5QA',
+        'md5': '1e528392822120c5740a5cfab5483dbc',
         'info_dict': {
-            # For videos, only the 'id' and 'ext' fields are required to RUN the test:
-            'id': '42',
+            'id': 'HwKAY4Id5QA',
+            'title': '2025.11.04  Диалог об алкоголе с Джеком Лондоном (МЕТРО) - Стас васильев',
+            'thumbnail': 'https://c2-images.cmtv.ru/video/wgKgXz6-xvY/HwKAY4Id5QA/frame/5/1280x720.png',
+            'description': '',
             'ext': 'mp4',
-            # Then if the test run fails, it will output the missing/incorrect fields.
-            # Properties can be added as:
-            # * A value, e.g.
-            #     'title': 'Video title goes here',
-            # * MD5 checksum; start the string with 'md5:', e.g.
-            #     'description': 'md5:098f6bcd4621d373cade4e832627b4f6',
-            # * A regular expression; start the string with 're:', e.g.
-            #     'thumbnail': r're:https?://.*\.jpg$',
-            # * A count of elements in a list; start the string with 'count:', e.g.
-            #     'tags': 'count:10',
-            # * Any Python type, e.g.
-            #     'view_count': int,
-        }
+        },
+    }, {
+        'url': 'https://matreshka.tv/video/KgBAzC_u0gA?playlistID=YwKA4Zq50gA',
+        'info_dict': {
+            'id': 'YwKA4Zq50gA',
+            'title': 'Андор - Пучков и Жуков',
+        },
+        'playlist_count': 9,
     }]
+
+    def _playlist_entries(self, playlist_info):
+        video_ids: list[str] = traverse_obj(
+            playlist_info,
+            ('data', 0, 'videos', ..., 'id'),
+            expected_type=str,
+        )
+
+        for video_id in video_ids:
+            yield self.url_result(
+                f'https://matreshka.tv/video/{video_id}',
+                ie=MatreshkaIE,
+                video_id=video_id,
+            )
+
+    def get_playlist_info(self, playlist_id):
+        post_request_data = json.dumps({
+            'field_mask': ['id',
+                           'name',
+                           'description',
+                           'videos.id',
+                           'videos'],
+            'filter': [{
+                'is': '=',
+                'field': 'id',
+                'value': playlist_id,
+            }],
+        }).encode('utf-8')
+
+        playlist_info = self._download_json('https://matreshka.tv/api/v2/playlist', playlist_id,
+                                            note='Downloading playlist info', data=post_request_data,
+                                            headers={'accept': 'application/json, text/plain, */*',
+                                                     'content-type': 'application/json'})
+        if playlist_info:
+            playlist_name = traverse_obj(playlist_info, ('data', 0, 'name', {str}), expected_type=str)
+            playlist_desc = traverse_obj(playlist_info, ('data', 0, 'description', {str}), expected_type=str)
+            return self.playlist_result(self._playlist_entries(playlist_info), playlist_id,
+                                        playlist_title=playlist_name,
+                                        playlist_description=playlist_desc)
+
+    def get_video_info(self, video_id):
+        video_info = self._download_json(f'https://matreshka.tv/api/video-service/v1/video/{video_id}',
+                                         video_id, note='Downloading video info')
+        if video_info:
+            video_dict = traverse_obj(video_info, ('data', {
+                'id': ('id', {str}),
+                'title': ('name', {str}),
+                'description': ('description', {str}),
+                'thumbnails': (
+                    'cover', 'png',
+                    {dict},
+                    {lambda d: [
+                        {
+                            'url': url,
+                            'width': int(size.split('x')[0]),
+                            'height': int(size.split('x')[1]),
+                        }
+                        for size, url in d.items()
+                    ]},
+                ),
+            }))
+            formats, subtitles = traverse_obj(video_info,
+                                              ('data', 'video_url', 'h264',
+                                               'src', 'hls', {url_or_none},
+                                                  {lambda video_url: self._extract_m3u8_formats_and_subtitles(video_url, video_id, m3u8_id='hls')},
+                                               ))
+            video_dict['formats'] = formats
+            video_dict['subtitles'] = subtitles
+            return video_dict
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
-
-        # TODO more code goes here, for example ...
-        title = self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title')
-
-        return {
-            'id': video_id,
-            'title': title,
-            'description': self._og_search_description(webpage),
-            'uploader': self._search_regex(r'<div[^>]+id="uploader"[^>]*>([^<]+)<', webpage, 'uploader', fatal=False),
-            # TODO more properties (see yt_dlp/extractor/common.py)
-        }
+        playlist_id = self._match_valid_url(url).group('playlist_id')
+        if playlist_id:
+            return self.get_playlist_info(playlist_id)
+        else:
+            return self.get_video_info(video_id)
