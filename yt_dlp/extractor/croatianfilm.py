e55e8fae5bcc7044634744288d233668be2f559c
0xvd <m.naiemahemad@gmail.com>
2026-01-04 14:21:16 +0530

    Added Full TESTS

--- yt_dlp/extractor/croatianfilm.py
+++ yt_dlp/extractor/croatianfilm.py
@@ -10,6 +10,7 @@ from ..utils.traversal import traverse_obj
 class CroatianFilmIE(InfoExtractor):
     IE_NAME = 'croatian.film'
     _VALID_URL = r'https://?(?:www\.)?croatian\.film/[a-z]{2}/[^/?#]+/(?P<id>\d+)'
+    _GEO_COUNTRIES = ['HR']
 
     _TESTS = [{
         'url': 'https://www.croatian.film/en/films/74757',
@@ -20,10 +21,39 @@ class CroatianFilmIE(InfoExtractor):
             'uploader_id': 'user94192658',
             'uploader_url': 'https://vimeo.com/user94192658',
             'duration': 937,
+            'thumbnails': list,
+            'formats': list,
+            'subtitles': dict,
+            'live_status': None,
+            'release_timestamp': None,
+            'ext': 'mp4',
+            'webpage_url_domain': 'player.vimeo.com',
+            'extractor': 'vimeo',
+            'extractor_key': 'Vimeo',
+            'release_year': None,
         },
+        'expected_warnings': ['Failed to parse XML: not well-formed'],
     }, {
         'url': 'https://www.croatian.film/en/films/77144',
-        'only_matching': True,
+        'info_dict': {
+            'id': '1144997795',
+            'title': '\u201cROKO\u201d r. Ivana Marini\u0107 Kragi\u0107',
+            'uploader': 'croatian.film',
+            'uploader_id': 'user94192658',
+            'uploader_url': 'https://vimeo.com/user94192658',
+            'duration': 1023,
+            'thumbnails': list,
+            'formats': list,
+            'subtitles': dict,
+            'live_status': None,
+            'release_timestamp': None,
+            'ext': 'mp4',
+            'webpage_url_domain': 'player.vimeo.com',
+            'extractor': 'vimeo',
+            'extractor_key': 'Vimeo',
+            'release_year': None,
+        },
+        'expected_warnings': ['Failed to parse XML: not well-formed'],
     }, {
         'url': 'https://www.croatian.film/en/films/75904/watch',
         'only_matching': True,
@@ -38,7 +68,7 @@ class CroatianFilmIE(InfoExtractor):
         if errors := traverse_obj(api_data, ('errors', lambda _, v: v['code'])):
             codes = traverse_obj(errors, (..., 'code', {str}))
             if 'INVALID_COUNTRY' in codes:
-                self.raise_geo_restricted(countries=['hr'])
+                self.raise_geo_restricted(countries=self._GEO_COUNTRIES)
             raise ExtractorError(join_nonempty(
                 *(traverse_obj(errors, (..., 'details', {str})) or codes),
                 delim='; '))
