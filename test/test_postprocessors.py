a972a15a9b43576a6e0bffb678c14705c7c85177
bashonly <88596187+bashonly@users.noreply.github.com>
2026-01-05 02:46:28 +0000

    fix copy/paste error

--- test/test_postprocessors.py
+++ test/test_postprocessors.py
@@ -35,7 +35,6 @@ class TestMetadataFromField(unittest.TestCase):
         self.assertEqual(MetadataParserPP.format_to_regex('é'), r'(?s)(?P<é>.+)')
         self.assertEqual(MetadataParserPP.format_to_regex('0invalid'), '0invalid')
         self.assertEqual(MetadataParserPP.format_to_regex('invalid '), 'invalid ')
-        self.assertEqual(MetadataParserPP.format_to_regex('invalid '), 'invalid ')
         self.assertEqual(MetadataParserPP.format_to_regex('é²'), 'é²')
 
     def test_field_to_template(self):
