1c7978b7a8094e926d38aabc0d9f37f43411dbfd
nihil-admirari <50202386+nihil-admirari@users.noreply.github.com>
2026-01-09 20:51:35 +0300

    Generate file and directory Bash completions programmatically

--- devscripts/bash-completion.py
+++ devscripts/bash-completion.py
@@ -4,6 +4,7 @@
 import os
 import shlex
 import sys
+from dataclasses import dataclass
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -14,19 +15,32 @@ BASH_COMPLETION_FILE = 'completions/bash/yt-dlp'
 BASH_COMPLETION_TEMPLATE = 'devscripts/bash-completion.in'
 
 
+@dataclass
+class YtDlpOpts:
+    opts: tuple[str, ...]
+    file_opts: tuple[str, ...]
+    dir_opts: tuple[str, ...]
+
+
 def yt_dlp_options():
     opts, long_opts = [], []
+    file_opts, dir_opts = [], []
 
     for opt in yt_dlp.parseOpts(ignore_config_files=True)[0]._get_all_options():
         opts.extend(opt._short_opts)
         long_opts.extend(opt._long_opts)
+        if opt.metavar in {'FILE', 'PATH', 'CERTFILE', 'KEYFILE'}:
+            file_opts.extend(opt._short_opts)
+            file_opts.extend(opt._long_opts)
+        elif opt.metavar == 'DIR':
+            dir_opts.extend(opt._short_opts)
+            dir_opts.extend(opt._long_opts)
 
     opts.sort()
     long_opts.sort()
     opts.extend(long_opts)
 
-    for o in opts:
-        yield shlex.quote(o)
+    return YtDlpOpts(tuple(opts), tuple(file_opts), tuple(dir_opts))
 
 
 def build_completion():
@@ -34,8 +48,14 @@ def build_completion():
         template = f.read()
     with open(BASH_COMPLETION_FILE, 'w') as f:
         # just using the special char
-        filled_template = template.replace('YT_DLP_OPTS', ' '.join(yt_dlp_options()))
-        f.write(filled_template)
+        opts = yt_dlp_options()
+        f.write(template.replace(
+            'YT_DLP_FILE_OPTS_CASE', ' | '.join(shlex.quote(o) for o in opts.file_opts),
+        ).replace(
+            'YT_DLP_DIR_OPTS_CASE', ' | '.join(shlex.quote(o) for o in opts.dir_opts),
+        ).replace(
+            'YT_DLP_OPTS_ARRAY', ' '.join(shlex.quote(o) for o in opts.opts),
+        ))
 
 
 build_completion()
