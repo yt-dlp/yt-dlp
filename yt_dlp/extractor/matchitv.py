857e83ac2b26264197468cbd17c541aaad2fda02
Gareth Seddon <gseddon@amazon.co.uk>
2026-01-16 19:04:53 +0000

    Update Matchi.tv extractor with further feedback

--- yt_dlp/extractor/matchitv.py
+++ yt_dlp/extractor/matchitv.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import traverse_obj, unified_strdate
+from ..utils import join_nonempty, traverse_obj, unified_strdate
 
 
 class MatchiTVIE(InfoExtractor):
@@ -19,24 +19,17 @@ class MatchiTVIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        # Extract JSON data from __NEXT_DATA__ script tag
-        page_data = self._search_json(
-            r'<script[^>]+id="__NEXT_DATA__"[^>]*>', webpage, 'next data', video_id, fatal=False)
-
-        loaded_media = traverse_obj(page_data, ('props', 'pageProps', 'loadedMedia'), default={})
+        nextjs_data = self._search_nextjs_data(webpage, video_id)
+        loaded_media = traverse_obj(nextjs_data, ('props', 'pageProps', 'loadedMedia'), default={})
         court_description = traverse_obj(loaded_media, 'courtDescription')
         start_date_time = traverse_obj(loaded_media, 'startDateTime')
 
         return {
             'id': video_id,
-            'title': f'{court_description} {start_date_time}',
+            'title': join_nonempty(court_description, start_date_time, delim=' '),
             'thumbnail': f'https://thumbnails.padelgo.tv/{video_id}.jpg',
             'upload_date': unified_strdate(start_date_time),
             'formats': self._extract_m3u8_formats(
                 f'https://streams.padelgo.tv/v2/streams/m3u8/{video_id}/anonymous/playlist.m3u8',
-                video_id,
-                'mp4',
-                entry_protocol='m3u8_native',
-                m3u8_id='hls',
-                fatal=False),
+                video_id, 'mp4', m3u8_id='hls'),
         }
