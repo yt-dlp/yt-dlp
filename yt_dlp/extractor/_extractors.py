fdca2520bb3be56824ba49f01aee13c26ec79011
akamayu <aka.mayu.ouo@gmail.com>
2026-01-25 23:41:14 +0800

    [ie/plurk.com] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1523,6 +1523,7 @@ from .pluralsight import (
     PluralsightCourseIE,
     PluralsightIE,
 )
+from .plurk import PlurkExtractorIE
 from .plutotv import PlutoTVIE
 from .plvideo import PlVideoIE
 from .plyr import PlyrEmbedIE
