9216a31b46efd4abde8dd1f78ad4763888848a6a
py-snake <py-snake@gmx.com>
2026-01-30 17:15:50 +0100

    mediaklikk extractor subtitle support, display_id regex fix

--- yt_dlp/extractor/mediaklikk.py
+++ yt_dlp/extractor/mediaklikk.py
@@ -13,62 +13,64 @@ class MediaKlikkIE(InfoExtractor):
     _VALID_URL = r'''(?x)https?://(?:www\.)?
                         (?:mediaklikk|m4sport|hirado)\.hu/.*?(?:videok?|cikk)/
                         (?:(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/)?
-                        (?P<id>[^/#?_]+)'''
+                        (?:[^/]+/)*(?P<id>[^/#?]+)'''
 
     _TESTS = [{
-        # mediaklikk
-        'url': 'https://mediaklikk.hu/ajanlo/video/2025/08/04/heviz-dzsungel-a-viz-alatt-ajanlo-08-10/',
+        # mediaklikk - link has SRT subtitle track available
+        'url': 'https://mediaklikk.hu/sorozat-0/video/2026/01/28/sorozat/koronas-sas-3-evad-396-resz',
         'info_dict': {
-            'id': '8573769',
-            'title': 'Hévíz - dzsungel a víz alatt – Ajánló (08.10.)',
-            'display_id': 'heviz-dzsungel-a-viz-alatt-ajanlo-08-10',
+            'id': '9308421',
+            'title': 'Koronás sas 3. évad, 396. rész',
+            'display_id': 'koronas-sas-3-evad-396-resz',
             'ext': 'mp4',
-            'upload_date': '20250804',
-            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2025/08/vlcsnap-2025-08-04-13h48m24s336.jpg',
+            'upload_date': '20260128',
+            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2026/01/2021-010143-M0151-02_3700_cover_09.jpg',
+            'episode_id': '2021-010143-M0151',
         },
     }, {
-        # mediaklikk - date in html
-        'url': 'https://mediaklikk.hu/video/hazajaro-bilo-hegyseg-verocei-barangolas-a-drava-menten/',
+        # mediaklikk
+        'url': 'https://mediaklikk.hu/ismeretterjeszto/video/2026/01/18/ismeretterjeszto/hazajaro-juliai-alpok-szeles-az-isonzo-vize/',
         'info_dict': {
-            'id': '8482167',
-            'title': 'Hazajáró, Bilo-hegység - Verőcei barangolás a Dráva mentén',
-            'display_id': 'hazajaro-bilo-hegyseg-verocei-barangolas-a-drava-menten',
+            'id': '9275589',
+            'title': 'Hazajáró, JÚLIAI-ALPOK – Széles az Isonzó vize',
+            'display_id': 'hazajaro-juliai-alpok-szeles-az-isonzo-vize',
             'ext': 'mp4',
-            'upload_date': '20250703',
-            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2025/07/2024-000307-M0010-01_3700_cover_01.jpg',
+            'upload_date': '20260118',
+            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2026/01/2026-000492-M0002-01_3700_cover_01.jpg',
+            'episode_id': '2026-000492-M0002',
         },
     }, {
         # m4sport
-        'url': 'https://m4sport.hu/video/2025/08/07/holnap-kezdodik-a-12-vilagjatekok/',
+        'url': 'https://m4sport.hu/video/2026/01/28/szalai-gabor-nehez-meccs-lesz-de-keszen-allunk-2',
         'info_dict': {
-            'id': '8581887',
-            'title': 'Holnap kezdődik a 12. Világjátékok',
-            'display_id': 'holnap-kezdodik-a-12-vilagjatekok',
+            'id': '9316011',
+            'title': 'Szalai Gábor: Nehéz meccs lesz, de készen állunk',
+            'display_id': 'szalai-gabor-nehez-meccs-lesz-de-keszen-allunk-2',
             'ext': 'mp4',
-            'upload_date': '20250807',
-            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2025/08/vlcsnap-2025-08-06-20h30m48s817.jpg',
+            'upload_date': '20260128',
+            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2026/01/vlcsnap-2026-01-28-13h34m15s032.jpg',
         },
     }, {
         # hirado
-        'url': 'https://hirado.hu/video/2025/08/09/idojaras-jelentes-2025-augusztus-9-2230',
+        'url': 'https://hirado.hu/video/2021/02/15/a-magyar-faust-mikrofilm/',
         'info_dict': {
-            'id': '8592033',
-            'title': 'Időjárás-jelentés, 2025. augusztus 9. 22:30',
-            'display_id': 'idojaras-jelentes-2025-augusztus-9-2230',
+            'id': '4352602',
+            'title': 'A magyar Faust - Mikrofilm',
+            'display_id': 'a-magyar-faust-mikrofilm',
             'ext': 'mp4',
-            'upload_date': '20250809',
-            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2025/08/Idojaras-jelentes-35-1.jpg',
+            'upload_date': '20210215',
+            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2021/02/vlcsnap-2021-02-15-18h02m40s081-1024x581.jpg',
         },
     }, {
         # hirado - subcategory
-        'url': 'https://hirado.hu/belfold/video/2025/08/09/nyitott-porta-napok-2025/',
+        'url': 'https://hirado.hu/belfold/video/2026/01/27/fidesz-kdnp-botranyos-bulihajok-a-fovarosban-a-hatterben-kartell-gyanus-cegekkel-2',
         'info_dict': {
-            'id': '8590581',
-            'title': 'Nyitott Porta Napok 2025',
-            'display_id': 'nyitott-porta-napok-2025',
+            'id': '9312042',
+            'title': 'Fidesz-KDNP: Botrányos bulihajók a fővárosban, a háttérben kartell gyanús cégekkel',
+            'display_id': 'fidesz-kdnp-botranyos-bulihajok-a-fovarosban-a-hatterben-kartell-gyanus-cegekkel-2',
             'ext': 'mp4',
-            'upload_date': '20250809',
-            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2025/08/vlcsnap-2025-08-09-10h35m01s887.jpg',
+            'upload_date': '20260127',
+            'thumbnail': 'https://cdn.cms.mtv.hu/wp-content/uploads/sites/4/2026/01/vlcsnap-2026-01-27-13h47m04s569.jpg',
         },
     }]
 
@@ -95,12 +97,20 @@ class MediaKlikkIE(InfoExtractor):
             query=player_data, headers={'Referer': url})
         player_json = self._search_json(
             r'\bpl\.setup\s*\(', player_page, 'player json', video_id, end_pattern=r'\);')
-        playlist_url = traverse_obj(
-            player_json, ('playlist', lambda _, v: v['type'] == 'hls', 'file', {url_or_none}), get_all=False)
+        playlist_item = traverse_obj(
+            player_json, ('playlist', lambda _, v: v['type'] == 'hls'), get_all=False)
+        playlist_url = traverse_obj(playlist_item, ('file', {url_or_none}))
         if not playlist_url:
             raise ExtractorError('Unable to extract playlist url')
 
         formats, subtitles = self._extract_m3u8_formats_and_subtitles(playlist_url, video_id)
+        for track in traverse_obj(playlist_item, ('tracks', lambda _, v: v.get('kind') == 'captions')) or []:
+            sub_url = traverse_obj(track, ('file', {url_or_none}))
+            if sub_url:
+                subtitles.setdefault('hu', []).append({
+                    'url': sub_url,
+                    'ext': 'srt',
+                })
 
         return {
             'id': video_id,
