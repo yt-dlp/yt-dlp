d4c31ccf2d653310e25748041292e2e0124a7870
doe1080 <98906116+doe1080@users.noreply.github.com>
2026-02-05 13:29:26 +0900

    fix

--- yt_dlp/extractor/tfo.py
+++ yt_dlp/extractor/tfo.py
@@ -1,44 +1,28 @@
 import urllib.parse
 
 from .common import InfoExtractor
-from .uplynk import UplynkBaseIE
+from ..networking.exceptions import HTTPError
 from ..utils import (
+    ExtractorError,
     clean_html,
     int_or_none,
     parse_iso8601,
     str_or_none,
     url_or_none,
+    urljoin,
+    variadic,
 )
 from ..utils.traversal import require, traverse_obj
 
 
-class TFOIE(UplynkBaseIE):
+class TFOIE(InfoExtractor):
     IE_NAME = 'tfo'
     IE_DESC = 'Télévision française de l\'Ontario'
 
     _BASE_URL = 'https://www.tfo.org'
+    _GEO_COUNTRIES = ['CA']
     _VALID_URL = r'https?://(?:www\.)?tfo\.org/(?:episode|film|regarder|titre)(?:/[\w-]+)+/(?P<id>(?:GP)?\d{6})'
     _TESTS = [{
-        'url': 'https://www.tfo.org/regarder/bardot-la-meprise/GP701766',
-        'info_dict': {
-            'id': 'GP701766',
-            'ext': 'mp4',
-            'title': 'Bardot, la Méprise',
-            'age_limit': 13,
-            'alt_title': 'bardot-la-meprise',
-            'description': 'md5:16ca832101b6c3838bb61cd8fa06aa9e',
-            'duration': 3134.8480000000022,
-            'genres': ['Biographie et portraits'],
-            'release_timestamp': 1747875610,
-            'release_date': '20250522',
-            'release_year': 2013,
-            'series': 'Bardot, la Méprise',
-            'tags': ['13+'],
-            'thumbnail': r're:https?://.+\.jpg',
-            'uploader_id': '872295f75a144bcf880cf68f4ad35db1',
-        },
-        'skip': True,
-    }, {
         'url': 'https://www.tfo.org/regarder/pouletosaure-rex-partie-1-2/GP639511',
         'info_dict': {
             'id': 'GP639511',
@@ -47,7 +31,6 @@ class TFOIE(UplynkBaseIE):
             'age_limit': 6,
             'alt_title': 'pouletosaure-rex-partie-1-2',
             'description': 'md5:24e1b629fab54d537eb40a0ef6630afa',
-            'duration': 1321.216000000001,
             'episode': 'Pouletosaure Rex - Partie 1 & 2',
             'episode_id': 'episode-1',
             'episode_number': 1,
@@ -61,10 +44,8 @@ class TFOIE(UplynkBaseIE):
             'series': 'Dino Dex',
             'series_id': '003051136',
             'tags': ['G'],
-            'thumbnail': r're:https?://.+\.jpg',
-            'uploader_id': '872295f75a144bcf880cf68f4ad35db1',
+            'thumbnail': r're:https?://.+\.(?:jpg|png)',
         },
-        'skip': True,
     }, {
         'url': 'https://www.tfo.org/episode/passeport-pour-le-monde/saison-2/episode-1/vietnam-dans-loeil-du-dragon/GP938523',
         'info_dict': {
@@ -74,7 +55,6 @@ class TFOIE(UplynkBaseIE):
             'age_limit': 18,
             'alt_title': 'vietnam-dans-loeil-du-dragon',
             'description': 'md5:ca182241d021ba832680ccbc09dc70fd',
-            'duration': 3120.0000000000023,
             'episode': 'VIETNAM : Dans l\'oeil du dragon',
             'episode_id': 'episode-1',
             'episode_number': 1,
@@ -88,10 +68,8 @@ class TFOIE(UplynkBaseIE):
             'series': 'Passeport pour le monde',
             'series_id': '002968508',
             'tags': ['G'],
-            'thumbnail': r're:https?://.+\.jpg',
-            'uploader_id': '872295f75a144bcf880cf68f4ad35db1',
+            'thumbnail': r're:https?://.+\.(?:jpg|png)',
         },
-        'skip': True,
     }, {
         'url': 'https://www.tfo.org/titre/entre-les-lignes/GP704192',
         'info_dict': {
@@ -100,37 +78,31 @@ class TFOIE(UplynkBaseIE):
             'title': 'Entre les lignes',
             'age_limit': 0,
             'alt_title': 'entre-les-lignes',
-            'duration': 2042.8800000000015,
             'genres': ['Société'],
             'release_date': '20231105',
             'release_timestamp': 1699146000,
             'release_year': 2008,
             'series': 'Entre les lignes',
             'tags': ['G'],
-            'thumbnail': r're:https?://.+\.jpg',
-            'uploader_id': '872295f75a144bcf880cf68f4ad35db1',
+            'thumbnail': r're:https?://.+\.(?:jpg|png)',
         },
-        'skip': True,
     }, {
-        'url': 'https://www.tfo.org/film/le-chat/498047',
+        'url': 'https://www.tfo.org/film/a-nous-la-liberte/852897',
         'info_dict': {
-            'id': '498047',
+            'id': '852897',
             'ext': 'mp4',
-            'title': 'Le Chat',
-            'age_limit': 16,
-            'alt_title': 'le-chat',
-            'description': 'md5:1e19c39fff1a48e3875feb73a52146b7',
-            'duration': 5257.7279999998755,
-            'genres': ['Drame', 'Psychologique'],
-            'release_date': '20250617',
-            'release_timestamp': 1750122010,
-            'release_year': 1971,
-            'series': 'Le Chat',
-            'tags': ['16+'],
-            'thumbnail': r're:https?://.+\.jpg',
-            'uploader_id': '872295f75a144bcf880cf68f4ad35db1',
+            'title': 'À nous la liberté',
+            'age_limit': 0,
+            'alt_title': 'a-nous-la-liberte',
+            'description': 'md5:2e7d617f0b7451b9f31c1bfb62d6f33b',
+            'genres': ['Comédie', 'Satirique'],
+            'release_date': '20250811',
+            'release_timestamp': 1754874007,
+            'release_year': 1931,
+            'series': 'À nous la liberté',
+            'tags': ['G'],
+            'thumbnail': r're:https?://.+\.(?:jpg|png)',
         },
-        'skip': True,
     }]
 
     def _real_extract(self, url):
@@ -138,24 +110,29 @@ class TFOIE(UplynkBaseIE):
         slug = urllib.parse.urlparse(url).path.rstrip('/').split('/')[-2]
         webpage = self._download_webpage(
             f'{self._BASE_URL}/regarder/{slug}/{video_id}', video_id)
+        nextjs_data = self._search_nextjs_data(webpage, video_id)
 
-        next_data = self._search_nextjs_data(webpage, video_id)
-        page_props = next_data['props']['pageProps']
-        season_id = traverse_obj(page_props, ('seasonId', {str_or_none}))
-
-        build_id, locale = traverse_obj(next_data, (('buildId', 'locale'), {str}, all))
+        build_id, locale = traverse_obj(nextjs_data, (('buildId', 'locale'), {str}, all))
         path = urllib.parse.urlparse(self._og_search_url(webpage)).path
 
-        video_data = self._download_json(
-            f'{self._BASE_URL}/_next/data/{build_id}/{locale}{path}.json',
-            video_id, expected_status=404)
+        try:
+            video_data = self._download_json(
+                f'{self._BASE_URL}/_next/data/{build_id}/{locale}{path}.json', video_id)
+        except ExtractorError as e:
+            if isinstance(e.cause, HTTPError) and e.cause.status == 404:
+                return self.url_result(url, self.ie_key())
         product = traverse_obj(video_data, (
             'pageProps', 'product', {require('video information')}))
 
+        page_props = nextjs_data['props']['pageProps']
+        season_id = traverse_obj(page_props, ('seasonId', {str_or_none}))
+        m3u8_url = traverse_obj(page_props, ('metadata', 'video', {url_or_none}))
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, video_id, 'mp4')
+
         return {
-            **self._extract_uplynk_info(traverse_obj(page_props, (
-                'metadata', 'video', {url_or_none},
-            ))),
+            'id': video_id,
+            'formats': formats,
+            'subtitles': subtitles,
             **traverse_obj(product, {
                 'title': ('name', {str}),
                 'age_limit': ('ratingCode', {int_or_none}),
@@ -195,7 +172,6 @@ class TFOIE(UplynkBaseIE):
                     'season_number': ('seasonNumber', {int_or_none}),
                 },
             )),
-            'id': video_id,
         }
 
 
@@ -216,19 +192,20 @@ class TFOSeriesIE(InfoExtractor):
             'id': '002981471',
             'title': 'Chacun son île | Saison 2',
         },
-        'playlist_mincount': 8,
+        'playlist_mincount': 13,
     }]
 
     def _real_extract(self, url):
-        season, series_id = self._match_valid_url(url).groups()
+        season_number, series_id = self._match_valid_url(url).group('season', 'id')
         webpage = self._download_webpage(url, series_id)
-        json_ld = next(self._yield_json_ld(webpage, series_id))
+        nextjs_data = self._search_nextjs_data(webpage, series_id)
+        path = (lambda _, v: v['seasonNumber'] == season_number) if season_number else ...
 
         entries = [
             self.url_result(x, TFOIE)
-            for x in traverse_obj(json_ld, (
-                '@graph', ..., *(() if season else ('seasons', ...)),
-                'episode', ..., 'url', {url_or_none},
+            for x in traverse_obj(nextjs_data, (
+                'props', 'pageProps', 'product', 'seasons', *variadic(path), 'episodes',
+                ..., 'canonicalUrl', {urljoin('https://www.tfo.org/')},
             ))
         ]
 
