4e155903029da3e41640ec584d4b63d92e518e32
Lutz3 <blutzi333@gmail.com>
2026-02-11 04:33:02 +0100

    Refactor fragment handling and error reporting

--- yt_dlp/downloader/fragment.py
+++ yt_dlp/downloader/fragment.py
@@ -110,11 +110,16 @@ class FragmentFD(FileDownloader):
 
     def _download_fragment(self, ctx, frag_url, info_dict, headers=None, request_data=None):
         fragment_filename = '%s-Frag%d' % (ctx['tmpfilename'], ctx['fragment_index'])
+        fragment_count = ctx.get('fragment_count') or ctx.get('total_frags')
+        is_last_fragment = fragment_count and ctx['fragment_index'] == fragment_count
         fragment_info_dict = {
             'url': frag_url,
             'http_headers': headers or info_dict.get('http_headers'),
             'request_data': request_data,
             'ctx_id': ctx.get('ctx_id'),
+            'extractor_key': info_dict.get('extractor_key') or info_dict.get('extractor'),
+            'fragment_number': ctx['fragment_index'],
+            'is_last_fragment': is_last_fragment,
         }
         frag_resume_len = 0
         if ctx['dl'].params.get('continuedl', True):
@@ -145,14 +150,19 @@ class FragmentFD(FileDownloader):
 
     def _append_fragment(self, ctx, frag_content):
         try:
-            ctx['dest_stream'].write(frag_content)
-            ctx['dest_stream'].flush()
+            if frag_content is not None:
+                ctx['dest_stream'].write(frag_content)
+                ctx['dest_stream'].flush()
         finally:
             if self.__do_ytdl_file(ctx):
                 self._write_ytdl_file(ctx)
-            if not self.params.get('keep_fragments', False):
-                self.try_remove(ctx['fragment_filename_sanitized'])
-            del ctx['fragment_filename_sanitized']
+            
+            fname = ctx.get('fragment_filename_sanitized')
+            if fname and os.path.exists(fname) and not self.params.get('keep_fragments', False):
+                self.try_remove(fname)
+            
+            if 'fragment_filename_sanitized' in ctx:
+                del ctx['fragment_filename_sanitized']
 
     def _prepare_frag_download(self, ctx):
         if not ctx.setdefault('live', False):
@@ -349,6 +359,8 @@ class FragmentFD(FileDownloader):
         def decrypt_fragment(fragment, frag_content):
             if frag_content is None:
                 return
+            if not frag_content:
+                return frag_content
             decrypt_info = fragment.get('decrypt_info')
             if not decrypt_info or decrypt_info['METHOD'] != 'AES-128':
                 return frag_content
@@ -470,20 +482,10 @@ class FragmentFD(FileDownloader):
                         raise
 
         def append_fragment(frag_content, frag_index, ctx):
-            if frag_content:
+            if frag_content is not None:
                 self._append_fragment(ctx, pack_func(frag_content, frag_index))
             elif not is_fatal(frag_index - 1):
-                # allow the last YouTube fragment to be empty
-                extractor = info_dict.get('extractor_key') or info_dict.get('extractor')
-                is_youtube = extractor and extractor.lower().startswith('youtube')
-                fragment_count = ctx.get('fragment_count') or ctx.get('total_frags')
-                is_last_fragment = fragment_count and frag_index == fragment_count
-                if is_youtube and is_last_fragment:
-                    self.to_screen(f'[download] Empty last YouTube fragment {frag_index}, skipping')
-                    if not self.params.get('keep_fragments', False):
-                        self.try_remove(ctx['fragment_filename_sanitized'])
-                else:
-                    self.report_skip_fragment(frag_index, 'fragment not found')
+                self.report_skip_fragment(frag_index, 'fragment not found')
             else:
                 ctx['dest_stream'].close()
                 self.report_error(f'fragment {frag_index} not found, unable to continue')
