f42d2ae677237a65bf18a209882f15f4dccdc68b
Luke <git@luke.fastmail.us>
2026-02-11 15:43:17 -0800

    [GDCVaultIE] Corrected all tests sans _5

--- yt_dlp/extractor/gdcvault.py
+++ yt_dlp/extractor/gdcvault.py
@@ -8,40 +8,38 @@ from ..utils.traversal import traverse_obj
 
 
 class GDCVaultIE(InfoExtractor):
-    _WORKING = False
+    _WORKING = True
     _VALID_URL = r'https?://(?:www\.)?gdcvault\.com/play/(?P<id>\d+)(?:/(?P<name>[\w-]+))?'
     _NETRC_MACHINE = 'gdcvault'
     _TESTS = [
         {
-            'url': 'http://www.gdcvault.com/play/1019721/Doki-Doki-Universe-Sweet-Simple',
-            'md5': '7ce8388f544c88b7ac11c7ab1b593704',
+            'url': 'https://www.gdcvault.com/play/1019721/Doki-Doki-Universe-Sweet-Simple',
+            'md5': '6f79bb5f7a743e6d7464877835405964',
             'info_dict': {
-                'id': '201311826596_AWNY',
+                'id': '1019721',
                 'display_id': 'Doki-Doki-Universe-Sweet-Simple',
                 'ext': 'mp4',
                 'title': 'Doki-Doki Universe: Sweet, Simple and Genuine (GDC Next 10)',
             },
         },
         {
-            'url': 'http://www.gdcvault.com/play/1015683/Embracing-the-Dark-Art-of',
+            'url': 'https://www.gdcvault.com/play/1015683/Embracing-the-Dark-Art-of',
             'info_dict': {
-                'id': '201203272_1330951438328RSXR',
+                'id': '1015683',
                 'display_id': 'Embracing-the-Dark-Art-of',
-                'ext': 'flv',
+                'ext': 'mp4',
                 'title': 'Embracing the Dark Art of Mathematical Modeling in AI',
-            },
-            'params': {
-                'skip_download': True,  # Requires rtmpdump
             },
         },
         {
-            'url': 'http://www.gdcvault.com/play/1015301/Thexder-Meets-Windows-95-or',
-            'md5': 'a5eb77996ef82118afbbe8e48731b98e',
+            # missing login to assert correct MD5 for test case
+            'url': 'https://gdcvault.com/play/1035154/How-Tabletop-Design-Helped-Shape',
+            'md5': '',
             'info_dict': {
-                'id': '1015301',
-                'display_id': 'Thexder-Meets-Windows-95-or',
-                'ext': 'flv',
-                'title': 'Thexder Meets Windows 95, or Writing Great Games in the Windows 95 Environment',
+                'id': '1035154',
+                'display_id': 'How-Tabletop-Design-Helped-Shape',
+                'ext': 'mp4',
+                'title': "How Tabletop Design Helped Shape 'UFO 50'",
             },
             'skip': 'Requires login',
         },
@@ -50,67 +48,38 @@ class GDCVaultIE(InfoExtractor):
             'only_matching': True,
         },
         {
-            # Hard-coded hostname
-            'url': 'http://gdcvault.com/play/1023460/Tenacious-Design-and-The-Interface',
-            'md5': 'a8efb6c31ed06ca8739294960b2dbabd',
+            # Historically was a hard-coded hostname
+            'url': 'https://gdcvault.com/play/1023460/Tenacious-Design-and-The-Interface',
+            'md5': '250f70ff9dfefbcc644c0a2d62767ce7',
             'info_dict': {
-                'id': '840376_BQRC',
+                'id': '1023460',
                 'ext': 'mp4',
                 'display_id': 'Tenacious-Design-and-The-Interface',
                 'title': 'Tenacious Design and The Interface of \'Destiny\'',
             },
         },
         {
-            # Multiple audios
-            'url': 'http://www.gdcvault.com/play/1014631/Classic-Game-Postmortem-PAC',
+            # Includes Ja/En Subtitles. Historically had multiple audio streams. Currently only
+            # has Japanese.
+            'url': 'https://www.gdcvault.com/play/1014631/Classic-Game-Postmortem-PAC',
             'info_dict': {
-                'id': '12396_1299111843500GMPX',
+                'id': '1014631',
                 'ext': 'mp4',
-                'title': 'How to Create a Good Game - From My Experience of Designing Pac-Man',
+                'display_id': 'Classic-Game-Postmortem-PAC',
+                'title': 'Classic Game Postmortem - PAC-MAN',
             },
-            # 'params': {
-            #     'skip_download': True,  # Requires rtmpdump
-            #     'format': 'jp',  # The japanese audio
-            # }
+            'params': {
+                'skip_download': True,  # Requires rtmpdump
+            },
         },
         {
             # gdc-player.html
-            'url': 'http://www.gdcvault.com/play/1435/An-American-engine-in-Tokyo',
+            'url': 'https://www.gdcvault.com/play/1435/An-American-engine-in-Tokyo',
             'info_dict': {
-                'id': '9350_1238021887562UHXB',
+                'id': '1435`',
                 'display_id': 'An-American-engine-in-Tokyo',
                 'ext': 'mp4',
                 'title': 'An American Engine in Tokyo:/nThe collaboration of Epic Games and Square Enix/nFor THE LAST REMINANT',
-            },
-        },
-        {
-            # Kaltura Embed
-            'url': 'https://www.gdcvault.com/play/1026180/Mastering-the-Apex-of-Scaling',
-            'info_dict': {
-                'id': '0_h1fg8j3p',
-                'ext': 'mp4',
-                'title': 'Mastering the Apex of Scaling Game Servers (Presented by Multiplay)',
-                'timestamp': 1554401811,
-                'upload_date': '20190404',
-                'uploader_id': 'joe@blazestreaming.com',
-            },
-            'params': {
-                'format': 'mp4-408',
-            },
-        },
-        {
-            # Kaltura embed, whitespace between quote and embedded URL in iframe's src
-            'url': 'https://www.gdcvault.com/play/1025699',
-            'info_dict': {
-                'id': '0_zagynv0a',
-                'ext': 'mp4',
-                'title': 'Tech Toolbox',
-                'upload_date': '20190408',
-                'uploader_id': 'joe@blazestreaming.com',
-                'timestamp': 1554764629,
-            },
-            'params': {
-                'skip_download': True,
             },
         },
         {
@@ -166,7 +135,7 @@ class GDCVaultIE(InfoExtractor):
             # same requested URL later on.
             _player_page = self._download_webpage(iframe_url, display_id)
 
-            js_url = f'{player_baseurl}/script_VOD.js'
+            js_url = f'{player_baseurl}script_VOD.js'
             player_url_js = self._download_webpage(js_url, display_id)
 
             cdn_url_js = self._search_regex(
@@ -174,18 +143,21 @@ class GDCVaultIE(InfoExtractor):
                 player_url_js, 'cdn_url_js')
             cdn_url_real = cdn_url_js.replace("'+videoId+'", blaze_id)
 
+            m3u8_formats, m3u8_subs = self._extract_m3u8_formats_and_subtitles(cdn_url_real, video_id)
+
             title = self._html_search_regex(
                 r'<[td]{2}[^>]*>\s*<strong>Session Name:?</strong>\s*</[td]{2}>\s*<[td]{2}[^>]*>\s*(.*?)\s*</[td]{2}>',
                 start_page, 'title', fatal=False)
             if not title:
                 title = remove_start(self._html_extract_title(start_page), 'GDC Vault - ')
 
-            if cdn_url_real and title:
+            if m3u8_formats and title:
                 return {
                     'id': video_id,
                     'display_id': display_id,
                     'title': title,
-                    'formats': self._extract_m3u8_formats(cdn_url_real, video_id),
+                    'formats': m3u8_formats,
+                    'subtitles': m3u8_subs,
                 }
 
         # fallback to legacy if any website still supports it
