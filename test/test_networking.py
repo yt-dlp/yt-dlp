f82cac62423708eea4674211921c6306e21ba9f1
bashonly <88596187+bashonly@users.noreply.github.com>
2026-02-15 11:46:29 +0000

    Update test/test_networking.py

--- test/test_networking.py
+++ test/test_networking.py
@@ -1005,7 +1005,8 @@ class TestUrllibRequestHandler(TestRequestHandlerBase):
 @pytest.mark.parametrize('handler', ['Requests'], indirect=True)
 class TestRequestsRequestHandler(TestRequestHandlerBase):
     @pytest.mark.parametrize('raised,expected', [
-        # ruff: disable[PLW0108] `requests` and/or `urllib3` may not be available
+        # `requests` and/or `urllib3` may not be available
+        # ruff: disable[PLW0108]
         (lambda: requests.exceptions.ConnectTimeout(), TransportError),
         (lambda: requests.exceptions.ReadTimeout(), TransportError),
         (lambda: requests.exceptions.Timeout(), TransportError),
