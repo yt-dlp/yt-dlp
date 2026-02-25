bb83fb14dbda866a3429b45ee041c02c62e5642c
JChris246 <JChris246@users.noreply.github.com>
2026-02-25 01:26:35 -0400

    refactor: use api to get video id and duration

--- yt_dlp/extractor/sasflix.py
+++ yt_dlp/extractor/sasflix.py
@@ -1,4 +1,5 @@
 from .common import InfoExtractor
+from ..utils import ExtractorError, traverse_obj
 
 
 class SasflixIE(InfoExtractor):
@@ -8,20 +9,26 @@ class SasflixIE(InfoExtractor):
         'md5': 'f63c16f8b48325731ca3529d0cad776c',
         'info_dict': {
             'id': 'a7851209-c06a-446a-9adc-aaf92ef8fae0',
-            'title': 'Сильный паспорт // Долину обездолили // Венесуэле хана №177 / Публикации / Сасфликс',
+            'title': 'Сильный паспорт // Долину обездолили // Венесуэле хана №177',
+            'duration': 2024,
             'ext': 'mp4',
         },
     }]
 
     def _real_extract(self, url):
         topic_id = self._match_id(url)
-        webpage = self._download_webpage(url, topic_id)
-        video_id = self._search_regex(r'id="video-player-([\w-]+)"',
-                                      webpage, 'video id')
+        topic_data = self._download_json(
+            f'https://sasflix.ru/api/web/topics/{topic_id}', topic_id)
+
+        if not topic_data.get('has_video'):
+            raise ExtractorError("no video available")
+
+        video_id = traverse_obj(topic_data, ('video', 'id'))
 
         return {
             'id': topic_id,
             'formats': self._extract_m3u8_formats(
                 f'https://sasflix.ru/api/video/{video_id}', video_id),
-            'title': self._og_search_title(webpage),
+            'title': topic_data.get('title'),
+            'duration': traverse_obj(topic_data, ('video', 'duration'))
         }
