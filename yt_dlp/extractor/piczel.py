346cb75bb688808f35d04a72e6b1bc31fb234b98
Mangrover007 <Mangrover@proton.me>
2026-03-07 20:44:16 +0530

    [ie/piczel] removed bad test

--- yt_dlp/extractor/piczel.py
+++ yt_dlp/extractor/piczel.py
@@ -32,26 +32,6 @@ class PiczelIE(InfoExtractor):
             # m3u8 download
             'skip_download': True,
         },
-    }, {
-        'url': 'https://piczel.tv/watch/Mangrover',
-        'info_dict': {
-            'id': '140239',
-            'formats': list,
-            'title': 'Stream',
-            'age_limit': int,
-            'uploader': 'Mangrover',
-            'uploader_id': 146061,
-            'ext': 'mp4',
-            'description': None,
-            'tags': list,
-            'concurrent_view_count': int,
-            'channel_follower_count': int,
-        },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
-        },
-        'skip': 'HTTP Error 404: Not Found',
     }]
 
     def _real_extract(self, url):
