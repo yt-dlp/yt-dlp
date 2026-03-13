f90512b0c9449c4a08cb75e095a0cee3c4b1ab4d
bashonly <bashonly@protonmail.com>
2026-03-13 02:47:34 -0500

    [test:jsc] add encode/decode tests
    Authored by: bashonly

--- test/test_jsc/test_ejs_integration.py
+++ test/test_jsc/test_ejs_integration.py
@@ -73,10 +73,13 @@ CHALLENGES: list[Challenge] = [
     Challenge('74edf1a3', Variant.tv, JsChallengeType.N, {
         'IlLiA21ny7gqA2m4p37': '9nRTxrbM1f0yHg',
         'eabGFpsUKuWHXGh6FR4': 'izmYqDEY6kl7Sg',
+        'eabGF/ps%UK=uWHXGh6FR4': 'LACmqlhaBpiPlgE-a',
     }),
     Challenge('74edf1a3', Variant.tv, JsChallengeType.SIG, {
         'NJAJEij0EwRgIhAI0KExTgjfPk-MPM9MAdzyyPRt=BM8-XO5tm5hlMCSVpAiEAv7eP3CURqZNSPow8BXXAoazVoXgeMP7gH9BdylHCwgw=gwzz':
             'NJAJEij0EwRgIhAI0KExTgjfPk-MPM9MAdzyyPRt=BM8-XO5tm5hzMCSVpAiEAv7eP3CURqZNSPow8BXXAoazVoXgeMP7gH9BdylHCwgw=gwzl',
+        '\x00\x01\x02%\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49':
+            '\x00\x01\x02%\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x49\x44\x45\x46\x47\x48\x43',
     }),
     # 20523
     Challenge('901741ab', Variant.tv, JsChallengeType.N, {
