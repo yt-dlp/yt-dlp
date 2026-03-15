0f4b390ad580eaa513c57c7458a7f2cc86410efc
mickael-covier <mick@laptop-7m7lk7bt.localdomain>
2026-03-15 23:17:22 +0100

    begin fixing cliprs extractor

--- yt_dlp/extractor/cliprs.py
+++ yt_dlp/extractor/cliprs.py
@@ -1,31 +1,39 @@
-from .onet import OnetBaseIE
+from .common import InfoExtractor
 
 
-class ClipRsIE(OnetBaseIE):
-    _WORKING = False
+class ClipRsIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
-    _TEST = {
-        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
-        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+    _TEST = {{
+        'url': 'https://www.clip.rs/premijera-4-ruze-okupila-poznate-glumica-u-prekratkoj-haljini-branka-i-gagi-ne-skidaju-osmeh-a-kubura-bez-zene-video/15231',  # 'rasrs-blic.pulsevideo.eu format"
         'info_dict': {
-            'id': '1488842.1399140381',
+            'id': '2156960.1306067903',
+            'display_id': 'premijera-4-ruze-okupila-poznate-glumica-u-prekratkoj-haljini-branka-i-gagi-ne-skidaju-osmeh-a-kubura-bez-zene-video',
             'ext': 'mp4',
-            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
-            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
-            'duration': 229,
-            'timestamp': 1459850243,
-            'upload_date': '20160405',
+            'title': 'PREMIJERA "4 RUŽE" OKUPILA POZNATE Glumica u PREKRATKOJ HALJINI, Branka i Gagi ne skidaju osmeh, a Kubura BEZ ŽENE (VIDEO)',
         },
-    }
+    }, {
+        'url': 'https://www.clip.rs/ceca-otputovala-sa-deckom-i-njegovom-cerkom-evo-gde-su-otisli-uzivace-zajedno-u-zimskoj-idili/15690',  # 'rasrs-blic.embed.videos.ringpublishing.com' format
+        'info_dict': {
+            'id': '2212947.809338313',
+            'display_id': 'ceca-otputovala-sa-deckom-i-njegovom-cerkom-evo-gde-su-otisli-uzivace-zajedno-u-zimskoj-idili',
+            'ext': 'mp4',
+            'title': 'Ceca otputovala sa DEČKOM I NJEGOVOM ĆERKOM! Evo gde su otišli, uživaće zajedno u zimskoj idili',
+        },
+
+    }}
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
-
         webpage = self._download_webpage(url, display_id)
 
-        mvp_id = self._search_mvp_id(webpage)
+        video_id = None
+        display_id = None
+        video_url = None
+        title = self._og_search_title(webpage)
 
-        info_dict = self._extract_from_id(mvp_id, webpage)
-        info_dict['display_id'] = display_id
-
-        return info_dict
+        return {
+            'id': video_id,
+            'display_id': display_id,
+            'url': video_url,
+            'title': title,
+        }
