7a2bc605d22d9e18907c797889e2d88269fcdc7e
deadmeu <dmu@duck.com>
2026-03-25 18:44:13 +1000

    [ie/tiktok:user] Extract user signature as playlist description

--- yt_dlp/extractor/tiktok.py
+++ yt_dlp/extractor/tiktok.py
@@ -1004,6 +1004,7 @@ class TikTokUserIE(TikTokBaseIE):
         'info_dict': {
             'id': 'MS4wLjABAAAAepiJKgwWhulvCpSuUVsp7sgVVsFJbbNaLeQ6OQ0oAJERGDUIXhb2yxxHZedsItgT',
             'title': 'corgibobaa',
+            'description': str,
         },
         'expected_warnings': ['TikTok API keeps sending the same page'],
         'params': {'extractor_retries': 10},
@@ -1013,6 +1014,7 @@ class TikTokUserIE(TikTokBaseIE):
         'info_dict': {
             'id': 'MS4wLjABAAAA0tF1nBwQVVMyrGu3CqttkNgM68Do1OXUFuCY0CRQk8fEtSVDj89HqoqvbSTmUP2W',
             'title': '6820838815978423302',
+            'description': str,
         },
         'expected_warnings': ['TikTok API keeps sending the same page'],
         'params': {'extractor_retries': 10},
@@ -1022,6 +1024,7 @@ class TikTokUserIE(TikTokBaseIE):
         'info_dict': {
             'id': 'MS4wLjABAAAAiKfaDWeCsT3IHwY77zqWGtVRIy9v4ws1HbVi7auP1Vx7dJysU_hc5yRiGywojRD6',
             'title': 'meme',
+            'description': str,
         },
         'expected_warnings': ['TikTok API keeps sending the same page'],
         'params': {'extractor_retries': 10},
@@ -1147,7 +1150,7 @@ class TikTokUserIE(TikTokBaseIE):
         return None
 
     def _real_extract(self, url):
-        user_name, sec_uid = self._match_id(url), None
+        user_name, sec_uid, signature = self._match_id(url), None, None
         if re.fullmatch(r'MS4wLjABAAAA[\w-]{64}', user_name):
             user_name, sec_uid = None, user_name
             fail_early = True
@@ -1170,6 +1173,7 @@ class TikTokUserIE(TikTokBaseIE):
             else:
                 sec_uid = self._extract_sec_uid_from_embed(user_name)
                 fail_early = False
+            signature = traverse_obj(detail, ('userInfo', 'user', 'signature', {str}))
 
         if not sec_uid:
             raise ExtractorError(
@@ -1177,7 +1181,7 @@ class TikTokUserIE(TikTokBaseIE):
                 'from a video posted by this user, try using "tiktokuser:channel_id" as the '
                 'input URL (replacing `channel_id` with its actual value)', expected=True)
 
-        return self.playlist_result(self._entries(sec_uid, user_name, fail_early), sec_uid, user_name)
+        return self.playlist_result(self._entries(sec_uid, user_name, fail_early), sec_uid, user_name, signature)
 
 
 class TikTokBaseListIE(TikTokBaseIE):  # XXX: Conventionally, base classes should end with BaseIE/InfoExtractor
