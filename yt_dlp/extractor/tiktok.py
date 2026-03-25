6bfede959836608113e3b7c58cb2b377224816d8
deadmeu <dmu@duck.com>
2026-03-25 21:32:15 +1000

    [ie/tiktok:user] Extract user nickname as playlist channel

--- yt_dlp/extractor/tiktok.py
+++ yt_dlp/extractor/tiktok.py
@@ -1002,6 +1002,7 @@ class TikTokUserIE(TikTokBaseIE):
         'url': 'https://tiktok.com/@corgibobaa?lang=en',
         'playlist_mincount': 45,
         'info_dict': {
+            'channel': str,
             'id': 'MS4wLjABAAAAepiJKgwWhulvCpSuUVsp7sgVVsFJbbNaLeQ6OQ0oAJERGDUIXhb2yxxHZedsItgT',
             'title': 'corgibobaa',
             'description': str,
@@ -1012,6 +1013,7 @@ class TikTokUserIE(TikTokBaseIE):
         'url': 'https://www.tiktok.com/@6820838815978423302',
         'playlist_mincount': 5,
         'info_dict': {
+            'channel': str,
             'id': 'MS4wLjABAAAA0tF1nBwQVVMyrGu3CqttkNgM68Do1OXUFuCY0CRQk8fEtSVDj89HqoqvbSTmUP2W',
             'title': '6820838815978423302',
             'description': str,
@@ -1022,6 +1024,7 @@ class TikTokUserIE(TikTokBaseIE):
         'url': 'https://www.tiktok.com/@meme',
         'playlist_mincount': 593,
         'info_dict': {
+            'channel': str,
             'id': 'MS4wLjABAAAAiKfaDWeCsT3IHwY77zqWGtVRIy9v4ws1HbVi7auP1Vx7dJysU_hc5yRiGywojRD6',
             'title': 'meme',
             'description': str,
@@ -1150,7 +1153,7 @@ class TikTokUserIE(TikTokBaseIE):
         return None
 
     def _real_extract(self, url):
-        user_name, sec_uid, signature = self._match_id(url), None, None
+        user_name, sec_uid, signature, nickname = self._match_id(url), None, None, None
         if re.fullmatch(r'MS4wLjABAAAA[\w-]{64}', user_name):
             user_name, sec_uid = None, user_name
             fail_early = True
@@ -1174,6 +1177,7 @@ class TikTokUserIE(TikTokBaseIE):
                 sec_uid = self._extract_sec_uid_from_embed(user_name)
                 fail_early = False
             signature = traverse_obj(detail, ('userInfo', 'user', 'signature', {str}))
+            nickname = traverse_obj(detail, ('userInfo', 'user', 'nickname', {str}))
 
         if not sec_uid:
             raise ExtractorError(
@@ -1181,7 +1185,7 @@ class TikTokUserIE(TikTokBaseIE):
                 'from a video posted by this user, try using "tiktokuser:channel_id" as the '
                 'input URL (replacing `channel_id` with its actual value)', expected=True)
 
-        return self.playlist_result(self._entries(sec_uid, user_name, fail_early), sec_uid, user_name, signature)
+        return self.playlist_result(self._entries(sec_uid, user_name, fail_early), sec_uid, user_name, signature, channel=nickname)
 
 
 class TikTokBaseListIE(TikTokBaseIE):  # XXX: Conventionally, base classes should end with BaseIE/InfoExtractor
