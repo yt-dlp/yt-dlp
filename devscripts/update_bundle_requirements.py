1d489f0d4bec3eff75ebc0ba46ee9989c82d758b
bashonly <bashonly@protonmail.com>
2026-03-27 18:12:23 -0500

    [devscripts] `update_bundle_requirements`: more cleanup
    Authored by: bashonly

--- devscripts/update_bundle_requirements.py
+++ devscripts/update_bundle_requirements.py
@@ -109,59 +109,24 @@ INSTALL_DEPS_TARGETS = {
 
 
 @dataclasses.dataclass
-class GroupTarget:
-    platform: str
-    version: str
-    install_args: list[str] = dataclasses.field(default_factory=list)
-
-
-BUILD_GROUP_TARGETS = {
-    'pypi-build': GroupTarget(
-        platform='linux',
-        version=LINUX_GNU_PYTHON_VERSION,
-    ),
-    'win-x64-build': GroupTarget(
-        platform='x86_64-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        install_args=['--cherry-pick', 'pip'],
-    ),
-    'win-x86-build': GroupTarget(
-        platform='i686-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        install_args=['--cherry-pick', 'pip'],
-    ),
-    'win-arm64-build': GroupTarget(
-        platform='aarch64-pc-windows-msvc',
-        version=WINDOWS_ARM64_PYTHON_VERSION,
-        install_args=['--cherry-pick', 'pip'],
-    ),
-    'macos-build': GroupTarget(
-        platform='macos',
-        version=MACOS_PYTHON_VERSION,
-        install_args=['--cherry-pick', 'pip'],
-    ),
-}
-
-
-@dataclasses.dataclass
-class PyinstallerTarget:
+class PyInstallerTarget:
     platform: str
     version: str
     asset_tag: str
 
 
 PYINSTALLER_BUILDS_TARGETS = {
-    'win-x64-pyinstaller': PyinstallerTarget(
+    'win-x64-pyinstaller': PyInstallerTarget(
         platform='x86_64-pc-windows-msvc',
         version=WINDOWS_INTEL_PYTHON_VERSION,
         asset_tag='win_amd64',
     ),
-    'win-x86-pyinstaller': PyinstallerTarget(
+    'win-x86-pyinstaller': PyInstallerTarget(
         platform='i686-pc-windows-msvc',
         version=WINDOWS_INTEL_PYTHON_VERSION,
         asset_tag='win32',
     ),
-    'win-arm64-pyinstaller': PyinstallerTarget(
+    'win-arm64-pyinstaller': PyInstallerTarget(
         platform='aarch64-pc-windows-msvc',
         version=WINDOWS_ARM64_PYTHON_VERSION,
         asset_tag='win_arm64',
@@ -178,7 +143,13 @@ PYINSTALLER_BUILDS_TMPL = '''\
 PYINSTALLER_VERSION_RE = re.compile(r'pyinstaller-(?P<version>[0-9]+\.[0-9]+\.[0-9]+)-')
 
 
-def run_pip_compile(python_platform, python_version, requirements_input_path, *args):
+def write_requirements_input(filepath: pathlib.Path, *args: str) -> None:
+    filepath.write_text(run_process(
+        sys.executable, '-m', 'devscripts.install_deps',
+        '--omit-default', '--print', *args).stdout)
+
+
+def run_pip_compile(python_platform: str, python_version: str, requirements_input_path: pathlib.Path, *args: str) -> str:
     return run_process(
         'uv', 'pip', 'compile',
         '--upgrade',
@@ -210,23 +181,25 @@ def main():
 
     for target_suffix, target in INSTALL_DEPS_TARGETS.items():
         requirements_input_path = REQUIREMENTS_PATH / INPUT_TMPL.format(target_suffix)
-        requirements_input_path.write_text(run_process(
-            sys.executable, '-m', 'devscripts.install_deps', '--omit-default', '--print',
+        write_requirements_input(
+            requirements_input_path,
             *itertools.chain.from_iterable(itertools.product(['--include-extra'], target.extras)),
-            *itertools.chain.from_iterable(itertools.product(['--include-group'], target.groups)),
-        ).stdout)
+            *itertools.chain.from_iterable(itertools.product(['--include-group'], target.groups)))
         run_pip_compile(
             target.platform, target.version, requirements_input_path, *target.compile_args,
             f'--output-file={REQUIREMENTS_PATH / OUTPUT_TMPL.format(target_suffix)}')
 
-    for target_suffix, target in BUILD_GROUP_TARGETS.items():
-        requirements_input_path = REQUIREMENTS_PATH / INPUT_TMPL.format(target_suffix)
-        requirements_input_path.write_text(run_process(
-            sys.executable, '-m', 'devscripts.install_deps',
-            '--omit-default', '--print', '--include-group', 'build', *target.install_args).stdout)
-        run_pip_compile(
-            target.platform, target.version, requirements_input_path,
-            f'--output-file={REQUIREMENTS_PATH / OUTPUT_TMPL.format(target_suffix)}')
+    pypi_input_path = REQUIREMENTS_PATH / INPUT_TMPL.format('pypi-build')
+    write_requirements_input(pypi_input_path, '--include-group', 'build')
+    run_pip_compile(
+        'linux', LINUX_GNU_PYTHON_VERSION, pypi_input_path,
+        f'--output-file={REQUIREMENTS_PATH / OUTPUT_TMPL.format("pypi-build")}')
+
+    pip_input_path = REQUIREMENTS_PATH / INPUT_TMPL.format('pip')
+    write_requirements_input(pip_input_path, '--include-group', 'build', '--cherry-pick', 'pip')
+    run_pip_compile(
+        'windows', WINDOWS_INTEL_PYTHON_VERSION, pip_input_path,
+        f'--output-file={REQUIREMENTS_PATH / OUTPUT_TMPL.format("pip")}')
 
 
 if __name__ == '__main__':
