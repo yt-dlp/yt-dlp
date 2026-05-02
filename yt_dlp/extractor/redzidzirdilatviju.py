6d7e0b7a95b6bcee7bea937b5b87d2e8ffa4dfc9
Marta Viana Machado <marta.machado@tecnico.ulisboa.pt>
2026-05-02 18:43:40 +0100

    Fix #15886: Make requested changes

--- yt_dlp/extractor/redzidzirdilatviju.py
+++ yt_dlp/extractor/redzidzirdilatviju.py
@@ -1,5 +1,4 @@
 from .common import InfoExtractor
-from ..utils import ExtractorError
 
 
 class RedzidzirdilatvijuIE(InfoExtractor):
@@ -43,41 +42,21 @@ class RedzidzirdilatvijuIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        # Try to get metadata from site API first
-        video_filename = None
-        title = None
-
-        api_data = self._download_json(
-            'https://www.redzidzirdilatviju.lv/index',
-            video_id,
+        doc = self._download_json(
+            'https://www.redzidzirdilatviju.lv/index', video_id,
             query={
                 'wt': 'json',
                 'q': f'ss_item_type:(movie OR photo OR sound) AND ss_item_entity_id:{video_id}',
                 'rows': '1'
-            },
-            fatal=False
-        )
+            })['response']['docs'][0]
+        video_filename = doc['ss_movie$field_video$file$name']
 
-        if api_data:
-            docs = api_data.get('response', {}).get('docs', [])
-            if docs:
-                doc = docs[0]
-                video_filename = doc.get('ss_movie$field_video$file$name')
-                if video_filename and not video_filename.endswith('.mp4'):
-                    video_filename += '.mp4'
-                title = doc.get('ts_movie$field_title') or doc.get('tm_movie$field_title')
-
-
-        # Build m3u8 URL
-        m3u8_url = f'https://filmas.arhivi.lv:30443/s/{video_filename}/playlist.m3u8'
-
-        formats = self._extract_m3u8_formats(m3u8_url, video_id, 'mp4', fatal=False)
-
-        if not formats:
-            raise ExtractorError('No video formats found', expected=True)
+        if not video_filename.endswith('.mp4'):
+            video_filename += '.mp4'
 
         return {
             'id': video_id,
-            'title': title,
-            'formats': formats,
+            'title': doc.get('ts_movie$field_title') or doc.get('tm_movie$field_title'),
+            'formats': self._extract_m3u8_formats(
+                f'https://filmas.arhivi.lv:30443/s/{video_filename}/playlist.m3u8', video_id, 'mp4'),
         }
