840fd7b576e4466bcde7a9afb2dfa1a72ab0a317
bashonly <bashonly@protonmail.com>
2026-05-23 23:44:25 -0500

    `update_reqs`: oops
    Authored by: bashonly

--- devscripts/update_requirements.py
+++ devscripts/update_requirements.py
@@ -649,7 +649,7 @@ def update_requirements(
     modify_and_write_pyproject(pyproject_text, table_name=EXTRAS_TABLE, table=extras)
 
     # Generate/upgrade final lockfile that includes pinned extras
-    print(f'Running: uv lock', file=sys.stderr)
+    print('Running: uv lock', file=sys.stderr)
     run_process('uv', 'lock', env=env)
 
     # Export bundle requirements; any updates to these are already recorded w/ uv.lock package diff
