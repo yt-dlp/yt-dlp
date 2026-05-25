8caac5bedb38c190891b366eb21e64f74abeffed
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2026-05-25 17:14:30 +0800

    tests that might work

--- yt_dlp/extractor/monstercat.py
+++ yt_dlp/extractor/monstercat.py
@@ -12,19 +12,43 @@ from ..utils.traversal import find_element, traverse_obj
 
 
 class MonstercatIE(InfoExtractor):
-    _VALID_URL = r'https?://www\.monstercat\.com/release/(?P<id>\d+|MC[A-Z]+\d+)'
-    _TESTS = [{
-        'url': 'https://www.monstercat.com/release/742779548009',
-        'playlist_count': 20,
-        'info_dict': {
-            'title': 'The Secret Language of Trees',
-            'id': '742779548009',
-            'thumbnail': 'https://www.monstercat.com/release/742779548009/cover',
-            'release_date': '20230711',
-            'album': 'The Secret Language of Trees',
-            'album_artists': ['BT'],
-        },
-    }]
+    _VALID_URL = r'https?://www\.monstercat\.com/release/(?P<id>\d{12}|MC[A-Z]+\d+)'
+    _TESTS = [
+        {
+            'url': 'https://www.monstercat.com/release/742779548009',
+            'playlist_count': 20,
+            'info_dict': {
+                'title': 'The Secret Language of Trees',
+                'id': '742779548009',
+                'thumbnail': 'https://www.monstercat.com/release/742779548009/cover',
+                'release_date': '20230711',
+                'album': 'The Secret Language of Trees',
+                'album_artists': ['BT'],
+            },
+        }, {
+            'url': 'https://www.monstercat.com/release/MCRAB001',
+            'playlist_count': 1,
+            'info_dict': {
+                'title': 'Crab Rave',
+                'id': 'MCRAB001',
+                'thumbnail': 'https://www.monstercat.com/release/MCRAB001/cover',
+                'release_date': '20180401',
+                'album': 'Crab Rave',
+                'album_artists': ['Noisestorm'],
+            },
+        }, {
+            'url': 'https://www.monstercat.com/release/MCEP209',
+            'playlist_count': 5,
+            'info_dict': {
+                'title': 'Somewhere New',
+                'id': 'MCEP209',
+                'thumbnail': 'https://www.monstercat.com/release/MCEP209/cover',
+                'release_date': '20210415',
+                'album': 'Somewhere New',
+                'album_artists': ['Bad Computer'],
+            },
+        }
+    ]
 
     def _extract_tracks(self, table, album_meta):
         for td in re.findall(r'<tr[^<]*>((?:(?!</tr>)[\w\W])+)', table):  # regex by chatgpt due to lack of get_elements_by_tag
