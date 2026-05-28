eab16a1b86f6598510e9e08d2731f626faeb9236
FraFraFra-LongD <ahsokaskyguy30@gmail.com>
2026-05-28 13:36:59 +0200

    [chore] revert my format

--- yt_dlp/extractor/thisvid.py
+++ yt_dlp/extractor/thisvid.py
@@ -2,6 +2,7 @@ import itertools
 import re
 import urllib.parse
 
+from .common import InfoExtractor
 from ..utils import (
     clean_html,
     get_element_by_class,
@@ -9,7 +10,6 @@ from ..utils import (
     url_or_none,
     urljoin,
 )
-from .common import InfoExtractor
 
 
 class ThisVidIE(InfoExtractor):
