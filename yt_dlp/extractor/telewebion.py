f205a91eef0b6a3b0daebee7479886801e33e7ae
BOplaid <146466352+BOplaid@users.noreply.github.com>
2026-06-18 00:51:38 +0330

    not saying it's working doesn't mean it ain't working
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/telewebion.py
+++ yt_dlp/extractor/telewebion.py
@@ -14,7 +14,6 @@ def _fmt_url(url):
 
 
 class TelewebionIE(InfoExtractor):
-    _WORKING = True
     _VALID_URL = r'https?://(?:www\.)?telewebion\.ir/episode/(?P<id>(?:0x[a-fA-F\d]+|\d+))'
     _TESTS = [{
         'url': 'http://www.telewebion.ir/episode/0x1b3139c/',
