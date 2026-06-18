ef769df5c1024c525ba7172e070d98b5fef185f9
bashonly <bashonly@protonmail.com>
2026-06-18 23:16:53 +0000

    [ie/youtube:tab] Fix `availability` extraction
    Authored by: bashonly

--- yt_dlp/extractor/youtube/_tab.py
+++ yt_dlp/extractor/youtube/_tab.py
@@ -862,13 +862,13 @@ class YoutubeTabBaseInfoExtractor(YoutubeBaseInfoExtractor):
     def _extract_availability(self, data):
         """
         Gets the availability of a given playlist/tab.
-        Note: Unless YouTube tells us explicitly, we do not assume it is public
         @param data: response
         """
-        sidebar_renderer = self._extract_sidebar_info_renderer(data, 'playlistSidebarPrimaryInfoRenderer') or {}
-        playlist_header_renderer = traverse_obj(data, ('header', 'playlistHeaderRenderer')) or {}
+        sidebar_renderer = self._extract_sidebar_info_renderer(data, ('playlistSidebarPrimaryInfoRenderer', {dict})) or {}
+        playlist_header_renderer = traverse_obj(data, ('header', 'playlistHeaderRenderer', {dict})) or {}
         player_header_privacy = playlist_header_renderer.get('privacy')
 
+        # NB: Badges are currently not extracted from playlists/tabs
         badges = self._extract_badges(traverse_obj(sidebar_renderer, 'badges'))
 
         # Personal playlists, when authenticated, have a dropdown visibility selector instead of a badge
@@ -878,27 +878,36 @@ class YoutubeTabBaseInfoExtractor(YoutubeBaseInfoExtractor):
              lambda _, v: v['privacyDropdownItemRenderer']['isSelected'], 'privacyDropdownItemRenderer', 'icon', 'iconType'),
             expected_type=str)
 
-        microformats_is_unlisted = traverse_obj(
-            data, ('microformat', 'microformatDataRenderer', 'unlisted'), expected_type=bool)
+        microformat_data_renderer = traverse_obj(data, ('microformat', 'microformatDataRenderer', {dict}))
 
-        return (
-            'public' if (
-                self._has_badge(badges, BadgeType.AVAILABILITY_PUBLIC)
-                or player_header_privacy == 'PUBLIC'
-                or privacy_setting_icon == 'PRIVACY_PUBLIC')
-            else self._availability(
-                is_private=(
-                    self._has_badge(badges, BadgeType.AVAILABILITY_PRIVATE)
-                    or player_header_privacy == 'PRIVATE' if player_header_privacy is not None
-                    else privacy_setting_icon == 'PRIVACY_PRIVATE' if privacy_setting_icon is not None else None),
-                is_unlisted=(
-                    self._has_badge(badges, BadgeType.AVAILABILITY_UNLISTED)
-                    or player_header_privacy == 'UNLISTED' if player_header_privacy is not None
-                    else privacy_setting_icon == 'PRIVACY_UNLISTED' if privacy_setting_icon is not None
-                    else microformats_is_unlisted if microformats_is_unlisted is not None else None),
-                needs_subscription=self._has_badge(badges, BadgeType.AVAILABILITY_SUBSCRIPTION) or None,
-                needs_premium=self._has_badge(badges, BadgeType.AVAILABILITY_PREMIUM) or None,
-                needs_auth=False))
+        if availability := self._availability(
+            # Private playlists are only viewable by the creator
+            is_private=(
+                self._has_badge(badges, BadgeType.AVAILABILITY_PRIVATE)
+                or player_header_privacy == 'PRIVATE' if player_header_privacy is not None
+                else privacy_setting_icon == 'PRIVACY_PRIVATE' if privacy_setting_icon is not None else None),
+            is_unlisted=(
+                self._has_badge(badges, BadgeType.AVAILABILITY_UNLISTED)
+                or player_header_privacy == 'UNLISTED' if player_header_privacy is not None
+                else privacy_setting_icon == 'PRIVACY_UNLISTED' if privacy_setting_icon is not None
+                else traverse_obj(microformat_data_renderer, ('unlisted', {bool}))),
+            # It is unlikely that members-only or premium-only playlists/tabs exist
+            needs_subscription=self._has_badge(badges, BadgeType.AVAILABILITY_SUBSCRIPTION) or None,
+            needs_premium=self._has_badge(badges, BadgeType.AVAILABILITY_PREMIUM) or None,
+            needs_auth=False,
+        ):
+            return availability
+
+        if (
+            self._has_badge(badges, BadgeType.AVAILABILITY_PUBLIC)
+            or player_header_privacy == 'PUBLIC'
+            or privacy_setting_icon == 'PRIVACY_PUBLIC'
+            or traverse_obj(microformat_data_renderer, ('noindex', {bool})) is False
+        ):
+            return 'public'
+
+        return None
+
 
     @staticmethod
     def _extract_sidebar_info_renderer(data, info_renderer, expected_type=dict):
