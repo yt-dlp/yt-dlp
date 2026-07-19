f9d3afdaedecdfed54a09a495f4616c6cb036d5e
cleanremote <27508072+cleanremote@users.noreply.github.com>
2026-07-18 18:10:42 -0700

    fix

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -106,7 +106,6 @@ from .archiveorg import (
 )
 from .arcpublishing import ArcPublishingIE
 from .ard import (
-    ARDIE,
     ARDAudiothekIE,
     ARDAudiothekPlaylistIE,
     ARDBetaMediathekIE,
@@ -1720,7 +1719,10 @@ from .shahid import (
 from .sharepoint import SharePointIE
 from .shemaroome import ShemarooMeIE
 from .shiey import ShieyIE
-from .showroomlive import ShowRoomLiveIE
+from .showroomlive import (
+    ShowRoomLiveIE,
+    ShowRoomVodIE,
+)
 from .sibnet import SibnetEmbedIE
 from .simplecast import (
     SimplecastEpisodeIE,
