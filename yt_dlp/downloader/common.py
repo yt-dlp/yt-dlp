916c916136a583e1aa9ae51ee542a0b9de3a2917
gamer191 <83270075+gamer191@users.noreply.github.com>
2026-07-20 01:00:17 +1000

    Simplify downloader.try_utime
    Note that this function does not have any tests. In my opinion this change is an obvious enough cleanup that it probably doesn't need testing

--- yt_dlp/downloader/common.py
+++ yt_dlp/downloader/common.py
@@ -272,12 +272,9 @@ class FileDownloader:
             return
         if not os.path.isfile(filename):
             return
-        timestr = last_modified_hdr
-        if timestr is None:
-            return
-        filetime = timeconvert(timestr)
+        filetime = timeconvert(last_modified_hdr)
         if filetime is None:
-            return filetime
+            return
         # Ignore obviously invalid dates
         if filetime == 0:
             return
