3bc60c515071191f412303567e4580b45a6cc522
E-Kretén <155096014+hun0r@users.noreply.github.com>
2026-08-01 17:06:16 +0200

    Removed debug line
    removed a line that raised the full error instead of a user readable one that i needed to debug the code

--- yt_dlp/extractor/youtube/_tab.py
+++ yt_dlp/extractor/youtube/_tab.py
@@ -2262,7 +2262,6 @@ class YoutubeTabIE(YoutubeTabBaseInfoExtractor):
                     try:
                         return self.playlist_result([self._real_extract(i) for i in yt_music_MPADUC_fix(self, item_id)])
                     except Exception as e:
-                        raise e
                         raise ExtractorError('Failed to resolve album to playlist')
                 return self.url_result(murl, YoutubeTabIE)
             elif mobj['channel_type'] == 'browse':  # Youtube music /browse/ should be changed to /channel/
