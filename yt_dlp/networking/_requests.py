07124382c0fe89951764cb40ce408165a45ae605
doe1080 <98906116+doe1080@users.noreply.github.com>
2026-08-04 12:33:48 +0900

    [cleanup] Remove obsolete Python compatibility code

--- yt_dlp/networking/_requests.py
+++ yt_dlp/networking/_requests.py
@@ -123,7 +123,6 @@ class RequestsResponseAdapter(Response):
         # Work around issue with `.read(amt)` then `.read()`
         # See: https://github.com/urllib3/urllib3/issues/3636
         if amt is None:
-            # compat: py3.9: Python 3.9 preallocates the whole read buffer, read in chunks
             read_chunk = functools.partial(self.fp.read, 1 << 20, decode_content=True)
             return b''.join(iter(read_chunk, b''))
         # Interact with urllib3 response directly.
