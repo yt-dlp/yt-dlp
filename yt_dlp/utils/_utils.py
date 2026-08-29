3d062c86eab289a4dfd5b4f8c1992dc2a96e8c78
doe1080 <98906116+doe1080@users.noreply.github.com>
2026-08-30 07:12:11 +0900

    [docs] Fix `Namespace` documentation (#17324)
    Authored by: doe1080

--- yt_dlp/utils/_utils.py
+++ yt_dlp/utils/_utils.py
@@ -5083,7 +5083,7 @@ class function_with_repr:
 
 
 class Namespace(types.SimpleNamespace):
-    """Immutable namespace"""
+    """SimpleNamespace iterable over attribute values"""
 
     def __iter__(self):
         return iter(self.__dict__.values())
